a5d0d18d72d501423a4b9ac1f979de7fcd6b74c3
Alex Weiksnar <alex@zooniverse.org>
2014-09-30 12:43:03 -0500

    add react-router and a sample component

--- packages/Zooniverse-React-Components/README.md
+++ packages/Zooniverse-React-Components/README.md
@@ -2,3 +2,7 @@ Zooniverse-React-Components
 ===========================
 
 What the repo name says
+
+`npm i`
+
+`gulp` or `npm start`
--- packages/Zooniverse-React-Components/gulpfile.js
+++ packages/Zooniverse-React-Components/gulpfile.js
@@ -1,12 +1,10 @@
 /*
-  gulpfile.js
-  ===========
+  gulpfile
 
   * browserify setup and /gulp dir organization borrowed from from:
     https://github.com/dqdinh/react-flux-coffeescript-browserify-gulp-demo/tree/master/gulp
 
   * add new tasks under gulp/tasks.
-
 */
 
 require('./gulp');
