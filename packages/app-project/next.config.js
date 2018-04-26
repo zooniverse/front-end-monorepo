c7aa406b2b04081141343051058ec58a37cd7107
Roger Hutchings <roger@hutchin.gs>
2018-04-26 17:47:03 +0100

    Add standard js linting

--- packages/app-project/next.config.js
+++ packages/app-project/next.config.js
@@ -4,7 +4,12 @@ module.exports = {
     config.node = {
       fs: 'empty'
     }
-
+    config.module.rules.push({
+      enforce: 'pre',
+      test: /\.js?$/,
+      loader: 'standard-loader',
+      exclude: /(node_modules)/,
+    })
     return config
   }
 }
