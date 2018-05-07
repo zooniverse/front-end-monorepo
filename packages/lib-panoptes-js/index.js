a07f6f554971dfb8ec3c5acef54a0f46e45ef35b
Sarah Allen <sarah@zooniverse.org>
2018-05-07 09:43:00 -0500

    Base functions. Start of projects helpers

---
+++ packages/lib-panoptes-js/index.js
@@ -0,0 +1,5 @@
+const panoptes = require('./panoptes');
+
+module.exports({
+  panoptes
+});
---
+++ packages/lib-panoptes-js/panoptes.js
@@ -0,0 +1,69 @@
+const superagent = require('superagent');
+const config = require('./config');
+
+function handleErrors(error) {
+  if (console) console.error(error);
+
+  return error;
+}
+
+// TODO: Write auth client
+function get(endpoint, query, host) {
+  const apiHost = host || config.host;
+  const request = superagent.get(`${apiHost}${endpoint}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/vnd.api+json; version=1')
+  // .set('Authorization', apiClient.headers.Authorization);
+  if (query && query.length > 0) {
+    query.forEach((object) => {
+      if (typeof object === 'object') request.query(obj);
+      if (typeof object !== 'object') throw new Error('Query must be an object');
+    });
+  } else {
+    throw new Error('Queries must be in an array or array is empty');
+  }
+  return request.then(response => response).catch(handleErrors);
+}
+
+function post(endpoint, data, host) {
+  const apiHost = host || config.host;
+
+  return superagent.post(`${apiHost}${endpoint}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/vnd.api+json; version=1')    
+    // .set('Authorization', apiClient.headers.Authorization)
+    .send(data)
+    .then(response => response).catch(handleErrors);
+}
+
+function put(endpoint, data, host) {
+  const apiHost = host || config.host;
+
+  return superagent.put(`${apiHost}${endpoint}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/vnd.api+json; version=1')    
+    // .set('Authorization', apiClient.headers.Authorization)
+    .send(data)
+    .then(response => response).catch(handleErrors);
+}
+
+function del(endpoint, host) {
+  const apiHost = host || config.host;
+
+  return superagent.delete(`${apiHost}${endpoint}`)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/vnd.api+json; version=1')    
+    // .set('Authorization', apiClient.headers.Authorization)
+    .then(response => response).catch(handleErrors);
+}
+
+const requests = {
+  get,
+  put,
+  post,
+  del
+}
+
+window.panoptesAPI = requests;
+
+module.exports(requests);
