e17bb5757c80fa2ff13df110b5c1e6abfad779a0
Roger Hutchings <roger@hutchin.gs>
2018-05-31 13:06:58 +0100

    Create sample compose viewers function

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewerComponent/getViewerComponent.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewerComponent/getViewerComponent.spec.js
@@ -8,15 +8,8 @@ describe('Helper > getViewerComponent', function () {
     expect(result).to.equal(null)
   })
 
-  it('should return a viewer for PNGs', function () {
+  it('should return a viewer for images', function () {
     const result = shallow(getViewerComponent({ 'image/png': 'baz' }))
     result.length.should.equal(1)
-  })
-
-  it('should return a viewer for JPEGs', function () {
-    const result1 = shallow(getViewerComponent({ 'image/jpg': 'baz' }))
-    result1.length.should.equal(1)
-    const result2 = shallow(getViewerComponent({ 'image/jpeg': 'baz' }))
-    result2.length.should.equal(1)
   })
 })
