03e8c6df209ffa5ec22e6ed790aac18df60662b6
Roger Hutchings <roger@hutchin.gs>
2018-06-01 10:50:37 +0100

    Make components function declarations

--- packages/app-project/components/nav.js
+++ packages/app-project/components/nav.js
@@ -1,40 +1,42 @@
 import { Link } from '../config/routes'
 import React from 'react'
 
-const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link prefetch href='/'>
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link
-          prefetch
-          route='project'
-          params={{
-            owner: 'emammal',
-            project: 'emammal'
-          }}
-        >
-          <a>Emammal</a>
-        </Link>
-      </li>
-      <li>
-        <Link
-          prefetch
-          route='project'
-          params={{
-            owner: 'dwright04',
-            project: 'supernova-hunters'
-          }}
-        >
-          <a>Supernova Hunters</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+function Nav () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link prefetch href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link
+            prefetch
+            route='project'
+            params={{
+              owner: 'emammal',
+              project: 'emammal'
+            }}
+          >
+            <a>Emammal</a>
+          </Link>
+        </li>
+        <li>
+          <Link
+            prefetch
+            route='project'
+            params={{
+              owner: 'dwright04',
+              project: 'supernova-hunters'
+            }}
+          >
+            <a>Supernova Hunters</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 export default Nav
