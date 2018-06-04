252612ddbb022c8848e11c6f41efc8b4abe1b24d
srallen <srallen@users.noreply.github.com>
2018-06-04 07:33:09 -0500

    Add project mocks to lib export (#32)

--- packages/lib-panoptes-js/src/index.js
+++ packages/lib-panoptes-js/src/index.js
@@ -1,9 +1,10 @@
 const { config } = require('./config')
 const panoptes = require('./panoptes')
-const projects = require('./resources/projects')
+const { projects, projectMocks } = require('./resources/projects')
 
 module.exports = {
   config,
   panoptes,
-  projects
+  projects,
+  projectMocks
 }
