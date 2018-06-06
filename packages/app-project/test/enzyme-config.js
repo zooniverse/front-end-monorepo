32a1ea059c4c8318563c33587f6d387a85209936
Roger Hutchings <roger@hutchin.gs>
2018-06-06 21:26:40 +0100

    Enable lifecycle methods in tests

--- packages/app-project/test/enzyme-config.js
+++ packages/app-project/test/enzyme-config.js
@@ -2,6 +2,5 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({
-  adapter: new Adapter(),
-  // disableLifecycleMethods: true
+  adapter: new Adapter()
 })
