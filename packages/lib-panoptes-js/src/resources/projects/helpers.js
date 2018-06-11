e2a4fda9ff278a30537fd784cdb813313d6c840c
Sarah Allen <sarah@zooniverse.org>
2018-06-11 09:26:05 -0500

    Commit helpers file and tests

---
+++ packages/lib-panoptes-js/src/resources/projects/helpers.js
@@ -0,0 +1,20 @@
+const URL = require('url-parse')
+
+function getProjectSlugFromURL(urlArg) {
+  const parsed = new URL(urlArg)
+  const [projectPath, owner, project] = parsed.pathname.split('/')
+    .filter(Boolean)
+
+  if (projectPath === 'projects' && owner && project) {
+    return `${owner}/${project}`
+  } else {
+    throw new Error(`Couldn't match a slug from ${urlArg}`)
+  }
+}
+
+function handleError(error) {
+  if (console && process.env.NODE_ENV !== 'test') console.error(error)
+  return Promise.reject(error)
+}
+
+module.exports = { getProjectSlugFromURL, handleError }
