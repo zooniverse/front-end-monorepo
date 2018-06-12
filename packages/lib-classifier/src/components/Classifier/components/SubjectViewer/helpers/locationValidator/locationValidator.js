7bfe0e8c7520864a1fb77f86907ffe84f6b1574c
Roger Hutchings <roger@hutchin.gs>
2018-06-12 13:17:43 +0100

    Add propType for subject locations

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/locationValidator/locationValidator.js
@@ -0,0 +1,17 @@
+import { isUri } from 'valid-url'
+
+const mimeTypeRe = /^[-\w+\.]+\/[-\w+\.]+$/
+
+function locationValidator (propValue, key, componentName, location, propFullName) {
+    const subjectLocation = propValue[key]
+    const mimeType = Object.keys(subjectLocation)[0]
+    const uri = Object.values(subjectLocation)[0]
+
+    if (!mimeTypeRe.test(mimeType) || !isUri(uri)) {
+      return new Error(
+        'Invalid prop `' + propFullName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.'
+      );
+    }
+  }
+export default locationValidator
