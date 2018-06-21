9e7819ee8fb3ef580b6665ccb528164a57c9348a
Roger Hutchings <roger@hutchin.gs>
2018-06-21 11:03:52 +0100

    Disable no-new-func for environment detection helpers

--- packages/lib-panoptes-js/src/helpers/index.js
+++ packages/lib-panoptes-js/src/helpers/index.js
@@ -1,8 +1,10 @@
 // Borrowed from https://stackoverflow.com/questions/17575790/environment-detection-node-js-or-browser top answer
 // Testing if in browser or node environments
 
+// eslint-disable-next-line no-new-func
 const isBrowser = new Function('try {return this===window;}catch(e){ return false;}')
 
+// eslint-disable-next-line no-new-func
 const isNode = new Function('try {return this===global;}catch(e){return false;}')
 
 module.exports = { isBrowser, isNode }
