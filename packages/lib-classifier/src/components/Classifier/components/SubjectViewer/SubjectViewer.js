d271af8ed5df437b49a4ac8446f787e261496296
Roger Hutchings <roger@hutchin.gs>
2018-07-03 15:50:19 +0100

    Add helper for deriving viewer type (as used in Layouts)
    - Also add basic test spec for Toolbar
    
    add some tests, a helper for deriving viewer type (similar to layouts)'

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.js
@@ -2,34 +2,46 @@ import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SingleImageViewer from './components/SingleImageViewer'
-
-const viewers = {
-  singleImage: SingleImageViewer
-}
+import asyncStates from '../../../../helpers/asyncStates'
+import getViewer from './helpers/getViewer'
 
 function storeMapper (stores) {
-  const subject = stores.classifierStore.subjects.active
+  const subjects = stores.classifierStore.subjects
   const workflow = stores.classifierStore.workflows.active
-  return { subject, workflow }
+  return {
+    subjects,
+    workflow
+  }
 }
 
 @inject(storeMapper)
 @observer
 class SubjectViewer extends React.Component {
   render () {
-    console.info(this.props.subject)
+    const { subjects } = this.props
+    const { loadingState } = subjects
 
-    if (!this.props.subject) {
+    if (loadingState === asyncStates.error) {
+      console.error('There was an error loading the subjects')
+      return null
+    }
+
+    if (loadingState !== asyncStates.success) {
       return (<div>Loading</div>)
     }
 
-    const Viewer = viewers[this.props.subject.viewer]
-    return <Viewer subject={this.props.subject} />
+    const Viewer = getViewer(subjects.active.viewer)
+    return <Viewer subject={subjects.active} />
   }
 }
 
 SubjectViewer.propTypes = {
-  subject: PropTypes.object,
+  subjects: PropTypes.shape({
+    loadingState: PropTypes.oneOf(asyncStates.values),
+    active: PropTypes.shape({
+      viewer: PropTypes.string
+    })
+  }),
   workflow: PropTypes.object
 }
 
