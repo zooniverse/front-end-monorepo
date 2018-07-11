385543a9de2cdc5970e60e9ece68cfabf82012e1
Roger Hutchings <roger@hutchin.gs>
2018-07-11 11:05:31 +0100

    Add behaviour for fullscreen button

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/FullscreenButton/FullscreenButton.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/FullscreenButton/FullscreenButton.spec.js
@@ -1,5 +1,7 @@
 import { shallow, mount } from 'enzyme'
 import React from 'react'
+import sinon from 'sinon'
+
 import FullscreenButton from './FullscreenButton'
 
 describe('Component > FullscreenButton', function () {
@@ -10,5 +12,29 @@ describe('Component > FullscreenButton', function () {
   it('should have an ARIA label', function () {
     const wrapper = mount(<FullscreenButton />)
     expect(wrapper.find('button').prop('aria-label')).to.equal('View subject in full screen mode')
+  })
+
+  it('should call the enableFullscreen prop function on click if fullscreen is not active', function () {
+    const spy = sinon.spy()
+    const wrapper = mount(
+      <FullscreenButton
+        fullscreen={false}
+        enableFullscreen={spy}
+      />
+    )
+    wrapper.find('button').simulate('click')
+    expect(spy.called).to.equal(true)
+  })
+
+  it('should call the disableFullscreen prop function on click if fullscreen is active', function () {
+    const spy = sinon.spy()
+    const wrapper = mount(
+      <FullscreenButton
+        fullscreen
+        disableFullscreen={spy}
+      />
+    )
+    wrapper.find('button').simulate('click')
+    expect(spy.called).to.equal(true)
   })
 })
--- packages/lib-classifier/src/store/ClassifierStore.js
+++ packages/lib-classifier/src/store/ClassifierStore.js
@@ -4,17 +4,22 @@ import subjectViewers from '../helpers/subjectViewers'
 
 const Classifier = types
   .model('Classifier', {
-    layout: types.optional(types.enumeration('layout', layouts.values), layouts.default)
+    layout: types.optional(types.enumeration('layout', layouts.values), layouts.default),
+    fullscreen: types.optional(types.boolean, false)
   })
 
-  .actions(self => {
-    function setLayout (layout = layouts.DefaultLayout) {
+  .actions(self => ({
+    enableFullscreen () {
+      self.fullscreen = true
+    },
+
+    disableFullscreen () {
+      self.fullscreen = false
+    },
+
+    setLayout (layout = layouts.DefaultLayout) {
       self.layout = layout
     }
-
-    return {
-      setLayout
-    }
-  })
+  }))
 
 export default Classifier
