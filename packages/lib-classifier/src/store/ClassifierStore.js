f194f79382199e4b54bd77aedc94f8be2b952065
Roger Hutchings <roger@hutchin.gs>
2018-07-12 12:32:46 +0100

    Add button state

--- packages/lib-classifier/src/store/ClassifierStore.js
+++ packages/lib-classifier/src/store/ClassifierStore.js
@@ -4,10 +4,31 @@ import subjectViewers from '../helpers/subjectViewers'
 
 const Classifier = types
   .model('Classifier', {
+    annotate: types.optional(types.boolean, true),
+    fullscreen: types.optional(types.boolean, false),
+    move: types.optional(types.boolean, false),
     layout: types.optional(types.enumeration('layout', layouts.values), layouts.default),
   })
 
   .actions(self => ({
+    enableAnnotate () {
+      self.annotate = true
+      self.move = false
+    },
+
+    enableFullscreen () {
+      self.fullscreen = true
+    },
+
+    enableMove () {
+      self.annotate = false
+      self.move = true
+    },
+
+    disableFullscreen () {
+      self.fullscreen = false
+    },
+
     setLayout (layout = layouts.DefaultLayout) {
       self.layout = layout
     }
