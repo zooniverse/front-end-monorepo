6c1a900a348f5f4fe6ef916bcaf05f82912707f9
Roger Hutchings <roger@hutchin.gs>
2018-07-16 15:23:21 +0100

    Add help link

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskArea.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskArea.js
@@ -1,9 +1,14 @@
+import counterpart from 'counterpart'
 import { Box, Text } from 'grommet'
 import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
 
 import Answer from './components/Answer'
+import en from './locales/en'
+
+counterpart.registerTranslations('en', en)
 
 const boxStyles = {
   pad: 'medium',
@@ -13,6 +18,22 @@ const boxStyles = {
   }
 }
 
+const HelpLink = styled.button`
+  background: none;
+  border: 0;
+  color: #00979d;
+  cursor: pointer;
+  font-weight: light;
+  font-size: 14px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`
+
 function storeMapper (stores) {
   const { active: task } = stores.classifierStore.tasks
   return { task }
@@ -21,6 +42,15 @@ function storeMapper (stores) {
 @inject(storeMapper)
 @observer
 class TaskArea extends React.Component {
+  constructor () {
+    super()
+    this.openHelp = this.openHelp.bind(this)
+  }
+
+  openHelp () {
+    console.info('Open Help')
+  }
+
   render () {
     const { task } = this.props
     if (!task) {
@@ -33,6 +63,13 @@ class TaskArea extends React.Component {
           {task.question}
         </Text>
         <Answer />
+        <Box align="center" margin={{ vertical: 'small' }}>
+          <HelpLink
+            onClick={this.openHelp}
+          >
+            {counterpart('TaskArea.help')}
+          </HelpLink>
+        </Box>
       </Box>
     )
   }
