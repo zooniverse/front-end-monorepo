0efcd90c4f165ad7069b2ea1ec2d286ab03c721a
Sarah Allen <sarah@zooniverse.org>
2018-07-24 14:31:43 +0100

    Actually commit the new files

---
+++ packages/lib-grommet-theme/src/light.js
@@ -0,0 +1,75 @@
+export default {
+  // darkTheme: {
+  //   background: {
+  //     border: '#2D2D2D',
+  //     default: '#333333'
+  //   },
+  //   button: {
+  //     answer: {
+  //       default: '#5C5C5C',
+  //       gradient: {
+  //         top: '#004954',
+  //         bottom: '#002C31'
+  //       }
+  //     },
+  //     done: {
+  //       default: '#B8E986',
+  //       hover: '#345446'
+  //     }
+  //   },
+  //   font: '#E2E5E9'
+  // },
+  // lightTheme: {
+  colors: {
+    background: {
+      border: '#ebebeb',
+      default: '#EEF2F5'
+    },
+    button: {
+      answer: {
+        disabled: '#A6A7A9',
+        gradient: {
+          top: '#C0E3E5',
+          bottom: '#8BCED2'
+        }
+      },
+      done: '#078F52',
+      nextHover: '#ea9300'
+    },
+    foreground: '#404040',
+    font: '#000000'    
+  }
+
+  // },
+  // black: {
+  //   default: '#000',
+  //   light: '#272727'
+  // },
+  // brand: {
+  //   default: '#007482',
+  //   light: '#16979C'
+  // },
+  // grey: {
+  //   light: '#CBCCCB',
+  //   mid: '#646464'
+  // },
+  // highlight: {
+  //   dark: '#CC9200',
+  //   default: '#F0B200',
+  //   light: '#F6D885'
+  // },
+  // navy: {
+  //   default: '#0C4881',
+  //   light: '#6D91B3'
+  // },
+  // status: {
+  //   alert: '#E45950',
+  //   disabled: '#C3C5C8',
+  //   success: '#009E0C'
+  // },
+  // teal: {
+  //   dark: '#005D69',
+  //   light: '#ADDDE0',
+  //   mid: '#00979D'
+  // }
+};
