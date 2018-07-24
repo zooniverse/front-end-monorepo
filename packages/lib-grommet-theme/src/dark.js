5ed4e7b3e45db9dfc7a3e1bc1819fa21bb4ef709
srallen <srallen@users.noreply.github.com>
2018-07-24 15:15:38 +0100

    Light and dark themes (#96)
    * Add light and dark theme files. Remove cruft
    
    * Actually commit the new files
    
    * Clean up light theme file

---
+++ packages/lib-grommet-theme/src/dark.js
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    background: {
+      border: '#2D2D2D',
+      default: '#333333'
+    },
+    button: {
+      answer: {
+        default: '#5C5C5C',
+        gradient: {
+          top: '#004954',
+          bottom: '#002C31'
+        }
+      },
+      done: {
+        default: '#B8E986',
+        hover: '#345446'
+      }
+    },
+    font: '#E2E5E9'
+  }
+}
---
+++ packages/lib-grommet-theme/src/light.js
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    background: {
+      border: '#ebebeb',
+      default: '#EEF2F5'
+    },
+    button: {
+      answer: {
+        disabled: '#A6A7A9',
+        gradient: {
+          top: '#C0E3E5',
+          bottom: '#8BCED2'
+        }
+      },
+      done: '#078F52',
+      nextHover: '#ea9300'
+    },
+    foreground: '#404040',
+    font: '#000000'    
+  }
+};
