440bebf976c60b6f6ecdf0464e3bf21986d083f8
Sarah Allen <sarah@zooniverse.org>
2018-07-26 14:44:46 +0100

    Remove comment

--- packages/lib-react-components/.storybook/webpack.config.js
+++ packages/lib-react-components/.storybook/webpack.config.js
@@ -1,7 +1,6 @@
 module.exports = (baseConfig, env, defaultConfig) => {
   // Extend defaultConfig as you need.
 
-  // For example, add typescript loader:
   defaultConfig.node = { fs: 'empty' }
 
   return defaultConfig;
