19d6eede7eb146201313cfa6f0381f1bfbe803d9
Sarah Allen <sarah@zooniverse.org>
2018-07-27 15:53:22 +0100

    Update to use storybook plugins. Update themes. Add stories

--- packages/lib-react-components/src/components/layout/ZooFooter/ZooFooter.js
+++ packages/lib-react-components/src/components/layout/ZooFooter/ZooFooter.js
@@ -38,10 +38,6 @@ export const StyledLogoAnchor = styled(Anchor)`
     border-bottom: solid thin transparent;
     fill: ${whichTealColorForTheme};
   }
-
-  g {
-    fill: ${whichTealColorForTheme};
-  }
   
   svg:hover, svg:focus {
     border-bottom: solid thin ${whichTealColorForTheme};
@@ -75,6 +71,7 @@ export const StyledEasterEgg = styled(Image)`
     opacity: 1;
   }
 `;
+
 
 const ZooFooter = (props) => {
   const {
