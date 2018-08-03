c6020556068892185ee42407d8291c2df7deeac0
Sarah Allen <sarah@zooniverse.org>
2018-08-03 09:51:42 -0500

    Tests for SocialAnchor

--- packages/lib-react-components/src/components/layout/ZooFooter/components/NavListItem/NavListItem.test.js
+++ packages/lib-react-components/src/components/layout/ZooFooter/components/NavListItem/NavListItem.test.js
@@ -13,7 +13,6 @@ describe('NavListItem', function () {
   const label = 'My link';
   const url = 'https://www.zooniverse.org';
   let wrapper;
-  let manyNavListItems;
   before(function () {
     wrapper = shallow(<NavListItem label={label} url={url} />)
   });
@@ -24,5 +23,8 @@ describe('NavListItem', function () {
     expect(wrapper).to.matchSnapshot();
   });
 
+  // Testing styled-components requires mount to work, but it currently isn't
+  // Patch in enzyme React 16 adapter may be coming soon:
+  // https://github.com/airbnb/enzyme/pull/1592
   xdescribe('light theme')
 })
