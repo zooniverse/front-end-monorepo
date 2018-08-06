088a10b5292892df02f2106eda94e3a17e148b2b
mcbouslog <mcbouslog@gmail.com>
2018-08-06 17:08:15 -0500

    Add ZooHeader to fe-project

--- packages/app-project/pages/index.js
+++ packages/app-project/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Grommet } from 'grommet'
-import { ZooFooter } from '@zooniverse/react-components'
+import { ZooFooter, ZooHeader } from '@zooniverse/react-components'
 import Head from '../components/head'
-import Nav from '../components/nav'
+// import Nav from '../components/nav'
 
 
 export default () => (
   <Grommet>
     <Head title='Home' />
-    <Nav />
+    <ZooHeader />
     <div>
       Index
     </div>
