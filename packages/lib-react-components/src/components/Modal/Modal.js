f796d05331334997ed619dc1253021363f5785a7
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2018-08-19 07:00:57 +0100

    Create modal component (#117)
    * Add modal component
    
    * Make modal a stateless function
    
    * Update snapshots

---
+++ packages/lib-react-components/src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import { Box, Button, Grommet, Layer } from 'grommet'
+import PropTypes from 'prop-types'
+import React from 'react'
+import zooTheme from '@zooniverse/grommet-theme'
+
+import WithLayer from './WithLayer'
+import ModalBody from './components/ModalBody'
+import ModalHeading from './components/ModalHeading'
+
+function Modal ({ children, closeFn, colorTheme, theme, title }) {
+  return (
+    <Grommet theme={theme}>
+      <ModalHeading closeFn={closeFn} title={title} />
+      <ModalBody colorTheme={colorTheme}>
+        {children}
+      </ModalBody>
+    </Grommet>
+  )
+}
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  closeFn: PropTypes.func,
+  colorTheme: PropTypes.oneOf(['light', 'dark']),
+  title: PropTypes.string.isRequired,
+  theme: PropTypes.object
+}
+
+Modal.defaultProps = {
+  colorTheme: 'light',
+  theme: zooTheme
+}
+
+export default WithLayer(Modal)
+
+export { Modal }
---
+++ packages/lib-react-components/src/components/Modal/Modal.spec.js
@@ -0,0 +1,20 @@
+import { shallow, render } from 'enzyme'
+import React from 'react'
+
+import { Modal } from './Modal'
+
+const title = 'Modal Heading'
+
+const content = (
+  <div>
+    Leo mollis dictum id dis maecenas consectetur metus elementum vivamus nisl, suscipit tristique lectus nulla mus etiam nisi facilisis magnis, scelerisque ligula montes luctus cursus nibh vulputate parturient risus.
+  </div>
+)
+
+describe('Modal', function () {
+  it('should render without crashing', function () {
+    shallow(<Modal title={title}>
+      {content}
+    </Modal>)
+  })
+})
