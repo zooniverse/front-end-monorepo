f3256b0b78ed6ab4089778c5ac50a00212e37af9
Roger Hutchings <roger@hutchin.gs>
2018-08-24 17:23:17 +0100

    Localize tab titles

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskArea.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskArea.js
@@ -1,11 +1,20 @@
+import counterpart from 'counterpart'
 import React from 'react'
+
 import { Tab, Tabs } from './components/Tabs'
+import en from './locales/en'
+
+counterpart.registerTranslations('en', en)
 
 function TaskArea () {
   return (
     <Tabs>
-      <Tab title='Question'>Question area content...</Tab>
-      <Tab title='Tutorial'>...2</Tab>
+      <Tab title={counterpart('TaskArea.task')}>
+        Question area content...
+      </Tab>
+      <Tab title={counterpart('TaskArea.tutorial')}>
+        ...2
+      </Tab>
     </Tabs>
   )
 }
