c6b6a7020ca0064b2d04e158c3f99a7667db403c
Sarah Allen <sarah@zooniverse.org>
2018-09-05 12:40:03 -0500

    Update lib-classifier MobX related packages. Updates for major version changes.

--- packages/lib-classifier/src/store/ProjectStore.js
+++ packages/lib-classifier/src/store/ProjectStore.js
@@ -4,7 +4,7 @@ import ResourceStore from './ResourceStore'
 
 const ProjectStore = types
   .model('ProjectStore', {
-    active: types.maybe(types.reference(Project)),
+    active: types.maybeNull(types.reference(Project)),
     resources: types.optional(types.map(Project), {}),
     type: types.optional(types.string, 'projects')
   })
