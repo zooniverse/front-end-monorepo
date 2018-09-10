6948e199b8c368e9ef4e4b6b2370224eef07eae4
Roger Hutchings <roger@hutchin.gs>
2018-09-10 17:10:48 +0100

    Add asyncStates lib

---
+++ packages/lib-async-states/src/async-states.spec.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+/* global expect */
+const asyncStates = require('./async-states')
+
+describe('asyncStates', function () {
+  const states = [
+    'initialized',
+    'loading',
+    'success',
+    'error'
+  ]
+
+  states.forEach(function (state) {
+    describe(`\`${state}\` state`, function () {
+      it('should exist', function () {
+        expect(asyncStates[state]).to.equal(state)
+      })
+
+      it('should be immutable', function () {
+        asyncStates[state] = 'foobar'
+        expect(asyncStates[state]).to.equal(state)
+      })
+    })
+  })
+
+  describe('`values` property', function () {
+    it('should return all available states', function () {
+      expect(asyncStates.values).to.deep.equal(states)
+    })
+
+    it('should be immutable', function () {
+      asyncStates.values = 'foobar'
+      expect(asyncStates.values).to.deep.equal(states)
+    })
+  })
+})
