4365f6e5f7837166fad3f7a0d8f53e92d0237761
Sarah Allen <sarah@zooniverse.org>
2018-10-01 13:04:02 -0500

    Update snapshots for tests

--- packages/lib-react-components/src/ZooHeader/components/UserMenu/UserMenu.spec.js
+++ packages/lib-react-components/src/ZooHeader/components/UserMenu/UserMenu.spec.js
@@ -1,4 +1,4 @@
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import React from 'react'
 import UserMenu from './UserMenu'
 
@@ -6,7 +6,7 @@ describe('UserMenu', function () {
   let wrapper
   const user = { display_name: 'zootester1', login: 'zootester1' }
   before(function () {
-    wrapper = shallow(<UserMenu user={user} />)
+    wrapper = shallow(<UserMenu signOut={() => {}} user={user} />)
   })
 
   it('renders without crashing', function () {})
