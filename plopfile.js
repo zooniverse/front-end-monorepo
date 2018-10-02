1882e7aa3fc957793515fc00f57687dbe13793a2
Roger Hutchings <roger@hutchin.gs>
2018-10-02 12:11:32 +0100

    Add plopjs setup for scaffolding apps and components

--- plopfile.js
+++ plopfile.js
@@ -1,22 +1,3 @@
-module.exports = function (plop) {
-  plop.setGenerator('App', {
-    description: 'Bootstrap a new Next.js app',
+const plopFunction = require('./plop/plopFile')
 
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'App name? (e.g. `projects`)'
-      }
-    ],
-
-    actions: [
-      {
-        type: 'addMany',
-        templateFiles: ['plop/app/**/*', 'plop/app/**/.*'],
-        destination: 'packages/app-{{dashCase name}}',
-        base: 'plop/app'
-      }
-    ]
-  })
-}
+module.exports = plopFunction
