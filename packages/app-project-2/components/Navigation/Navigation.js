eeb4c0135f4c94bd543952695a85fb82efa630d2
Roger Hutchings <roger@hutchin.gs>
2018-10-08 16:52:18 +0100

    Rename Nav component

--- packages/app-project-2/components/Navigation/Navigation.js
+++ packages/app-project-2/components/Navigation/Navigation.js
@@ -20,7 +20,7 @@ function Nav () {
                 project: 'what-is-beauty'
               }
             }}
-            as='projects/stephenserjeant/what-is-beauty'
+            as='/projects/stephenserjeant/what-is-beauty'
           >
             <a>Am I Hot or Not</a>
           </Link>
@@ -30,12 +30,12 @@ function Nav () {
             prefetch
             href={{
               pathname: 'projects',
-              query: { 
+              query: {
                 owner: 'markb-panoptes',
                 project: 'miniature-fossils-magnified'
               }
             }}
-            as='projects/markb-panoptes/miniature-fossils-magnified'
+            as='/projects/markb-panoptes/miniature-fossils-magnified'
           >
             <a>Miniature Fossils</a>
           </Link>
