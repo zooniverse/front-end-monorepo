1e295d1bfbf4a31b084475b5996d236cf3e05a0c
Roger Hutchings <roger@hutchin.gs>
2018-10-09 16:33:17 +0100

    Add interaction layer tests

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.spec.js
@@ -1,9 +1,43 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import sinon from 'sinon'
+
 import InteractionLayer from './InteractionLayer'
 
-describe('Component > InteractionLayer', function () {
-  it('should render without crashing', function () {
-    shallow(<InteractionLayer />)
+const onMouseMove = sinon.stub()
+const onMouseUp = sinon.stub()
+const onMouseDown = sinon.stub()
+
+let wrapper
+
+describe.only('Component > InteractionLayer', function () {
+  beforeEach(function () {
+    wrapper = shallow(<InteractionLayer onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp} onMouseDown={onMouseDown} />)
   })
+
+  it('should render without crashing', function () {})
+
+  it('should return a transparent rect', function () {
+    const rect = wrapper.find('rect')
+    expect(rect.exists()).to.be.true
+    expect(rect.prop('id')).to.equal('InteractionLayer')
+    expect(rect.prop('fill')).to.equal('transparent')
+  })
+
+  it('should bind the onMouseMove prop', function () {
+    wrapper.simulate('mousemove')
+    expect(onMouseMove.called).to.be.true
+  })
+
+  it('should bind the onMouseUp prop', function () {
+    wrapper.simulate('mouseup')
+    expect(onMouseUp.called).to.be.true
+  })
+
+  it('should bind the onMouseDown prop', function () {
+    wrapper.simulate('mousedown')
+    expect(onMouseDown.called).to.be.true
+  })
+
 })
