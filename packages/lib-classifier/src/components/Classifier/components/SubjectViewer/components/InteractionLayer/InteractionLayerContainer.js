fa79906f4a09a21e14c778b7528677a0800a33fc
Roger Hutchings <roger@hutchin.gs>
2018-10-10 15:00:54 +0100

    Add comment

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.js
@@ -24,6 +24,8 @@ class InteractionLayerContainer extends Component {
 
 
   componentDidMount () {
+    // TODO: We're simply logging the event stream here for now, but this will
+    // be passed to the active drawing tool for parsing
     const stream = this.props.drawing.eventStream
     stream.subscribe(z => console.log(z))
   }
