acf4aca28d79461d2042e8680151524af2cbb8f2
Sarah Allen <sarah@zooniverse.org>
2018-10-16 11:47:36 -0500

    Write tests for TaskNavButtonsContainer. Minor refactoring

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/TaskNavButtonsContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/TaskNavButtonsContainer.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { inject, observer } from 'mobx-react'
+import { inject, observer, PropTypes as MobXPropTypes } from 'mobx-react'
 import TaskNavButtons from './TaskNavButtons'
 
 function storeMapper (stores) {
   const {
-    active: step,
     activeStepTasks: tasks,
     isThereANextStep: showNextButton,
     selectStep
@@ -17,7 +16,6 @@ function storeMapper (stores) {
     createDefaultAnnotation,
     showNextButton,
     selectStep,
-    step,
     tasks
   }
 }
@@ -26,15 +24,16 @@ function storeMapper (stores) {
 @observer
 class TaskNavButtonsContainer extends React.Component {
   createDefaultAnnotationIfThereIsNone() {
-    const { classification, createDefaultAnnotation, step, tasks } = this.props
-    step.taskKeys.forEach((taskKey) => {
-      // User didn't submit annotation and task is not required
-      // Create the default annotation before going to the next step
-      if (!classification.annotations.get(taskKey)) {
-        const [task] = tasks.filter((task) => { return task.taskKey === taskKey })
-        createDefaultAnnotation(task)
-      }
-    })
+    const { classification, createDefaultAnnotation, tasks } = this.props
+    if (classification) {
+      tasks.forEach((task) => {
+        // User didn't submit annotation and task is not required
+        // Create the default annotation before going to the next step
+        if (!classification.annotations.get(task.taskKey)) {
+          createDefaultAnnotation(task)
+        }
+      })
+    }
   }
 
   goToNextStep () {
@@ -52,6 +51,16 @@ class TaskNavButtonsContainer extends React.Component {
       />
     )
   }
+}
+
+TaskNavButtonsContainer.propTypes = {
+  classification: PropTypes.shape({
+    annotation: MobXPropTypes.observableArrayOf(PropTypes.object)
+  }),
+  createDefaultAnnotation: PropTypes.func,
+  showNextButton: PropTypes.bool,
+  selectStep: PropTypes.func,
+  tasks: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default TaskNavButtonsContainer
--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/TaskNavButtonsContainer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/TaskNavButtonsContainer.spec.js
@@ -1,11 +1,125 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import { expect } from 'chai'
+import sinon from 'sinon'
 import TaskNavButtonsContainer from './TaskNavButtonsContainer'
 
+const tasks = [
+  {
+    answers: [{ label: 'yes' }, { label: 'no' }],
+    question: 'Is there a cat?',
+    required: true,
+    taskKey: 'init',
+    type: 'single'
+  }, {
+    answers: [{ label: 'napping' }, { label: 'standing' }, { label: 'playing' }],
+    question: 'What is/are the cat(s) doing?',
+    required: false,
+    taskKey: 'T1',
+    type: 'multiple'
+  }
+]
+
 describe('TaskNavButtonsContainer', function () {
-  it('should render without crashing', function () {
-    const wrapper = shallow(<TaskNavButtonsContainer />)
-    expect(wrapper).to.be.ok
+  describe('when it renders', function () {
+    let wrapper
+    before(function () {
+      wrapper = shallow(
+        <TaskNavButtonsContainer
+          showNextButton={true}
+          tasks={tasks}
+        />
+      )
+    })
+
+    it('should render without crashing', function () {
+      expect(wrapper).to.be.ok
+    })
+
+    it('should render TaskNavButtons', function () {
+      expect(wrapper.find('TaskNavButtons')).to.have.lengthOf(1)
+    })
+  })
+
+  describe('#goToNextStep', function () {
+    let wrapper
+    let createDefaultAnnotationIfThereIsNoneSpy
+    let selectStepSpy
+
+    before(function () {
+      createDefaultAnnotationIfThereIsNoneSpy = sinon.spy(TaskNavButtonsContainer.prototype, 'createDefaultAnnotationIfThereIsNone')
+      selectStepSpy = sinon.spy()
+
+      wrapper = shallow(
+        <TaskNavButtonsContainer
+          selectStep={selectStepSpy}
+          showNextButton={true}
+          tasks={tasks}
+        />
+      )
+    })
+
+    afterEach(function () {
+      createDefaultAnnotationIfThereIsNoneSpy.resetHistory()
+      selectStepSpy.resetHistory()
+    })
+
+    after(function () {
+      createDefaultAnnotationIfThereIsNoneSpy.restore()
+    })
+
+    it('should call #createDefaultAnnotationIfThereIsNone', function () {
+      wrapper.instance().goToNextStep()
+      expect(createDefaultAnnotationIfThereIsNoneSpy.called).to.be.true
+    })
+
+    it('should call props.selectStep', function () {
+      wrapper.instance().goToNextStep()
+      expect(selectStepSpy.called).to.be.true
+    })
+  })
+
+  describe('#createDefaultAnnoationIfThereIsNone', function () {
+    let wrapper
+    let createDefaultAnnotationSpy
+
+    before(function () {
+      createDefaultAnnotationSpy = sinon.spy()
+
+      wrapper = shallow(
+        <TaskNavButtonsContainer
+          createDefaultAnnotation={createDefaultAnnotationSpy}
+          showNextButton={true}
+          tasks={tasks}
+        />
+      )
+    })
+
+    afterEach(function () {
+      createDefaultAnnotationSpy.resetHistory()
+    })
+
+    it('should not call props.createDefaultAnnotation if there is not a props.classification', function () {
+      wrapper.instance().createDefaultAnnotationIfThereIsNone()
+      expect(createDefaultAnnotationSpy.called).to.be.false
+    })
+
+    it('should call props.createDefaultAnnotation if there is a props.classification and there aren\'t annotations for the tasks', function () {
+      const classification = { annotations: new Map() }
+      wrapper.setProps({ classification })
+      wrapper.instance().createDefaultAnnotationIfThereIsNone()
+      tasks.forEach((task) => {
+        expect(createDefaultAnnotationSpy.called).to.be.true
+        expect(createDefaultAnnotationSpy.calledWith(task)).to.be.true
+      })
+    })
+
+    it('should not call props.createDefaultAnnotation if props.classification has matching annotations for the tasks', function () {
+      const annotations = new Map()
+      tasks.forEach(task => annotations.set(task.taskKey, task))
+      wrapper.setProps({ classification: { annotations }})
+      wrapper.instance().createDefaultAnnotationIfThereIsNone()
+      expect(createDefaultAnnotationSpy.notCalled).to.be.true
+    })
   })
 })
