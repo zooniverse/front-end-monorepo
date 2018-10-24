853d7f59ebc1cc51a04ba8287717cbef26615d97
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-10-24 17:09:23 +0100

    Split D3 logic into the LightCurveViewer component (not the container)

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
@@ -2,25 +2,151 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import InteractionLayer from '../InteractionLayer'
+import * as d3 from 'd3'
 
 const SVG = styled.svg`
   height: 100%;
   width: 100%;
 `
 
-function LightCurveViewer ({ url }) {
-  return (
-    <SVG>
-      <image xlinkHref={url} />
-      <InteractionLayer />
-      <circle cx="100" cy="100" r="100" fill="#c33" />
-    </SVG>
-  )
+class LightCurveViewer extends React.Component {
+  
+  constructor() {
+    super()
+    
+    //React reference to the DOM.
+    //See: https://reactjs.org/docs/refs-and-the-dom.html
+    this.svgContainer = React.createRef()
+    
+    //D3 bits
+    this.d3svg = null
+    this.d3dataLayer = null
+  }
+  
+  componentDidMount () {
+    this.d3init()
+    this.d3loadData()
+  }
+
+  componentDidUpdate (prevProps) {
+    this.d3loadData()
+  }
+  
+  componentWillUnmount () {
+    //TODO: unregister all D3 event listeners
+  }
+  
+  d3init () {
+    //Prepare the main SVG
+    //--------------------------------
+    this.d3svg = d3.select(this.svgContainer.current).append('svg')
+      .attr('class', 'light-curve-viewer')
+      //.attr('viewBox', `0 0 ${this.props.width} ${this.props.height}`)  //TODO: check
+      //.attr('style', 'width: 100%; height: 100%')  //TODO: check
+      .attr('width', this.props.width)
+      .attr('height', this.props.height)
+    
+    //Deco: Background layer
+    this.d3svg.append('rect')
+      .attr('width', this.props.width)
+      .attr('height', this.props.height)
+      .attr('fill', '#f8f8f8')
+      .attr('stroke', '#333')
+      .attr('stroke-width', '2')
+    //--------------------------------
+    
+    //Prepare the data layer
+    //--------------------------------
+    this.d3dataLayer = this.d3svg.append('g')
+      .attr('class', 'data-layer')
+    //--------------------------------
+    
+    //Add interactive elements for panning and zooming
+    //--------------------------------
+    this.d3interfaceLayer = this.d3svg.append('rect')
+      .attr('class', 'interface-layer')
+      .attr('width', this.props.width)
+      .attr('height', this.props.height)
+      .style('fill', 'none')
+      .style('pointer-events', 'all')
+    
+    this.d3interfaceLayer.call(d3.zoom()
+      .scaleExtent([0.1, 10])
+      .on('zoom', () => { this.d3dataLayer.attr('transform', d3.event.transform) })
+    )
+    //--------------------------------
+  }
+  
+  d3resetData () {
+    //Sanity check
+    if (!this.d3svg) return
+    
+    this.d3dataLayer.selectAll('.data-point').exit()
+  }
+  
+  d3loadData () {
+    //Sanity check
+    if (!this.d3svg) return
+    
+    //Start with a clean slate
+    this.d3resetData()
+    
+    const jsonData = this.props.jsonData
+    if (!jsonData) {
+      return
+    }
+    
+    //WIP: Scale and Axis layer
+    //--------------------------------
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(jsonData.x))
+      .range([0, this.props.width])
+    const xAxis = d3.axisBottom(xScale)
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(jsonData.y))
+      .range([this.props.height, 0])  //REVERSE!
+    const yAxis = d3.axisLeft(yScale)
+    //--------------------------------
+    
+    //Insert the data
+    //--------------------------------
+    const data = jsonData.x.map((x, index) => {
+      const y = jsonData.y[index]
+      return {
+        x: xScale(x),
+        y: yScale(y)
+      }
+    })
+    
+    this.d3dataLayer.selectAll('circle')
+      .data(data)
+      .enter()
+        .append('circle')
+        .attr('class', 'data-point')
+        .attr('cx', (d) => d.x)
+        .attr('cy', (d) => d.y)
+        .attr('r', (d) => 1)
+        .attr('fill', '#066')
+    //--------------------------------
+  }
+  
+  render () {
+    return (
+      <div className="light-curve-viewer" ref={this.svgContainer}></div>
+    )
+  }
 }
 
 LightCurveViewer.propTypes = {
-  url: PropTypes.string.isRequired
+  jsonData: PropTypes.object,
+  width: PropTypes.number,
+  height: PropTypes.number,
+}
+
+LightCurveViewer.defaultProps = {
+  jsonData: null,
+  width: 500,
+  height: 500,
 }
 
 export default LightCurveViewer
--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewerContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewerContainer.js
@@ -3,30 +3,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import request from 'superagent'
 
-import * as d3 from 'd3'
-import exampleData from './example-planet-hunters-data.json'
-
 import LightCurveViewer from './LightCurveViewer'
 import locationValidator from '../../helpers/locationValidator'
 
 class LightCurveViewerContainer extends React.Component {
   constructor () {
     super()
-    this.state = {
-      loading: asyncStates.initialized
-    }
     
-    //React reference to the DOM.
-    //See: https://reactjs.org/docs/refs-and-the-dom.html
-    this.svgContainer = React.createRef();
+    this.state = {
+      loading: asyncStates.initialized,
+      jsonData: null,
+    }
     
     //TODO: turn into variables?
     this.width = 500
     this.height = 500
-    
-    //D3 bits
-    this.d3svg = null;
-    this.d3dataLayer = null;
   }
   
   componentDidMount () {
@@ -43,6 +34,8 @@ class LightCurveViewerContainer extends React.Component {
       this.handleSubject()
     }
   }
+  
+  componentWillUnmount () {}
 
   async handleSubject () {
     const { subject } = this.props
@@ -60,112 +53,25 @@ class LightCurveViewerContainer extends React.Component {
     
     this.setState({ loading: asyncStates.loading })
     try {
-      
       request.get(jsonLocation['application/json'] || jsonLocation['text/plain'])
         .then(res => {
           if (res.ok) {
             //Get the JSON data, or (as a failsafe) parse the JSON data if the response is returned as a string
             const jsonData = res.body || JSON.parse(res.text)
-            this.d3init(jsonData);
+            
+            this.setState({ jsonData })
           } else {
             throw 'ERROR: invalid response'
           }
         })
         .catch(err => {
           //Possible errors:
-          // - 
           throw(err)
         })
-      
-      //const img = await this.fetchImage(imageUrl)
-      //this.setState({
-      //  height: img.height,
-      //  width: img.width,
-      //  loading: asyncStates.success
-      //})
     } catch (err) {
       console.error(err)
-      this.setState({ loading: asyncStates.error })
+      this.setState({ loading: asyncStates.error, jsonData: null })
     }
-  }
-
-  d3init (dataJson) {
-    //Prepare the main SVG
-    //--------------------------------
-    this.d3svg = d3.select(this.svgContainer.current).append('svg')
-      .attr('class', 'light-curve-viewer')
-      //.attr('viewBox', `0 0 ${this.width} ${this.height}`)  //TODO: check
-      //.attr('style', 'width: 100%; height: 100%')  //TODO: check
-      .attr('width', this.width)
-      .attr('height', this.height)
-    
-    //Deco: Background layer
-    this.d3svg.append('rect')
-      .attr('width', this.width)
-      .attr('height', this.height)
-      .attr('fill', '#f8f8f8')
-      .attr('stroke', '#333')
-      .attr('stroke-width', '2')
-    //--------------------------------
-    
-    //Prepare the data layer
-    //--------------------------------
-    this.d3dataLayer = this.d3svg.append('g')
-      .attr('class', 'data-layer')
-    //--------------------------------
-    
-    //WIP: Scale and Axis layer
-    //--------------------------------
-    const xScale = d3.scaleLinear()
-      .domain(d3.extent(dataJson.x))
-      .range([0, this.width])
-    const xAxis = d3.axisBottom(xScale)
-    const yScale = d3.scaleLinear()
-      .domain(d3.extent(dataJson.y))
-      .range([this.height, 0])  //REVERSE!
-    const yAxis = d3.axisLeft(yScale)
-    //--------------------------------
-    
-    //Insert the data
-    //--------------------------------
-    const data = dataJson.x.map((x, index) => {
-      const y = dataJson.y[index]
-      return {
-        x: xScale(x),
-        y: yScale(y)
-      }
-    })
-    
-    this.d3dataLayer.selectAll('circle')
-      .data(data)
-      .enter()
-        .append('circle')
-        .attr('class', 'data-point')
-        .attr('cx', (d) => d.x)
-        .attr('cy', (d) => d.y)
-        .attr('r', (d) => 1)
-        .attr('fill', '#066')
-    //--------------------------------
-    
-    //Add interactive elements for panning and zooming
-    //--------------------------------
-    this.d3interfaceLayer = this.d3svg.append('rect')
-      .attr('class', 'interface-layer')
-      .attr('width', this.width)
-      .attr('height', this.height)
-      .style('fill', 'none')
-      .style('pointer-events', 'all')
-    
-    this.d3interfaceLayer.call(d3.zoom()
-      .scaleExtent([0.1, 10])
-      .on('zoom', () => { this.d3dataLayer.attr('transform', d3.event.transform) })
-    )
-    //--------------------------------
-      
-  }
-  
-  componentWillUnmount () {
-    //TODO: check if we need to unregister the D3 SVG element
   }
 
   render () {
@@ -173,9 +79,15 @@ class LightCurveViewerContainer extends React.Component {
     if (!subject) {
       return null
     }
+    
+    console.log('+++ Container.RENDER! ', this.state.jsonData)
 
     return (
-      <div className="light-curve-viewer" ref={this.svgContainer}></div>
+      <LightCurveViewer
+        jsonData={this.state.jsonData}
+        width={this.width}
+        width={this.height}
+      />
     )
   }
 }
