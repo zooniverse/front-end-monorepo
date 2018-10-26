ff4d35181d5d1752f766a36a6161fba2216965fc
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2018-10-26 19:56:10 +0100

    Use `nock` instead of `superagent-mock` (#206)
    * Update `package.json`
    
    - Add `nock`
    - Remove `superagent-mock`
    - Add author / repo fields
    - Add StandardJS config
    
    * Remove unnecessary `**` from glob
    
    * Rewrite tests to use nock
    
    * Remove unused variable

--- packages/lib-panoptes-js/src/resources/subjects/rest.spec.js
+++ packages/lib-panoptes-js/src/resources/subjects/rest.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai')
-const superagent = require('superagent')
-const mockSuperagent = require('superagent-mock')
+const nock = require('nock')
+
 const { config } = require('../../config')
 const { resources, responses } = require('./mocks')
 const { endpoint } = require('./helpers')
@@ -8,51 +8,47 @@ const subjects = require('./index')
 
 describe('Subjects resource REST requests', function () {
   describe('get', function () {
-    let superagentMock
-    let actualHeaders
     const expectedGetResponse = responses.get.subject
-    before(function () {
-      superagentMock = mockSuperagent(superagent, [{
-        pattern: `${config.host}${endpoint}`,
-        fixtures: (match, params, headers, context) => {
-          actualHeaders = headers
-          if (match.input.includes(resources.subject.id)) return expectedGetResponse
+    let scope
 
-          return { status: 404 }
-        },
-        get: (match, data) => {
-          return { body: data }
-        }
-      }])
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .get(uri => uri.includes(endpoint))
+        .query(true)
+        .reply(200, expectedGetResponse)
     })
 
     after(function () {
-      superagentMock.unset()
+      nock.cleanAll()
     })
 
-    it('should error if request does not have a subject id', function () {
-      subjects.get().catch((error) => {
+    it('should error if request does not have a subject id', async function () {
+      try {
+        await subjects.get()
+        expect.fail()
+      } catch (error) {
         expect(error.message).to.equal('Subjects: Get request must include a subject id.')
-      })
+      }
     })
 
-    it('should error if the subject id is not a string', function () {
-      subjects.get({ id: 1 }).catch((error) => {
+    it('should error if the subject id is not a string', async function () {
+      try {
+        await subjects.get({ id: 1 })
+        expect.fail()
+      } catch (error) {
         expect(error.message).to.equal('Subjects: Get request id must be a string.')
-      })
+      }
     })
 
-    it('should return the expected response', function () {
-      subjects.get({ id: '10' }).then((response) => {
-        expect(response.body).to.eql(expectedGetResponse)
-      })
+    it('should return the expected response', async function () {
+      const response = await subjects.get({ id: '10' })
+      expect(response.body).to.eql(expectedGetResponse)
     })
 
-    it('should add the Authorization header to the request if param is defined', function () {
-      return subjects.get({ id: '10', authorization: '12345' }).then((response) => {
-        expect(actualHeaders['Authorization']).to.exist
-        expect(actualHeaders['Authorization']).to.equal('12345')
-      })
+    it('should add the Authorization header to the request if param is defined', async function () {
+      const response = await subjects.get({ id: '10', authorization: '12345' })
+      expect(response.req.headers.authorization).to.equal('12345')
     })
   })
 })
