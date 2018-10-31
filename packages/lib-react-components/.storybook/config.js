6bf9cb235859f758ca9d8e4d8cbbaa2212c60df7
Sarah Allen <sarah@zooniverse.org>
2018-10-31 13:57:06 -0500

    Run lint

--- packages/lib-react-components/.storybook/config.js
+++ packages/lib-react-components/.storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { addDecorator } from '@storybook/react/dist/client/preview';
+import { addDecorator } from '@storybook/react/dist/client/preview'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/)
