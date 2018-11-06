fb4a69812fe0f5642ab02c46d2cf63f3e0542909
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-11-06 16:07:56 +0000

    Update setPointStyle tests

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/setPointStyle.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/setPointStyle.spec.js
@@ -16,8 +16,8 @@ describe('LightCurveViewer > d3 > setPointStyle', function () {
 
   it('should add the correct attributes to the selection', function () {
     setPointStyle(selectionFixture)
-    expect(attrStub.calledWith('r', 1)).to.be.true
+    expect(attrStub.calledWith('r', 1.25)).to.be.true
     expect(attrStub.calledWith('class', 'data-point')).to.be.true
-    expect(attrStub.calledWith('fill', '#3cc')).to.be.true
+    expect(attrStub.calledWith('fill', '#488')).to.be.true
   })
 })
