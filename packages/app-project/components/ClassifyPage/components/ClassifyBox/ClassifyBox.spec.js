c50a1135c27014794ea935319640aea933c97456
Roger Hutchings <roger@hutchin.gs>
2018-11-07 12:44:58 +0000

    Add ClassifyBox spec

--- packages/app-project/components/ClassifyPage/components/ClassifyBox/ClassifyBox.spec.js
+++ packages/app-project/components/ClassifyPage/components/ClassifyBox/ClassifyBox.spec.js
@@ -3,8 +3,21 @@ import React from 'react'
 
 import ClassifyBox from './ClassifyBox'
 
+let wrapper
+const Foobar = () => <div>Foobar</div>
+
 describe('Component > ClassifyBox', function () {
-  it('should render without crashing', function () {
-    shallow(<ClassifyBox />)
+  before(function () {
+    wrapper = shallow(
+      <ClassifyBox>
+        <Foobar />
+      </ClassifyBox>
+    )
+  })
+
+  it('should render without crashing', function () {})
+
+  it('should render its children', function () {
+    expect(wrapper.find(Foobar).length).to.equal(1)
   })
 })
