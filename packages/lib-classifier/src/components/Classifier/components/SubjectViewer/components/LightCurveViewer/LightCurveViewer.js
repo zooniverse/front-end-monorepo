6b9ca396584e3fa230cd020dbe5fe6b54b87cc9d
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-11-12 16:27:56 +0000

    Update layer comments and use .data-point instead of circle

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
@@ -82,7 +82,7 @@ class LightCurveViewer extends Component {
   }
 
   clearChart () {
-    this.d3dataLayer.selectAll('circle')
+    this.d3dataLayer.selectAll('.data-point')
       .remove()
   }
 
@@ -147,7 +147,7 @@ class LightCurveViewer extends Component {
   /*
   Initialises the D3 scatterplot chart.
   The chart is divided into multiple layers (both functional and decorative).
-  IMPORTANT: note the order these layers are added.
+  IMPORTANT: layers are added in z-index order, lowest first.
    */
   initChart () {
     const container = this.svgContainer.current
