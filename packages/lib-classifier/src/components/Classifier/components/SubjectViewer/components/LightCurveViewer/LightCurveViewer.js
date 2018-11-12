e1b61057b2905c1d543eb9a06398fc48c58ad9db
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2018-11-12 16:40:25 +0000

    Classifier - TESS Light Curve Viewer - Axis Scales and Single-Axis Zoom/Pan (#219)
    * Add comments and minor cleanup
    
    * WIP axis labels
    
    * Add basic scales that don't move with the pan or zoom
    
    * X and Y axes now respect the zoom and pan
    
    * WIP scale data points
    
    * Zoom and pan now locked to the x-direction only woo hoo
    
    * Users can only pan in the x-direction, and only within the bounds of the data
    
    * Fit data into viewer, again. No more shared frame of reference between Subjects.
    
    * Update comments
    
    * Add getCurrentTransformMethod()
    
    * Minor update for circle.data-point calls
    
    * Update setPointStyle tests
    
    * Update layer comments and use .data-point instead of circle

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
@@ -9,15 +9,53 @@ import addDataLayer from './d3/addDataLayer'
 import addInterfaceLayer from './d3/addInterfaceLayer'
 import setPointStyle from './d3/setPointStyle'
 
+//TODO: move into props or some other configurable
+const LIGHTCURVE_DATA_CONSTRAINTS = {
+  /*
+  Zoom range of 1x to 10x.
+  Minimum zoom prevent users from "zooming out" (< 1x zoom) beyond
+  reasonable subject area, maximum zoom is arbitrary.
+   */
+  minScale: 1,
+  maxScale: 10,  
+}
+
 class LightCurveViewer extends Component {
   constructor () {
     super()
+    
     this.svgContainer = React.createRef()
+    
+    // D3 Selection elements
     this.d3dataLayer = null
     this.d3interfaceLayer = null
     this.d3svg = null
+    
+    // D3 Zoom controller: manipulates and stores scale/translate values
+    this.zoom = null
+    
+    /*
+    The D3 x-scales/y-scales is used to map the x-y coordinates on the visual
+    <svg> to the x-y values of the data points.
+    
+    IMPORTANT: the y-axis in an <svg> increases as you go downwards, while the
+    y-axis in most scientific charts increases as you go upwards. We must
+    reverse the mapping accordingly when setting up this.yScale.
+     */
     this.xScale = null
     this.yScale = null
+    
+    /*
+    The "D3 axis" represents the visual axis and the axis labels.
+    Not to be confused with x-scales and y-scales.
+    Note the naming: d3axisX and d3axisY are used for the D3 selection (i.e. the
+    actual elements in the SVG) whereas xAxis and yAxis represent the data
+    model.
+     */
+    this.d3axisX = null
+    this.d3axisY = null
+    this.xAxis = null
+    this.yAxis = null
   }
 
   componentDidMount () {
@@ -40,37 +78,53 @@ class LightCurveViewer extends Component {
   }
 
   componentWillUnmount () {
-    this.d3interfaceLayer.on('.zoom', null)
+    this.d3interfaceLayer && this.d3interfaceLayer.on('.zoom', null)
   }
 
   clearChart () {
-    this.d3dataLayer.selectAll('circle')
+    this.d3dataLayer.selectAll('.data-point')
       .remove()
   }
 
+  /*
+  Updates the D3 chart to fit the size of the container, and adds/updates the
+  data points.
+  Called when new data (points) is received, and when chart is resized.
+   */
   drawChart (width, height, shouldAnimate = false) {
     if (!height || !width) {
       return false
     }
+    
+    /*
+    Limit zoom panning to x-direction (yMin=0, yMax=0), and don't allow panning
+    beyond the start (xMin=0) or end (xMax=width) of the light curve.
+    */
+    this.zoom.translateExtent([[0, 0], [width, 0]])
 
+    // Update x-y scales to fit current size of container
     this.xScale
       .domain(this.props.extent.x)
       .range([0, width])
-
     this.yScale
       .domain(this.props.extent.y)
-      .range([height, 0])
-
-    const points = this.d3dataLayer.selectAll('circle')
+      .range([height, 0])  //Note that this is reversed
+    
+    this.updateAxes()
+    
+    // Add the data points
+    const points = this.d3dataLayer.selectAll('.data-point')
       .data(this.props.points)
 
+    const t = this.getCurrentTransform()
     const setPointCoords = selection => selection
-      .attr('cx', d => this.xScale(d[0]))
+      // users can only zoom & pan in the x-direction
+      .attr('cx', d => t.rescaleX(this.xScale)(d[0]))
       .attr('cy', d => this.yScale(d[1]))
 
     if (shouldAnimate) {
       points.enter()
-        .append('circle')
+        .append('circle')  // Note: all circles are of class '.data-point'
         .call(setPointStyle)
         .merge(points)
         .transition()
@@ -83,30 +137,93 @@ class LightCurveViewer extends Component {
         .call(setPointCoords)
     }
   }
+  
+  getCurrentTransform () {
+    return (d3.event && d3.event.transform)
+      || d3.zoomTransform(this.d3interfaceLayer.node())
+      || d3.zoomIdentity
+  }
 
+  /*
+  Initialises the D3 scatterplot chart.
+  The chart is divided into multiple layers (both functional and decorative).
+  IMPORTANT: layers are added in z-index order, lowest first.
+   */
   initChart () {
     const container = this.svgContainer.current
     this.d3svg = d3.select(container)
       .append('svg')
-      .attr('class', 'light-curve-viewer')
-      .attr('height', '100%')
-      .attr('width', '100%')
-
-    this.d3svg.call(addBackgroundLayer)
-    this.d3dataLayer = this.d3svg
-      .append('g')
-      .attr('class', 'data-layer')
-    this.d3svg.call(addBorderLayer)
+        .attr('class', 'light-curve-viewer')
+        .attr('height', '100%')
+        .attr('width', '100%')
     this.xScale = d3.scaleLinear()
     this.yScale = d3.scaleLinear()
+    
+    // Deco layer
+    this.d3svg.call(addBackgroundLayer)
+    
+    /*
+    Data layer: data points are added here in drawChart()
+    */
+    this.d3dataLayer = this.d3svg.call(addDataLayer)
+    
+    /*
+    Axis layer
+    Actual scaling done in updateAxes()
+     */
+    this.xAxis = d3.axisBottom(this.yScale)
+    this.yAxis = d3.axisRight(this.yScale)
+    const axisLayer = this.d3svg
+      .append('g')
+        .attr('class', 'axis-layer')
+    this.d3axisX = axisLayer
+      .append('g')
+        .attr('class', 'x-axis')
+        .call(this.xAxis)
+    this.d3axisY = axisLayer
+      .append('g')
+        .attr('class', 'y-axis')
+        .call(this.yAxis)
+    
+    // Deco layer
+    this.d3svg.call(addBorderLayer)
 
-    this.d3interfaceLayer = this.d3svg.call(addInterfaceLayer)
-    this.d3interfaceLayer.call(d3.zoom()
-      .scaleExtent([1, 10])
+    // Zoom controller
+    this.zoom = d3.zoom()
+      .scaleExtent([LIGHTCURVE_DATA_CONSTRAINTS.minScale, LIGHTCURVE_DATA_CONSTRAINTS.maxScale])  // Limit zoom scale
+    
       .on('zoom', () => {
-        this.d3dataLayer.attr('transform', d3.event.transform)
+        const t = d3.event.transform
+        
+        // Re-draw the data points to fit the new view
+        // Note: users can only zoom & pan in the x-direction
+        this.d3dataLayer.selectAll('.data-point')
+          .attr('cx', d => t.rescaleX(this.xScale)(d[0]))
+        
+        this.updateAxes()
       })
-    )
+    
+    /*
+    The Interface Layer is the last (i.e. top-most) layer added, capturing all
+    mouse input but making it impossible to directly interact with any layer
+    elements beneath it.
+     */
+    this.d3interfaceLayer = this.d3svg.call(addInterfaceLayer)
+    this.d3interfaceLayer.call(this.zoom)
+  }
+  
+  /*
+  Update the x-axis and y-axis to fit the new zoom/pan view.
+  Note that for light curves, we only allow panning & zooming in the x-direction.
+   */
+  updateAxes() {
+    const t = this.getCurrentTransform()
+    
+    this.xAxis.scale(t.rescaleX(this.xScale))
+    this.d3axisX.call(this.xAxis)
+    
+    this.yAxis.scale(this.yScale)
+    this.d3axisY.call(this.yAxis)
   }
 
   render () {
