8067c1fac225a404937bc8f0721bc7ef81df9718
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2018-11-13 19:02:51 +0000

    Classifier - TESS Light Curve Viewer - Improved Axes (#261)
    * Add comments and minor cleanup
    
    * WIP axis labels
    
    * Add basic scales that don't move with the pan or zoom
    
    * X and Y axes now respect the zoom and pan
    
    * WIP scale data points
    
    * Zoom and pan now locked to the x-direction only woo hoo
    
    * Users can only pan in the x-direction, and only within the bounds of the data
    
    * Fit data into viewer, again. No more shared frame of reference between Subjects.
    
    * Update comments
    
    * Add getCurrentTransformMethod()
    
    * Minor update for circle.data-point calls
    
    * Update setPointStyle tests
    
    * Add margins to Light Curve Viewer
    
    * Fix incorrect assignments as a result of d3.selection.call
    
    * Add Grab cursor style to LCV SVG
    
    * Add axis labels
    
    * Alternative style: use more screen space
    
    * LightCurveViewer config moved to props
    
    * Define LCV PropTypes
    
    * AddDataMask now moved to its own D3 call
    
    * AddAxisLabel now its own D3 call
    
    * LightCurveViewer.updatePresentation broken into smaller more easily tested components

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addAxisLabel.js
@@ -0,0 +1,8 @@
+export default function addAxisLabel (selection, className, text, axisLabelStyle) {
+  return selection
+    .append('text')
+      .attr('class', className)
+      .style('font-size', axisLabelStyle.fontSize)
+      .style('font-family', axisLabelStyle.fontFamily)
+      .text(text)
+}
---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addAxisLabel.spec.js
@@ -0,0 +1,7 @@
+import addAxisLabel from './addAxisLabel'
+
+describe('LightCurveViewer > d3 > addAxisLabel', function () {
+  it('should exist', function () {
+    expect(addAxisLabel).to.be.a('function')
+  })
+})
