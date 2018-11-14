d5b8b94b09602784c031f829e4cdbd69c9900d18
Roger Hutchings <roger@hutchin.gs>
2018-11-14 14:24:56 +0000

    Add grommet-theme build to bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -10,6 +10,12 @@ LERNA=$ROOT_DIR/node_modules/.bin/lerna
 printf 'Installing root dependencies...'
 (cd $ROOT_DIR && npm install)
 
+printf 'Bootstrapping `lib-grommet-theme`...'
+$LERNA bootstrap --scope="@zooniverse/grommet-theme"
+printf '\n'
+$LERNA exec --scope="@zooniverse/grommet-theme" -- npm run build
+printf '\n'
+
 printf 'Bootstrapping `lib-react-components`...'
 $LERNA bootstrap --scope="@zooniverse/react-components"
 printf '\n'
