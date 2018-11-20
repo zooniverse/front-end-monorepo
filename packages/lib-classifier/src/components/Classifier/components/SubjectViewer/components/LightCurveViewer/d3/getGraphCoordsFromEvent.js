51ac5ea444d75efc2d1784978a4993144e308136
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-11-20 15:44:34 +0000

    WIP Add provisional Add Annotation logic that locates x-y coords

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/getGraphCoordsFromEvent.js
+++
@@ -1,8 +0,0 @@
-//TODO: generalise
-
-import * as d3 from 'd3'
-
- export default function getXFromEvent (context, xScale) {
-  const coords = d3.mouse(context)
-  return Math.round(xScale.invert(coords[0]))
-}
