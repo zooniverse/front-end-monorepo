8cf38bdef99c5e291e1e31a8729966072683d731
Sarah Allen <sarah@zooniverse.org>
2018-12-18 11:14:53 -0600

    Add support for jsonb default for steps

--- packages/lib-classifier/src/store/Workflow.js
+++ packages/lib-classifier/src/store/Workflow.js
@@ -1,14 +1,16 @@
 import { types } from 'mobx-state-tree'
 import Resource from './Resource'
 
+// The db type for steps is jsonb which is being serialized as an empty object when not defined.
+// Steps will be stored as an array of pairs to preserve order. 
 const Workflow = types
   .model('Workflow', {
     configuration: types.frozen({}),
     display_name: types.string,
     first_task: types.maybe(types.string),
-    steps: types.array(types.array(
+    steps: types.union(types.frozen({}), types.array(types.array(
       types.union(types.string, types.frozen())
-    )),
+    ))),
     tasks: types.maybe(types.frozen()),
     version: types.string
   })
