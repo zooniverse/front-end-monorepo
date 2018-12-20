e11053879169255c8a58394159e292195ec8f0ae
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2018-12-20 21:11:02 +0000

    Use id prop instead of autogenerating ids (which leads to SSR errors) (#330)

--- packages/lib-react-components/src/ZooFooter/components/ZooniverseLogotype/ZooniverseLogotype.spec.js
+++ packages/lib-react-components/src/ZooFooter/components/ZooniverseLogotype/ZooniverseLogotype.spec.js
@@ -1,20 +1,46 @@
-/* global expect */
-/* eslint-disable func-names, prefer-arrow-callback */
-/* eslint import/no-extraneous-dependencies: ["error", { "devDependencies": true  }] */
 import { mount } from 'enzyme'
 import React from 'react'
+
 import ZooniverseLogotype from './ZooniverseLogotype'
 
 describe('ZooniverseLogotype', function () {
-  it('mounts ZooniverseLogotype', function () {
-    const wrapper = mount(<ZooniverseLogotype />)
-    expect(wrapper.find(ZooniverseLogotype)).to.have.length(1)
+  let wrapper
+  const ID = 'foobar'
+  const WIDTH = 100
+
+  before(function () {
+    wrapper = mount(<ZooniverseLogotype id={ID} width={WIDTH} />)
   })
 
-  it('has a unique aria-labelledby for each instance mounted', function () {
-    const wrapper = mount(<div><ZooniverseLogotype /><ZooniverseLogotype /></div>)
-    const firstSVGlabel = wrapper.find('svg').first().prop('aria-labelledby')
-    const secondSVGlabel = wrapper.find('svg').last().prop('aria-labelledby')
-    expect(firstSVGlabel).to.not.equal(secondSVGlabel)
+  it('should render without crashing', function () {})
+
+  it('should use the `id` prop for `aria-labelledby` and `title`', function () {
+    const labelledby = wrapper.find('svg').prop('aria-labelledby')
+    const titleId = wrapper.find('title').prop('id')
+    expect(labelledby).to.equal(ID)
+    expect(titleId).to.equal(ID)
+  })
+
+  it('should set the width from the `width` prop', function () {
+    const svg = wrapper.find('svg')
+    expect(svg.prop('width')).to.equal(WIDTH)
+  })
+
+  it('should calculate the height', function () {
+    const svg = wrapper.find('svg')
+    expect(svg.prop('height')).to.be.ok
+  })
+
+  it('should pass through any other props to the SVG', function () {
+    const FOO = 'bar'
+    const wrapperWithProps = mount(
+      <ZooniverseLogotype
+        id={ID}
+        width={WIDTH}
+        foo={FOO}
+      />
+    )
+
+    expect(wrapperWithProps.find('svg').prop('foo')).to.equal(FOO)
   })
 })
--- packages/lib-react-components/src/ZooHeader/components/ZooniverseLogo/ZooniverseLogo.spec.js
+++ packages/lib-react-components/src/ZooHeader/components/ZooniverseLogo/ZooniverseLogo.spec.js
@@ -1,17 +1,42 @@
 import { mount } from 'enzyme'
 import React from 'react'
+
 import ZooniverseLogo from './ZooniverseLogo'
 
 describe('ZooniverseLogo', function () {
-  it('mounts ZooniverseLogo', function () {
-    const wrapper = mount(<ZooniverseLogo />)
-    expect(wrapper.find('ZooniverseLogo')).to.have.length(1)
+  let wrapper
+  const ID = 'foobar'
+  const SIZE = '100px'
+
+  before(function () {
+    wrapper = mount(<ZooniverseLogo id={ID} size={SIZE} />)
   })
 
-  it('has a unique aria-labelledby for each instance mounted', function () {
-    const wrapper = mount(<div><ZooniverseLogo /><ZooniverseLogo /></div>)
-    const firstSVGlabel = wrapper.find('svg').first().prop('aria-labelledby')
-    const secondSVGlabel = wrapper.find('svg').last().prop('aria-labelledby')
-    expect(firstSVGlabel).to.not.equal(secondSVGlabel)
+  it('should render without crashing', function () {})
+
+  it('should use the `id` prop for `aria-labelledby` and `title`', function () {
+    const labelledby = wrapper.find('svg').prop('aria-labelledby')
+    const titleId = wrapper.find('title').prop('id')
+    expect(labelledby).to.equal(ID)
+    expect(titleId).to.equal(ID)
+  })
+
+  it('should set the height and width from the `size` prop', function () {
+    const svg = wrapper.find('svg')
+    expect(svg.prop('height')).to.equal(SIZE)
+    expect(svg.prop('width')).to.equal(SIZE)
+  })
+
+  it('should pass through any other props to the SVG', function () {
+    const FOO = 'bar'
+    const wrapperWithProps = mount(
+      <ZooniverseLogo
+        id={ID}
+        size={SIZE}
+        foo={FOO}
+      />
+    )
+
+    expect(wrapperWithProps.find('svg').prop('foo')).to.equal(FOO)
   })
 })
