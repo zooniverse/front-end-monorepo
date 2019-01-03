bd632142a79b856ec21eacbd337edf08924ab84e
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-01-03 16:19:49 +0000

    Update bootstrap script (#344)
    Add comments, build async-states, install missing dependencies

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-# Installs top level dependencies, installs package dependencies for
-# `lib-react-components` and builds the library, then bootstraps the remaining
-# packages.
+# Script for bootstrapping the monorepo into a working state for development.
+#
+# Runs the following tasks:
+#   - Install top level dependencies
+#   - Install dependencies for `@zooniverse/async-states` and build
+#   - Install dependencies for `@zooniverse/panoptes-js`
+#   - Install dependencies for `@zooniverse/grommet-theme` and build
+#   - Install dependencies for `@zooniverse/react-components` and build
+#   - Install dependencies for `@zooniverse/auth` and build
+#   - Install dependencies for remaining packages
 
 ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd)"
 LERNA=$ROOT_DIR/node_modules/.bin/lerna
 
-printf 'Installing root dependencies...'
-(cd $ROOT_DIR && npm install)
+printf 'Bootstrapping the monorepo!\n\n'
 
-printf 'Bootstrapping `lib-grommet-theme`...'
-$LERNA bootstrap --scope="@zooniverse/grommet-theme"
+printf 'Installing root dependencies...\n'
+(cd $ROOT_DIR && npm install)
 printf '\n'
+
+printf 'Bootstrapping `lib-async-states`...\n'
+$LERNA bootstrap --scope="@zooniverse/async-states"
+printf '\n'
+
+printf 'Bootstrapping `lib-panoptes-js`...\n'
+$LERNA bootstrap --scope="@zooniverse/panoptes-js"
+printf '\n'
+
+printf 'Bootstrapping `lib-grommet-theme`...\n'
+$LERNA bootstrap --scope="@zooniverse/grommet-theme"
 $LERNA exec --scope="@zooniverse/grommet-theme" -- npm run build
 printf '\n'
 
-printf 'Bootstrapping `lib-react-components`...'
+printf 'Bootstrapping `lib-react-components`...\n'
 $LERNA bootstrap --scope="@zooniverse/react-components"
-printf '\n'
 $LERNA exec --scope="@zooniverse/react-components" -- npm run build
 printf '\n'
 
-printf 'Bootstrapping `lib-auth`...'
+printf 'Bootstrapping `lib-auth`...\n'
 $LERNA bootstrap --scope="@zooniverse/auth"
 printf '\n'
 $LERNA exec --scope="@zooniverse/auth" -- npm run build
 printf '\n'
 
-printf 'Bootstrapping remaining packages...'
+printf 'Bootstrapping remaining packages...\n'
+printf '\n'
 $LERNA bootstrap
