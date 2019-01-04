61a04bcfae0b5712d6f56d2e65aecd65f4a60aa9
Roger Hutchings <roger@hutchin.gs>
2019-01-04 15:31:26 +0000

    Add specific resolution for styled-components to avoid duplicate declaration warnings

--- packages/app-project/next.config.js
+++ packages/app-project/next.config.js
@@ -1,5 +1,15 @@
+const path = require('path')
+
 module.exports = {
   // Disable file-system routing
   // https://github.com/zeit/next.js#disabling-file-system-routing
-  useFileSystemPublicRoutes: false
+  useFileSystemPublicRoutes: false,
+
+  webpack: (config) => {
+    const modifiedConfig = Object.assign({}, config)
+
+    modifiedConfig.resolve.alias['styled-components'] = path.resolve('.', 'node_modules', 'styled-components')
+
+    return modifiedConfig
+  }
 }
