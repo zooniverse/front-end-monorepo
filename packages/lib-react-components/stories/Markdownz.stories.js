034d97c3178cb52ad0cdd1e257e9c2e0bc2365c8
Sarah Allen <sarah@zooniverse.org>
2019-01-09 17:07:36 -0600

    More tests and stories

--- packages/lib-react-components/stories/Markdownz.stories.js
+++ packages/lib-react-components/stories/Markdownz.stories.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Grommet, Box } from 'grommet'
+import styled from 'styled-components'
+import { Grommet, Box, TableRow } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
 
 import { storiesOf } from '@storybook/react'
 
 import { Markdownz } from '../src'
 import markdownExample from './lib/example.md'
+
+const TableRowWithBorder = styled(TableRow)`
+  border-top: solid thin black;
+  border-bottom: solid thin black;
+`
 
 storiesOf('Markdownz', module)
   // .addParameters({
@@ -29,6 +35,13 @@ storiesOf('Markdownz', module)
     <Grommet theme={zooTheme}>
       <Box>
         <Markdownz projectSlug='zooniverse/snapshot-wakanda'>{markdownExample}</Markdownz>
+      </Box>
+    </Grommet>
+  )
+  .add('With custom components', () =>
+    <Grommet theme={zooTheme}>
+      <Box>
+        <Markdownz components={{ tr: TableRowWithBorder }}>{markdownExample}</Markdownz>
       </Box>
     </Grommet>
   )
