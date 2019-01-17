8bb2f122f9246ea2957d500958c1e1a29179b64d
Roger Hutchings <roger@hutchin.gs>
2019-01-17 14:11:44 +0000

    Add modal focus wrapper

--- packages/app-project/components/AuthModals/components/LoginModal/LoginModal.js
+++ packages/app-project/components/AuthModals/components/LoginModal/LoginModal.js
@@ -1,63 +1,67 @@
 import counterpart from 'counterpart'
 import { Box, Button, FormField, Layer, TextInput } from 'grommet'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 
 import en from './locales/en'
+import withModalFocus from './withModalFocus'
 
 counterpart.registerTranslations('en', en)
 
-export default function LoginModal (props) {
-  const {
-    canLogin,
-    closeLoginModal,
-    formState,
-    loading,
-    updateField,
-    submit
-  } = props
+@withModalFocus
+export default class LoginModal extends Component {
+  render () {
+    const {
+      canLogin,
+      closeLoginModal,
+      formState,
+      loading,
+      updateField,
+      submit
+    } = this.props
 
-  return (
-    <Layer
-      onClickOutside={closeLoginModal}
-      onEsc={closeLoginModal}
-    >
-      <Box pad='medium'>
-        <FormField
-          htmlFor='login-modal-login'
-          label={counterpart('LoginModal.username')}
-          name='login'
-        >
-          <TextInput
-            disabled={loading}
-            id='login-modal-login'
+    return (
+      <Layer
+        onClickOutside={closeLoginModal}
+        onEsc={closeLoginModal}
+      >
+        <Box pad='medium'>
+          <FormField
+            htmlFor='login-modal-login'
+            label={counterpart('LoginModal.username')}
             name='login'
-            onChange={updateField}
-            value={formState.login}
+          >
+            <TextInput
+              disabled={loading}
+              id='login-modal-login'
+              name='login'
+              onChange={updateField}
+              value={formState.login}
+            />
+          </FormField>
+          <FormField
+            htmlFor='login-modal-password'
+            label={counterpart('LoginModal.password')}
+            name='login'
+          >
+            <TextInput
+              disabled={loading}
+              id='login-modal-password'
+              name='password'
+              onChange={updateField}
+              type='password'
+              value={formState.password}
+            />
+          </FormField>
+          <Button
+            disabled={!canLogin || loading}
+            label={counterpart('LoginModal.login')}
+            onClick={submit}
           />
-        </FormField>
-        <FormField
-          htmlFor='login-modal-password'
-          label={counterpart('LoginModal.password')}
-          name='login'
-        >
-          <TextInput
-            disabled={loading}
-            id='login-modal-password'
-            name='password'
-            onChange={updateField}
-            type='password'
-            value={formState.password}
-          />
-        </FormField>
-        <Button
-          disabled={!canLogin || loading}
-          label={counterpart('LoginModal.login')}
-          onClick={submit}
-        />
-      </Box>
-    </Layer>
-  )
+        </Box>
+      </Layer>
+    )
+  }
 }
 
 LoginModal.propTypes = {
