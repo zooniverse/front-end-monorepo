90375738f8ea88fe89c3c4cdacde4ca09b4805fe
Sarah Allen <sarah@zooniverse.org>
2019-01-22 13:34:06 -0600

    Make the fallback for video the same as audio

--- packages/lib-react-components/src/Media/components/Video/Video.js
+++ packages/lib-react-components/src/Media/components/Video/Video.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { Box, Paragraph, Video as GrommetVideo } from 'grommet'
-import counterpart from 'counterpart'
-import en from './locales/en'
+import { Anchor, Box, Video as GrommetVideo } from 'grommet'
 import { propTypes, defaultProps } from '../../helpers/mediaPropTypes'
-
-counterpart.registerTranslations('en', en)
 
 export default function Video({ alt, controls, fit, src }) {
   const controlsOption = (controls) ? 'below' : false
   return (
     <Box>
       <GrommetVideo a11yTitle={alt} controls={controlsOption} fit={fit} preload='metadata' src={src}>
-        <Paragraph>{counterpart('Video.support')}</Paragraph>
+        <Anchor href={src} label={alt} />
       </GrommetVideo>
     </Box>
   )
