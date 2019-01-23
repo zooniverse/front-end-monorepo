1abf1a3e84a3d5c418d53f2d5b228aaf32079788
Sarah Allen <sarah@zooniverse.org>
2019-01-23 10:57:54 -0600

    Fix rebase mistake

--- packages/lib-react-components/src/Media/Media.js
+++ packages/lib-react-components/src/Media/Media.js
@@ -1,18 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
-import PropTypes from 'prop-types'
->>>>>>> Add Media component. Make ZooniverseLogo available for export
 import mime from 'mime-types'
 import Audio from './components/Audio'
 import ThumbnailImage from './components/ThumbnailImage'
 import Video from './components/Video'
-<<<<<<< HEAD
 import { propTypes, defaultProps } from './helpers/mediaPropTypes'
-=======
-
-const DEFAULT_THUMBNAIL_DIMENSION = 999
->>>>>>> Add Media component. Make ZooniverseLogo available for export
 
 export default function Media(props) {
   const mimeType = mime.lookup(props.src)
@@ -39,32 +30,9 @@ export default function Media(props) {
 }
 
 Media.propTypes = {
-<<<<<<< HEAD
   ...propTypes
 }
 
 Media.defaultProps = {
   ...defaultProps
-=======
-  alt: PropTypes.string,
-  controls: PropTypes.bool,
-  delay: PropTypes.number,
-  fit: PropTypes.oneOf(['contain', 'cover']),
-  height: PropTypes.number,
-  origin: PropTypes.string,
-  placeholder: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
-  src: PropTypes.string.isRequired,
-  width: PropTypes.number
-}
-
-Media.defaultProps = {
-  alt: '',
-  controls: true,
-  delay: 0,
-  fit: 'cover',
-  height: DEFAULT_THUMBNAIL_DIMENSION,
-  origin: 'https://thumbnails.zooniverse.org',
-  placeholder: null,
-  width: DEFAULT_THUMBNAIL_DIMENSION
->>>>>>> Add Media component. Make ZooniverseLogo available for export
 }
