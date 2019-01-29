b94f2914f0f0a05c108cd407b8a0ec4ee2185662
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2019-01-29 12:04:47 +0000

    Classifier - TESS Light Curve Viewer - add Graph2dRangeX task (for Task Area) (#376)
    * LightCurveViewer: add Graph2dRangeXTask to Classifier Task Area
    
    * LightCurveViewer: add tests for Graph2dRangeXTask
    
    * LightCurveViewer: add 'number of marks' counter on Task
    
    * LightCurveViewer: remove unnecessary elements from Graph2dRangeX Task
    
    * LightCurveViewer: add counterpart translations to Graph2dRangeXTask

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/helpers/getTaskComponent.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/helpers/getTaskComponent.js
@@ -1,9 +1,11 @@
 import DrawingTask from '../components/DrawingTask'
+import Graph2dRangeXTask from '../components/Graph2dRangeXTask'
 import SingleChoiceTask from '../components/SingleChoiceTask'
 import MultipleChoiceTask from '../components/MultipleChoiceTask'
 
 const taskTypes = {
   drawing: DrawingTask,
+  graph2dRangeX: Graph2dRangeXTask,
   single: SingleChoiceTask,
   multiple: MultipleChoiceTask
 }
