0ca658e43c0499eedd0506e4b5018cbc6b235af5
Roger Hutchings <roger@hutchin.gs>
2019-02-05 17:07:56 +0000

    Remove outdated ProjectImage test
    This component now uses the Media component

--- packages/app-project/components/ClassifyPage/components/FinishedForTheDay/components/ProjectImage/ProjectImage.spec.js
+++ packages/app-project/components/ClassifyPage/components/FinishedForTheDay/components/ProjectImage/ProjectImage.spec.js
@@ -1,4 +1,4 @@
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import React from 'react'
 
 import ProjectImage from './ProjectImage'
@@ -10,27 +10,10 @@ const imageSrc = 'foobar.jpg'
 
 describe('Component > ProjectImage', function () {
   before(function () {
-    wrapper = mount(<ProjectImage imageSrc={imageSrc} projectName={projectName} />)
+    wrapper = shallow(<ProjectImage imageSrc={imageSrc} projectName={projectName} />)
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok
-  })
-
-  it('should contain a `Placeholder` component', function () {
-    expect(wrapper.find('Placeholder')).to.have.lengthOf(1)
-  })
-
-  it('should have a `<noscript />` image for SSR', function () {
-    const noscriptWrapper = wrapper.find('noscript')
-    expect(noscriptWrapper).to.have.lengthOf(1)
-    expect(noscriptWrapper.find('img')).to.have.lengthOf(1)
-  })
-
-  it('should have an alt', function () {
-    const imgsWrapper = wrapper.find('img')
-    imgsWrapper.forEach(imgWrapper =>
-      expect(imgWrapper.prop('alt')).to.equal(`Image for ${projectName}`)
-    )
   })
 })
