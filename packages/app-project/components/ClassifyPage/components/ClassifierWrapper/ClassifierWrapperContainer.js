d32567369150c598c255dd1f1a077816fdb1fd60
Roger Hutchings <roger@hutchin.gs>
2019-02-05 17:30:21 +0000

    Add classifier to app-project

---
+++ packages/app-project/components/ClassifyPage/components/ClassifierWrapper/ClassifierWrapperContainer.js
@@ -0,0 +1,38 @@
+import { inject, observer } from 'mobx-react'
+import { shape } from 'prop-types'
+import React, { Component } from 'react'
+import auth from 'panoptes-client/lib/auth'
+import Classifier from '@zooniverse/classifier'
+
+function storeMapper (stores) {
+  const { project } = stores.store
+  // We return a POJO here, as the `project` resource is also stored in a
+  // `mobx-state-tree` store in the classifier and an MST node can't be in two
+  // stores at the same time.
+  return {
+    project: project.toJSON()
+  }
+}
+
+@inject(storeMapper)
+@observer
+export default class ClassifierWrapperContainer extends Component {
+  render () {
+    const { authClient, project } = this.props
+    return (
+      <Classifier
+        authClient={authClient}
+        project={project}
+      />
+    )
+  }
+}
+
+ClassifierWrapperContainer.propTypes = {
+  authClient: shape({}),
+  project: shape({}),
+}
+
+ClassifierWrapperContainer.defaultProps = {
+  authClient: auth
+}
