a947a0a2cd93b08e2e5638743b7c05baa2f33bfe
Jim O'Donnell <jim@zooniverse.org>
2019-02-12 13:41:11 +0000

    Export collections mocks

--- packages/lib-panoptes-js/src/resources/collections/index.js
+++ packages/lib-panoptes-js/src/resources/collections/index.js
@@ -1,6 +1,7 @@
 const { create, get, update, del } = require('./rest')
 const { addSubjects, removeSubjects } = require('./commonRequests')
 const { endpoint } = require('./helpers')
+const mocks = require('./mocks')
 
 module.exports = {
   create,
@@ -9,5 +10,6 @@ module.exports = {
   update,
   endpoint,
   addSubjects,
-  removeSubjects
+  removeSubjects,
+  mocks
 }
