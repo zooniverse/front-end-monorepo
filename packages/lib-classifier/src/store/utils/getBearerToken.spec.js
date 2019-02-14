ab7f6a3b5a0d5e86bfd4c860b9b851706bc746eb
Sarah Allen <sarah@zooniverse.org>
2019-02-14 14:50:40 -0600

    Add tests for getBearerToken

---
+++ packages/lib-classifier/src/store/utils/getBearerToken.spec.js
@@ -0,0 +1,74 @@
+import sinon from 'sinon'
+import getBearerToken from './getBearerToken'
+
+describe('Store utils > getBearerToken', function () {
+  it('returns an empty string if the authClient is undefined', function () {
+    return getBearerToken().then((token) => {
+      return expect(token).to.equal('')
+    })
+  })
+
+  describe('when using a first party auth client', function () {
+    it('calls authClient.checkBearerToken', function () {
+      const mockAuthClient = {
+        checkBearerToken: sinon.spy()
+      }
+
+      getBearerToken(mockAuthClient)
+      expect(mockAuthClient.checkBearerToken).to.have.been.calledOnce
+    })
+
+    it('returns an empty string if token returned from auth client is falsey', function () {
+      const mockAuthClient = {
+        checkBearerToken: sinon.stub().callsFake(() => Promise.resolve(''))
+      }
+
+      return getBearerToken(mockAuthClient).then((token) => {
+        return expect(token).to.equal('')
+      })
+    })
+
+    it('returns the bearer token if token returned from auth client is truthy', function () {
+      const mockToken = '1234'
+      const mockAuthClient = {
+        checkBearerToken: sinon.stub().callsFake(() => Promise.resolve(mockToken))
+      }
+
+      return getBearerToken(mockAuthClient).then((token) => {
+        return expect(token).to.equal(`Bearer ${mockToken}`)
+      })
+    })
+  })
+
+  describe('when using an oauth client', function () {
+    it('calls authClient.checkBearerToken', function () {
+      const mockOAuthClient = {
+        checkBearerToken: sinon.spy()
+      }
+
+      getBearerToken(mockOAuthClient)
+      expect(mockOAuthClient.checkBearerToken).to.have.been.calledOnce
+    })
+
+    it('returns an empty string if token returned from auth client is falsey', function () {
+      const mockOAuthClient = {
+        checkBearerToken: sinon.stub().callsFake(() => Promise.resolve({ access_token: '' }))
+      }
+
+      return getBearerToken(mockOAuthClient).then((token) => {
+        return expect(token).to.equal('')
+      })
+    })
+
+    it('returns the bearer token if token returned from auth client is truthy', function () {
+      const mockToken = '1234'
+      const mockOAuthClient = {
+        checkBearerToken: sinon.stub().callsFake(() => Promise.resolve({ access_token: mockToken }))
+      }
+
+      return getBearerToken(mockOAuthClient).then((token) => {
+        return expect(token).to.equal(`Bearer ${mockToken}`)
+      })
+    })
+  })
+})
