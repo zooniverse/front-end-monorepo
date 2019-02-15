d2c252b3b336f0b48029b3aa07cf7f17bff3ebe5
Jim O'Donnell <jim@eatyourgreens.org.uk>
2019-02-15 09:24:49 +0000

    Add collections to the API client (#430)
    * Add basic collections API functions
    Add functions for creating, getting, updating and deleting collections. Add functions for adding/removing subjects to/from collections.
    
    * Create collection: add links
    Add linked projects and subjects to new collections.
    
    * Test collections.get
    Test collections get requests with a collection ID or with query params.
    Add some mock responses.
    
    * Test collections update
    Fix broken update method.
    
    * Test collections.delete
    
    * Test collection links
    Test adding and removing subjects.
    Fix a typo.
    
    * Add client.collections
    Add collections to the exported client.
    
    * Test collections.create
    Test collections.create(). Make linked projects optional for collections.
    
    * Export collections mocks
    
    * Link collections to a single project
    Replace params.projects with params.project in collections.create(params).
    Update tests.
    
    * Fix collections.create payload
    Update collections.create() to send the payload expected by Panoptes, which is { collections: data }.

---
+++ packages/lib-panoptes-js/src/resources/collections/commonRequests.js
@@ -0,0 +1,29 @@
+const panoptes = require('../../panoptes')
+const { endpoint } = require('./helpers')
+const { raiseError } = require('../../utilityFunctions')
+
+function addSubjects (params) {
+  const collectionId = params && params.id
+  if (!collectionId) return raiseError('Collections add subject: collections ID is required.', 'error')
+  const authorization = (params && params.authorization) ? params.authorization : ''
+  const subjects = params && params.subjects
+  if (!subjects || !Array.isArray(subjects)) return raiseError('Collections add subjects: subjects array is required.', 'error')
+
+  if (typeof collectionId !== 'string') return raiseError('Collections add link: collections id must be a string.', 'typeError')
+
+  return panoptes.post(`${endpoint}/${collectionId}/links/subjects`, { subjects }, authorization)
+}
+
+function removeSubjects (params) {
+  const collectionId = params && params.id
+  if (!collectionId) return raiseError('Collections remove subject: collections ID is required.', 'error')
+  const authorization = (params && params.authorization) ? params.authorization : ''
+  const subjects = params && params.subjects
+  if (!subjects || !Array.isArray(subjects)) return raiseError('Collections remove subjects: subjects array is required.', 'error')
+
+  if (typeof collectionId !== 'string') return raiseError('Collections remove link: collections id must be a string.', 'typeError')
+
+  return panoptes.del(`${endpoint}/${collectionId}/links/subjects/${subjects.join(',')}`, authorization)
+}
+
+module.exports = { addSubjects, removeSubjects }
---
+++ packages/lib-panoptes-js/src/resources/collections/rest.spec.js
@@ -0,0 +1,278 @@
+const { expect } = require('chai')
+const nock = require('nock')
+
+const { config } = require('../../config')
+const { resources, responses } = require('./mocks')
+const { endpoint } = require('./helpers')
+const collections = require('./index')
+
+describe('Collections resource REST requests', function () {
+  describe('get', function () {
+    describe('with a collection ID', function () {
+      const expectedGetResponse = responses.get.collection
+      let scope
+
+      before(function () {
+        scope = nock(config.host)
+          .persist()
+          .get(`${endpoint}/10`)
+          .query(true)
+          .reply(200, expectedGetResponse)
+      })
+
+      after(function () {
+        nock.cleanAll()
+      })
+
+      it('should error if the collection ID is not a string', async function () {
+        try {
+          await collections.get({ id: 1 })
+          expect.fail()
+        } catch (error) {
+          expect(error.message).to.equal('Collections: Get request id must be a string.')
+        }
+      })
+
+      it('should return a collection', async function () {
+        const response = await collections.get({ id: '10' })
+        expect(response.body).to.eql(expectedGetResponse)
+      })
+    })
+
+    describe('without a collection ID', function () {
+      const expectedGetResponse = responses.get.collections
+      const query = {
+        project_ids: '1',
+        owner: 'test',
+        http_cache: 'true'
+      }
+      let scope
+
+      before(function () {
+        scope = nock(config.host)
+          .persist()
+          .get(`${endpoint}`)
+          .query(query)
+          .reply(200, expectedGetResponse)
+      })
+
+      after(function () {
+        nock.cleanAll()
+      })
+
+      it('should query the API for a list of collections', async function () {
+        const response = await collections.get({ query })
+        expect(response.body).to.eql(expectedGetResponse)
+      })
+    })
+
+    describe('with an authorization param', function () {
+      const expectedGetResponse = responses.get.collection
+      let scope
+
+      before(function () {
+        scope = nock(config.host)
+          .persist()
+          .get(uri => uri.includes(endpoint))
+          .query(true)
+          .reply(200, expectedGetResponse)
+      })
+
+      after(function () {
+        nock.cleanAll()
+      })
+
+      it('should add the Authorization header to the request', async function () {
+        const response = await collections.get({ id: '10', authorization: '12345' })
+        expect(response.req.headers.authorization).to.equal('12345')
+      })
+    })
+  })
+
+  describe('update', function () {
+    const data = {
+      display_name: 'my test collection'
+    }
+    const expectedPutResponse = Object.assign({}, responses.get.collection, data)
+    const requestBody = { collections: data }
+    let scope
+
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .put(`${endpoint}/10`, requestBody)
+        .query(true)
+        .reply(200, expectedPutResponse)
+    })
+
+    after(function () {
+      nock.cleanAll()
+    })
+
+    it('should raise an error if a collection is not specified', async function () {
+      try {
+        await collections.update({})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections: Update request id must be present.')
+      }
+    })
+
+    it('should raise an error if no changes are specified', async function () {
+      try {
+        await collections.update({ id: '10' })
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collection update: payload not supplied.')
+      }
+    })
+
+    it('should update the specified collection', async function () {
+      const response = await collections.update({ id: '10', data })
+      expect(response.body).to.eql(expectedPutResponse)
+    })
+  })
+
+  describe('create', function () {
+    const expectedCreateResponse = responses.get.collection
+    const data = {
+      display_name: 'test collection'
+    }
+    const links = {
+      project: '2',
+      subjects: []
+    }
+    const changes = Object.assign({}, data, { links })
+    const payload = { collections: changes }
+    let scope
+
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .post(`${endpoint}`, payload)
+        .query(true)
+        .reply(201, expectedCreateResponse)
+    })
+
+    after(function () {
+      nock.cleanAll()
+    })
+
+    it('should create a new collection', async function () {
+      const response = await collections.create({ data, project: '2' })
+      expect(response.body).to.eql(expectedCreateResponse)
+    })
+  })
+
+  describe('delete', function () {
+    const expectedDeleteResponse = {}
+    let scope
+
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .delete(`${endpoint}/10`)
+        .query(true)
+        .reply(200, expectedDeleteResponse)
+    })
+
+    after(function () {
+      nock.cleanAll()
+    })
+
+    it('should raise an error if a collection is not specified', async function () {
+      try {
+        await collections.delete({})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections: Delete request id must be present.')
+      }
+    })
+
+    it('should delete the specified collection', async function () {
+      const response = await collections.delete({ id: '10' })
+      expect(response.body).to.eql(expectedDeleteResponse)
+    })
+  })
+
+  describe('add subjects', function () {
+    const expectedAddResponse = responses.get.collection
+    const subjects = ['2']
+    let scope
+
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .post(`${endpoint}/10/links/subjects`, { subjects })
+        .query(true)
+        .reply(200, expectedAddResponse)
+    })
+
+    after(function () {
+      nock.cleanAll()
+    })
+
+    it('should raise an error if a collection is not specified', async function () {
+      try {
+        await collections.addSubjects({})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections add subject: collections ID is required.')
+      }
+    })
+
+    it('should raise an error if subjects is not an array', async function () {
+      try {
+        await collections.addSubjects({ id: '10', subjects: '2'})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections add subjects: subjects array is required.')
+      }
+    })
+
+    it('should add subjects to the specified collection', async function () {
+      const response = await collections.addSubjects({ id: '10', subjects })
+      expect(response).to.be.ok
+    })
+  })
+
+  describe('remove subjects', function () {
+    const expectedDeleteResponse = {}
+    let scope
+
+    before(function () {
+      scope = nock(config.host)
+        .persist()
+        .delete(`${endpoint}/10/links/subjects/2`)
+        .query(true)
+        .reply(200, expectedDeleteResponse)
+    })
+
+    after(function () {
+      nock.cleanAll()
+    })
+
+    it('should raise an error if a collection is not specified', async function () {
+      try {
+        await collections.removeSubjects({})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections remove subject: collections ID is required.')
+      }
+    })
+
+    it('should raise an error if subjects is not an array', async function () {
+      try {
+        await collections.removeSubjects({ id: '10', subjects: '2'})
+        expect.fail()
+      } catch (error) {
+        expect(error.message).to.equal('Collections remove subjects: subjects array is required.')
+      }
+    })
+
+    it('should unlink the specified subjects', async function () {
+      const response = await collections.removeSubjects({ id: '10', subjects: ['2'] })
+      expect(response).to.be.ok
+    })
+  })
+})
