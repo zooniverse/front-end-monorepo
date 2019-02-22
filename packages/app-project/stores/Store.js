6a333f782e54a09a45bea233adcf958cf2e8b031
Jim O'Donnell <jim@eatyourgreens.org.uk>
2019-02-22 14:38:44 +0000

    Recent classifications for project (#463)
    * Store recent subjects for user and project
    Add a Recents store. Request recents on initial user login.
    Test that the store initialises, fetches recents on user change and adds new subjects properly for both authenticated and anonymous users.
    Actions:
    recents.fetch() fetches existing recents for authenticated users.
    recents.add({ subjectId, locations }) adds a recent subject to the beginning of the recents queue.
    
    * Linting
    
    * Add tests for error cases
    Test the cases where the check for an auth token fails, or the Panoptes API errors.
    
    * Review comments
    Remove types.optional from array definition.
    Define a recentsStore variable to save duplication in the Recents tests.

--- packages/app-project/stores/Store.js
+++ packages/app-project/stores/Store.js
@@ -2,12 +2,14 @@ import { getEnv, types } from 'mobx-state-tree'
 
 import Collections from './Collections'
 import Project from './Project'
+import Recents from './Recents'
 import User from './User'
 
 const Store = types
   .model('Store', {
     collections: types.optional(Collections, {}),
     project: types.optional(Project, {}),
+    recents: types.optional(Recents, {}),
     user: types.optional(User, {})
   })
 
