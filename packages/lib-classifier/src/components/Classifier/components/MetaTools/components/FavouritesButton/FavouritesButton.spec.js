ad9709dafafada8577d0cfbce8c77bb4de9ce0ed
Sarah Allen <sarah@zooniverse.org>
2019-03-11 15:41:07 -0500

    Fix classifier tests

--- packages/lib-classifier/src/components/Classifier/components/MetaTools/components/FavouritesButton/FavouritesButton.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/MetaTools/components/FavouritesButton/FavouritesButton.spec.js
@@ -1,9 +1,9 @@
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
 import sinon from 'sinon'
 import { PlainButton } from '@zooniverse/react-components'
 
-import FavouritesButton, { Favourite, IsFavourite } from './FavouritesButton'
+import FavouritesButton, { Favourite } from './FavouritesButton'
 
 let wrapper
 
@@ -19,22 +19,22 @@ describe('Component > FavouritesButton', function () {
   it('should display an empty icon', function () {
     const button = wrapper.find(PlainButton)
     const { icon } = button.props()
-    expect(icon).to.deep.equal(<Favourite filled={false} />)
+    expect(icon).to.deep.equal(<Favourite filled={undefined} />)
   })
 
   it('should not be checked', function () {
     const checked = wrapper.prop('aria-checked')
-    expect(checked).to.be.false
+    expect(checked).to.be.undefined
   })
 
   describe('on click', function () {
     const onClickSpy = sinon.spy()
     before(function () {
-      wrapper = shallow(<FavouritesButton checked={false} onClick={onClickSpy} />)
+      wrapper = mount(<FavouritesButton checked={false} onClick={onClickSpy} />)
     })
 
     it('should call props.onClick', function () {
-      wrapper.simulate('click')
+      wrapper.find('button').simulate('click')
       expect(onClickSpy).to.have.been.calledOnce
     })
   })
@@ -46,12 +46,12 @@ describe('Component > FavouritesButton', function () {
     it('should display a filled icon', function () {
       const button = wrapper.find(PlainButton)
       const { icon } = button.props()
-      expect(icon).to.deep.equal(<Favourite filled />)
+      expect(icon).to.deep.equal(<Favourite filled="true" />)
     })
 
     it('should be checked', function () {
-      const checked = wrapper.prop('aria-checked')
-      expect(checked).to.be.true
+      const checked = wrapper.childAt(0).prop('aria-checked')
+      expect(checked).to.equal(true)
     })
   })
 })
