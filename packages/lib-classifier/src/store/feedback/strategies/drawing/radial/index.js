7d552da2d226cb5aa270a24ff464b148ec70ec79
Jim O'Donnell <jim@zooniverse.org>
2019-03-12 10:58:45 +0000

    Remove references to lab components Lab components aren't used in this module, so remove them from the strategies.

--- packages/lib-classifier/src/store/feedback/strategies/drawing/radial/index.js
+++ packages/lib-classifier/src/store/feedback/strategies/drawing/radial/index.js
@@ -1,6 +1,5 @@
 import createRule from './create-rule';
 import FeedbackMark from './feedback-mark';
-import LabComponent from '../lab-component';
 import reducer from './reducer';
 import validations from '../validations';
 
@@ -8,7 +7,6 @@ export default {
   createRule,
   FeedbackMark,
   id: 'radial',
-  labComponent: LabComponent,
   reducer,
   title: 'Radial',
   validations
