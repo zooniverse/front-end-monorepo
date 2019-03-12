90d28cfdd1610caea93dcc2a0a0784a1c70a019c
Roger Hutchings <roger@hutchin.gs>
2019-03-12 11:56:58 +0000

    Add static build config

--- packages/app-project/next.config.js
+++ packages/app-project/next.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 
 module.exports = {
-  // Disable file-system routing
-  // https://github.com/zeit/next.js#disabling-file-system-routing
-  useFileSystemPublicRoutes: false,
-
   env: {
     PANOPTES_ENV: process.env.PANOPTES_ENV || 'staging'
   },
+
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/projects/nora-dot-eisner/test-json/classify': {
+        page: '/Classify',
+        query: {
+          owner: 'nora-dot-eisner',
+          project: 'test-json'
+        }
+      },
+    }
+  },
+
+  useFileSystemPublicRoutes: false,
 
   webpack: (config) => {
     const modifiedConfig = Object.assign({}, config)
