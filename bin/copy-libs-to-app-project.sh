6a0f489c02d7eff65d4137dabbd35d8b58914e77
Roger Hutchings <roger@hutchin.gs>
2019-03-12 20:17:06 +0000

    Clean up bash script, replace imports

--- bin/copy-libs-to-app-project.sh
+++ bin/copy-libs-to-app-project.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env sh
 set -ev
 
+relpath() { python -c "import os.path; print os.path.relpath('$1','${2:-$PWD}')" ; }
+
 # Script for copying the source code for lib-react-components and lib-classifier
 # into app-project, to avoid https://github.com/zooniverse/front-end-monorepo/issues/327
 
 ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd)"
+NEW_CLASSIFIER_DIR=$ROOT_DIR/packages/app-project/src/lib/lib-classifier
+NEW_COMPONENTS_DIR=$ROOT_DIR/packages/app-project/src/lib/lib-react-components
 
-mkdir -p $ROOT_DIR/packages/app-project/src/lib/lib-classifier
+mkdir -p $NEW_CLASSIFIER_DIR $NEW_COMPONENTS_DIR
 
-rsync -razvh $ROOT_DIR/packages/lib-classifier/src/ $ROOT_DIR/packages/app-project/src/lib/lib-classifier
+rsync -razvh $ROOT_DIR/packages/lib-classifier/src/ $NEW_CLASSIFIER_DIR
+rsync -razvh $ROOT_DIR/packages/lib-react-components/src/ $NEW_COMPONENTS_DIR
 
-mkdir -p $ROOT_DIR/packages/app-project/src/lib/lib-react-components
+# Replace imports in lib-classifier - there are only a few components in app-project, and I've replaced the imports there manually.
+PKG=@zooniverse/react-components
 
-rsync -razvh $ROOT_DIR/packages/lib-react-components/src/ $ROOT_DIR/packages/app-project/src/lib/lib-react-components
+find $NEW_CLASSIFIER_DIR -type f -name "*.js" | while read fname; do
+  if grep -q $PKG $fname
+  then
+    DIR=$(dirname "${fname}")
+    DIFF=$(relpath "$NEW_COMPONENTS_DIR" "$DIR")
+    sed -i -e "s|${PKG}|${DIFF}|g" "$fname"
+
+    # Maybe something's funky with my sed syntax, but it backs up the original
+    # as example.js-e, which we don't need.
+    rm "${fname}-e"
+  fi
+done
