10b560503d7ee8d663a951bd0996310deaddb0e9
Jim O'Donnell <jim@zooniverse.org>
2019-03-14 11:01:20 +0000

    Remove Grommet from the collections modal

--- packages/app-project/src/screens/ClassifyPage/components/CollectionsModal/CollectionsModal.js
+++ packages/app-project/src/screens/ClassifyPage/components/CollectionsModal/CollectionsModal.js
@@ -1,7 +1,7 @@
 import counterpart from 'counterpart'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Grid, Grommet } from 'grommet'
+import { Grid } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Modal } from '@zooniverse/react-components'
 import styled from 'styled-components'
@@ -21,11 +21,9 @@ function CollectionsModal ({
       closeFn={closeFn}
       title={counterpart('CollectionsModal.title')}
     >
-      <Grommet>
-        <Grid columns={['2fr', '1fr']} gap='small' rows={['1fr']}>
-          {children}
-        </Grid>
-      </Grommet>
+      <Grid columns={['2fr', '1fr']} gap='small' rows={['1fr']}>
+        {children}
+      </Grid>
     </Modal>
   )
 }
