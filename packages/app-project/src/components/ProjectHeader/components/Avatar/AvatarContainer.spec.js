6723ad939e6c0ebfc54ded2ab23121eeaf2cd928
Roger Hutchings <roger@hutchin.gs>
2019-03-15 15:50:25 +0000

    Improve tests

--- packages/app-project/src/components/ProjectHeader/components/Avatar/AvatarContainer.spec.js
+++ packages/app-project/src/components/ProjectHeader/components/Avatar/AvatarContainer.spec.js
@@ -4,10 +4,16 @@ import React from 'react'
 import AvatarContainer from './AvatarContainer'
 
 let wrapper
+const avatarSrc = 'https://example.com/image.jpg'
+const projectTitle = 'Example project'
 
 describe('Component > AvatarContainer', function () {
   before(function () {
-    wrapper = shallow(<AvatarContainer.wrappedComponent child={StubComponent} />)
+    wrapper = shallow(<AvatarContainer.wrappedComponent
+      avatarSrc={avatarSrc}
+      child={StubComponent}
+      projectTitle={projectTitle}
+    />)
   })
 
   it('should render without crashing', function () {
@@ -16,6 +22,12 @@ describe('Component > AvatarContainer', function () {
 
   it('should render the `child` component prop', function () {
     expect(wrapper.find(StubComponent)).to.have.lengthOf(1)
+  })
+
+  it('should pass the required props to the child component', function () {
+    const child = wrapper.find(StubComponent)
+    expect(child.prop('src')).to.equal(avatarSrc)
+    expect(child.prop('projectTitle')).to.equal(projectTitle)
   })
 })
 
