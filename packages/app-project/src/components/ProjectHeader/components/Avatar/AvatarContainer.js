7e0663340637e6a95e3b5562bbd0ed4bc7ac59b0
Roger Hutchings <roger@hutchin.gs>
2019-03-15 11:53:20 +0000

    fix tests

--- packages/app-project/src/components/ProjectHeader/components/Avatar/AvatarContainer.js
+++ packages/app-project/src/components/ProjectHeader/components/Avatar/AvatarContainer.js
@@ -1,12 +1,11 @@
 import { inject, observer } from 'mobx-react'
-import { string } from 'prop-types'
+import { node, string } from 'prop-types'
 import React, { Component } from 'react'
 
 import Avatar from './Avatar'
 
 function storeMapper (stores) {
   const { project } = stores.store
-  console.info(project)
   return {
     avatarSrc: project.avatar.src,
     projectTitle: project.display_name
@@ -17,16 +16,21 @@ function storeMapper (stores) {
 @observer
 class AvatarContainer extends Component {
   render () {
-    const { avatarSrc, projectTitle } = this.props
+    const { avatarSrc, child: Child, projectTitle } = this.props
     return (
-      <Avatar projectTitle={projectTitle} src={avatarSrc} />
+      <Child projectTitle={projectTitle} src={avatarSrc} />
     )
   }
 }
 
 AvatarContainer.propTypes = {
   avatarSrc: string,
+  child: node,
   projectTitle: string,
+}
+
+AvatarContainer.defaultProps = {
+  child: Avatar
 }
 
 export default AvatarContainer
