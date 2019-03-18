52d3e5265dd81c0f35bd9e002be65d0d6d0576b4
Roger Hutchings <roger@hutchin.gs>
2019-03-18 13:06:04 +0000

    Set a height for the classifier, restyle field guide button to valid HTML, hide unused toolbar buttons, and ensure toolbar stretches to height

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.js
@@ -5,10 +5,7 @@ import { withTheme } from 'styled-components'
 
 import AnnotateButton from './components/AnnotateButton'
 import FieldGuideButton from './components/FieldGuideButton'
-import FullscreenButton from './components/FullscreenButton'
 import MoveButton from './components/MoveButton'
-import ResetButton from './components/ResetButton'
-import RotateButton from './components/RotateButton'
 import ZoomInButton from './components/ZoomInButton'
 import ZoomOutButton from './components/ZoomOutButton'
 
@@ -18,25 +15,24 @@ class ImageToolbar extends Component {
     const { theme: { mode }, ...props } = this.props
 
     return (
-      <aside {...props}>
+      <Box as='aside' {...props}>
         <Box
           background={mode === 'light' ? 'white' : 'dark-3'}
           border={{
             color: mode === 'light' ? 'light-3' : 'dark-3',
             side: 'all'
           }}
+          direction='column'
+          fill
           pad='small'
         >
           <AnnotateButton />
           <MoveButton />
           <ZoomInButton />
           <ZoomOutButton />
-          <RotateButton />
-          <FullscreenButton />
-          <ResetButton />
         </Box>
         <FieldGuideButton />
-      </aside>
+      </Box>
     )
   }
 }
--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/FieldGuideButton/FieldGuideButton.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/FieldGuideButton/FieldGuideButton.js
@@ -1,34 +1,36 @@
+import { withFocusProps, withHoverProps } from '@klarna/higher-order-components'
+import { SpacedText } from '@zooniverse/react-components'
+import zooTheme from '@zooniverse/grommet-theme'
 import counterpart from 'counterpart'
 import { Box } from 'grommet'
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { withFocusProps, withHoverProps } from '@klarna/higher-order-components'
 
 import en from './locales/en'
-import helpIcon from './helpIcon'
+import HelpIcon from './HelpIcon'
 
 counterpart.registerTranslations('en', en)
 
 const StyledButton = styled.button`
-  background: #00979D;
-  border: 1px solid #00979D;
-  color: white;
-  cursor: pointer;
-  letter-spacing: 1px;
-  opacity: ${props => (props.hoveredOrFocused) ? '0.7' : '1'};
-  padding: 0;
-  text-transform: uppercase;
-  width: 100%;
+  height: auto;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background: ${zooTheme.global.colors.brand}
+  border: 0;
+  padding: 15px 10px;
+`
 
-  svg {
-    margin-top: 0.5rem;
-    width: 50%;
+const StyledSpacedText = styled(SpacedText)`
+  line-height: 1.2;
+`
 
-    path {
-      fill: white;
-    }
-  }
+const StyledHelpIcon = styled(HelpIcon)`
+  margin-top: 8px;
+  display: block;
+  fill: white;
 `
 
 @withHoverProps({ hovered: true })
@@ -68,11 +70,16 @@ class FieldGuideButton extends React.Component {
         aria-label={counterpart('FieldGuideButton.ariaLabel')}
         hoveredOrFocused={hoveredOrFocused}
         onClick={this.onClick}
+
       >
-        <Box align='center' pad='small'>
-          <div>{counterpart('FieldGuideButton.buttonLabel.field')}</div>
-          <div>{counterpart('FieldGuideButton.buttonLabel.guide')}</div>
-          {helpIcon}
+        <Box as='span' align='center' direction='column'>
+          <StyledSpacedText size='xsmall' color='white'>
+            {counterpart('FieldGuideButton.buttonLabel.field')}
+          </StyledSpacedText>
+          <StyledSpacedText size='xsmall' color='white'>
+            {counterpart('FieldGuideButton.buttonLabel.guide')}
+          </StyledSpacedText>
+          <StyledHelpIcon />
         </Box>
       </StyledButton>
     )
