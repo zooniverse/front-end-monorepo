38123703b3ac917b19d99b121165be72b9a1d880
mcbouslog <mcbouslog@gmail.com>
2019-03-19 08:51:31 -0500

    Add Boolean filter to feedback messages

--- packages/lib-classifier/src/store/FeedbackStore.js
+++ packages/lib-classifier/src/store/FeedbackStore.js
@@ -24,7 +24,7 @@ const FeedbackStore = types
           } else if (!rule.success && rule.failureEnabled) {
             return rule.failureMessage
           }
-        })
+        }).filter(Boolean)
     }
   }))
   .actions(self => {
