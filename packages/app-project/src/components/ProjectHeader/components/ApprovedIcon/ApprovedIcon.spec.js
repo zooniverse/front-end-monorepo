042f8472eb2b39d102cf0a8354254f1b56ebc6bd
Roger Hutchings <roger@hutchin.gs>
2019-03-20 14:49:42 +0000

    Add test for text alternative

--- packages/app-project/src/components/ProjectHeader/components/ApprovedIcon/ApprovedIcon.spec.js
+++ packages/app-project/src/components/ProjectHeader/components/ApprovedIcon/ApprovedIcon.spec.js
@@ -14,12 +14,26 @@ describe('Component > ApprovedIcon', function () {
     expect(wrapper).to.be.ok()
   })
 
-  it('should render null if the `approved` prop is `false`', function () {
-    expect(wrapper.isEmptyRender()).to.be.ok()
+  describe('for non-approved projects', function () {
+    it('should render null if the `approved` prop is `false`', function () {
+      expect(wrapper.isEmptyRender()).to.be.ok()
+    })
   })
 
-  it('should render an icon if `approved` is true', function () {
-    const approvedWrapper = shallow(<ApprovedIcon approved={true} />)
-    expect(approvedWrapper.find('FormCheckmark')).to.have.lengthOf(1)
+  describe('for approved projects', function () {
+    before(function () {
+      wrapper = shallow(<ApprovedIcon approved={true} />)
+    })
+
+    it('should render an icon if `approved` is true', function () {
+      expect(wrapper.find('FormCheckmark')).to.have.lengthOf(1)
+    })
+
+    it('should have a text equivalent for screen readers', function () {
+      expect(wrapper.prop('title')).to.equal('Zooniverse Approved')
+      expect(wrapper.prop('aria-label')).to.equal('Zooniverse Approved')
+    })
   })
+
+
 })
