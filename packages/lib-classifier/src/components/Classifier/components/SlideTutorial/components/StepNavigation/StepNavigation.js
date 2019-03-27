64e8b9d938661f4670cd580ea14a87f28c8427b5
Sarah Allen <sarah@zooniverse.org>
2019-03-27 15:48:56 -0500

    Get RadioButtonGroup working. Remove cruft.

--- packages/lib-classifier/src/components/Classifier/components/SlideTutorial/components/StepNavigation/StepNavigation.js
+++ packages/lib-classifier/src/components/Classifier/components/SlideTutorial/components/StepNavigation/StepNavigation.js
@@ -5,26 +5,20 @@ import { inject, observer, PropTypes as MobXPropTypes } from 'mobx-react'
 import { Button, Box, RadioButtonGroup } from 'grommet'
 import styled from 'styled-components'
 import { FormNext, FormPrevious } from 'grommet-icons'
-import zooTheme from '@zooniverse/grommet-theme'
 import en from './locales/en'
 
 counterpart.registerTranslations('en', en)
 
-// const HiddenRadioLabel = styled.span`
-//   display: flex;
-//   flex-direction: row;
-//   position: relative;
-//   ${StyledRadioButton} span {
-//     height: 0;
-//     overflow: hidden;
-//     position: absolute;
-//     width: 0;
-//   }
-// `
+const StyledRadioButtonGroup = styled(RadioButtonGroup)`
+  position: relative;
 
-// const StyledRadioButton = styled(RadioButton)`
-//   outline: ${(props) => (props.focused) ? zooTheme.global.colors['accent-2'] : 'none'}
-// `
+  > label > span {
+    height: 0;
+    overflow: hidden;
+    position: absolute;
+    width: 0;
+  }
+`
 
 function storeMapper (stores) {
   const { active: tutorial, activeStep, setTutorialStep } = stores.classifierStore.tutorials
@@ -38,25 +32,6 @@ function storeMapper (stores) {
 @inject(storeMapper)
 @observer
 class StepNavigation extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      focused: -1
-    }
-  }
-
-  onFocus (e) {
-    const keyPressed = e.keyCode ? e.keyCode : e.which
-    console.log('onfocus', keyPressed, e, e.target)
-    if (keyPressed === 9) { // tab is pressed
-      this.setState({ focused: e.target.value })
-    }
-  }
-
-  removeFocus () {
-    this.setState({ focused: -1 })
-  }
-
   onChange (event) {
     const { setTutorialStep } = this.props
     const indexValue = event.target.value.split('-')[1]
@@ -64,21 +39,23 @@ class StepNavigation extends React.Component {
   }
 
   render () {
-    const { activeStep, setTutorialStep, steps } = this.props
+    const { activeStep, className, setTutorialStep, steps } = this.props
     if (steps && steps.length > 1) {
       const nextStep = activeStep + 1
       const prevStep = activeStep - 1
       const options = steps.map((step, index) => {
-        const name = counterpart('StepNavigation.go', { index: index + 1 })
+        const label = counterpart('StepNavigation.go', { index: index + 1 })
+        // We can't just use index for the value
+        // because Grommet is using indexes internally as keys and this will error with a duplicate key
+        const value = `step-${index}`
         return {
-          id: `step-${index}`,
-          name,
-          // We can't just use index because Grommet is using indexes internally as keys and this will error with a duplicate key
-          value: `step-${index}` 
+          id: value,
+          label,
+          value
         }
       })
       return (
-        <Box direction='row' justify='center' tag='nav'>
+        <Box as='nav' className={className} direction='row' justify='center'>
           <Button
             a11yTitle={counterpart('StepNavigation.previous')}
             data-index={prevStep}
@@ -87,7 +64,7 @@ class StepNavigation extends React.Component {
             onClick={() => setTutorialStep(prevStep)}
             plain
           />
-          <RadioButtonGroup
+          <StyledRadioButtonGroup
             direction='row'
             name='step-selectors'
             onChange={this.onChange.bind(this)}
@@ -112,37 +89,16 @@ class StepNavigation extends React.Component {
 
 StepNavigation.wrappedComponent.defaultProps = {
   activeStep: 0,
+  className: '',
   setTutorialStep: () => {},
   steps: []
 }
 
 StepNavigation.wrappedComponent.propTypes = {
   activeStep: PropTypes.number,
+  className: PropTypes.string,
   setTutorialStep: PropTypes.func,
   steps: PropTypes.array
 }
 
 export default StepNavigation
-
-
-// {
-//   steps.map((step, index) => {
-//     const active = index === activeStep
-//     const key = `step-${index}`
-//     const focused = index === this.state.focused
-//     console.log(this.state.focused)
-//     return (
-//       <HiddenRadioLabel key={key}>
-//         <StyledRadioButton
-//           autoFocus={index === 0}
-//           checked={active}
-//           focused={focused}
-//           id={key}
-//           label={counterpart('StepNavigation.go', { index: index + 1 })}
-//           name='step-selectors'
-//           onChange={() => setTutorialStep(index)}
-//           value={index}
-//         />
-//       </HiddenRadioLabel>
-//     )
-    // })}
