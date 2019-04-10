436d1c55604b060ccf0cac96b9b3657e8e6e9384
Jim O'Donnell <jim@eatyourgreens.org.uk>
2019-04-10 14:02:55 +0100

    Fix sign out (#669)
    Fix an undefined user error on sign out.
    Add a test.

--- packages/app-project/src/components/ZooHeaderWrapper/ZooHeaderWrapperContainer.spec.js
+++ packages/app-project/src/components/ZooHeaderWrapper/ZooHeaderWrapperContainer.spec.js
@@ -1,18 +1,37 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import sinon from 'sinon'
+import auth from 'panoptes-client/lib/auth'
 
 import ZooHeaderWrapperContainer from './ZooHeaderWrapperContainer'
 
 let wrapper
 
 describe('Component > ZooHeaderWrapperContainer', function () {
+  const user = {
+    id: 'test',
+    clear: sinon.stub()
+  }
+
   before(function () {
+    sinon.stub(auth, 'signOut').callsFake(() => Promise.resolve())
     wrapper = shallow(<ZooHeaderWrapperContainer.wrappedComponent
-      user={{}}
+      user={user}
     />)
+  })
+
+  after(function () {
+    auth.signOut.restore()
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok()
+  })
+
+  it('should clear the user prop on sign out', function (done) {
+    wrapper.instance().signOut().then(function () {
+      expect(user.clear).to.have.been.calledOnce()
+    })
+    .then(done, done)
   })
 })
