8b48aec7c1210a3e344eef3d51dab759ffb7946c
Jim O'Donnell <jim@zooniverse.org>
2019-04-17 10:31:32 +0100

    Extract withKeyZoom and test Extract withKeyZoom to its own file. Add tests for the key bindings on a wrapped component.

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.js
@@ -1,6 +1,5 @@
 import { Box } from 'grommet'
-import { inject } from 'mobx-react'
-import { func, shape, string } from 'prop-types'
+import { shape, string } from 'prop-types'
 import React, { Component } from 'react'
 import { withTheme } from 'styled-components'
 
@@ -12,60 +11,7 @@ import ResetButton from './components/ResetButton'
 import RotateButton from './components/RotateButton'
 import ZoomInButton from './components/ZoomInButton'
 import ZoomOutButton from './components/ZoomOutButton'
-
-function storeMapper (stores) {
-  const {
-    zoomIn,
-    zoomOut
-  } = stores.classifierStore.subjectViewer
-
-  return {
-    zoomIn,
-    zoomOut
-  }
-}
-
-function withKeyZoom (WrappedComponent) {
-  return @inject(storeMapper)
-  class extends React.Component {
-    constructor () {
-      super()
-      this.onKeyDown = this.onKeyDown.bind(this)
-    }
-
-    onKeyDown (e) {
-      const { zoomIn, zoomOut } = this.props
-      switch (e.key) {
-        case '+':
-        case '=': {
-          zoomIn()
-          return true
-        }
-        case '-':
-        case '_': {
-          zoomOut()
-          return true
-        }
-        case 'ArrowRight': {
-          console.log('pan right')
-          return true
-        }
-        case 'ArrowLeft': {
-          console.log('pan left')
-          return true
-        }
-        default: {
-          return true
-        }
-      }
-    }
-
-    render () {
-      const { zoomIn, zoomOut, ...props } = this.props
-      return <WrappedComponent onKeyDown={this.onKeyDown} {...props} />
-    }
-  }
-}
+import withKeyZoom from '../withKeyZoom'
 
 @withTheme
 @withKeyZoom
