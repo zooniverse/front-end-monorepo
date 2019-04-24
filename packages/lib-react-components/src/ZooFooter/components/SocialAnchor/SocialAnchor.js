607e7051b01967f494093722af18e852d0a605b0
Roger Hutchings <roger@hutchin.gs>
2019-04-24 18:08:51 +0100

    add tests

--- packages/lib-react-components/src/ZooFooter/components/SocialAnchor/SocialAnchor.js
+++ packages/lib-react-components/src/ZooFooter/components/SocialAnchor/SocialAnchor.js
@@ -3,18 +3,22 @@ import { Anchor } from 'grommet'
 import { FacebookOption, Twitter, Instagram } from 'grommet-icons'
 import { objectOf, oneOf, string } from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 
 const StyledAnchor = styled(Anchor)`
   padding: 0;
 
   &:focus > svg,
   &:hover > svg {
-    fill: ${zooTheme.global.colors['accent-2']} !important;
+    fill: ${props => props.dark
+      ? `${zooTheme.global.colors['brand']} !important`
+      : `${zooTheme.global.colors['accent-2']} !important`
+    };
   }
 `
 
-export default function SocialAnchor ({ className, hrefs, service }) {
+function SocialAnchor (props) {
+  const { className, hrefs, service, theme: { dark } } = props
   const icons = {
     facebook: <FacebookOption size='25px' />,
     instagram: <Instagram size='25px' />,
@@ -25,6 +29,7 @@ export default function SocialAnchor ({ className, hrefs, service }) {
       <StyledAnchor
         a11yTitle={service}
         className={className}
+        dark={dark}
         icon={icons[service]}
         href={hrefs[service]}
       />
@@ -43,3 +48,6 @@ SocialAnchor.propTypes = {
   hrefs: objectOf(string),
   service: string.isRequired
 }
+
+export default withTheme(SocialAnchor)
+export { SocialAnchor }
