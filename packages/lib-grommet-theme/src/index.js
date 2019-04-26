cf48a02439d8c2f5eb74a606bd9306e59586706b
Sarah Allen <sarah@zooniverse.org>
2019-04-26 11:16:54 -0500

    Update theme for the radio buttons

--- packages/lib-grommet-theme/src/index.js
+++ packages/lib-grommet-theme/src/index.js
@@ -425,7 +425,8 @@ const theme = deepFreeze({
   },
   radioButton: {
     icon: {
-      size: 'small'
+      size: '15px',
+      extend: props => `circle { r: 10px; }`
     },
     size: '15px'
   }
