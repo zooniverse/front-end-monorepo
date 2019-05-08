6d68ffb3541772a5bef37ae65b18c31832320430
Jim O'Donnell <jim@zooniverse.org>
2019-05-08 14:41:24 +0100

    Add subject dimensions to the classification

--- packages/lib-classifier/src/store/ClassificationStore.js
+++ packages/lib-classifier/src/store/ClassificationStore.js
@@ -132,10 +132,12 @@ const ClassificationStore = types
 
     function completeClassification () {
       const classification = self.active
+      const subjectDimensions = getRoot(self).subjectViewer.dimensions.toJSON()
 
       const metadata = {
-        session: sessionUtils.getSessionID(),
         finishedAt: (new Date()).toISOString(),
+        session: sessionUtils.getSessionID(),
+        subjectDimensions,
         viewport: {
           width: window.innerWidth,
           height: window.innerHeight
