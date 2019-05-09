73d300b6441a632f888f20d60e5b7f66138a3a4f
Jim O'Donnell <jim@zooniverse.org>
2019-05-09 17:10:06 +0100

    Record subject image dimensions on load (#804)
    * Record subject image dimensions on load
    Get image dimensions from the HTML preloader. Create a new, fake event and fake target on SVG image load, since the original is read-only. Pass the fake event, with the HTML image dimensions, to the subject viewer store with the onReady event handler.
    
    * Enable lifecycle method testing
    Allow enzyme shallow rendering to run React lifecycle methods.
    
    * Test the single image preloader
    Mock window.Image() and expect onReady to be called with the original image dimensions on image load.
    
    * Fix data subjects
    Data subjects don't fire a load event, so height and width are undefined. Ignore naturalHeight and naturalWidth for these subjects, since those numbers are meaningless.
    
    * Add subject dimensions to subject viewer store
    Copy the subjectDimensions model from the classificationMetadata model: dimensions is an array of size objects for different frames of the subject.
    
    * Add subject dimensions to the classification
    
    * Reset subject dimensions on subject change
    
    * Add subject viewer to classification store tests
    
    * mock subject dimensions in classification tests
    Expect subject dimensions to be copied from the subject viewer to the classification store when submitting a classification.
    
    * Tidy up onSubjectReady
    
    * Record client dimensions for single image subjects
    Create a ref to the svg element for single image subjects. Add the client height and width of the svg element to the recorded subject dimensions.
    
    * Record client dimensions for light curve viewer
    Get a reference to the light curve viewer's container DOM node, then get client height and width from that node.
    
    * Use a forward ref for the light curve viewer
    Pass a ref to the LCV container node as a forward ref to the LightCurveViewer component.
    
    * Use toJS() instead of toJSON()
    
    * Default natural image dimensions to 0

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewerContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewerContainer.js
@@ -13,6 +13,7 @@ import withKeyZoom from '../../../withKeyZoom'
 class LightCurveViewerContainer extends Component {
   constructor () {
     super()
+    this.viewer = React.createRef()
     this.state = {
       loading: asyncStates.initialized,
       dataExtent: {
@@ -82,6 +83,7 @@ class LightCurveViewerContainer extends Component {
 
   onLoad (rawData) {
     const { onReady } = this.props
+    const target = this.viewer.current
     this.setState({
       dataExtent: {
         x: d3.extent(rawData.x),
@@ -90,7 +92,9 @@ class LightCurveViewerContainer extends Component {
       dataPoints: zip(rawData.x, rawData.y),
       loading: asyncStates.success
     },
-    onReady)
+    function () {
+      onReady({ target })
+    })
   }
 
   onError (error) {
@@ -109,6 +113,7 @@ class LightCurveViewerContainer extends Component {
 
     return (
       <LightCurveViewer
+        forwardRef={this.viewer}
         dataExtent={this.state.dataExtent}
         dataPoints={this.state.dataPoints}
         drawFeedbackBrushes={this.props.drawFeedbackBrushes}
