f6b74a824e63c02e908304caa221c0ec65c096e0
Sarah Allen <sarah@zooniverse.org>
2019-05-10 14:43:48 -0500

    Refactor ClassifyPage classifier area and components for mobile view. Update tests

--- packages/app-project/src/screens/ClassifyPage/ClassifyPage.spec.js
+++ packages/app-project/src/screens/ClassifyPage/ClassifyPage.spec.js
@@ -1,8 +1,12 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 
-import ClassifyPage from './ClassifyPage'
+import { ClassifyPage } from './ClassifyPage'
 import FinishedForTheDay from './components/FinishedForTheDay'
+import ThemeModeToggle from '../../components/ThemeModeToggle'
+import ProjectName from '../../components/ProjectName'
+import YourStats from './components/YourStats'
+import ConnectWithProject from '../../shared/components/ConnectWithProject'
 import ProjectStatistics from '../../shared/components/ProjectStatistics'
 
 let wrapper
@@ -22,5 +26,21 @@ describe('Component > ClassifyPage', function () {
 
   it('should render the `ProjectStatistics` component', function () {
     expect(wrapper.find(ProjectStatistics)).to.have.lengthOf(1)
+  })
+
+  it('should render the `ThemeModeToggle` component', function () {
+    expect(wrapper.find(ThemeModeToggle)).to.have.lengthOf(1)
+  })
+
+  it('should render the `ProjectName` component', function () {
+    expect(wrapper.find(ProjectName)).to.have.lengthOf(1)
+  })
+
+  it('should render the `YourStats` component', function () {
+    expect(wrapper.find(YourStats)).to.have.lengthOf(1)
+  })
+
+  it('should render the `ConnectWithProject` component', function () {
+    expect(wrapper.find(ConnectWithProject)).to.have.lengthOf(1)
   })
 })
