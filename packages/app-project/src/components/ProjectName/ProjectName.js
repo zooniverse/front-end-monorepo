c3dcaf462d000a8e167443ed31714e77a4835ef7
srallen <srallen@users.noreply.github.com>
2019-05-13 11:19:17 -0500

    Refactor ClassifyPage classifier area and components for mobile view. Update tests (#813)

--- packages/app-project/src/components/ProjectName/ProjectName.js
+++ packages/app-project/src/components/ProjectName/ProjectName.js
@@ -1,27 +1,34 @@
-import { SpacedText } from '@zooniverse/react-components'
-import { Box } from 'grommet'
+import { SpacedText, withResponsiveContext } from '@zooniverse/react-components'
 import { string } from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
 const StyledText = styled(SpacedText)`
+  text-align: center;
   transform: rotate(180deg);
   writing-mode: vertical-lr;
 `
 
 function ProjectName (props) {
-  const { projectName } = props
+  const { projectName, screenSize } = props
+
+  if (screenSize === 'small') return null
+
   return (
-    <Box pad={{ bottom: 'small', left: 'small', top: 'medium' }}>
-      <StyledText color={{ dark: 'white', light: 'black' }} size='medium' weight='bold'>
-        {projectName}
-      </StyledText>
-    </Box>
+    <StyledText
+      color={{ dark: 'white', light: 'black' }}
+      size='medium'
+      weight='bold'
+    >
+      {projectName}
+    </StyledText>
   )
 }
 
 ProjectName.propTypes = {
-  projectName: string
+  projectName: string.isRequired,
+  screenSize: string.isRequired
 }
 
-export default ProjectName
+export default withResponsiveContext(ProjectName)
+export { ProjectName }
