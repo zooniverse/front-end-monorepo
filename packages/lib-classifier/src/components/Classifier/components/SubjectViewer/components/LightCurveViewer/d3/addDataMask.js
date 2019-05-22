a986a792771a971875a861f62528fba5329be3cb
mcbouslog <mcbouslog@gmail.com>
2019-05-22 14:09:59 -0500

    Add random number to data-mask ID

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addDataMask.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addDataMask.js
@@ -1,7 +1,7 @@
 export default function addDataMask (selection, outerMargin) {
   return selection
     .append('clipPath')
-    .attr('id', 'data-mask')
+    .attr('id', `data-mask-${Math.random()}`)
     .append('rect')
     .attr('class', 'data-mask')
     .attr('transform', `translate(${outerMargin}, ${outerMargin})`)
