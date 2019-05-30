35529f3c06a9d7a8395057d5246ff15212d8f6b6
srallen <srallen@users.noreply.github.com>
2019-05-30 03:21:55 -0500

    Auth modal review (#888)
    * Update package to use tilde
    
    * Update tests. Add translations. Hide register tab for now. Link to Panoptes's form instead to buy us time
    
    * Actually add the changes
    
    * Add the placeholder to the translations

--- packages/app-project/src/components/AuthModal/AuthModal.spec.js
+++ packages/app-project/src/components/AuthModal/AuthModal.spec.js
@@ -1,7 +1,8 @@
 import { mount, shallow} from 'enzyme'
 import React from 'react'
-
+import { Modal } from '@zooniverse/react-components'
 import AuthModal from './AuthModal'
+import LoginForm from './components/LoginForm'
 
 const CLOSE_MODAL = Function.prototype
 const ON_ACTIVE = Function.prototype
@@ -30,7 +31,11 @@ describe('Component > AuthModal', function () {
   it('should pass through the `activeIndex` and `onActive` props to Tabs', function () {
     // `Tabs` should be the first component
     const tabsWrapper = wrapper.children().first()
-    expect(wrapper.prop('activeIndex')).to.equal(-1)
-    expect(wrapper.prop('onActive')).to.equal(ON_ACTIVE)
+    expect(tabsWrapper.prop('activeIndex')).to.equal(-1)
+    expect(tabsWrapper.prop('onActive')).to.equal(ON_ACTIVE)
+  })
+
+  it('should pass `closeModal` prop to Modal and the child form components', function () {
+    expect(wrapper.find(Modal).prop('closeFn')).to.equal(CLOSE_MODAL)
   })
 })
