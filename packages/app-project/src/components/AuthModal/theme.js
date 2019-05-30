8b2379a78f6609252a2aa6d90c79faa8a17ce2cc
Roger Hutchings <roger@hutchin.gs>
2019-05-30 13:46:04 +0100

    Fix text colours, facepalm

--- packages/app-project/src/components/AuthModal/theme.js
+++ packages/app-project/src/components/AuthModal/theme.js
@@ -1,10 +1,6 @@
 export default {
   formField: {
     border: {
-      color: {
-        dark: 'dark-3',
-        light: 'light-4'
-      },
       side: 'all'
     },
     label: {
@@ -17,19 +13,13 @@ export default {
   textInput: {
     extend: props => `
       color: ${props.theme.dark
-    ? props.theme.global.colors['white']
-    : props.theme.global.colors['dark-1']
-};
+        ? props.theme.global.colors['white']
+        : props.theme.global.colors['dark-1']
+      };
       font-weight: 400;
     `
   },
   global: {
-    colors: {
-      placeholder: {
-        dark: 'dark-1',
-        light: 'light-4'
-      },
-      text: 'white'
-    }
+
   }
 }
