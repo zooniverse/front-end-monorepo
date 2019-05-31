4c64a0b6823fc9186ace8bf5965229197f7acbab
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-05-31 20:06:07 +0100

    Add basic input tests for registration form (#894)
    * Add basic input tests for registration form
    
    * Update packages/app-project/src/components/AuthModal/components/RegisterForm/RegisterForm.spec.js
    
    Co-Authored-By: Jim O'Donnell <jim@eatyourgreens.org.uk>

---
+++ packages/app-project/src/components/AuthModal/components/RegisterForm/RegisterForm.spec.js
@@ -0,0 +1,65 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import RegisterForm from './RegisterForm'
+
+let wrapper
+
+describe('Component > RegisterForm', function () {
+  before(function () {
+    wrapper = shallow(<RegisterForm />)
+  })
+
+  it('should render without crashing', function () {
+    expect(wrapper).to.be.ok()
+  })
+
+  it('should call the `onSubmit` prop on submission', function () {
+    // https://github.com/jaredpalmer/formik/blob/f5d76609b222ce583663add279ac76e807e2d0ba/README.md#testing-formik
+  })
+
+  describe('fields', function () {
+    let rendered
+
+    before(function () {
+      rendered = wrapper.render()
+    })
+
+    it('should have a username field', function () {
+      const input = rendered.find('[name="username"]')
+      expect(input).to.have.lengthOf(1)
+      expect(input.attr('type')).to.equal('text')
+      expect(input.attr('required')).to.equal('required')
+    })
+
+    it('should have a password field', function () {
+      const input = rendered.find('[name="password"]')
+      expect(input).to.have.lengthOf(1)
+      expect(input.attr('type')).to.equal('password')
+      expect(input.attr('required')).to.equal('required')
+      expect(input.prop('minlength')).to.equal('8')
+    })
+
+    it('should have a passwordConfirm field', function () {
+      const input = rendered.find('[name="password"]')
+      expect(input).to.have.lengthOf(1)
+      expect(input.attr('type')).to.equal('password')
+      expect(input.attr('required')).to.equal('required')
+      expect(input.prop('minlength')).to.equal('8')
+    })
+
+    it('should have an email field', function () {
+      const input = rendered.find('[name="email"]')
+      expect(input).to.have.lengthOf(1)
+      expect(input.attr('type')).to.equal('email')
+      expect(input.attr('required')).to.equal('required')
+    })
+
+    it('should have a realName field', function () {
+      const input = rendered.find('[name="realName"]')
+      expect(input).to.have.lengthOf(1)
+      expect(input.attr('type')).to.equal('text')
+      expect(input.attr('required')).to.be.undefined()
+    })
+  })
+})
