f14ac20d67c7f9b32b010e5ed7ad25d89a8b8100
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-06-03 16:45:18 +0100

    New sign in modal (#884)
    * Set active border to transparent
    
    * First pass at new auth modal
    
    * start tests
    
    * Last tweaks before PR
    
    * Auth modal review (#888)
    
    * Update package to use tilde
    
    * Update tests. Add translations. Hide register tab for now. Link to Panoptes's form instead to buy us time
    
    * Actually add the changes
    
    * Add the placeholder to the translations
    
    * lint
    
    * Set correct text colours
    
    * Add tests for login form container
    
    * remove stray only
    
    * add expections for failing code paths
    
    * move box inside actual component

---
+++ packages/app-project/src/components/AuthModal/AuthModal.js
@@ -0,0 +1,53 @@
+import { Modal, Tabs, Tab, withOnlyRenderOnBrowser, withThemeContext } from '@zooniverse/react-components'
+import { Box } from 'grommet'
+import { func, string } from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+
+import authModalTheme from './theme'
+import LoginForm from './components/LoginForm'
+
+const StyledTabs = styled(Tabs)`
+  div[role="tabpanel"] {
+    background: ${props => props.theme.dark
+    ? props.theme.global.colors['dark-5']
+    : props.theme.global.colors['white']
+};
+  }
+  button[role="tab"][aria-selected="true"] > div {
+    background: ${props => props.theme.dark
+    ? props.theme.global.colors['dark-5']
+    : props.theme.global.colors['white']
+};
+  }
+`
+
+function AuthModal (props) {
+  const {
+    activeIndex,
+    className,
+    closeModal,
+    onActive
+  } = props
+
+  return (
+    <Modal
+      active={activeIndex > -1}
+      className={className}
+      closeFn={closeModal}
+    >
+      <StyledTabs activeIndex={activeIndex} onActive={onActive}>
+        <Tab title='Sign In'>
+          <LoginForm closeModal={closeModal} />
+        </Tab>
+      </StyledTabs>
+    </Modal>
+  )
+}
+
+AuthModal.propTypes = {
+  className: string,
+  closeModal: func.isRequired
+}
+
+export default withOnlyRenderOnBrowser(withThemeContext(AuthModal, authModalTheme))
---
+++ packages/app-project/src/components/AuthModal/theme.js
@@ -0,0 +1,35 @@
+export default {
+  formField: {
+    border: {
+      color: {
+        dark: 'dark-3',
+        light: 'light-4'
+      },
+      side: 'all'
+    },
+    label: {
+      margin: {
+        horizontal: 'none',
+        vertical: 'xsmall'
+      }
+    }
+  },
+  textInput: {
+    extend: props => `
+      color: ${props.theme.dark
+    ? props.theme.global.colors['white']
+    : props.theme.global.colors['dark-1']
+};
+      font-weight: 400;
+    `
+  },
+  global: {
+    colors: {
+      placeholder: {
+        dark: 'dark-1',
+        light: 'light-4'
+      },
+      text: 'white'
+    }
+  }
+}
