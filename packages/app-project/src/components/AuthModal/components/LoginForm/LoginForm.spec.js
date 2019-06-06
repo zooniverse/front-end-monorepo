889b87332c852c858128e9e1bffa14be922e2a72
Sarah Allen <sarah@zooniverse.org>
2019-06-06 16:26:14 -0500

    Let's not update the LoginForm tests for now

--- packages/app-project/src/components/AuthModal/components/LoginForm/LoginForm.spec.js
+++ packages/app-project/src/components/AuthModal/components/LoginForm/LoginForm.spec.js
@@ -1,33 +1,16 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import sinon from 'sinon'
-import { Formik } from 'formik'
 
 import LoginForm from './LoginForm'
 
 let wrapper
 
 describe('Component > LoginForm', function () {
-  let onSubmitStub = sinon.stub()
   before(function () {
-    wrapper = shallow(<LoginForm onSubmit={onSubmitStub} />)
+    wrapper = shallow(<LoginForm />)
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok()
-  })
-
-  it('should call the `onSubmit` prop on submission', function () {
-    const form = wrapper.find(Formik).dive().find({ as: 'form' })
-
-    form.simulate('submit', {
-      preventDefault: () => { }
-    })
-
-    // on submit wasn't being called without the timeout
-    window.setTimeout(() => {
-      expect(onSubmitStub).to.have.been.calledOnce
-      done()
-    }, 0)
   })
 })
