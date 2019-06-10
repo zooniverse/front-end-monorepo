40e3df102c8f62ecb7396ec42705dc2e0bede0ea
mcbouslog <mcbouslog@gmail.com>
2019-06-10 06:37:28 -0500

    Refactor DrawingStore for active drawing tool

--- packages/lib-classifier/src/store/DrawingStore.spec.js
+++ packages/lib-classifier/src/store/DrawingStore.spec.js
@@ -24,4 +24,18 @@ describe('Model > DrawingStore', function () {
     })
     model.addToStream(newEvent)
   })
+
+  it('should default the active tool index to the first tool in the tools array', function () {
+    expect(model.active).to.equal(0)
+  })
+
+  it('should set the active tool index', function () {
+    model.setActive(1)
+    expect(model.active).to.equal(1)
+  })
+
+  it('should reset the active tool to 0 when reset is called', function () {
+    model.reset()
+    expect(model.active).to.equal(0)
+  })
 })
