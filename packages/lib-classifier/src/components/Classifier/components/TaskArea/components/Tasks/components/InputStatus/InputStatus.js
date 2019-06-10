279e1c9b3e475833e082bd9a35f307c07b75058e
mcbouslog <mcbouslog@gmail.com>
2019-06-10 11:10:57 -0500

    Add InputStatus translations

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/InputStatus/InputStatus.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/InputStatus/InputStatus.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import counterpart from 'counterpart'
+import en from './locales/en'
+
+counterpart.registerTranslations('en', en)
 
 export const StyledInputStatus = styled.span`
   font-size: 0.8em;
@@ -8,22 +12,25 @@ export const StyledInputStatus = styled.span`
   text-align: right;
 `
 
-// TODO Add Translations
 export default function InputStatus ({ count, tool }) {
   const minStyleColor = (count < tool.min) ? 'red' : ''
   const maxStyleColor = (count === tool.max) ? 'orange' : ''
+  const drawn = counterpart('InputStatus.drawn')
+  const maximum = counterpart('InputStatus.maximum')
+  const of = counterpart('InputStatus.of')
+  const required = counterpart('InputStatus.required')
   return (
     <StyledInputStatus>
       {count}{' '}
       {(!!tool.min || !!tool.max) &&
-        'of '}
+        `${of} `}
       {!!tool.min &&
-        <span style={{ color: minStyleColor }}>{tool.min} required</span>}
+        <span style={{ color: minStyleColor }}>{`${tool.min} ${required}`}</span>}
       {!!tool.min && !!tool.max &&
         ', '}
       {!!tool.max &&
-        <span style={{ color: maxStyleColor }}>{tool.max} maximum</span>}
-      {' '}drawn
+        <span style={{ color: maxStyleColor }}>{`${tool.max} ${maximum}`}</span>}
+      {` ${drawn}`}
     </StyledInputStatus>
   )
 }
