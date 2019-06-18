f6a2bae8b4ddf32584362f3b3e981f7defed1c18
mcbouslog <mcbouslog@gmail.com>
2019-06-18 08:34:42 -0500

    Add drawing tool point model

---
+++ packages/lib-classifier/src/store/tasks/drawingTools/Point.spec.js
@@ -0,0 +1,18 @@
+import Point from './Point'
+
+const point = {
+  color: '#ff0000',
+  label: 'Point',
+  max: 10,
+  min: 1,
+  size: 'small',
+  type: 'point'
+}
+
+describe('Model > DrawingTools > Point', function () {
+  it('should exist', function () {
+    const pointToolInstance = Point.create(point)
+    expect(pointToolInstance).to.exist
+    expect(pointToolInstance).to.be.an('object')
+  })
+})
