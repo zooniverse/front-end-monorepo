c5ce94268d42b90a037e2cdf2a039e1377933cdf
mcbouslog <mcbouslog@gmail.com>
2019-06-24 16:06:51 -0500

    Update tests for grommet Text use

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/InputStatus/InputStatus.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/InputStatus/InputStatus.spec.js
@@ -15,24 +15,28 @@ describe('InputStatus', function () {
 
   it('should not render any requirements if props.tool.min or props.tool.max is not defined', function () {
     const wrapper = shallow(<InputStatus />)
-    expect(wrapper.find('span')).to.have.lengthOf(0)
+    expect(wrapper.find('Text')).to.have.lengthOf(0)
   })
 
   it('should render minimum drawing requirements if props.tool.min is defined', function () {
     const wrapper = shallow(<InputStatus tool={{ min: 1 }} />)
-    expect(wrapper.find('span')).to.have.lengthOf(1)
-    expect(wrapper.text()).to.equal('0 of 1 required drawn')
+    const inputText = wrapper.find('Text')
+    expect(inputText).to.have.lengthOf(1)
+    expect(inputText.html()).to.include('1 required')
   })
 
   it('should render maxmimum drawing requirements if props.tool.max is defined', function () {
     const wrapper = shallow(<InputStatus tool={{ max: 2 }} />)
-    expect(wrapper.find('span')).to.have.lengthOf(1)
-    expect(wrapper.text()).to.equal('0 of 2 maximum drawn')
+    const inputText = wrapper.find('Text')
+    expect(inputText).to.have.lengthOf(1)
+    expect(inputText.html()).to.include('2 maximum')
   })
 
   it('should render minimum and maxmimum drawing requirements if props.tool.min and props.tool.max are defined', function () {
     const wrapper = shallow(<InputStatus tool={{ min: 1, max: 2 }} />)
-    expect(wrapper.find('span')).to.have.lengthOf(2)
-    expect(wrapper.text()).to.equal('0 of 1 required, 2 maximum drawn')
+    const inputText = wrapper.find('Text')
+    expect(inputText).to.have.lengthOf(2)
+    expect(inputText.first().html()).to.include('1 required')
+    expect(inputText.last().html()).to.include('2 maximum')
   })
 })
