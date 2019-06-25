98afe2b3a17b716b0f943e531bf70f41c8ef223b
Roger Hutchings <roger@hutchin.gs>
2019-06-25 15:27:13 +0100

    Add express, morgan

--- packages/app-content-pages/server/server.js
+++ packages/app-content-pages/server/server.js
@@ -2,24 +2,31 @@ if (process.env.NEWRELIC_LICENSE_KEY) {
   require('newrelic')
 }
 
-const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
 const next = require('next')
 
-const configureNextServer = require('./configure-next-server')
+const setAssetPrefix = require('./set-asset-prefix')
+const setCacheHeaders = require('./set-cache-headers')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const port = parseInt(process.env.PORT, 10) || 3000
+const handle = app.getRequestHandler()
+
+setAssetPrefix(app)
 
 app.prepare().then(() => {
-  const server = new http.Server((req, res) =>
-    configureNextServer(app, req, res))
+  const server = express()
+  server.use(morgan('combined'))
+
+  server.get('*', (req, res) => {
+    setCacheHeaders(req, res)
+    return handle(req, res)
+  })
 
   server.listen(port, err => {
-    if (err) {
-      throw err
-    }
-
+    if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
 })
