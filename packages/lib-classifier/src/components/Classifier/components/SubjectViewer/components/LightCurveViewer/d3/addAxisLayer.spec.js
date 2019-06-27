43420a42341c36f59df9aba7ce3b9eacaa68ee39
Sarah Allen <sarah@zooniverse.org>
2019-06-27 09:53:22 -0500

    Start of tests

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addAxisLayer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/d3/addAxisLayer.spec.js
@@ -1,7 +1,59 @@
+import * as d3 from 'd3'
 import addAxisLayer from './addAxisLayer'
 
-describe('LightCurveViewer > d3 > addAxisLayer', function () {
+describe.only('LightCurveViewer > d3 > addAxisLayer', function () {
+  const style = {
+    color: 'blue',
+    fontSize: '12px',
+    fontFamily: 'Ariel'
+  }
+  const axisXLabel = 'x axis'
+  const axisYLabel = 'y axis'
+
+  let body, d3svg, node, xAxis, yAxis
+  beforeEach(function () {
+    const xScale = d3.scaleLinear()
+    const yScale = d3.scaleLinear()
+    xAxis = d3.axisTop(yScale)
+    yAxis = d3.axisRight(yScale)
+    body = document.getElementsByTagName('body')[0]
+    node = document.createElement('svg')
+    body.appendChild(node)
+    d3svg = d3.select(node)
+  })
+
+  afterEach(function () {
+    body.removeChild(node)
+  })
+
   it('should exist', function () {
     expect(addAxisLayer).to.be.a('function')
+  })
+
+  it('should add three svg groups with class names', function () {
+    addAxisLayer(d3svg, style, xAxis, yAxis, axisXLabel, axisYLabel)
+
+    expect(node.querySelectorAll('g.axis-layer')).to.have.lengthOf(1)
+    expect(node.querySelectorAll('g.x-axis')).to.have.lengthOf(1)
+    expect(node.querySelectorAll('g.y-axis')).to.have.lengthOf(1)
+  })
+
+  it('should style the axis with the color in the style object parameter', function () {
+    addAxisLayer(d3svg, style, xAxis, yAxis, axisXLabel, axisYLabel)
+    const xAxisNode = node.querySelector('g.x-axis')
+    const yAxisNode = node.querySelector('g.y-axis')
+    expect(xAxisNode.attributes.color).to.equal(styles.color)
+    expect(yAxisNode.attributes.color).to.equal(styles.color)
+  })
+
+  it('should add the text labels', function () {
+    addAxisLayer(d3svg, style, xAxis, yAxis, axisXLabel, axisYLabel)
+    const axis = node.querySelectorAll('text')
+    console.log('axis', axis)
+    // expect(axis.classList.contains('x-axis-label')).to.be.true
+    // expect(axis.innerHTML).to.equal(text)
+    // expect(axis.style.fill).to.equal(styles.color)
+    // expect(axis.style['font-size']).to.equal(styles.fontSize)
+    // expect(axis.style['font-family']).to.equal(styles.fontFamily)
   })
 })
