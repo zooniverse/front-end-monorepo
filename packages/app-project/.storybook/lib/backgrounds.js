b7db71c93bdc0cb85f6ae9e7fd69a8dcd43a5fef
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-07-03 12:04:55 +0100

    app-project housekeeping (#986)
    * bump dependencies
    
    * bump storybook and include addons

---
+++ packages/app-project/.storybook/lib/backgrounds.js
@@ -0,0 +1,14 @@
+import zooTheme from '@zooniverse/grommet-theme'
+
+const backgrounds = [
+  {
+    name: 'light',
+    value: zooTheme.global.colors['light-1']
+  },
+  {
+    name: 'dark',
+    value: zooTheme.global.colors['dark-1']
+  }
+]
+
+export default backgrounds
