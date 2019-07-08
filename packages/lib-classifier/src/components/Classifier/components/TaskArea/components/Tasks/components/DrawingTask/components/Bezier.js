24646b23537650721952642941a0d375d00b6b1e
mcbouslog <mcbouslog@gmail.com>
2019-07-08 09:49:51 -0500

    Clean up svg path fill

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/DrawingTask/components/Bezier.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/DrawingTask/components/Bezier.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Bezier = () => {
   return (
     <>
-      <path d='M90 90 Q 10 50 90 10' fill={'none'} />
-      <path d='M90 90 L 10 50 L 90 10' strokeDasharray={[10, 10]} fill={'none'} />
+      <path d='M90 90 Q 10 50 90 10' fill='none' />
+      <path d='M90 90 L 10 50 L 90 10' strokeDasharray={[10, 10]} fill='none' />
     </>
   )
 }
