aee25911b2d9a847cc7022767f170b46bc59f119
mcbouslog <mcbouslog@gmail.com>
2019-07-08 09:49:51 -0500

    Clean up svg path fill

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/DrawingTask/components/FreehandLine.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/DrawingTask/components/FreehandLine.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FreehandLine = () => {
   return (
-    <path d='M10,50 Q25,10,50,50,75,90,90,50' fill={'none'} />
+    <path d='M10,50 Q25,10,50,50,75,90,90,50' fill='none' />
   )
 }
 
