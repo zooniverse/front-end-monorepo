a31f4a57098e15a72613a290592d11cc3b0135c1
Roger Hutchings <roger@hutchin.gs>
2019-07-08 10:58:53 +0100

    Use yarn in scripts

--- bin/panic-button.sh
+++ bin/panic-button.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Trashes all build folders and `node_modules`
+# Trashes all `node_modules` folders, lockfiles and build artifacts to give you
+# a clean version of the project.
 
-printf 'Trashing `node_modules` folders and build artifacts...\n'
+printf 'Trashing `node_modules` folders, lockfiles and build artifacts...\n'
 
 ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd)"
 
 printf "Cleaning \`$ROOT_DIR\`..."
 rm -rf $ROOT_DIR/node_modules
+rm -rf $ROOT_DIR/yarn.lock
 printf " done!\n"
 
 for DIR in $(find $ROOT_DIR/packages -mindepth 1 -maxdepth 1 -type d) ; do
