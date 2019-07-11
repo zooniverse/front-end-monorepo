45beb7d476600d801c38fd38c03dd98fa7e5e9af
Jim O'Donnell <jim@zooniverse.org>
2019-07-11 19:39:36 +0100

    Cleanup classifier tests (#1007)
    * Remove .notCalled
    Replace .notCalled with .to.have.not.been.called() in tests.
    
    * Replace .calledWith
    Replace .calledWith with .withArgs when checking spies.
    
    * Replace .calledTwice
    Replace .calledTwice with .to.have.been.calledTwice().
    
    * Fix session ID tests
    Store mock session tokens before testing behaviour that uses stored tokens.
    
    * Describe behaviour in tests

--- packages/lib-classifier/src/store/utils/session.spec.js
+++ packages/lib-classifier/src/store/utils/session.spec.js
@@ -49,10 +49,10 @@ describe('Store utils > sessionUtils', function () {
     })
 
     it('it should retrieve id from session or local storage if it exists', function () {
-      sessionUtils.generateSessionID() // just setting up for test
-      generateSessionIDSpy.resetHistory()
+      const stored = { id: 'foobar', ttl: (Date.now() + 50000) }
+      sessionStorage.setItem('session_id', JSON.stringify(stored))
       sessionUtils.getSessionID()
-      expect(generateSessionIDSpy.notCalled).to.be.true()
+      expect(generateSessionIDSpy).to.have.not.been.called()
     })
 
     it('should call fiveMinutesFromNow if the ttl property is greater than Date.now()', function () {
@@ -69,25 +69,21 @@ describe('Store utils > sessionUtils', function () {
       // Unsure of the minor discrepency
       expect(new Date(stored.ttl).toString()).to.equal(fiveMinutesFromNowSpy.returnValues[0].toString())
     })
-  })
 
-  describe('getSessionID > when the ttl property is less than Date.now()', function () {
-    // Can't wrap the same method in a spy and a stub in the same describe block
-    // So this test is isolated here.
-    let generateSessionIDStub
-    before(function () {
-      generateSessionIDStub = sinon.stub(sessionUtils, 'generateSessionID')
-        .callsFake(() => { return { id: 'foobar', ttl: (Date.now() - 1) } })
-    })
+    describe('when the stored token has expired', function () {
+      before(function () {
+        const stored = { id: 'foobar', ttl: (Date.now() - 1) }
+        sessionStorage.setItem('session_id', JSON.stringify(stored))
+      })
 
-    after(function () {
-      generateSessionIDStub.restore()
-      sessionStorage.removeItem('session_id')
-    })
+      after(function () {
+        sessionStorage.removeItem('session_id')
+      })
 
-    it('should call generateSessionID', function () {
-      sessionUtils.getSessionID()
-      expect(generateSessionIDStub.calledTwice).to.be.true()
+      it('should generate a new session ID', function () {
+        sessionUtils.getSessionID()
+        expect(generateSessionIDSpy).to.have.been.calledOnce()
+      })
     })
   })
 
