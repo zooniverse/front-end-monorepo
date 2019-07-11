0db5f1b55b9cbbbeb70cbbb866e71c0781db53d8
srallen <srallen@users.noreply.github.com>
2019-07-11 13:44:42 -0500

    [MST Upgrade] Update project store (#1005)
    * Update how optional tree nodes are instantiated
    
    * Remove extra space
    
    * Use safeReference. Update test
    
    * Remove only
    
    * Use safeReference. Update test
    
    * Remove only again

--- packages/lib-classifier/src/store/ProjectStore.spec.js
+++ packages/lib-classifier/src/store/ProjectStore.spec.js
@@ -2,6 +2,6 @@ import ProjectStore from './ProjectStore'
 
 describe('Model > ProjectStore', function () {
   it('should exist', function () {
-    expect(ProjectStore).to.not.equal(undefined)
+    expect(ProjectStore).to.exist()
   })
 })
