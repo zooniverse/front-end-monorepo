df970295037473351387b993f315abf32e74c453
srallen <srallen@users.noreply.github.com>
2019-07-15 09:52:34 -0500

    Add names to disposer functions (#1025)

--- packages/lib-classifier/src/store/FeedbackStore.spec.js
+++ packages/lib-classifier/src/store/FeedbackStore.spec.js
@@ -4,7 +4,7 @@ import FeedbackStore from './FeedbackStore'
 import strategies from './feedback/strategies'
 import helpers from './feedback/helpers'
 
-describe('Model > FeedbackStore', function () {
+describe.only('Model > FeedbackStore', function () {
   let feedback
   let feedbackStub
 
