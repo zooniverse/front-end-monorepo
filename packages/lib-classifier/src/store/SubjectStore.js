ed6236e55fa637ee578a7a4ca2095009bb9e5077
Sarah Allen <sarah@zooniverse.org>
2019-07-17 15:07:12 -0500

    Update SubjectStore, SubjectsStore. Fix bugs

--- packages/lib-classifier/src/store/SubjectStore.js
+++ packages/lib-classifier/src/store/SubjectStore.js
@@ -1,6 +1,6 @@
 import asyncStates from '@zooniverse/async-states'
 import { autorun } from 'mobx'
-import { addDisposer, addMiddleware, flow, getRoot, onPatch, types } from 'mobx-state-tree'
+import { addDisposer, addMiddleware, flow, getRoot, isValidReference, onPatch, types } from 'mobx-state-tree'
 import { getBearerToken } from './utils'
 import { filterByLabel, filters } from '../components/Classifier/components/MetaTools/components/Metadata/components/MetadataModal'
 import ResourceStore from './ResourceStore'
@@ -20,14 +20,15 @@ function openTalkPage (talkURL, newTab = false) {
 
 const SubjectStore = types
   .model('SubjectStore', {
-    active: types.maybe(types.reference(Subject)),
-    resources: types.optional(types.map(Subject), {}),
+    active: types.safeReference(Subject),
+    resources: types.map(Subject),
     type: types.optional(types.string, 'subjects')
   })
 
   .views(self => ({
     get isThereMetadata () {
-      if (self.active) {
+      const validSubjectReference = isValidReference(() => self.active)
+      if (validSubjectReference) {
         const filteredMetadata = Object.keys(self.active.metadata)
           .filter((label) => filterByLabel(label, filters))
         return filteredMetadata.length > 0
@@ -39,35 +40,36 @@ const SubjectStore = types
 
   .actions(self => {
     function advance () {
-      if (self.active) {
+      const validSubjectReference = isValidReference(() => self.active)
+      if (validSubjectReference) {
         const idToRemove = self.active.id
         self.resources.delete(idToRemove)
-        self.active = undefined
       }
+
+      const nextSubject = self.resources.values().next().value
+      self.active = nextSubject && nextSubject.id
 
       if (self.resources.size < 3) {
         console.log('Fetching more subjects')
         self.populateQueue()
       }
-
-      const nextSubject = self.resources.values().next().value
-      self.active = nextSubject && nextSubject.id
     }
 
     function afterAttach () {
       createWorkflowObserver()
       createClassificationObserver()
       createSubjectMiddleware()
+      createQueueLoadObserver()
     }
 
     function createWorkflowObserver () {
       const workflowDisposer = autorun(() => {
-        const root = getRoot(self)
-        if (root.workflows && root.workflows.active) {
+        const validWorkflowReference = isValidReference(() => getRoot(self).workflows.active)
+        if (validWorkflowReference) {
           self.reset()
           self.populateQueue()
         }
-      })
+      }, { name: 'SubjectStore Workflow Observer autorun' })
       addDisposer(self, workflowDisposer)
     }
 
@@ -77,19 +79,25 @@ const SubjectStore = types
           const { path, value } = patch
           if (path === '/classifications/loadingState' && value === 'posting') self.advance()
         })
-      })
+      }, { name: 'SubjectStore Classification Observer autorun' })
       addDisposer(self, classificationDisposer)
     }
 
     function onSubjectAdvance (call, next, abort) {
       const root = getRoot(self)
-      const subject = self.active
-      const shouldShowFeedback = root.feedback.isActive && root.feedback.messages.length && !root.feedback.showModal
-      if (!shouldShowFeedback && subject && subject.shouldDiscuss) {
-        const { url, newTab } = subject.shouldDiscuss
-        openTalkPage(url, newTab)
+      const validSubjectReference = isValidReference(() => self.active)
+      if (validSubjectReference) {
+        const subject = self.active
+        const shouldShowFeedback = root.feedback.isActive && root.feedback.messages.length && !root.feedback.showModal
+        if (!shouldShowFeedback && subject && subject.shouldDiscuss) {
+          const { url, newTab } = subject.shouldDiscuss
+          openTalkPage(url, newTab)
+        } else {
+          next(call)
+        }
+      } else {
+        next(call)
       }
-      next(call)
     }
 
     function createSubjectMiddleware () {
@@ -101,51 +109,63 @@ const SubjectStore = types
             next(call)
           }
         })
-      })
+      }, { name: 'SubjectStore Middleware autorun' })
       addDisposer(self, subjectMiddleware)
+    }
+
+    function createQueueLoadObserver() {
+      const selfDisposer = autorun(() => {
+        onPatch(getRoot(self), (patch) => {
+          const { path, value } = patch
+          const validSubjectReference = isValidReference(() => self.active)
+          if (path === '/subjects/loadingState' &&
+            value === 'success' &&
+            self.resources.size > 0 &&
+            !validSubjectReference
+          ) {
+            self.advance()
+          }
+        })
+      }, { name: 'SubjectStore queue Observer autorun' })
+      addDisposer(self, selfDisposer)
     }
 
     function * populateQueue () {
       const root = getRoot(self)
       const client = root.client.panoptes
-      const workflowId = root.workflows.active.id
-      self.loadingState = asyncStates.loading
+      const validWorkflowReference = isValidReference(() => root.workflows.active)
+      if (validWorkflowReference) {
+        const workflowId = root.workflows.active.id
+        self.loadingState = asyncStates.loading
 
-      try {
-        const { authClient } = getRoot(self)
-        const authorization = yield getBearerToken(authClient)
-        const response = yield client.get(`/subjects/queued`, { workflow_id: workflowId }, { authorization })
+        try {
+          const { authClient } = getRoot(self)
+          const authorization = yield getBearerToken(authClient)
+          const response = yield client.get(`/subjects/queued`, { workflow_id: workflowId }, { authorization })
 
-        if (response.body.subjects && response.body.subjects.length > 0) {
-          response.body.subjects.forEach(subject => {
-            self.resources.put(subject)
-          })
-
-          if (!self.active) {
-            self.advance()
+          if (response.body.subjects && response.body.subjects.length > 0) {
+            response.body.subjects.forEach(subject => {
+              self.resources.put(subject)
+            })
           }
-        }
 
-        self.loadingState = asyncStates.success
-      } catch (error) {
-        console.error(error)
-        self.loadingState = asyncStates.error
+          self.loadingState = asyncStates.success
+        } catch (error) {
+          console.error(error)
+          self.loadingState = asyncStates.error
+        }
       }
     }
 
     function reset () {
-      self.active = undefined
       self.resources.clear()
     }
 
-    // We set ResourceStore methods we don't want to expose as `undefined`
     return {
       advance,
       afterAttach,
-      fetchResource: undefined,
       populateQueue: flow(populateQueue),
-      reset,
-      setActive: undefined
+      reset
     }
   })
 
