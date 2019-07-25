b449f70acd4d5578b42e78ca2c5b6756268555a8
Sarah Allen <sarah@zooniverse.org>
2019-07-25 15:43:38 -0500

    Add safeReference valid checks. Update tests. Update and add factories

--- packages/lib-classifier/src/store/Workflow.js
+++ packages/lib-classifier/src/store/Workflow.js
@@ -5,11 +5,12 @@ import Resource from './Resource'
 // Steps will be stored as an array of pairs to preserve order.
 const Workflow = types
   .model('Workflow', {
+    active: types.optional(types.boolean, false),
     configuration: types.frozen({
       hide_classification_summaries: types.optional(types.boolean, false)
     }),
     display_name: types.string,
-    first_task: types.maybe(types.string),
+    first_task: types.optional(types.string, ''),
     steps: types.union(types.frozen({}), types.array(types.array(
       types.union(types.string, types.frozen())
     ))),
