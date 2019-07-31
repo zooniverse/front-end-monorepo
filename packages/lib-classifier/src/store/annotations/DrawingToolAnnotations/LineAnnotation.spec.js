49644c4a0edd04b3e73c5b6491cbcb1e93482e6b
mcbouslog <mcbouslog@gmail.com>
2019-07-31 14:31:50 -0500

    Add line annotation type and coordinates tests

--- packages/lib-classifier/src/store/annotations/DrawingToolAnnotations/LineAnnotation.spec.js
+++ packages/lib-classifier/src/store/annotations/DrawingToolAnnotations/LineAnnotation.spec.js
@@ -1,6 +1,6 @@
 import LineAnnotation from './LineAnnotation'
 
-const annotation = {
+const lineAnnotation = {
   toolType: 'line',
   x1: 200,
   x2: 400,
@@ -10,8 +10,59 @@ const annotation = {
 
 describe('Model > LineAnnotation', function () {
   it('should exist', function () {
-    const annotationInstance = LineAnnotation.create(annotation)
-    expect(annotationInstance).to.exist()
-    expect(annotationInstance).to.be.an('object')
+    const lineAnnotationInstance = LineAnnotation.create(lineAnnotation)
+    expect(lineAnnotationInstance).to.exist()
+    expect(lineAnnotationInstance).to.be.an('object')
+  })
+
+  it('should have a toolType property of `line`', function () {
+    const lineAnnotationInstance = LineAnnotation.create(lineAnnotation)
+    expect(lineAnnotationInstance).to.deep.include({ toolType: 'line' })
+  })
+
+  it('should throw an error with incorrect toolType property', function () {
+    expect(() => LineAnnotation.create({
+      toolType: 'purple',
+      x1: 200,
+      x2: 400,
+      y1: 100,
+      y2: 150
+    })).to.throw()
+  })
+
+  it('should throw an error without x1 coordinate', function () {
+    expect(() => LineAnnotation.create({
+      toolType: 'line',
+      x2: 400,
+      y1: 100,
+      y2: 150
+    })).to.throw()
+  })
+
+  it('should throw an error without x2 coordinate', function () {
+    expect(() => LineAnnotation.create({
+      toolType: 'line',
+      x1: 200,
+      y1: 100,
+      y2: 150
+    })).to.throw()
+  })
+
+  it('should throw an error without y1 coordinate', function () {
+    expect(() => LineAnnotation.create({
+      toolType: 'line',
+      x1: 200,
+      x2: 400,
+      y2: 150
+    })).to.throw()
+  })
+
+  it('should throw an error without y2 coordinate', function () {
+    expect(() => LineAnnotation.create({
+      toolType: 'line',
+      x1: 200,
+      x2: 400,
+      y1: 100
+    })).to.throw()
   })
 })
