89ecd8e67f0cbd81ffe243080eb271f60694f1a9
Jim O'Donnell <jim@zooniverse.org>
2019-08-02 10:00:37 +0100

    Refactor metadata-to-rules Convert rule values to strings. Remove lodash and reduce the metadata keys array to an array of rules objects.

--- packages/lib-classifier/src/store/feedback/helpers/metadata-to-rules.js
+++ packages/lib-classifier/src/store/feedback/helpers/metadata-to-rules.js
@@ -1,17 +1,21 @@
-import _ from 'lodash'
-
 // Converts a subject metadata object into an array of feedback objects
-function metadataToRules (metadata) {
-  const rulesObject = _.reduce(metadata, (result, value, key) => {
-    const [prefix, ruleKey, propKey] = key.split('_')
+function metadataToRules (metadata = {}) {
+  const metadataKeys = Object.keys(metadata)
+  const rules = metadataKeys.reduce(function (result, key) {
+    const [prefix, ruleIndex, propKey] = key.split('_')
+    const value = metadata[key]
+    const stringValue = value.toString()
 
-    if (prefix === '#feedback' && value) {
-      _.set(result, `${ruleKey}.${propKey}`, value)
+    if (prefix === '#feedback' && stringValue) {
+      const rule = result[ruleIndex] || {}
+      rule[propKey] = value
+      result[ruleIndex] = rule
     }
 
     return result
-  }, {})
-  return _.toArray(rulesObject)
+  }, [])
+
+  return rules.filter(Boolean)
 }
 
 export default metadataToRules
