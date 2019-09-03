698585ff974edbad9a4e0b59b713dbe7dc555088
simensta <andrea@zooniverse.org>
2019-09-03 11:16:39 -0500

    Adds zoomLevelOnCreation data to the graph2DRangeXAnnotation tool (#1079)
    * Adds zoomLevelOnCreation data to the graph2DRangeXAnnotation tool
    
    * Fix missing zoomLevelOnCreation for Graph2dRangeXAnnotation tests

--- packages/lib-classifier/src/store/annotations/DataVisToolAnnotations/Graph2dRangeXAnnotation.js
+++ packages/lib-classifier/src/store/annotations/DataVisToolAnnotations/Graph2dRangeXAnnotation.js
@@ -4,7 +4,8 @@ const Graph2dRangeXAnnotation = types.model('Graph2dRangeXAnnotation', {
   tool: types.number,
   toolType: types.string,
   x: types.number,
-  width: types.number
+  width: types.number,
+  zoomLevelOnCreation: types.number
 })
 
 export default Graph2dRangeXAnnotation
