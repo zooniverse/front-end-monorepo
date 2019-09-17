0f705ebb262391bbe03e1d1041c2183d6f4a4526
Jim O'Donnell <jim@zooniverse.org>
2019-09-17 09:33:41 +0100

    Add tests for the chart container Test the conversion filter that generates labels for the chart data.

--- packages/app-project/src/screens/ClassifyPage/components/YourStats/components/DailyClassificationsChart/DailyClassificationsChartContainer.spec.js
+++ packages/app-project/src/screens/ClassifyPage/components/YourStats/components/DailyClassificationsChart/DailyClassificationsChartContainer.spec.js
@@ -4,15 +4,24 @@ import React from 'react'
 import DailyClassificationsChartContainer from './DailyClassificationsChartContainer'
 import DailyClassificationsChart from './DailyClassificationsChart'
 
-let wrapper
-let componentWrapper
-
 describe('Component > DailyClassificationsChartContainer', function () {
+  let wrapper
+  let componentWrapper
+  const MOCK_DAILY_COUNTS = [
+    { count: 12, period: '2019-09-30'},
+    { count: 13, period: '2019-10-01' },
+    { count: 14, period: '2019-10-02' },
+    { count: 10, period: '2019-10-03' },
+    { count: 11, period: '2019-10-04' },
+    { count: 8, period: '2019-10-05' },
+    { count: 15, period: '2019-10-06' }
+  ]
+
   before(function () {
     wrapper = shallow(
       <DailyClassificationsChartContainer.wrappedComponent
         projectName="Test Project"
-        thisWeek={[]}
+        thisWeek={MOCK_DAILY_COUNTS}
       />
     )
     componentWrapper = wrapper.find(DailyClassificationsChart)
@@ -24,5 +33,56 @@ describe('Component > DailyClassificationsChartContainer', function () {
 
   it('should render the `DailyClassificationsChart` component', function () {
     expect(componentWrapper).to.have.lengthOf(1)
+  })
+
+  describe('daily stats counts', function () {
+    let counts
+    let monday
+
+    before(function () {
+      monday = new Date(2019, 9, 30)
+      counts = componentWrapper.prop('counts')
+    })
+
+    it('should exist for the entire week', function () {
+      expect(counts.length).to.equal(7)
+    })
+
+    for(let day = 0; day < 7; day++) {
+      let today
+      let dayShort
+      let dayLong
+      let count
+
+      before(function () {
+        today = new Date()
+        today.setDate(monday.getDate() + day)
+        dayShort = today.toLocaleDateString('en', { weekday: 'narrow' })
+        dayLong = today.toLocaleDateString('en', { weekday: 'long' })
+        count = counts[day]
+      })
+
+      describe(MOCK_DAILY_COUNTS[day].period, function () {
+        it('should have a count', function () {
+          expect(count.count).to.equal(MOCK_DAILY_COUNTS[day].count)
+        })
+
+        it('should have a period', function () {
+          expect(count.period).to.equal(MOCK_DAILY_COUNTS[day].period)
+        })
+
+        it('should have a short label', function () {
+          expect(count.label).to.equal(dayShort)
+        })
+
+        it('should have a long label', function () {
+          expect(count.longLabel).to.equal(dayLong)
+        })
+
+        it('should have an accessible description', function () {
+          expect(count.alt).to.equal(`${dayLong}: ${count.count}`)
+        })
+      })
+    }
   })
 })
