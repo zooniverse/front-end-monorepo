3e5622154c1ef1de247f07b0b62442255b1374ea
Roger Hutchings <roger@hutchin.gs>
2019-09-19 10:18:35 +0100

    Resize arrow

--- packages/app-project/src/screens/ProjectHomePage/components/Hero/components/Introduction/Introduction.js
+++ packages/app-project/src/screens/ProjectHomePage/components/Hero/components/Introduction/Introduction.js
@@ -1,6 +1,6 @@
 import { SpacedText } from '@zooniverse/react-components'
 import counterpart from 'counterpart'
-import { Anchor, Box, Paragraph, Text } from 'grommet'
+import { Anchor, Box, Paragraph } from 'grommet'
 import { Next } from 'grommet-icons'
 import Link from 'next/link'
 import { object, string } from 'prop-types'
@@ -10,6 +10,13 @@ import styled from 'styled-components'
 import en from './locales/en'
 
 counterpart.registerTranslations('en', en)
+
+// Selecting the div here to resize the gap between label and icon
+const StyledAnchor = styled(Anchor)`
+  & div {
+    width: 10px
+  }
+`
 
 const StyledParagraph = styled(Paragraph)`
   font-weight: bold;
@@ -28,9 +35,9 @@ function Introduction(props) {
         {description}
       </StyledParagraph>
       <Link {...link} passHref>
-        <Anchor
+        <StyledAnchor
           gap='xsmall'
-          icon={<Next color='light-5' />}
+          icon={<Next color='light-5' size='12px' />}
           label={<SpacedText>{counterpart('Introduction.link')}</SpacedText>}
           reverse
         />
