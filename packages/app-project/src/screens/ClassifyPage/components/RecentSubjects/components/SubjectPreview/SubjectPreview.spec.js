b709b71a93c178fc7917bf369aa421c1177ddea9
Jim O'Donnell <jim@zooniverse.org>
2019-09-19 15:53:03 +0100

    Add subject preview tests Test that the linked media is displayed and linked. Test that the subject Talk, Favourite and Collections buttons exist.

--- packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/components/SubjectPreview/SubjectPreview.spec.js
+++ packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/components/SubjectPreview/SubjectPreview.spec.js
@@ -1,16 +1,66 @@
 import { shallow } from 'enzyme'
 import React from 'react'
+import { Anchor } from 'grommet'
+import { Media } from '@zooniverse/react-components'
 
 import SubjectPreview from './SubjectPreview'
-
-let wrapper
+import { CollectionsButton, FavouritesButton, TalkLink } from './components'
 
 describe('Component > SubjectPreview', function () {
+  let wrapper
+  const recent = {
+    favorite: false,
+    subjectId: '12345',
+    locations: [
+      { 'image/jpeg': 'https://somedomain/photo.jpg' }
+    ]
+  }
+  const slug = 'owner/projectName'
+
   before(function () {
-    wrapper = shallow(<SubjectPreview />)
+    wrapper = shallow(
+      <SubjectPreview
+        recent={recent}
+        slug={slug}
+      />
+    )
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok()
+  })
+
+  describe('the preview media', function () {
+    let link
+    let media
+
+    before(function () {
+      link = wrapper.find(Anchor)
+      media = link.find(Media)
+    })
+
+    it('should link to the subject Talk page', function () {
+      expect(link.prop('href')).to.equal(`/projects/${slug}/talk/subjects/${recent.subjectId}`)
+    })
+
+    it('should display the first subject location', function () {
+      expect(media.prop('src')).to.equal('https://somedomain/photo.jpg')
+    })
+
+    it('should have alt text', function () {
+      expect(media.prop('alt')).to.equal('subject 12345')
+    })
+
+    it('should have a talk link', function () {
+      expect(wrapper.find(TalkLink).length).to.equal(1)
+    })
+
+    it('should have a favourites button', function () {
+      expect(wrapper.find(FavouritesButton).length).to.equal(1)
+    })
+
+    it('should have a collections button', function () {
+      expect(wrapper.find(CollectionsButton).length).to.equal(1)
+    })
   })
 })
