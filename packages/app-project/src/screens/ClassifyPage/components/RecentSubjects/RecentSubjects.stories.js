40e4971880854ed7d9e76be91810a6b6876a6ad7
Jim O'Donnell <jim@zooniverse.org>
2019-09-20 09:14:08 +0100

    Add stories to test layout Add stories for projects with portrait images or video subjects.

--- packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/RecentSubjects.stories.js
+++ packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/RecentSubjects.stories.js
@@ -29,6 +29,54 @@ const RECENTS = [
   }
 ]
 
+const PORTRAIT_SUBJECTS = [
+  {
+    favorite: false,
+    subjectId: '123',
+    locations: [
+      {'image/jpeg': 'https://panoptes-uploads.zooniverse.org/production/subject_location/fb2c57e2-96f7-49b1-9bd4-9bdc73d847f8.jpeg'}
+    ]
+  },
+  {
+    favorite: true,
+    subjectId: '456',
+    locations: [
+      {'image/jpeg': 'https://panoptes-uploads.zooniverse.org/production/subject_location/7be26d91-9dcf-4a50-a443-51ea9c630c0d.jpeg'}
+    ]
+  },
+  {
+    favorite: false,
+    subjectId: '789',
+    locations: [
+      {'image/jpeg': 'https://panoptes-uploads.zooniverse.org/production/subject_location/8b7deee1-af99-4400-a235-2bbd21ed4c39.jpeg'}
+    ]
+  }
+]
+
+const VIDEO_SUBJECTS = [
+  {
+    favorite: false,
+    subjectId: '123',
+    locations: [
+      {'video/mp4': 'https://panoptes-uploads.zooniverse.org/production/subject_location/279b23de-b1e8-4a1c-90c4-2d25bbee787d.mp4'}
+    ]
+  },
+  {
+    favorite: true,
+    subjectId: '456',
+    locations: [
+      {'video/mp4': 'https://panoptes-uploads.zooniverse.org/production/subject_location/64e2df1e-830d-4126-a1bc-8f0eef6e9eb9.mp4'}
+    ]
+  },
+  {
+    favorite: false,
+    subjectId: '789',
+    locations: [
+      {'video/mp4': 'https://panoptes-uploads.zooniverse.org/production/subject_location/d3e90135-efda-4391-b7bf-cbed262c9dc8.mp4'}
+    ]
+  }
+]
+
 function toggle() {
   this.favorite = !this.favorite
 }
@@ -41,6 +89,24 @@ storiesOf('Project App / Screens / Classify / Recent Subjects', module)
         recents={RECENTS}
         projectName="Snapshot Serengeti"
         slug="zooniverse/snapshot-serengeti"
+      />
+    </Grommet>
+  ))
+  .add('transcription', () => (
+    <Grommet theme={zooTheme}>
+      <RecentSubjectsContainer
+        recents={PORTRAIT_SUBJECTS}
+        projectName="Notes from Nature"
+        slug="zooniverse/notes-from-nature"
+      />
+    </Grommet>
+  ))
+  .add('video', () => (
+    <Grommet theme={zooTheme}>
+      <RecentSubjectsContainer
+        recents={VIDEO_SUBJECTS}
+        projectName="NestCams"
+        slug="spotteron/nestcams"
       />
     </Grommet>
   ))
