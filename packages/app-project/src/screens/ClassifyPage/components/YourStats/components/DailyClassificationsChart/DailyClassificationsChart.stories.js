70d015e9483746a96cbde96c695401f7460e99d2
Jim O'Donnell <jim@zooniverse.org>
2019-09-23 16:30:37 +0100

    Fix the storybook Add mock total counts to the chart props.

--- packages/app-project/src/screens/ClassifyPage/components/YourStats/components/DailyClassificationsChart/DailyClassificationsChart.stories.js
+++ packages/app-project/src/screens/ClassifyPage/components/YourStats/components/DailyClassificationsChart/DailyClassificationsChart.stories.js
@@ -14,10 +14,15 @@ const MOCK_DAILY_COUNTS = [
   { count: 0, period: '2019-10-06' }
 ]
 
+const MOCK_TOTALS = {
+  today: 25
+}
+
 storiesOf('Project App / Screens / Classify / Daily Classifications Chart', module)
   .add('plain', () => (
     <Grommet theme={zooTheme}>
       <DailyClassificationsChart.wrappedComponent
+        counts={MOCK_TOTALS}
         thisWeek={MOCK_DAILY_COUNTS}
         projectName="Snapshot Serengeti"
       />
