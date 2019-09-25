01003adfb33fb212da8172bb30baa4ad7c7ba258
mcbouslog <mcbouslog@gmail.com>
2019-09-25 14:36:17 -0500

    Add lcv annotation filter check for width > 0

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/LightCurveViewer/LightCurveViewer.js
@@ -204,7 +204,7 @@ class LightCurveViewer extends Component {
     if (!this.isCurrentTaskValidForAnnotation()) return // Sanity check
 
     const annotations = this.annotationBrushes
-      .filter((raw) => (raw.minX !== undefined && raw.maxX !== undefined))
+      .filter((raw) => (raw.minX !== undefined && raw.maxX !== undefined && (raw.maxX - raw.minX) > 0))
       .map((raw) => {
         const x = (raw.minX + raw.maxX) / 2
         const width = (raw.maxX - raw.minX)
