4b6cdd431a62e4f972b26c2e74cb4fa2e5e1f6aa
Jim O'Donnell <jim@zooniverse.org>
2019-09-27 17:32:52 +0100

    Register and configure the a11y addon

--- packages/app-project/.storybook/config.js
+++ packages/app-project/.storybook/config.js
@@ -1,8 +1,10 @@
-import { addParameters, configure } from '@storybook/react'
+import { addDecorator, addParameters, configure } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y';
 
 import { backgrounds } from './lib'
 
 addParameters({ backgrounds })
+addDecorator(withA11y)
 
 function loadStories() {
   const req = require.context('../src', true, /\.stories\.js$/)
