9824a89ff9eba36a1c5d08e1f7d26c8037711928
Jim O'Donnell <jim@zooniverse.org>
2019-09-27 15:44:03 +0100

    Alias all the things Add @components, @screens and @stores

--- packages/app-project/webpack.config.js
+++ packages/app-project/webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path')
 module.exports = {
   resolve: {
     alias: {
-      '@helpers': path.resolve(__dirname, 'src/helpers/'),
-      '@shared': path.resolve(__dirname, 'src/shared')
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@helpers': path.resolve(__dirname, 'src/helpers'),
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@stores': path.resolve(__dirname, 'src/stores')
     }
   }
 }
