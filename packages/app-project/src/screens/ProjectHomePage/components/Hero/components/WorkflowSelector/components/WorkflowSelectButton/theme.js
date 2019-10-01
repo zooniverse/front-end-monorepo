d1a93ad1dad4c21e1b02c36d6c48a426ed01ca7b
Jim O'Donnell <jim@zooniverse.org>
2019-10-01 13:22:09 +0100

    Add focus and hover styles to workflow links Update the workflow theme to add focus and hover styles taken from the Next button theme in the classifier.

--- packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/WorkflowSelectButton/theme.js
+++ packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/WorkflowSelectButton/theme.js
@@ -1,20 +1,32 @@
+import { adjustHue } from 'polished'
+
 const theme = {
   button: {
     border: {
       width: '0'
     },
-    color: 'black',
-    extend: props => `
-      text-align: center;
-      background: ${props.theme.global.colors['neutral-4']};
-      &:hover {
-        box-shadow: none;
-      }
-      &:disabled {
-        cursor: not-allowed;
-      }
-    `
-  }
+    color: {
+      dark: 'neutral-4',
+      light: 'black'
+    },
+    extend: props => {
+      const { theme: { dark, global: { colors } } } = props
+      
+      return `
+        text-align: center;
+        background: ${colors['neutral-4']};
+        &:disabled {
+          cursor: not-allowed;
+        }
+        &:focus:not(:disabled),
+        &:hover:not(:disabled) {
+          background: ${adjustHue(-7, colors['neutral-4'])};
+          box-shadow: 1px 1px 2px rgba(0, 0, 0, .5);
+          color: white;
+        }
+    `}
+  },
+  
 }
 
 export default theme
