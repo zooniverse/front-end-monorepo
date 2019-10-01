a0538d1cde9b9db680bc6b8086745efef700a456
Jim O'Donnell <jim@zooniverse.org>
2019-10-01 14:51:17 +0100

    Add light/dark theming Again, copied from the styles for the classifier's Next button.

--- packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/WorkflowSelectButton/theme.js
+++ packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/WorkflowSelectButton/theme.js
@@ -3,7 +3,8 @@ import { adjustHue } from 'polished'
 const theme = {
   button: {
     border: {
-      width: '0'
+      color: 'neutral-4',
+      width: '1px'
     },
     color: {
       dark: 'neutral-4',
@@ -14,19 +15,18 @@ const theme = {
       
       return `
         text-align: center;
-        background: ${colors['neutral-4']};
+        background: ${dark ? colors['dark-3'] : colors['neutral-4']};
         &:disabled {
           cursor: not-allowed;
         }
         &:focus:not(:disabled),
         &:hover:not(:disabled) {
-          background: ${adjustHue(-7, colors['neutral-4'])};
+          background: ${dark ? colors['neutral-4'] : adjustHue(-7, colors['neutral-4'])};
           box-shadow: 1px 1px 2px rgba(0, 0, 0, .5);
-          color: white;
+          color: ${dark ? 'black' : 'white'};
         }
     `}
-  },
-  
+  }
 }
 
 export default theme
