7d6da386808fbefbb8b3e7a3de34e0f68c47ab82
mcbouslog <mcbouslog@gmail.com>
2019-10-07 16:23:53 -0500

    WIP tests for workflow step drawing task views

--- packages/lib-classifier/src/store/DrawingStore.spec.js
+++ packages/lib-classifier/src/store/DrawingStore.spec.js
@@ -1,14 +1,50 @@
+import RootStore from './RootStore'
+import {
+  DrawingTaskFactory,
+  ProjectFactory,
+  SingleChoiceTaskFactory,
+  WorkflowFactory
+} from '../../test/factories'
+import { Factory } from 'rosie'
+import stubPanoptesJs from '../../test/stubPanoptesJs'
+
 import DrawingStore from './DrawingStore'
 
-let model
+function setupStores (clientStub, project, workflow) {
+  const store = RootStore.create({
+    classifications: {},
+    dataVisAnnotating: {},
+    drawing: {},
+    feedback: {},
+    fieldGuide: {},
+    subjects: {},
+    subjectViewer: {},
+    tutorials: {},
+    userProjectPreferences: {}
+  }, {
+    client: clientStub,
+    authClient: { checkBearerToken: () => Promise.resolve(), checkCurrent: () => Promise.resolve() }
+  })
+
+  store.projects.setResource(project)
+  store.projects.setActive(project.id)
+  if (workflow) {
+    store.workflows.setResource(workflow)
+    store.workflows.setActive(workflow.id)
+  }
+
+  return store
+}
 
 describe('Model > DrawingStore', function () {
-  before(function () {
-    model = DrawingStore.create()
-  })
+  const model = DrawingStore.create()
 
   it('should exist', function () {
     expect(model).to.be.an('object')
+  })
+
+  it('should default the active tool index to the first tool in the tools array', function () {
+    expect(model.activeDrawingTool).to.equal(0)
   })
 
   it('should have an `eventStream` observable', function () {
@@ -16,26 +52,87 @@ describe('Model > DrawingStore', function () {
     expect(model.eventStream.subscribe).to.be.a('function')
   })
 
-  it('should add new events to the stream on calling `addToStream`', function (done) {
-    const newEvent = 'foo'
-    model.eventStream.subscribe(value => {
-      expect(value).to.equal(newEvent)
-      done()
+  describe('Views > activeDrawingTask', function () {
+    it('should return the active workflow step drawing task', function () {
+      const drawingTask = DrawingTaskFactory.build()
+      drawingTask.help = 'random help string'
+      const workflow = WorkflowFactory.build({
+        steps: [
+          ['S1', { taskKeys: ['T9'] }]
+        ],
+        tasks: {
+          T9: drawingTask
+        }
+      })
+      const project = ProjectFactory.build({}, { activeWorkflowId: workflow.id })
+      const panoptesClientStub = stubPanoptesJs({ projects: project, workflows: workflow })
+      const rootStore = setupStores(panoptesClientStub, project, workflow)
+
+      expect(rootStore.drawing.activeDrawingTask.help).to.eql('random help string')
+      expect(rootStore.drawing.activeDrawingTask.taskKey).to.eql('T9')
     })
-    model.addToStream(newEvent)
   })
 
-  it('should default the active tool index to the first tool in the tools array', function () {
-    expect(model.activeDrawingTool).to.equal(0)
+  describe('Views > isDrawingInActiveWorkflowStep', function () {
+    it('should return true if there is a drawing task in the active workflow step', function () {
+      const workflow = WorkflowFactory.build({
+        steps: [
+          ['S1', { taskKeys: ['T1'] }]
+        ],
+        tasks: {
+          T1: DrawingTaskFactory.build()
+        }
+      })
+      const project = ProjectFactory.build({}, { activeWorkflowId: workflow.id })
+      const panoptesClientStub = stubPanoptesJs({ projects: project, workflows: workflow })
+      const rootStore = setupStores(panoptesClientStub, project, workflow)
+
+      expect(rootStore.drawing.isDrawingInActiveWorkflowStep).to.be.true()
+    })
+
+    it('should return false if there is not a drawing task in the active workflow step', function () {
+      const workflow = WorkflowFactory.build({
+        steps: [
+          ['S1', { taskKeys: ['T1'] }]
+        ],
+        tasks: {
+          T1: SingleChoiceTaskFactory.build()
+        }
+      })
+      const project = ProjectFactory.build({}, { activeWorkflowId: workflow.id })
+      const panoptesClientStub = stubPanoptesJs({ projects: project, workflows: workflow })
+      const rootStore = setupStores(panoptesClientStub, project, workflow)
+
+      expect(rootStore.drawing.isDrawingInActiveWorkflowStep).to.be.false()
+    })
   })
 
-  it('should set the active tool index', function () {
-    model.setActiveDrawingTool(1)
-    expect(model.activeDrawingTool).to.equal(1)
+  describe('Views > coordinateStream', function () {
+    describe('with an eventStream of client events pointerdown, pointermoves, and pointerup', function () {
+      it('should return a coordinateStream view of the events converted to svg coordinates', function () {
+        expect(true).to.be.false()
+      })
+    })
   })
 
-  it('should reset the active tool to 0 when reset is called', function () {
-    model.reset()
-    expect(model.activeDrawingTool).to.equal(0)
+  describe('Actions', function () {
+    it('should set the active tool index', function () {
+      model.setActiveDrawingTool(1)
+      expect(model.activeDrawingTool).to.equal(1)
+    })
+
+    it('should reset the active tool to 0 when reset is called', function () {
+      model.reset()
+      expect(model.activeDrawingTool).to.equal(0)
+    })
+
+    it('should add new events to the eventStream with `addToStream`', function (done) {
+      const newEvent = 'foo'
+      model.eventStream.subscribe(value => {
+        expect(value).to.equal(newEvent)
+        done()
+      })
+      model.addToStream(newEvent)
+    })
   })
 })
