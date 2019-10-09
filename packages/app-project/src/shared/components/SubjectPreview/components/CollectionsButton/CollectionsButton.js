93afc9fafd4ab83a620f3ecf57fae3e4d2db13b9
Jim O'Donnell <jim@zooniverse.org>
2019-10-09 16:17:21 +0100

    Package up the collections button (#1202)
    * Package up the collections button
    Make the collections button self-contained by adding a collections modal to it. Open the modal when the button is clicked (the modal is responsible for closing itself.) Add tests to check that the modal is opened when the button is enabled and clicked.
    
    * Add documentation
    Add a brief README and a storybook story.

--- packages/app-project/src/shared/components/SubjectPreview/components/CollectionsButton/CollectionsButton.js
+++ packages/app-project/src/shared/components/SubjectPreview/components/CollectionsButton/CollectionsButton.js
@@ -1,6 +1,7 @@
 import counterpart from 'counterpart'
-import PropTypes from 'prop-types'
+import { array, bool, func, shape, string } from 'prop-types'
 import React from 'react'
+import CollectionsModal from '@shared/components/CollectionsModal'
 import MetaToolsButton from '../MetaToolsButton'
 import CollectionsIcon from './CollectionsIcon'
 
@@ -9,23 +10,47 @@ import en from './locales/en'
 counterpart.registerTranslations('en', en)
 
 export default function CollectionsButton (props) {
-  const { disabled, onClick } = props
+  const { disabled, onClick, subject } = props
+  const collectionsModal = React.createRef()
+
+  function addToCollections () {
+    collectionsModal.current.wrappedInstance.open(subject.id)
+    onClick()
+  }
+
   return (
-    <MetaToolsButton
-      disabled={disabled}
-      icon={<CollectionsIcon color='dark-5' size='1em' />}
-      text={counterpart('CollectionsButton.add')}
-      onClick={onClick}
-    />
+    <>
+      <CollectionsModal
+        ref={collectionsModal}
+      />
+      <MetaToolsButton
+        disabled={disabled}
+        icon={<CollectionsIcon color='dark-5' size='1em' />}
+        text={counterpart('CollectionsButton.add')}
+        onClick={addToCollections}
+      />
+    </>
   )
 }
 
 CollectionsButton.propTypes = {
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func
+  disabled: bool,
+  onClick: func,
+  subject: shape({
+    favorite: bool,
+    id: string,
+    toggleFavourite: func,
+    locations: array
+  })
 }
 
 CollectionsButton.defaultProps = {
   disabled: false,
-  onClick: () => false
+  onClick: () => false,
+  subject: {
+    favorite: false,
+    id: '',
+    toggleFavorite: () => false,
+    locations: []
+  }
 }
