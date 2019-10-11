0750234242285fecb5b5f3513385283c19472370
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-10-11 15:11:51 +0100

    Fix static paths (#1216)
    * Fix static file paths in production
    
    - Moves the `static` folder to `public`, as per the Next.js docs: https://github.com/zeit/next.js#static-file-serving-eg-images
    - Update image url in RecentClassifications
    
    * Remove unused imports
    
    * Bump next to 9.1.1 (#1217)
    
    * Fix static file paths in production
    
    - Moves the `static` folder to `public`, as per the Next.js docs: https://github.com/zeit/next.js#static-file-serving-eg-images
    - Update image url in RecentClassifications
    
    * Remove unused imports
    
    * Add assetPrefix to build-time config
    
    Add assetPrefix to build-time config so it's available for URL
    creation as well as configuring the next server.
    
    * Use assetPrefix for placeholder url
    
    * update storybook build

--- packages/app-project/next.config.js
+++ packages/app-project/next.config.js
@@ -12,13 +12,15 @@ const talkHosts = require('./config/talkHosts')
 
 const PANOPTES_ENV = process.env.PANOPTES_ENV || 'staging'
 const webpackConfig = require('./webpack.config')
+const assetPrefix = process.env.ASSET_PREFIX || ''
 
 console.info(PANOPTES_ENV, talkHosts[PANOPTES_ENV])
 
 module.exports = {
-  assetPrefix: process.env.ASSET_PREFIX || '',
+  assetPrefix,
 
   env: {
+    ASSET_PREFIX: assetPrefix,
     COMMIT_ID: execSync('git rev-parse HEAD').toString('utf8').trim(),
     PANOPTES_ENV,
     TALK_HOST: talkHosts[PANOPTES_ENV]
--- packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/RecentSubjects.js
+++ packages/app-project/src/screens/ClassifyPage/components/RecentSubjects/RecentSubjects.js
@@ -2,8 +2,6 @@ import counterpart from 'counterpart'
 import { Box, Grid, Paragraph } from 'grommet'
 import { array, bool, number, string } from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
-import { Media } from '@zooniverse/react-components'
 
 import en from './locales/en'
 
@@ -15,6 +13,9 @@ counterpart.registerTranslations('en', en)
 function RecentSubjects (props) {
   const { isLoggedIn, recents, projectName, size, slug } = props
   const height = (size === 1) ? '40vw' : '200px'
+  const placeholderUrl = process.env.ASSET_PREFIX
+    ? `${process.env.ASSET_PREFIX}/subject-placeholder.png`
+    : '/subject-placeholder.png'
 
   return (
     <ContentBox title={counterpart('RecentSubjects.title', { projectName })}>
@@ -38,7 +39,7 @@ function RecentSubjects (props) {
               height={height}
               key={recent.subjectId}
               isLoggedIn={isLoggedIn}
-              placeholder={<img alt='' role='presentation' src='/static/subject-placeholder.png' />}
+              placeholder={<img alt='' role='presentation' src={placeholderUrl} />}
               subject={subject}
               slug={slug}
               width={'100%'}
@@ -55,7 +56,7 @@ function RecentSubjects (props) {
               overflow='hidden'
               width={'100%'}
             >
-              <img alt='' role='presentation' src='/static/subject-placeholder.png' />
+              <img alt='' role='presentation' src={placeholderUrl} />
             </Box>
           )
         })}
