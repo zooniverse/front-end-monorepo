f9096bb58babb1d1e3e9b9df7c6f1306006c698d
Jim O'Donnell <jim@zooniverse.org>
2019-10-11 10:55:06 +0100

    Set up task stories Set up task stories with a basic mock store. Add a story for the single answer question task.

---
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/Tasks.stories.js
@@ -0,0 +1,70 @@
+import { withKnobs, boolean, select } from '@storybook/addon-knobs'
+import asyncStates from '@zooniverse/async-states'
+import { storiesOf } from '@storybook/react'
+import zooTheme from '@zooniverse/grommet-theme'
+import React from 'react'
+import { Grommet } from 'grommet'
+import { Provider } from "mobx-react"
+
+import Tasks from './Tasks'
+
+const mockStore = {
+  classifications: {},
+  subjects: {},
+  subjectViewer: {
+    loadingState: asyncStates.loading
+  },
+  workflows: {
+    loadingState: asyncStates.loading
+  },
+  workflowSteps: {
+    isThereANextStep: () => false,
+    isThereAPreviousStep: () => false
+  }
+}
+
+storiesOf('Tasks', module)
+.addDecorator(withKnobs)
+.add('loading', function () {
+  return (
+    <Provider classifierStore={mockStore}>
+      <Grommet theme={zooTheme}>
+        <Tasks/>
+      </Grommet>
+    </Provider>
+  )
+})
+.add('single answer question', function () {
+  const step = null
+  const tasks = [{
+    answers: [{ label: 'yes' }, { label: 'no' }],
+    question: 'Is there a cat?',
+    help: 'Choose an answer from the choices given, then press Done.',
+    required: true,
+    taskKey: 'init',
+    type: 'single'
+  }]
+  const dark = boolean('Dark theme', false)
+  const loadingState = select('Subject loading', asyncStates, asyncStates.success)
+  const store = Object.assign({}, mockStore, {
+    subjectViewer: {
+      loadingState
+    },
+    workflows: {
+      loadingState: asyncStates.success
+    },
+    workflowSteps: {
+      activeStepTasks: tasks,
+      isThereANextStep: () => false,
+      isThereAPreviousStep: () => false,
+      isThereTaskHelp: true
+    }
+  })
+  return (
+    <Provider classifierStore={store}>
+      <Grommet theme={Object.assign({}, zooTheme, { dark })}>
+        <Tasks/>
+      </Grommet>
+    </Provider>
+  )
+})
