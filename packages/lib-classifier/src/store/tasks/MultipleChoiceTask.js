c25abdfb459b355303e98ee383b869945a7c0bcf
Jim O'Donnell <jim@zooniverse.org>
2019-10-14 10:59:59 +0100

    Make MultipleChoiceTask functional Convert the MultipleChoiceTask to a functional component with a useStateHook. Add task.annotation and task.updateAnnotation(value) to the MultipleChoiceTask model. Add tests.

--- packages/lib-classifier/src/store/tasks/MultipleChoiceTask.js
+++ packages/lib-classifier/src/store/tasks/MultipleChoiceTask.js
@@ -1,5 +1,6 @@
 import { types } from 'mobx-state-tree'
 import Task from './Task'
+import MultipleChoiceAnnotation from '../annotations/MultipleChoiceAnnotation'
 
 // TODO: should we make question/instruction consistent between task types?
 // What should be it called? I think we should use 'instruction'
@@ -14,6 +15,25 @@ const MultipleChoice = types.model('MultipleChoice', {
   required: types.maybe(types.boolean),
   type: types.literal('multiple')
 })
+.views(self => ({
+  get annotation () {
+    const { currentAnnotations } = getRoot(self).classifications
+    let currentAnnotation
+    if (currentAnnotations && currentAnnotations.size > 0) {
+      currentAnnotation = currentAnnotations.get(self.taskKey)
+    }
+    return currentAnnotation || self.defaultAnnotation
+  },
+  get defaultAnnotation () {
+    return MultipleChoiceAnnotation.create({ task: self.taskKey })
+  }
+}))
+.actions(self => ({
+  updateAnnotation (value) {
+    const { addAnnotation } = getRoot(self).classifications
+    addAnnotation(value, self)
+  }
+}))
 
 const MultipleChoiceTask = types.compose('MultipleChoiceTask', Task, MultipleChoice)
 
