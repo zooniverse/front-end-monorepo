a6b1ccc631ba935b1f4f771ee7464f4ea60a419c
Jim O'Donnell <jim@zooniverse.org>
2019-10-15 09:00:39 +0100

    Set up Task stories (#1223)
    * Make TaskHelp a functional component
    Unwrap TaskHelp from the classifier store and update tests to test component behaviour.
    
    * Set up task stories
    Set up task stories with a basic mock store.
    Add a story for the single answer question task.
    
    * Export the unwrapped Tasks component
    Use the unwrapped Tasks component for stories and pass in mock properties.
    
    * Make SingleChoiceTask functional
    Convert the SingleChoiceTask to a functional component with a useStateHook.
    Add task.annotation and task.updateAnnotation(value) to the SingleChoiceTask model.
    Add tests.
    
    * Make MultipleChoiceTask functional
    Convert the MultipleChoiceTask to a functional component with a useStateHook.
    Add task.annotation and task.updateAnnotation(value) to the MultipleChoiceTask model.
    Add tests.
    
    * Add a story for multiple tasks
    Add a step with a multiple choice task and a single choice task.
    
    * Add some mock task help
    
    * Fix help with multiple tasks
    Render the horizontal rule outside the markdown component.
    
    * Update the base Task model
    Move generic task views and actions into the base task model. Single answer and multiple choice tasks now only need to define their respective default annotations.

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskHelp/TaskHelp.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskHelp/TaskHelp.js
@@ -3,7 +3,7 @@ import counterpart from 'counterpart'
 import { Button, Box } from 'grommet'
 import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import en from './locales/en'
@@ -14,81 +14,60 @@ export const StyledPlainButton = styled(PlainButton)`
   text-align: center;
 `
 
-function storeMapper (stores) {
-  const tasks = stores.classifierStore.workflowSteps.activeStepTasks
-  const { isThereTaskHelp } = stores.classifierStore.workflowSteps
-  return {
-    isThereTaskHelp,
-    tasks
-  }
-}
+function TaskHelp (props) {
+  const [ showModal, setShowModal ] = useState(false)
 
-@inject(storeMapper)
-@observer
-class TaskHelp extends React.Component {
-  constructor () {
-    super()
+  const label = counterpart('TaskHelp.label')
+  const { tasks } = props
 
-    this.state = {
-      showModal: false
-    }
-  }
-
-  render () {
-    const label = counterpart('TaskHelp.label')
-    const { isThereTaskHelp, tasks } = this.props
-
-    if (isThereTaskHelp) {
-      return (
+  return (
+    <>
+      <Box>
+        <StyledPlainButton
+          onClick={() => setShowModal(true)}
+          margin={{ bottom: 'small' }}
+          text={label}
+        />
+      </Box>
+      <Modal
+        active={showModal}
+        closeFn={() => setShowModal(false)}
+        title={label}
+      >
         <>
-          <Box>
-            <StyledPlainButton
-              onClick={() => this.setState({ showModal: true })}
-              margin={{ bottom: 'small' }}
-              text={label}
+          <Box
+            height='medium'
+            overflow='auto'
+          >
+            {tasks.map((task) => {
+              if (tasks.length > 1) {
+                return (
+                  <>
+                    <Markdownz key={task.taskKey}>
+                      {task.help}
+                    </Markdownz>
+                    <hr />
+                  </>
+                )
+              }
+
+              return <Markdownz key={task.taskKey}>{task.help}</Markdownz>
+            })}
+          </Box>
+          <Box pad={{ top: 'small' }}>
+            <Button
+              onClick={() => setShowModal(false)}
+              label={counterpart('TaskHelp.close')}
+              primary
             />
           </Box>
-          <Modal
-            active={this.state.showModal}
-            closeFn={() => this.setState({ showModal: false })}
-            title={label}
-          >
-            <>
-              <Box
-                height='medium'
-                overflow='auto'
-              >
-                {tasks.map((task) => {
-                  if (tasks.length > 1) {
-                    return (
-                      <Markdownz key={task.taskKey}>
-                        {task.help}
-                        <hr />
-                      </Markdownz>
-                    )
-                  }
-
-                  return <Markdownz key={task.taskKey}>{task.help}</Markdownz>
-                })}
-              </Box>
-              <Box pad={{ top: 'small' }}>
-                <Button
-                  onClick={() => this.setState({ showModal: false })}
-                  label={counterpart('TaskHelp.close')}
-                  primary
-                />
-              </Box>
-            </>
-          </Modal>
         </>
-      )
-    }
-
-    return null
-  }
+      </Modal>
+    </>
+  )
 }
 
-TaskHelp.wrappedComponent.propTypes = {
+TaskHelp.propTypes = {
   tasks: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
