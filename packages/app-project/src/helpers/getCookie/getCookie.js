43d3a9a3801bb02d44a9db5298678682ad470c33
Roger Hutchings <roger@hutchin.gs>
2019-10-22 09:57:12 +0100

    Remove log

--- packages/app-project/src/helpers/getCookie/getCookie.js
+++ packages/app-project/src/helpers/getCookie/getCookie.js
@@ -3,7 +3,6 @@ import cookie from 'cookie'
 export default function getCookie (context, name) {
   if (context && context.req && context.req.headers.cookie) {
     const parsedCookie = cookie.parse(context.req.headers.cookie)
-    console.info('parsedCookie', parsedCookie)
     if (parsedCookie && parsedCookie[name]) {
       return parsedCookie[name]
     }
