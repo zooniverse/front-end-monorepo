ef6c515d6d54b174615815164a4bafe29caab81d
Jim O'Donnell <jim@zooniverse.org>
2019-10-23 14:26:58 +0100

    Compose annotations Add the base Annotation model to text annotations.

--- packages/lib-classifier/src/plugins/tasks/TextTask/models/TextAnnotation.js
+++ packages/lib-classifier/src/plugins/tasks/TextTask/models/TextAnnotation.js
@@ -1,8 +1,9 @@
 import { types } from 'mobx-state-tree'
+import { Annotation } from '@store/annotations'
 
-const TextAnnotation = types.model('TextAnnotation', {
-  task: types.identifier,
+const Text = types.model('Text', {
   value: types.optional(types.string, '')
 })
 
+const TextAnnotation = types.compose('TextAnnotation', Annotation, Text)
 export default TextAnnotation
