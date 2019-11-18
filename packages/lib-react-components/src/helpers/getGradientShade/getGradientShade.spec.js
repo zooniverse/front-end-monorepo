6a49357bb6b64d6d15c88aa31d1c40283bdb3ccc
Roger Hutchings <roger@hutchin.gs>
2019-11-18 12:01:14 +0000

    rename test

--- packages/lib-react-components/src/helpers/getGradientShade/getGradientShade.spec.js
+++ packages/lib-react-components/src/helpers/getGradientShade/getGradientShade.spec.js
@@ -5,7 +5,7 @@ describe('Helper > getGradientShade', function () {
     expect(getGradientShade).to.be.a('function')
   })
 
-  it('should return the expected results', function () {
+  it('should return the expected shades from given colours', function () {
     expect(getGradientShade('#f0b200')).to.equal('#ea9300') // gold
     expect(getGradientShade('#00979d')).to.equal('#006d75') // teal
     expect(getGradientShade('#078F52')).to.equal('#046231') // green
