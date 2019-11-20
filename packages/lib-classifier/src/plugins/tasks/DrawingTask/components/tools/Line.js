bd0fb7be82b98ad5a7a9c93ea0c35245f4564108
Jim O'Donnell <jim@zooniverse.org>
2019-11-20 12:09:02 +0000

    Remove DrawingToolRoot from all marks Remove the drawing tool root from Line and Point marks. Add drag handles to Line marks.

--- packages/lib-classifier/src/plugins/tasks/DrawingTask/components/tools/Line.js
+++ packages/lib-classifier/src/plugins/tasks/DrawingTask/components/tools/Line.js
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import DrawingToolRoot from './DrawingToolRoot'
+import DragHandle from '../components/DragHandle'
 
-function Line ({ active, mark, tool }) {
+const GRAB_STROKE_WIDTH = 6
+
+function Line ({ active, children, mark, svg, tool }) {
   const { x1, y1, x2, y2 } = mark
 
+  function onHandleDrag (coords) {
+    mark.setCoordinates(coords)
+  }
+
   return (
-    <DrawingToolRoot active tool={tool}>
+    <g>
       <line x1={x1} y1={y1} x2={x2} y2={y2} />
-    </DrawingToolRoot>
+      <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={GRAB_STROKE_WIDTH} strokeOpacity="0" />
+      <DragHandle svg={svg} x={x1} y={y1} dragMove={(e, d) => onHandleDrag({ x1: x1 + d.x, y1: y1 + d.y, x2, y2 })} />
+      <DragHandle svg={svg} x={x2} y={y2} dragMove={(e, d) => onHandleDrag({ x1, y1, x2: x2 + d.x, y2: y2 + d.y})} />
+      {children}
+    </g>
   )
 }
 
