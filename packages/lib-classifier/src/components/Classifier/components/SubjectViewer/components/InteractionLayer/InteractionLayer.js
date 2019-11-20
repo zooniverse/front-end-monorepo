4b07d312e7781cef0598a4492d3c9d4ad2ff6044
Jim O'Donnell <jim@zooniverse.org>
2019-11-20 10:54:15 +0000

    Rewrite InteractionLayer Add event handlers and SVG coordinate transforms to InteractionLayer. Create marks on pointerDown, follow the pointer on pointerMove then finish mark creation on pointerUp. Enumerate tools in the active task, then render tool.marks for each tool.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -1,24 +1,94 @@
+import cuid from 'cuid'
+import { observer } from 'mobx-react'
 import { func } from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 
-function InteractionLayer ({ onPointerDown, onPointerMove, onPointerUp }) {
+function InteractionLayer ({ activeDrawingTask, svg }) {
+  const [ activeMark, setActiveMark ] = useState(null)
+
+  function convertEvent (event) {
+    const type = event.type
+
+    const clientX = event.clientX
+    const clientY = event.clientY
+    const svgEventOffset = getEventOffset(clientX, clientY)
+
+    const svgCoordinateEvent = {
+      pointerId: event.pointerId,
+      type,
+      x: svgEventOffset.x,
+      y: svgEventOffset.y
+    }
+
+    return svgCoordinateEvent
+  }
+
+  function getEventOffset (x, y) {
+    const svgEvent = svg.createSVGPoint()
+    svgEvent.x = x
+    svgEvent.y = y
+    const svgEventOffset = svgEvent.matrixTransform(svg.getScreenCTM().inverse())
+
+    return svgEventOffset
+  }
+
+  function onPointerDown (event) {
+    const activeTool = activeDrawingTask.activeTool
+    const activeMark = activeTool.createMark({
+      id: cuid(),
+      toolIndex: activeDrawingTask.activeToolIndex
+    })
+    activeMark.setCoordinates(convertEvent(event))
+    setActiveMark(activeMark)
+  }
+
+  function onPointerMove (event) {
+    if (activeMark) {
+      activeMark.setCoordinates(convertEvent(event))
+    }
+  }
+
+  function onPointerUp (event) {
+    if (activeMark) {
+      activeMark.setCoordinates(convertEvent(event))
+    }
+    setActiveMark(null)
+  }
+
   return (
-    <rect
-      id='InteractionLayer'
-      width='100%'
-      height='100%'
+    <g
       onPointerDown={onPointerDown}
       onPointerMove={onPointerMove}
       onPointerUp={onPointerUp}
-      fill='transparent'
-    />
+    >
+      <rect
+        id='InteractionLayer'
+        width='100%'
+        height='100%'
+        fill='transparent'
+      />
+      {activeDrawingTask &&
+        activeDrawingTask.tools.map( tool => {
+          const marksArray = Array.from(tool.marks.values())
+          return marksArray.map(mark => {
+
+            const MarkingComponent = observer(mark.toolComponent)
+
+            return (
+              <MarkingComponent
+                key={mark.id}
+                active={mark === activeMark}
+                mark={mark}
+                tool={tool}
+              />
+            )
+          })})
+      }
+    </g>
   )
 }
 
 InteractionLayer.propTypes = {
-  onPointerDown: func.isRequired,
-  onPointerMove: func.isRequired,
-  onPointerUp: func.isRequired
 }
 
 export default InteractionLayer
