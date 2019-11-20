3a4cbcbed02c4b65f56bce7c5571ecb984faf7c2
Roger Hutchings <roger@hutchin.gs>
2019-11-20 11:42:34 +0000

    Remove experimental related projects API

--- packages/app-related-projects-experiment/src/db.js
+++
@@ -1,9 +0,0 @@
-const low = require('lowdb')
-const Memory = require('lowdb/adapters/Memory')
-
-const db = low(new Memory())
-
-db.defaults({ projects: [], relatedProjects: [] })
-  .write()
-
-module.exports = db
