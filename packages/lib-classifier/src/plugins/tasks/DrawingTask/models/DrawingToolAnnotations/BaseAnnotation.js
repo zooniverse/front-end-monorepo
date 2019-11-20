1cdc854b48592594399088fc771791997891e603
Jim O'Donnell <jim@zooniverse.org>
2019-11-20 10:35:17 +0000

    Remove annotation models Drawing annotations are an array of marks, so we don't need separate annotation models.

--- packages/lib-classifier/src/plugins/tasks/DrawingTask/models/DrawingToolAnnotations/BaseAnnotation.js
+++
@@ -1,8 +0,0 @@
-import { types } from 'mobx-state-tree'
-
-const BaseAnnotation = types.model('BaseAnnotation', {
-  frame: types.optional(types.number, 0),
-  tool: types.optional(types.number, 0)
-})
-
-export default BaseAnnotation
