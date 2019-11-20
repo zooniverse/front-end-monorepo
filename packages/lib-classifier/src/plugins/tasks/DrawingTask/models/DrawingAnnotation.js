17c2ac2ed2461e579e1aa19b8e4f0a2929bab27a
Jim O'Donnell <jim@zooniverse.org>
2019-11-20 10:43:40 +0000

    Add marks to drawing annotation model Add marks to the annotation model and update the spec to use a valid snapshot.

--- packages/lib-classifier/src/plugins/tasks/DrawingTask/models/DrawingAnnotation.js
+++ packages/lib-classifier/src/plugins/tasks/DrawingTask/models/DrawingAnnotation.js
@@ -1,21 +1,10 @@
 import { types } from 'mobx-state-tree'
 import Annotation from '../../models/Annotation'
-import { LineAnnotation, PointAnnotation } from './DrawingToolAnnotations'
+import * as markTypes from './markings'
 
 const Drawing = types.model('Drawing', {
   value: types.array(
-    types.union({
-      dispatcher: (snapshot) => {
-        switch (snapshot.toolType) {
-          case 'line':
-            return LineAnnotation
-          case 'point':
-            return PointAnnotation
-          default:
-            return null
-        }
-      }
-    })
+    types.union(...Object.values(markTypes))
   )
 })
 
