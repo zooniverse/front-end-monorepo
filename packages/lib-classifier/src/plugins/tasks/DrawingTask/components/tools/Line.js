10d0784f443336721f147d222463cc926c275d40
Jim O'Donnell <jim@zooniverse.org>
2019-11-21 18:25:02 +0000

    Hide drag handles on inactive lines

--- packages/lib-classifier/src/plugins/tasks/DrawingTask/components/tools/Line.js
+++ packages/lib-classifier/src/plugins/tasks/DrawingTask/components/tools/Line.js
@@ -15,8 +15,10 @@ function Line ({ active, children, mark, svg, tool }) {
     <g>
       <line x1={x1} y1={y1} x2={x2} y2={y2} />
       <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={GRAB_STROKE_WIDTH} strokeOpacity="0" />
-      <DragHandle svg={svg} x={x1} y={y1} dragMove={(e, d) => onHandleDrag({ x1: x1 + d.x, y1: y1 + d.y, x2, y2 })} />
-      <DragHandle svg={svg} x={x2} y={y2} dragMove={(e, d) => onHandleDrag({ x1, y1, x2: x2 + d.x, y2: y2 + d.y})} />
+      {active &&
+        <DragHandle svg={svg} x={x1} y={y1} dragMove={(e, d) => onHandleDrag({ x1: x1 + d.x, y1: y1 + d.y, x2, y2 })} />}
+      {active &&
+        <DragHandle svg={svg} x={x2} y={y2} dragMove={(e, d) => onHandleDrag({ x1, y1, x2: x2 + d.x, y2: y2 + d.y})} />}
       {children}
     </g>
   )
