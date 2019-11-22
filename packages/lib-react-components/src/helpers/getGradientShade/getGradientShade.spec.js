bef2fb4d896a15ab0bc6998c4db7630d926b1e1e
Roger Hutchings <rogerhutchings@users.noreply.github.com>
2019-11-22 10:32:35 +0000

    Add the new workflow button (#1310)
    Adds the new design for the workflow selection button to match the latest design.
    
    * Add the new progress gradient
    * Move the text around
    * Add drop shadow
    * Add gradient shade helper
    * Update normal, hover and click background states
    * Add in the icon
    * Update tests

---
+++ packages/lib-react-components/src/helpers/getGradientShade/getGradientShade.spec.js
@@ -0,0 +1,13 @@
+import getGradientShade from './getGradientShade'
+
+describe('Helper > getGradientShade', function () {
+  it('should be a function', function () {
+    expect(getGradientShade).to.be.a('function')
+  })
+
+  it('should return the expected shades from given colours', function () {
+    expect(getGradientShade('#f0b200')).to.equal('#ea9300') // gold
+    expect(getGradientShade('#00979d')).to.equal('#006d75') // teal
+    expect(getGradientShade('#078F52')).to.equal('#046231') // green
+  })
+})
