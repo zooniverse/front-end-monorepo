zooniverse/front-end-monorepo

commit bd133d4ae6f6561f04491b662de22a7f5c3ad478

Drawing: select, move and delete marks (#1308)
- Differentiates between creating a mark (initial click and drag) and selecting a mark (click an existing mark and, optionally, drag.) 
 - Adds `mark.isValid`, which is checked on pointer up. Invalid marks are deleted.
 - starts an API for passing position changes to the Mark model, including `mark.move({ x, y })`, where `{ x, y }` is the relative change in the mark position.
 - Allows marks to be selected and dragged with a pointer.
 - Adds the delete button to marks.
 - Adds drag handles to the line tool, and a `draggable` decorator for SVG components.
 - updates the canvas AKA `InteractionLayer` when marks are moved, resized or deleted.

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -1,17 +1,23 @@
 import cuid from 'cuid'
-import { observer } from 'mobx-react'
 import { func } from 'prop-types'
 import React, { useState } from 'react'
+import styled from 'styled-components'
+import DrawingToolMarks from './components/DrawingToolMarks'
+
+const StyledRect = styled('rect')`
+  :hover {
+    cursor: crosshair;
+  }
+`
 
 function InteractionLayer ({ activeDrawingTask, svg }) {
   const [ activeMark, setActiveMark ] = useState(null)
+  const [ creating, setCreating ] = useState(false)
 
   function convertEvent (event) {
     const type = event.type
 
-    const clientX = event.clientX
-    const clientY = event.clientY
-    const svgEventOffset = getEventOffset(clientX, clientY)
+    const svgEventOffset = getEventOffset(event.clientX, event.clientY)
 
     const svgCoordinateEvent = {
       pointerId: event.pointerId,
@@ -28,61 +34,58 @@ function InteractionLayer ({ activeDrawingTask, svg }) {
     svgEvent.x = x
     svgEvent.y = y
     const svgEventOffset = svgEvent.matrixTransform(svg.getScreenCTM().inverse())
-
     return svgEventOffset
   }
 
   function onPointerDown (event) {
-    const activeTool = activeDrawingTask.activeTool
+    const { activeTool } = activeDrawingTask
     const activeMark = activeTool.createMark({
       id: cuid(),
       toolIndex: activeDrawingTask.activeToolIndex
     })
-    activeMark.setCoordinates(convertEvent(event))
+    activeMark.initialPosition(convertEvent(event))
     setActiveMark(activeMark)
+    setCreating(true)
   }
 
   function onPointerMove (event) {
-    if (activeMark) {
-      activeMark.setCoordinates(convertEvent(event))
-    }
+    creating && activeMark.initialDrag(convertEvent(event))
   }
 
-  function onPointerUp (event) {
-    if (activeMark) {
-      activeMark.setCoordinates(convertEvent(event))
+  function onPointerUp () {
+    setCreating(false)
+    if (activeMark && !activeMark.isValid) {
+      const { activeTool } = activeDrawingTask
+      activeTool.deleteMark(activeMark)
+      setActiveMark(null)
     }
-    setActiveMark(null)
   }
 
   return (
     <g
-      onPointerDown={onPointerDown}
       onPointerMove={onPointerMove}
       onPointerUp={onPointerUp}
     >
-      <rect
+      <StyledRect
         id='InteractionLayer'
         width='100%'
         height='100%'
         fill='transparent'
+        onPointerDown={onPointerDown}
       />
       {activeDrawingTask &&
-        activeDrawingTask.tools.map( tool => {
-          const marksArray = Array.from(tool.marks.values())
-          return marksArray.map(mark => {
-
-            const MarkingComponent = observer(mark.toolComponent)
-
-            return (
-              <MarkingComponent
-                key={mark.id}
-                active={mark === activeMark}
-                mark={mark}
-                tool={tool}
-              />
-            )
-          })})
+        activeDrawingTask.tools.map(tool => {
+          return (
+            <DrawingToolMarks
+              key={`${tool.type}-${tool.toolIndex}`}
+              activeMarkId={activeMark && activeMark.id}
+              onDelete={() => setActiveMark(null)}
+              onSelectMark={mark => setActiveMark(mark)}
+              svg={svg}
+              tool={tool}
+            />
+          )
+        })
       }
     </g>
   )

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.spec.js
@@ -9,6 +9,8 @@ import { Line, Point } from '@plugins/tasks/DrawingTask/components/tools'
 describe('Component > InteractionLayer', function () {
   let wrapper
   const mockMark = {
+    initialDrag: sinon.stub(),
+    initialPosition: sinon.stub(),
     setCoordinates: sinon.stub()
   }
   const mockDrawingTask = DrawingTask.TaskModel.create({
@@ -56,13 +58,34 @@ describe('Component > InteractionLayer', function () {
     expect(rect.prop('fill')).to.equal('transparent')
   })
 
-  it('should create a mark on pointer down', function () {
-    const fakeEvent = {
-      type: 'pointer'
-    }
-    sinon.spy(mockDrawingTask.activeTool, 'createMark')
-    wrapper.simulate('pointerdown', fakeEvent)
-    expect(mockDrawingTask.activeTool.createMark).to.have.been.calledOnce()
-    mockDrawingTask.activeTool.createMark.restore()
+  describe('on pointer events', function () {
+    before(function () {
+      sinon.stub(mockDrawingTask.activeTool, 'createMark').callsFake(() => mockMark)
+    })
+
+    after(function () {
+      mockDrawingTask.activeTool.createMark.restore()
+    })
+
+    it('should create a mark on pointer down', function () {
+      const fakeEvent = {
+        type: 'pointer'
+      }
+      wrapper.find('rect').simulate('pointerdown', fakeEvent)
+      expect(mockDrawingTask.activeTool.createMark).to.have.been.calledOnce()
+    })
+
+    it('should place a new mark on pointer down', function () {
+      expect(mockMark.initialPosition).to.have.been.calledOnce()
+    })
+
+    it('should drag the new mark on pointer down + move', function () {
+      const fakeEvent = {
+        type: 'pointer'
+      }
+      wrapper.find('rect').simulate('pointerdown', fakeEvent)
+      wrapper.find('rect').simulate('pointermove', fakeEvent)
+      expect(mockMark.initialDrag).to.have.been.calledOnce()
+    })
   })
 })

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.spec.js
@@ -8,9 +8,11 @@ describe('Component > InteractionLayerContainer', function () {
     shallow(<InteractionLayerContainer.wrappedComponent />)
   })
 
-  it('should render an InteractionLayer', function () {
-    const wrapper = shallow(<InteractionLayerContainer.wrappedComponent />)
-    expect(wrapper.find('InteractionLayer')).to.have.lengthOf(1)
+  describe('with an active drawing task', function () {
+    it('should render an InteractionLayer', function () {
+      const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeDrawingTask />)
+      expect(wrapper.find('InteractionLayer')).to.have.lengthOf(1)
+    })
   })
 
   describe('with active workflow step including a drawing task', function () {

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.js
@@ -17,15 +17,9 @@ function storeMapper (stores) {
 @inject(storeMapper)
 @observer
 class InteractionLayerContainer extends Component {
-
   render () {
     const { activeDrawingTask, svg } = this.props
-    return (
-      <InteractionLayer
-        activeDrawingTask={activeDrawingTask}
-        svg={svg}
-      />
-    )
+    return activeDrawingTask ? <InteractionLayer activeDrawingTask={activeDrawingTask} svg={svg} /> : null
   }
 }
 

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/DrawingToolMarks/DrawingToolMarks.js
@@ -1,0 +1,81 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import PropTypes from 'prop-types'
+import DrawingToolRoot from '@plugins/tasks/DrawingTask/components/tools/DrawingToolRoot'
+import DeleteButton from '@plugins/tasks/DrawingTask/components/tools/DeleteButton'
+
+function DrawingToolMarks ({ activeMarkId, onDelete, onDeselectMark, onSelectMark, svg, tool }) {
+  const marksArray = Array.from(tool.marks.values())
+
+  return marksArray.map((mark, index) => {
+    const MarkingComponent = observer(mark.toolComponent)
+    const ObservedDeleteButton = observer(DeleteButton)
+    const isActive = mark.id === activeMarkId
+
+    function deleteMark () {
+      tool.deleteMark(mark)
+      onDelete(mark)
+    }
+
+    function moveMark (event, difference) {
+      mark.move(difference)
+    }
+
+    function deselectMark () {
+      onDeselectMark(mark)
+    }
+
+    function selectMark () {
+      onSelectMark(mark)
+    }
+
+    return (
+      <DrawingToolRoot
+        key={mark.id}
+        isActive={isActive}
+        coords={mark.coords}
+        dragStart={selectMark}
+        dragMove={moveMark}
+        dragEnd={deselectMark}
+        label={`Mark ${index}`}
+        mark={mark}
+        onDelete={deleteMark}
+        onDeselect={onDeselectMark}
+        onSelect={onSelectMark}
+        svg={svg}
+        tool={tool}
+      >
+        <MarkingComponent
+          active={isActive}
+          mark={mark}
+          svg={svg}
+          tool={tool}
+        />
+        {isActive && <ObservedDeleteButton
+          label={`Delete ${tool.type}`}
+          mark={mark}
+          svg={svg}
+          onDelete={deleteMark}
+        />}
+      </DrawingToolRoot>
+    )
+  })
+}
+
+DrawingToolMarks.propTypes = {
+  activeMarkId: PropTypes.string,
+  onDelete: PropTypes.func,
+  onDeselectMark: PropTypes.func,
+  onSelectMark: PropTypes.func,
+  svg: PropTypes.instanceOf(Element).isRequired,
+  tool: PropTypes.object.isRequired
+}
+
+DrawingToolMarks.defaultProps = {
+  activeMarkId: '',
+  onDelete: () => true,
+  onDeselectMark: () => true,
+  onSelectMark: () => true
+}
+
+export default DrawingToolMarks

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/DrawingToolMarks/index.js
@@ -1,0 +1,1 @@
+export { default } from './DrawingToolMarks'

packages/lib-classifier/src/plugins/tasks/DrawingTask/components/components/DragHandle/DragHandle.js
@@ -1,0 +1,28 @@
+import React, { forwardRef } from 'react'
+import styled from 'styled-components'
+import draggable from '../draggable'
+
+const StyledCircle = styled('circle')`
+  &:hover {
+    cursor: move;
+  }
+`
+const RADIUS = screen.width > 900 ? 4 : 10
+const OVERSHOOT = screen.width > 900 ? 4 : 10
+
+const DragHandle = forwardRef(({ svg, x, y }, ref) => {
+  const matrix = svg.getScreenCTM()
+
+  const styleProps = {
+    fill: 'currentColor',
+    stroke: 'transparent',
+    strokeWidth: OVERSHOOT,
+    transform: `\
+translate(${x}, ${y})
+matrix( ${1 / matrix.a} 0 0 ${1 / matrix.d} 0 0)\
+`
+  }
+  return <StyledCircle ref={ref} r={RADIUS} {...styleProps} />
+})
+
+export default draggable(DragHandle)

packages/lib-classifier/src/plugins/tasks/DrawingTask/components/components/DragHandle/index.js
@@ -1,0 +1,1 @@
+export { default } from './DragHandle'

commit 1d0c138e33125d04ab4136376463379a18a23b1f

Fix broken tests Fix tests that broke in #1308. Remove the DrawingContainer from tests, as it's no longer used anywhere. Replace rect with a styled rect in InteractionLayer tests.

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -95,3 +95,4 @@ InteractionLayer.propTypes = {
 }
 
 export default InteractionLayer
+export { StyledRect }

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.spec.js
@@ -1,8 +1,9 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import sinon from 'sinon'
+import styled from 'styled-components'
 
-import InteractionLayer from './InteractionLayer'
+import InteractionLayer, { StyledRect } from './InteractionLayer'
 import DrawingTask from '@plugins/tasks/DrawingTask'
 import { Line, Point } from '@plugins/tasks/DrawingTask/components/tools'
 
@@ -51,8 +52,8 @@ describe('Component > InteractionLayer', function () {
     expect(wrapper).to.be.ok()
   })
 
-  it('should return a transparent rect', function () {
-    const rect = wrapper.find('rect')
+  it('should render a transparent rect', function () {
+    const rect = wrapper.find(StyledRect)
     expect(rect.exists()).to.be.true()
     expect(rect.prop('id')).to.equal('InteractionLayer')
     expect(rect.prop('fill')).to.equal('transparent')
@@ -71,7 +72,7 @@ describe('Component > InteractionLayer', function () {
       const fakeEvent = {
         type: 'pointer'
       }
-      wrapper.find('rect').simulate('pointerdown', fakeEvent)
+      wrapper.find(StyledRect).simulate('pointerdown', fakeEvent)
       expect(mockDrawingTask.activeTool.createMark).to.have.been.calledOnce()
     })
 
@@ -83,8 +84,8 @@ describe('Component > InteractionLayer', function () {
       const fakeEvent = {
         type: 'pointer'
       }
-      wrapper.find('rect').simulate('pointerdown', fakeEvent)
-      wrapper.find('rect').simulate('pointermove', fakeEvent)
+      wrapper.find(StyledRect).simulate('pointerdown', fakeEvent)
+      wrapper.simulate('pointermove', fakeEvent)
       expect(mockMark.initialDrag).to.have.been.calledOnce()
     })
   })

packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.spec.js
@@ -1,7 +1,6 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import InteractionLayerContainer from './InteractionLayerContainer'
-import DrawingContainer from '../Drawing/DrawingContainer'
 
 describe('Component > InteractionLayerContainer', function () {
   it('should render without crashing', function () {
@@ -12,20 +11,6 @@ describe('Component > InteractionLayerContainer', function () {
     it('should render an InteractionLayer', function () {
       const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeDrawingTask />)
       expect(wrapper.find('InteractionLayer')).to.have.lengthOf(1)
-    })
-  })
-
-  describe('with active workflow step including a drawing task', function () {
-    it('should render a DrawingContainer', function () {
-      const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeStepTasks={activeStepTasksWithDrawing} />)
-      expect(wrapper.find(DrawingContainer)).to.have.lengthOf(1)
-    })
-  })
-
-  describe('with active workflow step excluding a drawing task', function () {
-    it('should not render a DrawingContainer', function () {
-      const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeStepTasks={activeStepTasksWithSingleChoice} />)
-      expect(wrapper.find(DrawingContainer)).to.have.lengthOf(0)
     })
   })
 })