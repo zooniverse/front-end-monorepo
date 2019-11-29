71ee8ea48088ca82f320e20333e3c417f5161036
Jim O'Donnell <jim@zooniverse.org>
2019-11-29 09:01:15 +0000

    Fix broken tests (#1333)
    Fix tests that broke in #1308.
    Remove the DrawingContainer from tests, as it's no longer used anywhere.
    Replace rect with a styled rect in InteractionLayer tests.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.spec.js
@@ -1,7 +1,6 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import InteractionLayerContainer from './InteractionLayerContainer'
-import DrawingContainer from '../Drawing/DrawingContainer'
 
 describe('Component > InteractionLayerContainer', function () {
   it('should render without crashing', function () {
@@ -12,20 +11,6 @@ describe('Component > InteractionLayerContainer', function () {
     it('should render an InteractionLayer', function () {
       const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeDrawingTask />)
       expect(wrapper.find('InteractionLayer')).to.have.lengthOf(1)
-    })
-  })
-
-  describe('with active workflow step including a drawing task', function () {
-    it('should render a DrawingContainer', function () {
-      const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeStepTasks={activeStepTasksWithDrawing} />)
-      expect(wrapper.find(DrawingContainer)).to.have.lengthOf(1)
-    })
-  })
-
-  describe('with active workflow step excluding a drawing task', function () {
-    it('should not render a DrawingContainer', function () {
-      const wrapper = shallow(<InteractionLayerContainer.wrappedComponent activeStepTasks={activeStepTasksWithSingleChoice} />)
-      expect(wrapper.find(DrawingContainer)).to.have.lengthOf(0)
     })
   })
 })
