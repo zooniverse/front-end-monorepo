4b17c1ff5f4f789aed0015d4e30d2bb1f84f1312
Roger Hutchings <roger@hutchin.gs>
2019-12-03 11:53:39 +0000

    add tests

--- packages/lib-react-components/src/PrimaryButton/PrimaryButton.spec.js
+++ packages/lib-react-components/src/PrimaryButton/PrimaryButton.spec.js
@@ -1,13 +1,33 @@
-import { shallow } from 'enzyme'
+import { shallow, render } from 'enzyme'
+import { Grommet } from 'grommet'
 import React from 'react'
 
 import PrimaryButton from './PrimaryButton'
 
 const LABEL = 'Foobar'
 
-describe('Component > PrimaryButton', function () {
+describe.only('Component > PrimaryButton', function () {
   it('should render without crashing', function () {
-    const wrapper = shallow(<PrimaryButton label={LABEL} theme={THEME} />)
+    const wrapper = shallow(<PrimaryButton label={LABEL} />)
     expect(wrapper).to.be.ok()
+  })
+
+  it('should render the label', function () {
+    const wrapper = render(
+      <Grommet>
+        <PrimaryButton label={LABEL} />
+      </Grommet>
+    )
+    expect(wrapper.text()).to.equal(LABEL)
+  })
+
+  it('should pass down extra props', function () {
+    const wrapper = render(
+      <Grommet>
+        <PrimaryButton label={LABEL} disabled />
+      </Grommet>
+    )
+    const buttonAttributes = wrapper.find('button').get(0).attribs
+    expect(Object.keys(buttonAttributes)).to.contain('disabled')
   })
 })
