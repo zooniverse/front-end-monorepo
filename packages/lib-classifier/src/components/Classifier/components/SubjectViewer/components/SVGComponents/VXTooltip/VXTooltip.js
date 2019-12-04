7d599d21d88f08659830de126b84d18c52cfbd1f
Sarah Allen <sarah@zooniverse.org>
2019-12-04 14:40:21 -0600

    Add vx-tooltip

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/VXTooltip/VXTooltip.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withTooltip, TooltipWithBounds } from '@vx/tooltip'
+
+export default function VXTooltip ({ label, left, top }) {
+  return (
+    <TooltipWithBounds
+      left={left}
+      top={top}
+    >
+      {label}
+    </TooltipWithBounds>
+  )
+}
+
+VXTooltip.propTypes = {
+  label: PropTypes.string,
+  left: PropTypes.number,
+  top: PropTypes.number.number
+}
