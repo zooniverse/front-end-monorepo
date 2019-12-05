065324165c7b32042d0875e2fe242f4eef143b80
Jim O'Donnell <jim@zooniverse.org>
2019-12-05 15:21:48 +0000

    Drawing: disable mark creation (#1356)
    * Drawing: disable mark creation
    Disable pointerDown events when the interaction layer is disabled, but allow other pointer events to be handled.
    
    * Update interaction layer tests
    Set up a new drawing task before each test.
    Add required height and width to the test component.
    
    * Test disabled behaviour
    Expect mark creation to be disabled when the interaction layer is disabled.
    Expect initial drag to work even if the layer is disabled.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -6,7 +6,6 @@ import DrawingToolMarks from './components/DrawingToolMarks'
 
 const StyledRect = styled('rect')`
   cursor: ${props => props.disabled ? 'not-allowed' : 'crosshair'};
-  pointer-events: ${props => props.disabled ? 'none' : 'all'};
 `
 
 function InteractionLayer ({ activeDrawingTask, activeTool, disabled, height, scale, svg, width }) {
@@ -37,6 +36,9 @@ function InteractionLayer ({ activeDrawingTask, activeTool, disabled, height, sc
   }
 
   function onPointerDown (event) {
+    if (disabled) {
+      return false
+    }
     const activeMark = activeTool.createMark({
       id: cuid(),
       toolIndex: activeDrawingTask.activeToolIndex
