e9aa1a028beb440ffb19aa50d79259daa7e43179
Sarah Allen <sarah@zooniverse.org>
2019-12-05 15:34:33 -0600

    Styled tooltip

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/VXTooltip/VXTooltip.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/VXTooltip/VXTooltip.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withTooltip, TooltipWithBounds } from '@vx/tooltip'
+import { Tooltip } from '@vx/tooltip'
+import { Box } from 'grommet'
+import { SpacedText } from '@zooniverse/react-components'
+import Triangle from '../../../../Banners/components/Banner/components/Triangle'
+import zooTheme from '@zooniverse/grommet-theme'
 
-export default function VXTooltip ({ label, left, top }) {
+function getNumberValue (string) {
+  const splitString = string.split('px')
+  return +splitString[0]
+}
+
+export default function VXTooltip ({ backgroundColor, label, left, top }) {
+  // vx's Tooltip isn't easy to style. Have to pass in styles directly.
+  const fontSize = zooTheme.text.small.size
+  const padding = zooTheme.global.edgeSize.xsmall
+
+  const triangleHeight = 10
+  const triangleWidth = 20
+  const topPosition = top - getNumberValue(fontSize) - (getNumberValue(padding) * 2) - (triangleHeight * 2)
   return (
-    <TooltipWithBounds
+    <Tooltip
       left={left}
-      top={top}
+      top={topPosition}
+      style={{ background: 'transparent', borderRadius: 'none', boxShadow: 'none', padding: 0 }}
     >
-      {label}
-    </TooltipWithBounds>
+      <Box
+        background={{ color: backgroundColor, dark: true }}
+        elevation='medium'
+        pad={{ horizontal: 'medium', vertical: 'xsmall' }}
+      >
+        <SpacedText weight='bold'>{label}</SpacedText>
+      </Box>
+      <Triangle
+        backgroundColor='black'
+        height={triangleHeight}
+        justify='center'
+        pointDirection='down'
+        width={triangleWidth}
+      />
+    </Tooltip>
   )
+}
+
+VXTooltip.defaultProps = {
+  backgroundColor: 'rgb(0,0,0,0.5)'
 }
 
 VXTooltip.propTypes = {
