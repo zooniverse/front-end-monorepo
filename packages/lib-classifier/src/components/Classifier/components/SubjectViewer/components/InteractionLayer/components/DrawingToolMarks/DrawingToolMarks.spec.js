33dd339bcc0f34738183abb7b957904a635d8972
Jim O'Donnell <jim@zooniverse.org>
2019-12-13 13:01:26 +0000

    Add tests for bounds checking Add mock SVG and mark bounds to the tests. Test whether the mark is deleted when inside, overlapping or completely outside the SVG element.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/DrawingToolMarks/DrawingToolMarks.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/DrawingToolMarks/DrawingToolMarks.spec.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 import { PointTool, LineTool } from '@plugins/drawingTools/models/tools'
 import { Point, Line } from '@plugins/drawingTools/models/marks'
+import { DrawingToolRoot } from '@plugins/drawingTools/components'
 import DrawingToolMarks from './DrawingToolMarks'
 
 describe('Components > DrawingToolMarks', function () {
@@ -10,6 +12,8 @@ describe('Components > DrawingToolMarks', function () {
 
   beforeEach(function () {
     svg = document.createElement('svg')
+    const svgBounds = { left: 0, top: 0, right: 2000, bottom: 1000, width: 2000, height: 1000 }
+    sinon.stub(svg, 'getBoundingClientRect').callsFake(() => svgBounds )
     const lineTool = LineTool.create({
       help: '',
       label: 'Draw a line',
@@ -43,6 +47,77 @@ describe('Components > DrawingToolMarks', function () {
       const line = tool.marks.get('line1')
       const deleteButton = wrapper.find('DeleteButton')
       expect(deleteButton.prop('mark')).to.equal(line)
+    })
+  })
+
+  describe('when a mark is moved to a new position', function () {
+    let dragEnd
+    let onDeselectMark
+    let onDelete
+    let deleteMark
+
+    before(function () {
+      deleteMark = sinon.spy(tool, 'deleteMark')
+      onDeselectMark = sinon.stub()
+      onDelete = sinon.stub()
+      const wrapper = shallow(
+        <DrawingToolMarks
+          onDelete={onDelete}
+          onDeselectMark={onDeselectMark}
+          tool={tool}
+          svg={svg}
+        />
+      )
+      dragEnd = wrapper.find(DrawingToolRoot).first().prop('dragEnd')
+    })
+
+    it('should deselect the mark', function () {
+      const mockBounds = { left: 0, top: 0, right: 100, bottom: 100, width: 100, height: 100 }
+      const currentTarget = {
+        getBoundingClientRect: () => mockBounds
+      }
+      const fakeEvent = { currentTarget }
+      dragEnd(fakeEvent)
+      expect(onDeselectMark).to.have.been.calledOnce()
+    })
+
+    describe('when the mark is inside the SVG element', function () {
+      it('should do nothing', function () {
+        const mockBounds = { left: 0, top: 0, right: 100, bottom: 100, width: 100, height: 100 }
+        const currentTarget = {
+          getBoundingClientRect: () => mockBounds
+        }
+        const fakeEvent = { currentTarget }
+        dragEnd(fakeEvent)
+        expect(tool.marks.size).to.equal(2)
+        expect(onDelete).to.not.have.been.called()
+      })
+    })
+
+    describe('when the mark overlaps the SVG element', function () {
+      it('should do nothing', function () {
+        const mockBounds = { left: 1990, top: 20, right: 2090, bottom: 120, width: 100, height: 100 }
+        const currentTarget = {
+          getBoundingClientRect: () => mockBounds
+        }
+        const fakeEvent = { currentTarget }
+        dragEnd(fakeEvent)
+        expect(tool.marks.size).to.equal(2)
+        expect(onDelete).to.not.have.been.called()
+      })
+    })
+
+    describe('when the mark is outside the SVG element', function () {
+      it('should delete the mark', function () {
+        const mockBounds = { left: 2090, top: 20, right: 2190, bottom: 120, width: 100, height: 100 }
+        const currentTarget = {
+          getBoundingClientRect: () => mockBounds
+        }
+        const fakeEvent = { currentTarget }
+        dragEnd(fakeEvent)
+        expect(onDelete).to.have.been.calledOnce()
+        expect(deleteMark).to.have.been.calledOnce()
+      })
     })
   })
 })
