ff1a0faa9465a150b688a6e36bf5aa4b40b29f7a
simensta <andrea@zooniverse.org>
2019-12-16 20:25:07 +0100

    Add proptypes to multiFrame viewer

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/MultiFrameViewer/MultiFrameViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/MultiFrameViewer/MultiFrameViewer.js
@@ -1,19 +1,39 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
-
+import locationValidator from '../../helpers/locationValidator'
 
 class MultiFrameViewer extends React.Component {
   constructor () {
     super()
+    // current frame
   }
 
   render () {
+    // carousel component
     return (
       <div>
         <p>MultiFrameViewer Placeholder</p>
       </div>
     )
+  }
+}
+
+MultiFrameViewer.propTypes = {
+  loadingState: PropTypes.string,
+  onError: PropTypes.func,
+  onReady: PropTypes.func,
+  subject: PropTypes.shape({
+    locations: PropTypes.arrayOf(locationValidator),
+    metadata: PropTypes.shape({
+      default_frame: PropTypes.number
+    })
+  })
+}
+
+MultiFrameViewer.defaultProps = {
+  subject: {
+    default_frame: 0
   }
 }
 
