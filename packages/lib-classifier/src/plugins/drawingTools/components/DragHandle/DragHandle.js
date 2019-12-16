a4062b05341f7f2a7e16c94d8a18a862d4759dac
Jim O'Donnell <jim@zooniverse.org>
2019-12-16 10:04:21 +0000

    Add drag handle tests Add tests for drag handle rendering with the supplied position and scale, and the optional styling props.

--- packages/lib-classifier/src/plugins/drawingTools/components/DragHandle/DragHandle.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/DragHandle/DragHandle.js
@@ -1,4 +1,5 @@
 import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import draggable from '../draggable'
 
@@ -11,24 +12,36 @@ const RADIUS = screen.width > 900 ? 4 : 10
 
 const DragHandle = forwardRef(function DragHandle({ fill, radius, scale, x, y }, ref) {
 
+  const transform = `translate(${x}, ${y}) scale(${1 / scale})`
   const styleProps = {
-    fill: fill || 'currentColor',
+    fill,
     stroke: 'currentColor',
-    strokeWidth: 1,
-    transform: `\
-translate(${x}, ${y})
-scale(${1 / scale})\
-`
+    strokeWidth: 1
   }
-  const r = radius || RADIUS
+
   return (
     <g
       ref={ref}
+      transform={transform}
     >
-      <StyledCircle r={r} {...styleProps} />
-      <StyledCircle r={2 * r} fill='transparent' stroke='transparent' />
+      <StyledCircle r={radius} {...styleProps} />
+      <StyledCircle r={2 * radius} fill='transparent' stroke='transparent' />
     </g>
   )
 })
+
+DragHandle.propTypes = {
+  fill: PropTypes.string,
+  radius: PropTypes.number,
+  scale: PropTypes.number,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired
+}
+
+DragHandle.defaultProps = {
+  fill: 'currentColor',
+  radius: RADIUS,
+  scale: 1
+}
 
 export default draggable(DragHandle)
