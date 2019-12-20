266a294dc971e5644593d97496e3dd3923c9bb78
Jim O'Donnell <jim@zooniverse.org>
2019-12-20 11:25:28 +0000

    Add stories Add stories for completed, active and unfinished lines.

---
+++ packages/lib-classifier/src/plugins/drawingTools/experimental/components/TranscriptionLine/TranscriptionLine.stories.js
@@ -0,0 +1,96 @@
+import { withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
+import zooTheme from '@zooniverse/grommet-theme'
+import React, { Component, createRef, forwardRef } from 'react'
+import sinon from 'sinon'
+import { TranscriptionLineTool } from '@plugins/drawingTools/models/tools'
+import { DrawingToolRoot } from '@plugins/drawingTools/components'
+import TranscriptionLine from './TranscriptionLine'
+
+
+class DrawingStory extends Component {
+  constructor () {
+    super()
+    this.svg = React.createRef()
+  }
+
+  render () {
+    const { children, mark, tool } = this.props
+    const { svg } = this
+    return (
+      <svg viewBox='0 0 300 400' height={300} width={400} ref={this.svg}>
+        <DrawingToolRoot
+          label='Transcription line'
+          mark={mark}
+          svg={svg}
+          tool={tool}
+        >
+          {React.cloneElement(children, { svg: svg.current })}
+        </DrawingToolRoot>
+      </svg>
+    )
+  }
+}
+storiesOf('Drawing tools | Transcription Line', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    viewport: {
+      defaultViewport: 'responsive'
+    }
+  })
+  .add('complete', function () {
+    const tool = TranscriptionLineTool.create({
+      color: 'red',
+      type: 'polygon'
+    })
+    const mark = tool.createMark({
+      id: 'transcriptionLine1',
+      x1: 10,
+      y1: 20,
+      x2: 205,
+      y2: 15
+    })
+    mark.finish()
+    return (
+      <DrawingStory mark={mark} tool={tool}>
+        <TranscriptionLine mark={mark} />
+      </DrawingStory>
+    )
+  })
+  .add('active', function () {
+    const tool = TranscriptionLineTool.create({
+      color: 'red',
+      type: 'polygon'
+    })
+    const mark = tool.createMark({
+      id: 'transcriptionLine1',
+      x1: 10,
+      y1: 20,
+      x2: 205,
+      y2: 15
+    })
+    mark.finish()
+    return (
+      <DrawingStory mark={mark} tool={tool}>
+        <TranscriptionLine active mark={mark} />
+      </DrawingStory>
+    )
+  })
+  .add('unfinished', function () {
+    const tool = TranscriptionLineTool.create({
+      color: 'red',
+      type: 'polygon'
+    })
+    const mark = tool.createMark({
+      id: 'transcriptionLine1',
+      x1: 10,
+      y1: 20,
+      x2: 205,
+      y2: 15
+    })
+    return (
+      <DrawingStory mark={mark} tool={tool}>
+        <TranscriptionLine active mark={mark} />
+      </DrawingStory>
+    )
+  })
