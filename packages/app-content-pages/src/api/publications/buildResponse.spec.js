ac3d9618bfebfe63ccfbd0c0671a524656513239
Sarah Allen <sarah@zooniverse.org>
2020-01-14 14:23:08 -0600

    Refactor the publication response sorts

--- packages/app-content-pages/src/api/publications/buildResponse.spec.js
+++ packages/app-content-pages/src/api/publications/buildResponse.spec.js
@@ -1,7 +1,6 @@
 import buildResponse from './buildResponse'
 import publications from './mocks/contentfulResponse.mock'
 import projectAvatarsMap from './mocks/projectAvatarsMap.mock'
-import mockResponse from './mocks/response.mock.json'
 
 describe('API > Publications > buildResponse', function () {
   it('should exist', function () {
@@ -10,6 +9,37 @@ describe('API > Publications > buildResponse', function () {
 
   it('should construct a response object from the list of publications and avatars', function () {
     const response = buildResponse(publications, projectAvatarsMap)
-    expect(response).to.deep.equal(mockResponse)
+    expect(response).to.be.an('array')
+  })
+  
+  it('should sort the response by category weight', function () {
+    const response = buildResponse(publications, projectAvatarsMap)
+
+    response.forEach((category, index) => {
+      if (index === response.length - 1) {
+        expect(category.weight).to.be.above(response[index - 1].weight)
+      } else {
+        expect(category.weight).to.be.below(response[index + 1].weight)
+      }
+    })
+  })
+
+  it('should sort each category\'s projects alphabetically', function () {
+    const response = buildResponse(publications, projectAvatarsMap)
+
+    response.forEach((category) => {
+      const { projects } = category
+      projects.forEach((project, index) => {
+        const projectATitle = project.title.toLowerCase()
+        const projectBTitle = (projects[index + 1]) ? projects[index + 1].title.toLowerCase() : ''
+        const comparison = projectATitle.localeCompare(projectBTitle, { ignorePunctuation: true })
+
+        if (projectBTitle) {
+          expect(comparison).to.be.below(0)
+        } else {
+          expect(comparison).to.be.above(0)
+        }
+      })
+    })
   })
 })
