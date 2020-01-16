083facdb204501534669e3febad30d3c46d51491
Jim O'Donnell <jim@zooniverse.org>
2020-01-16 17:19:39 +0000

    Add Line and Point stories (#1413)
    * Add Line and Point stories
    Add stories for the Line and Point drawing marks. Each mark has an active and complete (deselected) state.
    
    * Update Line props
    Add missing types and defaults.
    
    Co-authored-by: Mark Bouslog <mark@zooniverse.org>

---
+++ packages/lib-classifier/src/plugins/drawingTools/components/Point/Point.stories.js
@@ -0,0 +1,66 @@
+import { withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
+import zooTheme from '@zooniverse/grommet-theme'
+import React, { Component, createRef, forwardRef } from 'react'
+import sinon from 'sinon'
+import { PointTool } from '@plugins/drawingTools/models/tools'
+import { DrawingToolRoot }from '@plugins/drawingTools/components'
+import Point from './Point'
+
+class DrawingStory extends Component {
+  render () {
+    const { children, mark } = this.props
+
+    return (
+      <svg viewBox='0 0 300 400' height={300} width={400}>
+        <DrawingToolRoot
+          label='Point'
+          mark={mark}
+        >
+          {children}
+        </DrawingToolRoot>
+      </svg>
+    )
+  }
+}
+storiesOf('Drawing tools | Point', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    viewport: {
+      defaultViewport: 'responsive'
+    }
+  })
+  .add('complete', function () {
+    const tool = PointTool.create({
+      color: 'red',
+      type: 'point'
+    })
+    const mark = tool.createMark({
+      id: 'point1',
+      toolType: 'point',
+      x: 100,
+      y: 120
+    })
+    return (
+      <DrawingStory mark={mark}>
+        <Point mark={mark} />
+      </DrawingStory>
+    )
+  })
+  .add('active', function () {
+    const tool = PointTool.create({
+      color: 'red',
+      type: 'point'
+    })
+    const mark = tool.createMark({
+      id: 'point1',
+      toolType: 'point',      
+      x: 100,
+      y: 120
+    })
+    return (
+      <DrawingStory mark={mark}>
+        <Point active mark={mark} />
+      </DrawingStory>
+    )
+  })
