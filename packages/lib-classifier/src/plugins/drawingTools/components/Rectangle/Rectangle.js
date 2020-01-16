f74688302dba510a9ec0102f8587fdcb26231481
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-01-16 17:26:20 +0000

    WIP add <rect>

--- packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.js
@@ -11,10 +11,21 @@ function Rectangle ({ active, children, mark, scale }) {
     mark.setCoordinates(coords)
   }
 
+  const x_center = (x1 + x2) / 2
+  const y_center = (y1 + y2) / 2
+  
+  const x_left = Math.min(x1, x2)
+  const x_right = Math.max(x1, x2)
+  const y_top = Math.min(y1, y2)
+  const y_bottom = Math.max(y1, y2)
+  
+  const width = Math.abs(x2 - x1)
+  const height = Math.abs(y2 - y1)
+  
   return (
     <g>
-      <line x1={x1} y1={y1} x2={x2} y2={y2} />
-      <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={GRAB_STROKE_WIDTH / scale} strokeOpacity='0' />
+      <rect x={x_left} y={y_top} width={width} height={height} />
+      <rect x={x_left} y={y_top} width={width} height={height} strokeWidth={GRAB_STROKE_WIDTH / scale} strokeOpacity='0' />
       {active &&
         <DragHandle scale={scale} x={x1} y={y1} dragMove={(e, d) => onHandleDrag({ x1: x1 + d.x, y1: y1 + d.y, x2, y2 })} />}
       {active &&
@@ -33,3 +44,5 @@ Rectangle.defaultProps = {
 }
 
 export default Rectangle
+
+
