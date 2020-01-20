2f4ac51814e85f7ddda266bf526606259e5168da
Roger Hutchings <roger@hutchin.gs>
2020-01-20 10:22:25 +0000

    Fix typo

--- packages/lib-auth/test/unit/helpers/create-immutable.spec.js
+++ packages/lib-auth/test/unit/helpers/create-immutable.spec.js
@@ -1,7 +1,7 @@
 const createImmutable = require('../../../src/helpers/create-immutable')
 
 describe('Unit Tests > helpers > createImmutable', function () {
-  it('should set an property on a given object that can\'t be changed', function () {
+  it('should set a property on a given object that can\'t be changed', function () {
     const object = {}
     const key = 'foo'
     const value = 'bar'
