5be4b857542cb2fd7fe695df0304a4cceda51a39
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-01-21 18:35:41 +0000

    Add RectangleTool tests

--- packages/lib-classifier/src/plugins/drawingTools/models/tools/RectangleTool/RectangleTool.spec.js
+++ packages/lib-classifier/src/plugins/drawingTools/models/tools/RectangleTool/RectangleTool.spec.js
@@ -1,4 +1,26 @@
 import RectangleTool from './RectangleTool'
 
+const rectangle = {
+  color: '#ff0000',
+  label: 'A Square With Unequal Sides',
+  max: 2,
+  min: 1,
+  type: 'rectangle'
+}
+
 describe('Model > DrawingTools > Rectangle', function () {
+  it('should exist', function () {
+    const rectangleToolInstance = RectangleTool.create(rectangle)
+    expect(rectangleToolInstance).to.exist()
+    expect(rectangleToolInstance).to.be.an('object')
+  })
+
+  it('should have a property `type` of `rectangle`', function () {
+    const rectangleToolInstance = RectangleTool.create(rectangle)
+    expect(rectangleToolInstance).to.deep.include({ type: 'rectangle' })
+  })
+
+  it('should throw an error with incorrect property `type`', function () {
+    expect(() => RectangleTool.create({ type: 'wakka wakka wakka' })).to.throw()
+  })
 })
