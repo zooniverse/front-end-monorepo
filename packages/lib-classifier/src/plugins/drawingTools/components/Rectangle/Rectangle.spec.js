eda8895bbe06fe5bbd6f62c1dab40a425edb123b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-01-21 18:29:32 +0000

    Add tests for Rectangle component

--- packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.spec.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.spec.js
@@ -4,4 +4,18 @@ import { expect } from 'chai'
 import Rectangle from './Rectangle'
 
 describe('Rectangle tool', function () {
+  it('should render without crashing', function () {
+    const wrapper = shallow(<Rectangle
+      mark={{ x_center: 100, y_center: 200, width: 30, height: 40 }}
+    />)
+    expect(wrapper).to.be.ok()
+  })
+
+  it('should render a line with the coordinates provided', function () {
+    const wrapper = shallow(
+      <Rectangle
+        mark={{ x_center: 100, y_center: 200, width: 30, height: 40 }}
+      />)
+    expect(wrapper.containsMatchingElement(<rect x={85} y={180} width={30} height={40} />)).to.be.true()
+  })
 })
