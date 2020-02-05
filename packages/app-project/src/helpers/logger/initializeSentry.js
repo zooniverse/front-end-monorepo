91fed9ccaf36c02d73ca94f3c7d70dc5416af5c6
Jim O'Donnell <jim@zooniverse.org>
2020-02-05 14:12:47 +0000

    Log Sentry status during builds (#1483)
    Log Sentry initialisation so that we can check its status in build logs and in the browser dev console.

--- packages/app-project/src/helpers/logger/initializeSentry.js
+++ packages/app-project/src/helpers/logger/initializeSentry.js
@@ -2,8 +2,10 @@ import * as Sentry from '@sentry/browser'
 
 export default function initializeSentry () {
   const dsn = process.env.SENTRY_DSN
+  console.log('Sentry DSN', dsn)
 
   if (dsn) {
+    console.log('initialising Sentry logger')
     Sentry.init({ dsn })
   }
 
