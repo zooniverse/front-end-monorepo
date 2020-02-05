480d9005dc49d8adfa6aa9876f40a581af0253b1
Campbell Allen <campbell.allen@gmail.com>
2020-02-05 14:04:11 +0000

    cleanup console.log for sentry debugging

--- packages/app-project/src/helpers/logger/initializeSentry.js
+++ packages/app-project/src/helpers/logger/initializeSentry.js
@@ -2,10 +2,8 @@ import * as Sentry from '@sentry/browser'
 
 export default function initializeSentry () {
   const dsn = process.env.SENTRY_DSN
-  console.log('Sentry DSN', dsn)
 
   if (dsn) {
-    console.log('initialising Sentry logger')
     Sentry.init({ dsn })
   }
 
