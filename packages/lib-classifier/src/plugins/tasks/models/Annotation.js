f8dd0a4b6a5cfc5101e0abec5d8daf66af5e9af9
Jim O'Donnell <jim@zooniverse.org>
2020-02-06 11:53:38 +0000

    Strip annotation IDs

--- packages/lib-classifier/src/plugins/tasks/models/Annotation.js
+++ packages/lib-classifier/src/plugins/tasks/models/Annotation.js
@@ -1,3 +1,4 @@
+import cuid from 'cuid'
 import { getSnapshot, types } from 'mobx-state-tree'
 
 const Annotation = types.model('Annotation', {
@@ -5,15 +6,25 @@ const Annotation = types.model('Annotation', {
   task: types.string,
   taskType: types.string
 })
+  .preProcessSnapshot(snapshot => {
+    const newSnapshot = Object.assign({}, snapshot)
+    // generate annotation IDs, if not present
+    newSnapshot.id = snapshot.id || cuid()
+    return newSnapshot
+  })
+  .postProcessSnapshot(snapshot => {
+    const newSnapshot = Object.assign({}, snapshot)
+    // remove annotation IDs
+    delete newSnapshot.id
+    return newSnapshot
+  })
   .views(self => ({
     get isComplete () {
       return true
     },
 
     get toSnapshot () {
-      const snapshot = getSnapshot(self)
-      const { id, ...filteredSnapshot } = snapshot
-      return filteredSnapshot
+      return getSnapshot(self)
     }
   }))
   .actions(self => ({
