2f1ce05774efe53c69c38545a2f64a4edd4f67d3
Campbell Allen <campbell.allen@gmail.com>
2020-02-11 12:59:08 +0000

    remove console log statements

--- packages/app-project/src/helpers/logger/initializeSentry.js
+++ packages/app-project/src/helpers/logger/initializeSentry.js
@@ -3,11 +3,8 @@ import * as Sentry from '@sentry/browser'
 export default function initializeSentry () {
   const dsn = process.env.SENTRY_DSN
   const appEnv = process.env.APP_ENV
-  console.log('Sentry DSN', dsn)
-  console.log('APP ENV', appEnv)
 
   if (dsn) {
-    console.log('initialising Sentry logger')
     Sentry.init({
       dsn,
       environment: appEnv,
