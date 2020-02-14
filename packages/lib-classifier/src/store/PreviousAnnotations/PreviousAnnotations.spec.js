92ccb825507e26927255a555c30930e889669271
Jim O'Donnell <jim@zooniverse.org>
2020-02-14 12:09:38 +0000

    Add basic tests for reductions Add some tests to check properties of the reduced annotations for subjects with and without Caesar reductions.

--- packages/lib-classifier/src/store/PreviousAnnotations/PreviousAnnotations.spec.js
+++ packages/lib-classifier/src/store/PreviousAnnotations/PreviousAnnotations.spec.js
@@ -22,8 +22,56 @@ describe.only('Models > PreviousAnnotations', function () {
     expect(previousAnnotations).to.be.ok()
   })
 
-  it('should update on frame change', function () {
-    previousAnnotations.changeFrame(2)
-    expect(previousAnnotations).to.be.ok()
+  describe('with previous annotations', function () {
+    before(async function () {
+      previousAnnotations = PreviousAnnotations.create({
+        caesarReducerKey: 'ext',
+        subjectId: '13971150',
+        workflowId: '5339'
+      })
+      await previousAnnotations.fetchCaesarReductions()
+    })
+
+    it('should default to frame 1', function () {
+      previousAnnotations.annotations.forEach(function (annotation) {
+        expect(annotation.frame).to.equal(1)
+      })
+    })
+
+    it('should have points', function () {
+      previousAnnotations.annotations.forEach(function (annotation) {
+        expect(annotation.points).to.be.a('array')
+        expect(annotation.points).not.to.be.empty
+      })
+    })
+
+    it('should have text options', function () {
+      previousAnnotations.annotations.forEach(function (annotation) {
+        expect(annotation.textOptions).to.be.a('array')
+        expect(annotation.textOptions).not.to.be.empty
+      })
+    })
+
+    it('should update on frame change', function () {
+      previousAnnotations.changeFrame(2)
+      previousAnnotations.annotations.forEach(function (annotation) {
+        expect(annotation.frame).to.equal(2)
+      })
+    })
+  })
+
+  describe('without previous annotations', function () {
+    before(async function () {
+      previousAnnotations = PreviousAnnotations.create({
+        caesarReducerKey: 'ext',
+        subjectId: '13971170',
+        workflowId: '5339'
+      })
+      await previousAnnotations.fetchCaesarReductions()
+    })
+
+    it('should not have any annotations', function () {
+      expect(previousAnnotations.annotations).to.be.empty()
+    })
   })
 })
