0064484fef120c09b51f587759ab76d48222ac40
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-02-20 18:21:15 +0000

    Add basic Point test inside SubTaskPopup test

--- packages/lib-classifier/src/components/Classifier/components/SubTaskPopup/SubTaskPopup.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubTaskPopup/SubTaskPopup.spec.js
@@ -4,13 +4,36 @@ import { shallow } from 'enzyme'
 import { expect } from 'chai'
 
 import { SubTaskPopup } from './SubTaskPopup'
-import asyncStates from '@zooniverse/async-states'
-import SingleChoiceTask from '@plugins/tasks/SingleChoiceTask'
-import ClassificationStore from '@store/ClassificationStore'
+import { PointTool } from '@plugins/drawingTools/models/tools'
 
 describe.only('SubTaskPopup', function () {
+  const tool = PointTool.create({
+    color: 'red',
+    type: 'point',
+    details: [
+      {
+        type: 'text',
+        required: false,
+        instructions: 'Type in any text',
+      }
+    ]
+  })
+  
+  const mark = tool.createMark({
+    id: 'point1',
+    toolType: 'point',
+    x: 100,
+    y: 160
+  })
+  
+  
   it('should render without crashing', function () {
     const wrapper = shallow(<SubTaskPopup />)
+    expect(wrapper).to.be.ok()
+  })
+  
+  it('should render without crashing when given an active mark', function () {
+    const wrapper = shallow(<SubTaskPopup activeMark={mark} />)
     expect(wrapper).to.be.ok()
   })
 })
