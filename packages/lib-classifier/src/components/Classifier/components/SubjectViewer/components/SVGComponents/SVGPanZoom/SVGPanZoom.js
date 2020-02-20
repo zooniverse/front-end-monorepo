9df22c02d69eaa0b067bc3b74bfbdbf166562c75
Jim O'Donnell <jim@zooniverse.org>
2020-02-20 22:19:25 +0000

    Single Image viewer: add SVGPanZoom component (#1493)
    * Create SVGPanZoom component
    Add an SVGPanZoom component, which handles pan and zoom by controlling the viewbox of its immediate children. Move pan and zoom tests out of the SingleImageViewer.
    
    * support wheel scroll
    Wrap the component in a div with an onWheel handler.
    
    * add drag support
    
    * Add subjectScale
    Add the rendered subject image scale (clientWidth / naturalWidth) and recalculate it on state change.
    
    * Use functional state changes
    Calculate new state from previous state inside state changes. Add an effect to handle component mount/mount. Add an effect that recalculates the viewbox on scale changes.
    Use mount rather than shallow in tests since shallow rendering does not support useEffect.
    
    * Prevent scrolling on wheel events
    Prevent default on wheel events to prevent window scrolling when zooming. Note that React's onWheel event is passive and so cannot preventDefault().
    
    * Add maxZoom, minZoom props
    Allow max/min zoom level to be set from props. Defaults to minimum zoom = actual size and maximum zoom = double size.
    
    * Rename variables for clarity
    Change scale to zoom, for zoom level.
    Change subjectScale to scale, for image rendered size / natural size.
    
    * Update image scale on viewbox change
    
    * Add the img prop to tests
    
    * Derive the viewbox prop from zoom state
    Derive the child props from zoom state, rather than chaining useState calls to re-render on zoom then again on viewBox change.
    
    * Further simplify state
    Remove scale state. Remove viewbox and img effects. Calculate image scale on each render.
    
    * Move rotation to its own element
    Move rotation transforms to a <g> element. Update SVGContext to account for the new transformation matrix.
    
    Co-authored-by: Mark Bouslog <mark@zooniverse.org>

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/SVGPanZoom/SVGPanZoom.js
@@ -0,0 +1,140 @@
+import PropTypes from 'prop-types'
+import React, { cloneElement, createRef, useEffect, useState } from 'react'
+import SVGContext from '@plugins/drawingTools/shared/SVGContext'
+
+function SVGPanZoom ({
+  children,
+  img,
+  maxZoom,
+  minZoom,
+  naturalHeight,
+  naturalWidth,
+  setOnDrag,
+  setOnPan,
+  setOnZoom
+}) {
+  const scrollContainer = createRef()
+  const defaultViewBox = {
+    x: 0,
+    y: 0,
+    height: naturalHeight,
+    width: naturalWidth
+  }
+
+  const [ zoom, setZoom ] = useState(1)
+  const [ viewBox, setViewBox ] = useState(defaultViewBox)
+
+  function onMount () {
+    setOnDrag(onDrag)
+    setOnPan(onPan)
+    setOnZoom(onZoom)
+    scrollContainer.current.addEventListener('wheel', e => e.preventDefault())
+  }
+
+  function onUnmount () {
+    setOnDrag(() => true)
+    setOnPan(() => true)
+    setOnZoom(() => true)
+    scrollContainer.current.removeEventListener('wheel', e => e.preventDefault())
+  }
+
+  useEffect(() => {
+    onMount()
+    return onUnmount
+  }, [])
+
+  useEffect(function onZoomChange () {
+    const newViewBox = scaledViewBox(zoom)
+    setViewBox(newViewBox)
+  }, [zoom])
+
+  function imageScale (img) {
+    const { width: clientWidth, height: clientHeight } = img ? img.getBoundingClientRect() : {}
+    const scale = clientWidth / naturalWidth
+    return scale
+  }
+
+  function scaledViewBox (scale) {
+    const viewBoxScale = 1 / scale
+    const xCentre = viewBox.x + viewBox.width / 2
+    const yCentre = viewBox.y + viewBox.height / 2
+    const width = parseInt(naturalWidth * viewBoxScale, 10)
+    const height = parseInt(naturalHeight * viewBoxScale, 10)
+    const x = xCentre - width / 2
+    const y = yCentre - height / 2
+    return { x, y, width, height }
+  }
+
+  function onDrag (event, difference) {
+    setViewBox(prevViewBox => {
+      const newViewBox = Object.assign({}, prevViewBox)
+      newViewBox.x -= difference.x / 1.5
+      newViewBox.y -= difference.y / 1.5
+      return newViewBox
+    })
+  }
+
+  function onPan (dx, dy) {
+    setViewBox(prevViewBox => {
+      const newViewBox = Object.assign({}, prevViewBox)
+      newViewBox.x -= dx * 10
+      newViewBox.y += dy * 10
+      return newViewBox
+    })
+  }
+
+  function onZoom (type) {
+    switch (type) {
+      case 'zoomin': {
+        setZoom(prevZoom => Math.min(prevZoom + 0.1, maxZoom))
+        return
+      }
+      case 'zoomout': {
+        setZoom(prevZoom => Math.max(prevZoom - 0.1, minZoom))
+        return
+      }
+      case 'zoomto': {
+        setZoom(1)
+        setViewBox({
+          x: 0,
+          y: 0,
+          width: naturalWidth,
+          height: naturalHeight
+        })
+      }
+    }
+  }
+
+  function onWheel (event) {
+    const { deltaY } = event
+    if (deltaY < 0) {
+      onZoom('zoomout', -1)
+    } else {
+      onZoom('zoomin', 1)
+    }
+  }
+
+  const { x, y, width, height } = scaledViewBox(zoom)
+  const scale = imageScale(img)
+  return (
+    <div ref={scrollContainer} onWheel={onWheel}>
+      {cloneElement(children, { scale, viewBox: `${x} ${y} ${width} ${height}` })}
+    </div>
+  )
+}
+
+SVGPanZoom.propTypes = {
+  children: PropTypes.node.isRequired,
+  maxZoom: PropTypes.number,
+  minZoom: PropTypes.number,
+  naturalHeight: PropTypes.number.isRequired,
+  naturalWidth: PropTypes.number.isRequired,
+  setOnPan: PropTypes.func,
+  setOnZoom: PropTypes.func
+}
+
+SVGPanZoom.defaultProps = {
+  maxZoom: 2,
+  minZoom: 1
+}
+export default SVGPanZoom
---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/SVGPanZoom/SVGPanZoom.spec.js
@@ -0,0 +1,101 @@
+import { mount } from 'enzyme'
+import React from 'react'
+import sinon from 'sinon'
+import SingleImageViewer from '../../SingleImageViewer'
+import SVGPanZoom from './SVGPanZoom'
+
+describe('Components > SVGPanZoom', function () {
+  let wrapper
+  let onDrag
+  let onPan
+  let onZoom
+  
+  beforeEach(function () {
+    const img = {
+      getBoundingClientRect () {
+        return {
+          height: 100,
+          width: 200
+        }
+      }
+    }
+    wrapper = mount(
+      <SVGPanZoom
+        img={img}
+        naturalHeight={200}
+        naturalWidth={400}
+        setOnDrag={callback => { onDrag = callback }}
+        setOnPan={callback => { onPan = callback }}
+        setOnZoom={callback => { onZoom = callback }}
+      >
+        <svg />
+      </SVGPanZoom>
+    )
+  })
+  
+  it('should enable zoom in', function () {
+    onZoom('zoomin', 1)
+    wrapper.update()
+    const viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('18.5 9.5 363 181')
+  })
+
+  it('should enable zoom out', function () {
+    onZoom('zoomin', 1)
+    wrapper.update()
+    let viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('18.5 9.5 363 181')
+    onZoom('zoomout', -1)
+    wrapper.update()
+    viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('0 0 400 200')
+  })
+
+  it('should enable horizontal panning', function () {
+    onPan(-1, 0)
+    wrapper.update()
+    const viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('10 0 400 200')
+  })
+
+  it('should enable vertical panning', function () {
+    onPan(0, -1)
+    wrapper.update()
+    const viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('0 -10 400 200')
+  })
+
+  it('should should pan horizontally on drag', function () {
+    onDrag({}, { x: -15, y: 0 })
+    wrapper.update()
+    const viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('10 0 400 200')
+  })
+
+  it('should should pan vertically on drag', function () {
+    onDrag({}, { x: 0, y: -15 })
+    wrapper.update()
+    const viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('0 10 400 200')
+  })
+
+  it('should should zoom out on wheel scroll up', function () {
+    let fakeEvent = { deltaY: 10, preventDefault: sinon.stub(), stopPropagation: sinon.stub() }
+    wrapper.simulate('wheel', fakeEvent)
+    let viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('18.5 9.5 363 181')
+    fakeEvent = { deltaY: -10, preventDefault: sinon.stub(), stopPropagation: sinon.stub() }
+    wrapper.simulate('wheel', fakeEvent)
+    viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('0 0 400 200')
+  })
+
+  it('should should zoom in on wheel scroll down', function () {
+    let viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('0 0 400 200')
+    const fakeEvent = { deltaY: 10, preventDefault: sinon.stub(), stopPropagation: sinon.stub() }
+    wrapper.simulate('wheel', fakeEvent)
+    viewBox = wrapper.find('svg').prop('viewBox')
+    expect(viewBox).to.equal('18.5 9.5 363 181')
+  })
+})
