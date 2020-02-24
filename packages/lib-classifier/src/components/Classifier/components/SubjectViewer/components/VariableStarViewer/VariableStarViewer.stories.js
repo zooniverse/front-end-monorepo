e27d96123ddadb49b572c777ec492dc1ffd06b5e
srallen <srallen@users.noreply.github.com>
2020-02-24 14:27:15 -0600

    [VSLCV] Add in basic specs for getting JSON subject (#1527)
    * Add in basic specs for getting JSON subject
    
    * Use container component in stories. Return state objects correctly in component mount

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VariableStarViewer/VariableStarViewer.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VariableStarViewer/VariableStarViewer.stories.js
@@ -3,7 +3,8 @@ import { storiesOf } from '@storybook/react'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Box, Grommet } from 'grommet'
 import { withKnobs, boolean, text, object } from '@storybook/addon-knobs'
-import VariableStarViewer from './VariableStarViewer'
+import { Factory } from 'rosie'
+import { VariableStarViewerContainer } from './VariableStarViewerContainer'
 import ZoomInButton from '../../../ImageToolbar/components/ZoomInButton/ZoomInButton'
 import ZoomOutButton from '../../../ImageToolbar/components/ZoomOutButton/ZoomOutButton'
 import ResetButton from '../../../ImageToolbar/components/ResetButton/ResetButton'
@@ -44,16 +45,19 @@ const barJSON = {
   period: variableStarPeriodMockData
 }
 
+const subject = Factory.build('subject', {
+  locations: [
+    { 'application/json': 'https://raw.githubusercontent.com/zooniverse/front-end-monorepo/master/packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/mockLightCurves/variableStar.json' }
+  ]
+})
+
 stories
   .add('light theme', () => {
     return (
       <Grommet theme={zooTheme}>
         <Box height='500px' width='700px'>
-          <VariableStarViewer
-            barJSON={barJSON}
-            imgSrc={image}
-            phasedJSON={variableStar}
-            rawJSON={object('data', variableStar)}
+          <VariableStarViewerContainer
+            subject={subject}
           />
         </Box>
       </Grommet>
@@ -63,12 +67,9 @@ stories
     const darkZooTheme = Object.assign({}, zooTheme, { dark: true })
     return (
       <Grommet theme={darkZooTheme}>
-        <Box height='medium' width='large'>
-          <VariableStarViewer
-            barJSON={barJSON}
-            imgSrc={image}
-            phasedJSON={variableStar}
-            rawJSON={object('data', variableStar)}
+        <Box height='500px' width='large'>
+          <VariableStarViewerContainer
+            subject={subject}
           />
         </Box>
       </Grommet>
@@ -78,12 +79,9 @@ stories
     const darkZooTheme = Object.assign({}, zooTheme, { dark: true })
     return (
       <Grommet theme={darkZooTheme}>
-        <Box height='medium' width='large'>
-          <VariableStarViewer
-            barJSON={barJSON}
-            imgSrc={image}
-            phasedJSON={variableStar}
-            rawJSON={object('data', variableStar)}
+        <Box height='500px' width='large'>
+          <VariableStarViewerContainer
+            subject={subject}
           />
         </Box>
       </Grommet>
