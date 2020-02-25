71c43c54d467e529ae0562f6a938e9319f73c71b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-02-25 15:06:40 +0000

    Remove describe.only

--- packages/lib-classifier/src/components/Classifier/components/SubTaskPopup/SubTaskPopup.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubTaskPopup/SubTaskPopup.spec.js
@@ -9,7 +9,7 @@ import { PointTool } from '@plugins/drawingTools/models/tools'
 import DrawingTask from '@plugins/tasks/DrawingTask'
 import TextTask from '@plugins/tasks/TextTask'
 
-describe.only('SubTaskPopup', function () {
+describe('SubTaskPopup', function () {
   
   const pointTool = PointTool.create({
     color: 'red',
