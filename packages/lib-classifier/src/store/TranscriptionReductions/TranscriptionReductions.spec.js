e4b2467391e25bb5c46e1520724d22305b60d357
Jim O'Donnell <jim@zooniverse.org>
2020-02-28 08:29:38 +0000

    Classifier: Add TranscriptionReductions (#1520)
    Add a TranscriptionReductions model, adapted from Anti-Slavery Manuscripts. The model requests reductions from Caesar for a given workflow, subject and reducer key, then further reduces them to an array of consensus lines for a given frame.

---
+++ packages/lib-classifier/src/store/TranscriptionReductions/TranscriptionReductions.spec.js
@@ -0,0 +1,206 @@
+import { GraphQLClient } from 'graphql-request'
+import sinon from 'sinon'
+import { reducedEmptySubject, reducedSubject } from './mocks'
+
+import TranscriptionReductions from './TranscriptionReductions'
+
+describe('Models > TranscriptionReductions', function () {
+
+  describe('with transcribed lines', function () {
+    const caesarClient = new GraphQLClient('https://caesar-staging.zooniverse.org/graphql')
+    let reductionsModel
+
+    before(async function () {
+      const response = {
+        workflow: {
+          subject_reductions: [{ data: reducedSubject }]
+        }
+      }
+      sinon.stub(caesarClient, 'request').callsFake(() => Promise.resolve(response))
+      reductionsModel = TranscriptionReductions.create({
+        subjectId: '13971150',
+        workflowId: '5339'
+      }, {
+        client: {
+          caesar: caesarClient
+        }
+      })
+      await reductionsModel.fetchCaesarReductions()
+    })
+
+    after(function () {
+      caesarClient.request.restore()
+    })
+
+    it('should exist', function () {
+      expect(reductionsModel).to.be.ok()
+    })
+
+    it('should have transcribed lines', function () {
+      reductionsModel.reductions.forEach(reduction => expect(reduction.data.transcribed_lines).to.equal(10))
+      expect(reductionsModel.consensusLines).not.to.be.empty()
+    })
+
+    it('should default to frame 0', function () {
+      reductionsModel.consensusLines.forEach(function (consensusLine) {
+        expect(consensusLine.frame).to.equal(0)
+      })
+    })
+
+    it('should have points', function () {
+      reductionsModel.consensusLines.forEach(function (consensusLine) {
+        expect(consensusLine.points).to.be.a('array')
+        expect(consensusLine.points).not.to.be.empty
+      })
+    })
+
+    it('should have text options', function () {
+      reductionsModel.consensusLines.forEach(function (consensusLine) {
+        expect(consensusLine.textOptions).to.be.a('array')
+        expect(consensusLine.textOptions).not.to.be.empty
+      })
+    })
+
+    it('should update on frame change', function () {
+      reductionsModel.changeFrame(2)
+      reductionsModel.consensusLines.forEach(function (consensusLine) {
+        expect(consensusLine.frame).to.equal(2)
+      })
+      reductionsModel.changeFrame(0)
+    })
+
+    describe('transcribed lines', function () {
+      let consensusLine
+
+      before(async function () {
+        consensusLine = reductionsModel.consensusLines[0]
+      })
+
+      it('should have consensus text', function () {
+        expect(consensusLine.consensusText).to.equal('Here are some test')
+      })
+
+      it('should have text options', function () {
+        expect(consensusLine.textOptions).to.deep.equal([
+          'Here are some test',
+          'Here are some test',
+          'Here are some test'
+        ])
+      })
+
+      it('should have two points', function () {
+        expect(consensusLine.points).to.have.lengthOf(2)
+      })
+
+      it('should have a start point', function () {
+        const x = 32.550689697265625
+        const y = 297.0990905761719
+        expect(consensusLine.points[0]).to.deep.equal({ x, y })
+      })
+
+      it('should have an end point', function () {
+        const x = 989.6483154296875
+        const y = 280.3498840332031
+        expect(consensusLine.points[1]).to.deep.equal({ x, y })
+      })
+    })
+  })
+
+  describe('without transcribed lines', function () {
+    const caesarClient = new GraphQLClient('https://caesar-staging.zooniverse.org/graphql')
+    let reductionsModel
+
+    before(async function () {
+      const response = {
+        workflow: {
+          subject_reductions: [{ data: reducedEmptySubject }]
+        }
+      }
+      sinon.stub(caesarClient, 'request').callsFake(() => Promise.resolve(response))
+      reductionsModel = TranscriptionReductions.create({
+        subjectId: '13971170',
+        workflowId: '5339'
+      }, {
+        client: {
+          caesar: caesarClient
+        }
+      })
+      await reductionsModel.fetchCaesarReductions()
+    })
+
+    after(function () {
+      caesarClient.request.restore()
+    })
+
+    it('should exist', function () {
+      expect(reductionsModel).to.be.ok()
+    })
+
+    it('should not have any transcribed lines', function () {
+      reductionsModel.reductions.forEach(reduction => expect(reduction.data.transcribed_lines).to.equal(0))
+      expect(reductionsModel.consensusLines).to.be.empty()
+    })
+  })
+
+  describe('without a configured reducer', function () {
+    const caesarClient = new GraphQLClient('https://caesar-staging.zooniverse.org/graphql')
+    let reductionsModel
+
+    before(async function () {
+      sinon.stub(console, 'error')
+      const error = new Error('Error: GraphQL Error (Code: 404)')
+      error.response = {
+        error: '',
+        status: 404
+      }
+      error.request = {
+        query: '{ workflow(id: 3389) { subject_reductions(subjectId: 13971170, reducerKey:"ext") { data } } }',
+        variables: undefined
+      }
+      sinon.stub(caesarClient, 'request').callsFake(() => Promise.reject(error))
+      reductionsModel = TranscriptionReductions.create({
+        subjectId: '13971170',
+        workflowId: '3389'
+      }, {
+        client: {
+          caesar: caesarClient
+        }
+      })
+      await reductionsModel.fetchCaesarReductions()
+    })
+
+    after(function () {
+      caesarClient.request.restore()
+      console.error.restore()
+    })
+
+    it('should exist', function () {
+      expect(reductionsModel).to.be.ok()
+    })
+
+    it('should record the error message', function () {
+      const { message } = reductionsModel.error
+      expect(message).to.equal('Error: GraphQL Error (Code: 404)')
+    })
+
+    it('should record the error response', function () {
+      const { response } = reductionsModel.error
+      expect(response.error).to.equal('')
+      expect(response.status).to.equal(404)
+    })
+
+    it('should record the error request', function () {
+      const { request } = reductionsModel.error
+      expect(request.query).to.equal('{ workflow(id: 3389) { subject_reductions(subjectId: 13971170, reducerKey:"ext") { data } } }')
+      expect(request.variables).to.be.undefined()
+    })
+
+    it('should not have any reductions', function () {
+      expect(reductionsModel.reductions).to.be.empty()
+    })
+
+    it('should not have any transcribed lines', function () {
+      expect(reductionsModel.consensusLines).to.be.empty()
+    })
+  })
+})
---
+++ packages/lib-classifier/src/store/TranscriptionReductions/mocks/reducedEmptySubject.js
@@ -0,0 +1,15 @@
+export default {
+  'reducer': 'optics_line_text_reducer',
+  'low_consensus_lines': 0,
+  'transcribed_lines': 0,
+  'frame5': [],
+  'parameters': {
+    'min_samples': 'auto',
+    'max_eps': null,
+    'xi': 0.05,
+    'angle_eps': 30.0,
+    'gutter_eps': 150.0,
+    'low_consensus_threshold': 3.0,
+    'min_line_length': 0.0
+  }
+}
