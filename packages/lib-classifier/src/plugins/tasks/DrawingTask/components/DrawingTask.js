e6817434f8414a58c365586f49f85f1d2188f39c
Jim O'Donnell <jim@zooniverse.org>
2020-03-02 08:58:21 +0000

    Remove observer from drawing task

--- packages/lib-classifier/src/plugins/tasks/DrawingTask/components/DrawingTask.js
+++ packages/lib-classifier/src/plugins/tasks/DrawingTask/components/DrawingTask.js
@@ -1,6 +1,5 @@
 import { Box, Text } from 'grommet'
 import { Blank } from 'grommet-icons'
-import { observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
@@ -28,47 +27,44 @@ const StyledText = styled(Text)`
     margin-top: 0;
   }
 `
-@observer
-class DrawingTask extends React.Component {
-  onChange (index, event) {
-    const { setActiveTool } = this.props.task
+function DrawingTask (props) {
+  const { task } = props
+  const { setActiveTool } = task
+  function onChange (index, event) {
     if (event.target.checked) {
       setActiveTool(index)
     }
   }
 
-  render () {
-    const { task } = this.props
-    return (
-      <Box>
-        <StyledText size='small' tag='legend'>
-          <Markdownz>
-            {task.instruction}
-          </Markdownz>
-        </StyledText>
+  return (
+    <Box>
+      <StyledText size='small' tag='legend'>
+        <Markdownz>
+          {task.instruction}
+        </Markdownz>
+      </StyledText>
 
-        {task.tools.map((tool, index) => {
-          const checked = task.activeToolIndex === index
-          // TODO add count for min/max
-          return (
-            <TaskInput
-              checked={checked}
-              disabled={tool.disabled}
-              index={index}
-              key={`${task.taskKey}_${index}`}
-              label={tool.label}
-              labelIcon={<InputIcon icon={<ToolIcon type={tool.type} />} tool={tool} />}
-              labelStatus={<InputStatus count={tool.marks.size} tool={tool} />}
-              name='drawing-tool'
-              onChange={this.onChange.bind(this, index)}
-              required={task.required}
-              type='radio'
-            />
-          )
-        })}
-      </Box>
-    )
-  }
+      {task.tools.map((tool, index) => {
+        const checked = task.activeToolIndex === index
+        // TODO add count for min/max
+        return (
+          <TaskInput
+            checked={checked}
+            disabled={tool.disabled}
+            index={index}
+            key={`${task.taskKey}_${index}`}
+            label={tool.label}
+            labelIcon={<InputIcon icon={<ToolIcon type={tool.type} />} tool={tool} />}
+            labelStatus={<InputStatus count={tool.marks.size} tool={tool} />}
+            name='drawing-tool'
+            onChange={event => onChange(index, event)}
+            required={task.required}
+            type='radio'
+          />
+        )
+      })}
+    </Box>
+  )
 }
 
 DrawingTask.defaultProps = {
