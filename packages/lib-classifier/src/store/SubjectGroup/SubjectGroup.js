c1eb67fbd574dbad320f8e0abd933cf97eae07d8
Jim O'Donnell <jim@zooniverse.org>
2020-03-11 17:42:28 +0000

    Add subject attributes Add subject attributes and views to subject groups. They should behave like ordinary subjects so have Talk URLs, can be favourited and can be added to collections.

--- packages/lib-classifier/src/store/SubjectGroup/SubjectGroup.js
+++ packages/lib-classifier/src/store/SubjectGroup/SubjectGroup.js
@@ -1,11 +1,68 @@
-import { types } from 'mobx-state-tree'
+import { getRoot, tryReference, types } from 'mobx-state-tree'
 import Resource from '../Resource'
+import subjectViewers from '../../helpers/subjectViewers'
 import SingleImageSubject from '../SingleImageSubject'
 
 const SubjectGroup = types
   .model('SubjectGroup', {
-    subjects: types.array(SingleImageSubject)
+    already_seen: types.optional(types.boolean, false),
+    favorite: types.optional(types.boolean, false),
+    finished_workflow: types.optional(types.boolean, false),
+    metadata: types.frozen({}),
+    retired: types.optional(types.boolean, false),
+    selected_at: types.maybe(types.string),
+    selection_state: types.maybe(types.string),
+    shouldDiscuss: types.maybe(types.frozen()),
+    subjects: types.array(SingleImageSubject),
+    user_has_finished_workflow: types.optional(types.boolean, false),
   })
+
+  .actions(self => {
+
+    function addToCollection () {
+      const rootStore = getRoot(self)
+      rootStore.onAddToCollection(self.id)
+    }
+
+    function openInTalk (newTab = false) {
+      self.shouldDiscuss = {
+        newTab,
+        url: self.talkURL
+      }
+    }
+
+    function toggleFavorite () {
+      const rootStore = getRoot(self)
+      self.favorite = !self.favorite
+      rootStore.onToggleFavourite(self.id, self.favorite)
+    }
+
+    return {
+      addToCollection,
+      openInTalk,
+      toggleFavorite
+    }
+  })
+
+  .views(self => ({
+    get talkURL () {
+      const project = tryReference(() => getRoot(self).projects?.active)
+      if (project) {
+        const { origin } = window.location
+        return `${origin}/projects/${project.slug}/talk/subjects/${self.id}`
+      }
+
+      return ''
+    },
+
+    get viewer () {
+      return subjectViewers.subjectGroup
+    },
+
+    get workflow () {
+      return tryReference(() => getRoot(self).workflows?.active)
+    }
+  }))
 
   
 
