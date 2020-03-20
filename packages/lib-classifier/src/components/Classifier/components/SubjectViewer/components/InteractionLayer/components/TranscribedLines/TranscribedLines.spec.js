79271590293e05daf5dbc26e98645385c85d2bde
Sarah Allen <sarah@zooniverse.org>
2020-03-20 16:26:09 -0500

    Fill out tests we can do right now

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/TranscribedLines/TranscribedLines.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/TranscribedLines/TranscribedLines.spec.js
@@ -3,11 +3,11 @@ import sinon from 'sinon'
 import React from 'react'
 import TranscriptionReductions from '@store/TranscriptionReductions'
 import taskRegistry from '@plugins/tasks'
-import { TranscribedLines, ConsensusLine } from './TranscribedLines'
+import { TranscribedLines } from './TranscribedLines'
 import { reducedSubject } from '@store/TranscriptionReductions/mocks'
 import { TranscriptionLine } from '@plugins/drawingTools/components'
 
-describe.only('Component > TranscribedLines', function () {
+describe('Component > TranscribedLines', function () {
   let wrapper, task, consensusLines
   before(function () {
     const transcriptionReductions = TranscriptionReductions.create({
@@ -29,58 +29,79 @@ describe.only('Component > TranscribedLines', function () {
     expect(wrapper).to.be.ok()
   })
 
-  describe.only('incomplete lines', function () {
+  describe('incomplete lines', function () {
     let lines
     before(function () {
       lines = wrapper.find(TranscriptionLine).find({ state: 'transcribed' })
     })
 
-    it('should be red', function () {
+    it('should render', function () {
       const transcribedLines = consensusLines.filter(line => !line.consensusReached)
       expect(lines).to.have.lengthOf(transcribedLines.length)
     })
 
     it('should be focusable', function () {
       lines.forEach((component) => {
-        const consensusLineWrappers = component.parent()
-        expect(consensusLineWrappers.props().tabIndex).to.equal(0)
+        const consensusLineWrapper = component.parent()
+        expect(consensusLineWrapper.props().tabIndex).to.equal(0)
       })
     })
 
-    it('should create a new mark on click', function () {
+    xit('should create a new mark on click', function () {
       
     })
 
     it('should have an explanatory tooltip', function () {
-      
+      lines.forEach((component, index) => {
+        const tooltip = wrapper.find({ id: `transcribed-${index}`})
+        expect(tooltip).to.have.lengthOf(1)
+      })
     })
   })
 
   describe('completed lines',function () {
-    it('should be grey', function () {})
+    let lines, completeLines
+    before(function () {
+      lines = wrapper.find(TranscriptionLine).find({ state: 'complete' })
+      completeLines = consensusLines.filter(line => line.consensusReached)
+    })
 
-    it('should be labelled with the consensus text', function () {})
+    it('should render', function () {
+      expect(lines).to.have.lengthOf(completeLines.length)
+    })
 
-    it('should show the label on hover', function () {})
-
-    it('should be focusable', function () {})
-
-    it('should show the label on focus', function () {})
-
-    it('should not be clickable', function () {
-      // TODO update test when functionality is built out
-      const alertStub = sinon.stub(window, 'alert')
-      const consensusLineWrappers = wrapper.find(TranscriptionLine).find({ state: 'complete' }).parent()
-      consensusLineWrappers.forEach((component) => {
-        component.simulate('click')
-        expect(alertStub).to.have.been.calledOnce()
-        alertStub.resetHistory()
+    it('should be labelled with the consensus text', function () {
+      lines.forEach((component, index) => {
+        const consensusLineWrapper = component.parent()
+        expect(consensusLineWrapper.props()['aria-label']).to.equal(completeLines[index].consensusText)
       })
-      alertStub.restore()
+    })
+
+    xit('should show the label on hover', function () {
+      lines.forEach((component, index) => {
+        const consensusLineWrapper = wrapper.find({ ['aria-describedby']: `complete-${index}`})
+        consensusLineWrapper.simulate('hover')
+      })
+    })
+
+    it('should be focusable', function () {
+      lines.forEach((component) => {
+        const consensusLineWrapper = component.parent()
+        expect(consensusLineWrapper.props().tabIndex).to.equal(0)
+      })
+    })
+
+    xit('should show the label on focus', function () {})
+
+    xit('should not be clickable', function () {
+      // TODO update test when functionality is built out
     })
 
     it('should have an explanatory tooltip', function () {
-
+      lines.forEach((component, index) => {
+        const tooltip = wrapper.find({ id: `complete-${index}` })
+        expect(tooltip).to.have.lengthOf(1)
+      })
     })
   })
 })
