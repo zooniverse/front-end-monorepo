8873e496f8d61d5d622c8dd10e68b25d7bd8c66f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-03-25 01:29:43 +0000

    Add tests for SGVGridCell

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SubjectGroupViewer/components/SGVGridCell/SGVGridCell.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SubjectGroupViewer/components/SGVGridCell/SGVGridCell.spec.js
@@ -11,22 +11,27 @@ const exampleImage = {
   naturalWidth: 300,
 }
 
+const cellWidth = 800
+const cellHeight = 600
+const gridRows = 3
+const gridColumns = 3
+
 describe('Component > SubjectGroupViewer > SGVGridCell', function () {
   beforeEach(function () {
     wrapper = shallow(
       <SGVGridCell
-        images={exampleImage}
+        image={exampleImage}
         index={0}
         dragMove={() => {}}
-        cellWidth={200}
-        cellHeight={200}
+        cellWidth={cellWidth}
+        cellHeight={cellHeight}
         cellStyle={{
           stroke: '#fff',
           strokeWidth: '4',
           fill: '#000',
         }}
-        gridRows={2}
-        gridColumns={2}
+        gridRows={gridRows}
+        gridColumns={gridColumns}
         panX={0}
         panY={0}
         zoom={1}
@@ -38,4 +43,33 @@ describe('Component > SubjectGroupViewer > SGVGridCell', function () {
     expect(wrapper).to.be.ok()
   })
   
+  it('should render an image', function () {
+    expect(wrapper.find('draggable(image)')).to.have.lengthOf(1)
+  })
+  
+  describe('with a pan and zoom', function () {
+    beforeEach(function () {
+      wrapper.setProps({ panX: 10, panY: 50, zoom: 1.5 })
+    })
+
+    it('should be transformed', function () {
+      const transform = wrapper.find('draggable(image)').prop('transform')
+      expect(transform).to.have.string('scale(1.5) translate(10, 50)')
+    })
+  })
+  
+  describe('when given an index', function () {
+    const x = 1
+    const y = 2
+    const index = x + y * gridColumns 
+    
+    beforeEach(function () {
+      wrapper.setProps({ index: index })
+    })
+
+    it('should position the cell according to predefined cell width/height', function () {
+      const transform = wrapper.first().prop('transform')
+      expect(transform).to.have.string(`translate(800, 1200)`)
+    })
+  })
 })
