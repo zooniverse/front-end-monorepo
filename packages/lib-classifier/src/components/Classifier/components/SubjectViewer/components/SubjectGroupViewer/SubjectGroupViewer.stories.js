6c75507776796c07be3f0a326b57e47bbd1b8b82
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-03-31 18:44:05 +0100

    Add functioning Storybook component

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SubjectGroupViewer/SubjectGroupViewer.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SubjectGroupViewer/SubjectGroupViewer.stories.js
@@ -4,10 +4,10 @@ import { storiesOf } from '@storybook/react'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Box, Grommet } from 'grommet'
 import { Provider } from 'mobx-react'
+import SubjectViewerStore from '@store/SubjectViewerStore'
 import SubjectGroupViewer, { SubjectGroupViewerContainer } from './SubjectGroupViewerContainer'
-import ZoomInButton from '../../../ImageToolbar/components/ZoomInButton/ZoomInButton'
-import ZoomOutButton from '../../../ImageToolbar/components/ZoomOutButton/ZoomOutButton'
-import ResetButton from '../../../ImageToolbar/components/ResetButton/ResetButton'
+import { AnnotateButton, MoveButton, ResetButton, RotateButton, ZoomInButton, ZoomOutButton } from '../../../ImageToolbar/components/'
+import withKeyZoom from '../../../withKeyZoom'
 import readme from './README.md'
 import backgrounds from '../../../../../../../.storybook/lib/backgrounds'
 
@@ -19,26 +19,13 @@ const config = {
 
 const subject = {
   locations: [
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' },
-    { 'image/jpeg': 'http://placekitten.com/500/300' }
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
+    { 'image/jpeg': 'http://placekitten.com/600/400' },
   ]
-}
-
-let zoomCallback
-
-function onZoom(type) {
-  zoomCallback(type)
-}
-
-function setZoomCallback(callback) {
-  zoomCallback = callback
 }
 
 const mockStore = {
@@ -50,9 +37,22 @@ const mockStore = {
   drawing: {
     addToStream: sinon.stub()
   },
-  subjectViewer: {
-    enableRotation: () => null,
-    setOnZoom: setZoomCallback
+  subjectViewer: SubjectViewerStore.create({}),
+  workflows: {
+    active: {
+      configuration: {
+        subject_viewer: 'subjectGroup',
+        cell_width: 300,
+        cell_height: 200,
+        cell_style: {
+          stroke: '#fff',
+          strokeWidth: '4',
+          fill: '#000'
+        },
+        grid_columns: 3,
+        grid_rows: 2
+      },
+    }
   },
   workflowSteps: {
     activeStepTasks: []
@@ -76,41 +76,43 @@ const darkThemeConfig = Object.assign({}, config, { backgrounds: backgrounds.dar
 storiesOf('Subject Viewers | SubjectGroupViewer', module)
   .add('light theme', () => {
     return (
-      <Grommet theme={zooTheme}>
-        <Box height='medium' width='large'>
-          <SubjectGroupViewer
-            enableInteractionLayer={false}
-            subject={subject}
-          />
-        </Box>
-      </Grommet>
-    )
-  }, config)
-  .add('dark theme', () => {
-    const darkZooTheme = Object.assign({}, zooTheme, { dark: true })
-    return (
-      <Grommet theme={darkZooTheme}>
-        <Box height='medium' width='large'>
-          <SubjectGroupViewer
-            enableInteractionLayer={false}
-            subject={subject}
-          />
-        </Box>
-      </Grommet>
-    )
-  }, darkThemeConfig)
-  .add('pan and zoom', () => {
-    return (
       <ViewerContext theme={zooTheme}>
         <Box height='medium' width='large'>
           <SubjectGroupViewer
             subject={subject}
           />
         </Box>
-        <Box direction='row'>
-          <ZoomInButton onClick={() => onZoom('zoomin')} />
-          <ZoomOutButton onClick={() => onZoom('zoomout')} />
-          <ResetButton onClick={() => onZoom('zoomto')} />
+      </ViewerContext>
+    )
+  }, config)
+  .add('dark theme', () => {
+    const darkZooTheme = Object.assign({}, zooTheme, { dark: true })
+    return (
+      <ViewerContext theme={darkZooTheme}>
+        <Box height='medium' width='large'>
+          <SubjectGroupViewer
+            subject={subject}
+          />
+        </Box>
+      </ViewerContext>
+    )
+  }, darkThemeConfig)
+  .add('with zoom controls', () => {
+    const Toolbar = withKeyZoom(Box)
+    return (
+      <ViewerContext theme={zooTheme}>
+        <Toolbar direction='row'>
+          <AnnotateButton />
+          <MoveButton />
+          <ZoomInButton />
+          <ZoomOutButton />
+          <RotateButton />
+          <ResetButton />
+        </Toolbar>
+        <Box height='medium' width='large'>
+          <SubjectGroupViewer
+            subject={subject}
+          />
         </Box>
       </ViewerContext>
     )
