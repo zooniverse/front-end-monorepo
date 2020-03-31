946738fa6cfee4b04f1c0e1af443b1d76906f371
Jim O'Donnell <jim@zooniverse.org>
2020-03-31 13:08:49 +0100

    Add sentry release IDs Add git commit hashes as the sentry release IDs to the project app and the content pages app.

--- packages/app-project/src/helpers/logger/initializeSentry.js
+++ packages/app-project/src/helpers/logger/initializeSentry.js
@@ -3,11 +3,13 @@ import * as Sentry from '@sentry/browser'
 export default function initializeSentry () {
   const dsn = process.env.SENTRY_DSN
   const appEnv = process.env.APP_ENV
+  const release = `@zooniverse/fe-project@${process.env.COMMIT_ID}`
 
   if (dsn) {
     Sentry.init({
       dsn,
       environment: appEnv,
+      release
     })
   }
 
