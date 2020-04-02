f06e3f35e28e3fdf6ff3c8589e6b66c1841c1f76
mcbouslog <mcbouslog@gmail.com>
2020-04-02 09:16:13 -0500

    Edit TextTagButtons specs

--- packages/lib-classifier/src/plugins/tasks/TextTask/components/TextTask/components/TextTagButtons/TextTagButtons.spec.js
+++ packages/lib-classifier/src/plugins/tasks/TextTask/components/TextTask/components/TextTagButtons/TextTagButtons.spec.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import sinon from 'sinon'
-import { Text } from 'grommet'
+import { PlainButton } from '@zooniverse/react-components'
 import { default as Task } from '@plugins/tasks/TextTask'
 import TextTagButtons from './TextTagButtons'
 
-describe.only('TextTask > Components > TextTagButtons', function () {
+describe('TextTask > Components > TextTagButtons', function () {
   const task = Task.TaskModel.create({
     instruction: 'Type something here',
     taskKey: 'T0',
@@ -42,27 +42,27 @@ describe.only('TextTask > Components > TextTagButtons', function () {
     })
 
     it('should render buttons for tagging text', function () {
-      expect(wrapper.find('button')).to.have.lengthOf(2)
+      expect(wrapper.find(PlainButton)).to.have.lengthOf(2)
     })
 
     task.text_tags.forEach(function (tag) {
       it('should be labeled', function () {
-        const button = wrapper.find('button').find({ value: tag })
-        const id = `textModifiers-${task.taskKey}`
-        const label = `textModifiers-${taskKey} ${taskKey}-${tag}`
-        expect(wrapper.find(Text).props().id).to.equal(id)
+        const button = wrapper.find(PlainButton).find({ value: tag })
+        const id = `${task.taskKey}-${tag}`
+        const label = `textModifiers-${task.taskKey} ${task.taskKey}-${tag}`
+        expect(button.props().id).to.equal(id)
         expect(button.props()['aria-labelledby']).to.equal(label)
         expect(button.props().text).to.equal(tag)
         expect(button.props().value).to.equal(tag)
       })
 
       it(`should render a ${tag} button`, function () {
-        const button = wrapper.find('button').find({ value: tag })
+        const button = wrapper.find(PlainButton).find({ value: tag })
         expect(button).to.have.lengthOf(1)
       })
 
       it('should call onClick when the button is clicked', function () {
-        const button = wrapper.find('button').find({ value: tag })
+        const button = wrapper.find(PlainButton).find({ value: tag })
         button.simulate('click')
         expect(onClickSpy).to.have.been.calledOnce()
         onClickSpy.resetHistory()
@@ -70,7 +70,7 @@ describe.only('TextTask > Components > TextTagButtons', function () {
 
       it('should disable the button', function () {
         wrapper.setProps({ disabled: true })
-        const button = wrapper.find('button').find({ value: tag })
+        const button = wrapper.find(PlainButton).find({ value: tag })
         expect(button.props().disabled).to.be.true()
       })
     })
