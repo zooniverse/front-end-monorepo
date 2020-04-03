e57bcc5186a17beb71f4fa1e42512f8fc541e936
Jim O'Donnell <jim@zooniverse.org>
2020-04-03 16:51:10 +0100

    Create Sentry releases on push to master (#1599)
    * Add GitHub Sentry release actions
    Add a couple of new GitHub actions.
    - on merge to master, create a new Sentry release from the commit SHA, link to all commits since the last finalised release and deploy it as a staging release.
    - on publishing a new release, which is always production-release for this repo, get the existing release for the commit SHA and finalise it, then deploy as a production release.
    
    Update the Sentry client to use commit SHA for release versions.

--- packages/app-content-pages/src/helpers/logger/initializeSentry.js
+++ packages/app-content-pages/src/helpers/logger/initializeSentry.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser'
 
 export default function initializeSentry () {
   const dsn = process.env.SENTRY_DSN
-  const release = `@zooniverse/fe-project@${process.env.COMMIT_ID}`
+  const release = process.env.COMMIT_ID
   const environment = process.env.APP_ENV
 
   if (dsn) {
