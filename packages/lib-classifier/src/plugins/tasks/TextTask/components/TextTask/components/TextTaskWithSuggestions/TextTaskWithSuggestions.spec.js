10b4d02a039a27f089cf7db03629ae7d0dfa4e77
Mark Bouslog <mark@zooniverse.org>
2020-04-09 09:22:59 -0500

    Refactor text task default, with suggestions, and tags (#1600)
    * Compentize different functions. Update tests
    
    * Add proptypes, defaults, and more specs
    
    * Edit TextTagButtons specs
    
    Co-authored-by: Sarah Allen <sarah@zooniverse.org>
    Co-authored-by: srallen <srallen@users.noreply.github.com>

---
+++ packages/lib-classifier/src/plugins/tasks/TextTask/components/TextTask/components/TextTaskWithSuggestions/TextTaskWithSuggestions.spec.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import { Text, TextInput } from 'grommet'
+import { default as Task } from '@plugins/tasks/TextTask'
+import TextTaskWithSuggestions from './TextTaskWithSuggestions'
+
+describe('TextTask > Components > TextTaskWithSuggestions', function () {
+  let wrapper
+  const task = Task.TaskModel.create({
+    instruction: 'Type something here',
+    taskKey: 'T0',
+    text_tags: ['insertion', 'deletion'],
+    type: 'text'
+  })
+  const annotation = task.defaultAnnotation
+
+  before(function () {
+    wrapper = shallow(
+      <TextTaskWithSuggestions
+        suggestions={['one', 'two', 'three']}
+        task={task}
+        value={annotation.value}
+      />
+    )
+  })
+
+  it('should have a labelled TextInput', function () {
+    const label = wrapper.find('label')
+    expect(label.find(Text).prop('children')).to.equal(task.instruction)
+    const textInput = label.find(TextInput)
+    expect(textInput.prop('value')).to.equal(annotation.value)
+  })
+})
