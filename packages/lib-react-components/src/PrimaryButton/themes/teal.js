5cdc1c74578bfe866f6b4dfb1616e26b0c08e3d9
Sarah Allen <sarah@zooniverse.org>
2020-04-16 10:24:28 -0500

    Remove cruft

--- packages/lib-react-components/src/PrimaryButton/themes/teal.js
+++ packages/lib-react-components/src/PrimaryButton/themes/teal.js
@@ -22,11 +22,6 @@ const theme = {
       const color = normalizeColor('brand', props.theme)
       const gradientShade = getGradientShade(color)
 
-      // This shouldn't be necessary where we're manually setting the color with our own ternary,
-      // but style collision is happening
-      // so the theme color under color.light isn't being applied
-      // needs more investigation, as it seems to be a Grommet bug
-      const fontColor = props.theme.dark ? color : 'white'
       return css`
         transition: none;
         text-align: center;
