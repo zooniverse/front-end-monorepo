39b75c7f043b00173ed08fe610f34bb1a392d48b
Sarah Allen <sarah@zooniverse.org>
2020-04-30 12:40:52 -0500

    Mostly working subtask story

--- packages/lib-classifier/src/plugins/drawingTools/components/Point/Point.stories.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Point/Point.stories.js
@@ -4,17 +4,15 @@ import React, { Component } from 'react'
 import { Box } from 'grommet'
 import sinon from 'sinon'
 import { Provider } from 'mobx-react'
+import { Grommet } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
+import asyncStates from '@zooniverse/async-states'
 import cuid from 'cuid'
 import SingleImageViewer from '@viewers/components/SingleImageViewer'
-import { PointTool } from '@plugins/drawingTools/models/tools'
 import ClassificationStore from '@store/ClassificationStore'
 import SubjectViewerStore from '@store/SubjectViewerStore'
 import DrawingTask from '@plugins/tasks/DrawingTask/models/DrawingTask'
-import Point from './Point'
 import { ProjectFactory, SubjectFactory, WorkflowFactory } from '@test/factories'
-
-const id = cuid()
 
 const subject = SubjectFactory.build({
   locations: [
@@ -24,8 +22,7 @@ const subject = SubjectFactory.build({
 
 const project = ProjectFactory.build()
 const workflow = WorkflowFactory.build()
-
-const drawingTask = DrawingTask.create({
+const drawingTaskSnapshot = {
   instruction: 'Draw a point',
   taskKey: 'T1',
   tools: [{
@@ -33,41 +30,112 @@ const drawingTask = DrawingTask.create({
     type: 'point'
   }],
   type: 'drawing'
-})
-drawingTask.setActiveTool(0)
-const point = drawingTask.activeTool.createMark()
-point.move({ x: 100, y: 100 })
-console.log('point', point)
-const drawingAnnotation = { id, task: 'T1', taskType: 'drawing', value: [point] }
-
-const mockStore = {
-  classifications: ClassificationStore.create(),
-  subjects: {
-    active: subject
-  },
-  subjectViewer: SubjectViewerStore.create(),
-  workflows: {
-    active: { id: cuid() }
-  },
-  workflowSteps: {
-    activeStepTasks: [drawingTask]
-  }
 }
 
-mockStore.classifications.createClassification(subject, workflow, project)
-mockStore.workflowSteps.activeStepTasks[0].updateAnnotation()
+const subTasksSnapshot = [
+  {
+    instruction: 'Name your favourite fruit.',
+    taskKey: 'T0.0',
+    type: 'text'
+  },
+  {
+    answers: [{ label: "yes" }, { label: "no" }],
+    help: "",
+    question: "Is it tasty?",
+    taskKey: 'T0.1',
+    type: 'single'
+  },
+  {
+    answers: [{ label: "cat" }, { label: "dog" }, { label: "bird" }],
+    help: "",
+    question: "Select your favourite animals.",
+    taskKey: 'T0.2',
+    type: 'multiple'
+  }
+]
+
+function setupStores ({ activeMark = false, subtask = false }) {
+  if (subtask) {
+    drawingTaskSnapshot.tools[0].details = subTasksSnapshot
+    drawingTaskSnapshot.subTaskVisibility = true
+    drawingTaskSnapshot.subTaskMarkBounds = {
+      x: 100,
+      y: 100,
+      width: 0,
+      height: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  } 
+
+  const drawingTask = DrawingTask.create(drawingTaskSnapshot)
+  drawingTask.setActiveTool(0)
+  const point = drawingTask.activeTool.createMark()
+  point.move({ x: 100, y: 100 })
+
+  const mockStores = {
+    classifications: ClassificationStore.create(),
+    subjects: {
+      active: subject
+    },
+    subjectViewer: SubjectViewerStore.create(),
+    workflows: {
+      active: { id: cuid() }
+    },
+    workflowSteps: {
+      activeStepTasks: [drawingTask]
+    }
+  }
+
+  mockStores.classifications.createClassification(subject, workflow, project)
+  mockStores.workflowSteps.activeStepTasks[0].updateAnnotation()
+  if (activeMark) {
+    mockStores.workflowSteps.activeStepTasks[0].setActiveMark(point.id)
+  }
+
+  if (subtask) {
+    mockStores
+  }
+
+  return mockStores
+}
 
 
 class DrawingStory extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      loadingState: asyncStates.initialized
+    }
+  }
+
+  componentDidMount () {
+    // what needs this time to make the svg ref to be defined?
+    // 100ms isn't enough time 1000ms is
+    setTimeout(() => this.setState({ loadingState: asyncStates.success }), 1000)
+  }
+
   render () {
     return (
-      <Provider classifierStore={mockStore}>
-        <Box height='medium' width='large'>
-          <SingleImageViewer
-            loadingState='success'
-            subject={subject}
-          />
-        </Box>
+      <Provider classifierStore={this.props.stores}>
+        <Grommet
+          background={{
+            dark: 'dark-1',
+            light: 'light-1'
+          }}
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <Box height='medium' width='large'>
+            <SingleImageViewer
+              loadingState={this.state.loadingState}
+              subject={subject}
+            />
+          </Box>
+        </Grommet>
       </Provider>
     )
   }
@@ -80,12 +148,20 @@ storiesOf('Drawing tools | Point', module)
     }
   })
   .add('complete', function () {
+    const stores = setupStores({})
     return (
-      <DrawingStory />
+      <DrawingStory stores={stores} />
     )
   })
   .add('active', function () {
+    const stores = setupStores({ activeMark: true })
     return (
-      <DrawingStory />
+      <DrawingStory stores={stores} />
+    )
+  })
+  .add('active with subtask', function () {
+    const stores = setupStores({ activeMark: true, subtask: true })
+    return (
+      <DrawingStory stores={stores} />
     )
   })
