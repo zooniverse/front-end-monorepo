3305d01d346db08dfde7d4e6b2da82112ee86b4f
srallen <srallen@users.noreply.github.com>
2020-05-11 11:18:50 -0500

    Subtask stories drawing tools (#1636)
    * Start of drawing tool stories refactor
    
    * Mostly working subtask story
    
    * Guess what the position should be
    
    * Remove cruft
    
    * Refactor other drawing tool stories. Add story for ellipse

--- packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.stories.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.stories.js
@@ -1,28 +1,142 @@
 import { withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import zooTheme from '@zooniverse/grommet-theme'
-import React, { Component, createRef, forwardRef } from 'react'
-import sinon from 'sinon'
-import { RectangleTool } from '@plugins/drawingTools/models/tools'
-import { Mark }from '@plugins/drawingTools/components'
-import Rectangle from './Rectangle'
+import React, { Component } from 'react'
+import { Box, Grommet } from 'grommet'
+import { Provider } from 'mobx-react'
+import asyncStates from '@zooniverse/async-states'
+import cuid from 'cuid'
+import SingleImageViewer from '@viewers/components/SingleImageViewer'
+import ClassificationStore from '@store/ClassificationStore'
+import SubjectViewerStore from '@store/SubjectViewerStore'
+import DrawingTask from '@plugins/tasks/DrawingTask/models/DrawingTask'
+import { DrawingTaskFactory, ProjectFactory, SubjectFactory, WorkflowFactory } from '@test/factories'
+
+const subject = SubjectFactory.build({
+  locations: [
+    { 'image/jpeg': 'http://placekitten.com/500/300' }
+  ]
+})
+
+const project = ProjectFactory.build()
+const workflow = WorkflowFactory.build()
+const drawingTaskSnapshot = DrawingTaskFactory.build({
+  instruction: 'Draw a rectangle',
+  taskKey: 'T1',
+  tools: [{
+    color: zooTheme.global.colors['drawing-red'],
+    type: 'rectangle'
+  }],
+  type: 'drawing'
+})
+
+const subTasksSnapshot = [
+  {
+    instruction: 'Name your favourite fruit.',
+    taskKey: 'T0.0',
+    type: 'text'
+  },
+  {
+    answers: [{ label: "yes" }, { label: "no" }],
+    help: "",
+    question: "Is it tasty?",
+    taskKey: 'T0.1',
+    type: 'single'
+  },
+  {
+    answers: [{ label: "cat" }, { label: "dog" }, { label: "bird" }],
+    help: "",
+    question: "Select your favourite animals.",
+    taskKey: 'T0.2',
+    type: 'multiple'
+  }
+]
+
+function setupStores({ activeMark, subtask }) {
+  if (subtask) {
+    drawingTaskSnapshot.tools[0].details = subTasksSnapshot
+    drawingTaskSnapshot.subTaskVisibility = true
+    // should think of a better way to do this for the story
+    // this is a rough approximation of what the positioning is like now
+    drawingTaskSnapshot.subTaskMarkBounds = {
+      x: 200,
+      y: 200,
+      width: 0,
+      height: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  }
+
+  const drawingTask = DrawingTask.create(drawingTaskSnapshot)
+  drawingTask.setActiveTool(0)
+  const rectangle = drawingTask.activeTool.createMark()
+  rectangle.initialPosition({ x: 100, y: 100 })
+  rectangle.initialDrag({ x: 150, y: 150 })
+
+  const mockStores = {
+    classifications: ClassificationStore.create(),
+    subjects: {
+      active: subject
+    },
+    subjectViewer: SubjectViewerStore.create(),
+    workflows: {
+      active: { id: cuid() }
+    },
+    workflowSteps: {
+      activeStepTasks: [drawingTask]
+    }
+  }
+
+  mockStores.classifications.createClassification(subject, workflow, project)
+  mockStores.workflowSteps.activeStepTasks[0].updateAnnotation()
+  if (activeMark) {
+    mockStores.workflowSteps.activeStepTasks[0].setActiveMark(rectangle.id)
+  }
+
+  return mockStores
+}
 
 class DrawingStory extends Component {
-  render () {
-    const { children, mark } = this.props
+  constructor() {
+    super()
 
+    this.state = {
+      loadingState: asyncStates.initialized
+    }
+  }
+
+  componentDidMount() {
+    // what needs this time to make the svg ref to be defined?
+    // 100ms isn't enough time 1000ms is
+    setTimeout(() => this.setState({ loadingState: asyncStates.success }), 1000)
+  }
+
+  render () {
     return (
-      <svg viewBox='0 0 300 400' height={300} width={400}>
-        <Mark
-          label='Rectangle'
-          mark={mark}
+      <Provider classifierStore={this.props.stores}>
+        <Grommet
+          background={{
+            dark: 'dark-1',
+            light: 'light-1'
+          }}
+          theme={zooTheme}
+          themeMode='light'
         >
-          {children}
-        </Mark>
-      </svg>
+          <Box height='medium' width='large'>
+            <SingleImageViewer
+              loadingState={this.state.loadingState}
+              subject={subject}
+            />
+          </Box>
+        </Grommet>
+      </Provider>
     )
   }
 }
+
 storiesOf('Drawing tools | Rectangle', module)
   .addDecorator(withKnobs)
   .addParameters({
@@ -31,40 +145,22 @@ storiesOf('Drawing tools | Rectangle', module)
     }
   })
   .add('complete', function () {
-    const tool = RectangleTool.create({
-      color: 'blue',
-      type: 'rectangle'
-    })
-    const mark = tool.createMark({
-      id: 'rect1',
-      toolType: 'rectangle',
-      x_center: 200,
-      y_center: 150,
-      width: 100,
-      height: 80,
-    })
+    const stores = setupStores({ activeMark: false, subtask: false })
+
     return (
-      <DrawingStory mark={mark}>
-        <Rectangle mark={mark} scale={1} />
-      </DrawingStory>
+      <DrawingStory stores={stores} />
     )
   })
   .add('active', function () {
-    const tool = RectangleTool.create({
-      color: 'blue',
-      type: 'rectangle'
-    })
-    const mark = tool.createMark({
-      id: 'rect1',
-      toolType: 'rectangle',      
-      x_center: 200,
-      y_center: 150,
-      width: 100,
-      height: 80,
-    })
+    const stores = setupStores({ activeMark: true, subtask: false })
+
     return (
-      <DrawingStory mark={mark}>
-        <Rectangle active mark={mark} scale={1} />
-      </DrawingStory>
+      <DrawingStory stores={stores} />
+    )
+  })
+  .add('active with subtask', function () {
+    const stores = setupStores({ activeMark: true, subtask: true })
+    return (
+      <DrawingStory stores={stores} />
     )
   })
