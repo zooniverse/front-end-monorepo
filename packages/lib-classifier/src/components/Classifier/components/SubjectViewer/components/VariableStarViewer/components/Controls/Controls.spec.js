134cdebaed1ad8ad15c0dcb3d1ee7628391875f7
Sarah Allen <sarah@zooniverse.org>
2020-07-15 15:12:34 -0500

    refactor Controls specs

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VariableStarViewer/components/Controls/Controls.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VariableStarViewer/components/Controls/Controls.spec.js
@@ -3,8 +3,10 @@ import React from 'react'
 import sinon from 'sinon'
 import { Grommet } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
-import Controls, { FlipButton, StyledRadioButtonGroup } from './Controls'
+import Controls, { FlipButton } from './Controls'
 import VisibilitySeriesCheckBoxes from './components/VisibilitySeriesCheckBoxes'
+import PeriodMultipleControls from './components/PeriodMultipleControls'
+import PhaseFocusControls from './components/PhaseFocusControls'
 import variableStar from '../../../../helpers/mockLightCurves/variableStar'
 
 const visibleSeriesMock = [
@@ -22,75 +24,84 @@ describe('VariableStarViewer > Component > Controls', function () {
     it('should call props.setYAxisInversion when the flip button is clicked', function () {
       const setYAxisInversionSpy = sinon.spy()
       const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls setYAxisInversion={setYAxisInversionSpy} />
-        </Grommet>
+        <Controls setYAxisInversion={setYAxisInversionSpy} />,
+        {
+          wrappingComponent: Grommet,
+          wrappingComponentProps: { theme: zooTheme }
+        }
       )
       wrapper.find(FlipButton).simulate('click')
       expect(setYAxisInversionSpy).to.have.been.calledOnce()
     })
   })
+  
+  describe('period multiple controls', function () {
+    let wrapper
+    before(function () {
+      wrapper = mount(
+        <Controls data={variableStar.scatterPlot.data} periodMultiple={2} setPeriodMultiple={sinon.spy()} />,
+        {
+          wrappingComponent: Grommet,
+          wrappingComponentProps: { theme: zooTheme }
+        }
+      )
+    })
+
+    it('should render PeriodMultipleControls', function () {
+      expect(wrapper.find(PeriodMultipleControls)).to.have.lengthOf(1)
+    })
+
+    it('should pass periodMultiple and setPeriodMultiple', function () {
+      const periodMultipleControls = wrapper.find(PeriodMultipleControls)
+      expect(periodMultipleControls.props().periodMultiple).to.equal(wrapper.props().periodMultiple)
+      expect(periodMultipleControls.props().setPeriodMultiple).to.equal(wrapper.props().setPeriodMultiple)
+    })
+  })
+
+  describe('phase focus controls', function () {
+    let wrapper
+    before(function () {
+      wrapper = mount(
+        <Controls data={variableStar.scatterPlot.data} phaseFocusedSeries={1} setSeriesPhaseFocus={sinon.spy()} />,
+        {
+          wrappingComponent: Grommet,
+          wrappingComponentProps: { theme: zooTheme }
+        }
+      )
+    })
+
+    it('should render PhaseFocusControls', function () {
+      expect(wrapper.find(PhaseFocusControls)).to.have.lengthOf(1)
+    })
+
+    it('should pass the data, phaseFocusedSeries, and setSeriesPhaseFocus', function () {
+      const phaseFocusControls = wrapper.find(PhaseFocusControls)
+      expect(phaseFocusControls.props().data).to.deep.equal(wrapper.props().data)
+      expect(phaseFocusControls.props().phaseFocusedSeries).to.equal(wrapper.props().phaseFocusedSeries)
+      expect(phaseFocusControls.props().setSeriesPhaseFocus).to.equal(wrapper.props().setSeriesPhaseFocus)
+    })
+  })
 
   describe('visible series checkbox controls', function () {
-    it('should render VisibilitySeriesCheckBoxes', function () {
-      const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls data={variableStar.data} visibleSeries={visibleSeriesMock} setSeriesVisibility={sinon.spy()} />
-        </Grommet>
+    let wrapper
+    before(function () {
+      wrapper = mount(
+        <Controls data={variableStar.scatterPlot.data} visibleSeries={visibleSeriesMock} setSeriesVisibility={sinon.spy()} />,
+        {
+          wrappingComponent: Grommet,
+          wrappingComponentProps: { theme: zooTheme }
+        }
       )
+    })
+    it('should render VisibilitySeriesCheckBoxes', function () {
       expect(wrapper.find(VisibilitySeriesCheckBoxes)).to.have.lengthOf(1)
     })
 
     it('should pass the data, visibleSeries, and setSeriesVisibility props', function () {
-      const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls data={variableStar.scatterPlot.data} visibleSeries={visibleSeriesMock} setSeriesVisibility={sinon.spy()} />
-        </Grommet>
-      )
-      const controls = wrapper.find(Controls)
       const visibilityControls = wrapper.find(VisibilitySeriesCheckBoxes)
-      expect(visibilityControls.props().data).to.deep.equal(controls.props().data)
-      expect(visibilityControls.props().visibleSeries).to.deep.equal(controls.props().visibleSeries)
-      expect(visibilityControls.props().setSeriesVisibility).to.deep.equal(controls.props().setSeriesVisibility)
-    })
-  })
-
-  describe('Period Multiple Controls', function () {
-    it('should render four multiple options', function () {
-      const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls />
-        </Grommet>
-      )
-
-      const radioInputs = wrapper.find('input[type="radio"]')
-      expect(radioInputs).to.have.lengthOf(4)
-    })
-
-    it('should set the checked state radio button inputs using the periodMultiple prop', function () {
-      const periodMultiple = 2
-      const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls periodMultiple={periodMultiple} />
-        </Grommet>
-      )
-      const checkedRadioInput = wrapper.find('input[type="radio"]').find({ value: periodMultiple.toString() })
-      expect(checkedRadioInput.props().checked).to.be.true()
-    })
-
-    it('should call setPeriodMultiple for the onChange event', function () {
-      const setPeriodMultipleSpy = sinon.spy()
-      const wrapper = mount(
-        <Grommet theme={zooTheme}>
-          <Controls setPeriodMultiple={setPeriodMultipleSpy} />
-        </Grommet>
-      )
-      const radioInputs = wrapper.find('input[type="radio"]') 
-      radioInputs.forEach((input) => {
-        input.simulate('change')
-        expect(setPeriodMultipleSpy).to.have.been.calledOnce()
-        setPeriodMultipleSpy.resetHistory()
-      })
+      expect(visibilityControls.props().data).to.deep.equal(wrapper.props().data)
+      expect(visibilityControls.props().visibleSeries).to.deep.equal(wrapper.props().visibleSeries)
+      expect(visibilityControls.props().setSeriesVisibility).to.deep.equal(wrapper.props().setSeriesVisibility)
     })
   })
 })
