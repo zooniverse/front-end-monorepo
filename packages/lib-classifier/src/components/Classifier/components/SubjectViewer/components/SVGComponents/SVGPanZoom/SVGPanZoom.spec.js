73e132048f1487ae3168e52f553093cf7e067c15
Sarah Allen <sarah@zooniverse.org>
2020-07-28 10:47:21 -0500

    Fix tests

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/SVGPanZoom/SVGPanZoom.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/SVGPanZoom/SVGPanZoom.spec.js
@@ -51,14 +51,14 @@ describe('Components > SVGPanZoom', function () {
   })
 
   it('should enable horizontal panning', function () {
-    onPan('', { dx: -1, dy: 0 })
+    onPan(-1, 0)
     wrapper.update()
     const viewBox = wrapper.find('svg').prop('viewBox')
     expect(viewBox).to.equal('10 0 400 200')
   })
 
   it('should enable vertical panning', function () {
-    onPan('', { dx: 0, dy: -1 })
+    onPan(0, -1)
     wrapper.update()
     const viewBox = wrapper.find('svg').prop('viewBox')
     expect(viewBox).to.equal('0 -10 400 200')
@@ -99,7 +99,7 @@ describe('Components > SVGPanZoom', function () {
   })
 
   it('should reset pan with new src', function () {
-    onPan('', { dx: -1, dy: 0 })
+    onPan(-1, 0)
     wrapper.update()
     let viewBox = wrapper.find('svg').prop('viewBox')
     expect(viewBox).to.equal('10 0 400 200')
