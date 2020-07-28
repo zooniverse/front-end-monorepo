01623bb2a4d49ad9950ce20376066e90374caf5d
Jim O'Donnell <jim@zooniverse.org>
2020-07-28 16:49:07 +0100

    Pass workflow and subject set ID as page props

--- packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/index.js
@@ -1,1 +1,7 @@
 export { default } from '@screens/ClassifyPage'
+
+export function getServerSideProps({ params, req, resp }) {
+  const { workflowID } = params
+  const props = { workflowID }
+  return ({ props })
+}
--- packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/index.js
@@ -1,1 +1,7 @@
 export { default } from '@screens/ClassifyPage'
+
+export function getServerSideProps({ params, req, resp }) {
+  const { subjectSetID, workflowID } = params
+  const props = { subjectSetID, workflowID }
+  return ({ props })
+}
