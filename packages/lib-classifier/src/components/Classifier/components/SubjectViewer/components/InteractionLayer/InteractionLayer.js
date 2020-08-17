91c14a942e73603d81048f3fcb11f580ebd76f20
srallen <srallen@users.noreply.github.com>
2020-08-17 14:31:49 -0500

    Make mark UX consistent and fix associated keyboard events (#1738)
    * Make UX consistent. Fix onfocus and onblur events for mark selection
    
    * Use green for active lines
    
    * Wrap DeleteButton in observer. Increase stroke width. Switch to green

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -94,6 +94,10 @@ function InteractionLayer ({
     }
   }
 
+  function inactivateMark () {
+    setActiveMark(undefined)
+  }
+
   return (
     <g
       onPointerMove={onPointerMove}
@@ -115,9 +119,8 @@ function InteractionLayer ({
         <DrawingToolMarks
           activeMark={activeMark}
           marks={marks}
-          onDelete={() => {
-            setActiveMark(undefined)
-          }}
+          onDelete={inactivateMark}
+          onDeselectMark={inactivateMark}
           onFinish={onFinish}
           onSelectMark={mark => setActiveMark(mark)}
           onMove={(mark, difference) => mark.move(difference)}
