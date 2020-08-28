0d24e06fa57d35a66e53ceda75e7da9719fcdced
Sarah Allen <sarah@zooniverse.org>
2020-08-28 12:26:26 -0500

    Add viewer types to workflow configuration model

--- packages/lib-classifier/src/store/Workflow/WorkflowConfiguration/WorkflowConfiguration.js
+++ packages/lib-classifier/src/store/Workflow/WorkflowConfiguration/WorkflowConfiguration.js
@@ -5,7 +5,14 @@ const WorkflowConfiguration = types.model({
   enable_switching_flipbook_and_separate: types.optional(types.boolean, false),
   hide_classification_summaries: types.optional(types.boolean, false),
   multi_image_mode: types.optional(types.enumeration('multiImageMode', ['flipbook', 'separate']), 'flipbook'),
-  subject_viewer: types.maybe(types.enumeration('subjectViewer', ['lightcurve', 'multiFrame', 'subjectGroup'])),
+  subject_viewer: types.maybe(types.enumeration('subjectViewer', [
+    'dataImage',
+    'lightcurve',
+    'multiFrame',
+    'singleImage',
+    'subjectGroup',
+    'variableStar'
+  ])),
   subject_viewer_config: types.frozen({})
 })
   .views(self => ({
