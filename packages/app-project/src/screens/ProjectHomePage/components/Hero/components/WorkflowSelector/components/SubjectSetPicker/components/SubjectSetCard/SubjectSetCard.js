e5d1fbfad64201b02470062dcd0d782e7e96f7a7
Jim O'Donnell <jim@zooniverse.org>
2020-09-03 14:15:07 +0100

    Add preview images Request the first subject for each subject set and add it to the subject set card as a preview image.

--- packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/SubjectSetPicker/components/SubjectSetCard/SubjectSetCard.js
+++ packages/app-project/src/screens/ProjectHomePage/components/Hero/components/WorkflowSelector/components/SubjectSetPicker/components/SubjectSetCard/SubjectSetCard.js
@@ -1,15 +1,35 @@
+import { panoptes } from '@zooniverse/panoptes-js'
 import { Media, SpacedText } from '@zooniverse/react-components'
 import { Box, Paragraph } from 'grommet'
 import getConfig from 'next/config'
 import { number, string } from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
+
+async function previewImage (subjectSetID) {
+  const subject = await panoptes
+    .get('/set_member_subjects', {
+      subject_set_id: subjectSetID,
+      include: 'subject',
+      page_size: 1
+    })
+    .then(response => {
+      const { linked } = response.body
+      const [subject] = linked.subjects
+      return subject
+    })
+  return subject
+}
 
 function SubjectSetCard (props) {
-  const { display_name, set_member_subjects_count } = props
+  const { display_name, id, set_member_subjects_count } = props
+  const [ subject, setSubject ] = useState(null)
   const { publicRuntimeConfig = {} } = getConfig() || {}
   const assetPrefix = publicRuntimeConfig.assetPrefix || ''
   const placeholderUrl = `${assetPrefix}/subject-placeholder.png`
-  const subjectURL = placeholderUrl
+  previewImage(id)
+    .then(subject => setSubject(subject))
+  const subjectURLs = subject ? subject.locations.map(location => Object.values(location)[0]) : []
+  const alt = subject ? `Subject ${subject.id}` : 'Loading'
 
   return (
     <Box
@@ -23,10 +43,10 @@ function SubjectSetCard (props) {
         width='100%'
       >
         <Media
-          alt={`placeholder subject`}
+          alt={alt}
           height={700}
           placeholder={<img alt='' role='presentation' src={placeholderUrl} />}
-          src={subjectURL}
+          src={subjectURLs[0]}
           width={700}
         />
       </Box>
