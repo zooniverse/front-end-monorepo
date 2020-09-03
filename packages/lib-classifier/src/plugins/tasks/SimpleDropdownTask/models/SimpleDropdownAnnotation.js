5aeff81b7c043d895d1988f5ed8c992bbdb4c952
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-03 01:06:01 +0100

    Refine SimpleDropdownAnnotation model

--- packages/lib-classifier/src/plugins/tasks/SimpleDropdownTask/models/SimpleDropdownAnnotation.js
+++ packages/lib-classifier/src/plugins/tasks/SimpleDropdownTask/models/SimpleDropdownAnnotation.js
@@ -1,11 +1,18 @@
 import { types } from 'mobx-state-tree'
 import Annotation from '../../models/Annotation'
 
+const presetOption = types.model('SimpleDropdownPresetOption', {
+  selection: types.number,  // Index of the selection chosen by the user
+  option: types.literal(true),
+})
+
+const otherOption = types.model('SimpleDropdownOtherOption', {
+  selection: types.string,  // Free answer typed in by the user
+  option: types.literal(false),
+})
+
 const SimpleDropdown = types.model('SimpleDropdown', {
-  value: types.maybeNull(types.model({
-    selection: types.union(types.number, types.string),  // Index of the selection chosen by the user
-    option: types.boolean,  // i.e. is this value in the list of preset options?
-  }), null),
+  value: types.maybeNull(types.union(presetOption, otherOption), null),
 })
   .views(self => ({
     get isComplete () {
