868223d9517e1dfc73a1819e45df7dad83d71ed7
Jim O'Donnell <jim@zooniverse.org>
2020-09-23 13:58:51 +0100

    Pass the API environment to workflow requests

--- packages/app-project/pages/projects/[owner]/[project]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/index.js
@@ -3,15 +3,18 @@ import fetchWorkflowsHelper from '@helpers/fetchWorkflowsHelper'
 import getDefaultPageProps from '@helpers/getDefaultPageProps'
 export { default } from '@screens/ProjectHomePage'
 
-async function getProjectWorkflows(project) {
-  const workflows = await fetchWorkflowsHelper('en', project.links['active_workflows'], project.configuration['default_workflow'])
+async function getProjectWorkflows(project, env) {
+  const language = 'en'
+  const { active_workflows, default_workflow } = project.links
+  const workflows = await fetchWorkflowsHelper(language, active_workflows, default_workflow, env)
   return workflows
 }
 
 export async function getServerSideProps({ params, query, req, res }) {
   const { props: defaultProps } = await getDefaultPageProps({ params, query, req, res })
   const { project } = defaultProps.initialState
-  const workflows = await getProjectWorkflows(project)
+  const { env } = query
+  const workflows = await getProjectWorkflows(project, env)
   const props = Object.assign({}, { workflows }, defaultProps)
   return ({ props })
 }
