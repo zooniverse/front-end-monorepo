c2cd00bf1a6c03c070c345246eaf7908479de568
Jim O'Donnell <jim@zooniverse.org>
2020-09-25 10:25:37 +0100

    Tidy up getServerSideProps Remove the getProjectWorkflowsHelper. Get the workflow language from the project.

--- packages/app-project/pages/projects/[owner]/[project]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/index.js
@@ -3,18 +3,13 @@ import fetchWorkflowsHelper from '@helpers/fetchWorkflowsHelper'
 import getDefaultPageProps from '@helpers/getDefaultPageProps'
 export { default } from '@screens/ProjectHomePage'
 
-async function getProjectWorkflows(project, env) {
-  const language = 'en'
-  const { active_workflows, default_workflow } = project.links
-  const workflows = await fetchWorkflowsHelper(language, active_workflows, default_workflow, env)
-  return workflows
-}
-
 export async function getServerSideProps({ params, query, req, res }) {
   const { props: defaultProps } = await getDefaultPageProps({ params, query, req, res })
   const { project } = defaultProps.initialState
   const { env } = query
-  const workflows = await getProjectWorkflows(project, env)
+  const language = project.primary_language
+  const { active_workflows, default_workflow } = project.links
+  const workflows = await fetchWorkflowsHelper(language, active_workflows, default_workflow, env)
   const props = Object.assign({}, { workflows }, defaultProps)
   return ({ props })
 }
