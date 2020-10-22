30a8516cb0d0d5b827da00b6b011e058f8f611b6
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2020-10-22 09:35:26 +0100

    Classifier Dropdown Task (v3) - Simple Dropdown (#1772)
    This PR adds a Simple Dropdown (type: dropdown-simple) task to the monorepo Classifier.
    The Simple Dropdown functions like a HTML <select> which allows a single value to be selected
    The Simple Dropdown is separate from the classic Dropdown (type: dropdown) from PFE
    At the moment, the dropdown-simple task type cannot be built in the PFE Project Builder, and has to be set up by a dev.
    
    * Classifier: Simple dropdown task adapter (#1830)
    
    This adds an adapter (and tests) for the simple dropdown task, so that we can test it out in staging. Dropdown tasks are converted to simple dropdown tasks if they contain only one select menu. More complex tasks are not transformed, and should throw an error in the classifier.
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>

---
+++ packages/lib-classifier/src/plugins/tasks/SimpleDropdownTask/models/SimpleDropdownTask.js
@@ -0,0 +1,33 @@
+import cuid from 'cuid'
+import { types } from 'mobx-state-tree'
+import { panoptesAdapter } from './helpers'
+import Task from '../../models/Task'
+import SimpleDropdownAnnotation from './SimpleDropdownAnnotation'
+
+// TODO: should we make question/instruction consistent between task types?
+// What should be it called? I think we should use 'instruction'
+
+const SimpleDropdown = types.model('SimpleDropdown', {
+  allowCreate: types.optional(types.boolean, false),
+  annotation: types.safeReference(SimpleDropdownAnnotation),
+  help: types.optional(types.string, ''),
+  instruction: types.optional(types.string, ''),
+  options: types.array(types.string),
+  type: types.literal('dropdown-simple'),
+})
+  .preProcessSnapshot(snapshot => {
+    return panoptesAdapter(snapshot)
+  })
+  .views(self => ({
+    get defaultAnnotation () {
+      return SimpleDropdownAnnotation.create({
+        id: cuid(),
+        task: self.taskKey,
+        taskType: self.type
+      })
+    }
+  }))
+
+const SimpleDropdownTask = types.compose('SimpleDropdownTask', Task, SimpleDropdown)
+
+export default SimpleDropdownTask
