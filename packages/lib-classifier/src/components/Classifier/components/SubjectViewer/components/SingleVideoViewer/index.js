59231b957faa300c9ebbd70afc548934246ccc7c
ErikOstlund <eostlund74@gmail.com>
2020-11-05 15:02:29 -0600

    Created SingleVideoViewer Component
    Just copied SingleImageViewer. Will be going through and updating var names and removing unnecessary code later.
    Also added SingleVideoViewer to getViewer.js

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/index.js
@@ -0,0 +1,3 @@
+export { default } from './SingleImageViewerConnector'
+export { default as SingleImageViewerContainer } from './SingleImageViewerContainer'
+export { default as SingleImageViewer } from './SingleImageViewer'
