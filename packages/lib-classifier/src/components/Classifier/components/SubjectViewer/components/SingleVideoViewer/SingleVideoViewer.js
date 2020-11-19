66e5871d7f4fdb69a4510b2a70115d1cae2eb0ba
ErikOstlund <eostlund74@gmail.com>
2020-11-19 15:49:28 -0600

    Set up SingleVideoViewer
    with the react-player component and basic settings. Will be adding to this later.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.js
@@ -1,9 +1,7 @@
+import React from 'react'
 import PropTypes from 'prop-types'
-import React, { createRef, forwardRef, useContext } from 'react'
 import styled from 'styled-components'
-import SVGContext from '@plugins/drawingTools/shared/SVGContext'
-
-import InteractionLayer from '../InteractionLayer'
+import ReactPlayer from 'react-player';
 
 const Container = styled.div`
   animation: fadein 1s 0s forwards;
@@ -22,76 +20,26 @@ const Container = styled.div`
   }
 `
 
-const SingleImageViewer = forwardRef(function SingleImageViewer(props, ref) {
-  const {
-    children,
-    enableInteractionLayer,
-    height,
-    onKeyDown,
-    rotate,
-    scale,
-    title,
-    viewBox,
-    width,
-    ...rest
-  } = props
+const SingleVideoViewer = ({
+  isPlaying,
+	url
+}) =>(
+  <Container>
+    <ReactPlayer
+      playing={ isPlaying }
+      url={ url }
+ />
+  </Container>
+)
 
-  const transformLayer = createRef()
-  const { svg } = useContext(SVGContext)
-  const transform = `rotate(${rotate} ${width / 2} ${height / 2 })`
-  const getScreenCTM = () => transformLayer.current.getScreenCTM()
-
-  return (
-    <SVGContext.Provider value={{ svg, getScreenCTM }}>
-      <Container>
-        <svg
-          ref={ref}
-          focusable
-          onKeyDown={onKeyDown}
-          tabIndex={0}
-          viewBox={viewBox}
-          {...rest}
-        >
-          {title?.id && title?.text &&
-            <title id={title.id}>{title.text}</title>}
-          <g
-            ref={transformLayer}
-            transform={transform}
-          >
-            {children}
-            {enableInteractionLayer &&
-              <InteractionLayer
-                scale={scale}
-                height={height}
-                width={width}
-              />}
-          </g>
-        </svg>
-      </Container>
-    </SVGContext.Provider>
-  )
-})
-
-SingleImageViewer.propTypes = {
-  enableInteractionLayer: PropTypes.bool,
-  height: PropTypes.number.isRequired,
-  onKeyDown: PropTypes.func,
-  rotate: PropTypes.number,
-  scale: PropTypes.number,
-  title: PropTypes.shape({
-    id: PropTypes.string,
-    text: PropTypes.string
-  }),
-  viewBox: PropTypes.string.isRequired,
-  width: PropTypes.number.isRequired
+SingleVideoViewer.propTypes = {
+  isPlaying: PropTypes.bool,
+	url: PropTypes.string
 }
 
-SingleImageViewer.defaultProps = {
-  enableInteractionLayer: true,
-  onKeyDown: () => true,
-  rotate: 0,
-  scale: 1,
-  title: {}
+SingleVideoViewer.defaultProps = {
+  isPlaying: false,
+	url: ''
 }
 
-export default SingleImageViewer
+export default SingleVideoViewer
