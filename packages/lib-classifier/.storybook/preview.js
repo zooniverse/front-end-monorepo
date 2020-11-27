a273b8392fd8c4284771082a3354cb4dd905cc00
Jim O'Donnell <jim@zooniverse.org>
2020-11-27 10:09:14 +0000

    Migrate lib-classifier config

---
+++ packages/lib-classifier/.storybook/preview.js
@@ -0,0 +1,15 @@
+import { addDecorator, addParameters } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+import backgrounds from './lib/backgrounds'
+
+addParameters({
+  backgrounds: {
+    values: backgrounds.lightDefault
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+})
+addDecorator(withA11y)
