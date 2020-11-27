22a988f26f189694d741f8979b52141d2b76f049
Jim O'Donnell <jim@zooniverse.org>
2020-11-27 10:25:56 +0000

    Migrate app-project config

---
+++ packages/app-project/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-notes',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource'
+  ]
+};
