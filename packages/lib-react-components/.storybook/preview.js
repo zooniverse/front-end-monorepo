2490b393b26194f9e50312fa4e061284b2880b9e
Jim O'Donnell <jim@zooniverse.org>
2020-11-27 10:02:05 +0000

    Migrate lib-react-components

---
+++ packages/lib-react-components/.storybook/preview.js
@@ -0,0 +1,16 @@
+import { addDecorator, addParameters } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+import backgrounds from './lib/backgrounds'
+
+addParameters({
+  backgrounds: {
+    values: backgrounds.lightDefault
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+})
+addDecorator(withA11y)
+
