36cb9af6c4a06afd03371205e4d96a84ae44c26c
Jim O'Donnell <jim@zooniverse.org>
2020-12-06 13:35:13 +0000

    Install Storybook Essentials

--- packages/lib-classifier/.storybook/main.js
+++ packages/lib-classifier/.storybook/main.js
@@ -1,12 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-backgrounds',
+    '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@storybook/addon-links',
     '@storybook/addon-notes',
-    '@storybook/addon-viewport',
     '@storybook/addon-a11y'
   ]
 };
