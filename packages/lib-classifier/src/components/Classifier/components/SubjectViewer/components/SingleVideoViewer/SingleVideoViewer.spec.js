bfb70a015c82b5ffb56ad6cef63cfdc3859e3e51
ErikOstlund <eostlund74@gmail.com>
2020-12-07 16:00:41 -0600

    SingleVideoViewer Unit Test
    Just testing if component renders properly.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.spec.js
@@ -1,46 +1,18 @@
-// import { shallow } from 'enzyme'
-// import React from 'react'
+import React from 'react'
+import { shallow } from 'enzyme'
 
-// import SingleImageViewer from './SingleImageViewer'
-// import InteractionLayer from '../InteractionLayer'
+import SingleVideoViewer from './SingleVideoViewer'
 
-// let wrapper
+let wrapper
 
-// describe('Component > SingleImageViewer', function () {
-//   beforeEach(function () {
-//     wrapper = shallow(<SingleImageViewer height={200} width={100} viewBox='0 0 100 100' />)
-//   })
+describe('Component > SingleVideoViewer', function() {
+	beforeEach(function() {
+		wrapper = shallow(
+			<SingleVideoViewer height={200} width={100} viewBox='0 0 100 100' />
+		)
+	})
 
-//   it('should render without crashing', function () {
-//     expect(wrapper).to.be.ok()
-//   })
-
-//   it('should be upright', function () {
-//     const transform = wrapper.find('svg').find('g').prop('transform')
-//     expect(transform).to.have.string('rotate(0 50 100)')
-//   })
-
-//   describe('with a rotation angle', function () {
-//     beforeEach(function () {
-//       wrapper.setProps({ rotate: -90 })
-//     })
-
-//     it('should be rotated', function () {
-//       const transform = wrapper.find('svg').find('g').prop('transform')
-//       expect(transform).to.have.string('rotate(-90 50 100)')
-//     })
-//   })
-
-//   describe('with interaction layer', function () {
-//     it('should default to render the InteractionLayer', function () {
-//       expect(wrapper.find(InteractionLayer)).to.have.lengthOf(1)
-//     })
-
-//     it('should be possible to disable the render of the InteractionLayer by prop', function () {
-//       expect(wrapper.find(InteractionLayer)).to.have.lengthOf(1)
-//       wrapper.setProps({ enableInteractionLayer: false })
-//       expect(wrapper.find(InteractionLayer)).to.have.lengthOf(0)
-//       wrapper.setProps({ enableInteractionLayer: true })
-//     })
-//   })
-// })
+	it('should render without crashing', function() {
+		expect(wrapper).to.be.ok()
+	})
+})
