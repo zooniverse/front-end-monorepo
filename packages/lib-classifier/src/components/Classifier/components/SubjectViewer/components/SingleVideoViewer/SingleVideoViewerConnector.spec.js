10ceebe03f62e48df68eb299d3a5ca3dd706a0d6
ErikOstlund <eostlund74@gmail.com>
2020-12-07 18:15:19 -0600

    SingleVideoViewerConnector Unit Test
    Updated to simply test render and prop passing.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewerConnector.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewerConnector.spec.js
@@ -1,63 +1,36 @@
-// import { shallow } from 'enzyme'
-// import React from 'react'
-// import sinon from 'sinon'
-// import SingleVideoViewerConnector from './SingleVideoViewerConnector'
-// import SingleVideoViewerContainer from './SingleVideoViewerContainer'
+import { shallow } from 'enzyme'
+import React from 'react'
+import sinon from 'sinon'
+import SingleVideoViewerConnector from './SingleVideoViewerConnector'
+import SingleVideoViewerContainer from './SingleVideoViewerContainer'
 
-// const mockStore = {
-//   classifierStore: {
-//     subjects: {
-//       active: { id: '1' }
-//     },
-//     subjectViewer: {
-//       enableRotation: sinon.spy(),
-//       move: false,
-//       rotation: 0,
-//       setOnPan: sinon.spy(),
-//       setOnZoom: sinon.spy()
-//     }
-//   }
-// }
+const mockStore = {
+	classifierStore: {
+		subjects: {
+			active: { id: '1' }
+		}
+	}
+}
 
-// describe('SingleVideoViewerConnector', function () {
-//   let wrapper, useContextMock, containerProps
-//   before(function () {
-//     useContextMock = sinon.stub(React, 'useContext').callsFake(() => mockStore)
-//     wrapper = shallow(
-//       <SingleVideoViewerConnector />
-//     )
-//     containerProps = wrapper.find(SingleVideoViewerContainer).props()
-//   })
+describe('SingleVideoViewerConnector', function() {
+	let wrapper, useContextMock, containerProps
+	before(function() {
+		useContextMock = sinon.stub(React, 'useContext').callsFake(() => mockStore)
+		wrapper = shallow(<SingleVideoViewerConnector />)
+		containerProps = wrapper.find(SingleVideoViewerContainer).props()
+	})
 
-//   after(function () {
-//     useContextMock.restore()
-//   })
+	after(function() {
+		useContextMock.restore()
+	})
 
-//   it('should render without crashing', function () {
-//     expect(wrapper).to.be.ok()
-//   })
+	it('should render without crashing', function() {
+		expect(wrapper).to.be.ok()
+	})
 
-//   it('should pass the active subject as a prop', function () {
-//     expect(containerProps.subject).to.deep.equal(mockStore.classifierStore.subjects.active)
-//   })
-
-//   it('should pass the enableRotation function', function () {
-//     expect(containerProps.enableRotation).to.deep.equal(mockStore.classifierStore.subjectViewer.enableRotation)
-//   })
-
-//   it('should pass the move boolean', function () {
-//     expect(containerProps.move).to.deep.equal(mockStore.classifierStore.subjectViewer.move)
-//   })
-
-//   it('should pass the rotation boolean', function () {
-//     expect(containerProps.rotation).to.deep.equal(mockStore.classifierStore.subjectViewer.rotation)
-//   })
-
-//   it('should pass the setOnPan function', function () {
-//     expect(containerProps.setOnPan).to.deep.equal(mockStore.classifierStore.subjectViewer.setOnPan)
-//   })
-
-//   it('should pass the setOnZoom function', function () {
-//     expect(containerProps.setOnZoom).to.deep.equal(mockStore.classifierStore.subjectViewer.setOnZoom)
-//   })
-// })
+	it('should pass the active subject as a prop', function() {
+		expect(containerProps.subject).to.deep.equal(
+			mockStore.classifierStore.subjects.active
+		)
+	})
+})
