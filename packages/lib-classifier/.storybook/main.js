dc114e37585d1233b4c3fca8e95592bb9749ed4c
Jim O'Donnell <jim@zooniverse.org>
2020-12-14 17:14:09 +0000

    [Security] Remove storybook/addon-notes (#1942)
    * [Security] Remove storybook/addon-notes
    Uninstall storybook/addon-notes. It's deprecated, in favour of addon-docs, and doesn't seem to be receiving security patches.
    
    * Update the Markdownz stories
    Change `notes.markdown` to `docs.description.component`.

--- packages/lib-classifier/.storybook/main.js
+++ packages/lib-classifier/.storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@storybook/addon-links',
-    '@storybook/addon-notes',
     '@storybook/addon-a11y'
   ]
 };
