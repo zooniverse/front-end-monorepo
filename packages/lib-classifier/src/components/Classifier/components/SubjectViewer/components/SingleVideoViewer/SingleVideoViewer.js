5af82ce983952d2ee1f9edf8ffaea08c94f1f26e
ErikOstlund <eostlund74@gmail.com>
2020-12-29 10:15:05 -0600

    Made Video Viewer responsive
    and some prettier formatting fixes: fixed tab spacing
    
    # Conflicts:
    #	packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.js

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleVideoViewer/SingleVideoViewer.js
@@ -5,9 +5,14 @@ import ReactPlayer from 'react-player'
 
 const Container = styled.div`
   animation: fadein 1s 0s forwards;
-  height: 100%;
-  overflow: hidden;
-  width: 100%;
+  position: relative;
+  padding-top: 56.25%;
+
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 
   @keyframes fadein {
     from {
@@ -22,7 +27,13 @@ const Container = styled.div`
 
 const SingleVideoViewer = ({ url }) => (
   <Container>
-    <ReactPlayer controls={true} url={url} />
+    <ReactPlayer
+      className='react-player'
+      controls={true}
+      url={url}
+      width='100%'
+      height='100%'
+    />
   </Container>
 )
 
