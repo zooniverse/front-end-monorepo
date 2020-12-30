7eaca72b6bd97e859e05f4fd37685f0b714aca40
ErikOstlund <eostlund74@gmail.com>
2020-12-30 11:56:34 -0600

    Controller layout work
    Interaction controlls on the left and video time info on the right.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VideoController/VideoController.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VideoController/VideoController.js
@@ -3,6 +3,18 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { ChapterPrevious, PlayFill, PauseFill } from 'grommet-icons'
 
+const ControllerWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  align-items: center;
+`
+
+const ButtonGroup = styled.div`
+  /* display: grid;
+  grid-template-rows: auto;
+  align-items: center; */
+`
+
 const Button = styled.button`
   border: none;
   outline: none;
@@ -16,23 +28,31 @@ const SpeedButton = styled(Button)`
   }
 `
 
+const TimeWrapper = styled.div`
+  justify-self: end;
+  margin-right: 10px;
+`
+
 const VideoController = ({
   onRewind,
   isPlaying,
   onPlayPause,
   onSpeedChange
 }) => (
-  <div>
-    <Button onClick={onRewind}>
-      <ChapterPrevious />
-    </Button>
-    <Button onClick={onPlayPause}>
-      {isPlaying ? <PauseFill /> : <PlayFill />}
-    </Button>
-    <SpeedButton onClick={() => onSpeedChange(0.25)}>0.25</SpeedButton>
-    <SpeedButton onClick={() => onSpeedChange(0.5)}>0.5</SpeedButton>
-    <SpeedButton onClick={() => onSpeedChange(1)}>1.0</SpeedButton>
-  </div>
+  <ControllerWrapper>
+    <ButtonGroup>
+      <Button onClick={onRewind}>
+        <ChapterPrevious />
+      </Button>
+      <Button onClick={onPlayPause}>
+        {isPlaying ? <PauseFill /> : <PlayFill />}
+      </Button>
+      <SpeedButton onClick={() => onSpeedChange(0.25)}>0.25</SpeedButton>
+      <SpeedButton onClick={() => onSpeedChange(0.5)}>0.5</SpeedButton>
+      <SpeedButton onClick={() => onSpeedChange(1)}>1.0</SpeedButton>
+    </ButtonGroup>
+    <TimeWrapper>Time</TimeWrapper>
+  </ControllerWrapper>
 )
 
 VideoController.propTypes = {
