128283dad23095e569fa49570275b27d9f11ef01
Jim O'Donnell <jim@zooniverse.org>
2021-01-06 15:28:34 +0000

    Default workflow ID to the project default

--- packages/app-project/src/screens/ClassifyPage/getServerSideProps.js
+++ packages/app-project/src/screens/ClassifyPage/getServerSideProps.js
@@ -3,11 +3,14 @@ import getDefaultPageProps from '@helpers/getDefaultPageProps'
 export async function getServerSideProps({ params, query, req, res }) {
   const { subjectSetID, workflowID } = params
   const { props } = await getDefaultPageProps({ params, query, req, res })
+  const { project } = props.initialState
   if (subjectSetID) {
     props.subjectSetID = subjectSetID
   }
   if (workflowID) {
     props.workflowID = workflowID
+  } else {
+    props.workflowID = project?.configuration['default_workflow']
   }
   return ({ props })
 }
