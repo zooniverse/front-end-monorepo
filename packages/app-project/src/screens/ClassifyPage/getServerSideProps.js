31f0c8991398eb7705dd9eb42fa3a865ef875ff7
Jim O'Donnell <jim@zooniverse.org>
2021-01-06 14:35:06 +0000

    Conditionally assign optional props workflowID and subjectSetID may not be present in the classify page URL, so assign them conditionally.

--- packages/app-project/src/screens/ClassifyPage/getServerSideProps.js
+++ packages/app-project/src/screens/ClassifyPage/getServerSideProps.js
@@ -2,7 +2,12 @@ import getDefaultPageProps from '@helpers/getDefaultPageProps'
 
 export async function getServerSideProps({ params, query, req, res }) {
   const { subjectSetID, workflowID } = params
-  const { props: defaultProps } = await getDefaultPageProps({ params, query, req, res })
-  const props = Object.assign({}, { subjectSetID, workflowID }, defaultProps)
+  const { props } = await getDefaultPageProps({ params, query, req, res })
+  if (subjectSetID) {
+    props.subjectSetID = subjectSetID
+  }
+  if (workflowID) {
+    props.workflowID = workflowID
+  }
   return ({ props })
 }
