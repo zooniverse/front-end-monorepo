d45d9e220bfd95eab417c2661422ae5f8cb6bbbc
ErikOstlund <eostlund74@gmail.com>
2021-01-08 11:28:09 -0600

    Removed most styled components
    and replaced with Grommet Box component. Have basic layout in place.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VideoController/VideoController.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/VideoController/VideoController.js
@@ -1,25 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { Box } from 'grommet'
 import { CirclePlay, PauseFill } from 'grommet-icons'
 import FormattedTime from './FormattedTime'
 import Slider from './Slider'
-
-const ControllerWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 0.5fr 0.5fr 2fr 0.5fr;
-  gap: 5px 5px;
-  /* display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  align-items: center; */
-`
-
-const ButtonGroup = styled.div`
-  border: 2px solid red;
-  /* display: grid;
-  grid-template-rows: auto;
-  align-items: center; */
-`
 
 const Button = styled.button`
   border: none;
@@ -27,25 +12,11 @@ const Button = styled.button`
   cursor: pointer;
 `
 
-const SliderWrapper = styled.div`
-  border: 2px solid blue;
-`
-
 const SpeedButton = styled(Button)`
   &:focus {
     font-weight: 700;
     color: #00979d;
   }
-`
-
-const TimeWrapper = styled.div`
-  border: 2px solid green;
-  /* justify-self: end; */
-  /* margin-right: 10px; */
-`
-
-const VideoSpeed = styled.div`
-  border: 2px solid orchid;
 `
 
 const VideoController = ({
@@ -59,31 +30,41 @@ const VideoController = ({
   onSliderChange
 }) => {
   return (
-    <ControllerWrapper>
-      <ButtonGroup>
-        <Button onClick={onPlayPause}>
-          {isPlaying ? <PauseFill /> : <CirclePlay />}
-        </Button>
-      </ButtonGroup>
-      <TimeWrapper>
-        <FormattedTime seconds={played * duration} />
-        {' / '}
-        <FormattedTime seconds={duration} />
-      </TimeWrapper>
-      <SliderWrapper>
+    <Box>
+      <Box border={{ color: 'teal', size: 'small' }}>
         <Slider
           played={played}
           onMouseUp={onSliderMouseUp}
           onMouseDown={onSliderMouseDown}
           onChange={onSliderChange}
         />
-      </SliderWrapper>
-      <VideoSpeed>
-        <SpeedButton onClick={() => onSpeedChange(0.25)}>0.25</SpeedButton>
-        <SpeedButton onClick={() => onSpeedChange(0.5)}>0.5</SpeedButton>
-        <SpeedButton onClick={() => onSpeedChange(1)}>1.0</SpeedButton>
-      </VideoSpeed>
-    </ControllerWrapper>
+      </Box>
+
+      <Box
+        direction='row'
+        justify='between'
+        border={{ color: 'black', size: 'small' }}
+      >
+        <Box direction='row' border={{ color: 'red', size: 'small' }}>
+          <Button onClick={onPlayPause}>
+            {isPlaying ? <PauseFill /> : <CirclePlay />}
+          </Button>
+          <SpeedButton onClick={() => onSpeedChange(0.25)}>0.25</SpeedButton>
+          <SpeedButton onClick={() => onSpeedChange(0.5)}>0.5</SpeedButton>
+          <SpeedButton onClick={() => onSpeedChange(1)}>1.0</SpeedButton>
+        </Box>
+
+        <Box
+          direction='row'
+          border={{ color: 'blue', size: 'small' }}
+          alignSelf='center'
+        >
+          <FormattedTime seconds={played * duration} />
+          {' / '}
+          <FormattedTime seconds={duration} />
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
