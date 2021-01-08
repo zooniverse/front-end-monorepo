34aca3d694756f0844039c41a13e5115f89687d0
Jim O'Donnell <jim@zooniverse.org>
2021-01-08 16:37:05 +0000

    Add a 301 redirect to /classify Redirect /classify to /classify/workflow/:workflow_id when a project has only one active workflow.

--- packages/app-project/pages/projects/[owner]/[project]/classify/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/classify/index.js
@@ -3,5 +3,12 @@ export { default } from '@screens/ClassifyPage'
 
 export async function getServerSideProps({ params, query, req, res }) {
   const { props } = await getDefaultPageProps({ params, query, req, res })
+  const { env } = query
+  if (props.workflowID) {
+    const redirect = env ? `workflow/${props.workflowID}?env=${env}` : `workflow/${props.workflowID}`
+    res.statusCode = 301
+    res.setHeader('Location', redirect)
+    res.end()
+  }
   return ({ props })
 }
