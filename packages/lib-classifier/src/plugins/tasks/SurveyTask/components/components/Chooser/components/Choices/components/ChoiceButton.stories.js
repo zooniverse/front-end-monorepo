4bcf16d8c1a8b324799280fac900e01aacb74b5b
mcbouslog <mcbouslog@gmail.com>
2021-01-18 12:04:50 -0600

    Add thumbnail variations to ChoiceButton stories

--- packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/Choices/components/ChoiceButton.stories.js
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/Choices/components/ChoiceButton.stories.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 import ChoiceButton from './ChoiceButton'
 
+const CARACAL_SRC = 'https://panoptes-uploads.zooniverse.org/staging/workflow_attached_image/41bddb25-bb8d-4734-88fe-643d88688489.jpeg'
+
 function StoryContext (props) {
   const { children, theme } = props
 
@@ -26,7 +28,13 @@ export default {
   component: ChoiceButton
 }
 
-const Template = ({ choiceId, choiceLabel, dark }) => (
+const Template = ({
+  choiceId,
+  choiceLabel,
+  dark,
+  src,
+  thumbnailSize
+}) => (
   <StoryContext
     theme={{ ...zooTheme, dark }}
   >
@@ -34,6 +42,8 @@ const Template = ({ choiceId, choiceLabel, dark }) => (
       choiceId={choiceId}
       choiceLabel={choiceLabel}
       onChoose={() => console.log(choiceId)}
+      src={src}
+      thumbnailSize={thumbnailSize}
     />
   </StoryContext>
 )
@@ -42,12 +52,43 @@ export const Default = Template.bind({});
 Default.args = {
   choiceId: 'AARDVARK',
   choiceLabel: 'Aardvark',
-  dark: false
+  dark: false,
+  src: '',
+  thumbnailSize: 'none'
 }
 
 export const LongLabel = Template.bind({});
 LongLabel.args = {
   choiceId: 'LONG',
   choiceLabel: 'Long label is really long label',
-  dark: false
+  dark: false,
+  src: '',
+  thumbnailSize: 'none'
+}
+
+export const SmallThumbnail = Template.bind({});
+SmallThumbnail.args = {
+  choiceId: 'CRCL',
+  choiceLabel: 'Caracal',
+  dark: false,
+  src: CARACAL_SRC,
+  thumbnailSize: 'small'
+}
+
+export const MediumThumbnail = Template.bind({});
+MediumThumbnail.args = {
+  choiceId: 'CRCL',
+  choiceLabel: 'Caracal',
+  dark: false,
+  src: CARACAL_SRC,
+  thumbnailSize: 'medium'
+}
+
+export const LargeThumbnail = Template.bind({});
+LargeThumbnail.args = {
+  choiceId: 'CRCL',
+  choiceLabel: 'Caracal',
+  dark: false,
+  src: CARACAL_SRC,
+  thumbnailSize: 'large'
 }
