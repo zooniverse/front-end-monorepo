b9e674822ea0ed8658a7831aaaa7e563c6c40b62
mcbouslog <mcbouslog@gmail.com>
2021-01-19 12:01:28 -0600

    Init add Characteristcs component

---
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/Characteristics/Characteristics.js
@@ -0,0 +1,37 @@
+import { Box, Button } from 'grommet'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import CharacteristicSection from './components/CharacteristicSection'
+
+export default function Characteristics (props) {
+  const { task } = props
+
+  return (
+    <Box
+      fill
+    >
+      {task.characteristicsOrder.map((characteristicId, i) => {
+        const characteristic = task.characteristics[characteristicId]
+
+        return (
+          <CharacteristicSection
+            characteristic={characteristic}
+            images={task.images}
+          />)
+      })}
+      <Button
+        label="Clear filters"
+      />
+    </Box>
+  )
+}
+
+Characteristics.propTypes = {
+  task: PropTypes.shape({
+    help: PropTypes.string,
+    required: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+    taskKey: PropTypes.string,
+    type: PropTypes.string
+  }).isRequired
+}
