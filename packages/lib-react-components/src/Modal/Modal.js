159ef20dd4911bda69d348eea032640924c16d0d
Jim O'Donnell <jim@zooniverse.org>
2021-01-20 12:03:50 +0000

    Add descriptions to Modal props

--- packages/lib-react-components/src/Modal/Modal.js
+++ packages/lib-react-components/src/Modal/Modal.js
@@ -67,13 +67,41 @@ class Modal extends React.Component {
   }
 }
 
+Modal.defaultProps = {
+  active: false,
+  className: '',
+  closeFn: undefined,
+  headingBackground: 'brand',
+  title: '',
+  titleColor: 'neutral-6'
+}
+
 Modal.propTypes = {
+  /**
+    Determines whether the modal is visible or not.
+  */
+  active: PropTypes.bool,
   children: PropTypes.node.isRequired,
+  /**
+    Optional CSS class applied to the modal content.
+  */
   className: PropTypes.string,
+  /**
+    Optional function called when clicking outside the modal, or when the Esc button is pressed. If this is not present, then the close button is not shown and the modal cannot be closed except by taking action in the content eg. pressing a button to continue.
+  */
   closeFn: PropTypes.func,
+  /**
+    The background color for the modal header. It can be set to any CSS color value or color string value from the Zooniverse Grommet theme or an object setting the color for the light and dark theme.
+  */
   headingBackground: PropTypes.string,
   overflow: PropTypes.string,
+  /**
+    string to use as the modal title
+  */
   title: PropTypes.string,
+  /**
+    The color of the title text. It can be set to any CSS color value or color string value from the Zooniverse Grommet theme or an object setting the color for the light and dark theme.
+  */
   titleColor: PropTypes.oneOfType([ PropTypes.object, PropTypes.string ])
 }
 
