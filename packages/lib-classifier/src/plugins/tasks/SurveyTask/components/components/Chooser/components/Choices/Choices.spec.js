4eec9b1103c626ebd260392af983c06014eb2c6d
Sarah Allen <sarah@zooniverse.org>
2021-02-01 12:14:48 -0600

    Finish column count tests

--- packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/Choices/Choices.spec.js
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/Choices/Choices.spec.js
@@ -38,14 +38,85 @@ describe.only('Component > Choices', function () {
   })
 
   describe('when the column count is 3', function () {
-    it('should set the set the number of grid columns to 3', function () {
+    let task, wrapper
+    before(function () {
       task = Task.TaskModel.create({
         choices: mockTask.choices,
         taskKey: 'T0',
         type: 'survey'
       })
+      wrapper = shallow(
+        <Choices
+          filteredChoices={Array.from(mockTask.choicesOrder)}
+          task={task}
+        />
+      )
+    })
 
+    it('should set the set the number of grid columns to 3', function () {
       expect(wrapper.find(Grid).props().columns.count).to.equal(3)
+    })
+  })
+
+  describe('when the column count is 2', function () {
+    let task, wrapper
+    before(function () {
+      let choices = {}
+      let choicesOrder = []
+      Object.keys(mockTask.choices).forEach((choiceKey, index) => {
+        if (index <=5) {
+          choices[choiceKey] = mockTask.choices[index] 
+          choicesOrder.push(mockTask.choicesOrder[index])
+        }
+      })
+
+      task = Task.TaskModel.create({
+        choices,
+        choicesOrder,
+        taskKey: 'T0',
+        type: 'survey'
+      })
+      wrapper = shallow(
+        <Choices
+          filteredChoices={choicesOrder}
+          task={task}
+        />
+      )
+    })
+
+    it('should set the set the number of grid columns to 2', function () {
+      expect(wrapper.find(Grid).props().columns.count).to.equal(2)
+    })
+  })
+
+  describe('when the column count is 1', function () {
+    let task, wrapper
+    before(function () {
+      let choices = {}
+      let choicesOrder = []
+      Object.keys(mockTask.choices).forEach((choiceKey, index) => {
+        if (index <= 4) {
+          choices[choiceKey] = mockTask.choices[index]
+          choicesOrder.push(mockTask.choicesOrder[index])
+        }
+      })
+
+      task = Task.TaskModel.create({
+        choices,
+        choicesOrder,
+        taskKey: 'T0',
+        type: 'survey'
+      })
+      wrapper = shallow(
+        <Choices
+          filteredChoices={choicesOrder}
+          task={task}
+        />
+      )
+    })
+
+    it('should set the set the number of grid columns to 1', function () {
+      expect(wrapper.find(Grid).props().columns.count).to.equal(1)
     })
   })
 })
