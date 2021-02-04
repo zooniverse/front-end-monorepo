cb4835c5fd79ab58cbd6ec55d7e9103f3c0e3f46
Jim O'Donnell <jim@zooniverse.org>
2021-02-04 16:54:12 +0000

    ContentBox: remove client-side links We aren't linking to any client-side pages from the ContentBox component, so this replaces Link with Anchor, which will load a new page in the browser. It also adds tests and fixes a small typo (linkUrl should be linkProps.)

--- packages/app-project/src/shared/components/ContentBox/ContentBox.spec.js
+++ packages/app-project/src/shared/components/ContentBox/ContentBox.spec.js
@@ -1,12 +1,13 @@
 import { shallow } from 'enzyme'
+import { Anchor } from 'grommet'
 import React from 'react'
 
 import { ContentBox } from './ContentBox'
 
-let wrapper
 const Foobar = () => (<div>Foobar</div>)
 
 describe('Component > ContentBox', function () {
+  let wrapper
   before(function () {
     wrapper = shallow(
       <ContentBox theme={{ dark: false }}>
@@ -21,5 +22,32 @@ describe('Component > ContentBox', function () {
 
   it('should render its children', function () {
     expect(wrapper.find(Foobar)).to.have.lengthOf(1)
+  })
+
+  describe('with link props', function () {
+    let wrapper
+
+    before(function () {
+      let linkProps = {
+        href: '/projects/test/project/stats'
+      }
+      wrapper = shallow(
+        <ContentBox
+          theme={{ dark: false }}
+          linkLabel='View more stats'
+          linkProps={linkProps}
+          >
+          <Foobar />
+        </ContentBox>
+      )
+    })
+
+    it('should render a link anchor', function () {
+      expect(wrapper.find(Anchor)).to.have.lengthOf(1)
+    })
+
+    it('should link to the specified href', function () {
+      expect(wrapper.find(Anchor).prop('href')).to.equal('/projects/test/project/stats')
+    })
   })
 })
