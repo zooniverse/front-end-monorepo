78b017f450ce15795a84ad0622c65da68c485e13
srallen <srallen@users.noreply.github.com>
2021-02-15 14:25:10 -0600

    Movable field guide (#1968)
    * Beginning of implementing the movable field guide
    
    * Update rendering logic
    
    * Fix position
    
    * Beginning of implementing the movable field guide
    
    * Update rendering logic
    
    * Fix position
    
    * Field guide refactoring (#1975)
    
    * Refactor to use context API for store connection. Remove redundant SpacedHeading.
    
    * Add connector specs
    
    * Update container props and specs
    
    * Rewrite FieldGuide specs
    
    * Fix FieldGuideItemAnchor specs'
    
    * Fix FieldGuideButton specs
    
    * Remove cruft
    
    * Make field guide optional with null default to account for projects without a field guide
    
    * Fix story. Fix resizability. Use standard Modal on small window sizes
    
    * Refactor to use Modal at small sizes and update tests
    
    * Fix return from context consumer. Use mount in tests
    
    * Control the height of the react-rnd container on resize
    
    * Refactor container component to separate out local component state usage. Fix resizing by manually setting it

---
+++ packages/lib-classifier/src/components/Classifier/components/FieldGuide/components/FieldGuide/FieldGuideContainer.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { MovableModal, Modal } from '@zooniverse/react-components'
+import counterpart from 'counterpart'
+import en from './locales/en'
+import FieldGuide from './FieldGuide'
+
+counterpart.registerTranslations('en', en)
+
+function FieldGuideContainer (props) {
+  const {
+    onClose,
+    size,
+    ...rest
+  } = props
+  const modalComponent = (size === 'small') ? Modal : MovableModal
+  const minHeight = 415
+  const minWidth = 490
+
+  const [height, setHeight] = React.useState(minHeight)
+
+  function onResize(e, direction, ref, delta, position) {
+    if (height !== 'auto' && modalComponent === MovableModal) setHeight('auto')
+  }
+
+  const boxHeight = (size === 'small') ? '100%' : `${minHeight}px`
+  const boxWidth = (size === 'small') ? '100%' : `${minWidth}px`
+  
+  const modalProps = {
+    active: true,
+    closeFn: onClose,
+    modal: false,
+    pad: 'medium',
+    position: 'right',
+    title: counterpart('FieldGuide.title')
+  }
+  const rndProps = {
+    minHeight,
+    minWidth,
+    onResize,
+    position: {
+      height,
+      x: 0 - (minWidth + 60), // width plus margins
+      y: 0 - (minHeight + 60) * 0.5 // centers vertically
+    }
+  }
+  const modalPropsToUse = (size === 'small') ? modalProps : Object.assign({}, modalProps, { rndProps })
+  return (
+    <FieldGuide
+      boxHeight={boxHeight}
+      boxWidth={boxWidth}
+      modalComponent={modalComponent}
+      modalProps={modalPropsToUse}
+      {...rest}
+    />
+  )
+}
+
+FieldGuideContainer.defaultProps = {
+  className: '',
+  onClose: () => { },
+  setActiveItemIndex: () => { },
+  size: 'large'
+}
+
+FieldGuideContainer.propTypes = {
+  className: PropTypes.string,
+  fieldGuide: PropTypes.object.isRequired,
+  onClose: PropTypes.func,
+  setActiveItemIndex: PropTypes.func,
+  size: PropTypes.string
+}
+
+export default FieldGuideContainer
