21130dbfc411bc1b5752524306d0646acd9138e9
Delilah <delilahclement@gmail.com>
2021-02-18 16:00:13 -0600

    add standard layout components to ProjectAboutPage

--- packages/app-project/src/screens/ProjectAboutPage/ProjectAboutPage.js
+++ packages/app-project/src/screens/ProjectAboutPage/ProjectAboutPage.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Box } from "grommet";
+import React from "react";
+import Announcements from "@components/Announcements"
+import ProjectHeader from "@components/ProjectHeader"
+import ZooHeaderWrapper from "@components/ZooHeaderWrapper"
+import { ZooFooter, Markdownz } from "@zooniverse/react-components"
 
-function ProjectAboutPage ({ aboutPageData }) {
-  // console.log(aboutPageData)
-  const { title = '' } = aboutPageData
+function ProjectAboutPage({ aboutPageData }) {
+
+  const { content = "", title = "" } = aboutPageData
 
   return (
-    <div>This is the {title} Page</div>
+    <Box>
+      <ZooHeaderWrapper />
+      <ProjectHeader />
+      <Announcements />
+      <Box
+        margin={{ left: "large", right: "large" }}
+        width={{ max: "xxlarge" }}
+      >
+        <Markdownz>{content}</Markdownz>
+      </Box>
+      <ZooFooter />
+    </Box>
   )
 }
 
