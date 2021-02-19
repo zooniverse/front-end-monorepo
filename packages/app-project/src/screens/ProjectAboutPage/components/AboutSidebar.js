94cf9ebf908b42fdbf06b2030a1a86d42969a431
Delilah <delilahclement@gmail.com>
2021-02-19 13:20:34 -0600

    change styling of AboutNavLink based on current page

--- packages/app-project/src/screens/ProjectAboutPage/components/AboutSidebar.js
+++ packages/app-project/src/screens/ProjectAboutPage/components/AboutSidebar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavLink from '@shared/components/NavLink'
 import { Nav } from 'grommet'
 import { withRouter } from 'next/router'
+import addQueryParams from '@helpers/addQueryParams'
+import PropTypes from 'prop-types'
 
 function AboutSidebar({ router }) {
   const { owner, project } = router.query
@@ -9,13 +11,67 @@ function AboutSidebar({ router }) {
 
   return (
     <Nav flex direction="column">
-      <NavLink link={{ href: `${baseUrl}/research`, text: 'Research' }} color="" weight="" />
-      <NavLink link={{ href: `${baseUrl}/team`, text: 'The Team' }} color="" weight="" />
-      <NavLink link={{ href: `${baseUrl}/results`, text: 'Results' }} color="" weight="" />
-      <NavLink link={{ href: `${baseUrl}/education`, text: 'Education' }} color="" weight="" />
-      <NavLink link={{ href: `${baseUrl}/faq`, text: 'FAQ' }} color="" weight="" />
+      <AboutNavLink
+        link={{ href: `${baseUrl}/research`, text: 'research' }}
+        router={router}
+      />
+      <AboutNavLink
+        link={{ href: `${baseUrl}/team`, text: 'the team' }}
+        router={router}
+      />
+      <AboutNavLink
+        link={{ href: `${baseUrl}/results`, text: 'results' }}
+        router={router}
+      />
+      <AboutNavLink
+        link={{ href: `${baseUrl}/education`, text: 'education' }}
+        router={router}
+      />
+      <AboutNavLink
+        link={{ href: `${baseUrl}/faq`, text: 'faq' }}
+        router={router}
+      />
     </Nav>
   )
+}
+
+AboutSidebar.propTypes = {
+  router: PropTypes.object,
+}
+
+
+const AboutNavLink = ({ router, link }) => {
+  const inActiveColor = 'grey'
+  const activeColor = 'brand'
+
+  const inActiveWeight = '400'
+  const activeWeight = '700'
+
+  const inActiveBg = 'white'
+  const activeBg = 'brand'
+
+  const [isCurrentPage, setCurrentPage] = useState(false)
+
+  useEffect(() => {
+    const { href } = link
+    setCurrentPage(router?.asPath === addQueryParams(href, router))
+  }, [router])
+
+  return (
+    <NavLink
+      link={link}
+      color={isCurrentPage ? activeColor : inActiveColor}
+      weight={isCurrentPage ? activeWeight : inActiveWeight}
+    />
+  )
+}
+
+AboutNavLink.propTypes = {
+  link: PropTypes.shape({
+    href: PropTypes.string,
+    text: PropTypes.string
+  }).isRequired,
+  router: PropTypes.object,
 }
 
 export default withRouter(AboutSidebar)
