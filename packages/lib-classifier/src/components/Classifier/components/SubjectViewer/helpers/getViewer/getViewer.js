0e0fe52c038f640ac1a312d21d19e1e4d4673936
srallen <srallen@users.noreply.github.com>
2021-02-19 09:48:54 -0600

    Make ScatterPlotViewer usable on its own (#2038)
    * Refactor to use connector component for ScatterPlotViwer. Add to the available viewer lists
    
    * Updates to actually make the scatter plot viewer functional in a project
    
    * Find location by mime type and validate. Add a width to the outer Box so the stories render

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewer/getViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewer/getViewer.js
@@ -1,15 +1,17 @@
 import DataImageViewer from '../../components/DataImageViewer'
-import SubjectGroupViewer from '../../components/SubjectGroupViewer'
-import SingleImageViewer from '../../components/SingleImageViewer'
-import SingleVideoViewer from '../../components/SingleVideoViewer'
 import LightCurveViewer from '../../components/LightCurveViewer'
 import MultiFrameViewer from '../../components/MultiFrameViewer'
+import ScatterPlotViewer from '../../components/ScatterPlotViewer'
+import SingleImageViewer from '../../components/SingleImageViewer'
+import SingleVideoViewer from '../../components/SingleVideoViewer'
+import SubjectGroupViewer from '../../components/SubjectGroupViewer'
 import VariableStarViewer from '../../components/VariableStarViewer'
 
 const viewers = {
   dataImage: DataImageViewer,
   subjectGroup: SubjectGroupViewer,
   singleImage: SingleImageViewer,
+  scatterPlot: ScatterPlotViewer,
   singleVideo: SingleVideoViewer,
   lightCurve: LightCurveViewer,
   multiFrame: MultiFrameViewer,
