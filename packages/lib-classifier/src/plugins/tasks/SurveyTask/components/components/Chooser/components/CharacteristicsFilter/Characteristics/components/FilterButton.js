fd0f9b1fe8fbd471f9a454f4ef01a5ee23059511
Mark Bouslog <mark@zooniverse.org>
2021-03-12 14:06:25 -0600

    Add survey task characteristics filter components (#1989)
    * Add choices, choice button components
    
    * Refactor Choices with Grid
    
    * Initial ChoiceButton styling
    
    * Reorganize Choices and related components
    
    * Add ChoiceButton story
    
    * Add story for ChoiceButton with long label
    
    * Init stories for Choices
    
    * Refactor Chooser with FilterStatus
    
    * Init FilterStatus component
    
    * Init add Characteristcs component
    
    * Init add CharacteristicSection component
    
    * Init add FilterButton component
    
    * Init filter functionality
    
    * Remove init clear button
    
    * Refactor FilterButton with CloseButton, basic functionality
    
    * Add Clear Filters button functionality
    
    * Refactor CloseButton styling
    
    * Remove setTimeout
    
    * Remove cruft and fix test
    
    * Add safety fallbacks
    
    Co-authored-by: srallen <srallen@users.noreply.github.com>
    
    * Refactor props, add basic smoke tests
    
    * Add FilterButton tests, related refactoring
    
    * Add CharacteristicSection tests, related refactors
    
    * Add Characteristics tests, refactor CharacteristicSection tests
    
    * Add FilterStatus tests
    
    * Add CharacteristicSection, FilterButton stories
    
    * Add FilterStatus stories, styling refactors
    
    * Add FilterStatus translations
    
    * Add Characteristics translations
    
    * Remove wrappingComponent from tests
    
    Co-authored-by: srallen <srallen@users.noreply.github.com>
    
    * Testing cleanup
    
    * Rename FilterButton sizing prop, add closeFn comment
    
    Co-authored-by: Sarah Allen <sarah@zooniverse.org>
    Co-authored-by: srallen <srallen@users.noreply.github.com>

---
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/Characteristics/components/FilterButton.js
@@ -0,0 +1,80 @@
+import { Box } from 'grommet'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+import { CloseButton, Media } from '@zooniverse/react-components'
+
+ export const StyledFilter = styled(Box)`
+  button {
+    display: none;
+  }
+
+  &:focus > button, 
+  &:hover > button {
+    display: block;
+    position: absolute;
+  }
+`
+
+export default function FilterButton (props) {
+  const {
+    characteristicId,
+    checked,
+    onFilter,
+    buttonSize,
+    valueImageSrc,
+    valueLabel
+  } = props
+
+  const backgroundColor = checked ? 'accent-2' : 'neutral-6'
+  const marginPerSize = buttonSize === 'small' ? 'none' : { bottom: 'xsmall' }
+  const containerSize = buttonSize === 'small' ? '30px' : '40px'
+  const mediaSize = buttonSize === 'small' ? '18' : '25'
+
+  return (
+    <StyledFilter
+      align='center'  
+      background={{ color: backgroundColor }}
+      height={containerSize}
+      justify='center'
+      margin={marginPerSize}
+      round='full'
+      width={containerSize}
+    >
+      <Media
+        alt={valueLabel}
+        height={mediaSize}
+        src={valueImageSrc}
+        width={mediaSize}
+      />
+      {checked && (
+        <CloseButton
+          closeFn={(event) => {
+            // Note: preventDefault and stopPropagation are to prevent the radio button input click handler from firing and re-selecting the characteristic filter
+            event.preventDefault()
+            event.stopPropagation()
+            onFilter(characteristicId)
+          }}
+        />
+      )}
+    </StyledFilter>
+  )
+}
+
+FilterButton.defaultProps = {
+  characteristicId: '',
+  checked: false,
+  onFilter: () => {},
+  buttonSize: 'medium',
+  valueImageSrc: '',
+  valueLabel: ''
+}
+
+FilterButton.propTypes = {
+  characteristicId: PropTypes.string,
+  checked: PropTypes.bool,
+  onFilter: PropTypes.func,
+  buttonSize: PropTypes.string,
+  valueImageSrc: PropTypes.string,
+  valueLabel: PropTypes.string
+}
