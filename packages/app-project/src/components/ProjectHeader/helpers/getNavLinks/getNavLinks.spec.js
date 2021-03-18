85cdd362dd1a7fbf6c67d8d1d478d7e4419012d8
Jim O'Donnell <jim@zooniverse.org>
2021-03-18 07:46:18 +0000

    Revert "Update getNavLink helper for about page routing (#2065)"
    This reverts commit 7a6732d7947b3a6d359c43110edae77c31282bcd.

--- packages/app-project/src/components/ProjectHeader/helpers/getNavLinks/getNavLinks.spec.js
+++ packages/app-project/src/components/ProjectHeader/helpers/getNavLinks/getNavLinks.spec.js
@@ -1,8 +1,6 @@
 import getNavLinks from './getNavLinks'
 
 const BASE_URL = '/foo/bar'
-const stagingOrigin = 'https://frontend.preview.zooniverse.org'
-const productionOrigin = 'https://www.zooniverse.org'
 
 describe('Helper > getNavLinks', function () {
   describe('when not logged in', function () {
@@ -10,7 +8,7 @@ describe('Helper > getNavLinks', function () {
       const links = getNavLinks(false, BASE_URL)
       expect(links).to.deep.equal([
         {
-          href: `${BASE_URL}/about/research`,
+          href: `${BASE_URL}/about`,
           text: 'About'
         },
         {
@@ -34,7 +32,7 @@ describe('Helper > getNavLinks', function () {
       const links = getNavLinks(true, BASE_URL)
       expect(links).to.deep.equal([
         {
-          href: `${BASE_URL}/about/research`,
+          href: `${BASE_URL}/about`,
           text: 'About'
         },
         {
@@ -62,44 +60,7 @@ describe('Helper > getNavLinks', function () {
       const links = getNavLinks(true, BASE_URL, '1234')
       expect(links).to.deep.equal([
         {
-          href: `${BASE_URL}/about/research`,
-          text: 'About'
-        },
-        {
-          href: `${BASE_URL}/classify/workflow/1234`,
-          text: 'Classify'
-        },
-        {
-          href: `${BASE_URL}/talk`,
-          text: 'Talk'
-        },
-        {
-          href: `${BASE_URL}/collections`,
-          text: 'Collect'
-        },
-        {
-          href: `${BASE_URL}/recents`,
-          text: 'Recents'
-        }
-      ])
-    })
-  })
-
-  describe('when PANOPTES_ENV is production', function () {
-    let previousEnv
-    before(function () {
-      previousEnv = process.env.PANOPTES_ENV
-      process.env.PANOPTES_ENV = 'production'
-    })
-
-    after(function () {
-      process.env.PANOPTES_ENV = previousEnv
-    })
-    it('should return the correct menu links', function () {
-      const links = getNavLinks(true, BASE_URL, '1234')
-      expect(links).to.deep.equal([
-        {
-          href: `${productionOrigin}${BASE_URL}/about`,
+          href: `${BASE_URL}/about`,
           text: 'About'
         },
         {
