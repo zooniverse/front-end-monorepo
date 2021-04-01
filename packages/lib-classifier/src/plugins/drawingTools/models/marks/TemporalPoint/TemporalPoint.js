5f95861bebbf879ed8e85e0d6ffc55592aa6df32
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-04-01 16:30:39 +0100

    Trim down TemporalPoint models/marks to inherit from Point

--- packages/lib-classifier/src/plugins/drawingTools/models/marks/TemporalPoint/TemporalPoint.js
+++ packages/lib-classifier/src/plugins/drawingTools/models/marks/TemporalPoint/TemporalPoint.js
@@ -9,30 +9,13 @@ import { TemporalPoint as TemporalPointComponent } from '../../../components'
 import { TemporalPointTool } from '@plugins/drawingTools/models/tools'
 
 import Mark from '../Mark'
+import Point from '../Point'
 
 const TemporalPointModel = types
   .model('TemporalPointModel', {
-    x: types.optional(types.number, 0),
-    y: types.optional(types.number, 0),
+    t: types.optional(types.number, 0),
   })
   .views((self) => ({
-    get coords() {
-      return {
-        x: self.x,
-        y: self.y
-      }
-    },
-
-    deleteButtonPosition(scale) {
-      const DELETE_BUTTON_ANGLE = 45
-      const theta = DELETE_BUTTON_ANGLE * (Math.PI / 180)
-      const dx = (20 / scale) * Math.cos(theta)
-      const dy = -1 * (20 / scale) * Math.sin(theta)
-      const x = dx
-      const y = dy
-      return { x, y }
-    },
-
     get tool() {
       return getParentOfType(self, TemporalPointTool)
     },
@@ -41,35 +24,8 @@ const TemporalPointModel = types
       return TemporalPointComponent
     }
   }))
-  .actions((self) => {
-    function initialDrag({ x, y }) {
-      self.x = x
-      self.y = y
-    }
 
-    function initialPosition({ x, y }) {
-      self.x = x
-      self.y = y
-    }
 
-    function move({ x, y }) {
-      self.x += x
-      self.y += y
-    }
-
-    function setCoordinates({ x, y }) {
-      self.x = x
-      self.y = y
-    }
-
-    return {
-      initialDrag,
-      initialPosition,
-      move,
-      setCoordinates
-    }
-  })
-
-const TemporalPoint = types.compose('TemporalPoint', Mark, TemporalPointModel)
+const TemporalPoint = types.compose('TemporalPoint', Point, TemporalPointModel)
 
 export default TemporalPoint
