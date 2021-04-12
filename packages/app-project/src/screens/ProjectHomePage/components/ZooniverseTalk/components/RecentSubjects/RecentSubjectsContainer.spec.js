bfc7c8de9a291c01d44bec2e6a12315dda245232
Jim O'Donnell <jim@zooniverse.org>
2021-04-12 15:39:23 +0100

    Project app: Add set_member_subjects to API mocks for page props (#2091)
    * Add set_member_subjects to API mocks
    Add set_member_subjects to the API mocks for page props. Add a mock subject to mock subject sets, to check that the API response is being added to page props as expected.
    
    * Mock the API for recent Talk comments
    Mock the Talk API in the tests for RecentSubjectsContainer.

--- packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/RecentSubjectsContainer.spec.js
+++ packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/RecentSubjectsContainer.spec.js
@@ -1,13 +1,42 @@
 import { shallow } from 'enzyme'
+import nock from 'nock'
 import React from 'react'
 
 import RecentSubjectsContainer from './RecentSubjectsContainer'
 
-let wrapper
-
 describe('Component > RecentSubjectsContainer', function () {
+  let wrapper
+
+  const TALK_URL = 'https://talk-staging.zooniverse.org'
+  const PANOPTES_URL = 'https://panoptes-staging.zooniverse.org/api'
+
+  const MOCK_COMMENTS = [
+    { focus_id: '1' },
+    { focus_id: '2' },
+    { focus_id: '3' }
+  ]
+
+  const MOCK_SUBJECTS = [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' }
+  ]
+
   before(function () {
+    process.env.TALK_HOST = TALK_URL
+    nock(TALK_URL)
+      .get('/comments')
+      .query(true)
+      .reply(200, { comments: MOCK_COMMENTS })
+    nock(PANOPTES_URL)
+      .get('/subjects')
+      .query(true)
+      .reply(200, { subjects: MOCK_SUBJECTS })
     wrapper = shallow(<RecentSubjectsContainer.wrappedComponent />)
+  })
+
+  after(function () {
+    delete process.env.TALK_HOST
   })
 
   it('should render without crashing', function () {
