996d41b6c5a7dd394f3b92cab9837b3d99b05f87
Delilah Clement <delilahclement@gmail.com>
2021-04-22 14:32:47 -0500

    provide default content if aboutPageData is missing

--- packages/app-project/src/screens/ProjectAboutPage/ProjectAboutPageConnector.js
+++ packages/app-project/src/screens/ProjectAboutPage/ProjectAboutPageConnector.js
@@ -4,6 +4,23 @@ import { arrayOf, bool, object, shape, string } from 'prop-types'
 
 import ProjectAboutPage from './ProjectAboutPage'
 
+const handleMissingTitle = pageType => {
+  switch (pageType) {
+    case 'science_case':
+      return 'Research'
+    case 'team':
+      return 'The Team'
+    case 'results':
+      return 'Results'
+    case 'education':
+      return 'Education'
+    case 'faq':
+      return 'FAQ'
+    default:
+      return 'Research'
+  }
+}
+
 /**
   Connect the about page to the store. Pass down correct aboutPages data.
 */
@@ -13,17 +30,19 @@ function ProjectAboutPageConnector({ pageType, teamArray }) {
       project: { inBeta = false, about_pages = [], display_name = '' }
     }
   } = React.useContext(MobXProviderContext)
-  const [aboutPageData] = about_pages.filter(page => page.url_key === pageType)
-  return aboutPageData ? (
+  let aboutPageData
+  if (about_pages.length) aboutPageData = about_pages.filter(page => page.url_key === pageType)[0]
+  else {
+    const pageTitle = handleMissingTitle(pageType)
+    aboutPageData = {
+      title: pageTitle,
+      content: 'No content yet.'
+    }
+  }
+  return (
     <ProjectAboutPage
       inBeta={inBeta}
       aboutPageData={aboutPageData}
-      teamArray={teamArray}
-      projectDisplayName={display_name}
-    />
-  ) : (
-    <ProjectAboutPage
-      inBeta={inBeta}
       teamArray={teamArray}
       projectDisplayName={display_name}
     />
