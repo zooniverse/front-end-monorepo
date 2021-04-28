0178e833ac5952cfd795c499efc01d88c64b5de4
Jim O'Donnell <jim@zooniverse.org>
2021-04-28 12:33:25 +0100

    Catch Panoptes API errors Catch API errors and log to Sentry. Return a sensible default if the API fails.

--- packages/app-project/src/shared/components/SubjectPicker/helpers/checkRetiredStatus.js
+++ packages/app-project/src/shared/components/SubjectPicker/helpers/checkRetiredStatus.js
@@ -1,20 +1,25 @@
-import { panoptes } from '@zooniverse/panoptes-js'
-const env = 'production'
+import { env, panoptes } from '@zooniverse/panoptes-js'
+import { logToSentry } from '@helpers/logger'
 
 export default async function checkRetiredStatus(subject_ids, workflow, page_size=20) {
-  const workflow_id = workflow.id
+  const workflow_id = '5329'
   const retirementStatuses = {}
-  const response = await panoptes
-    .get('/subject_workflow_statuses', {
-      env,
-      page_size,
-      subject_ids,
-      workflow_id
+  try {
+    const response = await panoptes
+      .get('/subject_workflow_statuses', {
+        env,
+        page_size,
+        subject_ids,
+        workflow_id
+      })
+    const { subject_workflow_statuses } = response.body
+    subject_workflow_statuses.forEach(status => {
+      const inProgress = status.classifications_count > 0 ? 'In progress' : 'Unclassified'
+      retirementStatuses[status.links.subject] = status.retired_at ? 'Retired' : inProgress
     })
-  const { subject_workflow_statuses } = response.body
-  subject_workflow_statuses.forEach(status => {
-    const inProgress = status.classifications_count > 0 ? 'In progress' : 'Unclassified'
-    retirementStatuses[status.links.subject] = status.retired_at ? 'Retired' : inProgress
-  })
+  } catch (error) {
+    console.error(error)
+    logToSentry(error)
+  }
   return retirementStatuses
 }
