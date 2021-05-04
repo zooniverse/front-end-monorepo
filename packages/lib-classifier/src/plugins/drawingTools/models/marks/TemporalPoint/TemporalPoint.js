03d428028bd969ade63f1e55b1c4a788f6c52c15
ErikOstlund <eostlund74@gmail.com>
2021-05-04 11:22:14 -0500

    Removed unused imports

--- packages/lib-classifier/src/plugins/drawingTools/models/marks/TemporalPoint/TemporalPoint.js
+++ packages/lib-classifier/src/plugins/drawingTools/models/marks/TemporalPoint/TemporalPoint.js
@@ -1,14 +1,7 @@
-import {
-  addDisposer,
-  getRoot,
-  getParentOfType,
-  isValidReference,
-  types
-} from 'mobx-state-tree'
+import { getParentOfType, types } from 'mobx-state-tree'
 import { Point as PointComponent } from '@plugins/drawingTools/components'
 import { TemporalPointTool } from '@plugins/drawingTools/models/tools'
 
-import Mark from '../Mark'
 import Point from '../Point'
 
 const TemporalPointModel = types
