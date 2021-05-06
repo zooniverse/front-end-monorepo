1b0e943e510a0da66d7154ae4883f6b9a2253724
Sarah Allen <sarah@zooniverse.org>
2021-05-06 11:01:34 -0500

    Add new files

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/TranscribedLines/components/TooltipIcon/TooltipIcon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { string } from 'prop-types'
+import { Blank } from 'grommet-icons'
+
+export default function TooltipIcon ({ fill }) {
+  return (
+    <Blank color={fill}>
+      <circle cx='12' cy='12' r='6' />
+    </Blank>
+  )
+}
+
+TooltipIcon.propTypes = {
+  fill: string
+}
