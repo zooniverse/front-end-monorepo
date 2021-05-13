9b987fc0543009fdb6b71a9158eab05402bb7e8e
ErikOstlund <eostlund74@gmail.com>
2021-05-13 17:58:09 -0500

    Created RotateRectangle from Rectangle

---
+++ packages/lib-classifier/src/plugins/drawingTools/components/RotateRectangle/RotateRectangle.js
@@ -0,0 +1,109 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import DragHandle from '../DragHandle'
+
+const GRAB_STROKE_WIDTH = 6
+
+function RotateRectangle({ active, children, mark, onFinish, scale }) {
+  const { x_center, y_center, width, height } = mark
+
+  function onHandleDrag(coords) {
+    mark.setCoordinates(coords)
+  }
+
+  const x_left = x_center - width / 2
+  const x_right = x_center + width / 2
+  const y_top = y_center - height / 2
+  const y_bottom = y_center + height / 2
+
+  const _onFinish =
+    onFinish ||
+    function () {
+      return true
+    }
+
+  return (
+    <g onPointerUp={active ? _onFinish : undefined}>
+      <rect x={x_left} y={y_top} width={width} height={height} />
+      <rect
+        x={x_left}
+        y={y_top}
+        width={width}
+        height={height}
+        strokeWidth={GRAB_STROKE_WIDTH / scale}
+        strokeOpacity='0'
+      />
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_left}
+          y={y_top}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left + d.x,
+              x_right: x_right,
+              y_top: y_top + d.y,
+              y_bottom: y_bottom
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_right}
+          y={y_bottom}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left,
+              x_right: x_right + d.x,
+              y_top: y_top,
+              y_bottom: y_bottom + d.y
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_left}
+          y={y_bottom}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left + d.x,
+              x_right: x_right,
+              y_top: y_top,
+              y_bottom: y_bottom + d.y
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_right}
+          y={y_top}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left,
+              x_right: x_right + d.x,
+              y_top: y_top + d.y,
+              y_bottom: y_bottom
+            })
+          }
+        />
+      )}
+      {children}
+    </g>
+  )
+}
+
+RotateRectangle.propTypes = {
+  active: PropTypes.bool
+}
+
+RotateRectangle.defaultProps = {
+  active: false
+}
+
+export default RotateRectangle
---
+++ packages/lib-classifier/src/plugins/drawingTools/models/tools/RotateRectangleTool/RotateRectangleTool.js
@@ -0,0 +1,24 @@
+import { types } from 'mobx-state-tree'
+import Tool from '../Tool'
+import { RotateRectangle } from '../../marks'
+
+const RotateRectangleTool = types
+  .model('RotateRectangle', {
+    marks: types.map(RotateRectangle),
+    type: types.literal('rotateRectangle')
+  })
+  .actions((self) => {
+    function createMark(mark) {
+      const newMark = RotateRectangle.create(
+        Object.assign({}, mark, { toolType: self.type })
+      )
+      self.marks.put(newMark)
+      return newMark
+    }
+
+    return {
+      createMark
+    }
+  })
+
+export default types.compose('RotateRectangleTool', Tool, RotateRectangleTool)
