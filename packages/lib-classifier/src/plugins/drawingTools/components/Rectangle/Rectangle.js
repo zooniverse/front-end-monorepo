4eb208ec3c9eda6e6ae0e6a63bbb414a93bfd97c
ErikOstlund <eostlund74@gmail.com>
2021-05-13 14:12:41 -0500

    ESlint/Prettier fixes per Zooniverse Style Guide

--- packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Rectangle/Rectangle.js
@@ -4,39 +4,51 @@ import DragHandle from '../DragHandle'
 
 const GRAB_STROKE_WIDTH = 6
 
-function Rectangle ({ active, children, mark, onFinish, scale }) {
+function Rectangle({ active, children, mark, onFinish, scale }) {
   const { x_center, y_center, width, height } = mark
 
-  function onHandleDrag (coords) {
+  function onHandleDrag(coords) {
     mark.setCoordinates(coords)
   }
-  
+
   const x_left = x_center - width / 2
   const x_right = x_center + width / 2
   const y_top = y_center - height / 2
   const y_bottom = y_center + height / 2
-  
-  const _onFinish = onFinish || function () { return true }
-  
+
+  const _onFinish =
+    onFinish ||
+    function () {
+      return true
+    }
+
   return (
     <g onPointerUp={active ? _onFinish : undefined}>
       <rect x={x_left} y={y_top} width={width} height={height} />
-      <rect x={x_left} y={y_top} width={width} height={height} strokeWidth={GRAB_STROKE_WIDTH / scale} strokeOpacity='0' />
-      {active &&
+      <rect
+        x={x_left}
+        y={y_top}
+        width={width}
+        height={height}
+        strokeWidth={GRAB_STROKE_WIDTH / scale}
+        strokeOpacity='0'
+      />
+      {active && (
         <DragHandle
           scale={scale}
           x={x_left}
           y={y_top}
-          dragMove={(e, d) => 
+          dragMove={(e, d) =>
             onHandleDrag({
               x_left: x_left + d.x,
               x_right: x_right,
               y_top: y_top + d.y,
-              y_bottom: y_bottom,
+              y_bottom: y_bottom
             })
           }
-        />}
-      {active &&
+        />
+      )}
+      {active && (
         <DragHandle
           scale={scale}
           x={x_right}
@@ -46,11 +58,12 @@ function Rectangle ({ active, children, mark, onFinish, scale }) {
               x_left: x_left,
               x_right: x_right + d.x,
               y_top: y_top,
-              y_bottom: y_bottom + d.y,
+              y_bottom: y_bottom + d.y
             })
           }
-        />}
-        {active &&
+        />
+      )}
+      {active && (
         <DragHandle
           scale={scale}
           x={x_left}
@@ -60,11 +73,12 @@ function Rectangle ({ active, children, mark, onFinish, scale }) {
               x_left: x_left + d.x,
               x_right: x_right,
               y_top: y_top,
-              y_bottom: y_bottom + d.y,
+              y_bottom: y_bottom + d.y
             })
           }
-        />}
-        {active &&
+        />
+      )}
+      {active && (
         <DragHandle
           scale={scale}
           x={x_right}
@@ -74,10 +88,11 @@ function Rectangle ({ active, children, mark, onFinish, scale }) {
               x_left: x_left,
               x_right: x_right + d.x,
               y_top: y_top + d.y,
-              y_bottom: y_bottom,
+              y_bottom: y_bottom
             })
           }
-        />}
+        />
+      )}
       {children}
     </g>
   )
@@ -92,5 +107,3 @@ Rectangle.defaultProps = {
 }
 
 export default Rectangle
-
-
--- packages/lib-classifier/src/plugins/drawingTools/models/tools/RectangleTool/RectangleTool.js
+++ packages/lib-classifier/src/plugins/drawingTools/models/tools/RectangleTool/RectangleTool.js
@@ -2,13 +2,16 @@ import { types } from 'mobx-state-tree'
 import Tool from '../Tool'
 import { Rectangle } from '../../marks'
 
-const RectangleTool = types.model('Rectangle', {
-  marks: types.map(Rectangle),
-  type: types.literal('rectangle')
-})
-  .actions(self => {
-    function createMark (mark) {
-      const newMark = Rectangle.create(Object.assign({}, mark, { toolType: self.type }))
+const RectangleTool = types
+  .model('Rectangle', {
+    marks: types.map(Rectangle),
+    type: types.literal('rectangle')
+  })
+  .actions((self) => {
+    function createMark(mark) {
+      const newMark = Rectangle.create(
+        Object.assign({}, mark, { toolType: self.type })
+      )
       self.marks.put(newMark)
       return newMark
     }
