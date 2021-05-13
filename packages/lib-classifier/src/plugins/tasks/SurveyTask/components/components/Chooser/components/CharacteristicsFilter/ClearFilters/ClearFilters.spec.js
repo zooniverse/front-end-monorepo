5827acb18f1517ae732791d30ca43d932f2f2a1e
Mark Bouslog <mcbouslog@gmail.com>
2021-05-13 10:55:15 -0500

    Add ClearFilters tests

--- packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/ClearFilters/ClearFilters.spec.js
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/ClearFilters/ClearFilters.spec.js
@@ -1,17 +1,44 @@
 import { shallow } from 'enzyme'
+import { Button } from 'grommet'
 import React from 'react'
+import sinon from 'sinon'
 
 import ClearFilters from './ClearFilters'
 
 describe('Component > ClearFilters', function () {
-  let wrapper
+  let wrapper, handleFilterSpy
+
   before(function () {
+    handleFilterSpy = sinon.spy()
     wrapper = shallow(
-      <ClearFilters />
+      <ClearFilters
+        handleFilter={handleFilterSpy}
+        showingChoices={5}
+        totalChoices={20}
+      />
     )
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok()
+  })
+
+  it('should render a Button', function () {
+    expect(wrapper.find(Button)).to.have.lengthOf(1)
+  })
+
+  it('should call handleFilter on button click', function () {
+    expect(handleFilterSpy).to.not.have.been.called()
+
+    wrapper.find(Button).simulate('click')
+
+    expect(handleFilterSpy).to.have.been.calledOnce()
+    handleFilterSpy.resetHistory()
+  })
+
+  it('should disable the button if showing choices = total choices', function () {
+    wrapper.setProps({ showingChoices: 20 })
+
+    expect(wrapper.find(Button).props().disabled).to.be.true()
   })
 })
