ed3dc3e2229d3361b423f727a905d63f1b9523b6
Sarah Allen <sarah@zooniverse.org>
2021-05-17 12:02:35 -0500

    Use shallow instead of render in project app tests.

--- packages/app-project/src/components/Announcements/components/GenericAnnouncement/components/NarrowGenericAnnouncement/NarrowGenericAnnouncement.spec.js
+++ packages/app-project/src/components/Announcements/components/GenericAnnouncement/components/NarrowGenericAnnouncement/NarrowGenericAnnouncement.spec.js
@@ -1,6 +1,6 @@
-import { render, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import React from 'react'
-
+import { CloseButton } from '@zooniverse/react-components'
 import NarrowGenericAnnouncement from './NarrowGenericAnnouncement'
 
 const ANNOUNCEMENT = 'Arcu scelerisque curae eu sapien euismod nisl, viverra gravida donec interdum tempor vulputate nec, nam morbi rhoncus porta sollicitudin.'
@@ -9,16 +9,15 @@ describe('Component > GenericAnnouncement > NarrowGenericAnnouncement', function
   let wrapper
 
   before(function () {
-    wrapper = render(<NarrowGenericAnnouncement announcement={ANNOUNCEMENT} />)
+    wrapper = shallow(<NarrowGenericAnnouncement announcement={ANNOUNCEMENT} />)
   })
 
   it('should render without crashing', function () {
     expect(wrapper).to.be.ok()
   })
 
-  it('should render a paragraph with the announcement text', function () {
-    const paragraphWrapper = wrapper.find('p')
-    expect(paragraphWrapper.text()).to.contain(ANNOUNCEMENT)
+  it('should render announcement text', function () {
+    expect(wrapper.contains(ANNOUNCEMENT)).to.be.true()
   })
 
   it('should be the full width of its container', function () {
@@ -27,24 +26,24 @@ describe('Component > GenericAnnouncement > NarrowGenericAnnouncement', function
   })
 
   it('should not render a close button', function () {
-    const buttonWrapper = wrapper.find('button')
+    const buttonWrapper = wrapper.find(CloseButton)
     expect(buttonWrapper).to.have.lengthOf(0)
   })
 
   describe('when dismissable', function () {
     before(function () {
-      wrapper = render(<NarrowGenericAnnouncement announcement={ANNOUNCEMENT} dismissable closeFn={() => { }} />)
+      wrapper = shallow(<NarrowGenericAnnouncement announcement={ANNOUNCEMENT} dismissable closeFn={() => { }} />)
     })
 
     it('should render a close button', function () {
-      const buttonWrapper = wrapper.find('button')
+      const buttonWrapper = wrapper.find(CloseButton)
       expect(buttonWrapper).to.have.lengthOf(1)
     })
   })
 
   describe('when there are children nodes', function () {
     before(function () {
-      wrapper = render(
+      wrapper = shallow(
         <NarrowGenericAnnouncement announcement={ANNOUNCEMENT}>
           <span id='child'></span>
         </NarrowGenericAnnouncement>
