82145a567184ac77dda346034b6f6498bb35e0bc
Mark Bouslog <mark@zooniverse.org>
2021-05-25 09:54:40 -0500

    Add deselect functionality to survey task question radio input (#2194)
    * Refactor RadioInputs and RadioInput to support deselect
    
    * Refactor Questions handleAnswer to delete unanswered question
    
    * Refactor tests, RadioInputs story cleanup
    
    * Edit test description
    
    Co-authored-by: srallen <srallen@users.noreply.github.com>
    
    Co-authored-by: srallen <srallen@users.noreply.github.com>

--- packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Choice/components/Questions/components/RadioInputs/components/RadioInput.js
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Choice/components/Questions/components/RadioInputs/components/RadioInput.js
@@ -1,39 +1,82 @@
 import { Box, Text } from 'grommet'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+export const StyledBox = styled(Box)`
+  cursor: pointer;
+  
+  > span {
+    cursor: pointer;
+  }
+  
+  > input {
+    cursor: pointer;
+    opacity: 0.01;
+    position: absolute;
+  }
+`
 
 export default function RadioInput (props) {
   const {
-    checked,
-    label
+    handleRadioChange,
+    isChecked,
+    option,
+    questionId
   } = props
 
-  const backgroundColor = checked ? 'accent-2' : 'neutral-6'
+  const backgroundColor = isChecked ? 'accent-2' : 'neutral-6'
 
   return (
-    <Box
-      align='center'
-      background={{ color: backgroundColor }}
-      height='40px'
-      justify='center'
-      margin={{ bottom: 'xsmall' }}
-      round='full'
-      width='40px'
-    >
-      <Text
-        color='dark-1'
-        weight={checked ? 'bold' : 'normal'}
+    <label>
+      <StyledBox
+        align='center'
+        background={{ color: backgroundColor }}
+        height='40px'
+        justify='center'
+        margin={{
+          bottom: 'xsmall',
+          right: 'xsmall'
+        }}
+        pad={{ horizontal: 'xsmall' }}
+        round='full'
+        width={{ min: '40px' }}
       >
-        {label}
-      </Text>
-    </Box>
+        <input
+          name={questionId}
+          value={option.value}
+          type='radio'
+          checked={isChecked}
+          onChange={({ target }) => (handleRadioChange(target.value))}
+          onClick={({ target }) => (handleRadioChange(target.value))}
+        />
+        <Text
+          color='dark-1'
+          weight={isChecked ? 'bold' : 'normal'}
+        >
+          {option.label}
+        </Text>
+      </StyledBox>
+    </label>
   )
 }
 
 RadioInput.defaultProps = {
-  checked: false
+  handleRadioChange: () => {},
+  isChecked: false,
+  option: {
+    label: '',
+    value: ''
+  },
+  questionId: ''
 }
 
 RadioInput.propTypes = {
-  checked: PropTypes.bool
+  handleRadioChange: PropTypes.func,
+  isChecked: PropTypes.bool,
+  option: PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string
+  }),
+  questionId: PropTypes.string
 }
