5f6dca82ab96b456992661274b1639371f858207
srallen <srallen@users.noreply.github.com>
2021-06-03 11:33:27 -0500

    Add basic UPP model (#2206)
    * Add basic UPP model
    
    * Add missing Preferences model.

---
+++ packages/app-project/stores/UserProjectPreferences.js
@@ -0,0 +1,24 @@
+import { types } from 'mobx-state-tree'
+
+const Preferences = types
+  .model('Preferences', {
+    minicourses: types.maybe(types.frozen()),
+    selected_workflow: types.maybe(types.string),
+    tutorials_completed_at: types.maybe(types.frozen())
+  })
+
+const UserProjectPreferences = types
+  .model('UserProjectPreferences', {
+    activity_count: types.maybe(types.number),
+    activity_count_by_workflow: types.maybe(types.frozen()),
+    links: types.maybe(
+      types.frozen({
+        project: types.string,
+        user: types.string
+      })
+    ),
+    preferences: types.maybe(Preferences),
+    settings: types.maybe(types.frozen())
+  })
+
+export default UserProjectPreferences
