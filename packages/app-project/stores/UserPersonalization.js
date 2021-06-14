fae3e5e60dd6423fc6de2ef831fc3ed722da4349
srallen <srallen@users.noreply.github.com>
2021-06-14 13:15:42 -0500

    Integrate UPP store in project app (#2238)
    * Create new parent store for stats and UPP. Make it child of User.
    
    * Tests for UPP
    
    * Only request for UPP if not already present. Rewrite tests to be async.
    
    * Fix spec after rebase

---
+++ packages/app-project/stores/UserPersonalization.js
@@ -0,0 +1,61 @@
+import { addDisposer, getRoot, types } from 'mobx-state-tree'
+import { autorun } from 'mobx'
+import UserProjectPreferences from './UserProjectPreferences'
+import YourStats from './YourStats'
+
+const UserPersonalization = types
+  .model('UserPersonalization', {
+    projectPreferences: types.optional(UserProjectPreferences, {}),
+    stats: types.optional(YourStats, {}),
+    totalClassificationCount: 0
+  })
+  .volatile(self => ({
+    sessionCount: 0
+  }))
+  .views(self => ({
+    get counts() {
+      const todaysDate = new Date()
+      let today
+      try {
+        const todaysCount = self.stats.thisWeek.length === 7
+          ? self.stats.thisWeek[todaysDate.getDay() - 1].count
+          : 0
+        today = todaysCount + self.sessionCount
+      } catch (error) {
+        today = 0
+      }
+
+      return {
+        today,
+        total: self.totalClassificationCount
+      }
+    }
+  }))
+  .actions(self => {
+    function createParentObserver() {
+      const parentDisposer = autorun(() => {
+        const { project, user } = getRoot(self)
+        if (project.id && user.id) {
+          self.projectPreferences.fetchResource()
+          self.stats.fetchDailyCounts()
+        }
+      })
+      addDisposer(self, parentDisposer)
+    }
+    return {
+      afterAttach() {
+        createParentObserver()
+      },
+
+      increment() {
+        self.sessionCount = self.sessionCount + 1
+        self.totalClassificationCount = self.totalClassificationCount + 1
+      },
+
+      setTotalClassificationCount(count) {
+        self.totalClassificationCount = count
+      }
+    }
+  })
+
+export default UserPersonalization
