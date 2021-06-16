25a311de58729ba674b963e2ee67db233c8c0d72
Erik <ErikOstlund@users.noreply.github.com>
2021-06-16 11:32:53 -0500

    Rotating rectangle tool (#2211)
    * ESlint/Prettier fixes per Zooniverse Style Guide
    
    * Created RotateRectangle from Rectangle
    
    * WIP RotateRectangle Marks model
    
    * Debugging RotateRectangle marks model
    
    * Removed inherited code
    
    * WIP save
    
    * Added the rotate guide line
    
    * Created the rotateHandle
    
    * Created onRotateDrag function
    
    Fixed rectangle onHandleDrag bug: added the angle
    
    * Change RotateRectangle to use Mark's transform=translate to position itself and move the origin point to (x,y)
    
    * Created x_roate and y_rotate
    
    In the transform, we now check for a x_rotate and y_rotate to use as a pivot point for rotating.
    
    * Created unit test
    
    * Code clean-up
    
    Declared the rotate() transform string as a variable to make the code easier to read.
    
    * Removed ‘only’
    
    Was being used during developing.
    
    * Added observer for RotateRectangle
    
    DrawingToolMarks no longer wraps each drawing tool. We must explicitly wrap each drawing tool with an observer to properly render and re-render the svg when resizing, reshaping.
    
    * Created new RotateHandle component
    
    to use for tools having a rotate feature. The handle is a SVG element.
    
    * Updated unit test for RotateHandle
    
    * Added unit test for draging the RotateHandle
    
    * Refactored to use ‘beforeEach’
    
    * Added unit test for onFinish
    
    * Updated RotateHandle for Storybook documentation
    
    * Removed _onFinish
    
    In favor of default onFinish which is passed down.
    
    * Updated RotateRectangle
    
    for Storybook documentation
    
    * Removed children
    
    ‘children’ is no longer used. Was left over from an earlier version of the drawing tools, where the delete button was inserted into each tool as a child.
    
    * Defined ‘mark’ with PropTypes.shape
    
    to type check and better define what was in ‘mark’.
    Alphabetized properties of ‘mark’ cause of my OCD! :)
    
    Co-authored-by: Shaun A. Noordin <shaun.a.noordin@gmail.com>

---
+++ packages/lib-classifier/src/plugins/drawingTools/components/RotateRectangle/RotateRectangle.js
@@ -0,0 +1,161 @@
+import { observer } from 'mobx-react'
+import PropTypes from 'prop-types'
+import React from 'react'
+import RotateHandle from '../RotateHandle'
+import DragHandle from '../DragHandle'
+
+const GUIDE_DASH = [4, 4]
+const GUIDE_WIDTH = 1
+const GRAB_STROKE_WIDTH = 6
+const BUFFER = 32
+
+function RotateRectangle({
+  active = false,
+  mark,
+  onFinish = () => true,
+  scale = 1
+}) {
+  const { angle, height, width, x_center, y_center } = mark
+  const guideWidth = GUIDE_WIDTH / scale
+
+  const x_left = x_center - width / 2
+  const x_right = x_center + width / 2
+  const y_top = y_center - height / 2
+  const y_bottom = y_center + height / 2
+  const xRotationHandle = x_center + width / 2 + BUFFER
+
+  function onHandleDrag(coords) {
+    mark.setCoordinates(coords)
+  }
+
+  function onRotateDrag(e) {
+    const angle = mark.getAngle(x_center, y_center, e.x, e.y)
+    mark.setCoordinates({ x_left, x_right, y_top, y_bottom, angle })
+  }
+
+  return (
+    <g onPointerUp={active ? onFinish : undefined}>
+      <rect x={x_left} y={y_top} width={width} height={height} />
+      <rect
+        x={x_left}
+        y={y_top}
+        width={width}
+        height={height}
+        strokeWidth={GRAB_STROKE_WIDTH / scale}
+        strokeOpacity='0'
+      />
+      {/* Rotate Handle */}
+      {active && (
+        <g>
+          <line
+            x1={x_center}
+            y1={y_center}
+            x2={xRotationHandle}
+            y2={y_center}
+            strokeWidth={guideWidth}
+            strokeDasharray={GUIDE_DASH}
+          />
+          <RotateHandle
+            dragMove={onRotateDrag}
+            scale={scale}
+            x={xRotationHandle}
+            y={y_center}
+          />
+        </g>
+      )}
+
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_left}
+          y={y_top}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left + d.x,
+              x_right: x_right,
+              y_top: y_top + d.y,
+              y_bottom: y_bottom,
+              angle: angle
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_right}
+          y={y_bottom}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left,
+              x_right: x_right + d.x,
+              y_top: y_top,
+              y_bottom: y_bottom + d.y,
+              angle: angle
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_left}
+          y={y_bottom}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left + d.x,
+              x_right: x_right,
+              y_top: y_top,
+              y_bottom: y_bottom + d.y,
+              angle: angle
+            })
+          }
+        />
+      )}
+      {active && (
+        <DragHandle
+          scale={scale}
+          x={x_right}
+          y={y_top}
+          dragMove={(e, d) =>
+            onHandleDrag({
+              x_left: x_left,
+              x_right: x_right + d.x,
+              y_top: y_top + d.y,
+              y_bottom: y_bottom,
+              angle: angle
+            })
+          }
+        />
+      )}
+    </g>
+  )
+}
+
+RotateRectangle.propTypes = {
+  /**
+    Modal active state.
+  */
+  active: PropTypes.bool,
+  /**
+    RotateRectangle data: { angle, height, width, x_center, y_center }
+  */
+  mark: PropTypes.shape({
+    angle: PropTypes.number,
+    height: PropTypes.number,
+    width: PropTypes.number,
+    x_center: PropTypes.number,
+    y_center: PropTypes.number
+  }).isRequired,
+
+  /**
+    Callback to reset the drawing canvas when creation of the rectangle is finished.
+  */
+  onFinish: PropTypes.func,
+  /**
+    Image scale factor. Used to keep line widths and sizes constant at all image scales.
+  */
+  scale: PropTypes.number
+}
+
+export default observer(RotateRectangle)
