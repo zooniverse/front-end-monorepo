c0683d0c7008b73ac6373ef72d22989a8b4c646f
Mark Bouslog <mcbouslog@gmail.com>
2021-06-23 13:40:37 -0500

    Add Chooser story

---
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/Chooser.stories.js
@@ -0,0 +1,66 @@
+import zooTheme from '@zooniverse/grommet-theme'
+import { Box, Grommet } from 'grommet'
+import React from 'react'
+
+import Chooser from './Chooser'
+import { task as mockTask } from '@plugins/tasks/SurveyTask/mock-data'
+
+function StoryContext (props) {
+  const { children, theme } = props
+
+  return (
+    <Grommet
+      background={{
+        dark: 'dark-1',
+        light: 'light-1'
+      }}
+      theme={theme}
+      themeMode={(theme.dark) ? 'dark' : 'light'}
+    >
+      <Box
+        align='end'
+        width='full'
+      >
+        <Box
+          background={{
+            dark: 'dark-1',
+            light: 'neutral-6'
+          }}
+          pad='medium'
+          width='380px'
+        >
+          {children}
+        </Box>
+      </Box>
+    </Grommet>
+  )
+}
+
+export default {
+  title: 'Tasks / SurveyTask / Chooser',
+  component: Chooser
+}
+
+const Template = ({ autoFocus, dark, filters, selectedChoiceIds, task }) => (
+  <StoryContext
+    theme={{ ...zooTheme, dark }}
+  >
+    <Chooser
+      autoFocus={autoFocus}
+      filters={filters}
+      handleFilter={() => console.log('handleFilter')}
+      onChoose={() => console.log('onChoose')}
+      selectedChoiceIds={selectedChoiceIds}
+      task={task}
+    />
+  </StoryContext>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  autoFocus: true,
+  dark: false,
+  filters: {},
+  selectedChoiceIds: [],
+  task: mockTask
+}
