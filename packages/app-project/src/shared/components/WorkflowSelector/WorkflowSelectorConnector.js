0789ac150b0ebdc7f1c4f571f3fc179b2f901e94
Sarah Allen <sarah@zooniverse.org>
2021-06-24 15:38:04 -0500

    Refactor store connection for WorkflowSelector

---
+++ packages/app-project/src/shared/components/WorkflowSelector/WorkflowSelectorConnector.js
@@ -0,0 +1,30 @@
+import { MobXProviderContext, observer } from 'mobx-react'
+import React from 'react'
+
+import WorkflowSelector from './WorkflowSelector'
+
+function useStores() {
+  const { store } = React.useContext(MobXProviderContext)
+
+  return {
+    uppSettings: store.user.personalization.projectPreferences.settings,
+    userReadyState: store.user.loadingState,
+    workflowDescription: store.project.workflow_description
+  }
+}
+
+function WorkflowSelectorConnector(props) {
+  const { uppSettings, userReadyState, workflowDescription } = useStores()
+  const assignedWorkflowID = uppSettings?.workflow_id || ''
+  return (
+    <WorkflowSelector
+      assignedWorkflowID={assignedWorkflowID}
+      userReadyState={userReadyState}
+      workflowDescription={workflowDescription}
+      {...props}
+    />
+  )
+}
+
+export default observer(WorkflowSelectorConnector)
+
