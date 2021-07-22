6e265ec6a48940e9dada0b2bbe235639fa05b6f9
Mark Bouslog <mark@zooniverse.org>
2021-07-22 12:01:10 -0500

    Remove console log (#2316)

--- packages/app-project/src/shared/components/WorkflowSelector/WorkflowSelectorConnector.js
+++ packages/app-project/src/shared/components/WorkflowSelector/WorkflowSelectorConnector.js
@@ -6,7 +6,6 @@ import WorkflowSelector from './WorkflowSelector'
 function useStores() {
   const { store } = React.useContext(MobXProviderContext)
 
-  console.log('store', store)
   return {
     uppSettings: store.user.personalization.projectPreferences.settings,
     userReadyState: store.user.loadingState,
