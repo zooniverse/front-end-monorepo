39f2cbde5e5796b2fa079b495336a250dea1c6dd
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-07-30 15:02:45 +0100

    Add fetchComments structure

--- packages/lib-classifier/src/components/Classifier/components/QuickTalk/QuickTalkContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/QuickTalk/QuickTalkContainer.js
@@ -3,6 +3,7 @@ import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
 
 import QuickTalk from './QuickTalk'
+import talkClient from 'panoptes-client/lib/talk-client'
 
 function storeMapper (stores) {
   const {
@@ -17,6 +18,33 @@ function storeMapper (stores) {
 class QuickTalkContainer extends React.Component {
   constructor () {
     super()
+  }
+  
+  componentDidMount () {
+    this.fetchComments()
+  }
+  
+  componentDidUpdate (prevProps) {
+    const props = this.props
+    if (props.subject?.id !== prevProps.subject?.id) {
+      this.fetchComments()
+    }
+    
+  }
+  
+  fetchComments () {
+    const subject = this.props?.subject
+    if (!subject) {
+      this.resetComments()
+      return
+    }
+    
+    console.log('+++ fetchComments')
+  }
+        
+  resetComments () {
+    // TODO
+    console.log('+++ resetComments')
   }
     
   render () {
