21dc04072ed7ef490c95a536481a56663bd1de46
Jim O'Donnell <jim@zooniverse.org>
2021-08-04 14:27:21 +0100

    Update page routes Fetch subject sets on workflow routes, for grouped workflows.

--- packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/subject/[subjectID]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/subject/[subjectID]/index.js
@@ -1,9 +1,16 @@
 import getDefaultPageProps from '@helpers/getDefaultPageProps'
+import fetchSubjectSets from '@helpers/fetchSubjectSets'
 export { default } from '@screens/ClassifyPage'
 
 export async function getServerSideProps({ params, query, req, res }) {
+  const { env } = query
   const { notFound, props: defaultProps } = await getDefaultPageProps({ params, query, req, res })
   const { subjectID, subjectSetID, workflowID } = params
   const props = { ...defaultProps, subjectID, subjectSetID, workflowID }
+  const { workflows } = defaultProps
+  const [ workflow ] = workflows.filter(workflow => workflow.id === params.workflowID)
+  if (workflow?.grouped) {
+    workflow.subjectSets = await fetchSubjectSets(workflow, env)
+  }
   return ({ notFound, props })
 }
