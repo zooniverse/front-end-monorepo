d4277c5ded030f80f394a04cd6df52efd5c7fd4e
Mark Bouslog <mcbouslog@gmail.com>
2021-08-19 12:32:48 -0500

    Add InputGroup and QuestionInput tests

---
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Choice/components/Questions/components/InputGroup/components/QuestionInput.spec.js
@@ -0,0 +1,259 @@
+import { expect } from 'chai'
+import { Grommet } from 'grommet'
+import React from 'react'
+import sinon from 'sinon'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import zooTheme from '@zooniverse/grommet-theme'
+
+import { QuestionInput } from './QuestionInput'
+
+describe('Component > QuestionInput', function () {
+  describe('with checkbox type', function () {
+    it('should render without crashing', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Eating',
+              value: 'TNG'
+            }}
+            questionId='WHTBHVRSDS'
+            theme={zooTheme}
+            type='checkbox'
+          />
+        </Grommet>
+      )
+      expect(screen).to.be.ok()
+    })
+
+    it('should render the label', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Eating',
+              value: 'TNG'
+            }}
+            questionId='WHTBHVRSDS'
+            theme={zooTheme}
+            type='checkbox'
+          />
+        </Grommet>
+      )
+      expect(screen.getByText('Eating')).to.exist()
+    })
+
+    it('should have input not as the document active element', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Eating',
+              value: 'TNG'
+            }}
+            questionId='WHTBHVRSDS'
+            theme={zooTheme}
+            type='checkbox'
+          />
+        </Grommet>
+      )
+      expect(screen.getByRole('checkbox', { name: 'Eating' })).to.not.equal(document.activeElement)
+    })
+
+    describe('with hasFocus true', function () {
+      it('should have input as the document active element', function () {
+        render(
+          <Grommet
+            theme={zooTheme}
+            themeMode='light'
+          >
+            <QuestionInput
+              hasFocus
+              option={{
+                label: 'Eating',
+                value: 'TNG'
+              }}
+              questionId='WHTBHVRSDS'
+              theme={zooTheme}
+              type='checkbox'
+            />
+          </Grommet>
+        )
+        expect(screen.getByRole('checkbox', { name: 'Eating' })).to.equal(document.activeElement)
+      })
+    })
+
+    describe('onChange', function () {
+      it('should call handleCheckBoxChange with checked and value', function () {
+        const handleCheckBoxChangeSpy = sinon.spy()
+        render(
+          <Grommet
+            theme={zooTheme}
+            themeMode='light'
+          >
+            <QuestionInput
+              handleCheckBoxChange={handleCheckBoxChangeSpy}
+              option={{
+                label: 'Eating',
+                value: 'TNG'
+              }}
+              questionId='WHTBHVRSDS'
+              theme={zooTheme}
+              type='checkbox'
+            />
+          </Grommet>
+        )
+        expect(handleCheckBoxChangeSpy).to.not.have.been.called()
+        userEvent.click(screen.getByRole('checkbox', { name: 'Eating' }))
+        expect(handleCheckBoxChangeSpy).to.have.been.calledWith('TNG', true)
+      })
+    })
+  })
+
+  describe('with radio type', function () {
+    it('should render without crashing', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Yes',
+              value: 'S'
+            }}
+            questionId='RTHRNNGPRSNT'
+            theme={zooTheme}
+            type='radio'
+          />
+        </Grommet>
+      )
+      expect(screen).to.be.ok()
+    })
+
+    it('should render the label', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Yes',
+              value: 'S'
+            }}
+            questionId='RTHRNNGPRSNT'
+            theme={zooTheme}
+            type='radio'
+          />
+        </Grommet>
+      )
+      expect(screen.getByText('Yes')).to.exist()
+    })
+
+    it('should have input not as the document active element', function () {
+      render(
+        <Grommet
+          theme={zooTheme}
+          themeMode='light'
+        >
+          <QuestionInput
+            option={{
+              label: 'Yes',
+              value: 'S'
+            }}
+            questionId='RTHRNNGPRSNT'
+            theme={zooTheme}
+            type='radio'
+          />
+        </Grommet>
+      )
+      expect(screen.getByRole('radio', { name: 'Yes' })).to.not.equal(document.activeElement)
+    })
+
+    describe('with hasFocus true', function () {
+      it('should have input as the document active element', function () {
+        render(
+          <Grommet
+            theme={zooTheme}
+            themeMode='light'
+          >
+            <QuestionInput
+              hasFocus
+              option={{
+                label: 'Yes',
+                value: 'S'
+              }}
+              questionId='RTHRNNGPRSNT'
+              theme={zooTheme}
+              type='radio'
+            />
+          </Grommet>
+        )
+        expect(screen.getByRole('radio', { name: 'Yes' })).to.equal(document.activeElement)
+      })
+    })
+
+    describe('onClick', function () {
+      it('should call handleRadioChange with value and checked', function () {
+        const handleRadioChangeSpy = sinon.spy()
+        render(
+          <Grommet
+            theme={zooTheme}
+            themeMode='light'
+          >
+            <QuestionInput
+              handleRadioChange={handleRadioChangeSpy}
+              option={{
+                label: 'Yes',
+                value: 'S'
+              }}
+              questionId='RTHRNNGPRSNT'
+              theme={zooTheme}
+              type='radio'
+            />
+          </Grommet>
+        )
+        expect(handleRadioChangeSpy).to.not.have.been.called()
+        userEvent.click(screen.getByRole('radio', { name: 'Yes' }))
+        expect(handleRadioChangeSpy).to.have.been.calledWith('S')
+      })
+    })
+
+    describe('onKeyDown', function () {
+      it('should call handleRadioKeyDown on keyDown of the input', function () {
+        const handleRadioKeyDownSpy = sinon.spy()
+        render(
+          <Grommet
+            theme={zooTheme}
+            themeMode='light'
+          >
+            <QuestionInput
+              handleRadioKeyDown={handleRadioKeyDownSpy}
+              option={{
+                label: 'Yes',
+                value: 'S'
+              }}
+              questionId='RTHRNNGPRSNT'
+              theme={zooTheme}
+              type='radio'
+            />
+          </Grommet>
+        )
+        expect(handleRadioKeyDownSpy).to.not.have.been.called()
+        userEvent.type(screen.getByRole('radio', { name: 'Yes' }), '{backspace}')
+        expect(handleRadioKeyDownSpy).to.have.been.calledOnce()
+      })
+    })
+  })
+})
