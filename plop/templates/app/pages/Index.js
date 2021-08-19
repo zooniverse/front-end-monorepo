cb2915f6520b7bd478de7a3254515a1580dee81f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-08-19 20:47:26 +0100

    Remove plop

--- plop/templates/app/pages/Index.js
+++
@@ -1,9 +0,0 @@
-import { Heading } from 'grommet'
-import React from 'react'
-
-export default () => (
-  <div>
-    <Heading><code>{{ name }}</code> app</Heading>
-    <p>This app handles all some routes, i.e. all routes under <code>/[ADD ROUTES HERE]</code>.</p>
-  </div>
-)
