d0480f4921d554cf409d817ab89e83030915ea72
Mark Bouslog <mcbouslog@gmail.com>
2021-09-02 17:09:41 -0500

    Add feedback survey reducer and tests

---
+++ packages/lib-classifier/src/store/feedback/strategies/survey/reducer.js
@@ -0,0 +1,22 @@
+function surveyReducer (rule, choices = []) {
+  const ruleIds = rule.choiceIds
+    .split(',')
+    .map(idString => idString.replace(/\W/g, '').toUpperCase())
+
+  const chosenIds = choices.map(choice => choice.choice)
+
+  let result
+
+  if (rule.choiceIds === '' && choices.length === 0) {
+    result = true
+  } else {
+    result = chosenIds.every(choiceId => ruleIds.indexOf(choiceId) > -1) &&
+      ruleIds.every(ruleId => chosenIds.indexOf(ruleId) > -1)
+  }
+
+  return Object.assign({}, rule, {
+    success: result
+  })
+}
+
+export default surveyReducer
---
+++ packages/lib-classifier/src/store/feedback/strategies/survey/reducer.spec.js
@@ -0,0 +1,58 @@
+import chai from 'chai'
+
+import reducer from './reducer'
+
+const expect = chai.expect
+
+describe('Feedback > Survey > Reducer', function () {
+  it('should not mutate the original object', function () {
+    const original = { choiceIds: 'AARDVARK,HONEYBADGER' }
+    const reduced = reducer(original)
+    expect(reduced).to.not.equal(original)
+  })
+
+  it('should handle correct choiceIds', function () {
+    const rule = { choiceIds: 'AARDVARK,HONEYBADGER' }
+    const result = reducer(rule, [
+      {
+        choice: 'AARDVARK'
+      },
+      {
+        choice: 'HONEYBADGER'
+      }
+    ])
+    expect(result.success).to.be.true()
+  })
+
+  it('should handle an incorrect choiceIds', function () {
+    const rule = { choiceIds: 'AARDVARK,HONEYBADGER' }
+    const result = reducer(rule, [
+      {
+        choice: 'BUSHPIG'
+      }
+    ])
+    expect(result.success).to.be.false()
+  })
+
+  it('should handle a partial match of choiceIds', function () {
+    const rule = { choiceIds: 'AARDVARK,HONEYBADGER' }
+    const result = reducer(rule, [
+      {
+        choice: 'HONEYBADGER'
+      }
+    ])
+    expect(result.success).to.be.false()
+  })
+
+  it('should handle a correct empty choiceIds', function () {
+    const rule = { choiceIds: '' }
+    const result = reducer(rule, [])
+    expect(result.success).to.be.true()
+  })
+
+  it('should handle an incorrect empty choiceIds', function () {
+    const rule = { choiceIds: 'AARDVARK,HONEYBADGER' }
+    const result = reducer(rule, [])
+    expect(result.success).to.be.false()
+  })
+})
