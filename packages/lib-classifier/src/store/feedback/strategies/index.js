e307916d676dda41735491947b6f1de9542ed81c
Mark Bouslog <mcbouslog@gmail.com>
2021-09-02 17:06:33 -0500

    Add survey to feedback index

--- packages/lib-classifier/src/store/feedback/strategies/index.js
+++ packages/lib-classifier/src/store/feedback/strategies/index.js
@@ -3,11 +3,13 @@ import column from './drawing/column'
 import radial from './drawing/radial'
 import dud from './dud'
 import singleAnswerQuestion from './single-answer-question'
+import survey from './survey'
 
 export default {
   column,
   dud,
   graph2drange,
   radial,
-  singleAnswerQuestion
+  singleAnswerQuestion,
+  survey
 }
