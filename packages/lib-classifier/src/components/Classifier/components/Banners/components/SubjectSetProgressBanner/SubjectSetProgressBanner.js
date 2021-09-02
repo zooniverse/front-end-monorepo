f12fb3b7d1ed510488233549d9595398482eddc6
Sarah Allen <sarah@zooniverse.org>
2021-09-02 16:35:53 -0500

    Remove cruft from refactoring work

--- packages/lib-classifier/src/components/Classifier/components/Banners/components/SubjectSetProgressBanner/SubjectSetProgressBanner.js
+++ packages/lib-classifier/src/components/Classifier/components/Banners/components/SubjectSetProgressBanner/SubjectSetProgressBanner.js
@@ -8,34 +8,28 @@ import Banner from '../Banner'
 counterpart.registerTranslations('en', en)
 
 function SubjectSetProgressBanner({ subject, workflow }) {
-  const subjectNumber = subject?.priority ?? -1
+  const subjectTotal = workflow?.subjectSet.set_member_subjects_count
+  const background = (subject?.alreadySeen || subject?.retired) ? 'status-critical' : 'status-ok'
+  const color = (subject?.alreadySeen || subject?.retired) ? 'neutral-6' : 'neutral-7'
+  let statusText = subject?.alreadySeen ? `${counterpart('SubjectSetProgressBanner.alreadySeen')}` : ''
+  statusText = subject?.retired ? `${counterpart('SubjectSetProgressBanner.finished')}` : statusText
+  const progressText = counterpart('SubjectSetProgressBanner.bannerText', {
+    number: subject?.priority,
+    total: subjectTotal
+  })
+  const tooltipText = counterpart('SubjectSetProgressBanner.tooltipText')
 
-  if (workflow?.grouped && subjectNumber !== -1) {
-    const subjectTotal = workflow?.subjectSet.set_member_subjects_count
-    const background = (subject?.alreadySeen || subject?.retired) ? 'status-critical' : 'status-ok'
-    const color = (subject?.alreadySeen || subject?.retired) ? 'neutral-6' : 'neutral-7'
-    let statusText = subject?.alreadySeen ? `${counterpart('SubjectSetProgressBanner.alreadySeen')}` : ''
-    statusText = subject?.retired ? `${counterpart('SubjectSetProgressBanner.finished')}` : statusText
-    const progressText = counterpart('SubjectSetProgressBanner.bannerText', {
-      number: subject?.priority,
-      total: subjectTotal
-    })
-    const tooltipText = counterpart('SubjectSetProgressBanner.tooltipText')
+  const bannerText = statusText ? `${progressText} (${statusText})` : progressText
 
-    const bannerText = statusText ? `${progressText} (${statusText})` : progressText
-
-    return (
-      <Banner
-        background={background}
-        bannerText={bannerText}
-        color={color}
-        show
-        tooltipText={tooltipText}
-      />
-    )
-  }
-
-  return null
+  return (
+    <Banner
+      background={background}
+      bannerText={bannerText}
+      color={color}
+      show
+      tooltipText={tooltipText}
+    />
+  )
 }
 
 SubjectSetProgressBanner.propTypes = {
