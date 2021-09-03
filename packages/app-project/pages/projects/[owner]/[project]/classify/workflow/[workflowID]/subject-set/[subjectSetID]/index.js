60e696d26059e9a34b254c5c5c6a09803edccb7e
Jim O'Donnell <jim@zooniverse.org>
2021-09-03 17:21:59 +0100

    Set default pageTitle as null (#2404)
    `undefined` values can't be encoded as JSON, so this sets the default `pageTitle` to `null` for workflow pages.

--- packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/index.js
+++ packages/app-project/pages/projects/[owner]/[project]/classify/workflow/[workflowID]/subject-set/[subjectSetID]/index.js
@@ -8,7 +8,7 @@ export async function getServerSideProps({ params, query, req, res }) {
   const { subjectSetID, workflowID } = params
   const { workflows } = defaultProps
   const workflow = workflows.find(workflow => workflow.id === workflowID)
-  let pageTitle = workflow?.displayName
+  let pageTitle = workflow?.displayName || null
   if (workflow?.grouped) {
     workflow.subjectSets = await fetchSubjectSets(workflow, env)
     const subjectSet = workflow.subjectSets.find(subjectSet => subjectSet.id === subjectSetID)
