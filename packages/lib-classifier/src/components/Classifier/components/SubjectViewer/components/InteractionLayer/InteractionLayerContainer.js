137210c2bc9e9600c4ff5c20f9128e176906e1a6
Jim O'Donnell <jim@zooniverse.org>
2021-09-15 14:43:02 +0100

    Remove inject Rewrite `InteractionLayerContainer` to use `withStores`, and remove `inject`.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayerContainer.js
@@ -1,15 +1,15 @@
-import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
 import InteractionLayer from './InteractionLayer'
 import PreviousMarks from './components/PreviousMarks'
 import SHOWN_MARKS from '@helpers/shownMarks'
+import { withStores } from '@helpers'
 
-function storeMapper(stores) {
-  const activeStepAnnotations = stores.classifierStore.annotatedSteps.latest?.annotations
-  const { activeStepTasks } = stores.classifierStore.workflowSteps
-  const { frame, move } = stores.classifierStore.subjectViewer
+function storeMapper(classifierStore) {
+  const activeStepAnnotations = classifierStore.annotatedSteps.latest?.annotations
+  const { activeStepTasks } = classifierStore.workflowSteps
+  const { frame, move } = classifierStore.subjectViewer
 
   const [activeInteractionTask] = activeStepTasks.filter(
     (task) => task.type === 'drawing' || task.type === 'transcription'
@@ -18,114 +18,102 @@ function storeMapper(stores) {
     annotation => annotation.taskType === 'drawing' || annotation.taskType === 'transcription'
   )
 
+  const {
+    activeMark,
+    activeTool,
+    activeToolIndex,
+    hidingIndex,
+    marks,
+    setActiveMark,
+    shownMarks,
+    taskKey
+  } = activeInteractionTask || {}
+
+  const disabled = activeTool?.disabled
+
   return {
-    activeInteractionTask,
+    activeMark,
+    activeTool,
+    activeToolIndex,
     annotation,
     frame,
-    move
+    hidingIndex,
+    marks,
+    move,
+    setActiveMark,
+    shownMarks,
+    taskKey
   }
 }
 
-@inject(storeMapper)
-@observer
-class InteractionLayerContainer extends Component {
-  render() {
-    const {
-      activeInteractionTask,
-      annotation,
-      frame,
-      height,
-      move,
-      scale,
-      width,
-      played,
-      duration
-    } = this.props
+export function InteractionLayerContainer({
+  activeMark,
+  activeTool,
+  activeToolIndex,
+  annotation,
+  disabled = false,
+  frame = 0,
+  height,
+  hidingIndex,
+  marks = [],
+  move = false,
+  scale = 1,
+  setActiveMark = () => {},
+  shownMarks = SHOWN_MARKS.ALL,
+  taskKey = '',
+  width,
+  played,
+  duration
+}) {
 
-    const {
-      activeMark,
-      activeTool,
-      activeToolIndex,
-      hidingIndex,
-      marks,
-      setActiveMark,
-      shownMarks,
-      taskKey
-    } = activeInteractionTask
+  const newMarks =
+    shownMarks === SHOWN_MARKS.NONE ? marks.slice(hidingIndex) : marks
+  const visibleMarksPerFrame = newMarks?.filter(
+    (mark) => mark.frame === frame
+  )
 
-    const newMarks =
-      shownMarks === SHOWN_MARKS.NONE ? marks.slice(hidingIndex) : marks
-    const visibleMarksPerFrame = newMarks?.filter(
-      (mark) => mark.frame === frame
-    )
-
-    return (
-      <>
-        {activeInteractionTask && activeTool && (
-          <InteractionLayer
-            activeMark={activeMark}
-            activeTool={activeTool}
-            activeToolIndex={activeToolIndex}
-            annotation={annotation}
-            disabled={activeTool.disabled}
-            frame={frame}
-            height={height}
-            key={taskKey}
-            marks={visibleMarksPerFrame}
-            move={move}
-            scale={scale}
-            played={played}
-            duration={duration}
-            setActiveMark={setActiveMark}
-            width={width}
-          />
-        )}
-        <PreviousMarks scale={scale} />
-      </>
-    )
-  }
+  return (
+    <>
+      {activeTool && (
+        <InteractionLayer
+          activeMark={activeMark}
+          activeTool={activeTool}
+          activeToolIndex={activeToolIndex}
+          annotation={annotation}
+          disabled={disabled}
+          frame={frame}
+          height={height}
+          key={taskKey}
+          marks={visibleMarksPerFrame}
+          move={move}
+          scale={scale}
+          played={played}
+          duration={duration}
+          setActiveMark={setActiveMark}
+          width={width}
+        />
+      )}
+      <PreviousMarks scale={scale} />
+    </>
+  )
 }
 
-InteractionLayerContainer.wrappedComponent.propTypes = {
-  activeInteractionTask: PropTypes.shape({
-    activeMark: PropTypes.object,
-    activeTool: PropTypes.shape({
-      disabled: PropTypes.bool
-    }),
-    marks: PropTypes.array,
-    setActiveMark: PropTypes.func,
-    shownMarks: PropTypes.string,
-    taskKey: PropTypes.string
-  }),
+InteractionLayerContainer.propTypes = {
+  activeMark: PropTypes.object,
+  activeTool: PropTypes.object,
   disabled: PropTypes.bool,
+  duration: PropTypes.number,
   frame: PropTypes.number,
   height: PropTypes.number.isRequired,
   interactionTaskAnnotations: PropTypes.array,
+  marks: PropTypes.array,
   move: PropTypes.bool,
-  scale: PropTypes.number,
   played: PropTypes.number,
-  duration: PropTypes.number,
+  scale: PropTypes.number,
+  setActiveMark: PropTypes.func,
+  shownMarks: PropTypes.string,
+  taskKey: PropTypes.string,
   width: PropTypes.number.isRequired
 }
 
-InteractionLayerContainer.wrappedComponent.defaultProps = {
-  activeInteractionTask: {
-    activeMark: undefined,
-    activeTool: {
-      disabled: false
-    },
-    marks: [],
-    setActiveMark: () => {},
-    shownMarks: SHOWN_MARKS.ALL,
-    taskKey: ''
-  },
-  disabled: false,
-  frame: 0,
-  interactionTaskAnnotations: [],
-  move: false,
-  scale: 1,
-  played: undefined, // used for tracking video progress from 0 - 1. if undefined, subject is not a video
-  duration: undefined // total length of video. if undefined, subject is not a video
-}
-
-export default InteractionLayerContainer
+export default withStores(InteractionLayerContainer, storeMapper)
