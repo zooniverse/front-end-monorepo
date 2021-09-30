610a06a671c32774cea288fb750484c486875c94
Campbell Allen <campbell.allen@gmail.com>
2021-09-30 19:41:34 +0100

    ensure datatable UI can use desc sort order at subject-set-search API (#2466)
    * allow desc sort order via datatable UI
    
    subject set search API allows the sort order to be desc via special _sort_desc param, https://docs.datasette.io/en/stable/json_api.html#special-table-arguments
    
    Grommet datatable provides the 'asc' or 'desc' direction key on the callback function - use this in the API call
    
    * Rename vars
    
    * Change sort order
    
    * Add fix for visual hiccup when sorting
    
    * Add isFetching message
    
    Co-authored-by: Shaun A. Noordin <shaun.a.noordin@gmail.com>

--- packages/app-project/src/shared/components/SubjectPicker/helpers/fetchSubjects.js
+++ packages/app-project/src/shared/components/SubjectPicker/helpers/fetchSubjects.js
@@ -1,11 +1,15 @@
 const API_HOST = 'https://subject-set-search-api.zooniverse.org/subjects'
+const ASCENDING_SORT = '_sort'
+const DESCENDING_SORT = '_sort_desc'
 
 export default async function fetchSubjects(
   subjectSetID,
   query='',
-  sortField='priority'
+  sortField='priority',
+  sortOrder='asc'
 ) {
-  const url = `${API_HOST}/${subjectSetID}.json?${query}&_sort=${sortField}`
+  const sortOrderParam = 'asc'.localeCompare(sortOrder) ? DESCENDING_SORT : ASCENDING_SORT
+  const url = `${API_HOST}/${subjectSetID}.json?${query}&${sortOrderParam}=${sortField}`
   const mode = 'cors'
   const response = await fetch(url, { mode })
   const data = await response.json()
