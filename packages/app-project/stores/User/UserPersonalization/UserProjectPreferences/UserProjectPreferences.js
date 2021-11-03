121b8a7dbd4abf40ac08cabbccfaa8c5c6f2ee6d
Jim O'Donnell <jim@zooniverse.org>
2021-11-03 14:36:53 +0000

    Refactor UPP data-fetching Fetch the full UPP resource when the stored user changes. When refreshing the UPP resource, fetch data in the background and only refresh stored preferences, so as to avoid overwriting the stored classification count.

--- packages/app-project/stores/User/UserPersonalization/UserProjectPreferences/UserProjectPreferences.js
+++ packages/app-project/stores/User/UserPersonalization/UserProjectPreferences/UserProjectPreferences.js
@@ -45,27 +45,19 @@ const UserProjectPreferences = types
 
   }))
   .actions(self => {
-    const _fetch = flow(function * _fetch() {
+    async function _fetch() {
       const { client, project, user } = getRoot(self)
-      self.setLoadingState(asyncStates.loading)
-      try {
-        const token = yield auth.checkBearerToken()
-        const authorization = `Bearer ${token}`
-        const query = {
-          project_id: project.id,
-          user_id: user.id
-        }
-
-        const response = yield client.panoptes.get('/project_preferences', query, { authorization })
-        const [preferences] = response.body.project_preferences
-        if (preferences) {
-          self.setResource(preferences)
-        }
-        self.setLoadingState(asyncStates.success)
-      } catch (error) {
-        self.handleError(error)
+      const token = await auth.checkBearerToken()
+      const authorization = `Bearer ${token}`
+      const query = {
+        project_id: project.id,
+        user_id: user.id
       }
-    })
+
+      const response = await client.panoptes.get('/project_preferences', query, { authorization })
+      const [preferences] = response.body.project_preferences
+      return preferences
+    }
 
     return {
       reset() {
@@ -97,13 +89,29 @@ const UserProjectPreferences = types
       },
 
       fetchResource: flow(function* fetchResource() {
-        if (!self.id) {
-          yield _fetch()
+        try {
+          if (!self.id) {
+            self.setLoadingState(asyncStates.loading)
+            const preferences = yield _fetch()
+            if (preferences) {
+              self.setResource(preferences)
+            }
+            self.setLoadingState(asyncStates.success)
+          }
+        } catch (error) {
+          self.handleError(error)
         }
       }),
 
       refreshResource: flow(function * refreshResource() {
-        yield _fetch()
+        try {
+          const preferences = yield _fetch()
+          if (preferences) {
+            self.preferences = preferences.preferences
+          }
+        } catch (error) {
+          self.handleError(error)
+        }
       })
     }
   })
