a3e24a97ad2f5b21f66f0f8fc3683ee78d27c978
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-11-15 17:27:30 +0800

    Project Announcements now have vertical layout

--- packages/app-project/src/components/Announcements/components/GenericAnnouncement/components/NarrowGenericAnnouncement/NarrowGenericAnnouncement.js
+++ packages/app-project/src/components/Announcements/components/GenericAnnouncement/components/NarrowGenericAnnouncement/NarrowGenericAnnouncement.js
@@ -1,8 +1,17 @@
 import { Box } from 'grommet'
 import { CloseButton, Markdownz } from '@zooniverse/react-components'
 import { bool, string, func } from 'prop-types'
+import styled from 'styled-components'
 
 import markdownzComponents from '../../helpers/markdownzComponents'
+
+// Spaces the paragraphs created by Markdown a little more nicely.
+// Box.gap='large' doesn't seem to work.
+const SpacedBox = styled(Box)`
+  p {
+    margin: 0.25em 0;
+  }
+`
 
 function NarrowGenericAnnouncement(props) {
   const {
@@ -16,11 +25,13 @@ function NarrowGenericAnnouncement(props) {
   return (
     <Box align='center' background={color} fill='horizontal' pad={{ horizontal: 'small', vertical: 'xsmall'} }>
       <Box align='center' direction='row' gap='small' justify='between'>
-        <Markdownz components={markdownzComponents}>
-          {announcement}
-        </Markdownz>
+        <SpacedBox direction='column'>
+          <Markdownz components={markdownzComponents}>
+            {announcement}
+          </Markdownz>
+        </SpacedBox>
         {children}
-        {dismissable && 
+        {dismissable &&
           <CloseButton closeFn={closeFn} />}
       </Box>
     </Box>
