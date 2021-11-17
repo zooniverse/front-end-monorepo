8a4ef9f358a8042e22d6c27b36ef0a8ab7a40a6a
Delilah Clement <delilahclement@gmail.com>
2021-11-16 18:41:35 -0600

    fix LocaleSwitcher storybook bug

--- packages/app-project/src/components/ProjectHeader/components/LocaleSwitcher/LocaleSwitcher.stories.js
+++ packages/app-project/src/components/ProjectHeader/components/LocaleSwitcher/LocaleSwitcher.stories.js
@@ -1,6 +1,9 @@
 import { Grommet } from 'grommet'
 import zooTheme from '@zooniverse/grommet-theme'
 import LocaleSwitcher from './LocaleSwitcher'
+import Router from 'next/router'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import PropTypes from 'prop-types'
 
 export default {
   title: 'Project App / Shared / Project Header / LocaleSwitcher',
@@ -9,8 +12,35 @@ export default {
 
 const availableLocales = ['en', 'fr']
 
+function RouterMock ({ children }) {
+  const mockRouter = {
+    asPath: '/projects/zooniverse/snapshot-serengeti/about/research',
+    locale: 'en',
+    push: () => {},
+    prefetch: () => new Promise((resolve, reject) => {}),
+    query: {
+      owner: 'zooniverse',
+      project: 'snapshot-serengeti'
+    }
+  }
+
+  Router.router = mockRouter
+
+  return (
+    <RouterContext.Provider value={mockRouter}>
+      {children}
+    </RouterContext.Provider>
+  )
+}
+
+RouterMock.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
 export const Default = () => (
   <Grommet theme={zooTheme}>
-    <LocaleSwitcher availableLocales={availableLocales} />
+    <RouterMock>
+      <LocaleSwitcher availableLocales={availableLocales} />
+    </RouterMock>
   </Grommet>
 )
