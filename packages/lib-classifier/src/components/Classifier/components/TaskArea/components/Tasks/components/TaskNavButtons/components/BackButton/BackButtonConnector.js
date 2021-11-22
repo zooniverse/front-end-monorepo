50177776f28f8de5e3395adabbccbe3d8f9ef36f
Jim O'Donnell <jim@zooniverse.org>
2021-11-22 09:27:51 +0000

    Refactor task nav buttons to use withStores Use `@helpers/withStores` to connect the task nav buttons to the stores. Refactor the views to check whether or not a button should be visible. Fallback to default view props if there's no active subject.

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/components/BackButton/BackButtonConnector.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/components/TaskNavButtons/components/BackButton/BackButtonConnector.js
@@ -1,44 +1,37 @@
-import { MobXProviderContext, observer } from 'mobx-react'
-import React, { useContext } from 'react'
+import { withStores } from '@helpers'
 
 import BackButton from './BackButton'
 
-function withStores(Component) {
-  function BackButtonConnector(props) {
-    const {
-      classifierStore: {
-        subjects: {
-          active: subject
-        },
-        workflows: {
-          active: {
-            configuration: {
-              persist_annotations: persistAnnotations
-            }
-          }
+function storeMapper(store) {
+  const {
+    subjects: {
+      active: subject
+    },
+    workflows: {
+      active: {
+        configuration: {
+          persist_annotations: persistAnnotations
         }
       }
-    } = props.store || useContext(MobXProviderContext)
-
-    const { back, canUndo } = subject?.stepHistory || { back: () => {}, canUndo: false }
-
-    if (!canUndo) {
-      return null
     }
+  } = store
+
+  if (subject?.stepHistory) {
+    const { back, canUndo } = subject?.stepHistory
 
     function onClick() {
       back(persistAnnotations)
     }
 
-    return (
-      <Component
-        onClick={onClick}
-        persistAnnotations={persistAnnotations}
-        {...props}
-      />
-    )
+    return {
+      canUndo,
+      onClick,
+      persistAnnotations
+    }
   }
-  return observer(BackButtonConnector)
+
+  return {}
+
 }
 
-export default withStores(BackButton)
+export default withStores(BackButton, storeMapper)
