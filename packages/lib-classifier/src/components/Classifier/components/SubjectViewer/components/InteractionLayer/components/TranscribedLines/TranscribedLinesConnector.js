e24741485b1196751f996cebe06118d6821fc933
Jim O'Donnell <jim@zooniverse.org>
2021-11-24 11:12:34 +0000

    Add the current annotation to TranscribedLines

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/TranscribedLines/TranscribedLinesConnector.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/components/TranscribedLines/TranscribedLinesConnector.js
@@ -23,14 +23,20 @@ function storeMapper(store) {
   } = store
 
   const consensusLines = subject.transcriptionReductions?.consensusLines(frame)
+  const activeStepAnnotations = subject.stepHistory.latest.annotations
 
   // We expect there to only be one
   const [transcriptionTask] = findTasksByType('transcription')
   // We want to observe the marks array for changes, so pass that as a separate prop.
   const marks = transcriptionTask?.marks
+  // find the corresponding annotation
+  const annotation = activeStepAnnotations.find(
+    annotation => annotation.task === transcriptionTask?.taskKey
+  )
 
   const valid = step?.isValid
   return {
+    annotation,
     frame,
     invalid: !valid,
     transcriptionTask,
@@ -45,6 +51,7 @@ const defaultWorkflow = {
 }
 
 function TranscribedLinesContainer ({
+  annotation,
   frame,
   invalid = false,
   transcriptionTask = {},
@@ -58,6 +65,7 @@ function TranscribedLinesContainer ({
   if (workflow?.usesTranscriptionTask && shownMarks === SHOWN_MARKS.ALL && consensusLines.length > 0) {
     return (
       <TranscribedLines
+        annotation={annotation}
         frame={frame}
         invalidMark={invalid}
         lines={consensusLines}
