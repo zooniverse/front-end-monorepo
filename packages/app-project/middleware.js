53a3e274353cf2cb36df7d051bbc029a4587ecbf
Jim O'Donnell <jim@zooniverse.org>
2021-12-06 11:08:15 +0000

    Rewrite URLs to set API environment Rewrite public URLs to add the Panoptes API environment to the page path: /[env]/[owner/[project]].

---
+++ packages/app-project/middleware.js
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req, event) {
+  const defaultEnv = process.env.PANOPTES_ENV ?? 'staging'
+  const { pathname, searchParams } = req.nextUrl
+  const env = searchParams.get('env') ?? defaultEnv
+
+  /*
+    Images etc. are served from /projects/assets.
+  */
+  if (pathname.startsWith('/assets')) {
+    return
+  }
+  /*
+    Bypass internal NextJS requests.
+  */
+  if (pathname.startsWith('/_next')) {
+    return
+  }
+  /*
+    Don't redirect or rewrite the health check.
+  */
+  if (pathname === '/Index') {
+    return
+  }
+
+  if (searchParams.has('language')) {
+    const locale = searchParams.get('language')
+    const url = req.nextUrl.clone()
+    url.searchParams.delete('language')
+    try {
+      url.locale = locale
+    } catch (error) {
+      url.pathname = `/${locale}${pathname}`
+    }
+    return NextResponse.redirect(url)
+  }
+
+  /*
+    Project pages are served from /projects/staging/[owner]/[project]
+    and /projects/production/[owner]/[project]
+  */
+  return NextResponse.rewrite(`/${env}${pathname}`)
+}
