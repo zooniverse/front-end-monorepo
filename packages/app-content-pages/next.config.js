f79584d8443ac50477fba8a5d4498867807e57be
Jim O'Donnell <jim@zooniverse.org>
2022-01-17 14:59:38 +0000

    Remove require-control (#2638)
    Remove `require-control` from the NextJS config for `app-content-pages`. It worked around an issue that’s since been fixed.

--- packages/app-content-pages/next.config.js
+++ packages/app-content-pages/next.config.js
@@ -3,14 +3,7 @@ require('dotenv').config()
 const { execSync } = require('child_process')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
-const { setAliases } = require('require-control')
 const withSourceMaps = require('@zeit/next-source-maps')()
-
-// Fixes the FOUC due to SC not collecting styles from symlinked packages
-// https://github.com/styled-components/styled-components/issues/2322
-setAliases({
-  'styled-components': resolveHoisted('styled-components')
-})
 
 function commitID () {
   try {
@@ -55,10 +48,6 @@ const nextConfig = {
     config.resolve = Object.assign({}, config.resolve, { alias })
     return config
   }
-}
-
-function resolveHoisted (packageName) {
-  return path.resolve(path.join(__dirname, '../../node_modules', packageName))
 }
 
 module.exports = withSourceMaps(nextConfig)
