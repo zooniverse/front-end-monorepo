7f9a9098098fdbf044001bbe23af9827a7ee522f
Jim O'Donnell <jim@zooniverse.org>
2022-01-20 13:05:31 +0000

    Check for prioritized workflows Reset the subject store on mount, if the workflow is not prioritized.

--- packages/lib-classifier/src/components/Classifier/Classifier.js
+++ packages/lib-classifier/src/components/Classifier/Classifier.js
@@ -111,9 +111,20 @@ export default function Classifier({
           whitelist: ['fieldGuide', 'projects', 'subjects', 'subjectSets', 'tutorials', 'workflows', 'workflowSteps']
         })
         console.log('store hydrated from local storage')
-        const { subjects } = classifierStore
+        const { subjects, workflows } = classifierStore
+        if (!workflows.active?.prioritized) {
+          /*
+          In this case, we delete the saved queue so that
+          refreshing the classifier will load a new, randomised
+          subject queue.
+          */
+          subjects.reset()
+        }
         if (subjects.active) {
-          classifierStore.subjects.setActiveSubject(classifierStore.subjects.active.id)
+          /*
+            This is a hack to start a new classification from a snapshot.
+          */
+          subjects.setActiveSubject(subjects.active.id)
         }
       } catch (error) {
         console.log('store snapshot error.')
