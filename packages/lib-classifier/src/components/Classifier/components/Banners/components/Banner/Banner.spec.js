1660652d5f6558c75c3ff78eadc239708b762ef4
Delilah Clement <delilahclement@gmail.com>
2022-01-24 16:44:50 -0600

    refactor Banner.spec

--- packages/lib-classifier/src/components/Classifier/components/Banners/components/Banner/Banner.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/Banners/components/Banner/Banner.spec.js
@@ -1,5 +1,8 @@
 import { shallow, mount } from 'enzyme'
 import React from 'react'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '../../../../../../../test/i18n/i18n-for-tests'
+import sinon from 'sinon'
 
 import { Banner, Tooltip } from './Banner'
 
@@ -21,7 +24,7 @@ const COMPONENT = (
   />
 )
 
-describe('Component > Banner', function () {
+describe.only('Component > Banner', function () {
   before(function () {
     wrapper = shallow(COMPONENT)
   })
@@ -35,13 +38,28 @@ describe('Component > Banner', function () {
   })
 
   describe('`Why am I seeing this?` button', function () {
+    let useTranslationStub
+
     before(function () {
-      wrapper = mount(COMPONENT)
+      useTranslationStub = sinon.stub(i18n, 't')
+      wrapper = mount(
+        <I18nextProvider i18n={i18n}>
+          {COMPONENT}
+        </I18nextProvider>
+      )
+    })
+
+    after(function () {
+      useTranslationStub.restore()
     })
 
     it('should exist', function () {
-      const button = wrapper.find('button')
-      expect(button.html()).to.include('Why am I seeing this?')
+      const button = wrapper.childAt(0).find('button')
+      expect(button).to.exist()
+    })
+    
+    it('should use a translation function for its label', function () {
+      expect(useTranslationStub).to.have.been.calledOnce()
     })
 
     describe('behaviour on click', function () {
