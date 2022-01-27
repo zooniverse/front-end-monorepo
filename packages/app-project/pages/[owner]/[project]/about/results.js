35ec3c43e2754ffdd1f86661437fa8219dc113cc
Delilah Clement <delilahclement@gmail.com>
2022-01-27 15:26:34 -0600

    refactor ProjectAboutPage

--- packages/app-project/pages/[owner]/[project]/about/results.js
+++ packages/app-project/pages/[owner]/[project]/about/results.js
@@ -1,12 +1,14 @@
 import getDefaultPageProps from '@helpers/getDefaultPageProps'
 export { default } from '@screens/ProjectAboutPage'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export async function getServerSideProps({ params, query, req, res }) {
+export async function getServerSideProps({ locale, params, query, req, res }) {
   const { notFound, props } = await getDefaultPageProps({ params, query, req, res })
 
   return {
     notFound,
     props: {
+      ...(await serverSideTranslations(locale, ['screens'])),
       pageTitle: 'Results',
       pageType: 'results',
       ...props
