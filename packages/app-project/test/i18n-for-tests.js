13832e9118317fd4bba6abcecabb72e3178fbeed
Delilah Clement <delilahclement@gmail.com>
2022-01-27 16:24:03 -0600

    refactor AboutDropdownNav.spec

---
+++ packages/app-project/test/i18n-for-tests.js
@@ -0,0 +1,15 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  lng: 'cimode', // this means the translation functions will simply return the key
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false // not needed for react as it escapes by default
+  },
+  react: {
+    useSuspense: false
+  }
+})
+
+export default i18n
