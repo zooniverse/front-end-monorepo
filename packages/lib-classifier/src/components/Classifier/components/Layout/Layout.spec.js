9fd455c88470f3f15be80418a5ca60cf1a6122b4
Jim O'Donnell <jim@zooniverse.org>
2022-02-01 20:33:52 +0000

    Classifier: remove @inject from Layout (#2732)
    Remove `@inject` from `Layout` and rewrite it as a functional component.

--- packages/lib-classifier/src/components/Classifier/components/Layout/Layout.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/Layout/Layout.spec.js
@@ -1,10 +1,11 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 
+import mockStore from '@test/mockStore'
 import Layout from './Layout'
 
 describe('Component > Layout', function () {
   it('should render without crashing', function () {
-    shallow(<Layout.wrappedComponent />)
+    shallow(<Layout store={mockStore()} />)
   })
 })
