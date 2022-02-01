e1c5857bac8f836282fffb37733ff14d373bdafa
Jim O'Donnell <jim@zooniverse.org>
2022-02-01 10:39:48 +0000

    Remove @inject from ImageToolbar Remove `@inject` from image toolbar buttons. Rewrite class components as functional components.

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/ResetButton/ResetButtonContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/ResetButton/ResetButtonContainer.js
@@ -1,38 +1,14 @@
-import { inject, observer } from 'mobx-react'
-import PropTypes from 'prop-types'
-import React from 'react'
-
+import { withStores } from '@helpers'
 import ResetButton from './ResetButton'
 
-function storeMapper (stores) {
+function storeMapper(classifierStore) {
   const {
     resetView
-  } = stores.classifierStore.subjectViewer
+  } = classifierStore.subjectViewer
 
   return {
-    resetView
+    onClick: resetView
   }
 }
 
-@inject(storeMapper)
-@observer
-class ResetButtonContainer extends React.Component {
-  render () {
-    const { disabled, resetView } = this.props
-    return (
-      <ResetButton disabled={disabled} onClick={resetView} />
-    )
-  }
-}
-
-ResetButtonContainer.wrappedComponent.propTypes = {
-  disabled: PropTypes.bool,
-  resetView: PropTypes.func
-}
-
-ResetButtonContainer.wrappedComponent.defaultProps = {
-  disabled: false,
-  resetView: () => console.log('reset view')
-}
-
-export default ResetButtonContainer
+export default withStores(ResetButton, storeMapper)
