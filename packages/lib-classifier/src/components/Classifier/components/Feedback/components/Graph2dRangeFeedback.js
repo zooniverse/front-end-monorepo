8c48b2703c37482640859fc5c9efb1a481d3596e
Jim O'Donnell <jim@zooniverse.org>
2022-02-22 10:50:38 +0000

    Refactor Graph2dRangeFeedback - replace `@inject` with `@helpers/withStores`. - rewrite `Graph2dRangeFeedback` as a function. - wrap `drawFeedbackBrushes` in `useCallback`.

--- packages/lib-classifier/src/components/Classifier/components/Feedback/components/Graph2dRangeFeedback.js
+++ packages/lib-classifier/src/components/Classifier/components/Feedback/components/Graph2dRangeFeedback.js
@@ -1,19 +1,24 @@
-import * as d3 from 'd3'
+import { brushX, select } from 'd3'
 import { toJS } from 'mobx'
-import { inject, observer } from 'mobx-react'
-import React, { Component } from 'react'
+import React, { useCallback } from 'react'
+
+import { withStores } from '@helpers'
 
 import LightCurveViewer from '../../SubjectViewer/components/LightCurveViewer'
 
-function storeMapper (stores) {
-  const annotations = stores.classifierStore.classifications.currentAnnotations
+function storeMapper(classifierStore) {
   const {
-    applicableRules,
-    showModal: feedback
-  } = stores.classifierStore.feedback
-  const {
-    active: subject
-  } = stores.classifierStore.subjects
+    classifications: {
+      currentAnnotations: annotations
+    },
+    feedback: {
+      applicableRules,
+      showModal: feedback
+    },
+    subjects: {
+      active: subject
+    }
+  } = classifierStore
 
   return {
     annotations,
@@ -23,24 +28,21 @@ function storeMapper (stores) {
   }
 }
 
-// Noting that mobx decorators are outdated https://michel.codes/blogs/mobx6
-@inject(storeMapper)
-@observer
-class Graph2dRangeFeedback extends Component {
-  constructor () {
-    super()
+function Graph2dRangeFeedback({
+  annotations = [],
+  applicableRules = [],
+  feedback = false,
+  subject = null
+}) {
 
-    this.drawFeedbackBrushes = this.drawFeedbackBrushes.bind(this)
-  }
-
-  drawFeedbackBrushes (d3annotationsLayer, repositionBrush) {
+  const drawFeedbackBrushes = useCallback(function (d3annotationsLayer, repositionBrush) {
     const annotationBrushes = []
-    this.props.annotations.forEach(annotation => {
+    annotations.forEach(annotation => {
       const { value } = toJS(annotation)
       value.forEach((marking, i) => {
         const markingBrush = {
           id: i,
-          brush: d3.brushX(),
+          brush: brushX(),
           maxX: (marking.x + (marking.width / 2)),
           minX: (marking.x - (marking.width / 2))
         }
@@ -48,16 +50,14 @@ class Graph2dRangeFeedback extends Component {
       })
     })
 
-    const ruleBrushes = []
-    this.props.applicableRules.forEach(rule => {
-      const ruleBrush = {
+    const ruleBrushes = applicableRules.map(rule => {
+      return {
         id: rule.id,
-        brush: d3.brushX(),
+        brush: brushX(),
         maxX: (parseFloat(rule.x) + (parseFloat(rule.width) / 2) + parseFloat(rule.tolerance)),
         minX: (parseFloat(rule.x) - (parseFloat(rule.width) / 2) - parseFloat(rule.tolerance)),
         success: rule.success
       }
-      ruleBrushes.push(ruleBrush)
     })
 
     const feedbackBrushes = annotationBrushes.concat(ruleBrushes)
@@ -72,14 +72,14 @@ class Graph2dRangeFeedback extends Component {
       .insert('g', '.brush')
       .attr('class', 'brush')
       .attr('id', (brush) => (`brush-${brush.id}`))
-      .each(function applyBrushLogic (feedbackBrush) { // Don't use ()=>{}
-        feedbackBrush.brush(d3.select(this)) // Apply the brush logic to the <g.brush> element (i.e. 'this')
+      .each(function applyBrushLogic(feedbackBrush) { // Don't use ()=>{}
+        feedbackBrush.brush(select(this)) // Apply the brush logic to the <g.brush> element (i.e. 'this')
       })
 
     // Modify brush fill color...
     brushSelection
-      .each(function fill (feedbackBrush) { // Don't use ()=>{}
-        d3.select(this)
+      .each(function fill(feedbackBrush) { // Don't use ()=>{}
+        select(this)
           .attr('class', 'brush')
           .selectAll('.selection')
           .style('fill', () => {
@@ -99,19 +99,17 @@ class Graph2dRangeFeedback extends Component {
 
       repositionBrush(feedbackBrush, d3brush)
     })
-  }
+  }, [annotations, applicableRules])
 
-  render () {
-    const { feedback, subject } = this.props
-    return (
-      <LightCurveViewer
-        drawFeedbackBrushes={this.drawFeedbackBrushes}
-        feedback={feedback}
-        subject={subject}
-        subjectId={subject.id}
-      />
-    )
-  }
+  return (
+    <LightCurveViewer
+      drawFeedbackBrushes={drawFeedbackBrushes}
+      feedback={feedback}
+      subject={subject}
+      subjectId={subject.id}
+    />
+  )
 }
 
-export default Graph2dRangeFeedback
+export default withStores(Graph2dRangeFeedback, storeMapper)
+export { Graph2dRangeFeedback }
