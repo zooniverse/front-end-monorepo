73f5d3e7f44eb34b8d64dfc54d52f819de509348
Delilah Clement <delilahclement@gmail.com>
2022-02-21 19:34:52 -0600

    refactor AboutProject

--- packages/app-project/src/shared/components/AboutProject/AboutProject.js
+++ packages/app-project/src/shared/components/AboutProject/AboutProject.js
@@ -1,20 +1,18 @@
 import { Markdownz } from '@zooniverse/react-components'
-import counterpart from 'counterpart'
 import { string } from 'prop-types'
 import { Paragraph } from 'grommet'
+import { useTranslation } from 'next-i18next'
 
-import en from './locales/en'
 import ContentBox from '../ContentBox'
-
-counterpart.registerTranslations('en', en)
 
 const components = {
   p: (nodeProps) => <Paragraph children={nodeProps.children} margin={{ top: 'none' }} />
 }
 
 function AboutProject ({ description, projectName }) {
+  const { t } = useTranslation('components')
   return (
-    <ContentBox title={counterpart('AboutProject.title', { projectName })}>
+    <ContentBox title={t('AboutProject.title', { projectName })}>
       <Markdownz children={description} components={components} />
     </ContentBox>
   )
