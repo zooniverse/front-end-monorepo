40b1103ec029eb9ed6bac0d8e78add85792d10b6
Delilah Clement <delilahclement@gmail.com>
2022-03-09 20:57:36 -0600

    refactor FavouritesButton stories

--- packages/lib-react-components/src/FavouritesButton/FavouritesButton.stories.js
+++ packages/lib-react-components/src/FavouritesButton/FavouritesButton.stories.js
@@ -1,44 +1,57 @@
-import { storiesOf } from '@storybook/react'
-import { withKnobs, boolean } from '@storybook/addon-knobs'
 import zooTheme from '@zooniverse/grommet-theme'
-import { Grommet } from 'grommet'
+import { Box, Grommet } from 'grommet'
 import React from 'react'
 import readme from './README.md'
 
 import FavouritesButton from './'
 
-const config = {
-  docs: {
-    description: {
-      component: readme
-    }
-  }
-}
-
 const CAT = {
   favorite: false,
   id: '123',
   locations: [
-    { 'image/jpeg': 'https://panoptes-uploads.zooniverse.org/335/0/2d63944e-f0bc-4fc5-8531-f603886513a1.jpeg' }
+    {
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/335/0/2d63944e-f0bc-4fc5-8531-f603886513a1.jpeg'
+    }
   ]
 }
 
-storiesOf('Components/Favourites Button', module)
-  .addDecorator(withKnobs)
-  .add('plain', () => (
-    <Grommet theme={{ ...zooTheme, dark: boolean('Dark theme', false) }}>
-      <FavouritesButton
-        disabled={boolean('disabled', false)}
-        subject={CAT}
-      />
+export default {
+  title: 'Components/FavouritesButton',
+  component: FavouritesButton,
+  args: {
+    dark: false,
+    disabled: false,
+    subject: CAT
+  },
+  argTypes: {
+    onClick: {
+      action: 'clicked'
+    }
+  },
+  notes: {
+    markdown: readme
+  }
+}
+
+export function Default({ dark, disabled, onClick, subject }) {
+  const theme = { ...zooTheme, dark }
+  return (
+    <Grommet
+      background={{
+        dark: 'dark-1',
+        light: 'light-1'
+      }}
+      theme={theme}
+      themeMode={dark ? 'dark' : 'light'}
+    >
+      <Box align='center' justify='center' height='medium'>
+        <FavouritesButton
+          disabled={disabled}
+          onClick={onClick}
+          subject={subject}
+        />
+      </Box>
     </Grommet>
-  ), config)
-  .add('initially checked', () => (
-    <Grommet theme={{ ...zooTheme, dark: boolean('Dark theme', false) }}>
-      <FavouritesButton
-        checked
-        disabled={boolean('disabled', false)}
-        subject={CAT}
-      />
-    </Grommet>
-  ), config)
+  )
+}
