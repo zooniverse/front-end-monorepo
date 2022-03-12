afddbb4683c5664eea33d6889257316d5f5b5f11
Jim O'Donnell <jim@zooniverse.org>
2022-03-12 15:27:36 +0000

    Classifier: Remove @inject from SubjectViewer (#2890)
    Refactor the generic `SubjectViewer` container as a functional component, and remove the deprecated `@inject` decorator.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.spec.js
@@ -2,33 +2,33 @@ import asyncStates from '@zooniverse/async-states'
 import { shallow } from 'enzyme'
 import React from 'react'
 
-import SubjectViewer from './SubjectViewer'
+import { SubjectViewer } from './SubjectViewer'
 import SingleImageViewer from './components/SingleImageViewer'
 import VariableStarViewer from './components/VariableStarViewer'
 
 describe('Component > SubjectViewer', function () {
   it('should render without crashing', function () {
-    const wrapper = shallow(<SubjectViewer.wrappedComponent />)
+    const wrapper = shallow(<SubjectViewer />)
     expect(wrapper).to.be.ok()
   })
 
   it('should render nothing if the subject store is initialized', function () {
-    const wrapper = shallow(<SubjectViewer.wrappedComponent subjectQueueState={asyncStates.initialized} />)
+    const wrapper = shallow(<SubjectViewer subjectQueueState={asyncStates.initialized} />)
     expect(wrapper.type()).to.be.null()
   })
 
   it('should render a loading indicator if the subject store is loading', function () {
-    const wrapper = shallow(<SubjectViewer.wrappedComponent subjectQueueState={asyncStates.loading} />)
+    const wrapper = shallow(<SubjectViewer subjectQueueState={asyncStates.loading} />)
     expect(wrapper.text()).to.equal('Loading')
   })
 
   it('should render nothing if the subject store errors', function () {
-    const wrapper = shallow(<SubjectViewer.wrappedComponent subjectQueueState={asyncStates.error} />)
+    const wrapper = shallow(<SubjectViewer subjectQueueState={asyncStates.error} />)
     expect(wrapper.type()).to.be.null()
   })
 
   it('should render a subject viewer if the subject store successfully loads', function () {
-    const wrapper = shallow(<SubjectViewer.wrappedComponent subjectQueueState={asyncStates.success} subject={{ viewer: 'singleImage' }} />)
+    const wrapper = shallow(<SubjectViewer subjectQueueState={asyncStates.success} subject={{ viewer: 'singleImage' }} />)
     expect(wrapper.find(SingleImageViewer)).to.have.lengthOf(1)
   })
 
@@ -43,14 +43,14 @@ describe('Component > SubjectViewer', function () {
       }
     }
 
-    const wrapper = shallow(<SubjectViewer.wrappedComponent subjectQueueState={asyncStates.success} subject={{ viewer: 'variableStar', viewerConfiguration }} />)
+    const wrapper = shallow(<SubjectViewer subjectQueueState={asyncStates.success} subject={{ viewer: 'variableStar', viewerConfiguration }} />)
     expect(wrapper.find(VariableStarViewer).props().viewerConfiguration).to.deep.equal(viewerConfiguration)
   })
 
   describe('when there is an null viewer because of invalid subject media', function () {
     it('should render null', function () {
       const wrapper = shallow(
-        <SubjectViewer.wrappedComponent
+        <SubjectViewer
           subjectQueueState={asyncStates.success}
           subject={{ viewer: null }}
         />
