e2419c51d0e4eac2e56dda946d4dba65dc45429a
ErikOstlund <eostlund74@gmail.com>
2022-03-23 11:35:29 -0500

    refactored to use beforeEach

--- packages/lib-classifier/src/plugins/drawingTools/components/Circle/Circle.spec.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Circle/Circle.spec.js
@@ -6,36 +6,33 @@ import { default as CircleMark } from '../../models/marks/Circle'
 import DragHandle from '../DragHandle'
 
 describe('Circle tool', function () {
-  it('should render without crashing', function () {
-    const wrapper = shallow(
-      <Circle mark={{ x_center: 200, y_center: 200, r: 50 }} />
-    )
-    expect(wrapper).to.be.ok()
-  })
-
-  it('should render a circle with the coordinates provided', function () {
-    const wrapper = shallow(
-      <Circle mark={{ x_center: 200, y_center: 200, r: 50 }} scale={1} />
-    )
-    expect(wrapper.containsMatchingElement(<circle r={50} />)).to.be.true()
-  })
-
-  it('should render an active circle with one drag handle', function () {
-    const wrapper = shallow(
-      <Circle active mark={{ x_center: 200, y_center: 200, r: 50 }} scale={1} />
-    )
-    expect(wrapper.find(DragHandle)).to.have.lengthOf(1)
-  })
-
-  it('should change the radius when drag handle is moved', function () {
-    const mark = CircleMark.create({
+  let mark
+  beforeEach(function () {
+    mark = CircleMark.create({
       id: 'circle1',
       toolType: 'circle',
       x_center: 200,
       y_center: 200,
       r: 50
     })
+  })
 
+  it('should render without crashing', function () {
+    const wrapper = shallow(<Circle mark={mark} />)
+    expect(wrapper).to.be.ok()
+  })
+
+  it('should render a circle with the coordinates provided', function () {
+    const wrapper = shallow(<Circle mark={mark} scale={1} />)
+    expect(wrapper.containsMatchingElement(<circle r={50} />)).to.be.true()
+  })
+
+  it('should render an active circle with one drag handle', function () {
+    const wrapper = shallow(<Circle active mark={mark} scale={1} />)
+    expect(wrapper.find(DragHandle)).to.have.lengthOf(1)
+  })
+
+  it('should change the radius when drag handle is moved', function () {
     const wrapper = mount(<Circle active mark={mark} scale={1} />)
 
     const dragMove = wrapper
