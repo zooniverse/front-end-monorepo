42c37305b3071ff027bfe7e18f9ec5a6e68821a3
ErikOstlund <eostlund74@gmail.com>
2022-03-23 15:08:20 -0500

    Code format fixes per our style guide

--- packages/lib-classifier/src/plugins/drawingTools/components/Ellipse/Ellipse.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Ellipse/Ellipse.js
@@ -6,36 +6,53 @@ import DragHandle from '../DragHandle'
 const GUIDE_DASH = [4, 4]
 const GUIDE_WIDTH = 1
 
-function Ellipse ({ active, mark, onFinish, scale }) {
+function Ellipse({ active, mark, onFinish, scale }) {
   const { x, y, rx, ry, angle } = mark
-  const guideWidth = GUIDE_WIDTH / scale;
+  const guideWidth = GUIDE_WIDTH / scale
 
-  function onXHandleDrag (e, d) {
-    const r = mark.getDistance(x, y , e.x, e.y)
-    const angle = mark.getAngle(x, y , e.x, e.y)
+  function onXHandleDrag(e, d) {
+    const r = mark.getDistance(x, y, e.x, e.y)
+    const angle = mark.getAngle(x, y, e.x, e.y)
     mark.setCoordinates({ x, y, rx: r, ry, angle })
   }
 
-  function onYHandleDrag (e, d) {
-    const r = mark.getDistance(x, y , e.x, e.y)
-    const angle = mark.getAngle(x, y , e.x, e.y) + 90
+  function onYHandleDrag(e, d) {
+    const r = mark.getDistance(x, y, e.x, e.y)
+    const angle = mark.getAngle(x, y, e.x, e.y) + 90
     mark.setCoordinates({ x, y, rx, ry: r, angle })
   }
 
   return (
-    <g
-      onPointerUp={active ? onFinish : undefined}
-    >
+    <g onPointerUp={active ? onFinish : undefined}>
       <ellipse rx={rx} ry={ry} />
 
-      {active &&
+      {active && (
         <g>
-          <line x1="0" y1="0" x2={rx} y2="0" strokeWidth={guideWidth} strokeDasharray={GUIDE_DASH} />
-          <line x1="0" y1="0" x2="0" y2={-1 * ry} strokeWidth={guideWidth} strokeDasharray={GUIDE_DASH} />
+          <line
+            x1='0'
+            y1='0'
+            x2={rx}
+            y2='0'
+            strokeWidth={guideWidth}
+            strokeDasharray={GUIDE_DASH}
+          />
+          <line
+            x1='0'
+            y1='0'
+            x2='0'
+            y2={-1 * ry}
+            strokeWidth={guideWidth}
+            strokeDasharray={GUIDE_DASH}
+          />
           <DragHandle dragMove={onXHandleDrag} x={rx} y={0} scale={scale} />
-          <DragHandle dragMove={onYHandleDrag} x={0} y={-1 * ry} scale={scale} />
+          <DragHandle
+            dragMove={onYHandleDrag}
+            x={0}
+            y={-1 * ry}
+            scale={scale}
+          />
         </g>
-      }
+      )}
     </g>
   )
 }
