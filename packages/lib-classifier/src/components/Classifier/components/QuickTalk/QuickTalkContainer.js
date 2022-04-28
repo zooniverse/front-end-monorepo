4ed27c936435ba7cd39ab9788aed2cdf2160908f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-04-28 22:03:40 +0100

    add fixTalkClientAuth to resolve issues of Talk comments not posting on app-project

--- packages/lib-classifier/src/components/Classifier/components/QuickTalk/QuickTalkContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/QuickTalk/QuickTalkContainer.js
@@ -13,7 +13,7 @@ function storeMapper (stores) {
   const {
     active: subject
   } = stores.classifierStore.subjects
-  
+
   /*
   Experimental Check
   - As of Aug 2021, this component is experimental.
@@ -23,14 +23,14 @@ function storeMapper (stores) {
     active: project
   } = stores.classifierStore.projects
   const enabled = project?.experimental_tools.includes('quicktalk')
-  
+
   /*
   Quick Fix: use authClient to check User resource within the QuickTalk component itself
   - Long-term, app-project should be responsible for managing the User resource.
   - see https://github.com/zooniverse/front-end-monorepo/discussions/2362
    */
   const authClient = stores.classifierStore.authClient
-  
+
   return {
     authClient,
     enabled,
@@ -41,7 +41,7 @@ function storeMapper (stores) {
 class QuickTalkContainer extends React.Component {
   constructor () {
     super()
-    
+
     this.state = {
       comments: [],
       authors: {},
@@ -49,26 +49,27 @@ class QuickTalkContainer extends React.Component {
       postCommentStatus: asyncStates.initialized,
       postCommentStatusMessage: '',
       userId: '',
+      bearerToken: undefined,
     }
   }
-  
+
   componentDidMount () {
     if (!this.props.enabled) return
-    
+
     this.fetchComments()
     this.checkUser()
   }
-  
+
   componentDidUpdate (prevProps) {
     if (!this.props.enabled) return
-    
+
     const props = this.props
     if (props.subject !== prevProps.subject) {  // Note: this high level comparison actually works. Comparing props.subject?.id !== prevProps.subject?.id however causes a crash when getting a new subject, since prevProps.subject would have been removed from memory.
       this.fetchComments()
       this.checkUser()
     }
   }
-  
+
   /*
   Quick Fix: use authClient to check User resource within the QuickTalk component itself
   - see https://github.com/zooniverse/front-end-monorepo/discussions/2362
@@ -76,23 +77,24 @@ class QuickTalkContainer extends React.Component {
   async checkUser () {
     const authClient = this.props?.authClient
     if (!authClient) return
-    
+
     const authorization = await getBearerToken(authClient)  // Check bearer token to ensure session hasn't timed out
     const user = await authClient.checkCurrent()
     this.setState({
-      userId: (authorization && user) ? user.id : undefined
+      userId: (authorization && user) ? user.id : undefined,
+      bearerToken: (authorization && user) ? authorization : undefined
     })
   }
-  
+
   fetchComments () {
     this.resetComments()
-    
+
     const subject = this.props?.subject
     const project = subject?.project
     if (!subject || !project) {
       return
     }
-    
+
     const section = 'project-' + project.id
     const query = {
       section: section,
@@ -101,24 +103,24 @@ class QuickTalkContainer extends React.Component {
       page: 1,
       sort: 'created_at',  // PFE used '-created_at' to sort in reverse order, and I have no idea why.
     }
-        
+
     talkClient.type('comments').get(query)
       .then (comments =>{
         this.setState({ comments })
-        
+
         let author_ids = []
         let authors = {}
         let authorRoles = {}
 
         author_ids = comments.map(comment => comment.user_id)
         author_ids = author_ids.filter((id, i) => author_ids.indexOf(id) === i)
-        
+
         apiClient.type('users').get({ id: author_ids })
           .then(users => {
             users.forEach(user => authors[user.id] = user)
             this.setState({ authors })
           })
-        
+
         talkClient.type('roles')
           .get({
             user_id: author_ids,
@@ -134,7 +136,7 @@ class QuickTalkContainer extends React.Component {
           })
       })
   }
-  
+
   resetComments () {
     this.setState({
       comments: [],
@@ -142,7 +144,7 @@ class QuickTalkContainer extends React.Component {
       authorRoles: {},
     })
   }
-  
+
   async postComment (text) {
     const { t } = this.props
     const subject = this.props?.subject
@@ -151,15 +153,15 @@ class QuickTalkContainer extends React.Component {
     if (!subject || !project || !authClient) {
       return
     }
-    
+
     const section = `project-${project.id}`
     const discussionTitle = `Subject ${subject.id}`
-    
+
     this.setState({
       postCommentStatus: asyncStates.loading,
       postCommentStatusMessage: '',
     })
-    
+
     try {
       if (!text || text.trim().length === 0) {
         throw new Error(t('QuickTalk.errors.noText'))
@@ -180,7 +182,7 @@ class QuickTalkContainer extends React.Component {
       const boards = await talkClient.type('boards').get({ section, subject_default: true })
       const defaultBoard = boards && boards[0]
       if (!defaultBoard) throw('A board for subject comments has not been setup for this project yet.')
-      
+
       // Next, attempt to find if the Subject already has a discussion attached to it.
       const discussions = await talkClient.type('discussions').get({
         board_id: defaultBoard.id,
@@ -188,7 +190,7 @@ class QuickTalkContainer extends React.Component {
         subject_default: true,
       })
       const existingDiscussion = discussions && discussions[0]
-      
+
       if (existingDiscussion) { // Add to the existing discussion
 
         const comment = {
@@ -197,6 +199,7 @@ class QuickTalkContainer extends React.Component {
           discussion_id: +existingDiscussion.id,
         }
 
+        this.fixTalkClientAuth()
         await talkClient.type('comments').create(comment).save()
         this.setState({
           postCommentStatus: asyncStates.success,
@@ -221,6 +224,7 @@ class QuickTalkContainer extends React.Component {
           comments: comments,
         }
 
+        this.fixTalkClientAuth()
         await talkClient.type('discussions').create(discussion).save()
         this.setState({
           postCommentStatus: asyncStates.success,
@@ -237,13 +241,27 @@ class QuickTalkContainer extends React.Component {
       })
     }
   }
-    
+
+  /*
+  Authentication data in talkClient.headers is lost when user is logged in
+  via app-client instead of lib-classifier. Reasons are unknown so far, but
+  this fixes it.
+   */
+  fixTalkClientAuth () {
+    if (!talkClient?.headers) return
+    if (this.state.bearerToken) {
+      talkClient.headers.Authorization = this.state.bearerToken
+    } else {
+      delete talkClient.headers.Authorization
+    }
+  }
+
   render () {
     const {
       subject,
       enabled,
     } = this.props
-    
+
     const {
       comments,
       authors,
@@ -256,7 +274,7 @@ class QuickTalkContainer extends React.Component {
     if (!subject || !enabled) {
       return null
     }
-    
+
     return (
       <QuickTalk
         subject={subject}
