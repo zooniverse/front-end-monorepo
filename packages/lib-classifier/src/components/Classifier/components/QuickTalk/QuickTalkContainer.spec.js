55665986f1c490bb5c1d919222c90ba7c118f232
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-04-29 17:35:44 +0100

    QuickTalkContainer - add very basic tests

---
+++ packages/lib-classifier/src/components/Classifier/components/QuickTalk/QuickTalkContainer.spec.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { QuickTalkContainer } from './QuickTalkContainer'
+
+const subject = {
+  id: '100001',
+  talkURL: 'https://www.zooniverse.org/projects/zootester/test-project/talk/subjects/100001',
+}
+
+const authClient = {}
+
+describe.only('Component > QuickTalkContainer', function () {
+  describe('when collapsed', function () {
+    beforeEach(function () {
+      render(
+        <QuickTalkContainer
+          authClient={authClient}
+          enabled={true}
+          subject={subject}
+        />
+      )
+    })
+
+    it('should render without crashing', function () {
+      screen.debug()
+      expect(screen.queryByTestId('quicktalk-button')).to.exist()
+    })
+  })
+})
