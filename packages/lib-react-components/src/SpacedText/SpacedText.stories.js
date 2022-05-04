12433ea7ebe4126319b8dca402980c8f0f1b6611
Delilah Clement <delilahclement@gmail.com>
2022-05-04 14:07:52 -0500

    refactor SpacedText story to use 'controls'

--- packages/lib-react-components/src/SpacedText/SpacedText.stories.js
+++ packages/lib-react-components/src/SpacedText/SpacedText.stories.js
@@ -1,42 +1,50 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
-import zooTheme from '@zooniverse/grommet-theme';
-import { Grommet } from 'grommet';
-import React from 'react';
+import zooTheme from '@zooniverse/grommet-theme'
+import { Grommet } from 'grommet'
+import React from 'react'
 
-import readme from './README.md';
-import SpacedText from './SpacedText';
-
-const config = {
-  docs: {
-    description: {
-      component: readme,
-    },
-  },
-};
+import readme from './README.md'
+import SpacedText from './SpacedText'
 
 export default {
-  title: 'Components/SpacedText',
-  decorators: [withKnobs],
-};
+  title: 'Components / SpacedText',
+  component: SpacedText,
+  args: {
+    children: 'Zooniverse Spaced Text',
+    dark: false,
+    size: 'medium',
+    uppercase: true,
+    weight: 'normal'
+  },
+  argTypes: {
+    size: {
+      options: ['small', 'medium', 'large', 'xlarge'],
+      control: { type: 'select' }
+    },
+    weight: {
+      options: ['normal', 'bold'],
+      control: { type: 'radio' }
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: readme
+      }
+    }
+  }
+}
 
-export const Default = () => (
-  <Grommet theme={zooTheme}>
-    <SpacedText>{text('Text', 'Zooniverse spaced text')}</SpacedText>
+export const Default = ({ children, dark, size, uppercase, weight }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
+    <SpacedText size={size} uppercase={uppercase} weight={weight}>
+      {children}
+    </SpacedText>
   </Grommet>
-);
-
-Default.story = {
-  name: 'default',
-  parameters: config,
-};
-
-export const Bold = () => (
-  <Grommet theme={zooTheme}>
-    <SpacedText weight="bold">{text('Text', 'Zooniverse spaced text')}</SpacedText>
-  </Grommet>
-);
-
-Bold.story = {
-  name: 'bold',
-  parameters: config,
-};
+)
