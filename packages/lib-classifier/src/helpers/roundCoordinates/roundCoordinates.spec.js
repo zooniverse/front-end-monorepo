f7f5bbafecd5b4f28f866e33f0c752b4c5a8a665
ErikOstlund <eostlund74@gmail.com>
2022-05-05 11:35:30 -0500

    Added more test case

--- packages/lib-classifier/src/helpers/roundCoordinates/roundCoordinates.spec.js
+++ packages/lib-classifier/src/helpers/roundCoordinates/roundCoordinates.spec.js
@@ -12,5 +12,25 @@ describe('round coordinates', () => {
       roundedX: 34.79,
       roundedY: 234.89
     })
+
+    expect(
+      roundCoordinates({
+        x: 201.005,
+        y: 110.0000000005
+      })
+    ).to.deep.equal({
+      roundedX: 201.01,
+      roundedY: 110
+    })
+
+    expect(
+      roundCoordinates({
+        x: 129.1,
+        y: 252.0
+      })
+    ).to.deep.equal({
+      roundedX: 129.1,
+      roundedY: 252.0
+    })
   })
 })
