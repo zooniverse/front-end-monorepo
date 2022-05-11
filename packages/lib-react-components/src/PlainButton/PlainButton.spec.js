211ed889512ad888f1f10ddb2d5303ccaee72970
Delilah Clement <delilahclement@gmail.com>
2022-05-10 20:26:51 -0500

    add extra checks for link or button in specs

--- packages/lib-react-components/src/PlainButton/PlainButton.spec.js
+++ packages/lib-react-components/src/PlainButton/PlainButton.spec.js
@@ -14,21 +14,27 @@ describe('Components > PlainButton', function () {
   })
 
   it('should render a button element when not disabled and href is empty', function () {
-    const { getByRole } = render(<Default href='' />)
+    const { getByRole, queryByRole } = render(<Default href='' />)
     const button = getByRole('button')
+    const link = queryByRole('link')
+    expect(link).to.be.null()
     expect(button).exists()
   })
 
   it('should render an anchor element when not disabled and href is defined', function () {
-    const { getByRole } = render(<Default href={mockHref} />)
+    const { getByRole, queryByRole } = render(<Default href={mockHref} />)
     const link = getByRole('link')
+    const button = queryByRole('button')
     expect(link).exists()
     expect(link.href).include(mockHref)
+    expect(button).to.be.null()
   })
 
   it('should render a button element when disabled and href is defined', function () {
-    const { getByRole } = render(<Default disabled href={mockHref} />)
+    const { getByRole, queryByRole } = render(<Default disabled href={mockHref} />)
     const button = getByRole('button')
+    const link = queryByRole('link')
     expect(button).exists()
+    expect(link).to.be.null()
   })
 })
