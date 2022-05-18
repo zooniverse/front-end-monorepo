29a429da8a9824d46a04e59bc5f7b970cea39873
Mark Bouslog <mcbouslog@gmail.com>
2022-05-18 09:29:48 -0500

    Refactor SingleTextViewer story

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleTextViewer/SingleTextViewer.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleTextViewer/SingleTextViewer.stories.js
@@ -1,17 +1,31 @@
 import { Box, Grommet } from 'grommet'
+import { Provider } from 'mobx-react'
 import React from 'react'
+import asyncStates from '@zooniverse/async-states'
 import zooTheme from '@zooniverse/grommet-theme'
 
-import SingleTextViewer from './SingleTextViewer'
+import { SubjectFactory } from '@test/factories'
+import mockStore from '@test/mockStore'
 
-const content = 'Herbarium of the University of North Carolina\nSOUTH CAROLINA\nCharleston County\nGnaphalium peregrinum Fern,\nrailroad right-of-way, Johns Island Station on\nCounty Rt. 20 (wes t of Charleston.\nHarry E. Ahles 22002 April 2, 1957\nwith John G. Haesloop\nCollected for the “Flora of the Carolinas"'
+import SingleTextViewerConnector from './SingleTextViewerConnector'
+
+const defaultContent = 'Herbarium of the University of North Carolina\nSOUTH CAROLINA\nCharleston County\nGnaphalium peregrinum Fern,\nrailroad right-of-way, Johns Island Station on\nCounty Rt. 20 (wes t of Charleston.\nHarry E. Ahles 22002 April 2, 1957\nwith John G. Haesloop\nCollected for the “Flora of the Carolinas"'
 
 export default {
   title: 'Subject Viewers / SingleTextViewer',
-  component: SingleTextViewer,
+  component: SingleTextViewerConnector,
   args: {
-    content,
+    content: defaultContent,
+    contentLoadingState: asyncStates.success,
     dark: false
+  },
+  argTypes: {
+    contentLoadingState: {
+      control: {
+        type: 'select'
+      },
+      options: Object.values(asyncStates)
+    }
   },
   parameters: {
     viewport: {
@@ -20,7 +34,20 @@ export default {
   }
 }
 
-export function Default ({ content, dark }) {
+const store = mockStore()
+
+export function Default ({ content, contentLoadingState, dark }) {
+  const subjectSnapshot = SubjectFactory.build({
+    content,
+    contentLoadingState,
+    locations: [
+      { 'text/plain': 'http://localhost:8080/subjectContent.txt' }
+    ]
+  })
+
+  store.subjects.setResources([subjectSnapshot])
+  store.subjects.setActive(subjectSnapshot.id)
+
   return (
     <Grommet
       background={{
@@ -30,9 +57,11 @@ export function Default ({ content, dark }) {
       theme={zooTheme}
       themeMode={dark ? 'dark' : 'light'}
     >
-      <Box height='500px' width='large'>
-        <SingleTextViewer content={content} />
-      </Box>
+      <Provider classifierStore={store}>
+        <Box height='500px' width='large'>
+          <SingleTextViewerConnector />
+        </Box>
+      </Provider>
     </Grommet>
   )
 }
