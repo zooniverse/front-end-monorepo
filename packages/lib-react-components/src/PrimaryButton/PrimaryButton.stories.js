da32a7f5664dc2cfa64c81996b850e96ad884ee0
Delilah Clement <delilahclement@gmail.com>
2022-05-31 20:38:36 -0500

    add image label story and invalid option to theme dropdown

--- packages/lib-react-components/src/PrimaryButton/PrimaryButton.stories.js
+++ packages/lib-react-components/src/PrimaryButton/PrimaryButton.stories.js
@@ -16,7 +16,7 @@ export default {
   },
   argTypes: {
     color: {
-      options: ['blue', 'gold', 'green', 'teal'],
+      options: ['blue', 'gold', 'green', 'teal', 'invalid'],
       control: { type: 'select' }
     },
     onClick: {
@@ -45,3 +45,36 @@ export const Default = ({ color, dark, disabled, href, label, onClick }) => (
     </Box>
   </Grommet>
 )
+
+const imgLabel = (
+  <img
+    src='https://static.zooniverse.org/login.zooniverse.org/img/avatar.png'
+    alt='test image label'
+    width='30px'
+  />
+)
+
+export const ImgLabel = ({ color, dark, disabled, href, label, onClick }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
+    <Box pad='medium'>
+      <PrimaryButton
+        color={color}
+        disabled={disabled}
+        href={href}
+        label={label}
+        onClick={onClick}
+      />
+    </Box>
+  </Grommet>
+)
+
+ImgLabel.args = {
+  label: imgLabel
+}
