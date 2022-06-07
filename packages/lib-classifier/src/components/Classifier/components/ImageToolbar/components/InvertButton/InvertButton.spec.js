c64fdc356dcbcfcddbac7aae3e311e184c32ee1f
Mark Bouslog <mark@zooniverse.org>
2022-06-07 17:06:50 -0500

    Refactor InvertButton tests with react-testing-library

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/InvertButton/InvertButton.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/InvertButton/InvertButton.spec.js
@@ -1,31 +1,92 @@
-import { shallow } from 'enzyme'
+import { expect } from 'chai'
+import { Grommet } from 'grommet'
+import { Provider } from 'mobx-react'
 import React from 'react'
-import sinon from 'sinon'
-import InvertButton from './InvertButton'
-import i18n from '@test/i18n/i18n-for-tests'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import zooTheme from '@zooniverse/grommet-theme'
+
+import { WorkflowFactory } from '@test/factories'
+import mockStore from '@test/mockStore'
+
+import InvertButtonContainer from './InvertButtonContainer'
 
 describe('Component > InvertButton', function () {
-  it('should render without crashing', function () {
-    const wrapper = shallow(<InvertButton />)
-    expect(wrapper).to.be.ok()
+  function withStore (store) {
+    return function Wrapper ({ children }) {
+      return (
+        <Grommet theme={zooTheme}>
+          <Provider classifierStore={store}>
+            {children}
+          </Provider>
+        </Grommet>
+      )
+    }
+  }
+
+  it('should render null if disabled', function () {
+    const workflowSnapshot = WorkflowFactory.build({
+      configuration: {
+        invert_subject: false
+      }
+    })
+
+    const store = mockStore({
+      workflow: workflowSnapshot
+    })
+
+    render(
+      <InvertButtonContainer />, {
+        wrapper: withStore(store)
+      }
+    )
+
+    expect(screen.queryByRole('button')).to.be.null()
   })
 
   it('should have an `a11yTitle` label', function () {
-    const useTranslationStub = sinon.stub(i18n, 't').callsFake((key) => key)
-    const wrapper = shallow(<InvertButton />)
-    expect(wrapper.prop('a11yTitle')).exists()
-    expect(useTranslationStub).to.have.been.calledWith('ImageToolbar.InvertButton.ariaLabel')
-    useTranslationStub.restore()
+    const workflowSnapshot = WorkflowFactory.build({
+      configuration: {
+        invert_subject: true
+      }
+    })
+
+    const store = mockStore({
+      workflow: workflowSnapshot
+    })
+
+    render(
+      <InvertButtonContainer />, {
+        wrapper: withStore(store)
+      }
+    )
+
+    expect(screen.getByRole('button', { name: 'ImageToolbar.InvertButton.ariaLabel' })).to.exist()
   })
 
-  it('should call the onClick prop function on click', function () {
-    const spy = sinon.spy()
-    const wrapper = shallow(
-      <InvertButton
-        onClick={spy}
-      />
+  it('should change the subject viewer invert property on click', async function () {
+    const user = userEvent.setup({ delay: null })
+
+    const workflowSnapshot = WorkflowFactory.build({
+      configuration: {
+        invert_subject: true
+      }
+    })
+
+    const store = mockStore({
+      workflow: workflowSnapshot
+    })
+
+    expect(store.subjectViewer.invert).to.be.false()
+
+    render(
+      <InvertButtonContainer />, {
+        wrapper: withStore(store)
+      }
     )
-    wrapper.simulate('click')
-    expect(spy).to.have.been.called()
+
+    await user.click(screen.getByRole('button', { name: 'ImageToolbar.InvertButton.ariaLabel' }))
+
+    expect(store.subjectViewer.invert).to.be.true()
   })
 })
