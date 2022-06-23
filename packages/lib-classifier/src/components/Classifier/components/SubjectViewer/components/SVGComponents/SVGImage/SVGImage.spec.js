5c6884f3bd994673771099f157484ba468e1f84b
Mark Bouslog <mark@zooniverse.org>
2022-06-23 01:58:42 -0500

    Add SVGImage tests

---
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SVGComponents/SVGImage/SVGImage.spec.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import SVGImage from './SVGImage'
+
+describe('SVGImage', function () {
+  it('should render an image', function () {
+    render(
+      <SVGImage
+        naturalHeight={100}
+        naturalWidth={100}
+        src='https://some.domain/image.jpg'
+        subjectID='1234'
+      />
+    )
+
+    const image = screen.getByRole('img')
+    expect(image).to.exist()
+    expect(image.getAttribute('xlink:href')).to.equal('https://some.domain/image.jpg')
+    expect(image.getAttribute('aria-label')).to.equal('Subject 1234')
+    expect(image.getAttribute('filter')).to.be.null()
+  })
+
+  describe('with invert', function () {
+    it('should render an image with a defined filter attribute', function () {
+      render(
+        <SVGImage
+          invert
+          naturalHeight={100}
+          naturalWidth={100}
+          src='https://some.domain/image.jpg'
+          subjectID='1234'
+        />
+      )
+
+      const filter = document.querySelector('filter')
+      expect(filter).to.exist()
+      expect(filter.getAttribute('id')).to.equal('svg-invert-filter')
+
+      const image = screen.getByRole('img')
+      expect(image).to.exist()
+      expect(image.getAttribute('filter')).to.equal('url("#svg-invert-filter")')
+    })
+  })
+})
