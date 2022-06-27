2fbfa3e0ec2713cb7551e4ab39c70ab326f02d32
Mark Bouslog <mcbouslog@gmail.com>
2022-06-27 14:15:17 -0500

    Fix invert button for multi-frame subjects and line color consistency (#3325)
    * Fix invert for MultiFrameViewer
    
    * Refactor invert filter to exclusively SubjectImage
    
    * Show invert button, if enabled
    
    * Refactor stories for ImageToolbar and MultiFrameViewer
    
    * Refactor MultiFrame and SingleImage containers with SVGImage
    
    * Refactor tests for DraggableImage move
    
    * Remove useEffect from SVGImage
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>
    
    * Refactor SVGImage from style to filter property
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>
    
    * Add propTypes
    
    * Add SVGImage tests
    
    * Refactor tests with name, use alias path import
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>
    
    * Remove unnecessary attribute expectation
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/ImageToolbar.stories.js
@@ -7,6 +7,7 @@ import zooTheme from '@zooniverse/grommet-theme'
 import { SubjectFactory, WorkflowFactory } from '@test/factories'
 import mockStore from '@test/mockStore'
 import { StyledImageToolbarContainer, StyledImageToolbar, ViewerGrid } from '../Layout/components/DefaultLayout/DefaultLayout'
+import MultiFrameViewer from '../SubjectViewer/components/MultiFrameViewer'
 import SingleImageViewer from '../SubjectViewer/components/SingleImageViewer'
 import ImageToolbar from './ImageToolbar'
 
@@ -26,6 +27,26 @@ const subjectSnapshot = SubjectFactory.build({
   ]
 })
 
+const multiFrameSubjectSnapshot = SubjectFactory.build({
+  locations: [
+    {
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/production/subject_location/11f98201-1c3f-44d5-965b-e00373daeb18.jpeg'
+    },
+    {
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/production/subject_location/512649e3-33d7-4811-b0f2-64703e687160.jpeg'
+    },
+    {
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/production/subject_location/3eecf251-7205-4ef3-b13f-e930c97d92de.jpeg'
+    }
+  ],
+  metadata: {
+    default_frame: 1
+  }
+})
+
 const workflowSnapshot = WorkflowFactory.build({
   configuration: {
     invert_subject: true
@@ -37,7 +58,12 @@ const store = mockStore({
   workflow: workflowSnapshot
 })
 
-export function Default ({ dark }) {
+const multiFrameSubjectStore = mockStore({
+  subject: multiFrameSubjectSnapshot,
+  workflow: workflowSnapshot
+})
+
+export function withSingleImageViewer ({ dark }) {
   return (
     <Grommet
       background={{
@@ -55,6 +81,35 @@ export function Default ({ dark }) {
             <SingleImageViewer
               loadingState={asyncStates.success}
               subject={store.subjects.active}
+            />
+          </Box>
+          <StyledImageToolbarContainer>
+            <StyledImageToolbar />
+          </StyledImageToolbarContainer>
+        </ViewerGrid>
+      </Provider>
+    </Grommet>
+  )
+}
+
+export function withMultiFrameViewer ({ dark }) {
+  return (
+    <Grommet
+      background={{
+        dark: 'dark-1',
+        light: 'light-1'
+      }}
+      theme={zooTheme}
+      themeMode={dark ? 'dark' : 'light'}
+    >
+      <Provider
+        classifierStore={multiFrameSubjectStore}
+      >
+        <ViewerGrid>
+          <Box gridArea='subject'>
+            <MultiFrameViewer
+              loadingState={asyncStates.success}
+              subject={multiFrameSubjectStore.subjects.active}
             />
           </Box>
           <StyledImageToolbarContainer>
