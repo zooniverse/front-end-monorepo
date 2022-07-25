88c0d613b1ba2b599c3a4de5ff2d347833ab1556
Delilah Clement <delilahclement@gmail.com>
2022-07-25 10:31:14 -0500

    fix test env

--- packages/app-project/src/helpers/addQueryParams/addQueryParams.js
+++ packages/app-project/src/helpers/addQueryParams/addQueryParams.js
@@ -1,12 +1,12 @@
-/** Next.js withRouter decorator injects router into components as null in node so we need to explicitly check for falsy values */
+/** Next.js withRouter decorator injects router into components as null in node so we need to explicitly check for falsy values such as path and locale */
 /** This helper is used to form ProjectHeader links to PFE pages: Talk, Collections, Recents */
 
 import queryString from 'query-string'
 
 function addQueryParams (href, router, pfe) {
   const path = (router) ? router.asPath : ''
+  const locale = (router) ? router.locale : 'en'
   const query = queryString.extract(path)
-  const { locale } = router
 
   let newHref
   if (locale !== 'en' && pfe) {
