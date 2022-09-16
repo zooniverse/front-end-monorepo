2b2fadce22fe6bc01ea923fe68402600e739b0ae
Jim O'Donnell <jim@zooniverse.org>
2022-09-16 16:34:35 +0100

    Projects: add Admin Mode (#3666)
    * Projects: add Admin Mode
    - add `user.admin` to logged-in users.
    - add a `useAdminMode` hook to manage admin state across project pages.
    - add `AdminContainer` to the footer, which manages the 'Admin Mode' toggle button.
    - add the project admin link to project navigation.
    - add named exports for project components to make imports easier to write.
    
    * Add tests

--- packages/app-project/src/shared/components/StandardLayout/StandardLayout.spec.js
+++ packages/app-project/src/shared/components/StandardLayout/StandardLayout.spec.js
@@ -1,17 +1,95 @@
-import { shallow } from 'enzyme'
-import * as Router from 'next/router'
-import sinon from 'sinon'
+import zooTheme from '@zooniverse/grommet-theme'
+import { within } from '@testing-library/dom'
+import { render, screen } from '@testing-library/react'
+import { Grommet } from 'grommet'
+import { Provider } from 'mobx-react'
+import { applySnapshot } from 'mobx-state-tree'
 
-import StandardLayout from './StandardLayout'
+import initStore from '@stores'
+import StandardLayout from './StandardLayout.js'
 
 describe('Component > StandardLayout', function () {
-  let wrapper
+  let adminToggle
+  let zooHeader
+  let zooFooter
+
+  function withStore(snapshot) {
+    const store = initStore(false)
+    applySnapshot(store, snapshot)
+
+    return function Wrapper({ children }) {
+      return (
+        <Grommet theme={zooTheme}>
+          <Provider store={store}>
+            {children}
+          </Provider>
+        </Grommet>
+      )
+    }
+  }
 
   before(function () {
-    wrapper = shallow(<StandardLayout />)
+    const snapshot = {
+      project: {
+        configuration: {
+          languages: ['en']
+        },
+        slug: 'Foo/Bar',
+        strings: {
+          display_name: 'Foobar'
+        },
+        links: {
+          active_workflows: ['1']
+        }
+      }
+    }
+    render(<StandardLayout />, { wrapper: withStore(snapshot)})
+    zooHeader = screen.getByRole('banner')
+    zooFooter = screen.getByRole('contentinfo')
+    adminToggle = within(zooFooter).queryByRole('checkbox', { name: 'Admin Mode' })
   })
 
-  it('should render without crashing', function () {
-    expect(wrapper).to.be.ok()
+  it('should show the Zooniverse header', function () {
+    expect(zooHeader).to.be.ok()
+  })
+
+  it('should show the Zooniverse footer', function () {
+    expect(zooFooter).to.be.ok()
+  })
+
+  it('should not show the admin toggle', function () {
+    expect(adminToggle).to.be.null()
+  })
+
+  describe('with a logged-in admin', function () {
+    before(function () {
+      const snapshot = {
+        project: {
+          configuration: {
+            languages: ['en']
+          },
+          slug: 'Foo/Bar',
+          strings: {
+            display_name: 'Foobar'
+          },
+          links: {
+            active_workflows: ['1']
+          }
+        },
+        user: {
+          id: '1',
+          admin: true,
+          login: 'zooVolunteer'
+        }
+      }
+      render(<StandardLayout />, { wrapper: withStore(snapshot)})
+      zooHeader = screen.getByRole('banner')
+      zooFooter = screen.getByRole('contentinfo')
+      adminToggle = within(zooFooter).getByRole('checkbox', { name: 'Admin Mode' })
+    })
+
+    it('should show the admin toggle', function () {
+      expect(adminToggle).to.be.ok()
+    })
   })
 })
