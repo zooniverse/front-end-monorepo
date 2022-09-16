53260f7e3f57b69c0769f0c07ab4d13bf2d5345b
Jim O'Donnell <jim@zooniverse.org>
2022-09-16 17:01:30 +0100

    Project: Admin mode border styling Add the admin mode border styling to all project pages, except for beta projects. Beta projects always show a teal border.

--- packages/app-project/src/shared/components/StandardLayout/StandardLayout.spec.js
+++ packages/app-project/src/shared/components/StandardLayout/StandardLayout.spec.js
@@ -6,10 +6,11 @@ import { Provider } from 'mobx-react'
 import { applySnapshot } from 'mobx-state-tree'
 
 import initStore from '@stores'
-import StandardLayout from './StandardLayout.js'
+import StandardLayout, { adminBorderImage } from './StandardLayout.js'
 
 describe('Component > StandardLayout', function () {
   let adminToggle
+  let projectPage
   let zooHeader
   let zooFooter
 
@@ -90,6 +91,52 @@ describe('Component > StandardLayout', function () {
 
     it('should show the admin toggle', function () {
       expect(adminToggle).to.be.ok()
+      expect(adminToggle.checked).to.be.false()
+    })
+  })
+
+  describe('in admin mode', function () {
+    before(function () {
+      const snapshot = {
+        project: {
+          configuration: {
+            languages: ['en']
+          },
+          slug: 'Foo/Bar',
+          strings: {
+            display_name: 'Foobar'
+          },
+          links: {
+            active_workflows: ['1']
+          }
+        },
+        user: {
+          id: '1',
+          admin: true,
+          login: 'zooVolunteer'
+        }
+      }
+      window.localStorage.setItem('adminFlag', true)
+      render(<StandardLayout />, { wrapper: withStore(snapshot)})
+      projectPage = screen.getByTestId('project-page')
+      zooHeader = screen.getByRole('banner')
+      zooFooter = screen.getByRole('contentinfo')
+      adminToggle = within(zooFooter).getByRole('checkbox', { name: 'Admin Mode' })
+    })
+
+    after(function () {
+      window.localStorage.removeItem('adminFlag')
+    })
+
+    it('should have a striped border', function () {
+      expect(projectPage).to.be.ok()
+      const borderImage = window.getComputedStyle(projectPage)['border-image']
+      expect(borderImage).to.equal(adminBorderImage)
+    })
+
+    it('should show the admin toggle in the footer', function () {
+      expect(adminToggle).to.be.ok()
+      expect(adminToggle.checked).to.be.true()
     })
   })
 })
