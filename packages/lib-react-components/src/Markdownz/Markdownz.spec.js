5bb0a262f663f56d3d24b163cf6261dc62918e06
Jim O'Donnell <jim@zooniverse.org>
2022-09-26 17:43:12 +0100

    Remove unused imports
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>

--- packages/lib-react-components/src/Markdownz/Markdownz.spec.js
+++ packages/lib-react-components/src/Markdownz/Markdownz.spec.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import sinon from 'sinon'
-import { Anchor, Paragraph } from 'grommet'
 import { render, screen } from '@testing-library/react'
 
 import Media from '../Media'
