d49d66185547e3fb61e805f46cdc72fa1211a968
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-10-03 23:35:01 +0100

    Update tests for RotateHandle

--- packages/lib-classifier/src/plugins/drawingTools/components/RotateHandle/RotateHandle.spec.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/RotateHandle/RotateHandle.spec.js
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react'
 
 import RotateHandle from './RotateHandle'
 
-describe('Component > Rotatehandle', function () {
+describe.only('Component > Rotatehandle', function () {
   beforeEach(function () {
     render(
       <svg xmlns='http://www.w3.org/2000/svg'>
         <RotateHandle
           fill='red'
-          scale={1}
+          scale={2}
           x={100}
           y={200}
         />
@@ -19,5 +19,15 @@ describe('Component > Rotatehandle', function () {
 
   it('should render without crashing', function () {
     expect(screen.queryByTestId('rotate-handle')).to.exist()
+  })
+
+  it('should have the correct transform', function () {
+    const rotateHandle = screen.queryByTestId('rotate-handle')
+    expect(rotateHandle.getAttribute('transform')).to.equal('translate(100, 200) scale(0.5)')
+  })
+
+  it('should have the correct colour', function () {
+    const innerSVG = screen.queryByTestId('rotate-handle-inner-svg')
+    expect(innerSVG.getAttribute('fill')).to.equal('red')
   })
 })
