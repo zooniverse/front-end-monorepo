a17545093edb164bda57b0f7fbdc34ac61e6b3d7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-10-04 18:06:11 +0100

    Update tests for ZooniverseLogotpye - remove enzyme

--- packages/lib-react-components/src/ZooniverseLogotype/ZooniverseLogotype.spec.js
+++ packages/lib-react-components/src/ZooniverseLogotype/ZooniverseLogotype.spec.js
@@ -1,5 +1,5 @@
-import { mount } from 'enzyme'
 import React from 'react'
+import { render, screen } from '@testing-library/react'
 
 import ZooniverseLogotype from './ZooniverseLogotype'
 
@@ -8,41 +8,50 @@ describe('ZooniverseLogotype', function () {
   const ID = 'foobar'
   const WIDTH = 100
 
-  before(function () {
-    wrapper = mount(<ZooniverseLogotype id={ID} width={WIDTH} />)
+  describe('with standard props', function () {
+    beforeEach(function () {
+      wrapper = render(
+        <ZooniverseLogotype id={ID} width={WIDTH} />
+      )
+    })
+
+    it('should render without crashing', function () {
+      expect(screen).to.be.ok()
+    })
+
+    it('should use the `id` prop for `aria-labelledby` and `title`', function () {
+      const zooLogo = screen.getByLabelText('Zooniverse Logo')  // Gets the SVG
+      const titleElement = document.querySelector('title')
+
+      expect(zooLogo.getAttribute('aria-labelledby')).to.equal(ID)
+      expect(titleElement.getAttribute('id')).to.equal(ID)
+    })
+
+    it('should set the width from the `width` prop', function () {
+      const zooLogo = screen.getByLabelText('Zooniverse Logo')  // Gets the SVG
+      expect(parseInt(zooLogo.getAttribute('width'))).to.equal(WIDTH)
+    })
+
+    it('should calculate the height', function () {
+      const zooLogo = screen.getByLabelText('Zooniverse Logo')  // Gets the SVG
+      expect(zooLogo.getAttribute('height')).to.be.ok()
+    })
   })
 
-  it('should render without crashing', function () {
-    expect(wrapper).to.be.ok()
-  })
+  describe('with additional props', function () {
+    it('should pass through any other props to the SVG', function () {
+      const FOO = 'bar'
 
-  it('should use the `id` prop for `aria-labelledby` and `title`', function () {
-    const labelledby = wrapper.find('svg').prop('aria-labelledby')
-    const titleId = wrapper.find('title').prop('id')
-    expect(labelledby).to.equal(ID)
-    expect(titleId).to.equal(ID)
-  })
+      render(
+        <ZooniverseLogotype
+          id={ID}
+          width={WIDTH}
+          foo={FOO}
+        />
+      )
 
-  it('should set the width from the `width` prop', function () {
-    const svg = wrapper.find('svg')
-    expect(svg.prop('width')).to.equal(WIDTH)
-  })
-
-  it('should calculate the height', function () {
-    const svg = wrapper.find('svg')
-    expect(svg.prop('height')).to.be.ok()
-  })
-
-  it('should pass through any other props to the SVG', function () {
-    const FOO = 'bar'
-    const wrapperWithProps = mount(
-      <ZooniverseLogotype
-        id={ID}
-        width={WIDTH}
-        foo={FOO}
-      />
-    )
-
-    expect(wrapperWithProps.find('svg').prop('foo')).to.equal(FOO)
+      const zooLogo = screen.getByLabelText('Zooniverse Logo')  // Gets the SVG
+      expect(zooLogo.getAttribute('foo')).to.equal(FOO)
+    })
   })
 })
