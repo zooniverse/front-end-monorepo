283a2a7986500685d968e4e713bdea881f9aba6e
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-10-10 17:19:39 +0100

    Add tab switching tests for Tabs

--- packages/lib-react-components/src/Tabs/Tabs.spec.js
+++ packages/lib-react-components/src/Tabs/Tabs.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Grommet } from 'grommet'
 
@@ -32,5 +33,20 @@ describe('Component > Tabs', function () {
   it('should have exactly one active panel', function () {
     const tabPanel = screen.getAllByRole('tabpanel')
     expect(tabPanel).to.have.length(1)
+  })
+
+  it('should change the active tab panel when a tab button is clicked', async function () {
+    const user = userEvent.setup({ delay: null })
+    const tabPanel = screen.getByRole('tabpanel')  // You only need to query the panel once, even if the content changes.
+    const tabButton1 = screen.getByRole('tab', { name: 'bananas' })
+    const tabButton2 = screen.getByRole('tab', { name: 'cherries' })
+
+    expect(within(tabPanel).getByText('An apple is a red fruit.')).to.exist()
+
+    await user.click(tabButton1)
+    expect(within(tabPanel).getByText('A banana is a long fruit.')).to.exist()
+
+    await user.click(tabButton2)
+    expect(within(tabPanel).getByText('A cherry is a stone fruit.')).to.exist()
   })
 })
