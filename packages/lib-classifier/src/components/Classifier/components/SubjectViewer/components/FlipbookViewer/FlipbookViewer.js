763b7aebe75f5a56b7ce72fe7a6846e0b0337ce3
Delilah Clement <delilahclement@gmail.com>
2022-10-14 13:42:33 -0500

    Build the SingleImageViewer into FlipbookViewer

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/FlipbookViewer/FlipbookViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/FlipbookViewer/FlipbookViewer.js
@@ -1,20 +1,181 @@
-import React from 'react'
+import asyncStates from '@zooniverse/async-states'
 import { Box } from 'grommet'
 import PropTypes from 'prop-types'
-import locationValidator from '../../helpers/locationValidator'
+import React, { useEffect, useRef, useState } from 'react'
 
-function FlipbookViewer({ subject }) {
-  return (
-    <Box>
-      This is the flipbook viewer. Subject locations: {subject?.locations?.length}
-    </Box>
+import withKeyZoom from '@components/Classifier/components/withKeyZoom'
+import { withStores } from '@helpers'
+
+import locationValidator from '../../helpers/locationValidator'
+import useSubjectImage, { placeholder } from '../SingleImageViewer/hooks/useSubjectImage'
+import SingleImageViewer from '../SingleImageViewer/SingleImageViewer'
+import SVGImage from '../SVGComponents/SVGImage'
+import SVGPanZoom from '../SVGComponents/SVGPanZoom'
+
+function storeMapper(store) {
+  const {
+    enableRotation,
+    frame,
+    invert,
+    move,
+    rotation,
+    setFrame,
+    setOnPan,
+    setOnZoom
+  } = store.subjectViewer
+
+  const { activeStepTasks } = store.workflowSteps
+
+  const [activeInteractionTask] = activeStepTasks.filter(
+    (task) => task.type === 'drawing' || task.type === 'transcription'
   )
+  const {
+    activeTool
+  } = activeInteractionTask || {}
+
+  return {
+    activeTool,
+    enableRotation,
+    frame,
+    invert,
+    move,
+    rotation,
+    setFrame,
+    setOnPan,
+    setOnZoom
+  }
+}
+
+const defaultTool = {
+  validate: () => {}
+}
+
+function FlipbookViewer({
+  activeTool = defaultTool,
+  enableInteractionLayer = true,
+  enableRotation = () => null,
+  frame = 0,
+  ImageObject = window.Image,
+  invert = false,
+  loadingState = asyncStates.initialized,
+  move,
+  onError = () => true,
+  onKeyDown = () => true,
+  onReady = () => true,
+  rotation,
+  // setFrame = () => true,
+  setOnPan = () => true,
+  setOnZoom = () => true,
+  subject
+}) {
+  const subjectImage = useRef()
+  const [dragMove, setDragMove] = useState()
+  const imageUrl = subject ? Object.values(subject.locations[frame])[0] : null
+  const { img, error } = useSubjectImage(ImageObject, imageUrl)
+  const { naturalHeight, naturalWidth, src } = img
+
+  if (error) {
+    console.error(error)
+    onError(error)
+  }
+
+  useEffect(function onMount() {
+    enableRotation()
+  }, [])
+
+  useEffect(function onImageLoad() {
+    if (src !== placeholder.src) {
+      const svgImage = subjectImage.current
+      const { width: clientWidth, height: clientHeight } = svgImage
+        ? svgImage.getBoundingClientRect()
+        : {}
+      const target = { clientHeight, clientWidth, naturalHeight, naturalWidth }
+      onReady({ target })
+    }
+  }, [src])
+
+  useEffect(function onFrameChange() {
+    activeTool?.validate()
+  }, [frame])
+
+  function setOnDrag(callback) {
+    setDragMove(() => callback)
+  }
+
+  function onDrag(event, difference) {
+    dragMove?.(event, difference)
+  }
+
+  if (loadingState === asyncStates.error) {
+    return (
+      <div>Something went wrong.</div>
+    )
+  }
+
+  const enableDrawing = loadingState === asyncStates.success && enableInteractionLayer
+
+  if (loadingState !== asyncStates.initialized) {
+    const subjectID = subject?.id || 'unknown'
+    return (
+      <Box
+        direction='row'
+        fill='horizontal'
+      >
+        <SVGPanZoom
+          img={subjectImage.current}
+          maxZoom={5}
+          minZoom={0.1}
+          naturalHeight={naturalHeight}
+          naturalWidth={naturalWidth}
+          setOnDrag={setOnDrag}
+          setOnPan={setOnPan}
+          setOnZoom={setOnZoom}
+          src={src}
+        >
+          <SingleImageViewer
+            enableInteractionLayer={enableDrawing}
+            height={naturalHeight}
+            onKeyDown={onKeyDown}
+            rotate={rotation}
+            width={naturalWidth}
+          >
+            <g ref={subjectImage}>
+              <SVGImage
+                invert={invert}
+                move={move}
+                naturalHeight={naturalHeight}
+                naturalWidth={naturalWidth}
+                onDrag={onDrag}
+                src={src}
+                subjectID={subjectID}
+              />
+            </g>
+          </SingleImageViewer>
+        </SVGPanZoom>
+      </Box>
+    )
+  }
+  return null
 }
 
 FlipbookViewer.propTypes = {
+  activeTool: PropTypes.shape({
+    validate: PropTypes.func
+  }),
+  enableInteractionLayer: PropTypes.bool,
+  enableRotation: PropTypes.func,
+  frame: PropTypes.number,
+  invert: PropTypes.bool,
+  loadingState: PropTypes.string,
+  onError: PropTypes.func,
+  onReady: PropTypes.func,
+  setFrame: PropTypes.func,
+  setOnPan: PropTypes.func,
+  setOnZoom: PropTypes.func,
   subject: PropTypes.shape({
     locations: PropTypes.arrayOf(locationValidator)
   }).isRequired
 }
 
-export default FlipbookViewer
+export default withStores(withKeyZoom(FlipbookViewer), storeMapper)
+export { FlipbookViewer }
--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/FlipbookViewer/FlipbookViewer.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/FlipbookViewer/FlipbookViewer.stories.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import zooTheme from '@zooniverse/grommet-theme'
-import { Grommet } from 'grommet'
-import { Factory } from 'rosie'
+import { Box, Grommet } from 'grommet'
 import { Provider } from 'mobx-react'
 import mockStore from '@test/mockStore'
+import { SubjectFactory } from '@test/factories'
+import asyncStates from '@zooniverse/async-states'
 
 import FlipbookViewer from './FlipbookViewer'
 
@@ -12,26 +13,35 @@ const background = {
   light: 'light-1'
 }
 
-const subject = Factory.build('subject', {
+const subject = SubjectFactory.build({
   locations: [
     {
-      'image/jpeg': 'https://panoptes-uploads.zooniverse.org/subject_location/1e54b552-4608-4701-9db9-b8342b81278a.jpeg'
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/subject_location/1e54b552-4608-4701-9db9-b8342b81278a.jpeg'
     },
     {
-      'image/jpeg': 'https://panoptes-uploads.zooniverse.org/subject_location/098f3fb6-5021-410a-82a2-477a28b2bcd6.jpeg'
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/subject_location/098f3fb6-5021-410a-82a2-477a28b2bcd6.jpeg'
     },
     {
-      'image/jpeg': 'https://panoptes-uploads.zooniverse.org/subject_location/8fcb18b0-de80-42cd-ba2a-4871da30c74f.jpeg'
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/subject_location/8fcb18b0-de80-42cd-ba2a-4871da30c74f.jpeg'
     },
     {
-      'image/jpeg': 'https://panoptes-uploads.zooniverse.org/subject_location/85d8d82a-c88d-493c-b3db-7cd9f2ca5ad8.jpeg'
+      'image/jpeg':
+        'https://panoptes-uploads.zooniverse.org/subject_location/85d8d82a-c88d-493c-b3db-7cd9f2ca5ad8.jpeg'
     }
-  ]
+  ],
+  metadata: {
+    default_frame: 0
+  }
 })
 
 const store = mockStore({
   subject: subject
 })
+
+console.log(store)
 
 export default {
   title: 'Subject Viewers / FlipbookViewer',
@@ -46,7 +56,12 @@ export const Default = ({ dark }) => {
   return (
     <Grommet background={background} theme={zooTheme} themeMode={themeMode}>
       <Provider classifierStore={store}>
-        <FlipbookViewer subject={subject} />
+        <Box height='500px' width='large'>
+          <FlipbookViewer
+            loadingState={asyncStates.success}
+            subject={store.subjects.active}
+          />
+        </Box>
       </Provider>
     </Grommet>
   )
