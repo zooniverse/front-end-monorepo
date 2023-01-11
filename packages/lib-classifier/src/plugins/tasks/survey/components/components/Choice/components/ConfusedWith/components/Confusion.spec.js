23f0a57bdfb4595a55bfdd6757605ee24bfb31cd
Mark Bouslog <mark@zooniverse.org>
2023-01-10 23:26:22 -0600

    Refactor Confusion tests

--- packages/lib-classifier/src/plugins/tasks/survey/components/components/Choice/components/ConfusedWith/components/Confusion.spec.js
+++ packages/lib-classifier/src/plugins/tasks/survey/components/components/Choice/components/ConfusedWith/components/Confusion.spec.js
@@ -6,87 +6,66 @@ import { task } from '@plugins/tasks/survey/mock-data'
 import Confusion from './Confusion'
 
 describe('Component > Confusion', function () {
+  let label, images, confusionText, cancel, itsThis
+  
   const mockTask = SurveyTask.TaskModel.create(task)
   const KUDU = mockTask.choices.KD
   const HUMAN = mockTask.choices.HMN
 
-  it('should show the confusion choice label', function () {
-    render(
-      <Confusion
-        confusion={KUDU}
-        confusionId='KD'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.KD.label')}
-      />
-    )
-    expect(screen.getByText(KUDU.label)).to.be.ok()
+  describe('with choice with images', function () {
+    before(function () {
+      render(
+        <Confusion
+          confusion={KUDU}
+          confusionId='KD'
+          confusionText='Test confusion text.'
+          images={mockTask.images}
+          label={mockTask.strings.get('choices.KD.label')}
+        />
+      )
+
+      label = screen.getByText(KUDU.label)
+      images = screen.getByTestId('confusion-images')
+      confusionText = screen.getByText('Test confusion text.')
+      cancel = screen.getByRole('button', { name: 'SurveyTask.ConfusedWith.cancel' })
+      itsThis = screen.getByRole('button', { name: 'SurveyTask.ConfusedWith.itsThis' })
+    })
+
+    it('should show the confusion choice label', function () {
+      expect(label).to.be.ok()
+    })
+
+    it('should show confusion images', function () {
+      expect(images).to.be.ok()
+    })
+
+    it('should show confusion text', function () {
+      expect(confusionText).to.be.ok()
+    })
+
+    it('should show a "Cancel" button', function () {
+      /** The translation function will simply return keys in a testing env */
+      expect(cancel).to.be.ok()
+    })
+  
+    it('should show a "I think it\'s this" button', function () {
+      /** The translation function will simply return keys in a testing env */
+      expect(itsThis).to.be.ok()
+    })
   })
 
-  it('should show confusion images with choice with images', function () {
-    render(
-      <Confusion
-        confusion={KUDU}
-        confusionId='KD'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.KD.label')}
-      />
-    )
-    expect(screen.getByTestId('confusion-images')).to.be.ok()
-  })
-
-  it('should not render confusion images with choice without images', function () {
-    render(
-      <Confusion
-        confusion={HUMAN}
-        confusionId='HMN'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.HMN.label')}
-      />
-    )
-    expect(screen.queryByTestId('confusion-images')).to.be.null()
-  })
-
-  it('should show confusion text', function () {
-    render(
-      <Confusion
-        confusion={KUDU}
-        confusionId='KD'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.KD.label')}
-      />
-    )
-    expect(screen.getByText('Test confusion text.')).to.be.ok()
-  })
-
-  it('should show a "Cancel" button', function () {
-    render(
-      <Confusion
-        confusion={KUDU}
-        confusionId='KD'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.KD.label')}
-      />
-    )
-    /** The translation function will simply return keys in a testing env */
-    expect(screen.getByRole('button', { name: 'SurveyTask.ConfusedWith.cancel' })).to.be.ok()
-  })
-
-  it('should show a "I think it\'s this" button', function () {
-    render(
-      <Confusion
-        confusion={KUDU}
-        confusionId='KD'
-        confusionText='Test confusion text.'
-        images={mockTask.images}
-        label={mockTask.strings.get('choices.KD.label')}
-      />
-    )
-    /** The translation function will simply return keys in a testing env */
-    expect(screen.getByRole('button', { name: 'SurveyTask.ConfusedWith.itsThis' })).to.be.ok()
+  describe('with choice without images', function () {
+    it('should not render confusion images', function () {
+      render(
+        <Confusion
+          confusion={HUMAN}
+          confusionId='HMN'
+          confusionText='Test confusion text.'
+          images={mockTask.images}
+          label={mockTask.strings.get('choices.HMN.label')}
+        />
+      )
+      expect(screen.queryByTestId('confusion-images')).to.be.null()
+    })
   })
 })
