1e43eedd2b69787c7d37580b852f7e1741a0df7f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-01-12 00:08:31 +0800

    Label test: de-enzyme

--- packages/lib-react-components/src/Tooltip/components/Label/Label.spec.js
+++ packages/lib-react-components/src/Tooltip/components/Label/Label.spec.js
@@ -1,47 +1,41 @@
-import { shallow } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import Triangle from '../Triangle'
 import { Label } from './Label'
 
 describe('Tooltip > Component > Label', function () {
-  let wrapper
-  beforeEach(function () {
-    wrapper = shallow(<Label label='helpful tip' />)
-  })
-
-  it('renders without crashing', function () {
-    expect(wrapper).to.be.ok()
-  })
-
-  it('should render the label', function () {
-    expect(wrapper.contains('helpful tip')).to.be.true()
-  })
-
-  it('should set the animation of the wrapper Box', function () {
-    expect(wrapper.props().animation).to.equal('fadeIn')
-    wrapper.setProps({ animation: 'zoomIn' })
-    expect(wrapper.props().animation).to.equal('zoomIn')
-  })
-
-  describe('arrow', function () {
+  describe('with simple props', function () {
     beforeEach(function () {
-      wrapper = shallow(<Label data-position='top' label='helpful tip' />)
+      render(<Label label='helpful tip' />)
     })
 
-    it('should render an arrow based on the prop', function () {
-      expect(wrapper.find(Triangle)).to.have.lengthOf(1)
-      wrapper.setProps({ arrow: false })
-      expect(wrapper.find(Triangle)).to.have.lengthOf(0)
+    it('renders without crashing', function () {
+      expect(screen).to.be.ok()
     })
 
-    it('should position the arrow based on the data-position prop', function () {
-      let arrow = wrapper.find('Box').first().childAt(1)
-      expect(arrow).to.have.lengthOf(1)
-      expect(arrow.props().pointDirection).to.equal('down')
-      wrapper.setProps({ 'data-placement': 'bottom' })
-      arrow = wrapper.find('Box').first().childAt(0)
-      expect(arrow).to.have.lengthOf(1)
-      expect(arrow.props().pointDirection).to.equal('up')
+    it('should render the label', function () {
+      expect(screen.queryByText('helpful tip')).to.exist()
+    })
+  })
 
+  describe('with specific props', function () {
+    it('should render a triangle/arrow, by default', function () {
+      render(<Label label='helpful tip' />)
+      const triangle = document.querySelector('svg polygon')
+      expect(triangle).to.exist()
+      expect(triangle.getAttribute('points')).to.equal('5,0 10,10 0,10')
+    })
+
+    it('should render a triangle/arrow if arrow=true', function () {
+      render(<Label label='helpful tip' arrow={true} />)
+      const triangle = document.querySelector('svg polygon')
+      expect(triangle).to.exist()
+      expect(triangle.getAttribute('points')).to.equal('5,0 10,10 0,10')
+    })
+
+    it('should not render a triangle/arrow if arrow=false', function () {
+      render(<Label label='helpful tip' arrow={false} />)
+      const triangle = document.querySelector('svg polygon')
+      expect(triangle).to.not.exist()
     })
   })
 })
