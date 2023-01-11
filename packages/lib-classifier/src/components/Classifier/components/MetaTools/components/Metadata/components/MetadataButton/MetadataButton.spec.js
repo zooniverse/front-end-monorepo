38ed4747eba6b51ce12a3f5888ffa0355c22b55c
Delilah Clement <delilahclement@gmail.com>
2023-01-11 16:47:17 -0600

    update MetadataButton spec

--- packages/lib-classifier/src/components/Classifier/components/MetaTools/components/Metadata/components/MetadataButton/MetadataButton.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/MetaTools/components/Metadata/components/MetadataButton/MetadataButton.spec.js
@@ -1,7 +1,7 @@
 import { Grommet } from 'grommet'
 import sinon from 'sinon'
 import zooTheme from '@zooniverse/grommet-theme'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import MetadataButton from './MetadataButton'
@@ -11,12 +11,12 @@ describe('MetadataButton', function () {
     const onClickSpy = sinon.spy()
     const user = userEvent.setup({ delay: null })
 
-    const { getByText } = render(
+    render(
       <Grommet theme={zooTheme}>
         <MetadataButton disabled onClick={onClickSpy} />
       </Grommet>
     )
-    const button = getByText('MetaTools.MetadataButton.label')
+    const button = screen.getByText('MetaTools.MetadataButton.label')
 
     await user.pointer({
       keys: '[MouseLeft]',
