76fa4356b9e5107efee5d420806cf5844e4c2188
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2023-01-12 18:21:50 +0000

    Maintenance Week: add tests & story for Zooniverse Logo & Logotype (#4123)
    * ZooniverseLogotype test: added
    
    * ZooniverseLogo story: added
    
    * ZooniverseLogotype story: added
    
    * ZooniverseLogo stories: change incorrect id value
    
    * ZooniverseLogotype story: fix id value

---
+++ packages/lib-react-components/src/ZooniverseLogo/ZooniverseLogo.stories.js
@@ -0,0 +1,29 @@
+import zooTheme from '@zooniverse/grommet-theme'
+import { Box, Button, Grommet } from 'grommet'
+
+import readme from './README.md'
+import ZooniverseLogo from './ZooniverseLogo'
+
+export default {
+  title: 'Components/ZooniverseLogo',
+  component: ZooniverseLogo,
+  args: {
+    id: 'the-zooniverse',
+    size: '200px',
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: readme
+      }
+    }
+  }
+}
+
+export function ZooniverseLogo_ ({ id, size }) {
+  return (
+    <Box align='center' justify='center' pad='medium'>
+      <ZooniverseLogo id={id} size={size} />
+    </Box>
+  )
+}
---
+++ packages/lib-react-components/src/ZooniverseLogotype/ZooniverseLogo.stories.js
@@ -0,0 +1,29 @@
+import zooTheme from '@zooniverse/grommet-theme'
+import { Box, Button, Grommet } from 'grommet'
+
+import readme from './README.md'
+import ZooniverseLogotype from './ZooniverseLogotype'
+
+export default {
+  title: 'Components/ZooniverseLogotype',
+  component: ZooniverseLogotype,
+  args: {
+    id: 'the-zooniverse',
+    width: 200,
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: readme
+      }
+    }
+  }
+}
+
+export function ZooniverseLogotype_ ({ id, width }) {
+  return (
+    <Box align='center' justify='center' pad='medium'>
+      <ZooniverseLogotype id={id} width={width} />
+    </Box>
+  )
+}
