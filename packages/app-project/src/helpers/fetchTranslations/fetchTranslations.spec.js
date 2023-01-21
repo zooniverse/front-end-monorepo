9534b07695322a04c50535042e04b32689720e0d
Delilah Clement <delilahclement@gmail.com>
2023-01-21 15:03:47 -0600

    refactor fetchTranslations.spec

--- packages/app-project/src/helpers/fetchTranslations/fetchTranslations.spec.js
+++ packages/app-project/src/helpers/fetchTranslations/fetchTranslations.spec.js
@@ -2,51 +2,74 @@ import nock from 'nock'
 
 import fetchTranslations from './'
 
-describe('helpers > fetchTranslations', function () {
+describe.only('helpers > fetchTranslations', function () {
   before(function () {
     nock('https://panoptes-staging.zooniverse.org/api')
-    .persist()
-    .get('/translations')
-    .query(query => query.translated_type === 'project')
-    .reply(200, {
-      translations: [
-        {
-          language: 'zh-cn',
-          strings: {
-            display_name: 'Chinese translation'
+      .get('/translations')
+      .query(query => query.translated_type === 'project')
+      .reply(200, {
+        translations: [
+          {
+            language: 'fr',
+            strings: {
+              display_name: 'Le project'
+            }
+          },
+          {
+            language: 'en',
+            strings: {
+              display_name: 'test project'
+            }
           }
-        },
-        {
-          language: 'fr',
-          strings: {
-            display_name: 'Le project'
+        ]
+      })
+      .get('/translations')
+      .query(query => query.translated_type === 'project' && query.language === 'fr,en')
+      .reply(200, {
+        translations: [
+          {
+            language: 'en',
+            strings: {
+              display_name: 'test project'
+            }
           }
-        },
-        {
-          language: 'en',
-          strings: {
-            display_name: 'test project'
+        ]
+      })
+      .get('/translations')
+      .query(query => query.translated_type === 'project' && query.language === 'zh-cn,en')
+      .reply(200, {
+        translations: [
+          {
+            language: 'en',
+            strings: {
+              display_name: 'test project'
+            }
+          },
+          {
+            language: 'zh-cn',
+            strings: {
+              display_name: 'Chinese translation'
+            }
           }
-        }
-      ]
-    })
-    .get('/translations')
-    .query(query => query.translated_type === 'project_page')
-    .reply(200, {
-      translations: [
-        {
-          language: 'en',
-          strings: {
-            content: 'test project page'
+        ]
+      })
+      .get('/translations')
+      .query(query => query.translated_type === 'project_page')
+      .reply(200, {
+        translations: [
+          {
+            language: 'en',
+            strings: {
+              content: 'test project page'
+            }
           }
-        }
-      ]
-    })
-    .get('/translations')
-    .query(query => query.translated_type === 'made_up')
-    .reply(200, {
-      translations: []
-    })
+        ]
+      })
+      .get('/translations')
+      .query(query => query.translated_type === 'made_up')
+      .reply(200, {
+        translations: []
+      })
   })
 
   it('should fetch project translations', async function () {
@@ -70,22 +93,11 @@ describe('helpers > fetchTranslations', function () {
     expect(translation?.strings?.content).to.equal('test project page')
   })
 
-  it('should handle language codes that have partial capitalization', async function () {
+  it("should use the fallback language when translations don't exist", async function () {
     const translation = await fetchTranslations({
       translated_id: 1234,
       translated_type: 'project',
-      language: 'zh-CN',
-      fallback: 'en',
-      env: 'staging'
-    })
-    expect(translation?.strings?.display_name).to.equal('Chinese translation')
-  })
-
-  it('should use the fallback language when translations don\'t exist', async function () {
-    const translation = await fetchTranslations({
-      translated_id: 1234,
-      translated_type: 'project',
-      language: 'es',
+      language: 'fr',
       fallback: 'en',
       env: 'staging'
     })
@@ -100,5 +112,16 @@ describe('helpers > fetchTranslations', function () {
       env: 'staging'
     })
     expect(translation).to.be.undefined()
+  })
+
+  it('should handle language codes that have partial capitalization', async function () {
+    const translation = await fetchTranslations({
+      translated_id: 1234,
+      translated_type: 'project',
+      language: 'zh-CN',
+      fallback: 'en',
+      env: 'staging'
+    })
+    expect(translation?.strings?.display_name).to.equal('Chinese translation')
   })
 })
