ab26be4b01835e8b968a576a0224fcfec65b067f
Mark Bouslog <mark@zooniverse.org>
2023-01-27 14:26:09 -0600

    Add test with undefined filters

--- packages/lib-classifier/src/components/Classifier/components/MetaTools/components/Metadata/components/MetadataModal/MetadataModal.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/MetaTools/components/Metadata/components/MetadataModal/MetadataModal.spec.js
@@ -13,4 +13,19 @@ describe('MetadataModal', function () {
     const rows = within(tableBody).getAllByRole('row')
     expect(rows).to.have.lengthOf(3)
   })
+
+  describe('with undefined filters', function () {
+    it('should render the correct number of table rows', function () {
+      render(
+        <DefaultStory
+          filters={undefined}
+        />
+      )
+      const tableBody = document.querySelector('tbody')
+      const rows = within(tableBody).getAllByRole('row')
+
+      // the DefaultStory mock subject metadata includes 5 items, 2 of which should be filtered/hidden per default filters, leaving 3 rows rendered
+      expect(rows).to.have.lengthOf(3)
+    })
+  })
 })
