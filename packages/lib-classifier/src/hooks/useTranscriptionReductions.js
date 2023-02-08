9594c7dd0e497117fea2a878de7a084408961b17
Jim O'Donnell <jim@zooniverse.org>
2023-02-08 09:12:29 +0000

    Add a useTranscriptionReductions hook Add a `useTranscriptionReductions` hook, which replaces the old `TranscribedLinesConnector` component and handles fetching a subject's reductions from Caesar.

---
+++ packages/lib-classifier/src/hooks/useTranscriptionReductions.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import { applySnapshot } from 'mobx-state-tree'
+
+import SHOWN_MARKS from '@helpers/shownMarks'
+import { useStores } from '@hooks'
+
+const REDUCER_KEY = 'alice'
+
+async function fetchReductions(caesarClient, subjectID, workflowID) {
+  const query = `{
+    workflow(id: ${workflowID}) {
+      subject_reductions(subjectId: ${subjectID}, reducerKey:"${REDUCER_KEY}")
+      {
+        data
+      }
+    }
+  }`
+  const response = await caesarClient.request(query.replace(/\s+/g, ' '))
+  return response?.workflow?.subject_reductions
+}
+
+export default function useTranscriptionReductions() {
+  const [loaded, setLoaded] = useState(false)
+
+  const {
+    client: {
+      caesar
+    },
+    subjects: {
+      active: subject
+    },
+    subjectViewer: {
+      frame
+    },
+    workflows: {
+      active: workflow
+    },
+    workflowSteps: {
+      active: step,
+      findTasksByType
+    }
+  } = useStores()
+
+  useEffect(function onSubjectChange() {
+    async function updateReductions(caesarClient, subject, workflow) {
+      const reductions = await fetchReductions(caesarClient, subject.id, workflow.id)
+      applySnapshot(subject.transcriptionReductions, {
+        subjectId: subject.id,
+        workflowId: workflow.id,
+        reductions
+      })
+      setLoaded(true)
+    }
+
+    if (subject?.id && workflow?.id) {
+      setLoaded(false)
+      updateReductions(caesar, subject, workflow)
+    }
+  }, [caesar, subject, workflow])
+
+  const lines = subject?.transcriptionReductions?.consensusLines(frame)
+  const activeStepAnnotations = subject?.stepHistory.latest.annotations
+
+  // We expect there to only be one
+  const [task] = findTasksByType('transcription')
+  // We want to observe the marks array for changes, so pass that as a separate prop.
+  const marks = task?.marks
+  // find the corresponding annotation
+  const annotation = activeStepAnnotations.find(
+    annotation => annotation.task === task?.taskKey
+  )
+
+  const invalidMark = !step?.isValid
+  const visible = workflow?.usesTranscriptionTask &&
+    loaded &&
+    task?.shownMarks === SHOWN_MARKS.ALL &&
+    lines.length > 0
+
+  return {
+    annotation,
+    frame,
+    invalidMark,
+    lines,
+    marks,
+    task,
+    visible,
+    workflow
+  }
+}
