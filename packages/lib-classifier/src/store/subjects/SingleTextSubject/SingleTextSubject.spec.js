7ae3d1b1319b4d72d8dfe1d5d122b71ad24388bb
Mark Bouslog <mark@zooniverse.org>
2023-02-16 23:48:18 -0600

    Refactor data fetching from text related subjects

--- packages/lib-classifier/src/store/subjects/SingleTextSubject/SingleTextSubject.spec.js
+++ packages/lib-classifier/src/store/subjects/SingleTextSubject/SingleTextSubject.spec.js
@@ -1,41 +1,30 @@
 import { expect } from 'chai'
-import { when } from 'mobx'
-import nock from 'nock'
-import asyncStates from '@zooniverse/async-states'
+import sinon from 'sinon'
 
-import { SubjectFactory, WorkflowFactory } from '@test/factories'
-import mockStore from '@test/mockStore'
 import subjectViewers from '@helpers/subjectViewers'
+import RootStore from '@store/'
+import { SubjectFactory, WorkflowFactory } from '@test/factories'
+import stubPanoptesJs from '@test/stubPanoptesJs'
 
 import SingleTextSubject from './SingleTextSubject'
 
 describe('Model > SingleTextSubject', function () {
-  const subjectSnapshot = SubjectFactory.build({
-    content: 'This is test subject content',
-    contentLoadingState: asyncStates.success,
+  const singleTextSubjectSnapshot = SubjectFactory.build({
     locations: [
-      { 'text/plain': 'http://localhost:8080/subjectContent.txt' }
+      { 'text/plain': 'https://foo.bar/subjectText.txt' }
     ]
   })
 
-  const successSubjectSnapshot = SubjectFactory.build({
+  const invalidLocationsSubjectSnapshot = SubjectFactory.build({
     locations: [
-      { 'text/plain': 'http://localhost:8080/success.txt' }
+      { 'audio/mpeg': 'https://foo.bar/example.mp3' }
     ]
   })
 
   const multipleLocationsSubjectSnapshot = SubjectFactory.build({
     locations: [
-      { 'text/plain': 'http://localhost:8080/success.txt' },
-      { 'audio/mpeg': 'http://localhost:8080/example.mp3' }
-    ]
-  })
-
-  const imageSubjectSnapshot = SubjectFactory.build()
-
-  const failureSubjectSnapshot = SubjectFactory.build({
-    locations: [
-      { 'text/plain': 'http://localhost:8080/failure.txt' }
+      { 'text/plain': 'https://foo.bar/subjectText1.txt' },
+      { 'text/plain': 'https://foo.bar/subjectText2.txt' }
     ]
   })
 
@@ -43,7 +32,7 @@ describe('Model > SingleTextSubject', function () {
   let singleTextSubject
 
   before(function () {
-    singleTextSubject = SingleTextSubject.create(subjectSnapshot)
+    singleTextSubject = SingleTextSubject.create(singleTextSubjectSnapshot)
   })
 
   it('should exist', function () {
@@ -52,101 +41,47 @@ describe('Model > SingleTextSubject', function () {
   })
 
   it('should have a `locations` property', function () {
-    expect(singleTextSubject.locations).to.deep.equal(subjectSnapshot.locations)
+    expect(singleTextSubject.locations).to.deep.equal(singleTextSubjectSnapshot.locations)
   })
 
   it('should have one location', function () {
     expect(singleTextSubject.locations).to.have.lengthOf(1)
   })
 
-  it('should have content as expected', function () {
-    expect(singleTextSubject.content).to.equal(subjectSnapshot.content)
-  })
+  describe('with an invalid subject location', function () {
+    it('of invalid location type, should throw an error', function () {
+      expect(() => SingleTextSubject.create(invalidLocationsSubjectSnapshot)).to.throw()
+    })
 
-  it('should have contentLoadingState as expected', function () {
-    expect(singleTextSubject.contentLoadingState).to.equal(subjectSnapshot.contentLoadingState)
-  })
-
-  describe('with multiple subject locations', function () {
-    it('should throw an error', function () {
+    it('of multiple locations, should throw an error', function () {
       expect(() => SingleTextSubject.create(multipleLocationsSubjectSnapshot)).to.throw()
     })
   })
 
-  describe('with an invalid (not .txt) subject location', function () {
-    it('should throw an error', function () {
-      expect(() => SingleTextSubject.create(imageSubjectSnapshot)).to.throw()
-    })
-  })
-
-  describe('with location request response that fails', function () {
-    let subjectWithRequestFailure
-
-    before(async function () {
-      nock('http://localhost:8080')
-        .get('/failure.txt')
-        .reply(404)
-
-      const store = mockStore({
-        subject: failureSubjectSnapshot,
-        workflow: workflowSnapshot
-      })
-
-      subjectWithRequestFailure = store.subjects.active
-
-      await when(() => subjectWithRequestFailure.contentLoadingState === asyncStates.error)
-    })
-
-    it('should have contentLoadingState as expected', function () {
-      expect(subjectWithRequestFailure.contentLoadingState).to.equal(asyncStates.error)
-    })
-
-    it('should have error as expected', function () {
-      expect(subjectWithRequestFailure.error.message).to.equal('Not Found')
-    })
-  })
-
-  describe('with location request response that succeeds', function () {
-    let subjectWithRequestSuccess
-
-    before(async function () {
-      nock('http://localhost:8080')
-        .get('/success.txt')
-        .reply(200, 'This is test subject content')
-
-      const store = mockStore({
-        subject: successSubjectSnapshot,
-        workflow: workflowSnapshot
-      })
-
-      subjectWithRequestSuccess = store.subjects.active
-
-      await when(() => subjectWithRequestSuccess.contentLoadingState === asyncStates.success)
-    })
-
-    it('should have contentLoadingState as expected', function () {
-      expect(subjectWithRequestSuccess.contentLoadingState).to.equal(asyncStates.success)
-    })
-
-    it('should have content as expected', function () {
-      expect(subjectWithRequestSuccess.content).to.equal('This is test subject content')
-    })
-  })
-
   describe('Views > viewer', function () {
-    let subjectWithSingleTextViewer
-
     before(function () {
-      const store = mockStore({
-        subject: subjectSnapshot,
-        workflow: workflowSnapshot
+      const { panoptes } = stubPanoptesJs({
+        subjects: [ singleTextSubject ],
+        workflows: [ workflowSnapshot ]
       })
-
-      subjectWithSingleTextViewer = store.subjects.active
+      const client = {
+        caesar: {
+          request: sinon.stub().callsFake(() => Promise.resolve({}))
+        },
+        panoptes,
+        tutorials: {
+          get: sinon.stub().callsFake(() => Promise.resolve({ body: { tutorials: [] } }))
+        }
+      }
+      const rootStore = RootStore.create({}, { client })
+      rootStore.workflows.setResources([workflowSnapshot])
+      rootStore.workflows.setActive(workflowSnapshot.id)
+      rootStore.subjects.setResources([singleTextSubject])
+      rootStore.subjects.setActive(singleTextSubject.id)
     })
 
-    it('should return the single text viewer', function () {
-      expect(subjectWithSingleTextViewer.viewer).to.equal(subjectViewers.singleText)
+    it('should use the single text viewer', function () {
+      expect(singleTextSubject.viewer).to.equal(subjectViewers.singleText)
     })
   })
 })
