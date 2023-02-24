e45a06af33260282de2789db7b587934be5187a3
Jim O'Donnell <jim@zooniverse.org>
2023-02-24 22:18:00 +0000

    Fix active subjects in the mock store Add the active subject to the mock store after mocking the rest of the queue, not before, to avoid ID collisions.

--- packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.spec.js
@@ -4,9 +4,9 @@ import { composeStory } from '@storybook/testing-react'
 import Meta, { Default, mockTask } from './MaxWidth.stories.js'
 
 describe('Component > Layouts > MaxWidth', function () {
-  const DefaultStory = composeStory(Default, Meta)
 
   it('should render a subject and a task', function () {
+    const DefaultStory = composeStory(Default, Meta)
     render(<DefaultStory />)
     expect(screen.getByLabelText('Subject 1')).exists() // img aria-label from SVGImage
     expect(screen.getByText(mockTask.init.strings.question)).exists() // task question paragraph
