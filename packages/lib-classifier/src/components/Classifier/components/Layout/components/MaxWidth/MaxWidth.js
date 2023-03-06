a78989e7318a003ac0d99b5c31032d74e88d514e
Delilah Clement <delilahclement@gmail.com>
2023-03-06 16:01:12 -0600

    fix layout style typo

--- packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.js
+++ packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.js
@@ -86,7 +86,7 @@ export default function MaxWidth({ className = '', displayNaturalDimensions = fa
       {...containerGridProps}
     >
       <ViewerGrid>
-        <Box gridArea='subject' align={displayNaturalDimensions ? 'end' : 'start'}>
+        <Box gridArea='subject' align={displayNaturalDimensions ? 'end' : 'inherit'}>
           <Banners />
           <SubjectViewer />
           <MetaTools />
