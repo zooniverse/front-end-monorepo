e9bd78d28fdbbb1cadcae90411834373f7a92dac
Delilah Clement <delilahclement@gmail.com>
2023-03-29 15:41:36 -0500

    refactor MetaToolsButton stories

--- packages/lib-react-components/src/MetaToolsButton/MetaToolsButton.stories.js
+++ packages/lib-react-components/src/MetaToolsButton/MetaToolsButton.stories.js
@@ -1,47 +1,44 @@
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
-import zooTheme from '@zooniverse/grommet-theme';
-import { Grommet } from 'grommet';
-import { Add } from 'grommet-icons';
-import readme from './README.md';
+import zooTheme from '@zooniverse/grommet-theme'
+import { Grommet } from 'grommet'
+import { Add } from 'grommet-icons'
 
-import MetaToolsButton from './';
-
-const config = {
-  docs: {
-    description: {
-      component: readme,
-    },
-  },
-};
+import MetaToolsButton from './'
 
 export default {
   title: 'Components/MetaToolsButton',
-  decorators: [withKnobs],
-};
+  component: MetaToolsButton,
+  args: {
+    dark: false
+  }
+}
 
-export const Plain = () => (
-  <Grommet theme={{ ...zooTheme, dark: boolean('Dark theme', false) }}>
-    <MetaToolsButton disabled={boolean('disabled', false)} icon={<Add size="small" />} text="Add" />
+export const Default = ({ dark }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
+    <MetaToolsButton disabled={false} icon={<Add size='small' />} text='Add' />
   </Grommet>
-);
+)
 
-Plain.story = {
-  name: 'plain',
-  parameters: config,
-};
-
-export const AsLink = () => (
-  <Grommet theme={{ ...zooTheme, dark: boolean('Dark theme', false) }}>
+export const AsLink = ({ dark }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
     <MetaToolsButton
-      disabled={boolean('disabled', false)}
-      href={text('href', '/mypage')}
-      icon={<Add size="small" />}
-      text="Add"
+      disabled={false}
+      href='/mypage'
+      icon={<Add size='small' />}
+      text='Add'
     />
   </Grommet>
-);
-
-AsLink.story = {
-  name: 'as link',
-  parameters: config,
-};
+)
