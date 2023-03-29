b83140075122aa19c4bf63d232d481ef218caa9b
Delilah Clement <delilahclement@gmail.com>
2023-03-29 15:34:00 -0500

    refactor CloseButton stories

--- packages/lib-react-components/src/CloseButton/CloseButton.stories.js
+++ packages/lib-react-components/src/CloseButton/CloseButton.stories.js
@@ -1,92 +1,56 @@
-// import { withActions } from '@storybook/addon-actions';
-// import zooTheme from '@zooniverse/grommet-theme';
-// import { Box, Grommet } from 'grommet';
+import zooTheme from '@zooniverse/grommet-theme'
+import { Box, Grommet } from 'grommet'
 
-// import CloseButton from './CloseButton';
-// import readme from './README.md';
+import CloseButton from './CloseButton'
 
-// const config = {
-//   docs: {
-//     description: {
-//       component: readme,
-//     },
-//   },
-// };
+export default {
+  title: 'Components/CloseButton',
+  component: CloseButton,
+  args: {
+    dark: false
+  }
+}
 
-// const darkZooTheme = { ...zooTheme, dark: true };
+export const Default = ({ dark }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
+    <Box align='center' height='small' justify='center' width='small'>
+      <CloseButton closeFn={() => {}} />
+    </Box>
+  </Grommet>
+)
 
-// export default {
-//   title: 'Components/CloseButton',
-//   decorators: [withActions('click button')],
-// };
+export const WithTealBackground = ({ dark }) => (
+  <Grommet theme={zooTheme} themeMode={dark ? 'dark' : 'light'}>
+    <Box
+      align='center'
+      background='brand'
+      height='small'
+      justify='center'
+      width='small'
+    >
+      <CloseButton color='neutral-6' closeFn={() => {}} />
+    </Box>
+  </Grommet>
+)
 
-// export const LightThemeDefault = () => (
-//   <Grommet
-//     background={{
-//       dark: 'dark-1',
-//       light: 'light-1',
-//     }}
-//     theme={zooTheme}
-//     themeMode="light"
-//   >
-//     <Box align="center" height="small" justify="center" width="small">
-//       <CloseButton closeFn={() => {}} />
-//     </Box>
-//   </Grommet>
-// );
-
-// LightThemeDefault.story = {
-//   name: 'Light theme (default)',
-//   parameters: config,
-// };
-
-// export const DarkTheme = () => (
-//   <Grommet
-//     background={{
-//       dark: 'dark-1',
-//       light: 'light-1',
-//     }}
-//     theme={darkZooTheme}
-//     themeMode="dark"
-//   >
-//     <Box align="center" height="small" justify="center" width="small">
-//       <CloseButton closeFn={() => {}} />
-//     </Box>
-//   </Grommet>
-// );
-
-// DarkTheme.story = {
-//   name: 'Dark theme',
-//   parameters: config,
-// };
-
-// export const WithTealBackground = () => (
-//   <Grommet theme={zooTheme}>
-//     <Box align="center" background="brand" height="small" justify="center" width="small">
-//       <CloseButton color="neutral-6" closeFn={() => {}} />
-//     </Box>
-//   </Grommet>
-// );
-
-// WithTealBackground.story = {
-//   name: 'with teal background',
-//   parameters: config,
-// };
-
-// export const Disabled = () => (
-//   <Grommet
-//     background={{
-//       dark: 'dark-1',
-//       light: 'light-1',
-//     }}
-//     theme={zooTheme}
-//   >
-//     <Box align="center" height="small" justify="center" width="small">
-//       <CloseButton disabled closeFn={() => {}} />
-//     </Box>
-//   </Grommet>
-// );
-
-// Disabled.story = {
-//   parameters: config,
-// };
+export const Disabled = ({ dark }) => (
+  <Grommet
+    background={{
+      dark: 'dark-1',
+      light: 'light-1'
+    }}
+    theme={zooTheme}
+    themeMode={dark ? 'dark' : 'light'}
+  >
+    <Box align='center' height='small' justify='center' width='small'>
+      <CloseButton disabled closeFn={() => {}} />
+    </Box>
+  </Grommet>
+)
