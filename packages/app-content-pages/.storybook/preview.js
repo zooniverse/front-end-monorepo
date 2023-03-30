c17d076a707d6a1bb1bcbdd51be74bc9bb47a1b4
Delilah Clement <delilahclement@gmail.com>
2023-03-29 19:04:39 -0500

    create global decorator for Grommet context

---
+++ packages/app-content-pages/.storybook/preview.js
@@ -0,0 +1,39 @@
+import zooTheme from '@zooniverse/grommet-theme'
+import { Grommet } from 'grommet'
+
+export const parameters = {
+  layout: 'fullscreen'
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Grommet Theme',
+    description: 'Global Grommet theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'arrowdown',
+      items: ['light', 'dark'],
+      showName: true,
+    }
+  }
+}
+
+const background = {
+  dark: 'dark-1',
+  light: 'light-1'
+}
+
+export const decorators = [
+  (Story, context) => {
+    return (
+      <Grommet
+        background={background}
+        theme={zooTheme}
+        themeMode={context.globals.theme}
+        full
+      >
+        <Story />
+      </Grommet>
+    )
+  }
+]
--- packages/app-content-pages/src/screens/Publications/components/Publication/Publication.stories.js
+++ packages/app-content-pages/src/screens/Publications/components/Publication/Publication.stories.js
@@ -1,51 +1,41 @@
 import { Box } from 'grommet'
-import GrommetWrapper from '@helpers/stories/GrommetWrapper.js'
 import Publication from './Publication.js'
-
-const publicationArgs = {
-  authors: 'Mock Author',
-  avatarSrc: 'https://static.zooniverse.org/www.zooniverse.org/assets/simple-avatar.png',
-  title: 'Mock Publication',
-  url: 'https://www.zooniverse.org',
-  year: '2023'
-}
 
 export default {
   title: 'Publications / Publication',
   component: Publication,
   args: {
-    dark: false,
-    ...publicationArgs
+    authors: 'Mock Author',
+    avatarSrc:
+      'https://static.zooniverse.org/www.zooniverse.org/assets/simple-avatar.png',
+    title: 'Mock Publication',
+    url: 'https://www.zooniverse.org',
+    year: '2023'
   }
 }
 
 /** Publication Component with all props provided */
 
-export const Default = ({ authors, avatarSrc, dark, title, url, year }) => {
+export const Default = ({ authors, avatarSrc, title, url, year }) => {
   return (
-    <GrommetWrapper dark={dark}>
-      <Box width='100%'>
-        <Publication
-          authors={authors}
-          avatarSrc={avatarSrc}
-          title={title}
-          url={url}
-          year={year}
-        />
-      </Box>
-    </GrommetWrapper>
+    <Box width='100%'>
+      <Publication
+        authors={authors}
+        avatarSrc={avatarSrc}
+        title={title}
+        url={url}
+        year={year}
+      />
+    </Box>
   )
 }
 
-
 /** Publication Component with no props provided */
 
-export const Placeholders = ({ dark }) => {
+export const Placeholders = () => {
   return (
-    <GrommetWrapper dark={dark}>
-      <Box width='100%'>
-        <Publication />
-      </Box>
-    </GrommetWrapper>
+    <Box width='100%'>
+      <Publication />
+    </Box>
   )
 }
