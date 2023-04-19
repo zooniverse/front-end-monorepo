5bd13e2da7a873fac47ccabd26484034b90e0bba
Delilah Clement <delilahclement@gmail.com>
2023-04-19 14:05:41 -0500

    remove dark arg from SeparateFrame story

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SeparateFramesViewer/components/SeparateFrame/SeparateFrame.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SeparateFramesViewer/components/SeparateFrame/SeparateFrame.stories.js
@@ -1,5 +1,3 @@
-import zooTheme from '@zooniverse/grommet-theme'
-import { Grommet } from 'grommet'
 import { Provider } from 'mobx-react'
 import mockStore from '@test/mockStore'
 
@@ -9,7 +7,6 @@ export default {
   title: 'Subject Viewers / SeparateFrame',
   component: SeparateFrame,
   args: {
-    dark: false,
     frameUrl:
       'https://panoptes-uploads.zooniverse.org/subject_location/1e54b552-4608-4701-9db9-b8342b81278a.jpeg',
     limitSubjectHeight: false
@@ -19,21 +16,13 @@ export default {
 const store = mockStore()
 store.subjectViewer.setSeparateFramesView(true)
 
-const background = {
-  dark: 'dark-1',
-  light: 'light-1'
-}
-
-export const Default = ({ dark, frameUrl, limitSubjectHeight }) => {
-  const themeMode = dark ? 'dark' : 'light'
+export const Default = ({ frameUrl, limitSubjectHeight }) => {
   return (
-    <Grommet background={background} theme={zooTheme} themeMode={themeMode}>
-      <Provider classifierStore={store}>
-        <SeparateFrame
-          frameUrl={frameUrl}
-          limitSubjectHeight={limitSubjectHeight}
-        />
-      </Provider>
-    </Grommet>
+    <Provider classifierStore={store}>
+      <SeparateFrame
+        frameUrl={frameUrl}
+        limitSubjectHeight={limitSubjectHeight}
+      />
+    </Provider>
   )
 }
