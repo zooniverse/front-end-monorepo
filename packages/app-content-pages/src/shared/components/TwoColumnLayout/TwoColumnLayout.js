f6dff2786fd4bcac58d8ed31d72f71cdabfd9ef8
Delilah Clement <23665803+goplayoutside3@users.noreply.github.com>
2023-05-25 15:28:30 -0500

    remove unneeded layout tests

--- packages/app-content-pages/src/shared/components/TwoColumnLayout/TwoColumnLayout.js
+++ packages/app-content-pages/src/shared/components/TwoColumnLayout/TwoColumnLayout.js
@@ -3,44 +3,24 @@ import { node } from 'prop-types'
 
 import AboutHeader from '../AboutHeader'
 
-function TwoColumnLayout (props) {
-  const { heading, sidebar, main } = props
+function TwoColumnLayout({ heading, sidebar, main }) {
   return (
     <>
       <AboutHeader />
       <Box align='center' pad={{ horizontal: 'medium', vertical: 'large' }}>
-        <Box 
-          direction='row'
-          gap='medium'
-          width='xlarge'
-        >
-          <Box
-            width='medium'
-          />
+        <Box direction='row' gap='medium' width='xlarge'>
+          <Box width='medium' />
           <Box fill gridArea='heading'>
             {heading}
           </Box>
         </Box>
-
-        <Box
-          direction='row'
-          gap='medium'
-          width='xlarge'
-        >
-          
-
-          <Box
-            as='aside'
-            gridArea='sidebar'
-            width='medium'
-          >
+        <Box direction='row' gap='medium' width='xlarge'>
+          <Box as='aside' gridArea='sidebar' width='medium'>
             {sidebar}
           </Box>
-          
           <Box fill gridArea='main'>
             {main}
           </Box>
-          
         </Box>
       </Box>
     </>
