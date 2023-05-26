d5934b7d71800cbcaaa45fcb222826d36a92d293
Mark Bouslog <mark@zooniverse.org>
2023-05-26 14:06:45 -0500

    Refactor ResetButton tests with testing-library

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/ResetButton/ResetButton.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/ResetButton/ResetButton.spec.js
@@ -1,30 +1,57 @@
-import { shallow } from 'enzyme'
-import sinon from 'sinon'
-import ResetButton from './ResetButton'
-import i18n from '@test/i18n/i18n-for-tests'
+import { expect } from 'chai'
+import { Grommet } from 'grommet'
+import { Provider } from 'mobx-react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import zooTheme from '@zooniverse/grommet-theme'
+
+import mockStore from '@test/mockStore'
+
+import ResetButtonContainer from './ResetButtonContainer'
 
 describe('Component > ResetButton', function () {
-  it('should render without crashing', function () {
-    const wrapper = shallow(<ResetButton />)
-    expect(wrapper).to.be.ok()
-  })
+  function withStore (store) {
+    return function Wrapper ({ children }) {
+      return (
+        <Grommet theme={zooTheme}>
+          <Provider classifierStore={store}>
+            {children}
+          </Provider>
+        </Grommet>
+      )
+    }
+  }
 
-  it('should have an `a11yTitle` label', function () {
-    const useTranslationStub = sinon.stub(i18n, 't').callsFake((key) => key)
-    const wrapper = shallow(<ResetButton />)
-    expect(wrapper.prop('a11yTitle')).exists()
-    expect(useTranslationStub).to.have.been.calledWith('ImageToolbar.ResetButton.ariaLabel')
-    useTranslationStub.restore()
-  })
+  it('should have an accessible name', function () {
+    const store = mockStore()
 
-  it('should call the onClick prop function on click', function () {
-    const spy = sinon.spy()
-    const wrapper = shallow(
-      <ResetButton
-        onClick={spy}
-      />
+    render(
+      <ResetButtonContainer />, {
+        wrapper: withStore(store)
+      }
     )
-    wrapper.simulate('click')
-    expect(spy).to.have.been.called()
+
+    expect(screen.getByRole('button', { name: 'ImageToolbar.ResetButton.ariaLabel' })).to.be.ok()
+  })
+
+  it('should reset the subject viewer when clicked', async function () {
+    const store = mockStore()
+    store.subjectViewer.enableRotation()
+    store.subjectViewer.rotate()
+    store.subjectViewer.invertView()
+
+    expect(store.subjectViewer.rotation).to.equal(-90)
+    expect(store.subjectViewer.invert).to.be.true()
+
+    render(
+      <ResetButtonContainer />, {
+        wrapper: withStore(store)
+      }
+    )
+
+    await userEvent.click(screen.getByRole('button', { name: 'ImageToolbar.ResetButton.ariaLabel' }))
+
+    expect(store.subjectViewer.rotation).to.equal(0)
+    expect(store.subjectViewer.invert).to.be.false()
   })
 })
