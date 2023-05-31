46b3c108c00db66f2421bb749afb8a321d23e6dd
Mark Bouslog <mcbouslog@gmail.com>
2023-05-31 11:21:12 -0500

    lib-classifier: Refactor ImageToolbar buttons tests with RTL (#4702)
    * Delete icon tests
    
    * Delete container tests
    
    * Refactor AnnotateButton tests with testing-library
    
    * Refactor FullscreenButton tests with testing-library
    
    * Refactor MoveButton tests with testing-library
    
    * Refactor ResetButton tests with testing-library
    
    * Refactor RotateButton tests with testing-library
    
    * Refactor Zoom button tests with testing-library

--- packages/lib-classifier/src/components/Classifier/components/ImageToolbar/components/MoveButton/MoveIcon.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme'
-import MoveIcon from './MoveIcon'
-
-describe('Component > MoveIcon', function () {
-  it('should render without crashing', function () {
-    const wrapper = shallow(<MoveIcon />)
-    expect(wrapper).to.be.ok()
-  })
-})
