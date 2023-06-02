056625e2981e78488de34f08aa3f2ee4809b84a2
Delilah C <23665803+goplayoutside3@users.noreply.github.com>
2023-06-02 16:34:18 -0500

    app-content-pages: Add stories and RTL unit tests to publications components (#4673)
    * refactor components: Publication, Category, Project
    
    * handle PublicationsContainer stories and tests
    
    * apply PR review comments
    
    ---------
    
    Co-authored-by: Jim O'Donnell <jim@zooniverse.org>

--- packages/app-content-pages/pages/publications.js
+++ packages/app-content-pages/pages/publications.js
@@ -1,11 +1,11 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import PublicationsAPI from '../src/api/publications'
+import createPublicationsResponse from '../src/api/publications/publications.js'
 import logNodeError from '../src/helpers/logger/logNodeError.js'
 export { default } from '../src/screens/Publications'
 
 export async function getStaticProps({ locale }) {
   try {
-    const publicationsData = await PublicationsAPI.createPublicationsResponse()
+    const publicationsData = await createPublicationsResponse()
     return {
       props: {
         publicationsData,
