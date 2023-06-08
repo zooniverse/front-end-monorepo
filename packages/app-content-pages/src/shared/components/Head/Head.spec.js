208e7644429e7d147fd87605a782c0cd1a2fbea6
Delilah Clement <23665803+goplayoutside3@users.noreply.github.com>
2023-06-08 15:06:33 -0500

    refactor app-content-pages Head unit tests

--- packages/app-content-pages/src/shared/components/Head/Head.spec.js
+++ packages/app-content-pages/src/shared/components/Head/Head.spec.js
@@ -1,41 +1,21 @@
-import { shallow } from 'enzyme'
+import NextHead from 'next/head'
+import sinon from 'sinon'
+import { render, screen } from '@testing-library/react'
 
 import Head from './Head'
 
-let wrapper
-
 describe('Component > Head', function () {
   before(function () {
-    wrapper = shallow(<Head />)
+    const stub = sinon.stub(NextHead, 'default').callsFake(({ children }) => <>{children}</>)
+    render(<Head />)
   })
 
-  it('should render without crashing', function () {
-    expect(wrapper).to.be.ok()
-  })
-
-  it('should render a meta `og:url` tag', function () {
-    const tag = wrapper.find(`meta[property='og:url']`)
-    expect(tag).to.have.lengthOf(1)
-  })
-
-  it('should render a meta `og:title` tag', function () {
-    const tag = wrapper.find(`meta[property='og:title']`)
-    expect(tag).to.have.lengthOf(1)
-  })
-
-  it('should render a meta `og:description` tag', function () {
-    const tag = wrapper.find(`meta[property='og:description']`)
-    expect(tag).to.have.lengthOf(1)
-  })
-
-  it('should render a meta `twitter:site` tag', function () {
-    const tag = wrapper.find(`meta[name='twitter:site']`)
-    expect(tag).to.have.lengthOf(1)
-  })
-
-  it('should render a meta `twitter:card` tag', function () {
-    const tag = wrapper.find(`meta[name='twitter:card']`)
-    expect(tag).to.have.lengthOf(1)
-    expect(tag.prop('content')).to.equal('summary')
+  it('should render the required tags', function () {
+    screen.debug()
+    expect(document.querySelector(`meta[property='og:url']`)).to.be.ok()
+    expect(document.querySelector(`meta[property='og:title']`)).to.be.ok()
+    expect(document.querySelector(`meta[property='og:description']`)).to.be.ok()
+    expect(document.querySelector(`meta[name='twitter:site']`)).to.be.ok()
+    expect(document.querySelector(`meta[name='twitter:card']`)).to.be.ok()
   })
 })
