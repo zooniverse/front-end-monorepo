ea9c7a065ed8b9c352112970aaabf6ce49f509fe
Jim O'Donnell <jim@zooniverse.org>
2023-06-13 09:30:33 +0100

    Organisations: small fixes - add `organization` to the store during SSR, and pass a snapshot to the browser. - return null when organisations aren't found eg. they exist but are not public.

--- packages/app-project/stores/Organization.js
+++ packages/app-project/stores/Organization.js
@@ -5,9 +5,11 @@ import numberString from './types/numberString'
 const Organization = types
   .model('Organization', {
     id: types.maybeNull(numberString),
+    display_name: types.optional(types.string, ''),
+    listed: types.optional(types.boolean, false),
     primary_language: types.optional(types.string, 'en'),
     slug: types.optional(types.string, ''),
-    strings: types.frozen({}),
+    strings: types.frozen({})
   })
 
   .views(self => ({
