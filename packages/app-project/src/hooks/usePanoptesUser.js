6139f9204a85f5da4d697dc70154ab46ec420ef0
Jim O'Donnell <jim@zooniverse.org>
2023-06-15 09:05:53 +0100

    Get the user from the Panoptes JWT Refactor `usePanoptesUser` to get the current user from the stored Panoptes JWT. Fall back to `auth.checkCurrent()`, which is slower, if we don't have a stored token.

--- packages/app-project/src/hooks/usePanoptesUser.js
+++ packages/app-project/src/hooks/usePanoptesUser.js
@@ -1,3 +1,4 @@
+import * as panoptesJS from '@zooniverse/panoptes-js'
 import auth from 'panoptes-client/lib/auth'
 import useSWR from 'swr'
 
@@ -9,8 +10,35 @@ const SWRoptions = {
   refreshInterval: 0
 }
 
+async function decodeJWT(token) {
+  let user = null
+  let error = null
+  const decodedToken = await panoptesJS.auth.verify(token)
+  const { data } = decodedToken
+  error = decodedToken.error
+  if (data) {
+    user = {
+      id: data.id.toString(),
+      login: data.login,
+      display_name: data.dname,
+      admin: data.admin
+    }
+  }
+  return { user, error }
+}
+
 async function fetchPanoptesUser() {
   try {
+    const token = await auth.checkBearerToken()
+    if (token) {
+      const { user, error } = await decodeJWT(token)
+      if (user) {
+        return user
+      }
+      if (error) {
+        throw error
+      }
+    }
     return await auth.checkCurrent()
   } catch (error) {
     console.log(error)
@@ -18,7 +46,7 @@ async function fetchPanoptesUser() {
   }
 }
 
-export default function usePanoptesUser(key) {
+export default function usePanoptesUser(key = 'no-user') {
   const { data } = useSWR(key, fetchPanoptesUser, SWRoptions)
   return data
 }
