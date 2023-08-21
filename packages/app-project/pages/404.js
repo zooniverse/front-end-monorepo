7277d24051b8b0eb3ed5d39089f2b915d30f9da3
kieftrav <kiefertravis@gmail.com>
2023-08-21 09:31:03 -0500

    Add in the paragraph text and links back to the homepage and projects page. Convert the images to jpg and make them much smaller.

--- packages/app-project/pages/404.js
+++ packages/app-project/pages/404.js
@@ -1,9 +1,9 @@
+import { ZooHeaderWrapper } from '@components'
 import { ZooFooter } from '@zooniverse/react-components'
-import { Box, Image } from 'grommet'
+import { Box, Image, Heading, Text, Paragraph, Anchor } from 'grommet'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import styled from 'styled-components'
 
-import { ZooHeaderWrapper } from '@components'
 
 const ContainerBox = styled(Box)`
   position: relative;
@@ -34,10 +34,10 @@ export async function getStaticProps({ locale }) {
   }
 }
 
-export default function Error404({ locale = 'en', project404Fragment = '', staticAssetsPrefix = '' }) {
+export default function Error404({ locale = 'en' }) {
   const randomImage = Math.round(Math.random() * 8) + 1 // 1-9
-  const backgroundURL = `${staticAssetsPrefix}/projects/assets/background${randomImage}.png`
-  
+  const backgroundURL = `/projects/assets/background${randomImage}.jpg`
+
   return (
     <>
       <ZooHeaderWrapper locale={locale} />
@@ -58,16 +58,41 @@ export default function Error404({ locale = 'en', project404Fragment = '', stati
           height="44px"
           alignContent="center"
           justify="center"
+          textAlign="center"
         >
-          <Image
-            id="404-logo"
-            fit="contain"
-            height="44px"
-            alt="404"
-            src={`${staticAssetsPrefix}/projects/assets/logoWhite404.png`}
-          />
+          <Paragraph textAlign="center" style={{ lineHeight: '2em' }}>
+            <Image
+              id="404-logo"
+              fit="contain"
+              height="44px"
+              alt="404"
+              src={`/projects/assets/logoWhite404.png`}
+            />
+
+            <Heading level="2" textAlign="center" style={{ maxWidth: '100%' }}>Nothing here</Heading>
+
+            The page you're looking for no longer exists.
+            <br />
+            Return to the
+            &nbsp;
+            <Anchor
+              id="404-to-project-home"
+              color="white"
+              weight="bold"
+              href={`/`}
+              label="home page"
+            />
+            &nbsp;
+            or <br />
+            <Anchor
+              id="404-to-project-home"
+              color="white"
+              weight="bold"
+              href={`/projects`}
+              label="find a new project to explore"
+            />
+          </Paragraph>
         </PageContent>
-        {project404Fragment}
       </ContainerBox>
       <ZooFooter locale={locale} />
     </>
