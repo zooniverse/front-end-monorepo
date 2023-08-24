015b7a3ade629866da165f60abc183aa476ef0e5
kieftrav <kiefertravis@gmail.com>
2023-08-24 13:33:20 -0500

    ContentBox: Remove enzyme and convert tests to RTL

---
+++ packages/app-project/src/shared/components/ContentBox/ContentBox.mock.js
@@ -0,0 +1,18 @@
+export const ContentBoxMock = {
+  title: 'Test Title',
+  linkLabel: 'Test Label',
+  linkProps: {
+    href: 'https://www.zooniverse.org/projects/zooniverse/serengeti'
+  },
+  content: (
+    <div>
+      A natoque elementum venenatis tempus nec vel placerat vehicula vestibulum
+      platea, posuere eget quam donec magna sodales euismod parturient fermentum.
+      Gravida suspendisse dictum consectetur nec convallis ridiculus nisi, integer
+      proin condimentum urna in vestibulum adipiscing, lectus eros tempus metus
+      primis fermentum. Maecenas torquent posuere sit lorem vehicula conubia
+      habitant, vel est placerat semper hendrerit neque porta, parturient mi massa
+      metus integer quam.
+    </div>
+  )
+}
