18b1250be95ac54170ba6b99c8cde2a6e59d5a45
kieftrav <kiefertravis@gmail.com>
2023-08-31 09:26:21 -0500

    404 Page Design Updates

--- packages/app-project/pages/404.js
+++ packages/app-project/pages/404.js
@@ -1,14 +1,17 @@
+import { ThemeContext } from 'grommet'
 import { ZooHeaderWrapper } from '@components'
 import { ZooFooter } from '@zooniverse/react-components'
 import { Box, Image, Heading, Text, Paragraph, Anchor } from 'grommet'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import theme404 from './../src/helpers/404theme.js'
 import styled from 'styled-components'
 
 
 const ContainerBox = styled(Box)`
   position: relative;
   color: white;
+  background-color: #000;
   z-index: 1;
 `
 
@@ -47,30 +50,26 @@ export default function Error404({
   const { t } = useTranslation('components')
 
   return (
-    <>
+    <ThemeContext.Extend value={theme404}>
       <ZooHeaderWrapper locale={locale} />
       <ContainerBox
         width="100%"
-        height="50vh"
+        height="80vh"
         alignContent="center"
-        background="#1e1e1e"
         justify="center"
-        style={{ backgroundColor: '#000' }}
       >
         <Overlay
           background={`url("${backgroundURL}")`}
           width="100%"
-          height="50vh"
+          height="80vh"
           justify="center"
-          style={{ opacity: '50%' }}
         />
-
         <PageContent
           alignContent="center"
           justify="center"
           textAlign="center"
         >
-          <Paragraph textAlign="center">
+          <Paragraph textAlign="center" style={{ margin: 0 }}>
             <Image
               id="404-logo"
               fit="contain"
@@ -80,31 +79,37 @@ export default function Error404({
               src={`/projects/assets/logoWhite404.png`}
             />
           </Paragraph>
-          <Heading level="2" textAlign="center" style={{ maxWidth: '100%' }}>{t('404.heading')}</Heading>
+          <Heading
+            level="1"
+            textAlign="center"
+            margin={{ top: '20px', bottom: '20px' }}
+            style={{ maxWidth: '100%' }}
+          >{t('404.heading')}</Heading>
           <Paragraph textAlign="center">
-            {t('404.message')}
-            <br />
-            <Anchor
-              id="404-to-project-home"
-              color="white"
-              weight="bold"
-              href={`/`}
-              label={t('404.returnHome')}
-            />
-            <br />
-            {t('404.or')}
-            <br />
-            <Anchor
-              id="404-to-project-home"
-              color="white"
-              weight="bold"
-              href={`/projects`}
-              label={t('404.findNewProject')}
-            />
+            <Text style={{ fontStyle: 'italic', display: 'block', marginBottom: '30px' }}>{t('404.message')}</Text>
+            <Text style={{ display: 'block', marginTop: '30px', marginBottom: '15px' }}>• <Anchor
+                id="404-to-project-home"
+                color="white"
+                weight="bold"
+                href={`/`}
+                label={t('404.returnHome')}
+                style={{ textDecoration: 'underline' }}
+              />
+            </Text>
+            <Text style={{ display: 'block', marginTop: '15px' }}>•&nbsp;
+              <Anchor
+                id="404-to-project-home"
+                color="white"
+                weight="bold"
+                href={`/projects`}
+                label={t('404.findNewProject')}
+                style={{ textDecoration: 'underline' }}
+              />
+            </Text>
           </Paragraph>
         </PageContent>
       </ContainerBox>
       <ZooFooter locale={locale} />
-    </>
+    </ThemeContext.Extend>
   )
 }
