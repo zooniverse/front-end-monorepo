eaef472b826f2b68a1f345fbc548db951400025d
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-10-05 11:35:01 -0500

    add bundle analyzer for now, can be removed

--- packages/app-root/next.config.mjs
+++ packages/app-root/next.config.mjs
@@ -1,5 +1,11 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
-  /* config options here */
+
 }
 
-export default nextConfig
+export default bundleAnalyzer(nextConfig)
