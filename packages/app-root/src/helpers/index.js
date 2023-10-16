9d9f9ff12fec9ba1243a17abd36f17f4f4d2c779
Jim O'Donnell <jim@zooniverse.org>
2023-10-16 09:47:06 +0100

    Clean up usePanoptesUser - move `fetchPanoptesUser` into a helper. - add explanatory comments. - restore the missing `avatar_src` when getting the user object from a JWT.

---
+++ packages/app-root/src/helpers/index.js
@@ -0,0 +1,1 @@
+export { default as fetchPanoptesUser } from './fetchPanoptesUser.js'
--- packages/app-root/src/hooks/usePanoptesUser.js
+++ packages/app-root/src/hooks/usePanoptesUser.js
@@ -1,38 +1,21 @@
 import auth from 'panoptes-client/lib/auth'
-import { auth as authHelpers } from '@zooniverse/panoptes-js'
 import { useEffect, useState } from 'react'
 
-if (typeof window !== 'undefined') {
+import { fetchPanoptesUser } from '../helpers'
+
+const isBrowser = typeof window !== 'undefined'
+
+if (isBrowser) {
   auth.checkCurrent()
 }
 
-async function fetchPanoptesUser() {
-  try {
-    const authorization = await auth.checkBearerToken()
-    /* 
-      Use crypto.subtle as a proxy for checking for SSL.
-      It will only exist for https:// URLs.
-    */
-    const isSecure = crypto?.subtle
-    if (authorization && isSecure) {
-      const { user, error } = await authHelpers.decodeJWT(authorization)
-      if (user) {
-        return user
-      }
-      if (error) {
-        throw error
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-  return await auth.checkCurrent()
-}
-
-const isBrowser = typeof window !== 'undefined'
 const localStorage = isBrowser ? window.localStorage : null
 const storedUserJSON = localStorage?.getItem('panoptes-user')
 let user = storedUserJSON && JSON.parse(storedUserJSON)
+/*
+  Null users crash the ZooHeader component.
+  Set them to undefined for now.
+*/
 if (user === null) {
   user = undefined
 }
@@ -45,11 +28,10 @@ export default function usePanoptesUser() {
     async function checkUserSession() {
       setLoading(true)
       try {
-        const panoptesUser = await fetchPanoptesUser()
+        const panoptesUser = await fetchPanoptesUser(user)
         if (panoptesUser) {
-          const { admin, avatar_src, display_name, id, login } = panoptesUser
-          user = { admin, avatar_src, display_name, id, login }
-          localStorage?.setItem('panoptes-user', JSON.stringify(user))
+          localStorage?.setItem('panoptes-user', JSON.stringify(panoptesUser))
+          user = panoptesUser
         } else {
           user = undefined
           localStorage?.removeItem('panoptes-user')
@@ -60,7 +42,7 @@ export default function usePanoptesUser() {
       setLoading(false)
     }
 
-    if (typeof window !== 'undefined') {
+    if (isBrowser) {
       checkUserSession()
     }
     auth.listen('change', checkUserSession)
