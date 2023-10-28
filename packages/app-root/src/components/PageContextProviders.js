5a9a09fdda05c9d54803ebeea9eb53dd4aa888d6
Jim O'Donnell <jim@zooniverse.org>
2023-10-28 07:10:09 +0100

    Add Panoptes auth to app-root (#5459)
    * add Panoptes auth to app-root
    - add the Panoptes auth client, from `panoptes-client`.
    - add `usePanoptesUser` for SWR-style user fetching.
    - add an express server to handle HTTPS in local development.
    - add the panoptes user to both page header and footer.
    
    * Use a global user object (experimental)
    
    * Add unread messages and notifications
    - add `swr`.
    - `useUnreadMessages` fetches your unread message count with `useSWR`.
    - `useUnreadNotifications` fetchs your unread notifications count with `useSWR`.
    
    * Add admin mode
    - persist the current user in local storage.
    - check admin mode with `useAdminMode`.
    - add the admin mode toggle to the footer, and the admin mode border to the page.
    
    * Global page context
    Add `PageContextProvider`, responsible for:
    - global page styles.
    - providing the Zooniverse Grommet theme.
    - providing Panoptes auth context (user and admin mode.)
    
    * Clean up usePanoptesUser
    - move `fetchPanoptesUser` into a helper.
    - add explanatory comments.
    - restore the missing `avatar_src` when getting the user object from a JWT.
    
    * Configure ESLint
    
    * Optimise package imports
    
    * Rewrite usePanoptesUser with useSWR
    
    - use `useSWR` to fetch the user object from Panoptes.
    - persist the returned data in local storage.
    - reset the current Panoptes session when auth changes.
    
    * Add named page headers

---
+++ packages/app-root/src/components/PageContextProviders.js
@@ -0,0 +1,42 @@
+'use client'
+
+import zooTheme from '@zooniverse/grommet-theme'
+import { Grommet } from 'grommet'
+import { createGlobalStyle } from 'styled-components'
+
+import { PanoptesAuthContext } from '../contexts'
+import { useAdminMode, usePanoptesUser } from '../hooks'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
+/**
+  Context for every page:
+  - global page styles.
+  - Zooniverse Grommet theme.
+  - Panoptes auth (user account and admin mode.)
+*/
+export default function PageContextProviders({ children }) {
+  const { data: user, error, isLoading } = usePanoptesUser()
+  const { adminMode, toggleAdmin } = useAdminMode(user)
+  const authContext = { adminMode, error, isLoading, toggleAdmin, user }
+
+  return (
+    <PanoptesAuthContext.Provider value={authContext}>
+      <GlobalStyle />
+      <Grommet
+        background={{
+          dark: 'dark-1',
+          light: 'light-1'
+        }}
+        theme={zooTheme}
+      >
+        {children}
+      </Grommet>
+    </PanoptesAuthContext.Provider>
+  )
+
+}
