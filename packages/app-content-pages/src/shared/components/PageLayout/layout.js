ae77745da7fe2c959e60c5499f60a155d9337bdd
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-10-30 17:16:24 -0500

    add max width and elevation elements to layout.js

--- packages/app-content-pages/src/shared/components/PageLayout/layout.js
+++ packages/app-content-pages/src/shared/components/PageLayout/layout.js
@@ -1,11 +1,43 @@
 // Note that this file is named in the Next.js App Router convention for future refactor
+
+import { Box } from 'grommet'
+import styled from 'styled-components'
 import AboutHeader from '../AboutHeader'
+
+// elevation elements
+const ContainerBox = styled(Box)`
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -30px;
+    width: 30px;
+    height: clamp(100px, 10%, 500px);
+    background: linear-gradient(to bottom left, rgba(92, 92, 92, 0.25) 0%, rgba(92, 92, 92, 0) 60%);
+    clip-path: polygon(100% 0, 0 0, 100% 100%);
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: -30px;
+    width: 30px;
+    height: clamp(100px, 10%, 500px);
+    background: linear-gradient(to bottom right, rgba(92, 92, 92, 0.25) 0%, rgba(92, 92, 92, 0) 60%);
+    clip-path: polygon(100% 0, 0 0, 0 100%);
+  }
+`
 
 function PageLayout({ children }) {
   return (
     <>
       <AboutHeader />
-      {children}
+      <Box background='#EFF2F5' align='center'>
+        <ContainerBox background='white' width={{ max: '1440px' }}>{children}</ContainerBox>
+      </Box>
     </>
   )
 }
