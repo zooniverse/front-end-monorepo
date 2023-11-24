0f9098fc0d9315015d6a2b0df353279a28539ec3
Jim O'Donnell <jim@zooniverse.org>
2023-11-24 14:44:28 +0000

    Compose AboutDropdownNav stories

--- packages/app-project/src/screens/ProjectAboutPage/components/AboutDropdownNav/AboutDropdownNav.stories.js
+++ packages/app-project/src/screens/ProjectAboutPage/components/AboutDropdownNav/AboutDropdownNav.stories.js
@@ -1,48 +1,36 @@
 import { Box } from 'grommet'
 import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime'
-import Router from 'next/router'
 import PropTypes from 'prop-types'
 import AboutDropdownNav from '../AboutDropdownNav'
 
-function RouterMock({ children }) {
-  const mockRouter = {
-    asPath: '/projects/zooniverse/snapshot-serengeti/about/research',
-    push: () => {},
-    prefetch: () => new Promise((resolve, reject) => {}),
-    query: {
-      owner: 'zooniverse',
-      project: 'snapshot-serengeti'
-    }
+const mockRouter = {
+  asPath: '/projects/zooniverse/snapshot-serengeti/about/research',
+  push: () => {},
+  prefetch: () => new Promise((resolve, reject) => {}),
+  query: {
+    owner: 'zooniverse',
+    project: 'snapshot-serengeti'
   }
-
-  Router.router = mockRouter
-
-  return (
-    <RouterContext.Provider value={mockRouter}>
-      <Box pad='xsmall'>{children}</Box>
-    </RouterContext.Provider>
-  )
 }
 
-RouterMock.propTypes = {
-  children: PropTypes.node.isRequired
+function RouterMock(Story) {
+  return (
+    <RouterContext.Provider value={mockRouter}>
+      <Box pad='xsmall'>
+        <Story />
+      </Box>
+    </RouterContext.Provider>
+  )
 }
 
 const mockAboutNavLinks = ['research', 'team', 'education', 'faq']
 
 export default {
   title: 'Project App / Screens / About Pages / AboutDropdownNav',
-  component: AboutDropdownNav
+  component: AboutDropdownNav,
+  decorators: [RouterMock]
 }
 
-export const Default = ({ dark }) => (
-  <RouterMock>
-    <AboutDropdownNav aboutNavLinks={[]} />
-  </RouterMock>
-)
+export const Default = ({ dark }) => <AboutDropdownNav aboutNavLinks={[]} />
 
-export const MoreLinks = ({ dark }) => (
-  <RouterMock>
-    <AboutDropdownNav aboutNavLinks={mockAboutNavLinks} />
-  </RouterMock>
-)
+export const MoreLinks = ({ dark }) => <AboutDropdownNav aboutNavLinks={mockAboutNavLinks} />
