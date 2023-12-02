2831aff9f454d7bb8769030302c717002f840790
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-12-02 12:38:16 -0600

    uninstall enzyme

--- packages/app-content-pages/src/shared/components/AboutHeader/AboutHeader.spec.js
+++ packages/app-content-pages/src/shared/components/AboutHeader/AboutHeader.spec.js
@@ -4,24 +4,24 @@ import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtim
 import Router from 'next/router'
 import Meta, { Default } from './AboutHeader.stories.js'
 
-function RouterMock({ children }) {
-  const mockRouter = {
-    locale: 'en',
-    push: () => {},
-    prefetch: () => new Promise((resolve, reject) => {}),
-    query: {}
+describe('Component > AboutHeader', function () {
+  function RouterMock({ children }) {
+    const mockRouter = {
+      locale: 'en',
+      push: () => {},
+      prefetch: () => new Promise((resolve, reject) => {}),
+      query: {}
+    }
+
+    Router.router = mockRouter
+
+    return (
+      <RouterContext.Provider value={mockRouter}>
+        {children}
+      </RouterContext.Provider>
+    )
   }
 
-  Router.router = mockRouter
-
-  return (
-    <RouterContext.Provider value={mockRouter}>
-      {children}
-    </RouterContext.Provider>
-  )
-}
-
-describe.skip('Component > AboutHeader', function () {
   const DefaultStory = composeStory(Default, Meta)
 
   before(function () {
@@ -32,7 +32,7 @@ describe.skip('Component > AboutHeader', function () {
     )
   })
 
-  xit('should have the following links', function () {
+  it('should have the following links', function () {
     expect(document.querySelector('[href="/"]')).to.be.ok()
     expect(document.querySelector('[href="/publications"]')).to.be.ok()
     expect(document.querySelector('[href="/team"]')).to.be.ok()
