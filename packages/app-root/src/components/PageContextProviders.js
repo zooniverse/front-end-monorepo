57a056fb1e07611f8977823e972549e320cceece
Delilah C <23665803+goplayoutside3@users.noreply.github.com>
2023-12-04 15:27:16 -0600

    Build new ThemeModeToggle in ZooHeader (#5669)
    * build new ThemeModeToggle
    
    * Move over the translations

--- packages/app-root/src/components/PageContextProviders.js
+++ packages/app-root/src/components/PageContextProviders.js
@@ -1,11 +1,12 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import zooTheme from '@zooniverse/grommet-theme'
 import { usePanoptesUser } from '@zooniverse/react-components/hooks'
 import { Grommet } from 'grommet'
 import { createGlobalStyle } from 'styled-components'
 
-import { PanoptesAuthContext } from '../contexts'
+import { PanoptesAuthContext, ThemeModeContext } from '../contexts'
 import { useAdminMode } from '../hooks'
 
 const GlobalStyle = createGlobalStyle`
@@ -17,27 +18,57 @@ const GlobalStyle = createGlobalStyle`
 /**
   Context for every page:
   - global page styles.
-  - Zooniverse Grommet theme.
+  - Zooniverse Grommet theme and themeMode.
   - Panoptes auth (user account and admin mode.)
 */
 export default function PageContextProviders({ children }) {
+  const [themeMode, setThemeMode] = useState('light')
+
   const { data: user, error, isLoading } = usePanoptesUser()
   const { adminMode, toggleAdmin } = useAdminMode(user)
   const authContext = { adminMode, error, isLoading, toggleAdmin, user }
 
+  useEffect(() => {
+    const isBrowser = typeof window !== 'undefined'
+    const localStorage = isBrowser ? window.localStorage : null
+
+    // If no theme item in localStorage, see if the user's browser settings prefer dark mode
+    // If theme key is in localStorage, use that for themeMode
+    // The same key is used in PFE's theme mode toggle
+    if (isBrowser && !localStorage?.getItem('theme') ) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        setThemeMode('dark')
+        localStorage?.setItem('theme', 'dark')
+      }
+    } else if (isBrowser) {
+      setThemeMode(localStorage?.getItem('theme'))
+    }
+  }, [])
+
+  function toggleTheme() {
+    const newTheme = (themeMode === 'light') ? 'dark' : 'light'
+
+    setThemeMode(newTheme)
+    localStorage?.setItem('theme', newTheme)
+  }
+
+  const themeContext = { themeMode, toggleTheme }
+
   return (
     <PanoptesAuthContext.Provider value={authContext}>
-      <GlobalStyle />
-      <Grommet
-        background={{
-          dark: 'dark-1',
-          light: 'light-1'
-        }}
-        theme={zooTheme}
-      >
-        {children}
-      </Grommet>
+      <ThemeModeContext.Provider value={themeContext}>
+        <GlobalStyle />
+        <Grommet
+          background={{
+            dark: 'dark-1',
+            light: 'light-1'
+          }}
+          theme={zooTheme}
+          themeMode={themeMode}
+        >
+          {children}
+        </Grommet>
+      </ThemeModeContext.Provider>
     </PanoptesAuthContext.Provider>
   )
-
 }
