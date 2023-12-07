3c3b51b28bf88123470340e367b2d6b7bde2e05b
Mark Bouslog <mark@zooniverse.org>
2023-12-07 13:36:52 -0600

    Refactor stories with decorator

--- packages/lib-user/src/components/UserStats/components/ProfileHeader.stories.js
+++ packages/lib-user/src/components/UserStats/components/ProfileHeader.stories.js
@@ -2,12 +2,7 @@ import { Box } from 'grommet'
 
 import ProfileHeader from './ProfileHeader.js'
 
-export default {
-  title: 'Components/UserStats/ProfileHeader',
-  component: ProfileHeader
-}
-
-function WrappedStory ({ children }) {
+function WrappedStory (Story) {
   return (
     <Box
       background={{ dark: 'dark-3', light: 'neutral-6' }}
@@ -15,54 +10,52 @@ function WrappedStory ({ children }) {
       fill
       pad={{ horizontal: 'xlarge', vertical: 'small' }}
     >
-      {children}
+      <Story />
     </Box>
   )
 }
 
+export default {
+  title: 'Components/UserStats/ProfileHeader',
+  component: ProfileHeader,
+  decorators: [WrappedStory]
+}
+
 export const Default = () => (
-  <WrappedStory>
-    <ProfileHeader
-      avatar='https://panoptes-uploads-staging.zooniverse.org/user_avatar/e638f5a3-7ffb-4d23-bb08-f296377a2e74.jpeg'
-      classifications={384}
-      displayName='Test User'
-      login='TestUser'
-      projects={22}
-    />
-  </WrappedStory>
+  
+  <ProfileHeader
+    avatar='https://panoptes-uploads-staging.zooniverse.org/user_avatar/e638f5a3-7ffb-4d23-bb08-f296377a2e74.jpeg'
+    classifications={384}
+    displayName='Test User'
+    login='TestUser'
+    projects={22}
+  />
 )
 
 export const UserWithHours = () => (
-  <WrappedStory>
-    <ProfileHeader
-      avatar='https://panoptes-uploads-staging.zooniverse.org/user_avatar/e638f5a3-7ffb-4d23-bb08-f296377a2e74.jpeg'
-      displayName='Test User'
-      hours={5}
-      login='TestUser'
-      projects={22}
-    />
-  </WrappedStory>
+  <ProfileHeader
+    avatar='https://panoptes-uploads-staging.zooniverse.org/user_avatar/e638f5a3-7ffb-4d23-bb08-f296377a2e74.jpeg'
+    displayName='Test User'
+    hours={5}
+    login='TestUser'
+    projects={22}
+  />
 )
 
 export const Group = () => (
-  <WrappedStory>
-    <ProfileHeader
-      classifications={1526}
-      contributors={386}
-      displayName='Test Group'
-      projects={31}
-    />
-  </WrappedStory>
+  <ProfileHeader
+    classifications={1526}
+    contributors={386}
+    displayName='Test Group'
+    projects={31}
+  />
 )
 
 export const GroupWithHours = () => (
-  <WrappedStory>
-    <ProfileHeader
-      contributors={386}
-      displayName='Test Group'
-      hours={12}
-      projects={31}
-    />
-  </WrappedStory>
+  <ProfileHeader
+    contributors={386}
+    displayName='Test Group'
+    hours={12}
+    projects={31}
+  />
 )
-
