0ef574013f6e96550c8d5eca7cac8eeb3430df5d
Mark Bouslog <mark@zooniverse.org>
2023-12-07 13:36:26 -0600

    Remove duplicated DefaultStory

--- packages/lib-user/src/components/UserStats/components/ProfileHeader.spec.js
+++ packages/lib-user/src/components/UserStats/components/ProfileHeader.spec.js
@@ -5,36 +5,33 @@ import Meta, { Default, Group, GroupWithHours, UserWithHours } from './ProfileHe
 
 describe('components > UserStats > ProfileHeader', function () {
   describe('with a user', function () {
+    const DefaultStory = composeStory(Default, Meta)
+    
     it('should show the user\'s avatar', function () {
-      const DefaultStory = composeStory(Default, Meta)
       render(<DefaultStory />)
       const avatar = screen.getByAltText('TestUser avatar')
       expect(avatar).to.be.ok()
     })
 
     it('should show the user\'s display name', function () {
-      const DefaultStory = composeStory(Default, Meta)
       render(<DefaultStory />)
       const displayName = screen.getByText('Test User')
       expect(displayName).to.be.ok()
     })
 
     it('should show the user\'s login', function () {
-      const DefaultStory = composeStory(Default, Meta)
       render(<DefaultStory />)
       const login = screen.getByText('@TestUser')
       expect(login).to.be.ok()
     })
 
     it('should show the user\'s classifications', function () {
-      const DefaultStory = composeStory(Default, Meta)
       render(<DefaultStory />)
       const classifications = screen.getByText('384')
       expect(classifications).to.be.ok()
     })
 
     it('should show the user\'s projects', function () {
-      const DefaultStory = composeStory(Default, Meta)
       render(<DefaultStory />)
       const projects = screen.getByText('22')
       expect(projects).to.be.ok()
@@ -49,29 +46,27 @@ describe('components > UserStats > ProfileHeader', function () {
   })
 
   describe('with a group', function () {
+    const GroupStory = composeStory(Group, Meta)
+    
     it('should show the group\'s display name', function () {
-      const GroupStory = composeStory(Group, Meta)
       render(<GroupStory />)
       const displayName = screen.getByText('Test Group')
       expect(displayName).to.be.ok()
     })
 
     it('should show the group\'s classifications', function () {
-      const GroupStory = composeStory(Group, Meta)
       render(<GroupStory />)
       const classifications = screen.getByText('1526')
       expect(classifications).to.be.ok()
     })
 
     it('should show the group\'s contributors', function () {
-      const GroupStory = composeStory(Group, Meta)
       render(<GroupStory />)
       const contributors = screen.getByText('386')
       expect(contributors).to.be.ok()
     })
 
     it('should show the group\'s projects', function () {
-      const GroupStory = composeStory(Group, Meta)
       render(<GroupStory />)
       const projects = screen.getByText('31')
       expect(projects).to.be.ok()
