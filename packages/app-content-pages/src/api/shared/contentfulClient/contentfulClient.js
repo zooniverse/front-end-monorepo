413924601414b6a058a22bed66ff59a232ffd139
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-12-07 12:23:03 -0600

    return mock data if no contentful tokens

--- packages/app-content-pages/src/api/shared/contentfulClient/contentfulClient.js
+++ packages/app-content-pages/src/api/shared/contentfulClient/contentfulClient.js
@@ -1,10 +1,15 @@
 import { createClient } from 'contentful'
 
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN || 'Mock token'
-const space = process.env.CONTENTFUL_SPACE_ID || 'Mock space ID'
-const client = createClient({
-  accessToken,
-  space
-})
+let client = null
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
+const space = process.env.CONTENTFUL_SPACE_ID
+
+if (accessToken && space) {
+  console.log('TOKEN', accessToken)
+  client = createClient({
+    accessToken,
+    space
+  })
+}
 
 export default client
--- packages/app-content-pages/src/api/team/team.js
+++ packages/app-content-pages/src/api/team/team.js
@@ -1,7 +1,12 @@
 import processTeamData from './processTeamData'
 import client from '../shared/contentfulClient'
+import mockResponse from './processTeamData.result.json'
 
 export default async function createTeamResponse () {
-  const data = await client.getEntries({ content_type: 'person' })
-  return processTeamData(data)
+  if (client) {
+    const data = await client.getEntries({ content_type: 'person' })
+    return processTeamData(data)
+  } else {
+    return mockResponse
+  }
 }
