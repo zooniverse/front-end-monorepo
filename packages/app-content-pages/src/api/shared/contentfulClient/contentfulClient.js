b437aa4355b4d5ae2753e3ede8002af65332b272
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-12-07 12:39:02 -0600

    remove stray console

--- packages/app-content-pages/src/api/shared/contentfulClient/contentfulClient.js
+++ packages/app-content-pages/src/api/shared/contentfulClient/contentfulClient.js
@@ -5,7 +5,6 @@ const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
 const space = process.env.CONTENTFUL_SPACE_ID
 
 if (accessToken && space) {
-  console.log('TOKEN', accessToken)
   client = createClient({
     accessToken,
     space
