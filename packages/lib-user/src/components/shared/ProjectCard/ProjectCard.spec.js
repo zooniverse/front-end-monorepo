2dbf2d6fe530ab62d9a2926667990656636e93d7
Mark Bouslog <mark@zooniverse.org>
2023-12-07 16:13:10 -0600

    Add ProjectCard tests

---
+++ packages/lib-user/src/components/shared/ProjectCard/ProjectCard.spec.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import { composeStory } from '@storybook/react'
+
+import Meta, { Default } from './ProjectCard.stories.js'
+
+describe('components > shared > ProjectCard', function () {
+  const DefaultStory = composeStory(Default, Meta)
+
+  it('should show the project name', function () {
+    render(<DefaultStory />)
+
+    expect(screen.getByText('Notes from Nature - Capturing California\'s Flowers')).to.be.ok()
+  })
+
+  it('should show the project description', function () {
+    render(<DefaultStory />)
+
+    expect(screen.getByText('Using digital images to investigate ​phenological change in a biodiversity hotspot​')).to.be.ok()
+  })
+
+  it('should link to the project', function () {
+    render(<DefaultStory />)
+
+    expect(screen.getByRole('link', { href: 'https://www.zooniverse.org/projects/md68135/notes-from-nature-capturing-californias-flowers'})).to.be.ok()
+  })
+})
