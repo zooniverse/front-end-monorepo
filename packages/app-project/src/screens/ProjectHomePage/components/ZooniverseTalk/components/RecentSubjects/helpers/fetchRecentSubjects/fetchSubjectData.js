8cc7f25e6d18095705e6ee2e0696ade12ca62a59
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-12-11 14:14:20 -0600

    missing return value

--- packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchSubjectData.js
+++ packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchSubjectData.js
@@ -2,6 +2,7 @@ import { panoptes } from '@zooniverse/panoptes-js'
 import logToSentry from '@helpers/logger/logToSentry.js'
 
 async function fetchSubjectData(subjectIds) {
+  let recentlyDiscussedSubjects = []
   try {
     const response = await panoptes.get('/subjects', {
       id: subjectIds.join(',')
@@ -9,17 +10,17 @@ async function fetchSubjectData(subjectIds) {
     const { subjects } = response.body
     // response does not come back in the same order as subjectIds array
     // so we have to order them correctly here
-    const recentlyDiscussedSubjects = subjectIds.reduce((acc, id) => {
+    recentlyDiscussedSubjects = subjectIds.reduce((acc, id) => {
       // sometimes old subjects have been deleted, but their Talk thread still exists via their subject.id
       if (subjects.find(subject => subject.id === id)) {
         acc.push(subjects.find(subject => subject.id === id))
       }
       return acc
     }, [])
-    return recentlyDiscussedSubjects
   } catch (error) {
     logToSentry(error)
   }
+  return recentlyDiscussedSubjects
 }
 
 export default fetchSubjectData
