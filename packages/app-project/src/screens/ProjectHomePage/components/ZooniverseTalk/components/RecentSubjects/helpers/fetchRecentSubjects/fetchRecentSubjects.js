eb2d2ee37745059c8431bf126f0c442aa89c4fd2
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2023-12-11 14:02:50 -0600

    check if subject exists for recent subjects on homepage

--- packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchRecentSubjects.js
+++ packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchRecentSubjects.js
@@ -2,11 +2,12 @@ import fetchSubjectData from './fetchSubjectData'
 import fetchTalkData from './fetchTalkData'
 import getSubjectIdsFromTalkData from './getSubjectIdsFromTalkData'
 
-async function fetchRecentSubjects ({ projectId }) {
+async function fetchRecentSubjects({ projectId }) {
   const talkData = await fetchTalkData(projectId)
 
   if (talkData.length > 0) {
     const subjectIds = getSubjectIdsFromTalkData(talkData)
+    // const subjectIds = ['3815973', '1', '2']
     return await fetchSubjectData(subjectIds)
   }
 
--- packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchSubjectData.js
+++ packages/app-project/src/screens/ProjectHomePage/components/ZooniverseTalk/components/RecentSubjects/helpers/fetchRecentSubjects/fetchSubjectData.js
@@ -1,11 +1,25 @@
 import { panoptes } from '@zooniverse/panoptes-js'
+import logToSentry from '@helpers/logger/logToSentry.js'
 
-async function fetchSubjectData (subjectIds) {
-  const response = await panoptes.get('/subjects', {
-    id: subjectIds.join(',')
-  })
-  const { subjects } = response.body
-  return subjectIds.map(subjectID => subjects.find(subject => subject.id === subjectID))
+async function fetchSubjectData(subjectIds) {
+  try {
+    const response = await panoptes.get('/subjects', {
+      id: subjectIds.join(',')
+    })
+    const { subjects } = response.body
+    // response does not come back in the same order as subjectIds array
+    // so we have to order them correctly here
+    const recentlyDiscussedSubjects = subjectIds.reduce((acc, id) => {
+      // sometimes old subjects have been deleted, but their Talk thread still exists via their subject.id
+      if (subjects.find(subject => subject.id === id)) {
+        acc.push(subjects.find(subject => subject.id === id))
+      }
+      return acc
+    }, [])
+    return recentlyDiscussedSubjects
+  } catch (error) {
+    logToSentry(error)
+  }
 }
 
 export default fetchSubjectData
