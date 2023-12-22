476a438286e8a392b187535b6e667abefa6a8f12
Mark Bouslog <mark@zooniverse.org>
2023-12-22 00:27:17 -0600

    Add BarChart stories

---
+++ packages/lib-user/src/components/shared/BarChart/BarChart.mock.js
@@ -0,0 +1,101 @@
+const last7days = [
+  {
+    "period": "2023-05-27T00:00:00Z",
+    "count": 2468,
+    "session_time": 3234
+  },
+  {
+    "period": "2023-05-28T00:00:00Z",
+    "count": 1234,
+    "session_time": 1678
+  },
+  {
+    "period": "2023-05-29T00:00:00Z",
+    "count": 1456,
+    "session_time": 2432
+  },
+  {
+    "period": "2023-05-30T00:00:00Z",
+    "count": 1000,
+    "session_time": 2333
+  },
+  {
+    "period": "2023-05-31T00:00:00Z",
+    "count": 235,
+    "session_time": 1346
+  },
+  {
+    "period": "2023-06-01T00:00:00Z",
+    "count": 467,
+    "session_time": 1234
+  },
+  {
+    "period": "2023-06-02T00:00:00Z",
+    "count": 1876,
+    "session_time": 3234
+  }
+]
+
+const last30days = Array.from(
+  { length: 30 }, 
+  (_, i) => ({
+    period: new Date(2023, 4, 7 + i).toISOString(),
+    count: Math.floor(Math.random() * 3000),
+    session_time: Math.floor(Math.random() * 5400),
+  })
+)
+
+const thisMonth = Array.from(
+  { length: 18 },
+  (_, i) => ({
+    period: new Date(2023, 6, 1 + i).toISOString(),
+    count: Math.floor(Math.random() * 3000),
+    session_time: Math.floor(Math.random() * 5400),
+  })
+)
+
+const last3months = Array.from(
+  { length: 12 },
+  (_, i) => ({
+    period: new Date(2023, 4, 1 + (i * 7)).toISOString(),
+    count: Math.floor(Math.random() * 21000),
+    session_time: Math.floor(Math.random() * 37800),
+  })
+)
+
+const thisYear = Array.from(
+  { length: 9 },
+  (_, i) => ({
+    period: new Date(2023, i, 1).toISOString(),
+    count: Math.floor(Math.random() * 84000),
+    session_time: Math.floor(Math.random() * 151200),
+  })
+)
+
+const last12months = Array.from(
+  { length: 12 },
+  (_, i) => ({
+    period: new Date(2022, 9 + i, 1).toISOString(),
+    count: Math.floor(Math.random() * 84000),
+    session_time: Math.floor(Math.random() * 151200),
+  })
+)
+
+const allTime = Array.from(
+  { length: 9 },
+  (_, i) => ({
+    period: new Date(2015 + i, 0, 1).toISOString(),
+    count: Math.floor(Math.random() * 200000),
+    session_time: Math.floor(Math.random() * 302400),
+  })
+)
+
+export {
+  last7days,
+  last30days,
+  thisMonth,
+  last3months,
+  thisYear,
+  last12months,
+  allTime
+}
---
+++ packages/lib-user/src/components/shared/BarChart/BarChart.stories.js
@@ -0,0 +1,141 @@
+import { Box } from 'grommet'
+
+import BarChart from './BarChart.js'
+import {
+  last7days,
+  last30days,
+  thisMonth,
+  last3months,
+  thisYear,
+  last12months,
+  allTime
+} from './BarChart.mock.js'
+
+export default {
+  title: 'Components/shared/BarChart',
+  component: BarChart,
+  decorators: [ComponentDecorator]
+}
+
+function ComponentDecorator (Story) {
+  return (
+    <Box
+      background={{
+        dark: 'dark-3',
+        light: 'neutral-6'
+      }}
+      height='medium'
+      pad='30px'
+    >
+      <Story />
+    </Box>
+  )
+}
+
+export const Default = {
+  args: {
+    data: last7days,
+    dateRange: 'Last7Days'
+  },
+}
+
+export const Last7DaysHours = {
+  args: {
+    data: last7days,
+    dateRange: 'Last7Days',
+    type: 'session_time'
+  },
+}
+
+export const Last30Days = {
+  args: {
+    data: last30days,
+    dateRange: 'Last30Days'
+  },
+}
+
+export const Last30DaysHours = {
+  args: {
+    data: last30days,
+    dateRange: 'Last30Days',
+    type: 'session_time'
+  },
+}
+
+export const ThisMonth = {
+  args: {
+    data: thisMonth,
+    dateRange: 'ThisMonth'
+  },
+}
+
+export const ThisMonthHours = {
+  args: {
+    data: thisMonth,
+    dateRange: 'ThisMonth',
+    type: 'session_time'
+  },
+}
+
+export const Last3Months = {
+  args: {
+    data: last3months,
+    dateRange: 'Last3Months'
+  },
+}
+
+export const Last3MonthsHours = {
+  args: {
+    data: last3months,
+    dateRange: 'Last3Months',
+    type: 'session_time'
+  },
+}
+
+// maybe someday ThisYear by week if < 7 months
+export const ThisYear = {
+  args: {
+    data: thisYear,
+    dateRange: 'ThisYear'
+  },
+}
+
+export const ThisYearHours = {
+  args: {
+    data: thisYear,
+    dateRange: 'ThisYear',
+    type: 'session_time'
+  },
+}
+
+export const Last12Months = {
+  args: {
+    data: last12months,
+    dateRange: 'Last12Months'
+  },
+}
+
+export const Last12MonthsHours = {
+  args: {
+    data: last12months,
+    dateRange: 'Last12Months',
+    type: 'session_time'
+  },
+}
+
+export const AllTime = {
+  args: {
+    data: allTime,
+    dateRange: 'AllTime'
+  },
+}
+
+export const AllTimeHours = {
+  args: {
+    data: allTime,
+    dateRange: 'AllTime',
+    type: 'session_time'
+  },
+}
+
+// Custom
