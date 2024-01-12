569ae431822e68434d509d96cd4a58c80899e029
Mark Bouslog <mcbouslog@gmail.com>
2024-01-11 23:47:09 -0600

    lib-user: Add BarChart component (#5806)
    * Add ProfileHeader component
    
    * Add ProfileHeader tests
    
    * Refactor from type module
    
    * Remove from lib-user @node-loader/babel
    
    * Add type commonjs to package.json
    
    * Add mobile styling
    
    * Remove duplicated DefaultStory
    
    * Refactor stories with decorator
    
    * Fix props
    
    * Refactors for dev server
    
    * Add ProjectCard component
    
    * Add ProjectCard story
    
    * Update package.json for storybook build
    
    * Add ProjectCard tests
    
    * Add test:ci script
    
    * Refactor storybook setup
    
    * Refactor story decorators
    
    * Update stories, reorganize components
    
    * Refactor ProjectCard per design review
    
    * Add tabIndex to StyledSpacedText
    
    * Remove tabIndex from StyledSpacedText
    
    * Add BarChart component
    
    * Add getDateRangeLabel helper
    
    * Add BarChart stories
    
    * Add BarChart tests
    
    * Refactors for responsiveness
    
    * Add dateRanges map, related refactors
    
    * Refactors for x-axis label granularity
    
    * Fix BarChart test per refactored x-axis granularity
    
    * Add aria-label to BarChart

---
+++ packages/lib-user/src/components/shared/BarChart/BarChart.stories.js
@@ -0,0 +1,143 @@
+import { Box } from 'grommet'
+
+import dateRanges from './helpers/dateRanges.js'
+import {
+  last7days,
+  last30days,
+  thisMonth,
+  last3months,
+  thisYear,
+  last12months,
+  allTime
+} from './BarChart.mock.js'
+
+import BarChart from './BarChart.js'
+
+export default {
+  title: 'Components/shared/BarChart',
+  component: BarChart,
+  decorators: [ComponentDecorator]
+}
+
+function ComponentDecorator (Story) {
+  return (
+    <Box
+      background={{
+        dark: 'dark-3',
+        light: 'neutral-6'
+      }}
+      height='medium'
+      pad='30px'
+    >
+      <Story />
+    </Box>
+  )
+}
+
+export const Default = {
+  args: {
+    data: last7days,
+    dateRange: dateRanges.Last7Days
+  },
+}
+
+export const Last7DaysHours = {
+  args: {
+    data: last7days,
+    dateRange: dateRanges.Last7Days,
+    type: 'session_time'
+  },
+}
+
+export const Last30Days = {
+  args: {
+    data: last30days,
+    dateRange: dateRanges.Last30Days
+  },
+}
+
+export const Last30DaysHours = {
+  args: {
+    data: last30days,
+    dateRange: dateRanges.Last30Days,
+    type: 'session_time'
+  },
+}
+
+export const ThisMonth = {
+  args: {
+    data: thisMonth,
+    dateRange: dateRanges.ThisMonth
+  },
+}
+
+export const ThisMonthHours = {
+  args: {
+    data: thisMonth,
+    dateRange: dateRanges.ThisMonth,
+    type: 'session_time'
+  },
+}
+
+export const Last3Months = {
+  args: {
+    data: last3months,
+    dateRange: dateRanges.Last3Months
+  },
+}
+
+export const Last3MonthsHours = {
+  args: {
+    data: last3months,
+    dateRange: dateRanges.Last3Months,
+    type: 'session_time'
+  },
+}
+
+// maybe someday ThisYear by week if < 7 months
+export const ThisYear = {
+  args: {
+    data: thisYear,
+    dateRange: dateRanges.ThisYear
+  },
+}
+
+export const ThisYearHours = {
+  args: {
+    data: thisYear,
+    dateRange: dateRanges.ThisYear,
+    type: 'session_time'
+  },
+}
+
+export const Last12Months = {
+  args: {
+    data: last12months,
+    dateRange: dateRanges.Last12Months
+  },
+}
+
+export const Last12MonthsHours = {
+  args: {
+    data: last12months,
+    dateRange: dateRanges.Last12Months,
+    type: 'session_time'
+  },
+}
+
+export const AllTime = {
+  args: {
+    data: allTime,
+    dateRange: dateRanges.AllTime
+  },
+}
+
+export const AllTimeHours = {
+  args: {
+    data: allTime,
+    dateRange: dateRanges.AllTime,
+    type: 'session_time'
+  },
+}
+
+// Custom
