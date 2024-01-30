4cba700945323dbe028279cafd9a8d9f45e1cc66
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-01-29 21:07:24 -0600

    build Contact section and freshdesk widget

--- packages/app-content-pages/src/screens/About/About.js
+++ packages/app-content-pages/src/screens/About/About.js
@@ -1,6 +1,8 @@
-import { Box, Heading } from 'grommet'
+import { Box } from 'grommet'
 import { useTranslation } from 'next-i18next'
 import styled from 'styled-components'
+import Script from 'next/script'
+import { useState } from 'react'
 
 import PageLayout from '../../shared/components/PageLayout/layout.js'
 import DropdownNav from '../../shared/components/DropdownNav/DropdownNav.js'
@@ -14,6 +16,7 @@ import {
   StyledGrid,
   StyledHeading
 } from '../../shared/components/SharedStyledComponents/SharedStyledComponents.js'
+import Contact from './components/Contact.js'
 import OurMission from './components/OurMission.js'
 
 const StyledSidebar = styled(Sidebar)`
@@ -37,7 +40,7 @@ function HeadingForAboutNav({ sectionName, slug }) {
       size='1.5rem'
       tabIndex={-1}
       textAlign='center'
-      style={{ padding: '30px 0 15px 0' }}
+      style={{ padding: '30px 0 10px 0' }}
     >
       {sectionName}
     </HeadingForNav>
@@ -46,6 +49,7 @@ function HeadingForAboutNav({ sectionName, slug }) {
 
 function AboutPage() {
   const { t } = useTranslation('components')
+  const [widgetLoaded, setWidgetLoaded] = useState(false)
 
   const sidebarSections = [
     { name: t('AboutPage.ourMission.heading'), slug: '' },
@@ -57,6 +61,28 @@ function AboutPage() {
 
   return (
     <>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.fwSettings={
+          'widget_id':44000004375,
+          'locale': 'en'
+        };
+        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
+      `
+        }}
+      />
+      {/** Init the Freshdesk Widget, but hide it until volunteer clicks Contact section button */}
+      <Script
+        src='https://widget.freshworks.com/widgets/44000004375.js'
+        async
+        defer
+        onLoad={() => {
+          window.FreshworksWidget('hide', 'launcher')
+          setWidgetLoaded(true)
+        }}
+      />
+
       <Head
         description={t('AboutPage.description')}
         title={t('AboutPage.title')}
@@ -70,7 +96,11 @@ function AboutPage() {
           sections={sidebarSections}
         />
         <MaxWidthContent>
-          <StyledHeading color='brand' level='1' size='small'>
+          <StyledHeading
+            color={{ light: 'brand', dark: 'accent-1' }}
+            level='1'
+            size='small'
+          >
             {t('AboutPage.title')}
           </StyledHeading>
         </MaxWidthContent>
@@ -89,9 +119,6 @@ function AboutPage() {
                 sectionName={t('AboutPage.ourMission.heading')}
                 slug={sidebarSections[0].slug}
               />
-              <Heading level={3} size='1.5rem' alignSelf='center' margin='0'>
-                &quot;{t('AboutPage.ourMission.subheadings.one')}&quot;
-              </Heading>
               <OurMission />
             </Box>
           </article>
@@ -111,22 +138,23 @@ function AboutPage() {
             sectionName={t('AboutPage.mobile.heading')}
             slug={sidebarSections[2].slug}
           />
-
         </MaxWidthContent>
+
         {/** Highlights */}
         <MaxWidthContent>
           <HeadingForAboutNav
             sectionName={t('AboutPage.highlights.heading')}
             slug={sidebarSections[3].slug}
           />
-
         </MaxWidthContent>
+
         {/** Contact Us */}
         <MaxWidthContent>
           <HeadingForAboutNav
             sectionName={t('AboutPage.contact.heading')}
             slug={sidebarSections[4].slug}
           />
+          <Contact widgetLoaded={widgetLoaded} />
         </MaxWidthContent>
       </PageLayout>
     </>
