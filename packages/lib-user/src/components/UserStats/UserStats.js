b81d2a91b045b5625803c68e1d4024ad3773b81e
Mark Bouslog <mark@zooniverse.org>
2024-02-14 15:08:12 -0600

    Init add projects and date range selects

--- packages/lib-user/src/components/UserStats/UserStats.js
+++ packages/lib-user/src/components/UserStats/UserStats.js
@@ -14,19 +14,64 @@ import dateRanges from '../../utils/dateRanges'
 import Layout from '../shared/Layout/Layout'
 import ContentBox from '../shared/ContentBox/ContentBox'
 import ProfileHeader from '../shared/ProfileHeader/ProfileHeader'
+import Select from '../shared/Select/Select'
+
+const DEFAULT_PROJECT = {
+  label: 'ALL PROJECTS',
+  value: 'AllProjects'
+}
+
+const DEFAULT_DATE_RANGE = {
+  label: 'LAST 7 DAYS',
+  value: 'Last7Days'
+}
 
 function UserStats ({
   authClient,
   login = ''
 }) {
   const [activeTab, setActiveTab] = useState(0)
-  const { data: user, error, isLoading } = usePanoptesUser(authClient)
+  const [selectedProject, setSelectedProject] = useState(DEFAULT_PROJECT)
+  const [selectedDateRange, setSelectedDateRange] = useState(DEFAULT_DATE_RANGE)
 
+  let projectIDs = []
+
+  const { data: user, error, isLoading } = usePanoptesUser(authClient)
   const { data: userStats, error: statsError, isLoading: statsLoading } = useUserStats({ authClient, userID: user?.id })
+
   function onActive (index) {
     setActiveTab(index)
   }
 
+  function handleProjectSelect (project) {
+    setSelectedProject(project)
+  }
+
+  function handleDateRangeSelect (dateRange) {
+    setSelectedDateRange(dateRange)
+  }
+
+  if (userStats?.project_contributions?.length > 0) {
+    projectIDs = userStats.project_contributions.map(project => project.project_id)
+  }
+
+  let projectOptions = []
+  if (projectIDs?.length > 0) {
+    projectOptions = projectIDs.map(projectID => ({
+      label: projectID.toString(),
+      value: projectID.toString()
+    }))
+    projectOptions.unshift(DEFAULT_PROJECT)
+  }
+  
+  const dateRangeOptions = dateRanges.values.map((dateRange) => ({
+    label: dateRange
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/([0-9]+)/g, ' $1')
+      .toUpperCase()
+      .trim(),
+    value: dateRange.value
+  }))
 
   return (
     <Layout>
@@ -70,6 +115,20 @@ function UserStats ({
             direction='row'
             gap='32px'
           >
+            <Select
+              id='project-select'
+              name='project-select'
+              handleChange={handleProjectSelect}
+              options={projectOptions}
+              value={selectedProject}
+            />
+            <Select
+              id='date-range-select'
+              name='date-range-select'
+              handleChange={handleDateRangeSelect}
+              options={dateRangeOptions}
+              value={selectedDateRange}
+            />
           </Box>
         </Box>
       </ContentBox>
