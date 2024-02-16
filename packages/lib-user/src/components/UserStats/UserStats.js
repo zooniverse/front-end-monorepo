84fdb04b81f4048a4acd7e230752e9cbe791e56e
Mark Bouslog <mark@zooniverse.org>
2024-02-16 12:06:53 -0600

    Add MainContent component for ProfileHeader, BarChart, and related components

--- packages/lib-user/src/components/UserStats/UserStats.js
+++ packages/lib-user/src/components/UserStats/UserStats.js
@@ -55,14 +55,6 @@ function UserStats ({
   // set stats based on selected project or all projects
   const stats = selectedProject === 'AllProjects' ? allProjectsStats : projectStats
 
-  // determine top projects per date range
-  let topProjects = []
-  if (allProjectsStats?.project_contributions?.length > 0) {
-    topProjects = allProjectsStats.project_contributions
-    .sort((a, b) => b.count - a.count)
-    .slice(0, 5)
-  }
-
   return (
     <Layout>
       <MainContent
