eae9fe2def99b25e128417772c8773476e68b194
Mark Bouslog <mark@zooniverse.org>
2024-02-16 15:16:22 -0600

    Refactor ProfileHeader titled stats

--- packages/lib-user/src/components/shared/ProfileHeader/ProfileHeader.js
+++ packages/lib-user/src/components/shared/ProfileHeader/ProfileHeader.js
@@ -16,6 +16,11 @@ function TitledStat ({
   title = '',
   value = 0 
 }) {
+  let displayValue = value
+  if (isNaN(value)) {
+    displayValue = 0
+  }
+
   return (
     <Box
       align='center'
@@ -31,7 +36,7 @@ function TitledStat ({
         size='xlarge'
         weight='bold'
       >
-        {value.toLocaleString()}
+        {Math.round(displayValue).toLocaleString()}
       </SpacedText>
     </Box>
   )
@@ -44,12 +49,12 @@ TitledStat.propTypes = {
 
 function ProfileHeader ({
   avatar = '',
-  classifications = 0,
-  contributors = 0,
+  classifications = undefined,
+  contributors = undefined,
   displayName = '',
-  hours = 0,
+  hours = undefined,
   login = '',
-  projects = 0,
+  projects = undefined,
   screenSize = 'medium'
 }) {
   return (
@@ -96,25 +101,25 @@ function ProfileHeader ({
         direction='row'
         gap='small'
       >
-        {classifications ?
+        {classifications !== undefined ?
           <TitledStat
             title='Classifications'
             value={classifications} 
           />
           : null}
-        {hours ?
+        {hours !== undefined ?
           <TitledStat
             title='Hours'
             value={hours}
           /> 
           : null}
-        {contributors ?
+        {contributors !== undefined ?
           <TitledStat
             title='Contributors'
             value={contributors}
           />
           : null}
-        {projects ?
+        {projects !== undefined ?
           <TitledStat
             title='Projects'
             value={projects}
