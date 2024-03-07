d3fdd2a0f4a78f99c058801d15823cef9f64e03e
Mark Bouslog <mark@zooniverse.org>
2024-03-07 14:14:15 -0600

    Revert changes to webpack setup

--- packages/lib-user/webpack.dev.js
+++ packages/lib-user/webpack.dev.js
@@ -40,6 +40,11 @@ module.exports = {
   ],
   mode: 'development',
   resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils')
+    },
     fallback: {
       fs: false,
       // for markdown-it plugins
