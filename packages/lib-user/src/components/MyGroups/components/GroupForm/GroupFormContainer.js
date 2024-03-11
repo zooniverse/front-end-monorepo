b4129e99182eb333374a648f4551886073993f44
Mark Bouslog <mark@zooniverse.org>
2024-03-11 17:04:35 -0500

    Refactor create user_group

--- packages/lib-user/src/components/MyGroups/components/GroupForm/GroupFormContainer.js
+++ packages/lib-user/src/components/MyGroups/components/GroupForm/GroupFormContainer.js
@@ -1,22 +1,25 @@
-import GroupForm from './GroupForm'
+import { v4 as uuidv4 } from 'uuid'
 
 import {
-  createPanoptesUserGroup,
-  getBearerToken
+  createPanoptesUserGroup
 } from '@utils'
+
+import GroupForm from './GroupForm'
 
 function GroupFormContainer() {
   async function onSubmit(event) {
-    console.log('submitting...', event.value)
+    const { display_name, stats_visibility } = event.value
+    const name = uuidv4()
+    const data = {
+      name,
+      display_name,
+      stats_visibility
+    }
 
     try {
-      // const authorization = await getBearerToken(authClient)
-      // const newGroup = await createPanoptesUserGroup({
-      //   data,
-      //   authorization 
-      // })
-      // console.log('newGroup', newGroup)
-      // window.location.reload()
+      const newGroup = await createPanoptesUserGroup({ data })
+      // TODO: route to new group stats page
+      window.location.reload()
     } catch (error) {
       console.error(error)
     }
