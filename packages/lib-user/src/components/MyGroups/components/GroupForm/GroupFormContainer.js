db135081c563d53b1be5d2d07d92a118537e5d31
Mark Bouslog <mark@zooniverse.org>
2024-03-17 23:09:44 -0500

    Refactor GroupFormContainer create group

--- packages/lib-user/src/components/MyGroups/components/GroupForm/GroupFormContainer.js
+++ packages/lib-user/src/components/MyGroups/components/GroupForm/GroupFormContainer.js
@@ -1,12 +1,21 @@
 import { v4 as uuidv4 } from 'uuid'
 
 import {
+  usePanoptesAuth
+} from '@hooks'
+
+import {
   createPanoptesUserGroup
 } from '@utils'
 
 import GroupForm from './GroupForm'
 
-function GroupFormContainer() {
+function GroupFormContainer({
+  authClient = {},
+  authUserId = ''
+}) {
+  const authorization = usePanoptesAuth({ authClient, authUserId })
+  
   async function onSubmit(event) {
     const { display_name, stats_visibility } = event.value
     const name = uuidv4()
@@ -16,8 +25,9 @@ function GroupFormContainer() {
       stats_visibility
     }
 
+
     try {
-      const newGroup = await createPanoptesUserGroup({ data })
+      const newGroup = await createPanoptesUserGroup({ data, authorization })
       // TODO: route to new group stats page
       window.location.reload()
     } catch (error) {
