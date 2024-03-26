4330b93dca4e1055a80a4b53155590f0cdc2cfcb
Mark Bouslog <mark@zooniverse.org>
2024-03-26 13:11:58 -0500

    Refactor MyGroups create group

--- packages/lib-user/src/components/MyGroups/MyGroups.js
+++ packages/lib-user/src/components/MyGroups/MyGroups.js
@@ -1,45 +1,26 @@
 import { Grid } from 'grommet'
-import { func, node } from 'prop-types'
-
-import { ContentBox, Layout } from '@components/shared'
-import CreateGroup from './CreateGroup'
-
-const DEFAULT_HANDLER = () => true
+import { node } from 'prop-types'
 
 function MyGroups({
-  children,
-  handleGroupCreate = DEFAULT_HANDLER
+  children
 }) {
   return (
-    <Layout>
-      <ContentBox
-        linkLabel='Learn more about Groups'
-        linkProps={{ href: '/groups' }}
-        title='My Groups'
-        pad={{ horizontal: '60px', vertical: '30px' }}
-      >
-        <Grid
-          as='ul'
-          columns={{
-            count: 2,
-            size: 'auto'
-          }}
-          gap={{ row: '20px', column: '40px' }}
-          pad='none'
-        >
-          {children}
-        </Grid>
-        <CreateGroup
-          handleGroupCreate={handleGroupCreate}
-        />
-      </ContentBox>
-    </Layout>
+    <Grid
+      as='ul'
+      columns={{
+        count: 2,
+        size: 'auto'
+      }}
+      gap={{ row: '20px', column: '40px' }}
+      pad='none'
+    >
+      {children}
+    </Grid>
   )
 }
 
 MyGroups.propTypes = {
-  children: node,
-  handleGroupCreate: func
+  children: node
 }
 
 export default MyGroups
