1daebf369423d110bede685f00d3db97bf7a012a
Delilah C <23665803+goplayoutside3@users.noreply.github.com>
2024-04-03 14:46:18 -0500

    backfill translation.strings keys when a project team partially translates a language (#6016)

--- packages/lib-classifier/src/hooks/usePanoptesTranslations.js
+++ packages/lib-classifier/src/hooks/usePanoptesTranslations.js
@@ -1,5 +1,6 @@
 import useSWR from 'swr'
 import { panoptes } from '@zooniverse/panoptes-js'
+import { mergeWith } from 'lodash'
 
 const SWRoptions = {
   revalidateIfStale: true,
@@ -9,7 +10,12 @@ const SWRoptions = {
   refreshInterval: 0
 }
 
-async function fetchTranslations({ translated_id, translated_type, language, fallback = 'en' }) {
+async function fetchTranslations({
+  translated_id,
+  translated_type,
+  language,
+  fallback = 'en'
+}) {
   const lowerCaseLang = language?.toLowerCase() // zh-CN and zh-TW are stored as lowercase in panoptes
   const languages = language === fallback ? fallback : `${lowerCaseLang},${fallback}`
   const response = await panoptes.get('/translations', {
@@ -17,13 +23,29 @@ async function fetchTranslations({ translated_id, translated_type, language, fal
     translated_id,
     translated_type
   })
+
   const translation = response.body.translations.find(t => t.language === lowerCaseLang)
   const original = response.body.translations.find(t => t.language === fallback)
-  return translation || original
+
+  if (!translation || !Object.keys(translation.strings).length) {
+    return original
+  }
+
+  /* If a project team leaves part of a language untranslated, backfill it with the strings from the fallback language */
+  const customizer = (objValue, srcValue) => objValue === "" ? srcValue : objValue
+
+  const mergedStrings = mergeWith(translation.strings, original.strings, customizer)
+  return Object.assign(translation, {strings: mergedStrings})
 }
 
-export default function usePanoptesTranslations({ translated_id, translated_type, language }) {
-  const key = translated_id ? { translated_id, translated_type, language } : null
+export default function usePanoptesTranslations({
+  translated_id,
+  translated_type,
+  language
+}) {
+  const key = translated_id
+    ? { translated_id, translated_type, language }
+    : null
   const { data } = useSWR(key, fetchTranslations, SWRoptions)
   return data
 }
