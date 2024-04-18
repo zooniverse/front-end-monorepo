44c108e5ddc17abe3b9b48a43df0a37f7a1ae481
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-04-18 15:22:17 -0500

    move root About page components to lib-content

--- packages/app-root/src/app/about/page.js
+++ packages/app-root/src/app/about/page.js
@@ -1,15 +1,12 @@
+import { About } from '@zooniverse/content'
+
 export const metadata = {
   title: 'About',
   description: 'About The Zooniverse'
 }
 
 export default function AboutPage() {
-
   return (
-    <>
-      <div>
-        <p>This is lib-content</p>
-      </div>
-    </>
+    <About />
   )
 }
--- packages/lib-content/src/index.js
+++ packages/lib-content/src/index.js
@@ -1,2 +1,3 @@
+export { default as About } from './screens/About/About.js'
 export { default as FAQ } from './screens/FAQ/FAQ.js'
 export { default as Resources } from './screens/Resources/Resources.js'
