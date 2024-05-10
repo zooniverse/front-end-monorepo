eaf2248780fdac513abce2f09c84093746b82cb2
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-05-10 13:57:04 -0500

    wip

--- packages/app-root/src/app/page.js
+++ packages/app-root/src/app/page.js
@@ -1,8 +1,21 @@
+'use client'
+
+import { useContext } from 'react'
+import { PanoptesAuthContext } from '../contexts'
+
 export default function HomePage() {
+  const { isLoading, user } = useContext(PanoptesAuthContext)
+
+  // While user is loading, show placeholder larger than 100vh
+  // so that once correct component renders, there's not a big
+  // page jump
   return (
     <main>
+      {!isLoading && <div>
+        {user ? <p>Signed-in</p> : <p>Signed-out</p>}
+      </div>}
       <div>
-        <p>This is the zooniverse.org home page</p>
+        Blog section regardless
       </div>
     </main>
   )
