41e8c0e1be45071d9035f68b985e9117779d0873
Mark Bouslog <mark@zooniverse.org>
2024-05-31 09:50:55 -0500

    Add Contributors data export tests

---
+++ packages/lib-user/src/components/Contributors/helpers/generateExport.spec.js
@@ -0,0 +1,38 @@
+import sinon from 'sinon'
+
+import { PROJECTS, USER_GROUPS, USER, GROUP_MEMBER_USER, GROUP_ADMIN_USER } from '../../../../test/mocks/panoptes'
+import { group_member_stats_breakdown } from '../../../../test/mocks/stats.mock'
+
+import { generateExport } from './generateExport'
+
+describe('Contributors > generateExport', function () {
+  let setFilename
+  let projects
+  let stats
+  let users
+
+  before(function () {
+    setFilename = sinon.stub()
+    projects = PROJECTS
+    stats = {
+      group_member_stats_breakdown
+    }
+    users = [USER, GROUP_MEMBER_USER, GROUP_ADMIN_USER]
+  })
+
+  after(function () {
+    setFilename.resetHistory()
+  })
+
+  it('should call setFilename with the correct filename', function () {
+    generateExport({
+      group: USER_GROUPS[0],
+      handleFileName: setFilename,
+      projects,
+      stats,
+      users
+    })
+
+    expect(setFilename).to.be.calledWithMatch(/Test Group 🍩.data_export.\d+.csv/)
+  })
+})
