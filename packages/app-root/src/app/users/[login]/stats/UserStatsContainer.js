f4d862102b804f07e7f8ef7cf16c6b57555fe581
Mark Bouslog <mark@zooniverse.org>
2024-07-04 10:30:13 -0500

    Refactor user stats project state per search param

--- packages/app-root/src/app/users/[login]/stats/UserStatsContainer.js
+++ packages/app-root/src/app/users/[login]/stats/UserStatsContainer.js
@@ -1,21 +1,39 @@
 'use client'
 
 import { UserStats } from '@zooniverse/user'
+import { useRouter } from 'next/navigation'
 import { useContext } from 'react'
 
-import { PanoptesAuthContext, UserStatsContext } from '../../../../contexts'
 import AuthenticatedUsersPageContainer from '../../../../components/AuthenticatedUsersPageContainer'
+import { PanoptesAuthContext, UserStatsContext } from '../../../../contexts'
 
 function UserStatsContainer({
-  login
+  login,
+  projectId
 }) {
   const { adminMode, isLoading, user } = useContext(PanoptesAuthContext)
   const {
     selectedDateRange,
-    selectedProject,
-    setSelectedDateRange,
-    setSelectedProject
+    setSelectedDateRange
   } = useContext(UserStatsContext)
+  const router = useRouter()
+  const selectedProject = projectId || 'AllProjects'
+
+  function updateQueryParam({ key, value }) {
+    const queryParams = new URLSearchParams(window.location.search)
+  
+    if (key === 'project_id' && value === 'AllProjects') {
+      queryParams.delete(key);
+    } else {
+      queryParams.set(key, value);
+    }
+  
+    router.push(`${window.location.pathname}?${queryParams.toString()}`)
+  }
+
+  function setSelectedProject(selectedProjectId) {
+    updateQueryParam({ key: 'project_id', value: selectedProjectId })
+  }
 
   return (
     <AuthenticatedUsersPageContainer
--- packages/app-root/src/app/users/[login]/stats/page.js
+++ packages/app-root/src/app/users/[login]/stats/page.js
@@ -5,10 +5,11 @@ export const metadata = {
   description: 'My Zooniverse user stats page'
 }
 
-export default function UserStatsPage({ params }) {
+export default function UserStatsPage({ params, searchParams }) {
   return (
     <UserStatsContainer
       login={params.login}
+      projectId={searchParams.project_id}
     />
   )
 }
