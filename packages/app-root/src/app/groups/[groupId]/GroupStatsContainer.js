688d29c97c4fff216dea3fa91d1b7286627db190
Mark Bouslog <mark@zooniverse.org>
2024-07-07 09:46:38 -0500

    Refactor app-root group stats date range state per search param

--- packages/app-root/src/app/groups/[groupId]/GroupStatsContainer.js
+++ packages/app-root/src/app/groups/[groupId]/GroupStatsContainer.js
@@ -7,29 +7,69 @@ import { useContext } from 'react'
 import { PanoptesAuthContext } from '../../../contexts'
 
 function GroupStatsContainer({
+  endDate,
   groupId,
   joinToken,
-  projectId
+  projectId,
+  startDate
 }) {
   const { adminMode, isLoading, user } = useContext(PanoptesAuthContext)
 
   const router = useRouter()
+  
+  // set end date per query params or default to today
+  let selectedEndDate = endDate
+  if (!selectedEndDate) {
+    selectedEndDate = new Date().toISOString().substring(0, 10)
+  }
+  // set start date per query params or default to 7 days ago
+  let selectedStartDate = startDate
+  if (!selectedStartDate) {
+    selectedStartDate = new Date(new Date().setDate(new Date().getDate() - 6)).toISOString().substring(0, 10)
+  }
+  
+  // set selected project per query params or default to 'AllProjects'
   const selectedProject = projectId || 'AllProjects'
 
-  function updateQueryParam({ key, value }) {
+  function updateQueryParams(newQueryParams) {
     const queryParams = new URLSearchParams(window.location.search)
-  
-    if (key === 'project_id' && value === 'AllProjects') {
-      queryParams.delete(key);
-    } else {
-      queryParams.set(key, value);
+
+    for (const [key, value] of newQueryParams) {  
+      if (!value) {
+        queryParams.delete(key);
+      } else {
+        queryParams.set(key, value);
+      }
     }
   
     router.push(`${window.location.pathname}?${queryParams.toString()}`)
   }
 
+  function setSelectedDateRange({ endDate, startDate }) {
+    // TODO: validate dates
+    
+    const todayUTC = new Date().toISOString().substring(0, 10)
+    if (endDate === todayUTC) {
+      updateQueryParams([
+        ['end_date', null],
+        ['start_date', startDate]
+      ])
+    } else {
+      updateQueryParams([
+        ['end_date', endDate],
+        ['start_date', startDate]
+      ])
+    } 
+  }
+
   function setSelectedProject(selectedProjectId) {
-    updateQueryParam({ key: 'project_id', value: selectedProjectId })
+    // TODO: validate selected project ID
+
+    if (selectedProjectId === 'AllProjects') {
+      updateQueryParams([['project_id', null]])
+    } else {
+      updateQueryParams([['project_id', selectedProjectId]])
+    }
   }
 
   if (typeof window === 'undefined' || isLoading) {
@@ -44,7 +84,12 @@ function GroupStatsContainer({
       authUser={user}
       groupId={groupId}
       joinToken={joinToken}
+      selectedDateRange={{
+        endDate: selectedEndDate,
+        startDate: selectedStartDate
+      }}
       selectedProject={selectedProject}
+      setSelectedDateRange={setSelectedDateRange}
       setSelectedProject={setSelectedProject}
     />
   )
