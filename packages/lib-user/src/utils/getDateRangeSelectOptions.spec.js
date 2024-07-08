b59bea7f4d269f2f12d1c2eacb564c94929f2759
Mark Bouslog <mark@zooniverse.org>
2024-07-08 10:44:24 -0500

    Add getDateRangeSelectOptions test

---
+++ packages/lib-user/src/utils/getDateRangeSelectOptions.spec.js
@@ -0,0 +1,49 @@
+import sinon from 'sinon'
+
+import { getDateRangeSelectOptions } from './getDateRangeSelectOptions'
+
+describe('utils > getDateRangeSelectOptions', function () {
+  let clock
+
+  beforeEach(function () {
+    clock = sinon.useFakeTimers(new Date(2023, 3, 15))
+  })
+
+  afterEach(function () {
+    clock.restore()
+  })
+
+  it('should return the expected date range select options', function () {
+    const dateRangeSelectOptions = getDateRangeSelectOptions('2015-07-01')
+    expect(dateRangeSelectOptions).to.deep.equal([
+      {
+        label: 'LAST 7 DAYS',
+        value: '2023-04-09'
+      },
+      {
+        label: 'LAST 30 DAYS',
+        value: '2023-03-17'
+      },
+      {
+        label: 'THIS MONTH',
+        value: '2023-04-01'
+      },
+      {
+        label: 'LAST 3 MONTHS',
+        value: '2023-01-15'
+      },
+      {
+        label: 'THIS YEAR',
+        value: '2023-01-01'
+      },
+      {
+        label: 'LAST 12 MONTHS',
+        value: '2022-05-01'
+      },
+      {
+        label: 'ALL TIME',
+        value: '2015-07-01'
+      }
+    ])
+  })
+})
