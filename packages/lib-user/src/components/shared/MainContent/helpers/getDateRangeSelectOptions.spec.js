1a2f7148504606c6ed358d5140cca6d82c94e647
Mark Bouslog <mark@zooniverse.org>
2024-07-23 08:40:56 +0100

    Add getDateRangeSelectOptions tests before and after UTC

--- packages/lib-user/src/components/shared/MainContent/helpers/getDateRangeSelectOptions.spec.js
+++ packages/lib-user/src/components/shared/MainContent/helpers/getDateRangeSelectOptions.spec.js
@@ -2,48 +2,96 @@ import sinon from 'sinon'
 
 import { getDateRangeSelectOptions } from './getDateRangeSelectOptions'
 
-describe('utils > getDateRangeSelectOptions', function () {
+describe.only('utils > getDateRangeSelectOptions', function () {
   let clock
 
-  beforeEach(function () {
-    clock = sinon.useFakeTimers(new Date(2023, 3, 15))
+  describe('when the current date is after UTC', function () {
+
+    beforeEach(function () {
+      clock = sinon.useFakeTimers(new Date('2023-04-15T12:00:00+02:00'))
+    })
+
+    afterEach(function () {
+      clock.restore()
+    })
+
+    it('should return the expected date range select options', function () {
+      const dateRangeSelectOptions = getDateRangeSelectOptions('2015-07-01')
+      expect(dateRangeSelectOptions).to.deep.equal([
+        {
+          label: 'LAST 7 DAYS',
+          value: '2023-04-09'
+        },
+        {
+          label: 'LAST 30 DAYS',
+          value: '2023-03-17'
+        },
+        {
+          label: 'THIS MONTH',
+          value: '2023-04-01'
+        },
+        {
+          label: 'LAST 3 MONTHS',
+          value: '2023-01-15'
+        },
+        {
+          label: 'THIS YEAR',
+          value: '2023-01-01'
+        },
+        {
+          label: 'LAST 12 MONTHS',
+          value: '2022-05-01'
+        },
+        {
+          label: 'ALL TIME',
+          value: '2015-07-01'
+        }
+      ])
+    })
   })
 
-  afterEach(function () {
-    clock.restore()
-  })
+  describe('when the current date is before UTC', function () {
+      
+    beforeEach(function () {
+      clock = sinon.useFakeTimers(new Date('2023-04-15T12:00:00-02:00'))
+    })
 
-  it('should return the expected date range select options', function () {
-    const dateRangeSelectOptions = getDateRangeSelectOptions('2015-07-01')
-    expect(dateRangeSelectOptions).to.deep.equal([
-      {
-        label: 'LAST 7 DAYS',
-        value: '2023-04-09'
-      },
-      {
-        label: 'LAST 30 DAYS',
-        value: '2023-03-17'
-      },
-      {
-        label: 'THIS MONTH',
-        value: '2023-04-01'
-      },
-      {
-        label: 'LAST 3 MONTHS',
-        value: '2023-01-15'
-      },
-      {
-        label: 'THIS YEAR',
-        value: '2023-01-01'
-      },
-      {
-        label: 'LAST 12 MONTHS',
-        value: '2022-05-01'
-      },
-      {
-        label: 'ALL TIME',
-        value: '2015-07-01'
-      }
-    ])
+    afterEach(function () {
+      clock.restore()
+    })
+
+    it('should return the expected date range select options', function () {
+      const dateRangeSelectOptions = getDateRangeSelectOptions('2015-07-01')
+      expect(dateRangeSelectOptions).to.deep.equal([
+        {
+          label: 'LAST 7 DAYS',
+          value: '2023-04-09'
+        },
+        {
+          label: 'LAST 30 DAYS',
+          value: '2023-03-17'
+        },
+        {
+          label: 'THIS MONTH',
+          value: '2023-04-01'
+        },
+        {
+          label: 'LAST 3 MONTHS',
+          value: '2023-01-15'
+        },
+        {
+          label: 'THIS YEAR',
+          value: '2023-01-01'
+        },
+        {
+          label: 'LAST 12 MONTHS',
+          value: '2022-05-01'
+        },
+        {
+          label: 'ALL TIME',
+          value: '2015-07-01'
+        }
+      ])
+    })
   })
 })
