6e0366287aab1b87e3fe404b6d87df079857c0f0
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-09-10 12:44:41 -0500

    content pages copy and link updates

--- packages/lib-content/src/screens/FAQ/FAQ.js
+++ packages/lib-content/src/screens/FAQ/FAQ.js
@@ -39,7 +39,11 @@ function FAQPage() {
         </MobileHeading>
         <Box pad={{ horizontal: 'medium' }} align='center'>
           <MaxWidthContent>
-            <StyledHeading color={{ light: 'neutral-1', dark: 'accent-1' }} level='1' size='small'>
+            <StyledHeading
+              color={{ light: 'neutral-1', dark: 'accent-1' }}
+              level='1'
+              size='small'
+            >
               {t('FAQ.title')}
             </StyledHeading>
             <Paragraph margin={{ vertical: 'medium' }}>
@@ -55,10 +59,7 @@ function FAQPage() {
                     key='talk-page'
                     href='https://www.zooniverse.org/talk'
                   />,
-                  <Anchor
-                    key='contact-us'
-                    href='/about#contact'
-                  />
+                  <Anchor key='contact-us' href='/about#contact' />
                 ]}
               />
             </Paragraph>
@@ -73,24 +74,24 @@ function FAQPage() {
               </Box>
               <Box as='li'>
                 <Question>{t('FAQ.item2.question')}</Question>
-                <Answer>
-                  <Trans
-                    i18nKey='FAQ.item2.answer'
-                    t={t}
-                    components={[
-                      <Anchor
-                        key='privacy-page'
-                        href='https://www.zooniverse.org/privacy'
-                      />
-                    ]}
-                  />
-                </Answer>
+                <Answer>{t('FAQ.item2.answer')}</Answer>
               </Box>
-              <Box as='li'>
+              {/** Okay to enable with launch of new homepage */}
+              {/* <Box as='li'>
                 <Question>{t('FAQ.item3.question')}</Question>
                 <Answer>
                   <Trans
                     i18nKey='FAQ.item3.answer'
+                    t={t}
+                    components={[<Anchor key='stats-blog-post' href='' />]}
+                  />
+                </Answer>
+              </Box> */}
+              <Box as='li'>
+                <Question>{t('FAQ.item4.question')}</Question>
+                <Answer>
+                  <Trans
+                    i18nKey='FAQ.item4.answer'
                     t={t}
                     components={[
                       <Anchor
@@ -106,10 +107,10 @@ function FAQPage() {
                 </Answer>
               </Box>
               <Box as='li'>
-                <Question>{t('FAQ.item4.question')}</Question>
+                <Question>{t('FAQ.item5.question')}</Question>
                 <Answer>
                   <Trans
-                    i18nKey='FAQ.item4.answer'
+                    i18nKey='FAQ.item5.answer'
                     t={t}
                     components={[
                       <Anchor
@@ -118,17 +119,17 @@ function FAQPage() {
                       />,
                       <Anchor
                         key='FEM-github-issues'
-                        href='https://github.com/zooniverse/front-end-monorepo/issues'
+                        href='https://github.com/zooniverse'
                       />
                     ]}
                   />
                 </Answer>
               </Box>
               <Box as='li'>
-                <Question>{t('FAQ.item5.question')}</Question>
+                <Question>{t('FAQ.item6.question')}</Question>
                 <Answer>
                   <Trans
-                    i18nKey='FAQ.item5.answer'
+                    i18nKey='FAQ.item6.answer'
                     t={t}
                     components={[
                       <Anchor
@@ -140,41 +141,46 @@ function FAQPage() {
                 </Answer>
               </Box>
               <Box as='li'>
-                <Question>{t('FAQ.item6.question')}</Question>
-                <Answer>{t('FAQ.item6.answer0')}</Answer>
-                <Paragraph margin='0'>{t('FAQ.item6.answer1')}</Paragraph>
-                <Answer>
+                <Question>{t('FAQ.item7.question')}</Question>
+                <Answer>{t('FAQ.item7.answer0')}</Answer>
+                <Paragraph margin='0'>
                   <Trans
-                    i18nKey='FAQ.item6.answer2'
+                    i18nKey='FAQ.item7.answer1'
                     t={t}
                     components={[
                       <Anchor
-                        key='donate-link'
-                        href='/get-involved/donate'
+                        key='503c'
+                        href='https://drive.google.com/file/d/108Ocx76oQLCJ8H_g9ZlF5wl4wa75mHyB/view'
                       />
                     ]}
                   />
-                </Answer>
-              </Box>
-              <Box as='li'>
-                <Question>{t('FAQ.item7.question')}</Question>
+                </Paragraph>
                 <Answer>
                   <Trans
-                    i18nKey='FAQ.item7.answer'
+                    i18nKey='FAQ.item7.answer2'
                     t={t}
                     components={[
-                      <Anchor
-                        key='resources-page'
-                        href='/about/resources'
-                      />
+                      <Anchor key='donate-link' href='/get-involved/donate' />
                     ]}
                   />
                 </Answer>
               </Box>
               <Box as='li'>
                 <Question>{t('FAQ.item8.question')}</Question>
+                <Answer>
+                  <Trans
+                    i18nKey='FAQ.item8.answer'
+                    t={t}
+                    components={[
+                      <Anchor key='resources-page' href='/about/resources' />
+                    ]}
+                  />
+                </Answer>
+              </Box>
+              <Box as='li'>
+                <Question>{t('FAQ.item9.question')}</Question>
                 <Paragraph margin={{ top: 'small', bottom: 'medium' }}>
-                  {t('FAQ.item8.answer')}
+                  {t('FAQ.item9.answer')}
                 </Paragraph>
               </Box>
             </StyledList>
