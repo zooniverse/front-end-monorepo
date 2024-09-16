7ce7131b2fd8a7441cb76b7b8a724fcb37ff0ad8
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2024-09-16 10:46:00 -0500

    app-project: Refactor Announcement banners styling and remove artsy/fresnel (#6291)
    * refactor GenericBanner and remove artsy/fresnel
    
    * center-align banner contents except the close button
    
    * minor adjustments
    
    * refactor ProjectAnnouncement unit test
    
    * fix unit test

--- packages/app-project/src/components/Announcements/components/ProjectAnnouncement/ProjectAnnouncementConnector.spec.js
+++ packages/app-project/src/components/Announcements/components/ProjectAnnouncement/ProjectAnnouncementConnector.spec.js
@@ -1,46 +1,12 @@
-import { shallow } from 'enzyme'
-import sinon from 'sinon'
-import { ProjectAnnouncementConnector } from './ProjectAnnouncementConnector'
-import GenericAnnouncement from '../GenericAnnouncement'
+import { render, screen } from '@testing-library/react'
+import { composeStory } from '@storybook/react'
+import Meta, { Default, mockStore } from './ProjectAnnouncement.stories.js'
 
 describe('Component > ProjectAnnouncementConnector', function () {
-  let wrapper
-  let componentWrapper
-  const announcement = 'Arcu scelerisque curae eu sapien euismod nisl, viverra gravida donec interdum tempor vulputate nec, nam morbi rhoncus porta sollicitudin.'
-
-  const mockStore = {
-    store: {
-      project: {
-        configuration: {
-          announcement
-        }
-      },
-      ui: {
-        dismissProjectAnnouncementBanner: sinon.spy(),
-        showAnnouncement: false
-      }
-    }
-  }
-
-  before(function () {
-    wrapper = shallow(<ProjectAnnouncementConnector mockStore={mockStore.store} />)
-    componentWrapper = wrapper.find(GenericAnnouncement)
-  })
-
-  it('should render without crashing', function () {
-    expect(wrapper).to.be.ok()
-  })
-
-  it('should render the `GenericAnnouncement` component if visible', function () {
-    expect(wrapper.html()).to.be.null()
-    expect(componentWrapper).to.have.lengthOf(0)
-    mockStore.store.ui.showAnnouncement = true
-    wrapper = shallow(<ProjectAnnouncementConnector mockStore={mockStore.store} />)
-    componentWrapper = wrapper.find(GenericAnnouncement)
-    expect(componentWrapper).to.have.lengthOf(1)
-  })
-
-  it('should pass down the required props', function () {
-    expect(componentWrapper.props().announcement).to.equal(announcement)
-  })
+	it('should render the ProjectAnnouncement component', function () {
+		const DefaultStory = composeStory(Default, Meta)
+		render(<DefaultStory />)
+		expect(screen.findByText(mockStore.project.configuration.announcement)).to.exist();
+		expect(screen.queryByRole('button', { name: 'Close' })).to.exist()
+	})
 })
--- packages/app-project/src/shared/components/Media/README.md
+++
@@ -1,3 +0,0 @@
-# Media
-
-Components created by [`@artsy/fresnel`](https://github.com/artsy/fresnel) for responsive SSR. Breakpoints are determined by those defined in our Grommet theme - note that since it counts _up_ from breakpoint values, `default` applies from `0px` to the start of the `small` breakpoint.
