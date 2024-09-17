ffaab1e09f52712fbc97dec3aa1b0b66a28f9afd
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2024-09-17 13:48:02 -0500

    app-project: Announcements markdown direction (#6308)
    * minor styling updates to paragraphs used in markdown of announcement banners
    
    * address styling for multiple paragraphs with Storybook

--- packages/app-project/src/components/Announcements/components/GenericAnnouncement/markdownzComponents.js
+++ packages/app-project/src/components/Announcements/components/GenericAnnouncement/markdownzComponents.js
@@ -4,10 +4,15 @@ import styled from 'styled-components'
 const StyledParagraph = styled(Paragraph)`
   font-weight: bold;
   line-height: 1.25;
+  margin: 0 0 0 0;
+
+  &:not(:first-child) {
+    margin: 10px 0 0 0;
+  }
 `
 function MarkdownParagraph(nodeProps) {
   return (
-    <StyledParagraph color='black' margin='0'>
+    <StyledParagraph color='black' margin='0' textAlign='center'>
       {nodeProps.children}
     </StyledParagraph>
   )
