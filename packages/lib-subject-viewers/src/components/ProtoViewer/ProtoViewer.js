855db24d636863920744cbce82aa8922e9122093
kieftrav <kiefertravis@gmail.com>
2024-09-27 12:39:35 -0500

    Add VolumetricViewer to lib-subject-viewers
    - Add "data-testid" to all DOM elements we want to test for existence
    - Add PropTypes to all Components based on linter output
    - Add shims for Canvas, WebGL, and requestAnimationFrame for Cube rendering and testing
    - Restructure VolumetricViewer directory to have all components, css, data, helpers, models, and tests in respective directories
    - Specs for AlgorithmAStar
    - Specs for ModelAnnotation
    - Specs for ModelTool
    - Specs for ModelViewer
    - Specs for pointColor
    - Specs for SortedSet
    - Specs for VolumetricViewer that simply tests for existence in the DOM
    - Write skeleton of a README

--- packages/lib-subject-viewers/src/components/ProtoViewer/ProtoViewer.js
+++ packages/lib-subject-viewers/src/components/ProtoViewer/ProtoViewer.js
@@ -1,4 +1,4 @@
-export default function Home () {
+export default function ProtoViewer () {
   return (
     <div>
       <h2>ProtoViewer</h2>
--- packages/lib-subject-viewers/src/components/ProtoViewer/ProtoViewer.spec.js
+++ packages/lib-subject-viewers/src/components/ProtoViewer/ProtoViewer.spec.js
@@ -2,16 +2,16 @@ import { render } from '@testing-library/react'
 import { composeStory } from '@storybook/react'
 import Meta, { Default } from './ProtoViewer.stories.js'
 
-describe('Component > AboutHeader', function () {
+describe('Component > ProtoViewer', () => {
   const DefaultStory = composeStory(Default, Meta)
 
-  before(function () {
+  before(() => {
     render(
       <DefaultStory />
     )
   })
 
-  it('should load without errors', function () {
+  it('should load without errors', () => {
     expect(document.querySelector('h2')).to.be.ok()
   })
 })
