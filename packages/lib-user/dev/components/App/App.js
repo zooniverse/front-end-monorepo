b97b746e0d6d78a7f326609d9c714819187786d7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-10-01 13:34:03 +0100

    Experimental Auth: fix tests

--- packages/lib-user/dev/components/App/App.js
+++ packages/lib-user/dev/components/App/App.js
@@ -58,16 +58,16 @@ function App({
       }
     }
 
-    async function experimentalEventHandler() {
-      console.log ('+++ experimentalEventHandler')
+    async function experimentalEventHandler (args) {
+      console.log ('+++ 🟢 ', args)
     }
 
     auth.listen('change', checkUserSession)
-    experimentalAuth.addEventListener('change', checkUserSession)
+    experimentalAuth.addEventListener('change', experimentalEventHandler)
 
     return function () {
       auth.stopListening('change', checkUserSession)
-      experimentalAuth.removeEventListener('change', checkUserSession)
+      experimentalAuth.removeEventListener('change', experimentalEventHandler)
     }
   }, [])
 
