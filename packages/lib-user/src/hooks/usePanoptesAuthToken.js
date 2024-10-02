cfeaa3cabe3210a57a603fc3e10b03687b8eaeda
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-10-02 16:31:16 +0100

    fix formatting

--- packages/lib-user/src/hooks/usePanoptesAuthToken.js
+++ packages/lib-user/src/hooks/usePanoptesAuthToken.js
@@ -1,23 +1,23 @@
-import auth from 'panoptes-client/lib/auth'
-import { useState } from 'react'
+import auth from "panoptes-client/lib/auth";
+import { useState } from "react";
 
-const isBrowser = typeof window !== 'undefined'
+const isBrowser = typeof window !== "undefined";
 
 if (isBrowser) {
-    auth.checkCurrent()
+  auth.checkCurrent();
 }
 
 export default function usePanoptesAuthToken() {
-    const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
 
-    async function fetchPanoptesAuthToken() {
-        await auth.checkCurrent()
-        const newToken = await auth.checkBearerToken()
-        if (newToken !== token) {
-            setToken(newToken)
-        }   
+  async function fetchPanoptesAuthToken() {
+    await auth.checkCurrent();
+    const newToken = await auth.checkBearerToken();
+    if (newToken !== token) {
+      setToken(newToken);
     }
+  }
 
-    fetchPanoptesAuthToken()
-    return token
+  fetchPanoptesAuthToken();
+  return token;
 }
