5f2535274b4912d447b4d0d981b88da4de527ef0
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-10-04 22:23:29 +0100

    fix(lib-react-components): thumbnail image sizing
    Remove the Grommet `fill` prop from thumbnail images, which should fix odd sizing problems in markdown images.

--- packages/lib-react-components/src/Media/components/ThumbnailImage/ThumbnailImage.js
+++ packages/lib-react-components/src/Media/components/ThumbnailImage/ThumbnailImage.js
@@ -63,7 +63,6 @@ export default function ThumbnailImage({
           <Image
             alt={alt}
             fit={fit}
-            fill
             src={imageSrc}
           />
         </Box>}
