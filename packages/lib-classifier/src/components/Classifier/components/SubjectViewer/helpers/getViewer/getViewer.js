7d4903570d79a3d7bc631ee4afc55a77a4229165
kieftrav <kiefertravis@gmail.com>
2024-10-09 14:51:02 -0500

    Import VV into Classifier
    Also makes fetching the viewer an async process to prevent the unnecessary preloading of all viewers. Fixed related tests.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewer/getViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/helpers/getViewer/getViewer.js
@@ -1,30 +1,37 @@
-import DataImageViewer from '../../components/DataImageViewer'
-import FlipbookViewer from '../../components/FlipbookViewer'
-import JSONDataViewer from '../../components/JSONDataViewer'
-import ImageAndTextViewer from '../../components/ImageAndTextViewer'
-import MultiFrameViewer from '../../components/MultiFrameViewer'
-import SingleImageViewer from '../../components/SingleImageViewer'
-import SingleTextViewer from '../../components/SingleTextViewer'
-import SingleVideoViewer from '../../components/SingleVideoViewer'
-import SubjectGroupViewer from '../../components/SubjectGroupViewer'
+async function getViewer (viewer) {
+  let view = null;
 
-const viewers = {
-  dataImage: DataImageViewer,
-  flipbook: FlipbookViewer,
-  jsonData: JSONDataViewer,
-  imageAndText: ImageAndTextViewer,
-  lightCurve: JSONDataViewer,
-  multiFrame: MultiFrameViewer,
-  scatterPlot: JSONDataViewer,
-  singleImage: SingleImageViewer,
-  singleText: SingleTextViewer,
-  singleVideo: SingleVideoViewer,
-  subjectGroup: SubjectGroupViewer,
-  variableStar: JSONDataViewer
-}
+  if (viewer === 'dataImage') {
+    view = await import('./../../components/DataImageViewer');
+  } else if (viewer === 'flipbook') {
+    view = await import('./../../components/FlipbookViewer');
+  } else if (viewer === 'jsonData') {
+    view = await import('./../../components/JSONDataViewer');
+  } else if (viewer === 'imageAndText') {
+    view = await import('./../../components/ImageAndTextViewer');
+  } else if (viewer === 'lightCurve') {
+    view = await import('./../../components/JSONDataViewer');
+  } else if (viewer ==='multiFrame') {
+    view = await import('./../../components/MultiFrameViewer');
+  } else if (viewer ==='scatterPlot') {
+    view = await import('./../../components/JSONDataViewer');
+  } else if (viewer === 'singleImage') {
+    view = await import('./../../components/SingleImageViewer');
+  } else if (viewer ==='singleText') {
+    view = await import('./../../components/SingleTextViewer');
+  } else if (viewer === 'singleVideo') {
+    view = await import('./../../components/SingleVideoViewer');
+  } else if (viewer ==='subjectGroup') {
+    view = await import('./../../components/SubjectGroupViewer');
+  } else if (viewer === 'variableStar') {	
+    view = await import('./../../components/JSONDataViewer');
+  } else if (viewer === 'volumetric') {
+    view = await import('@zooniverse/subject-viewers');
+  };
 
-function getViewer (viewer) {
-  return viewers[viewer] || null
+  if (view && view.default) view = view.default;
+
+  return view;
 }
 
 export default getViewer
