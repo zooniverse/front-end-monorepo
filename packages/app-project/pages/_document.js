449acace217779a57b204eddc7e5adc2d9593115
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2024-10-18 14:03:47 -0500

    Migrate Sentry packages to v8 (#6387)
    * build(deps): bump @sentry/browser from 7.109.0 to 8.34.0
    
    Bumps [@sentry/browser](https://github.com/getsentry/sentry-javascript) from 7.109.0 to 8.34.0.
    - [Release notes](https://github.com/getsentry/sentry-javascript/releases)
    - [Changelog](https://github.com/getsentry/sentry-javascript/blob/8.34.0/CHANGELOG.md)
    - [Commits](https://github.com/getsentry/sentry-javascript/compare/7.109.0...8.34.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@sentry/browser"
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * migrate @sentry/nextjs to v8
    
    * configure SENTRY_AUTH_TOKEN
    
    * replace missing tilda
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- packages/app-project/pages/_document.js
+++ packages/app-project/pages/_document.js
@@ -8,37 +8,6 @@ const GA_TRACKING_ID = 'GTM-WDW6V4'
 const GA_TRACKING_SCRIPT = `
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');
 `
-
-const ERROR_LOGGING_SCRIPT = `
-  function setUpLogging() {
-    Sentry.init({
-      dsn: '${process.env.SENTRY_PROJECT_DSN}',
-      environment: '${process.env.APP_ENV}',
-      release: '${process.env.COMMIT_ID}',
-      integrations: [new Sentry.BrowserTracing()],
-      tracesSampleRate: 1.0
-    });
-    console.log('Sentry init: ${process.env.SENTRY_PROJECT_DSN} ${process.env.APP_ENV}');
-  }
-
-  function onError(e) {
-    const error = new Error('External script failed to load');
-    Sentry.withScope((scope) => {
-      scope.setTag('ScriptError', 'loadFailed');
-      scope.setExtra('scriptSrc', e.srcElement.src);
-      Sentry.captureException(error);
-    });
-    console.log('errored', e.srcElement.src);
-  };
-
-  const scripts = document.querySelectorAll('script');
-  scripts.forEach(scriptNode => {
-    scriptNode.onload = e => console.log('loaded', e.srcElement.src);
-    scriptNode.onerror = onError;
-  });
-  const sentryScript = document.querySelector('#sentryScript');
-  sentryScript.onload = setUpLogging;
-`
 const isProduction = process.env.NODE_ENV === 'production'
 
 process.on('unhandledRejection', logToSentry)
@@ -77,14 +46,6 @@ export default class MyDocument extends Document {
           {isProduction && (
             <script dangerouslySetInnerHTML={{ __html: GA_TRACKING_SCRIPT }} />
           )}
-          {/* https://docs.sentry.io/platforms/javascript/install/loader/#default-bundle */}
-          <script
-            src="https://browser.sentry-cdn.com/7.109.0/bundle.tracing.min.js"
-            integrity="sha384-FyHLiOgn1wcBUetfKq3+RF+aukePV5acbpdkgdYJRWepBQ8AMoNNEucU/6+JYRuJ"
-            crossOrigin='anonymous'
-            defer
-            id='sentryScript'
-          ></script>
         </Head>
         <body>
           {isProduction && (
@@ -100,7 +61,6 @@ export default class MyDocument extends Document {
           )}
           <Main />
           <NextScript />
-          <script dangerouslySetInnerHTML={{ __html: ERROR_LOGGING_SCRIPT }} />
         </body>
       </Html>
     )
