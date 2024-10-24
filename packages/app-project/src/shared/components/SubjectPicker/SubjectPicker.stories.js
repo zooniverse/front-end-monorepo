6562fcc0ba8c7a031bb8580f4627ad7599df1840
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-10-24 18:03:44 +0100

    Subject Picker stories

--- packages/app-project/src/shared/components/SubjectPicker/SubjectPicker.stories.js
+++ packages/app-project/src/shared/components/SubjectPicker/SubjectPicker.stories.js
@@ -1,5 +1,6 @@
 import { Box } from 'grommet'
 import SubjectPicker from './SubjectPicker'
+import { http, HttpResponse } from 'msw'
 
 function DecoratedStory(Story) {
   return (
@@ -12,7 +13,48 @@ function DecoratedStory(Story) {
 export default {
   title: 'Project App / Shared / Subject Picker',
   component: SubjectPicker,
-  decorators: [DecoratedStory]
+  decorators: [DecoratedStory],
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://subject-set-search-api.zooniverse.org/subjects/15582.json', ({ request }) => {
+          const columns = [
+            'subject_id',
+            'Page',
+            'Date'
+          ]
+          const rows = [
+            [1, '43', '23 January 1916'],
+            [2, '44', '24 January 1916'],
+            [3, '45', '25 January 1916']
+          ]
+          
+          return HttpResponse.json({
+            columns,
+            rows
+          })
+        }),
+        http.get('https://panoptes-staging.zooniverse.org/api/subjects/selection', ({ request }) => {
+          return HttpResponse.json({
+            subjects: [
+              { id: 1, already_seen: false, retired: false },
+              { id: 2, already_seen: true, retired: false },
+              { id: 3, already_seen: true, retired: true }
+            ]
+          })
+        }),
+        http.get('https://www.zooniverse.org/api/subjects/selection', ({ request }) => {
+          return HttpResponse.json({
+            subjects: [
+              { id: 1, already_seen: false, retired: false },
+              { id: 2, already_seen: true, retired: false },
+              { id: 3, already_seen: true, retired: true }
+            ]
+          })
+        }),
+      ],
+    },
+  }
 }
 
 export function Default(args) {
@@ -25,7 +67,7 @@ Default.args = {
   closeFn: e => true,
   subjectSet: {
     id: '15582',
-    title: 'Anti-Slavery Letters: 1800-1839',
+    display_name: 'Anti-Slavery Letters: 1800-1839',
     metadata: {
       indexFields: 'date,title,creators'
     }
@@ -47,7 +89,7 @@ Tablet.args = {
   closeFn: e => true,
   subjectSet: {
     id: '15582',
-    title: 'Anti-Slavery Letters: 1800-1839',
+    display_name: 'Anti-Slavery Letters: 1800-1839',
     metadata: {
       indexFields: 'date,title,creators'
     }
