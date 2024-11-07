9cf65e459a42da92a5663c6e3bef4c182bf0c2b5
Mark Bouslog <mark@zooniverse.org>
2024-11-07 13:31:56 -0600

    Refactor MultiFrameViewer with SingleImageViewer

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/MultiFrameViewer/MultiFrameViewerContainer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/MultiFrameViewer/MultiFrameViewerContainer.js
@@ -1,26 +1,19 @@
 import asyncStates from '@zooniverse/async-states'
 import { Box } from 'grommet'
 import PropTypes from 'prop-types'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { withStores } from '@helpers'
-import { useSubjectImage } from '@hooks'
 
 import locationValidator from '../../helpers/locationValidator'
-import SingleImageViewer from '../SingleImageViewer/SingleImageViewer'
+import SingleImageViewer from '../SingleImageViewer'
 import FrameCarousel from './FrameCarousel'
 
 function storeMapper(store) {
   const {
-    enableRotation,
     frame,
-    invert,
-    move,
     resetView,
-    rotation,
-    setFrame,
-    setOnPan,
-    setOnZoom
+    setFrame
   } = store.subjectViewer
 
   const { activeStepTasks } = store.workflowSteps
@@ -32,64 +25,31 @@ function storeMapper(store) {
     activeTool
   } = activeInteractionTask || {}
 
-  const {
-    limit_subject_height: limitSubjectHeight
-  } = store.workflows?.active?.configuration
-
   return {
     activeTool,
-    enableRotation,
     frame,
-    invert,
-    limitSubjectHeight,
-    move,
     resetView,
-    rotation,
-    setFrame,
-    setOnPan,
-    setOnZoom
+    setFrame
   }
 }
 
-const defaultTool = {
+const DEFAULT_HANDLER = () => true
+
+const DEFAULT_TOOL = {
   validate: () => {}
 }
 
 function MultiFrameViewerContainer({
-  activeTool = defaultTool,
+  activeTool = DEFAULT_TOOL,
   enableInteractionLayer = false,
-  enableRotation = () => null,
   frame = 0,
-  invert = false,
-  limitSubjectHeight = false,
   loadingState = asyncStates.initialized,
-  move,
-  onError = () => true,
-  onReady = () => true,
-  resetView,
-  rotation,
-  setFrame = () => true,
-  setOnPan = () => true,
-  setOnZoom = () => true,
+  onError = DEFAULT_HANDLER,
+  onReady = DEFAULT_HANDLER,
+  resetView = DEFAULT_HANDLER,
+  setFrame = DEFAULT_HANDLER,
   subject
 }) {
-  // TODO: replace this with a better function to parse the image location from a subject.
-  const imageLocation = subject ? subject.locations[frame] : null
-  const { img, error, loading, subjectImage } = useSubjectImage({
-    frame,
-    src: imageLocation?.url,
-    onReady,
-    onError
-  })
-  const {
-    naturalHeight = 600,
-    naturalWidth = 800
-  } = img
-
-  useEffect(function onMount() {
-    enableRotation()
-  }, [])
-
   useEffect(function onFrameChange() {
     activeTool?.validate()
     resetView()
@@ -102,7 +62,6 @@ function MultiFrameViewerContainer({
   }
 
   if (loadingState !== asyncStates.initialized) {
-    const subjectID = subject?.id || 'unknown'
     return (
       <Box
         direction='row'
@@ -114,21 +73,9 @@ function MultiFrameViewerContainer({
           locations={subject.locations}
         />
         <SingleImageViewer
-          enableInteractionLayer={loadingState === asyncStates.success && enableInteractionLayer}
-          frame={frame}
-          imgRef={subjectImage}
-          invert={invert}
-          limitSubjectHeight={limitSubjectHeight}
-          move={move}
-          naturalHeight={naturalHeight}
-          naturalWidth={naturalWidth}
-          rotation={rotation}
-          setOnPan={setOnPan}
-          setOnZoom={setOnZoom}
-          src={img.src}
-          subject={subject}
-          subjectId={subjectID}
-          title={{ id: subjectID, text: subjectID }}
+          enableInteractionLayer={enableInteractionLayer}
+          onError={onError}
+          onReady={onReady}
         />
       </Box>
     )
@@ -141,16 +88,12 @@ MultiFrameViewerContainer.propTypes = {
     validate: PropTypes.func
   }),
   enableInteractionLayer: PropTypes.bool,
-  enableRotation: PropTypes.func,
   frame: PropTypes.number,
-  invert: PropTypes.bool,
-  limitSubjectHeight: PropTypes.bool,
   loadingState: PropTypes.string,
   onError: PropTypes.func,
   onReady: PropTypes.func,
+  resetView: PropTypes.func,
   setFrame: PropTypes.func,
-  setOnPan: PropTypes.func,
-  setOnZoom: PropTypes.func,
   subject: PropTypes.shape({
     locations: PropTypes.arrayOf(locationValidator)
   }).isRequired
