39f75e447cb3a1ca8f9289285d92d0a3c8659d24
Mark Bouslog <mark@zooniverse.org>
2024-11-13 18:43:04 -0600

    Fix tests, cleanup

--- packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/Layout/components/MaxWidth/MaxWidth.spec.js
@@ -1,13 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { composeStory } from '@storybook/react'
+
 import Meta, { Default, mockTasks } from './MaxWidth.stories.js'
 
 describe('Component > Layouts > MaxWidth', function () {
 
-  it('should render a subject and a task', function () {
+  it('should render a subject and a task', async function () {
     const DefaultStory = composeStory(Default, Meta)
     render(<DefaultStory />)
-    expect(screen.getByLabelText('Subject 1')).exists() // img aria-label from SVGImage
-    expect(screen.getByText(mockTasks.init.strings.question)).exists() // task question paragraph
+
+    // Mock the loading state transition
+    Default.store.subjectViewer.onSubjectReady()
+
+    await waitFor(() => expect(screen.getByLabelText('Subject 1')).exists()) // img aria-label from SVGImage
+    await waitFor(() => expect(screen.getByText(mockTasks.init.strings.question)).exists()) // task question paragraph
   })
 })
