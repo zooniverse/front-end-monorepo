9378b1b0c89cb408cdba5be95410ff05045de572
kieftrav <kiefertravis@gmail.com>
2024-11-21 07:31:46 -0600

    Reimplement rendering viewer if the subject store is ready.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.spec.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/SubjectViewer.spec.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import asyncStates from '@zooniverse/async-states'
-import { SubjectViewer } from './SubjectViewer'
+import { Factory } from 'rosie'
+import mockStore from '@test/mockStore'
+import { Provider } from 'mobx-react'
+import SubjectType from '@store/SubjectStore/SubjectType'
+import { default as SubjectViewerWithStore, SubjectViewer } from './SubjectViewer'
 
 describe('Component > SubjectViewer', function () {
   it('should render without crashing', function () {
@@ -21,6 +25,18 @@ describe('Component > SubjectViewer', function () {
   it('should render nothing if the subject store errors', function () {
     const { container } = render(<SubjectViewer subjectQueueState={asyncStates.error} />)
     expect(container.firstChild).to.be.null()
+  })
+
+  it('should render a subject viewer if the subject store successfully loads', async function () {
+    const store = mockStore({
+      subject: SubjectType.create(Factory.build('subject', { id: '1234' }))
+    })
+    
+    render(<Provider classifierStore={store}>
+      <SubjectViewerWithStore />
+    </Provider>)
+
+    expect(screen.getByLabelText('Subject 1234')).to.exist()
   })
 
   it('should render the VolumetricViewer if isVolumetricViewer = true', async function () {
