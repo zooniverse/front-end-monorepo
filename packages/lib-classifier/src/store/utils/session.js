d550b58588f094fb84ed976f85f4c0271370137f
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-11-27 12:23:24 -0600

    Remove the faulty session id code

--- packages/lib-classifier/src/store/utils/session.js
+++ packages/lib-classifier/src/store/utils/session.js
@@ -1,19 +1,12 @@
 import hash from 'hash.js'
 
-const storage = window.sessionStorage || window.localStorage
+const storage = window.sessionStorage
 
 const sessionUtils = {
-  fiveMinutesFromNow () {
-    const d = new Date()
-    d.setMinutes(d.getMinutes() + 5)
-    return d
-  },
-
   generateSessionID () {
     const sha2 = hash.sha256()
     const id = sha2.update(`${Math.random() * 10000}${Date.now()}${Math.random() * 1000}`).digest('hex')
-    const ttl = this.fiveMinutesFromNow()
-    const stored = { id, ttl }
+    const stored = { id }
     try {
       storage.setItem('session_id', JSON.stringify(stored))
     } catch (e) {
@@ -24,18 +17,7 @@ const sessionUtils = {
 
   getSessionID () {
     const stored = (storage.getItem('session_id')) ? JSON.parse(storage.getItem('session_id')) : this.generateSessionID()
-    let { id, ttl } = stored
-
-    if (ttl < Date.now()) {
-      id = this.generateSessionID()
-    } else {
-      ttl = this.fiveMinutesFromNow()
-    }
-    try {
-      storage.setItem('session_id', JSON.stringify({ id, ttl }))
-    } catch (e) {
-      console.error(e)
-    }
+    const { id } = stored
     return id
   }
 }
