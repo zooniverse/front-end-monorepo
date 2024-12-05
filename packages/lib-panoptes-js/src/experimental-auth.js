f94b11822053df4a753d2aeda1125f25bebd4c43
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-12-05 22:30:39 +0000

    signIn: add error handling

--- packages/lib-panoptes-js/src/experimental-auth.js
+++ packages/lib-panoptes-js/src/experimental-auth.js
@@ -128,86 +128,107 @@ async function signIn (login, password, _store) {
   // const login = 'janezooniverse'
   // const password = 'bleepbloop'
 
-  // Step 1: get a CSRF token.
-  // - The CSRF token (or rather, the anti-cross-site request forgery token) is a
-  //   unique, one-off, time-sensitive token. Kinda like the time-based OTPs
-  //   provided by apps like the Google Authenticator. 
-  // - This "authenticity token", as it will be later be called, prevents third
-  //   parties from simply replaying the HTTPs-encoded sign-in request.
-  // - In our case, the CSRF token is provided Panoptes itself.
-  const request1 = new Request(`https://panoptes-staging.zooniverse.org/users/sign_in/?now=${Date.now()}`, {
-    credentials: 'include',
-    method: 'GET',
-    headers: PANOPTES_HEADERS,
-  })
-  const response1 = await fetch(request1)
-  const csrfToken = response1?.headers.get('x-csrf-token')  // The CSRF Token is in the response header
-  console.log('+++ Step 1: csrfToken received: ', csrfToken)
-  // Note: we don't actually care about the response body, which happens to be blank.
+  try {
 
-  // Step 2: submit the login details.
-  // - IMPORTANT: at this point, Panoptes should be attaching (HTTP-only)
-  //   cookies (_Panoptes_session and remember_user_token) to its responses.
-  // - These HTTP cookies identify requests as coming from us (or rather, from
-  //   our particular session.
-  // - This is how request2 (submit username & password) and request3 (request
-  //   bearer token for a logged in user) are magically linked and recognised
-  //   as coming from the same person/session, even though request3 isn't
-  //   providing any login data explicitly via the JavaScript code.
-  // - HTTP-only cookies can't be viewed or edited by JavaScript, as it happens.
-  // - HTTP-only cookies are automagically handled by the web browser and the
-  //   server.
-  // - (If you're curious about the mechanics, check the first response headers'
-  //   "Set-Cookie" values, and the following second request header's "Cookie"
-  //   values.)
-  // - Our only control as front-end devs is to specify the fetch() of Request()'s
-  //   `credentials` option to either "omit" (bad idea for us), "same-origin"
-  //   (the default), or "include" (when you need things to work cross-origin)
-  // - ❗️ That `credentials: "include"` option is probably important if we need
-  //   Panoptes.JS to work on non-*.zooniverse.org domains!
+    // Step 1: get a CSRF token.
+    // - The CSRF token (or rather, the anti-cross-site request forgery token) is a
+    //   unique, one-off, time-sensitive token. Kinda like the time-based OTPs
+    //   provided by apps like the Google Authenticator. 
+    // - This "authenticity token", as it will be later be called, prevents third
+    //   parties from simply replaying the HTTPs-encoded sign-in request.
+    // - In our case, the CSRF token is provided Panoptes itself.
+    const request1 = new Request(`https://panoptes-staging.zooniverse.org/users/sign_in/?now=${Date.now()}`, {
+      credentials: 'include',
+      method: 'GET',
+      headers: PANOPTES_HEADERS,
+    })
+    const response1 = await fetch(request1)
+    const csrfToken = response1?.headers.get('x-csrf-token')  // The CSRF Token is in the response header
+    console.log('+++ Step 1: csrfToken received: ', csrfToken, response1)
+    // Note: we don't actually care about the response body, which happens to be blank.
 
-  const request2 = new Request(`https://panoptes-staging.zooniverse.org/users/sign_in`, {
-    body: JSON.stringify({
-      authenticity_token: csrfToken,
-      user: {
-        login,
-        password,
-        remember_me: true,
-      },
-    }),
-    credentials: 'include',
-    method: 'POST',
-    headers: PANOPTES_HEADERS,
-  })
-  const response2 = await fetch(request2)
-  console.log('+++ Step 2: login status: ', response2)
+    // Step 2: submit the login details.
+    // - IMPORTANT: at this point, Panoptes should be attaching (HTTP-only)
+    //   cookies (_Panoptes_session and remember_user_token) to its responses.
+    // - These HTTP cookies identify requests as coming from us (or rather, from
+    //   our particular session.
+    // - This is how request2 (submit username & password) and request3 (request
+    //   bearer token for a logged in user) are magically linked and recognised
+    //   as coming from the same person/session, even though request3 isn't
+    //   providing any login data explicitly via the JavaScript code.
+    // - HTTP-only cookies can't be viewed or edited by JavaScript, as it happens.
+    // - HTTP-only cookies are automagically handled by the web browser and the
+    //   server.
+    // - (If you're curious about the mechanics, check the first response headers'
+    //   "Set-Cookie" values, and the following second request header's "Cookie"
+    //   values.)
+    // - Our only control as front-end devs is to specify the fetch() of Request()'s
+    //   `credentials` option to either "omit" (bad idea for us), "same-origin"
+    //   (the default), or "include" (when you need things to work cross-origin)
+    // - ❗️ That `credentials: "include"` option is probably important if we need
+    //   Panoptes.JS to work on non-*.zooniverse.org domains!
 
-  // Possible responses from /users/sign_in:
-  // 1. valid login & password => 200 with User resource 
-  // 2. blank login / password => 401 with { error: "You need to sign in or sign up before continuing." }
-  // 3. invalid login / password => 401 with { error: "Invalid email or password." }
-  // 4. invalid authenticity token => 
-  // 5. already logged in => 200 with User resource of the logged-in user(!) This means if Panoptes things you're already logged in (see notes on http-only cookies), then any subsequent login attempts are ignored!
-  // X. Unexpected error, e.g. network down
+    const request2 = new Request(`https://panoptes-staging.zooniverse.org/users/sign_in`, {
+      body: JSON.stringify({
+        authenticity_token: csrfToken,
+        user: {
+          login,
+          password,
+          remember_me: true,
+        },
+      }),
+      credentials: 'include',
+      method: 'POST',
+      headers: PANOPTES_HEADERS,
+    })
+    const response2 = await fetch(request2)
+    console.log('+++ Step 2: login status: ', response2)
 
-  // Note: old PJC doesn't actually care about the response body, which is the
-  // logged-in user's User resource. This is because PJC has a separate call for
-  // fetching the user resource, which is, uh, kinda extra work, but keeps the
-  // calls standardised I guess?
+    // Sanity checkpoint for Step 2
+    // NOTE: this is way more thorough than necessary.
+    if (!response2.ok) {
+      const jsonData = await response2.json()
+      const error = jsonData?.error || 'No idea what went wrong; no specific error message detected.'
+      throw new Error(`Error from API. ${error}`)
+    }
+    const jsonData = await response2.json()
+    const userData = jsonData?.users?.[0]
+    if (!userData) {
+      throw new Error('Impossible API response. No user returned.')
+    } else if (userData.login && userData.login !== login) {
+      throw new Error('Impossible API response. User returned is different from login attempt. Did you forget to sign out first?')
+    }
 
-  // Step 3: get the bearer token.
+    // Possible responses from /users/sign_in:
+    // 1. valid login & password => 200 with User resource 
+    // 2. blank login / password => 401 with { error: "You need to sign in or sign up before continuing." }
+    // 3. invalid login / password => 401 with { error: "Invalid email or password." }
+    // 4. invalid authenticity token => see https://github.com/zooniverse/operations/issues/561
+    // 5. already logged in => 200 with User resource of the logged-in user(!) This means if Panoptes things you're already logged in (see notes on http-only cookies), then any subsequent login attempts are ignored.
+    // X. Unexpected error, e.g. network down.
 
-  const request3 = new Request(`https://panoptes-staging.zooniverse.org/oauth/token`, {
-    body: JSON.stringify({
-      client_id: '535759b966935c297be11913acee7a9ca17c025f9f15520e7504728e71110a27',
-      grant_type: 'password',
-    }),
-    credentials: 'include',
-    method: 'POST',
-    headers: PANOPTES_HEADERS,
-  })
-  const response3 = await fetch(request3)
-  console.log('+++ Step 3: ', response3)
+    // Note: old PJC doesn't actually care about the response body, which is the
+    // logged-in user's User resource. This is because PJC has a separate call for
+    // fetching the user resource, which is, uh, kinda extra work, but keeps the
+    // calls standardised I guess?
+
+    // Step 3: get the bearer token.
+
+    const request3 = new Request(`https://panoptes-staging.zooniverse.org/oauth/token`, {
+      body: JSON.stringify({
+        client_id: '535759b966935c297be11913acee7a9ca17c025f9f15520e7504728e71110a27',
+        grant_type: 'password',
+      }),
+      credentials: 'include',
+      method: 'POST',
+      headers: PANOPTES_HEADERS,
+    })
+    const response3 = await fetch(request3)
+    console.log('+++ Step 3: ', response3)
+
+  } catch (err) {
+    console.error('+++ ERROR: signIn() ', err)
+  }
 
   // TODO
 
