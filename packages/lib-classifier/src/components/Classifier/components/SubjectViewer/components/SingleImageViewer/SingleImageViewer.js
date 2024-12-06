471347d648fdf4c4c3effa14cd74e3156dcff150
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-12-06 13:00:09 +0000

    revert changes to SingleImageViewer

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleImageViewer/SingleImageViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleImageViewer/SingleImageViewer.js
@@ -34,10 +34,12 @@ function SingleImageViewer({
   zoomControlFn = null,
   zooming = false
 }) {
+  const canvasLayer = useRef()
+  const canvas = canvasLayer.current
   const transform = `rotate(${rotate} ${width / 2} ${height / 2})`
 
   return (
-    <SVGContext.Provider value={{ viewBox, rotate, width, height }}>
+    <SVGContext.Provider value={{ canvas, viewBox, rotate, width, height }}>
       {zoomControlFn && (
         <ZoomControlButton
           onClick={zoomControlFn}
@@ -66,6 +68,7 @@ function SingleImageViewer({
             transform={transform}
           >
             <SVGImageCanvas
+              ref={canvasLayer}
               viewBox={viewBox}
             >
               {children}
