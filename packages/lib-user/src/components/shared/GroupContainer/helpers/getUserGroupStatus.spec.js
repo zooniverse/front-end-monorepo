c0a0d9742bc3e970fd956b569ae0eb2b54d51b12
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2024-12-14 21:17:49 -0600

    Setup react-i18next for BarChart and GroupContainer

--- packages/lib-user/src/components/shared/GroupContainer/helpers/getUserGroupStatus.spec.js
+++ packages/lib-user/src/components/shared/GroupContainer/helpers/getUserGroupStatus.spec.js
@@ -5,17 +5,17 @@ import { getUserGroupStatus } from './getUserGroupStatus'
 describe('components > shared > GroupContainer > getUserGroupStatus', function () {
   it('should return a message to log in if there is a join token and no auth user', function () {
     const result = getUserGroupStatus({ joinToken: 'token' })
-    expect(result).to.equal('Log in to join the group.')
+    expect(result).to.equal('GroupContainer.loginToJoin')
   })
 
   it('should return a message when joining a group', function () {
     const result = getUserGroupStatus({ createGroupMembershipLoading: true })
-    expect(result).to.equal('Joining group...')
+    expect(result).to.equal('GroupContainer.joining')
   })
 
   it('should return a message when joining a group fails', function () {
     const result = getUserGroupStatus({ createGroupMembershipError: { message: 'error message' } })
-    expect(result).to.equal('Join failed.')
+    expect(result).to.equal('GroupContainer.joinFail')
   })
 
   it('should return a message when loading the group', function () {
@@ -27,17 +27,17 @@ describe('components > shared > GroupContainer > getUserGroupStatus', function (
 
   it('should return a message when there is a group error', function () {
     const result = getUserGroupStatus({ groupError: { message: 'error message' } })
-    expect(result).to.equal('Error: error message.')
+    expect(result).to.equal('error message')
   })
 
   it('should return a message when there is no group and no auth user', function () {
     const result = getUserGroupStatus({})
-    expect(result).to.equal('Group not found. You must be logged in to access a private group.')
+    expect(result).to.equal('GroupContainer.noAuth')
   })
 
   it('should return a message when there is no group and there is an auth user', function () {
     const result = getUserGroupStatus({ authUserId: '1' })
-    expect(result).to.equal('Group not found.')
+    expect(result).to.equal('GroupContainer.notFound')
   })
 
   it('should return null when there is a group and an auth user', function () {
