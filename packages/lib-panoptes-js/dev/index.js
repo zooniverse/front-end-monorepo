e93760f75f872596cbcf526565d8d0dc76585c1e
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-12-19 23:41:45 +0000

    ExperimentalAuth: wire up signOut() to dev server

--- packages/lib-panoptes-js/dev/index.js
+++ packages/lib-panoptes-js/dev/index.js
@@ -2,6 +2,7 @@ import {
   checkBearerToken,
   checkCurrentUser,
   signIn,
+  signOut,
   addEventListener,
 } from '@src/experimental-auth.js'
 
@@ -9,11 +10,13 @@ class App {
   constructor () {
     this.html = {
       checkCurrentUserButton: document.getElementById('check-current-user-button'),
+      signOutButton: document.getElementById('sign-out-button'),
       loginForm: document.getElementById('login-form'),
       message: document.getElementById('message'),
     }
 
     this.html.checkCurrentUserButton.addEventListener('click', this.checkCurrentUserButton_onClick.bind(this))
+    this.html.signOutButton.addEventListener('click', this.signOutButton_onClick.bind(this))
     this.html.loginForm.addEventListener('submit', this.loginForm_onSubmit.bind(this))
     addEventListener('change', this.onAuthChange)
   }
@@ -22,13 +25,28 @@ class App {
     try {
       const user = await checkCurrentUser()
       if (user) {
-        this.html.message.innerHTML += `> Current user: ${user.login}\n`
+        this.html.message.innerHTML += `> Current user: ${user.login} \n`
       } else {
         this.html.message.innerHTML += `> Current user: [nobody] \n`
       }
     } catch (err) {
       console.error(err)
-      this.html.message.innerHTML += `> [ERROR] ${err.toString()}\n`
+      this.html.message.innerHTML += `> [ERROR] ${err.toString()} \n`
+    }
+    return false
+  }
+
+  async signOutButton_onClick (e) {
+    try {
+      const success = await signOut()
+      if (success) {
+        this.html.message.innerHTML += `> Successfully signed out \n`
+      } else {
+        this.html.message.innerHTML += `> There's no user to sign out \n`
+      }
+    } catch (err) {
+      console.error(err)
+      this.html.message.innerHTML += `> [ERROR] ${err.toString()} \n`
     }
     return false
   }
@@ -41,13 +59,13 @@ class App {
       // Note: await is necessary to catch sign in errors.
       const user = await signIn(formData.get('login'), formData.get('password'))
       if (user) {
-        this.html.message.innerHTML += `> Logged in as ${user.login}\n`
+        this.html.message.innerHTML += `> Logged in as ${user.login} \n`
       } else {
         throw new Error('No user?')
       }
     } catch (err) {
       console.error(err)
-      this.html.message.innerHTML += `> [ERROR] ${err.toString()}\n`
+      this.html.message.innerHTML += `> [ERROR] ${err.toString()} \n`
     }
     return false
   }
