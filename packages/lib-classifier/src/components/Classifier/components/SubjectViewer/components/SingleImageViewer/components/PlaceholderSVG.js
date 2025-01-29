63a1876d68e60c896ee4d26c1ca9e58b9c0bbcb8
Mark Bouslog <mark@zooniverse.org>
2025-01-29 10:00:55 -0600

    Remove PlaceholderSVG xmlns, refactor maxHeight and maxWidth properties

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleImageViewer/components/PlaceholderSVG.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/SingleImageViewer/components/PlaceholderSVG.js
@@ -4,8 +4,8 @@ import styled, { css } from 'styled-components'
 const StyledPlaceholderSVG = styled.svg`
   background: no-repeat center / cover url('https://static.zooniverse.org/www.zooniverse.org/assets/fe-project-subject-placeholder-800x600.png');
   touch-action: pinch-zoom;
-  max-width: ${props => props.maxWidth};
-  ${props => props.maxHeight && css`max-height: ${props.maxHeight};`}
+  max-width: ${props => props.$maxWidth};
+  ${props => props.$maxHeight && css`max-height: ${props.$maxHeight};`}
 `
 
 function PlaceholderSVG({
@@ -17,11 +17,10 @@ function PlaceholderSVG({
     <StyledPlaceholderSVG
       data-testid='placeholder-svg'
       focusable
-      maxHeight={maxHeight}
-      maxWidth={maxWidth}
+      $maxHeight={maxHeight}
+      $maxWidth={maxWidth}
       tabIndex={0}
       viewBox={viewBox}
-      xmlns='http://www.w3.org/2000/svg'
     />
   )
 }
