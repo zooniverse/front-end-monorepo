031f71838e1f351b0b33e303299be22a9db079da
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-02-13 16:58:22 +0000

    refactor(drawing-tools): drag handle sizing (#6685)
    * refactor(drawing-tools): drag handle sizing
    
    - Remove `defaultProps` from functional components.
    - Refactor transcription lines to use translation and scale transforms for the line handles.
    - Style drag handles and point marks with `non-scaling-size`.
    - Fixes a small issue where drag handles aren't rendered in the correct proportion with respect to marks.
    
    * Ignore CI errors when coveralls is down

--- packages/lib-classifier/src/plugins/drawingTools/components/Point/Point.js
+++ packages/lib-classifier/src/plugins/drawingTools/components/Point/Point.js
@@ -14,22 +14,27 @@ const SELECTED_RADIUS = {
 const CROSSHAIR_SPACE = 0.2
 const CROSSHAIR_WIDTH = 1
 
-function Point({ active, children, mark, onFinish, scale }) {
+const DEFAULT_MARK = {
+  tool: {
+    size: 'large'
+  }
+}
+function Point({ active = false, children, mark = {DEFAULT_MARK}, onFinish, scale = 1 }) {
   const { size } = mark.tool
   const crosshairSpace = CROSSHAIR_SPACE
   const crosshairWidth = CROSSHAIR_WIDTH
-  const selectedRadius = SELECTED_RADIUS[size] / scale
-  const radius = RADIUS[size] / scale
+  const selectedRadius = SELECTED_RADIUS[size]
+  const radius = RADIUS[size]
 
   return (
-    <g onPointerUp={active ? onFinish : undefined}>
+    <g transform={`scale(${1 / scale})`} onPointerUp={active ? onFinish : undefined}>
       <line
         x1='0'
         y1={-1 * crosshairSpace * selectedRadius}
         x2='0'
         y2={-1 * selectedRadius}
         strokeWidth={crosshairWidth}
-        vectorEffect={'non-scaling-stroke'}
+        vectorEffect={'non-scaling-size'}
       />
       <line
         x1={-1 * crosshairSpace * selectedRadius}
@@ -37,7 +42,7 @@ function Point({ active, children, mark, onFinish, scale }) {
         x2={-1 * selectedRadius}
         y2='0'
         strokeWidth={crosshairWidth}
-        vectorEffect={'non-scaling-stroke'}
+        vectorEffect={'non-scaling-size'}
       />
       <line
         x1='0'
@@ -45,7 +50,7 @@ function Point({ active, children, mark, onFinish, scale }) {
         x2='0'
         y2={selectedRadius}
         strokeWidth={crosshairWidth}
-        vectorEffect={'non-scaling-stroke'}
+        vectorEffect={'non-scaling-size'}
       />
       <line
         x1={crosshairSpace * selectedRadius}
@@ -53,9 +58,9 @@ function Point({ active, children, mark, onFinish, scale }) {
         x2={selectedRadius}
         y2='0'
         strokeWidth={crosshairWidth}
-        vectorEffect={'non-scaling-stroke'}
+        vectorEffect={'non-scaling-size'}
       />
-      <circle r={active ? selectedRadius : radius} vectorEffect={'non-scaling-stroke'} />
+      <circle r={active ? selectedRadius : radius} vectorEffect={'non-scaling-size'} />
     </g>
   )
 }
@@ -64,16 +69,6 @@ Point.propTypes = {
   active: PropTypes.bool,
   mark: PropTypes.object,
   scale: PropTypes.number
-}
-
-Point.defaultProps = {
-  active: false,
-  mark: {
-    tool: {
-      size: 'large'
-    }
-  },
-  scale: 1
 }
 
 export default observer(Point)
