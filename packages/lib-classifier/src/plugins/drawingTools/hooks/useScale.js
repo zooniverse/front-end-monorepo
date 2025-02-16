bf399d69fd40167cf09f2373c5659dde74bc5b5a
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-02-16 09:20:40 +0000

    Docs and example

--- packages/lib-classifier/src/plugins/drawingTools/hooks/useScale.js
+++ packages/lib-classifier/src/plugins/drawingTools/hooks/useScale.js
@@ -2,18 +2,37 @@ import { useContext, useState } from 'react'
 
 import SVGContext from '@plugins/drawingTools/shared/SVGContext'
 
-function getScale(canvas) {
-  // JSDOM doesn't support SVG, so we need to check for `getBBox` to run the tests.
-  if (canvas?.getBBox) {
-    const { width: clientWidth, height: clientHeight } = canvas.getBoundingClientRect()
-    const actualWidth = canvas.getBBox().width
-    const transformRoot = canvas.closest('g[transform]')
+/**
+ * Get the rotation angle of the drawing canvas.
+ * @param {SVGGraphicsElement} node - An SVG node on the drawing canvas.
+ * @returns {number} The rotation angle of the drawing canvas.
+ * @default 0
+ */
+function getRotationAngle(node) {
+  const transformRoot = node.closest('g[transform]')
+  if (transformRoot) {
     const transformList = transformRoot.transform?.baseVal
     // the rotation transform is the only item in the list.
     const transform = transformList?.numberOfItems > 0
       ? transformList.getItem(0)
       : null
-    const rotationAngle = transform?.angle || 0
+    return transform?.angle || 0
+  }
+  return 0
+}
+
+/**
+ * Get the scale (screen width / intrinsic width) of the zoomed and rotated drawing canvas.
+ * @param {SVGGraphicsElement} canvas - The drawing canvas node from the DOM.
+ * @returns {number} The scale of the drawing canvas.
+ * @default 1
+ */
+function getScale(canvas) {
+  // JSDOM doesn't support SVG, so we need to check for `getBBox` to run the tests.
+  if (canvas?.getBBox) {
+    const { width: clientWidth, height: clientHeight } = canvas.getBoundingClientRect()
+    const actualWidth = canvas.getBBox().width
+    const rotationAngle = getRotationAngle(canvas)
     const scale = (rotationAngle % 180 === 0)
       ? clientWidth / actualWidth // rotation is 0 or 180 degrees.
       : clientHeight / actualWidth // rotation is 90 or 270 degrees.
@@ -23,6 +42,22 @@ function getScale(canvas) {
   return 1
 }
 
+/**
+ * Use the scale (screen width / intrinsic SVG width) of the drawing canvas.
+ * Useful for emulating `vector-effect:non-scaling-size` by inverting the scale.
+ * ```jsx
+ * // Use this inside an SVGContext.Provider.
+ * const scale = useScale()
+ * 
+ * return (
+ *  <g transform={`translate(50, 50) scale(${1 / scale})`}>
+ *    <circle r={50} />
+ *  </g>
+ * )
+ * ```
+ * @returns {number} The scale of the drawing canvas. 
+ * @default 1
+ */
 export default function useScale() {
   const { canvas } = useContext(SVGContext)
   const [scale, setScale] = useState(1)
