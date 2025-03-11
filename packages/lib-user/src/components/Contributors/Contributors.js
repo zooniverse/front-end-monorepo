74317b1b86ef7de986b4abe97ae69269408d1778
Mark Bouslog <mark@zooniverse.org>
2025-03-10 23:07:31 -0500

    Add estimated progress

--- packages/lib-user/src/components/Contributors/Contributors.js
+++ packages/lib-user/src/components/Contributors/Contributors.js
@@ -33,6 +33,7 @@ function Contributors({
 }) {
   const { t } = useTranslation()
   const [exportLoading, setExportLoading] = useState(false)
+  const [exportProgress, setExportProgress] = useState(0)
   const [page, setPage] = useState(1)
 
   const showContributors = adminMode
@@ -117,34 +118,46 @@ function Contributors({
 
   async function handleGenerateExport() {
     setExportLoading(true)
+    setExportProgress(0)
 
     let allUsers = []
-    const CHUNK_SIZE = 100 // maximum number of IDs per request, see GitHub issue front-end-monorepo/issues/6363
+    
+    // maximum number of IDs per request, see GitHub issue front-end-monorepo/issues/6363
+    // to keep request less then 2084 characters, with IDs ~6 characters long plus %2C2 (encoded comma), use chunk size of 200
+    const CHUNK_SIZE = 200
 
     try {
       if (memberIdsPerStats && memberIdsPerStats.length > 0) {
         // If there are more than CHUNK_SIZE users, make multiple requests
         if (memberIdsPerStats.length > CHUNK_SIZE) {
           const userPromises = []
-          
+          const totalChunks = Math.ceil(memberIdsPerStats.length / CHUNK_SIZE)
+
           for (let i = 0; i < memberIdsPerStats.length; i += CHUNK_SIZE) {
             const chunk = memberIdsPerStats.slice(i, i + CHUNK_SIZE)
             const chunkQuery = {
               id: chunk.join(','),
               page_size: 100 // per panoptes, maximum page_size is 100
             }
-            userPromises.push(fetchPanoptesUsers(chunkQuery))
+
+            const wrappedPromise = fetchPanoptesUsers(chunkQuery).then(result => {
+              setExportProgress(prevProgress => prevProgress + ((1 / totalChunks) * 100))
+              return result
+            })
+
+            userPromises.push(wrappedPromise)
           }
           
           const userResults = await Promise.all(userPromises)
           allUsers = userResults.flat()
         } else {
-          // If there are 100 or fewer users, make a single request
+          // If there are fewer users than CHUNK_SIZE, make a single request
           const allUsersQuery = {
             id: memberIdsPerStats.join(','),
-            page_size: CHUNK_SIZE
+            page_size: 100 // per panoptes, maximum page_size is 100
           }
           allUsers = await fetchPanoptesUsers(allUsersQuery)
+          setExportProgress(100)
         }
       }
 
@@ -195,6 +208,9 @@ function Contributors({
               <Loader
                 loadingMessage={loadingExportMessage}
               />
+              <SpacedText>
+                {`Progress: ${Math.round(exportProgress)}%`}
+              </SpacedText>
             </Box>
           </Layer>
         ) : null
