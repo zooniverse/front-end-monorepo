4d2841d882ac4cb99e862c8b8d1ac319b4d1c4ab
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-04-10 14:54:46 -0500

    use page_size of 20 to fetch project data from panoptes to match project_contributions

--- packages/lib-user/src/components/GroupAllProjects/GroupAllProjects.js
+++ packages/lib-user/src/components/GroupAllProjects/GroupAllProjects.js
@@ -1,7 +1,7 @@
 import { arrayOf, bool, shape, string } from 'prop-types'
 import { useTranslation } from '../../translations/i18n.js'
 
-import { usePanoptesProjects, useStats } from '@hooks'
+import { useStats } from '@hooks'
 import { AllProjects, ContentBox, HeaderLink, Layout } from '@components/shared'
 
 const STATS_ENDPOINT = '/classifications/user_groups'
@@ -10,7 +10,7 @@ const STATS_ENDPOINT = '/classifications/user_groups'
 function GroupAllProjects({ authUser, group }) {
   const { t } = useTranslation()
 
-  // fetch all projects stats; date range is all time
+  // fetch all projects contributions from ERAS; Date range is "All Time"
   const {
     data: allProjectsStats,
     error: statsError,
@@ -21,43 +21,10 @@ function GroupAllProjects({ authUser, group }) {
     sourceId: group?.id
   })
 
-  // fetch project data from panoptes
-  const projectIds = allProjectsStats?.project_contributions?.map(
-    project => project.project_id
-  )
-  const projectsQuery = {
-    cards: true,
-    id: projectIds?.join(','),
-    page_size: 100
-  }
-  const {
-    data: projects,
-    error: projectsError,
-    isLoading: projectsLoading
-  } = usePanoptesProjects(projectsQuery)
-
-  // order by top projects
-  let topProjects = []
-  const topProjectContributions = allProjectsStats?.project_contributions?.sort(
+  // order by most contributions --> least contributions
+  const projectContributions = allProjectsStats?.project_contributions?.sort(
     (a, b) => b.count - a.count
   )
-
-  if (topProjectContributions?.length > 0) {
-    topProjects = topProjectContributions
-      ?.map(projectContribution => {
-        const projectData = projects?.find(
-          project => project.id === projectContribution.project_id.toString()
-        )
-        return {
-          count: projectContribution.count,
-          ...projectData
-        }
-      })
-      .filter(project => project?.id)
-  }
-
-  const error = statsError || projectsError
-  const loading = statsLoading || projectsLoading
 
   return (
     <Layout
@@ -69,8 +36,8 @@ function GroupAllProjects({ authUser, group }) {
         />
       }
     >
-      <ContentBox title='All Projects' pad='45px'>
-        <AllProjects error={error} loading={loading} projects={topProjects} />
+      <ContentBox title={t('AllProjects.title')} pad='45px'>
+        <AllProjects error={statsError} loading={statsLoading} projectContributions={projectContributions} />
       </ContentBox>
     </Layout>
   )
--- packages/lib-user/src/components/shared/AllProjects/AllProjects.js
+++ packages/lib-user/src/components/shared/AllProjects/AllProjects.js
@@ -1,9 +1,14 @@
 import { Box, Text } from 'grommet'
+import { useState } from 'react'
 import styled from 'styled-components'
 import { arrayOf, bool, number, shape, string } from 'prop-types'
 import ProjectCard from '@zooniverse/react-components/ProjectCard'
 import Loader from '@zooniverse/react-components/Loader'
+
 import { useTranslation } from '../../../translations/i18n.js'
+import { usePanoptesProjects } from '@hooks'
+
+const PAGE_SIZE = 20
 
 const StyledBox = styled(Box)`
   list-style: none;
@@ -13,26 +18,64 @@ const StyledBox = styled(Box)`
   row-gap: 20px;
 `
 
-function AllProjects({ error, loading, projects }) {
+function AllProjects({ error, loading, projectContributions }) {
+  console.log(projectContributions)
   const { t } = useTranslation()
+  const [page, setPage] = useState(0)
+
+  // grab the project ids
+  const projectIds = projectContributions?.map(project => project.project_id)
+
+  const numProjects = projectIds?.length
+  const numPages = Math.ceil(numProjects / PAGE_SIZE)
+  const startSlice = page * PAGE_SIZE
+  const endSlice = page === numPages ? numProjects : (page + 1) * PAGE_SIZE
+
+  // fetch project data from panoptes
+  const projectsQuery = {
+    cards: true,
+    id: projectIds?.slice(startSlice, endSlice).join(','),
+    page_size: PAGE_SIZE
+  }
+  const {
+    data: projects,
+    error: projectsError,
+    isLoading: projectsLoading
+  } = usePanoptesProjects(projectsQuery)
+
+  // Match project data from panoptes to the stat from ERAS
+  const renderedProjects = projectContributions
+    ?.map(projectContribution => {
+      const projectData = projects?.find(
+        project => project.id === projectContribution.project_id.toString()
+      )
+      return {
+        count: projectContribution.count, // for the badge in ProjectCard
+        ...projectData
+      }
+    })
+    .filter(project => project?.id) // in case of private project not returned from panoptes?
+
+  const showLoading = loading || projectsLoading
+  const showError = error || projectsError
 
   return (
     <>
-      {loading ? (
+      {showLoading ? (
         <Box fill align='center' justify='center'>
           <Loader />
         </Box>
-      ) : projects?.length === 0 ? (
+      ) : numProjects === 0 ? (
         <Box fill align='center' pad='medium'>
           <Text>{t('AllProjects.noProjects')}</Text>
         </Box>
-      ) : error ? (
+      ) : showError ? (
         <Box fill align='center' pad='medium'>
           <Text>{t('AllProjects.error')}</Text>
         </Box>
       ) : (
         <StyledBox forwardedAs='ul' direction='row' wrap justify='center'>
-          {projects.map(project => {
+          {renderedProjects.map(project => {
             return (
               <li key={project?.id}>
                 <ProjectCard
@@ -58,14 +101,10 @@ AllProjects.propTypes = {
     status: number
   }),
   loading: bool,
-  projects: arrayOf(
+  projectContributions: arrayOf(
     shape({
-      avatar_src: string,
       count: number,
-      description: string,
-      display_name: string,
-      id: string,
-      slug: string
+      project_id: number,
     })
   )
 }
