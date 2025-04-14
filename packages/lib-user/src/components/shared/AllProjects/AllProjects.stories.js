50390669b5467bfe54f8463e98864d36eb49d8bd
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-04-14 10:58:30 +0100

    Add a mock OAuth API to All Projects stories

--- packages/lib-user/src/components/shared/AllProjects/AllProjects.stories.js
+++ packages/lib-user/src/components/shared/AllProjects/AllProjects.stories.js
@@ -14,18 +14,32 @@ export default {
   parameters: {
     msw: {
       handlers: [
-        // http.post(`${PANOPTES_HOST}/oauth/token`), () => {
-        //   return HttpResponse.json({
-        //     access_token: 'defg',
-        //     token_type: 'Bearer',
-        //     expires_in: 7200,
-        //     refresh_token: 'abc',
-        //     created_at: 1744597727
-        //   })
-        // },
+        http.post(`${PANOPTES_HOST}/oauth/token`, () => {
+          return HttpResponse.json({
+            access_token: 'defg',
+            token_type: 'Bearer',
+            expires_in: 7200,
+            refresh_token: 'abc',
+            created_at: 1744597727
+          })
+        }),
+        http.get(`${PANOPTES_HOST}/api/me`, ({ request }) => {
+          if (request.headers.authorization === 'Bearer defg') {
+            return HttpResponse.json({
+              users: [{
+                id: '1234',
+                login: 'testuser',
+                display_name: 'Test User',
+              }]
+            })
+          }
+          return HttpResponse.json({
+            error: 'Unauthorized'
+          }, { status: 401 })
+        }),
         http.get(`${PANOPTES_HOST}/api/projects`, ({ request }) => {
           return HttpResponse.json({
-            projects: []
+            projects: PROJECTS
           })
         })
       ]
@@ -35,21 +49,23 @@ export default {
 
 export const Default = {
   args: {
+    containerError: null,
+    containerLoading: false,
     projectContributions: PROJECT_CONTRIBUTIONS
   }
 }
 
 export const Error = {
   args: {
-    error: { message: `Couldn't fetch projects`, status: 500 },
-    loading: false,
+    containerError: { message: `Couldn't fetch projects`, status: 500 },
+    containerLoading: false,
     projectContributions: PROJECT_CONTRIBUTIONS
   }
 }
 export const NoProjects = {
   args: {
-    error: null,
-    loading: false,
+    containerError: null,
+    containerLoading: false,
     projectContributions: []
   }
 }
