79e55c346eea81adcc15659622f9f1b77a252dcf
Mark Bouslog <mark@zooniverse.org>
2025-05-07 16:35:10 -0500

    Refactor InteractionLayer svgContext canvas definition if canvasRef defined

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/InteractionLayer/InteractionLayer.js
@@ -53,7 +53,10 @@ function InteractionLayer({
   const [creating, setCreating] = useState(false)
   const svgContext = useContext(SVGContext)
   const canvasRef = useRef()
-  svgContext.canvas = canvasRef.current
+  
+  if (canvasRef.current) {
+    svgContext.canvas = canvasRef.current
+  }
 
   if (creating && !activeMark) {
     setCreating(false)
