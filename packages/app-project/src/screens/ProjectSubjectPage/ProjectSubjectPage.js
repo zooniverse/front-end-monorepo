139c550804960b0ac6953db381427c94afdd866e
Mark Bouslog <mark@zooniverse.org>
2025-05-23 13:38:12 -0500

    Add initial ProjectSubjectPage layout

--- packages/app-project/src/screens/ProjectSubjectPage/ProjectSubjectPage.js
+++ packages/app-project/src/screens/ProjectSubjectPage/ProjectSubjectPage.js
@@ -1,24 +1,99 @@
-import { Box, Grid, ResponsiveContext } from 'grommet'
-import dynamic from 'next/dynamic'
+import { Media } from '@zooniverse/react-components'
+import { Box, Grid } from 'grommet'
+import { Bookmark, Favorite, ShareOption, SubtractCircle } from 'grommet-icons'
 import { shape, string } from 'prop-types'
 import styled from 'styled-components'
 
+import ContentBox from '@shared/components/ContentBox'
 import StandardLayout from '@shared/components/StandardLayout'
+
+// based on the lib-classifier MaxWidth layout
+const ContainerGrid = styled(Grid)`
+  position: relative;
+  grid-gap: 1.875rem;
+  grid-template-areas: 'viewer talkData';
+  grid-template-columns: minmax(auto, 100rem) '25rem';
+  margin: auto;
+
+  @media screen and (max-width: 1024px) {
+    grid-gap: 1.25rem;
+    grid-template-areas:
+      'viewer'
+      'talkData';
+    grid-template-columns: 100%;
+    grid-template-rows: auto auto;
+    margin: 0;
+  }
+`
 
 function ProjectSubjectPage({
   appLoadingState,
   subject,
   subjectID
 }) {
+  const subjectURLs = subject.locations.map(location => Object.values(location)[0])
+  const subjectURL = subjectURLs[0]
+
   return (
     <StandardLayout>
-      <Box pad="medium">
-        <h1>Subject ID: {subjectID}</h1>
-        <pre>
-          <code>
-            {JSON.stringify(subject, null, 2)}
-          </code>
-        </pre>
+      <Box
+        align='center'
+        gap='medium'
+        pad='medium'
+      >
+        <ContainerGrid>
+          <Box>
+            <Media
+              alt={`subject ${subjectID}`}
+              controls={true}
+              subject={subject}
+              src={subjectURL}
+            />
+            {/* <MetaTools /> */}
+            <Box
+              direction='row'
+              gap='small'
+              justify='center'
+              margin='small'
+            >
+              <Favorite />
+              <Bookmark />
+              <ShareOption />
+              <SubtractCircle />
+            </Box>
+          </Box>
+          <Box gap='small'>
+            {/* <TalkSearch /> */}
+            <input type='text' placeholder='Search this project for tags, subjects, or @users' />
+            {/* <TalkData /> */}
+            <Box>
+              Talk Data goes here
+            </Box>
+          </Box>
+        </ContainerGrid>
+        <Box
+          as='aside'
+          gap='medium'
+          width='min(100%, 90rem)'
+        >
+          {/* <MetaData /> */}
+          <ContentBox
+            title='Subject Metadata'
+          />
+          {/* <AncillaryData /> */}
+          {/* <FeaturedCollections /> */}
+          <ContentBox
+            title='Featured in these Collections'
+            linkLabel='See more'
+            linkProps={{
+              href: ''
+            }}
+          />
+          {/* <RelatedSubjects /> */}
+          <ContentBox
+            title='Other subject being discussed'
+          />
+        </Box>
       </Box>
     </StandardLayout>
   )
