648f69b5c73aca66287c7edab8a20f2c1c2c83a8
Mark Bouslog <mark@zooniverse.org>
2025-07-15 10:06:06 -0500

    Update InvertIconButton tests

--- packages/lib-react-components/src/InvertIconButton/InvertIconButton.spec.js
+++ packages/lib-react-components/src/InvertIconButton/InvertIconButton.spec.js
@@ -7,7 +7,7 @@ describe('InvertIconButton', function () {
   it('should have an aria label', function () {
     const InvertIconButtonStory = composeStory(Default, Meta)
     render(<InvertIconButtonStory />)
-    const button = screen.getByRole('checkbox', { name: 'Invert subject' })
+    const button = screen.getByRole('checkbox', { name: 'Invert subject color' })
     expect(button).to.exist()
   })
 
@@ -15,7 +15,7 @@ describe('InvertIconButton', function () {
     it('should have aria-checked set to true', function () {
       const InvertIconButtonStory = composeStory(Checked, Meta)
       render(<InvertIconButtonStory />)
-      const button = screen.getByRole('checkbox', { name: 'Reset subject' })
+      const button = screen.getByRole('checkbox', { name: 'Reset subject color' })
       expect(button.getAttribute('aria-checked')).to.equal('true')
     })
   })
@@ -24,7 +24,7 @@ describe('InvertIconButton', function () {
     it('should not be clickable', function () {
       const InvertIconButtonStory = composeStory(Disabled, Meta)
       render(<InvertIconButtonStory />)
-      const button = screen.getByRole('checkbox', { name: 'Invert subject' })
+      const button = screen.getByRole('checkbox', { name: 'Invert subject color' })
       expect(button.disabled).to.be.true()
     })
   })
