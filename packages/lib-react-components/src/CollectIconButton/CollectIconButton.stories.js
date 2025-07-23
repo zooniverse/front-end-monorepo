3d81eec4fb9687faadd3d761b4bb4bd92a36ed61
Mark Bouslog <mark@zooniverse.org>
2025-07-23 18:20:28 -0500

    Refactor CollectIconButton with CollectionsModal

--- packages/lib-react-components/src/CollectIconButton/CollectIconButton.stories.js
+++ packages/lib-react-components/src/CollectIconButton/CollectIconButton.stories.js
@@ -16,12 +16,16 @@ export default {
 
 export const Default = {
   args: {
-    disabled: false
+    disabled: false,
+    subjectId: '12345',
+    userId: '6789'
   }
 }
 
 export const Disabled = {
   args: {
-    disabled: true
+    disabled: true,
+    subjectId: '12345',
+    userId: '6789'
   }
 }
