5c67922c20805ad5535e4406c22fad987ea4c74a
Mark Bouslog <mark@zooniverse.org>
2025-07-31 17:06:25 -0500

    Add useDiscussions hook

--- packages/app-project/src/hooks/index.js
+++ packages/app-project/src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useAdminMode } from './useAdminMode.js'
+export { default as useDiscussions } from './useDiscussions.js'
 export { default as useUserFavourites } from './useUserFavourites.js'
 export { default as usePanoptesAuth } from './usePanoptesAuth.js'
 export { default as usePanoptesAuthToken } from './usePanoptesAuthToken.js'
