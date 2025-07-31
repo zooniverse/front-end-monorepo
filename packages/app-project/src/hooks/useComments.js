3fa88769a66614749ce4604c2c322e08c1c1b7dd
Mark Bouslog <mark@zooniverse.org>
2025-07-31 17:14:41 -0500

    Add useComments hook

---
+++ packages/app-project/src/hooks/useComments.js
@@ -0,0 +1,31 @@
+import { talkAPI } from '@zooniverse/panoptes-js'
+import useSWR from 'swr'
+
+import usePanoptesAuthToken from '@hooks/usePanoptesAuthToken'
+
+const SWROptions = {
+  revalidateIfStale: true,
+  revalidateOnMount: true,
+  revalidateOnFocus: true,
+  revalidateOnReconnect: true,
+  refreshInterval: 0
+}
+
+async function fetchComments({ query, token }) {
+  const authorization = token ? `Bearer ${token}` : undefined
+
+  return talkAPI.get('/comments', query, { authorization })
+    .then(response => response?.body?.comments)
+    .catch(error => {
+      console.error(error)
+      throw error
+    })
+}
+
+export default function useComments(query) {
+  const token = usePanoptesAuthToken()
+  
+  const key = query ? { query, token } : null
+
+  return useSWR(key, fetchComments, SWROptions)
+}
