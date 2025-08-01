9fc2464e801fb1735f7c4f39d11b116add0c70d7
Mark Bouslog <mark@zooniverse.org>
2025-08-01 13:41:20 -0500

    Refactor SubjectTalkData with Discussions component

--- packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/SubjectTalkData.js
+++ packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/SubjectTalkData.js
@@ -1,29 +1,21 @@
-import { Box } from 'grommet'
+import { Box, Heading } from 'grommet'
 import { useTranslation } from 'next-i18next'
 import { string } from 'prop-types'
+import styled from 'styled-components'
 
-import { useDiscussions } from '@hooks'
+import Discussions from './components/Discussions'
 
-import Discussion from './components/Discussion'
+const StyledHeading = styled(Heading)`
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 1.2px;
+`
 
 function SubjectTalkData({
   projectId,
   subjectId
 }) {
   const { t } = useTranslation('screens')
-
-  const query = {
-    section: `project-${projectId}`,
-    focus_id: subjectId,
-    focus_type: 'Subject',
-    sort: '-created_at',
-  }
-
-  const {
-    data: discussions,
-    isLoading,
-    error
-  } = useDiscussions(query)
 
   return (
     <Box
@@ -32,25 +24,43 @@ function SubjectTalkData({
     >
       {/* <TalkSearch /> */}
       <input type='text' placeholder={t('Talk.searchPlaceholder')} />
-      {/* <TalkData /> */}
       <Box
         background={{
           dark: 'dark-3',
-          light: 'white'
+          light: 'neutral-6'
         }}
+        border={[
+          { color: 'light-5', side: 'all', size: '0.5px' },
+          { color: 'light-5', side: 'between', size: '0.5px' }
+        ]}
+        gap='0.5px'
+        round='8px'
       >
-        <h2>{t('Home.ZooniverseTalk.RecentSubjects.subjectLabel', { id: subjectId })}</h2>
-        <Box pad='medium'>
+        <Box
+          align='start'
+          pad='small'
+          round={{ corner: 'top', size: '8px' }}
+        >
+          <StyledHeading
+            color={{ dark: 'light-1', light: 'dark-4' }}
+            level={2}
+          >
+            {t('Home.ZooniverseTalk.RecentSubjects.subjectLabel', { id: subjectId })}
+          </StyledHeading>
+        </Box>
+        <Box
+          pad='small'
+        >
           <h3>Tags</h3>
         </Box>
-        <Box pad='small'>
-          <h3>Discussions</h3>
-          {discussions?.map((discussion) => (
-            <Discussion
-              key={discussion.id}
-              discussion={discussion}
-            />
-          ))}
+        <Discussions
+          projectId={projectId}
+          subjectId={subjectId}
+        />
+        <Box
+          pad='small'
+        >
+          <h3>Mentions</h3>
         </Box>
       </Box>
     </Box>
