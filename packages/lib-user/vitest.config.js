b7c00ab92620d490a67db4dfc0ea2203c4b12b7c
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-08-04 13:25:20 -0500

    increase test timeout limit to 10sec

--- packages/lib-user/vitest.config.js
+++ packages/lib-user/vitest.config.js
@@ -18,5 +18,6 @@ export default defineConfig({
     setupFiles: './test/setup.js',
     // you might want to disable the css propery here, if you don't have tests that rely on CSS since parsing CSS is slow
     css: true,
+    testTimeout: 10000 // default is 5000ms
   },
 })
