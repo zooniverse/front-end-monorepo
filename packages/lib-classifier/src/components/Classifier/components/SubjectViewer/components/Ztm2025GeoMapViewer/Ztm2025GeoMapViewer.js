f0e5f6a215d74775bec6d903648d774ab9542084
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-08-14 09:23:14 -0500

    Ztm2025GeoMapViewer: implement basic OpenLayers map

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/Ztm2025GeoMapViewer/Ztm2025GeoMapViewer.js
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/Ztm2025GeoMapViewer/Ztm2025GeoMapViewer.js
@@ -1,7 +1,50 @@
+import { useEffect, useId } from 'react'
+// import 'ol/ol.css'  // TODO: check if this does anything.
+import olCss from 'ol/ol.css'  // TODO: check if this does anything.
+import OpenLayersMap from 'ol/Map.js'
+import OpenStreetMaps from 'ol/source/OSM.js'
+import TileLayer from 'ol/layer/Tile.js'
+import View from 'ol/View.js'
+
+
 function Ztm2025GeoMapViewer (props) {
+  const olMapId = useId()
+  let olMap = undefined
+
+  console.log('+++ check what\'s ol.css:', olCss)
+
+  useEffect(function loadMap () {
+    // Init map
+    olMap = new OpenLayersMap({
+      target: olMapId,
+    })
+
+    // Set map tiles
+    const osmSource = new OpenStreetMaps()
+    const osmLayer = new TileLayer({source: osmSource});
+    olMap.addLayer(osmLayer);
+
+    // Set view on map
+    olMap.setView(new View({
+      center: [0, 0],
+      zoom: 2,
+    }))
+
+    return function unloadMap () {
+      olMap?.setTarget(undefined)
+    }
+  }, [])
+
   return (
     <div>
       <h6>Ztm2025GeoMapViewer</h6>
+      <div
+        id={olMapId}
+        style={{
+          width: '100%',
+          height: '400px'
+        }}
+      ></div>
     </div>
   )
 }
