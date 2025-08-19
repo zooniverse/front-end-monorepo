e3ff37584ceae40c65b44084e215a642570d7a71
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-08-18 20:34:14 -0500

    remove modal height constraint and improve Tasks story

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/Tasks.stories.js
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/components/Tasks/Tasks.stories.js
@@ -1,80 +1,109 @@
 import asyncStates from '@zooniverse/async-states'
-import { MockTask } from '@stories/components'
-import Tasks from './Tasks'
+import { Provider } from 'mobx-react'
+import { applySnapshot } from 'mobx-state-tree'
+import { Box } from 'grommet'
+
+import mockStore from '@test/mockStore'
+import { SubjectFactory, WorkflowFactory } from '@test/factories'
+
+import TasksConnector from './TasksConnector'
 
 export default {
   title: 'Tasks / General',
-  component: Tasks,
-  args: {
-    isThereTaskHelp: true,
-    required: false,
-    subjectReadyState: asyncStates.success
+  component: TasksConnector
+}
+
+// export function Loading() {
+//   return (
+//     <MockTask
+//       loadingState={asyncStates.loading}
+//     />
+//   )
+// }
+
+// export function Error() {
+//   return (
+//     <MockTask
+//       loadingState={asyncStates.error}
+//     />
+//   )
+// }
+
+const singleChoiceTask = {
+  answers: [
+    { label: 'yes', next: 'P1' },
+    { label: 'no', next: 'P1' }
+  ],
+  question: 'Yes or No?',
+  required: true,
+  type: 'single'
+}
+
+const multipleChoiceTask = {
+  answers: [{ label: 'Jimin' }, { label: 'Jungkook' }, { label: 'j-hope' }],
+  question: 'Who is your favorite member of BTS?',
+  required: true,
+  type: 'multiple'
+}
+
+const workflowStrings = {
+  'tasks.T0.answers.0.label': 'Yes',
+  'tasks.T0.answers.1.label': 'No',
+  'tasks.T0.help':
+    'Choose an answer from the choices given, then press Done. This is help text from the first task.',
+  'tasks.T0.question': 'Yes or No?',
+  'tasks.T1.answers.0.label': 'Jimin',
+  'tasks.T1.answers.1.label': 'Jungkook',
+  'tasks.T1.answers.2.label': 'j-hope',
+  'tasks.T1.help':
+    'Pick as many answers as apply, then press Done. This is help text of the second task.',
+  'tasks.T1.question': 'Who is your favorite member of BTS?'
+}
+
+/*
+  Build a mock workflow that has one step and two tasks in that step.
+  We're manually adding strings to the workflow object because normally
+  there are API requests to /translations and the returned translations strings
+  are attached to the workflow store in ClassifierContainer.
+*/
+const mockWorkflow = WorkflowFactory.build({
+  display_name: 'Multi-task step',
+  tasks: {
+    T0: singleChoiceTask,
+    T1: multipleChoiceTask
   },
-  argTypes: {
-    loadingState: {
-      control: {
-        type: 'select',
-        options: asyncStates
-      }
-    },
-    subjectReadyState: {
-      control: {
-        type: 'select',
-        options: asyncStates
-      }
-    }
-  }
-}
+  steps: [['S0', { stepKey: 'S0', taskKeys: ['T0', 'T1'] }]]
+})
 
-export function Loading() {
-  return (
-    <MockTask
-      loadingState={asyncStates.loading}
-    />
-  )
-}
+/* Create a very simple subject */
+const mockSubject = SubjectFactory.build()
 
-export function Error() {
-  return (
-    <MockTask
-      loadingState={asyncStates.error}
-    />
-  )
-}
+/* Init the mock store */
+const mockedStore = mockStore({ subject: mockSubject, workflow: mockWorkflow })
 
-export function MultipleTasks({ isThereTaskHelp, required, subjectReadyState }) {
-  const tasks = {
-    init: {
-      answers: [{ label: 'yes' }, { label: 'no' }],
-      required,
-      strings: {
-        help: isThereTaskHelp ? 'Choose an answer from the choices given, then press Done.' : '',
-        question: 'Is there a cat?',
-        'answers.0.label': 'yes',
-        'answers.1.label': 'no'
-      },
-      taskKey: 'init',
-      type: 'single'
-    },
-    T1: {
-      answers: [{ label: 'sleeping' }, { label: 'playing' }, { label: 'looking indifferent' }],
-      required,
-      strings: {
-        help: isThereTaskHelp ? 'Pick as many answers as apply, then press Done.' : '',
-        question: 'What is it doing?',
-        'answers.0.label': 'sleeping',
-        'answers.1.label': 'playing',
-        'answers.2.label': 'looking indifferent'
-      },
-      taskKey: 'T1',
-      type: 'multiple'
-    }
-  }
+/*
+  This is what happens in ClassifierContainer and provides the labels to the
+  task's questions, answers, and help text.
+*/
+const storedWorkflow = mockedStore.workflows.resources.get(mockWorkflow.id)
+applySnapshot(storedWorkflow.strings, workflowStrings)
+
+/* Subject viewer must be ready for the task area to be interactive */
+const { subjectViewer } = mockedStore
+subjectViewer.onSubjectReady()
+
+export function MultipleTasks() {
   return (
-    <MockTask
-      isThereTaskHelp={isThereTaskHelp}
-      subjectReadyState={subjectReadyState}
-      tasks={tasks}
-    />
+    <Provider classifierStore={mockedStore}>
+      <Box
+        width='25rem'
+        pad='medium'
+        margin='medium'
+        border={{ style: 'solid', color: { dark: 'white', light: 'light-3' } }}
+        background={{ light: 'white'}}
+      >
+        <TasksConnector />
+      </Box>
+    </Provider>
   )
 }
