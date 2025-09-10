e9b26f9cfb570b0686ca7047b8ec64b949e1babc
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-09-10 15:11:36 -0500

    Round completeness percentage to nearest integar

--- packages/app-project/src/shared/components/ProjectStatistics/components/CompletionBar/CompletionBar.jsx
+++ packages/app-project/src/shared/components/ProjectStatistics/components/CompletionBar/CompletionBar.jsx
@@ -9,8 +9,8 @@ const Relative = styled(Box)`
 
 const StyledText = styled(SpacedText)`
   position: absolute;
-  left: ${props => props.$completeness < 0.7 ? '100%' : 'auto'};
-  right: ${props => props.$completeness < 0.7 ? 'auto' : '0'};
+  left: ${props => props.$completeness < 0.5 ? '100%' : 'auto'};
+  right: ${props => props.$completeness < 0.5 ? 'auto' : '0'};
   top: 50%;
   transform: translateY(-50%);
 `
@@ -21,15 +21,15 @@ function CompletionBar({ completeness = 0 }) {
       <Relative
         height='40px'
         background='brand'
-        width={`${completeness * 100}%`}
+        width={`${Math.round(completeness * 100)}%`}
       >
         <StyledText
           margin={{ horizontal: '10px' }}
           size='1.2rem'
-          color={completeness < 0.7 ? 'brand' : 'white'}
+          color={completeness < 0.5 ? 'brand' : 'white'}
           $completeness={completeness}
         >
-          {completeness * 100}%
+          {Math.round(completeness * 100)}%
         </StyledText>
       </Relative>
     </Box>
