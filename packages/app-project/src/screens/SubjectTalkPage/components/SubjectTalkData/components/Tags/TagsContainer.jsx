6dfab5e796598663c3a2aae57549608318e76766
Mark Bouslog <mcbouslog@gmail.com>
2025-09-18 00:26:55 -0500

    app-project: Add Tags and Tag components and useTags, useVotableTags, and useTagVotes hooks (#7007)
    * Add Tag component and stories
    
    * Add Tags component
    
    * Add useTags, useVotableTags, and useTagVotes hooks
    
    * Add addTagVote, createVotableTag, and removeTagVote helpers
    
    * Refactor SubjectTalkPage with Tags
    
    * Refactor Tag disabled styling
    
    * Refactor Tags for tagVote loading state
    
    * Tags cleanup
    
    * Refactor Tag width
    
    * Convert SubjectTalkPage to jsx
    
    * Convert Tags and Tag to jsx
    
    * Refactor Tags height
    
    * Refactor Tags with TagsContainer
    
    * Add Tags mock data and stories
    
    * Refactor PlainButton with shared SpacedText
    
    * Refactor SectionHeading with shared SpacedHeading
    
    * Add Tag tests
    
    * Add Tags tests
    
    * Add TagsContainer comments
    
    * Refactor Tags for error, loading, empty states
    
    * Refactor Tags with Add a Tag button
    
    * Refactor Tags stories and tests for Add a Tag button, other updates
    
    * Refactor font-size with 1rem and SpacedHeading
    
    * Refactor TagsContainer voteUpdating state
    
    * Refactor font-size with rem
    
    * Refactor list with row-gap and column-gap
    
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
    
    * Refactor addTagVote and createVoteTag try catch blocks
    
    * Refactor handleRemoveVote tag match
    
    * Refactor Tag focus styling
    
    * refactor TagsContainer's mutation patterns (#7045)
    
    ---------
    
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>

---
+++ packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Tags/TagsContainer.jsx
@@ -0,0 +1,344 @@
+import { string } from 'prop-types'
+import { useState } from 'react'
+
+import {
+  addTagVote,
+  createVotableTag,
+  removeTagVote
+} from '@helpers'
+
+import {
+  usePopularTags,
+  useTagVotes,
+  useVotableTags
+} from '@hooks'
+
+import Tags from './Tags'
+
+function TagsContainer({
+  projectId,
+  subjectId,
+  userId
+}) {
+  const [voteUpdating, setVoteUpdating] = useState(false)
+
+  // Fetch popular tags for the subject, unrelated to the user
+  const popularTagsQuery = {
+    limit: 10,
+    page_size: 10,
+    section: `project-${projectId}`,
+    taggable_type: 'Subject',
+    taggable_id: subjectId,
+  }
+
+  const {
+    data: popularTags,
+    error: popularTagsError,
+    isLoading: popularTagsIsLoading,
+    mutate: mutatePopularTags
+  } = usePopularTags(popularTagsQuery)
+
+  // Fetch votable tags for the subject, unrelated to the user
+  const votableTagsQuery = {
+    page_size: 10,
+    section: `project-${projectId}`,
+    taggable_type: 'Subject',
+    taggable_id: subjectId
+  }
+
+  const {
+    data: votableTags,
+    error: votableTagsError,
+    isLoading: votableTagsIsLoading,
+    mutate: mutateVotableTags
+  } = useVotableTags(votableTagsQuery)
+
+  // Fetch the user's tag votes for the subject
+  const tagVotesQuery = {
+    page_size: 100,
+    section: `project-${projectId}`,
+    taggable_id: subjectId,
+    taggable_type: 'Subject',
+    user_id: userId
+  }
+
+  const {
+    data: tagVotes,
+    error: tagVotesError,
+    isLoading: tagVotesIsLoading,
+    isValidating: tagVotesIsValidating,
+    mutate: mutateTagVotes
+  } = useTagVotes(tagVotesQuery)
+
+  /*
+    Combine popular tags and votable tags, ensuring no duplicates, based on tag name.
+    If a tag appears in both lists, use the votable tag (which has vote_count).
+    Add a userVoted property to each tag based on whether the signed-in user has voted for it.
+    The rendered array (combinedTags) is ordered in most votes --> least votes.
+  */
+  const votableTagNames = votableTags?.map(tag => tag.name) || []
+  const filteredTags = popularTags?.filter(tag => !votableTagNames.includes(tag.name)) || []
+  const combinedTags = [...(votableTags || []), ...filteredTags].map(tag => ({
+    ...tag,
+    userVoted: tagVotes?.some(vote => vote.votable_tag.name === tag.name)
+  }))
+
+  /*
+    This function runs when a user clicks a "votable tag" to add their vote
+  */
+  async function handleAddVote(tag) {
+    // optimistically increment vote_count of the votableTag
+    mutateVotableTags(
+        prevData => {
+          if (!prevData) {
+            return []
+          } else {
+            return prevData.map(t => {
+              if (t.id === tag.id) {
+                return {
+                  ...t,
+                  vote_count: t.vote_count + 1
+                }
+              }
+              return t
+            })
+          }
+        },
+        { revalidate: false } // don't need to revalidate votableTags because optimistic data is enough
+    )
+
+    mutateTagVotes(
+      async () => {
+        // Set a loading state while the request is in progress to prevent an attempt to delete the tagVote before it's created in the database.
+        setVoteUpdating(true)
+        const newTagVote = await addTagVote({ votable_tag_id: tag.id })
+        setVoteUpdating(false)
+        // this function returns newTagVote as the first arg in populateCache
+        return newTagVote
+      },
+      {
+        // optimistically add the tagVote to the tagVotes array before network request happens
+        optimisticData: prevData => {
+          if (!prevData) {
+            return [{ votable_tag: { ...tag } }]
+          } else {
+            return [...prevData, { votable_tag: { ...tag } }]
+          }
+        },
+        populateCache: (newTagVote, prevData) => {
+          // if network request failed in addTagVote()
+          if (!newTagVote) {
+            return prevData
+          // else update tagVotes array with the newTagVote from addTagVote()
+          // this replaces optimistic data in the tagVotes array
+          } else {
+            return [...prevData, newTagVote]
+          }
+        },
+        // don't need to revalidate tagVotes because populateCache updates tagVotes as needed
+        revalidate: false,
+        // rollbackOnError responds to an error thrown by addTagVote()
+        // This could be improved to handle rollback of all mutations in handleAddVote()
+        rollbackOnError(err) {
+          console.error(err)
+          return true
+        }
+      }
+    )
+  }
+
+  /*
+    This function is called when a user clicks on a popular tag to add their vote.
+  */
+  function handleCreateVotableTag(tag) {
+    // Note: Don't mutate votableTags in this function in order to preserve the order of combinedTags
+
+    // Mutate popularTags by adding a new tag object that has vote_count (a property of a votable tag),
+    // so it prevents a shift in the tags list. This is purely an optimistic UI strategy, no network requests.
+    const modifiedTag = {
+      ...tag,
+      vote_count: 1
+    }
+    mutatePopularTags(
+      prevData => {
+        if (!prevData) {
+          return [modifiedTag]
+        } else {
+          return prevData.map(t => {
+            if (t.name === tag.name) {
+              return modifiedTag
+            }
+            return t
+          })
+        }
+      },
+      { revalidate: false } // don't need to revalidate any request to /tags/popular
+    )
+
+    mutateTagVotes(
+        async (prevData) => {
+          const newVotableTag = {
+            name: tag.name,
+            section: `project-${projectId}`,
+            taggable_id: subjectId,
+            taggable_type: 'Subject'
+          }
+          // Set a loading state while the request is in progress to prevent an attempt to delete the tagVote before it's created in the database.
+          setVoteUpdating(true)
+          await createVotableTag(newVotableTag)
+          setVoteUpdating(false)
+
+          // whatever is returned from this async function replaces tagVotes
+          // so optimistically update the tagVotes array before revalidation
+          if (!prevData) {
+            return [{ votable_tag: { ...tag } }]
+          } else {
+            return [...prevData, { votable_tag: { ...tag } }]
+          }
+        },
+        {
+        // optimistically update the tagVotes array before revalidation
+        // this tagVote object will be missing votable_tag_id until tagVotes is revalidated
+        optimisticData: prevData => {
+          if (!prevData) {
+            return [{ votable_tag: { ...tag } }]
+          } else {
+            return [...prevData, { votable_tag: { ...tag } }]
+          }
+        },
+        // revalidate tagVotes once the async function createVotableTag() is successful
+        // this will replace the optimisticData in tagVotes, and we see the newly created tagVote
+        // from the database with votable_tag_id necessary for removeTagVote()
+        revalidate: true,
+        // rollbackOnError will respond to an error thrown in createVotableTag()
+        // This could be improved to handle rollback of all mutations in handleCreateVotableTag()
+        rollbackOnError(err) {
+          console.error(err)
+          return true
+        }
+      }
+    )
+  }
+
+  /*
+    When this function is called, we can assume the clicked Tag is a "votable tag"
+    and therefore exists in tagVotes and votableTags.
+  */
+  function handleRemoveVote(tag) {
+    const tagVoteToRemove = tagVotes?.find(vote => vote.votable_tag.name === tag.name)
+    if (!tagVoteToRemove || !tagVoteToRemove.id) return
+
+    // if the user is removing their vote from a popular tag, they must have just voted on a
+    // popular tag that intially had no votes,
+    // it's still in the popularTags array, so update the tag to show zero votes
+    if (tag.usages) {
+      mutatePopularTags(
+        prevData => {
+          if (!prevData) {
+            return []
+          } else {
+            return prevData.map(t => {
+              if (t.id === tag.id) {
+                return {
+                  ...t,
+                  vote_count: 0
+                }
+              }
+              return t
+            })
+          }
+        },
+        { revalidate: false } // don't need to revalidate any request to /tags/popular
+      )
+    } else {
+      // Modify vote_count of the tag in the votableTags array
+      mutateVotableTags(
+        prevData => {
+          if (!prevData) {
+            return []
+          } else {
+            return prevData.map(t => {
+              if (t.id === tag.id) {
+                return {
+                  ...t,
+                  vote_count: t.vote_count > 0 ? t.vote_count - 1 : 0
+                }
+              }
+              return t
+            })
+          }
+        },
+        { revalidate: false } // don't need to revalidate votableTags because optimistic data is enough
+      )
+    }
+
+    mutateTagVotes(
+      async (prevData) => {
+        setVoteUpdating(true)
+        // remove the "tag vote" from the database and the database changes the relevant votableTag's vote_count to 0
+        await removeTagVote(tagVoteToRemove.id)
+        setVoteUpdating(false)
+
+        // this async function must return something to put into the tagVotes data array
+        // so remove the tag vote from the tagVotes array
+        if (!prevData) {
+          return []
+        } else {
+          return prevData.filter(
+            vote => vote.id !== tagVoteToRemove.id
+          )
+        }
+      },
+      {
+        // optimistically remove the tag vote from the tagVotes array before the network request happens
+        optimisticData: prevData => {
+          if (!prevData) {
+            return []
+          } else {
+            return prevData.filter(
+              vote => vote.id !== tagVoteToRemove.id
+            )
+          }
+        },
+        // don't need to revalidate tagVotes because optimisticData is enough
+        revalidate: false,
+        // rollbackOnError will respond to an error thrown by removeTagVote()
+        // tagVotes will be rolled back, but the above mutations to popularTags or votableTags will not,
+        // this could be improved to rollback all mutations in handleRemoveVote()
+        rollbackOnError(err) {
+          console.error(err)
+          return true
+        }
+      }
+    )
+  }
+
+  function handleClick(tag) {
+    if (!tag.vote_count) {
+      handleCreateVotableTag(tag) // add vote to popular tag
+    } else if (tag.userVoted) {
+      handleRemoveVote(tag) // remove vote is the same process regardless of tag type
+    } else {
+      handleAddVote(tag) // add vote to votable tag
+    }
+  }
+
+  return (
+    <Tags
+      loading={popularTagsIsLoading || votableTagsIsLoading || tagVotesIsLoading}
+      error={popularTagsError || votableTagsError || tagVotesError}
+      tags={combinedTags}
+      onTagClick={handleClick}
+      userId={userId}
+      voteUpdating={voteUpdating || tagVotesIsValidating}
+    />
+  )
+}
+
+TagsContainer.propTypes = {
+  projectId: string,
+  subjectId: string,
+  userId: string
+}
+
+export default TagsContainer
