97be39b802bf1d5e4089732a5d35b554f3de9852
Mark Bouslog <mark@zooniverse.org>
2025-09-30 12:40:40 -0500

    Add styling for AddTagModal

--- packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Tags/components/AddTagModal/AddTagModal.jsx
+++ packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Tags/components/AddTagModal/AddTagModal.jsx
@@ -1,8 +1,39 @@
 import { Modal, SpacedText } from '@zooniverse/react-components'
 import { Box, Button, Form, FormField, TextInput } from 'grommet'
 import { useState } from 'react'
+import styled from 'styled-components'
 
 import TagList from '../TagList'
+
+const StyledButton = styled(Button)`
+  border-radius: 4px;
+  border: ${props => props.theme.dark ?
+    `0.5px solid #E0D4F6` 
+    : `0.5px solid #4D2A8E`
+  };
+  color: ${props => props.theme.dark ?
+    '#E0D4F6'
+    : '#4D2A8E'
+  };
+  height: 40px;
+  min-width: 110px;
+`
+
+const StyledFormField = styled(FormField)`
+  flex: 1;
+  margin: 0;
+
+  label {
+    margin-left: 0;
+  }
+`
+
+const StyledTextInput = styled(TextInput)`
+  border-radius: 4px;
+  border: 1px solid #A6A7A9;
+  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, 0.25) inset;
+  height: 40px;
+`
 
 function AddTagModal({
   active = false,
@@ -28,18 +59,20 @@ function AddTagModal({
   return (
     <Modal
       active={active}
+      bodyBackground='transparent'
       closeFn={onClose}
       headingBackground='transparent'
+      pad={{ horizontal: 'small', top: 'xsmall', bottom: 'medium' }}
       role='dialog'
-      round='8px'
-      title='Add a Popular Tag'
+      title='Add a Tag'
       titleColor='black'
     >
       <Box
-        gap='small'
+        gap='xsmall'
+        width='600px'
       >
         <SpacedText size='small'>
-          {`From ${projectDisplayName}`}
+          {`Popular Tags From ${projectDisplayName}`}
         </SpacedText>
         <TagList
           disabled={disabled}
@@ -49,28 +82,38 @@ function AddTagModal({
         <Form
           onSubmit={handleSubmit}
         >
-            <FormField
-              label='Add a tag'
+          <Box
+            align='end'
+            direction='row'
+            gap='small'
+          >
+            <StyledFormField
               htmlFor='tag-name'
+              label={<SpacedText>Create a new tag</SpacedText>}
               name='tag-name'
               validate={[
                 (name) => {
-                  if (name && !/^[\w-]{3,40}$/.test(name)) {
+                  if (!name || name && !/^[\w-]{3,40}$/.test(name)) {
                     return 'Use 3-40 characters, letters, numbers, hyphens, and underscores. No spaces or other characters.'
                   }
                   return undefined
                 }
               ]}
               validateOn='submit'
+              // style={{ width: '100%' }}
             >
-              <TextInput
+              <StyledTextInput
                 id='tag-name'
                 name='tag-name'
                 onChange={handleChange}
                 value={newTagName}
               />
-            </FormField>
-            <Button type='submit' label='submit' />
+            </StyledFormField>
+            <StyledButton
+              label='Add'
+              type='submit'
+            />
+          </Box>
         </Form>
       </Box>
     </Modal>
---
+++ packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Tags/components/AddTagModal/AddTagModal.stories.jsx
@@ -0,0 +1,37 @@
+import { Box } from 'grommet'
+
+import AddTagModal from './AddTagModal'
+
+export default {
+  title: 'Project App / Screens / Subject Talk / Talk Data / Tags / AddTagModal',
+  component: AddTagModal,
+  decorators: [(Story) => (
+    <Box
+      pad='large'
+    >
+      <Story />
+    </Box>
+  )]
+}
+
+export const Default = {
+  args: {
+    active: true,
+    disabled: false,
+    onClose: () => console.log('closing modal'),
+    onTagClick: (tag) => console.log('tag clicked', tag),
+    projectDisplayName: 'Redshift Wrangler',
+    tags: [
+      { id: '1', name: 'emission-line' },
+      { id: '2', name: 'emission_line' },
+      { id: '3', name: 'no_emission_absorption_lines' },
+      { id: '4', name: 'emission_lines' },
+      { id: '5', name: 'sky_lines' },
+      { id: '6', name: 'noise' },
+      { id: '7', name: 'absorption_line' },
+      { id: '8', name: 'absorption_lines' },
+      { id: '9', name: 'chip_gap' },
+      { id: '10', name: 'serendipitous_object' }
+    ]
+  }
+}
