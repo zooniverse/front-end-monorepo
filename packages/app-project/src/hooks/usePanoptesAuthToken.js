53938a08ee52f7ac53af02a76be4a51f930a3799
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2025-10-01 10:16:53 -0500

    Build and run with Node 22 (#7055)
    * build and run FEM with Node 22
    
    * Ignore sugarClient during SSR (#7056)
    
    * update comments about top-level await
    
    ---------
    
    Co-authored-by: Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>

--- packages/app-project/src/hooks/usePanoptesAuthToken.js
+++ packages/app-project/src/hooks/usePanoptesAuthToken.js
@@ -7,8 +7,8 @@ let defaultToken
 /*
   See comments in https://github.com/zooniverse/front-end-monorepo/pull/6345
   Top-level await in modules has been supported in Node
-  and in all browsers since 2021. However, ES modules are still
-  not supported in the monorepo. An immediately-invoked async
+  and in all browsers since 2021, and only in ESM.
+  However, app-project still has a mix of CJS and  ESM. An immediately-invoked async
   function is a workaround when top-level await is not supported.
   https://v8.dev/features/top-level-await
 */
