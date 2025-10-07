887dc3b3da7e7156ea79f525f385e414c552b04e
Mark Bouslog <mark@zooniverse.org>
2025-10-07 01:21:59 -0500

    Refactor StartDiscussionModal styling

--- packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Discussions/components/StartDiscussionModal/StartDiscussionModal.jsx
+++ packages/app-project/src/screens/SubjectTalkPage/components/SubjectTalkData/components/Discussions/components/StartDiscussionModal/StartDiscussionModal.jsx
@@ -1,9 +1,26 @@
 import { Loader, Modal, SpacedText } from '@zooniverse/react-components'
-import { Box, Button, Form, FormField, RadioButtonGroup, TextArea, TextInput } from 'grommet'
+import { Box, Button, Form, FormField, RadioButtonGroup, Text, TextArea, TextInput, ThemeContext } from 'grommet'
 import { CircleInformation } from 'grommet-icons'
 import { useTranslation } from 'next-i18next'
 import { arrayOf, bool, func, shape, string } from 'prop-types'
 import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const StyledRadioButtonGroup = styled(RadioButtonGroup)`
+  & > label > div {
+    margin-right: 5px;
+  }
+`
+
+const StyledTextArea = styled(TextArea)`
+  height: 132px;
+`
+
+const StyledButton = styled(Button)`
+  border-radius: 4px;
+  font-size: 1rem;
+  width: 300px;
+`
 
 const DEFAULT_HANDLER = () => true
 
@@ -40,7 +57,16 @@ function StartDiscussionModal({
   }, [boards])
 
   const boardOptions = boards?.map(board => ({
-    label: board.title,
+    label: (
+      <Text
+        color={{ dark: 'light-1', light: 'dark-4' }}
+        margin='none'
+        pad='none'
+        size='.875rem'
+      >
+        {board.title}
+      </Text>
+    ),
     value: board.id
   })) || []
   const selectedBoard = boards?.find(board => board.id === value.discussion_board)
@@ -85,78 +111,117 @@ function StartDiscussionModal({
       <Box
         gap='small'
         width='600px'
-      >{error ? (
+      >
+        {error ? (
           <Box align='center' justify='center' fill pad='medium'>
             <SpacedText uppercase={false}>
               {t('Talk.Tags.somethingWentWrong')}
             </SpacedText>
           </Box>
-      ) : loading ? (
-        <Box align='center' justify='center' fill pad='medium'>
-          <Loader />
-        </Box>
-      ) : (
-        <Form
-          onChange={handleChange}
-          onSubmit={handleSubmit}
-          value={value}
-        >
-          <FormField
-            htmlFor='discussion_board'
-            label={<SpacedText>{t('Talk.Discussions.discussionBoard')}</SpacedText>}
-            name='discussion_board'
-            required
-          >
-            <RadioButtonGroup
-              name='discussion_board'
-              options={boardOptions}
-            />
-          </FormField>
-          {showCommentMessage && (
-            <Box
-              direction='row'
-              gap='xxsmall'
-              align='center'
-            >
-              <CircleInformation size='0.75rem' />
-              <SpacedText
-                size='xsmall'
-                uppercase={false}
-              >
-                {t('Talk.Discussions.commentMessage')}
-              </SpacedText>
-            </Box>
-          )}
-          <FormField
-            disabled={selectedBoard?.subject_default}
-            htmlFor='discussion_title'
-            label={<SpacedText>{t('Talk.Discussions.discussionTitle')}</SpacedText>}
-            name='discussion_title'
-            required
-          >
-            <TextInput disabled={selectedBoard?.subject_default} name='discussion_title' />
-          </FormField>
-          <FormField
-            htmlFor='discussion_comment'
-            label={<SpacedText>{commentInputLabel}</SpacedText>}
-            name='discussion_comment'
-            required
-          >
-            <TextArea name='discussion_comment' />
-          </FormField>
-          <Box
-            align='center'
-            direction='row'
-            justify='end'
-          >
-            <Button
-              label={submitButtonLabel}
-              primary
-              type='submit'
-            />
+        ) : loading ? (
+          <Box align='center' justify='center' fill pad='medium'>
+            <Loader />
           </Box>
-        </Form>
-      )}
+        ) : (
+          <Form
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            value={value}
+          >
+            <Box gap='small'>
+              <ThemeContext.Extend value={{
+                formField: {
+                  border: false,
+                  label: { margin: { horizontal: 'none', top: 'none' }},
+                  margin: 'none'
+                },
+              }}>
+                <FormField
+                  htmlFor='discussion_board'
+                  label={<SpacedText>{t('Talk.Discussions.discussionBoard')}</SpacedText>}
+                  name='discussion_board'
+                  required
+                >
+                  <StyledRadioButtonGroup
+                    direction='row'
+                    name='discussion_board'
+                    options={boardOptions}
+                  />
+                </FormField>
+              </ThemeContext.Extend>
+              {showCommentMessage && (
+                <Box
+                  align='center'
+                  direction='row'
+                  gap='xxsmall'
+                >
+                  <CircleInformation size='0.75rem' />
+                  <SpacedText
+                    size='xsmall'
+                    uppercase={false}
+                  >
+                    {t('Talk.Discussions.commentMessage')}
+                  </SpacedText>
+                </Box>
+              )}
+              <ThemeContext.Extend value={{
+                formField: {
+                  border: {
+                    color: 'light-5',
+                    side: 'all',
+                    size: '1px'
+                  },
+                  label: { margin: { horizontal: 'none', top: 'none' }},
+                  margin: 'none'
+                }
+              }}>
+                <FormField
+                  contentProps={{ round: '4px' }}
+                  disabled={selectedBoard?.subject_default}
+                  htmlFor='discussion_title'
+                  label={<SpacedText>{t('Talk.Discussions.discussionTitle')}</SpacedText>}
+                  name='discussion_title'
+                  required
+                >
+                  <TextInput disabled={selectedBoard?.subject_default} name='discussion_title' />
+                </FormField>
+              </ThemeContext.Extend>
+              <ThemeContext.Extend value={{
+                formField: {
+                  border: {
+                    color: 'light-5',
+                    side: 'all',
+                    size: '0.5px'
+                  },
+                  label: { margin: { horizontal: 'none', top: 'none' }},
+                  margin: 'none'
+                }
+              }}>
+                <FormField
+                  contentProps={{ round: '4px' }}
+                  htmlFor='discussion_comment'
+                  label={<SpacedText>{commentInputLabel}</SpacedText>}
+                  name='discussion_comment'
+                  required
+                >
+                  <StyledTextArea name='discussion_comment' plain />
+                </FormField>
+              </ThemeContext.Extend>
+              <Box
+                align='center'
+                direction='row'
+                justify='end'
+              >
+                <StyledButton
+                  color={{ light: 'neutral-1', dark: 'accent-1' }}
+                  label={submitButtonLabel}
+                  primary
+                  type='submit'
+                />
+              </Box>
+            </Box>
+          </Form>
+        )}
       </Box>
     </Modal>
   )
