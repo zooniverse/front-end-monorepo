a3238a53845efae3c164ffa96b7c8fc9174b89a7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-10-14 13:09:57 -0500

    GeoMapViewer: set new default coords, add notes on projections and coordinates

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
@@ -9,31 +9,21 @@ function GeoMapViewer (props) {
   const olMapId = useId()
   let olMap = undefined
 
-  // Projections??
-  // --------------------------------
-  // When the map loads, we want to look at the Denys Wilkinson Building, in
-  // the University of Oxford.
-  // Challenge: we need to decide which projection we want to use.
+  // Note on projections:
+  // By default, OpenLayers uses the Web Mercator (EPSG:3857) projection, which
+  // is used by most web mapping apps. (e.g. Google Maps)
 
-  // By default, OpenLayers uses the Web Mercator (EPSG:3857) projection.
-  // Pro: the standard used by most web mapping apps, e.g. Google Maps.
-  //      Map looks nice.
-  // Con: matching web mercator coordinate to GPS coodinates is wonky. For
-  //      example, DWB's 51.7595ºN, 1.2595ºW had to be manually guesstimated
-  //      to 6756800, -140200 to get this example to work.
+  // Web Mercator's units are in metres, while GPS coordinates (WGS84, or
+  // EPSG:4326) use degrees. We'll need to convert common latitude/longitude
+  // coordinates for use on our map.
+
+  // e.g. Buckingham Palace is at latitude 51°30'5.94" N and longitude
+  // 0°8'30.66" W (51.50165, -0.14185), which converts to 
+  // x=-15790.669769025857 and y=6710514.145334988
+
   const webMercatorView = new View({
-    center: [-140200, 6756800],  // longitude (West is negative, East is positive), latitude (South is negative, North is positive)
-    zoom: 18,
-  })
-
-  // GPS coordinates use the general WGS84 (aka EPSG:4326) projection.
-  // (For bonus confusion, Web Mercator is a subset/variant of WGS84)
-  // Pro: much easier to use GPS coordinates.
-  // Con: map looks preeetty stretched out when viewing more northern/southern locations.
-  const wgs84View = new View({
-    center: [-1.2595, 51.7595], // 51.7595ºN, 1.2595ºW
-    projection: 'EPSG:4326',
-    zoom: 18,
+    center: [0, 0],  // longitude (West is negative, East is positive), latitude (South is negative, North is positive)
+    zoom: 1,
   })
 
   // Render the map!
@@ -58,30 +48,10 @@ function GeoMapViewer (props) {
 
   // --------------------------------
 
-  function selectProjection (e) {
-    const projection = e?.currentTarget.dataset.projection
-
-    switch (projection) {
-      case 'webMercator':
-        olMap?.setView(webMercatorView)
-        break
-
-      case 'wgs84':
-        olMap?.setView(wgs84View)
-        break
-    }
-    
-  }
-
   return (
     <div>
       <OpenLayersCss />
       <h6 style={{ margin: '0.25em', padding: 0 }}>OpenLayersGeoMap</h6>
-      <div style={{ display: 'flex' }}>
-        <b>Projection:</b>
-        <button onClick={selectProjection} data-projection="webMercator">Web Mercator (default)</button>
-        <button onClick={selectProjection} data-projection="wgs84">WGS84 (GPS)</button>
-      </div>
       <div
         id={olMapId}
         style={{
