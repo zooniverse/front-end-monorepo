adcaa16b2814ce896cd9cc32e9dc3db90efab571
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-10-14 14:27:33 -0500

    GeoMapViewer: increase height

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
@@ -83,7 +83,7 @@ function GeoMapViewer ({
         id={olMapId}
         style={{
           width: '100%',
-          height: '400px'
+          height: '500px'
         }}
       ></div>
     </div>
