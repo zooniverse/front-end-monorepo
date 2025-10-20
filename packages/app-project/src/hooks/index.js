faa9d9be7315739d0bc33eae82346ff3f5f998c5
Mark Bouslog <mark@zooniverse.org>
2025-10-20 10:38:10 -0500

    Add useMentions hook

--- packages/app-project/src/hooks/index.js
+++ packages/app-project/src/hooks/index.js
@@ -1,6 +1,7 @@
 export { default as useAdminMode } from './useAdminMode.js'
 export { default as useComments } from './useComments.js'
 export { default as useDiscussions } from './useDiscussions.js'
+export { default as useMentions } from './useMentions.js'
 export { default as useUserFavourites } from './useUserFavourites.js'
 export { default as usePanoptesAuth } from './usePanoptesAuth.js'
 export { default as usePanoptesAuthToken } from './usePanoptesAuthToken.js'
