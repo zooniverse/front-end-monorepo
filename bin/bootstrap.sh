ba893db8106b7a30b45e955dd06798ccc19338b4
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-10-22 15:54:30 -0500

    Use pnpm to bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # Script for bootstrapping the monorepo into a working state for development.
-#
+
 # Runs the following tasks in order:
 #   - Install dependencies
 #   - Build `@zooniverse/react-components`
@@ -10,37 +10,19 @@ set -ev
 #   - Build `@zooniverse/lib-user`
 #   - Build `@zooniverse/lib-subject-viewers`
 #   - Build `@zooniverse/lib-classifier`
+#   - Build `@zooniverse/fe-project`
+#   - Build `@zooniverse/fe-root`
 
 
 ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd)"
 cd $ROOT_DIR
 
-printf 'Building `lib-react-components`...\n'
-yarn workspace @zooniverse/react-components install --frozen-lockfile
-printf '\n'
+printf 'Installing dependencies...\n'
+# All `prepare` scripts are run in this step.
+pnpm install
 
-printf 'Building `lib-content`...\n'
-yarn workspace @zooniverse/content install --frozen-lockfile
-printf '\n'
-
-printf 'Building `lib-user`...\n'
-yarn workspace @zooniverse/user install --frozen-lockfile
-printf '\n'
-
-printf 'Building `lib-subject-viewers`...\n'
-yarn workspace @zooniverse/subject-viewers install --frozen-lockfile
-printf '\n'
-
-printf 'Building `lib-classifier`...\n'
-yarn workspace @zooniverse/classifier install --frozen-lockfile
-printf '\n'
-
-printf 'Building `fe-project`...\n'
-yarn workspace @zooniverse/fe-project build
-printf '\n'
-
-printf 'Building `fe-root`...\n'
-yarn workspace @zooniverse/fe-root build
-printf '\n'
+# Then build the apps:
+pnpm --filter @zooniverse/fe-project build
+pnpm --filter @zooniverse/fe-root build
 
 echo 'Done!'
