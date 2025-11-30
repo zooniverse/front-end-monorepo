c8220fd57cac5abd67038663ee66edba9ace0fff
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2025-11-30 17:56:54 -0600

    upgrade app-project to Storybook v9

--- packages/app-project/.storybook/preview.jsx
+++ packages/app-project/.storybook/preview.jsx
@@ -1,9 +1,8 @@
 import { initialize, mswLoader } from 'msw-storybook-addon'
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { INITIAL_VIEWPORTS } from 'storybook/viewport'
 import i18n from './lib/i18n'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Grommet } from 'grommet'
-import { StrictMode } from 'react'
 
 const background = {
   dark: 'dark-1',
@@ -13,58 +12,57 @@ const background = {
 // Initialize MSW
 initialize({
   serviceWorker: {
-    url: './mockServiceWorker.js',
+    url: './mockServiceWorker.js'
   },
-  onUnhandledRequest: 'bypass',
+  onUnhandledRequest: 'bypass'
 })
-
-const globalTypes = {
-  theme: {
-    name: 'Grommet Theme',
-    description: 'Global Grommet theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'arrowdown',
-      items: ['light', 'dark'],
-      title: 'Grommet Theme',
-    }
-  }
-}
 
 const decorators = [
   (Story, context) => {
     return (
-      <StrictMode>
-        <Grommet
-          background={background}
-          theme={zooTheme}
-          themeMode={context.globals.theme}
-          full
-        >
-          <Story />
-        </Grommet>
-      </StrictMode>
+      <Grommet
+        background={background}
+        theme={zooTheme}
+        themeMode={context.globals.theme}
+        full
+      >
+        <Story />
+      </Grommet>
     )
   }
 ]
 
 const preview = {
-  globals: {
+  docs: {
+    tags: ['autodocs']
+  },
+  initialGlobals: {
     locale: 'en',
     locales: {
       en: 'English',
       test: 'Test Language'
     },
+    theme: 'light'
   },
   parameters: {
     i18n,
     layout: 'fullscreen',
     viewport: {
-      viewports: INITIAL_VIEWPORTS
+      options: INITIAL_VIEWPORTS
     }
   },
   decorators,
-  globalTypes,
+  globalTypes: {
+    theme: {
+      name: 'Grommet Theme',
+      description: 'Global Grommet theme for components',
+      toolbar: {
+        icon: 'arrowdown',
+        items: ['light', 'dark'],
+        title: 'Grommet Theme'
+      }
+    }
+  },
   loaders: [mswLoader]
 }
 
