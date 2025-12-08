233bb5a30ac674a2ab1012861720d76fc5465992
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2025-12-08 17:08:53 -0500

    Upgrade to Storybook 9 (#7127)
    * upgrade app-project to Storybook v9
    
    * upgrade to Storybook 9 in all packages
    
    * clean up autodocs and strictMode
    
    * fix ref to `process` in lib-react-components' Storybook config

--- packages/app-root/src/app/groups/[groupId]/projects/page.js
+++ packages/app-root/src/app/groups/[groupId]/projects/page.js
@@ -5,7 +5,10 @@ export const metadata = {
   description: 'Zooniverse group all projects page'
 }
 
-export default function GroupAllProjectsPage({ params, searchParams }) {
+export default async function GroupAllProjectsPage(props) {
+  const params = await props.params
+  const searchParams = await props.searchParams
+
   return (
     <GroupAllProjectsContainer
       groupId={params.groupId}
--- packages/lib-content/.storybook/preview.jsx
+++ packages/lib-content/.storybook/preview.jsx
@@ -1,7 +1,6 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { INITIAL_VIEWPORTS } from 'storybook/viewport'
 import zooTheme from '@zooniverse/grommet-theme'
 import { Grommet } from 'grommet'
-import { StrictMode } from 'react'
 
 import i18n from '../src/translations/i18n'
 
@@ -10,52 +9,48 @@ const background = {
   light: 'light-1'
 }
 
-const globalTypes = {
-  theme: {
-    name: 'Grommet Theme',
-    description: 'Global Grommet theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'arrowdown',
-      items: ['light', 'dark'],
-      title: 'Grommet Theme',
-    }
-  }
-}
-
 const decorators = [
   (Story, context) => {
     return (
-      <StrictMode>
-        <Grommet
-          background={background}
-          theme={zooTheme}
-          themeMode={context.globals.theme}
-          full
-        >
-          <Story />
-        </Grommet>
-      </StrictMode>
+      <Grommet
+        background={background}
+        theme={zooTheme}
+        themeMode={context.globals.theme}
+        full
+      >
+        <Story />
+      </Grommet>
     )
   }
 ]
 
 const preview = {
-  globals: {
+  initialGlobals: {
     locale: 'en',
     locales: {
       en: 'English'
     },
+    theme: 'light'
   },
   parameters: {
     i18n,
     layout: 'fullscreen',
     viewport: {
-      viewports: INITIAL_VIEWPORTS
+      options: INITIAL_VIEWPORTS
     }
   },
   decorators,
-  globalTypes,
+  globalTypes: {
+    theme: {
+      name: 'Grommet Theme',
+      description: 'Global Grommet theme for components',
+      toolbar: {
+        icon: 'arrowdown',
+        items: ['light', 'dark'],
+        title: 'Grommet Theme'
+      }
+    }
+  }
 }
 
 export default preview
