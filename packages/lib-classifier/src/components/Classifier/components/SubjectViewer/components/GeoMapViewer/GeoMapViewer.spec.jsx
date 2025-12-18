9a8fc4c7abb2533b766b05e040bc47954dffaa76
Mark Bouslog <mcbouslog@gmail.com>
2025-12-17 19:00:29 -0600

    lib-classifier: Refactor GeoMapViewer with GeoJSON features (#7142)
    * Add GeoMapViewer stories with GeoJSON
    
    * Refactor GeoMapViewer for geoJSON features
    
    * Add GeoMapViewer tests
    
    * Refactor GeoMapViewer propTypes
    
    * Refactor geoJSON features update

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.spec.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.spec.jsx
@@ -1,16 +1,50 @@
 import { composeStory } from '@storybook/react'
-import { render, waitFor } from '@testing-library/react'
-import Meta, { Default } from './GeoMapViewer.stories'
+import { render, screen, waitFor } from '@testing-library/react'
+import Meta, { Default, EiffelTower } from './GeoMapViewer.stories'
+
+const DefaultStory = composeStory(Default, Meta)
+const EiffelTowerStory = composeStory(EiffelTower, Meta)
 
 describe('Component > GeoMapViewer', function () {
   describe('default map', function () {
     it('should show the map container', async function () {
-      const MockGeoMapViewer = composeStory(Default, Meta)
-      render(<MockGeoMapViewer />)
-      const mapContainer = document.querySelector('.map-container')
+      render(<DefaultStory />)
       await waitFor(() => {
-        expect(mapContainer).to.exist
+        expect(screen.getByTestId('geo-map-container')).to.exist
       })
+    })
+  })
+
+  describe('with geoJSON data', function () {
+    it('should show the map container with geoJSON features', async function () {
+      render(<EiffelTowerStory />)
+      
+      await waitFor(() => {
+        expect(screen.getByTestId('geo-map-container')).to.exist
+      })
+    })
+
+    it('should add a vector layer when geoJSON is provided', async function () {
+      render(<EiffelTowerStory />)
+      
+      await waitFor(() => {
+        const mapContainer = screen.getByTestId('geo-map-container')
+        // OpenLayers creates viewport div when layers are added
+        const viewport = mapContainer.querySelector('.ol-viewport')
+        expect(viewport).to.exist
+      })
+    })
+
+    it('should cleanup the map on unmount', async function () {
+      const { unmount } = render(<EiffelTowerStory />)
+      
+      await waitFor(() => {
+        expect(screen.getByTestId('geo-map-container')).to.exist
+      })
+
+      unmount()
+      
+      expect(screen.queryByTestId('geo-map-container')).to.not.exist
     })
   })
 })
--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.stories.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.stories.jsx
@@ -8,3 +8,64 @@ export default {
 export const Default = {
   args: {},
 }
+
+export const EiffelTower = {
+  args: {
+    geoJSON: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [2.2944810, 48.8583701]
+          },
+          properties: {
+            name: 'Eiffel Tower'
+          }
+        }
+      ]
+    }
+  }
+}
+
+export const StatueOfLiberty = {
+  args: {
+    geoJSON: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-74.0445004, 40.6892494]
+          },
+          properties: {
+            name: 'Statue of Liberty'
+          }
+        }
+      ]
+    }
+  }
+}
+
+export const FourCornersMarker = {
+  args: {
+    geoJSON: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-109.0451723, 36.9989766]
+
+          },
+          properties: {
+            name: 'Four Corners Monument'
+          }
+        }
+      ]
+    }
+  }
+}
