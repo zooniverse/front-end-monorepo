17f2a6f88e3988e9447bd0f99a72db2a6d414343
Mark Bouslog <mcbouslog@gmail.com>
2025-12-17 19:00:29 -0600

    lib-classifier: Refactor GeoMapViewer with GeoJSON features (#7142)
    * Add GeoMapViewer stories with GeoJSON
    
    * Refactor GeoMapViewer for geoJSON features
    
    * Add GeoMapViewer tests
    
    * Refactor GeoMapViewer propTypes
    
    * Refactor geoJSON features update

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewer.jsx
@@ -1,7 +1,11 @@
 import { Box } from 'grommet'
 import { Map, View } from 'ol'
+import GeoJSON from 'ol/format/GeoJSON'
 import TileLayer from 'ol/layer/Tile'
+import VectorLayer from 'ol/layer/Vector'
 import OSM from 'ol/source/OSM'
+import VectorSource from 'ol/source/Vector'
+import { shape } from 'prop-types'
 import { useEffect, useRef } from 'react'
 import styled from 'styled-components'
 
@@ -11,42 +15,91 @@ const MapContainer = styled.div`
   width: 100%;
 `
 
-function GeoMapViewer() {
+function GeoMapViewer({
+  geoJSON = undefined
+}) {
+  // mapContainerRef: reference to the map container DOM element
+  const mapContainerRef = useRef()
+  // mapRef: reference to the OpenLayers Map instance
   const mapRef = useRef()
+  // featuresRef: reference to the OpenLayers VectorSource for GeoJSON features
+  const featuresRef = useRef()
+  // geoJSONFormatRef: reference to the OpenLayers GeoJSON format reader
+  const geoJSONFormatRef = useRef()
 
-  useEffect(function loadMap() {
-    let map
+  // Create the map once on mount
+  useEffect(function createMapOnce() {
+    // if the map is already created, do nothing
+    // or if the map container ref is not set yet, do nothing
+    if (mapRef.current || !mapContainerRef.current) return undefined
 
-    function createMap(target) {
-      const osmLayer = new TileLayer({
-        // preload tiles for 1 level of zooming
-        preload: 1,
-        // use OpenStreetMap as the base layer
-        source: new OSM(),
-      })
+    // create a GeoJSON format reader
+    const format = new GeoJSON()
+    geoJSONFormatRef.current = format
 
-      return new Map({
-        target,
-        layers: [osmLayer],
-        view: new View({
-          center: [0, 0],
-          zoom: 0,
-        }),
-      })
+    const osmLayer = new TileLayer({
+      // preload tiles for 1 level of zooming
+      preload: 1,
+      // use OpenStreetMap as the base layer
+      source: new OSM(),
+    })
+
+    // create a vector source and layer for the GeoJSON features
+    const featuresSource = new VectorSource({
+      features: []
+    })
+    featuresRef.current = featuresSource
+    const featuresLayer = new VectorLayer({
+      source: featuresSource
+    })
+
+    const map = new Map({
+      target: mapContainerRef.current,
+      layers: [
+        osmLayer,
+        featuresLayer
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 0,
+      }),
+    })
+
+    mapRef.current = map
+
+    return () => {
+      map.setTarget(undefined)
+      mapRef.current = undefined
+      featuresRef.current = undefined
+      geoJSONFormatRef.current = undefined
     }
-
-    function unloadMap(mapInstance) {
-      if (mapInstance) {
-        mapInstance.setTarget(undefined)
-      }
-    }
-
-    if (mapRef.current) {
-      map = createMap(mapRef.current)
-    }
-
-    return () => unloadMap(map)
   }, [])
+
+  // Update features when geoJSON changes, without recreating the map
+  useEffect(function updateFeatures() {
+    const map = mapRef.current
+    const features = featuresRef.current
+    // if the map or features source is not ready yet, do nothing
+    if (!map || !features) return undefined
+
+    // get or create the GeoJSON format reader
+    const format = geoJSONFormatRef.current || new GeoJSON()
+    geoJSONFormatRef.current = format
+
+    // clear existing features
+    features.clear()
+
+    if (geoJSON) {
+      // read and add new features from the provided GeoJSON
+      const geoJSONfeatures = format.readFeatures(geoJSON, {
+        dataProjection: 'EPSG:4326', // incoming GeoJSON coords in WGS 84
+        featureProjection: 'EPSG:3857' // map display projection in Web Mercator
+      })
+      features.addFeatures(geoJSONfeatures)
+    }
+
+    return undefined
+  }, [geoJSON])
 
   return (
     <Box
@@ -54,11 +107,16 @@ function GeoMapViewer() {
       fill
     >
       <MapContainer
-        ref={mapRef}
+        ref={mapContainerRef}
         className='map-container'
+        data-testid='geo-map-container'
       />
     </Box>
   )
+}
+
+GeoMapViewer.propTypes = {
+  geoJSON: shape({})
 }
 
 export default GeoMapViewer
