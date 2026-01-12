fa8ad65576d7469ff8d30f99b3c4a68bd149a927
Mark Bouslog <mcbouslog@gmail.com>
2026-01-12 14:03:58 -0600

    Refactor GeoMapViewerContainer per expected NfN subjects

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewerContainer.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/GeoMapViewer/GeoMapViewerContainer.jsx
@@ -31,19 +31,29 @@ function GeoMapViewerContainer ({
   onReady = DEFAULT_HANDLER
 }) {
   const { latest, subject } = useStores(storeMapper)
-  const { data, error, loading } = useSubjectJSON({
+  const { data, error, loading, type, viewer } = useSubjectJSON({
     onError,
     onReady,
     subject
   })
+  // Eventually, we will determine the geoJSON data from the subject's location, Caesar reductions, other sources, or a combination thereof.
+  // Requesting Caesar reductions might look like something like this:
+  // const { data: caesarData, error: caesarError, loading: caesarLoading } = useCaesarReductions({ subjectId: subject.id })
+  // For now, we are only loading geoJSON data from the subject JSON data, if applicable.
 
-  // Initialize the geoDrawing annotation with loaded GeoJSON data on first load.
+  let geoJSONData = null
+  
+  if (data && type?.name === 'GeoJSON') {
+    geoJSONData = data
+  }
+
+  // Initialize the geoDrawing annotation with GeoJSON data
   useEffect(() => {
     const geoAnnotation = latest?.annotations?.find(annotation => annotation.taskType === 'geoDrawing')
-    if (geoAnnotation && data && geoAnnotation.value.length === 0 && typeof geoAnnotation.update === 'function') {
-      geoAnnotation.update([data])
+    if (geoAnnotation && geoJSONData && geoAnnotation.value.length === 0) {
+      geoAnnotation.update([geoJSONData])
     }
-  }, [data, latest?.annotations])
+  }, [geoJSONData, latest?.annotations])
 
   if (loading) {
     return null
@@ -54,7 +64,7 @@ function GeoMapViewerContainer ({
 
   return (
     <GeoMapViewer
-      geoJSON={data}
+      geoJSON={geoJSONData}
       subjectId={subject.id}
     />
   )
