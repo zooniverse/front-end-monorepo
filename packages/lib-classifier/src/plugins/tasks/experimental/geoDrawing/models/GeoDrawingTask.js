7b24d068ed8975f6457c46526158e598e7009afe
Mark Bouslog <mcbouslog@gmail.com>
2026-01-19 12:31:36 -0600

    lib-classifier: Add GeoDrawingTask, GeoDrawingAnnotation, and GeoMapViewerContainer (#7178)
    * Add GeoMapViewer stories with GeoJSON
    
    * Refactor GeoMapViewer for geoJSON features
    
    * Add GeoMapViewer tests
    
    * Add GeoMapViewer interaction
    
    * Organize imports
    
    * Refactor point styling
    
    * Refactor GeoMapViewer to fit provided geoJSON features
    
    * Refactor GeoMapViewer with initial fit and recenter
    
    * Refactor select and translate interactions, add comments
    
    * Refactor GeoMapViewer to fit view to provided features
    
    * Add GeoMapViewer ControlButton and RecenterButton
    
    * Add RecenterButton test
    
    * Refactor GeoMapViewer with RecenterButton and fitViewToFeatures
    
    * Add GeoMapViewer test to conditionally show RecenterButton
    
    * Add RecenterButton translation
    
    * Add GeoMapViewer ResetButton with story
    
    * Add ResetButton test
    
    * Refactor GeoMapViewer with ResetButton, handleReset handler
    
    * Edit comments
    
    * Edit comments
    
    * Add initial GeoDrawingTask
    
    * Add initial GeoMapViewerConnector and GeoMapViewerContainer
    
    * Refactor JSONData with GeoJSON
    
    * Refactor GeoMapContainer to initialize geoDrawing annotation value with geoJSON
    
    * Add initial GeoDrawing task README
    
    * Add GeoDrawing annotation and task tests
    
    * Refactor JSONData tests for GeoJSON data
    
    * Refactor GeoMapViewerContainer comments, props
    
    * Refactor propTypes
    
    * Refactor GeoMapViewer with useStores
    
    * Revert changes to JSONDataViewer
    
    * Refactor useSubjectJSON to return raw JSON data if data does not match JSONData types
    
    * Refactor GeoMapViewerContainer per expected NfN subjects
    
    * Refactor GeoDrawingTask component comments
    
    * Add confirm modal to ResetButton
    
    * Remove unused tool properties
    
    * Refactor GeoJSON Geometry model types

---
+++ packages/lib-classifier/src/plugins/tasks/experimental/geoDrawing/models/GeoDrawingTask.js
@@ -0,0 +1,30 @@
+import cuid from 'cuid'
+import { types } from 'mobx-state-tree'
+import Task from '../../../models/Task'
+import GeoDrawingAnnotation from './GeoDrawingAnnotation'
+
+const GeoPointTool = types.model('GeoPointTool', {
+  label: types.string,
+  type: types.literal('geoPoint')
+})
+
+const GeoDrawing = types.model('GeoDrawing', {
+  annotation: types.safeReference(GeoDrawingAnnotation),
+  required: types.maybe(types.union(types.string, types.boolean)),
+  tools: types.array(GeoPointTool),
+  type: types.literal('geoDrawing')
+})
+  .views(self => ({
+    defaultAnnotation(id = cuid()) {
+      return GeoDrawingAnnotation.create({
+        id,
+        task: self.taskKey,
+        taskType: self.type,
+        value: []
+      })
+    }
+  }))
+
+const GeoDrawingTask = types.compose('GeoDrawingTask', Task, GeoDrawing)
+
+export default GeoDrawingTask
