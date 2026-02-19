c9022cdb4b3db02642beb380431b962dfb616d03
kieftrav <kiefertravis@gmail.com>
2026-02-19 07:49:30 -0600

    Fix 7168 scroll task area on navigation (#7218)
    * fix(classifier): scroll to task area on next/back/done navigation (#7168)
    * Accessibility improvements

--- packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskAreaConnector.jsx
+++ packages/lib-classifier/src/components/Classifier/components/TaskArea/TaskAreaConnector.jsx
@@ -13,15 +13,31 @@ function storeMapper(store) {
     },
     workflows: {
       active: workflow
+    },
+    workflowSteps: {
+      active: step,
+      steps
     }
   } = store
 
+  let currentStepIndex = 1
+  let totalSteps = 1
+
+  if (step?.stepKey && steps) {
+    const stepKeysArray = Array.from(steps.keys())
+    const currentIndex = stepKeysArray.indexOf(step.stepKey)
+    currentStepIndex = currentIndex + 1
+    totalSteps = stepKeysArray.length
+  }
+
   return {
     alreadySeen: subject?.alreadySeen,
+    currentStepIndex,
     disableTutorialTab,
     setActiveTutorial,
     retired: subject?.retired,
     subject,
+    totalSteps,
     tutorial,
     workflow
   }
