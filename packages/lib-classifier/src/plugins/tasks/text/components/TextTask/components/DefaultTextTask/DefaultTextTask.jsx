045fc4936228a23db02000a733bf8cbdb49a87cb
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-02-20 17:09:53 -0600

    reduce the default rows in TextTask's textarea element to 1

--- packages/lib-classifier/src/plugins/tasks/text/components/TextTask/components/DefaultTextTask/DefaultTextTask.jsx
+++ packages/lib-classifier/src/plugins/tasks/text/components/TextTask/components/DefaultTextTask/DefaultTextTask.jsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react'
-import { useRef } from 'react';
+import { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Text, TextArea } from 'grommet'
 import { Markdownz } from '@zooniverse/react-components'
@@ -11,6 +11,10 @@ const StyledText = styled(Text)`
   margin: 10px 0;
 `
 
+const StyledTextArea = styled(TextArea)`
+  padding: 7px;
+`
+
 function DefaultTextTask ({
   autoFocus = false,
   disabled = false,
@@ -19,10 +23,30 @@ function DefaultTextTask ({
   value,
   updateAnnotation = () => true
 }) {
-  const textArea = useRef()
+  const textAreaRef = useRef()
+  const [rows, setRows] = useState(1)
+
+  /*
+    On value change, get the <textarea>'s scrollHeight divided by height of one line
+    rounded down to the nearest integar. Without this function, <textarea>'s becomes a
+    scrollable container.
+
+    This only works as content is expanding, not if content is backspaced. In the future,
+    this resize can be improved with the CSS property `field-sizing` once supported.
+  */
+  function handleResize() {
+    const LINEHEIGHT = 23 // ~1rem + some vertical padding
+    const oldRows = rows
+    let newRows = textAreaRef.current ? Math.floor(textAreaRef.current.scrollHeight / LINEHEIGHT) : oldRows
+
+    if (newRows !== oldRows) {
+      setRows(newRows)
+    }
+  }
 
   function onChange() {
-    updateAnnotation(textArea)
+    updateAnnotation(textAreaRef)
+    handleResize()
   }
 
   const components = {
@@ -45,18 +69,19 @@ function DefaultTextTask ({
         htmlFor={`${task.taskKey}-${task.type}`}
       >
         <Markdownz components={components}>{task.instruction}</Markdownz>
-        <TextArea
-          ref={textArea}
+        <StyledTextArea
+          ref={textAreaRef}
           autoFocus={autoFocus}
           disabled={disabled}
           id={`${task.taskKey}-${task.type}`}
           value={value}
           onChange={onChange}
+          rows={rows}
         />
       </label>
       <TextTagButtons
         disabled={disabled}
-        onClick={(event) => setTagSelection(event, textArea)}
+        onClick={(event) => setTagSelection(event, textAreaRef)}
         taskKey={task.taskKey}
         tags={task.text_tags}
       />
