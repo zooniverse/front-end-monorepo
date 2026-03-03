a65610c9dae2aba8ffc834b33054f3482e0b58c0
Mark Bouslog <mcbouslog@gmail.com>
2026-03-03 10:24:13 -0600

    Add JSONViewer, with shared PreDisplay component from Text viewer

--- packages/lib-react-components/src/Media/components/Data/components/index.js
+++ packages/lib-react-components/src/Media/components/Data/components/index.js
@@ -1,2 +1,3 @@
 export { default as DataSeriesPlot } from './DataSeriesPlot'
+export { default as JSONViewer } from './JSONViewer'
 export { default as Volumetric } from './Volumetric'
--- packages/lib-react-components/src/Media/components/Text/Text.jsx
+++ packages/lib-react-components/src/Media/components/Text/Text.jsx
@@ -1,32 +1,6 @@
-import { Box } from 'grommet'
-import styled, { css, useTheme } from 'styled-components'
-
 import { propTypes, defaultProps } from '../../helpers/mediaPropTypes'
 import { useTextData } from '../../../hooks'
-
-const StyledBox = styled(Box)`
-  ${props => props.maxHeight && css`max-height: ${props.maxHeight}px;`}
-  ${props => props.maxWidth && css`max-width: ${props.maxWidth}px;`}
-`
-
-const StyledPre = styled.pre`
-  white-space: pre-wrap;
-  color: ${props => props.theme.dark ? 'white' : 'black'};
-  font-family: 'Anonymous Pro', monospace;
-  font-weight: 400;
-  @font-face {
-    font-family: 'Anonymous Pro';
-    font-style: normal;
-    font-weight: 400;
-    src:
-      local('Anonymous Pro'),
-      local('Anonymous Pro-Regular'),
-      url(https://fonts.gstatic.com/s/anonymouspro/v21/rP2Bp2a15UIB7Un-bOeISG3pHls29QP-4Ks.woff2) 
-      format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-  overflow-y: scroll;
-`
+import PreDisplay from '../PreDisplay'
 
 export default function Text({
   alt = defaultProps.alt,
@@ -37,7 +11,6 @@ export default function Text({
   width = defaultProps.width,
   ...rest
 }) {
-  const theme = useTheme()
   const { data, error, loading } = useTextData(src)
   let content = null
 
@@ -50,29 +23,14 @@ export default function Text({
   }
 
   if (data) {
-    content = data
+    content = <PreDisplay content={data} maxHeight={height} maxWidth={width} {...rest} />
   }
 
-  return (
-    <StyledBox
-      a11yTitle={alt}
-      flex={flex}
-      forwardedAs='section'
-      height='100%'
-      maxWidth={width}
-      maxHeight={height}
-      pad={{ horizontal: 'xsmall', top: 'xsmall', bottom: 'medium' }}
-      width='100%'
-      {...rest}
-    >
-      <StyledPre
-        tabIndex='0'
-        theme={theme}
-      >
-        {content}
-      </StyledPre>
-    </StyledBox>
-  )
+  return content
+}
+
+Text.propTypes = {
+  ...propTypes
 }
 
 Text.propTypes = {
