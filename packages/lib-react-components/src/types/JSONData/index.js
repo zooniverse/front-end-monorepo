80094b43297fc156f86ef101f501a822ebb56e49
Mark Bouslog <mcbouslog@gmail.com>
2026-03-10 17:08:16 -0500

    Add comments for similar JSON type models

--- packages/lib-react-components/src/types/JSONData/index.js
+++ packages/lib-react-components/src/types/JSONData/index.js
@@ -6,4 +6,8 @@ import GeoJSON from './GeoJSON'
 import TESSLightCurve from './TESSLightCurve'
 import Volumetric from './Volumetric'
 
+// NOTE: Union order is intentional. Similar, but not identical, to lib-classifier/src/store/JSONData/index.js.
+// MST union matching can be order-sensitive when model snapshots overlap,
+// so changing this order may change which model a payload resolves to.
+// This react-components package includes Volumetric in the union.
 export default types.union(GeoJSON, BarChart, DataSeriesPlot, Volumetric, TESSLightCurve)
