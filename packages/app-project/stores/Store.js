cd1595463c7d0b0fcdc918688cac2cb264e9b054
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-03-14 01:55:22 +0000

    Store: add store.organizations

--- packages/app-project/stores/Store.js
+++ packages/app-project/stores/Store.js
@@ -11,6 +11,7 @@ import User from './User'
 const Store = types
   .model('Store', {
     organization: types.optional(Organization, {}),
+    organizations: types.array(Organization),
     project: types.optional(Project, {}),
     ui: types.optional(UI, {}),
     user: types.optional(User, {})
