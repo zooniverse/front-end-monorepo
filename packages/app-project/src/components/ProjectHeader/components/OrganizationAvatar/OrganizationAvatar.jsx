2758b812623bc96c71310acb64a46abb8e0cd0fe
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-03-17 22:49:51 +0000

    Add OrganizationAvatar

---
+++ packages/app-project/src/components/ProjectHeader/components/OrganizationAvatar/OrganizationAvatar.jsx
@@ -0,0 +1,44 @@
+import { shape, string } from 'prop-types'
+import { observer } from 'mobx-react'
+import styled, { css } from 'styled-components'
+
+const StyledAvatar = styled.img`
+  border-radius: 100%;
+  box-shadow: 5px 10px 20px rgba(0, 0, 0, 0.22);
+  object-fit: cover;
+  overflow: hidden;
+  ${props => css`width: ${props.width};`}
+`
+
+const FALLBACK_AVATAR_SRC = ''
+
+function OrganizationAvatar({
+  organization,
+  width='30px',
+  ...rest
+}) {
+  if (!organization) {
+    return null
+  }
+
+  // TODO: maybe add a better description for alt, e.g. "Organization Avatar for XYZ". See <Avatar> component. Requires translations, though.
+  const alt = organization.display_name || ''
+  const src = organization.avatar?.src || FALLBACK_AVATAR_SRC
+  return (
+    <StyledAvatar alt={alt} src={src} width={width} {...rest} />
+  )
+}
+
+OrganizationAvatar.propTypes = {
+  /** Organization resource */
+  organization: shape({
+    title: string,
+    avatar: shape({
+      src: string
+    }),
+  }),
+  /** Avatar size in CSS units. */
+  width: string,
+}
+
+export default observer(OrganizationAvatar)
