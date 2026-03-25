bf869faaf00ce86d71a2628119850a75ecefb49e
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-03-24 19:10:19 -0500

    import ProjectStatistics shared component into the stats page

--- packages/app-project/src/screens/ProjectStatsPage/ProjectStatsPage.jsx
+++ packages/app-project/src/screens/ProjectStatsPage/ProjectStatsPage.jsx
@@ -2,11 +2,16 @@ import { useTranslation } from 'next-i18next'
 import { string } from 'prop-types'
 
 import StandardLayout from '@shared/components/StandardLayout'
+import ProjectStatistics from '@shared/components/ProjectStatistics'
 
 function ProjectStatsPage({ projectDisplayName, projectId }) {
   const { t } = useTranslation('screens')
 
-  return <StandardLayout />
+  return (
+    <StandardLayout>
+      <ProjectStatistics />
+    </StandardLayout>
+  )
 }
 
 ProjectStatsPage.propTypes = {
