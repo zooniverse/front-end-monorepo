6fb4ea295ddabbb57e48deeb2b9a8693e7a1679b
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-05-18 09:54:39 -0500

    modify addQueryParams to handle env and demo, only

--- packages/app-project/src/helpers/addQueryParams/addQueryParams.spec.js
+++ packages/app-project/src/helpers/addQueryParams/addQueryParams.spec.js
@@ -14,17 +14,32 @@ describe('Helper > addQueryParams', function () {
       })
     })
   })
-  describe('when there are query params', function () {
+
+  describe('when there are env or demo query params', function () {
     it('should return the url with the query parameters added', function () {
       global.dom.reconfigure({
-        url: 'https://localhost/foo/bar?baz=qux&page=4'
+        url: 'https://localhost/foo/bar?env=production&demo=true'
       })
       const url = '/different/page'
       const result = addQueryParams(url)
-      expect(result).to.equal(url + '?baz=qux&page=4')
+      expect(result).to.equal(url + '?env=production&demo=true')
       global.dom.reconfigure({
         url: 'https://localhost'
       })
     })
   })
-})
+
+  describe('when there are query params other than demo or env', function () {
+    it('should return the url without those params added', function () {
+      global.dom.reconfigure({
+        url: 'https://localhost/foo/bar?baz=qux&page=4'
+      })
+      const url = '/different/page'
+      const result = addQueryParams(url)
+      expect(result).to.equal(url)
+      global.dom.reconfigure({
+        url: 'https://localhost'
+      })
+      })
+    })
+  })
