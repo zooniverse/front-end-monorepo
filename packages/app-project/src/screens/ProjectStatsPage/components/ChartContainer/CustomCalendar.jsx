ecd2efbe78c474d9ec307e308316f2302efb744c
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-05-18 20:51:55 -0500

    Adding external selects to the CustomCalendar wip

--- packages/app-project/src/screens/ProjectStatsPage/components/ChartContainer/CustomCalendar.jsx
+++ packages/app-project/src/screens/ProjectStatsPage/components/ChartContainer/CustomCalendar.jsx
@@ -1,7 +1,7 @@
 import { Modal, MovableModal } from '@zooniverse/react-components'
 import { useContext, useState } from 'react'
 import { useTranslation } from 'next-i18next'
-import { Box, Button, Calendar, ResponsiveContext } from 'grommet'
+import { Box, Button, Calendar, ResponsiveContext, Select } from 'grommet'
 import styled from 'styled-components'
 import { bool, func, string } from 'prop-types'
 
@@ -14,9 +14,7 @@ const StyledCalendarButton = styled(Button)`
       : props.theme.global.colors['neutral-6']};
   border-radius: 4px;
   color: ${props =>
-    props.theme.dark
-      ? props.theme.global.colors['light-3']
-      : props.theme.global.colors['dark-5']};
+    props.theme.dark ? props.theme.global.colors['light-3'] : props.theme.global.colors['dark-5']};
   min-width: 100px;
   text-transform: uppercase;
 `
@@ -29,13 +27,16 @@ function CustomCalendar({
   setShowCalendar,
   showCalendar
 }) {
-  const { t } = useTranslation('screens')
+  const { t, i18n } = useTranslation('screens')
+  const locale = i18n.language === 'test' ? 'en' : i18n.language
 
-  const today = new Date()
-  const todayUTC = getStatsDateString(today)
   const size = useContext(ResponsiveContext)
   const CalendarModal = size === 'small' ? Modal : MovableModal
 
+  const today = new Date()
+  const todayUTC = getStatsDateString(today)
+
+  /* Calendar Selected Dates States */
   const [calendarStart, setCalendarStart] = useState(defaultStartDate)
   const [calendarEnd, setCalendarEnd] = useState(todayUTC)
 
@@ -43,8 +44,8 @@ function CustomCalendar({
     if (!date?.length) {
       return
     }
-    setCalendarEnd(date[0][1])
-    setCalendarStart(date[0][0])
+    setCalendarEnd(getStatsDateString(date[0][1]))
+    setCalendarStart(getStatsDateString(date[0][0]))
   }
 
   function handleCalendarSave() {
@@ -52,6 +53,50 @@ function CustomCalendar({
     setStartDate(calendarStart)
     setShowCalendar(false)
   }
+
+  /* Calendar Month or Year External Dropdowns */
+  const [reference, setReference] = useState(todayUTC) // reference is in ISO8601 format for Grommet Calendar
+  console.log(reference)
+
+  // Create an array of years in the specified range
+  const launch = new Date(launchDate)
+  const launchYear = +launch.toLocaleString(locale, { year: 'numeric' })
+  const thisYear = +today.toLocaleString(locale, { year: 'numeric' })
+
+  const years = Array.from({ length: thisYear - launchYear + 1 }, (_, index) => launchYear + index)
+
+  // Create an array of months based on locale
+  const months = Array.from({ length: 12 }, (_, i) => i)
+  const monthsFormatter = new Intl.DateTimeFormat(locale, { month: 'short' })
+  const localizedMonths = months.map(month => {
+    return {
+      value: month,
+      label: monthsFormatter.format(new Date(thisYear, month, 1))
+    }
+  })
+
+  // Determine what labels are selected in the dropdowns
+  const dropdownYear = +reference.slice(0, 4)
+  const dropdownMonth = localizedMonths.find(
+    month => month.value === new Date(reference).getMonth()
+  )
+
+  function handleOnReference(date) {
+    setReference(date)
+  }
+
+  function handleMonthSelect(option) {
+    // use the last day of each month (keeping with Grommet norms for onReference)
+    const newReference = new Date(dropdownYear, option.value + 1, 0).toISOString()
+    setReference(newReference)
+  }
+
+  function handleYearSelect(option) {
+    // use the last day of each month (keeping with Grommet norms for onReference)
+    const newReference = new Date(option, dropdownMonth?.value + 1, 0).toISOString()
+    setReference(newReference)
+  }
+
   return (
     <CalendarModal
       active={showCalendar}
@@ -71,20 +116,37 @@ function CustomCalendar({
           top: 'none'
         }}
       >
+        <Select
+          name='stats-month-select'
+          // aria-label={t('ProjectStats.selectDateRange')}
+          labelKey='label'
+          onChange={({ option }) => handleMonthSelect(option)}
+          options={localizedMonths}
+          value={dropdownMonth?.label}
+          size='medium'
+          valueKey={{ key: 'label', reduce: true }}
+        />
+        <Select
+          name='stats-year-select'
+          // aria-label={t('ProjectStats.selectDateRange')}
+          onChange={({ option }) => handleYearSelect(option)}
+          options={years}
+          value={dropdownYear}
+          size='medium'
+        />
         <Box>
           <Calendar
+            activeDate={reference}
+            animate={false}
             bounds={[getStatsDateString(launchDate), todayUTC]}
-            dates={[calendarStart, calendarEnd]}
+            // dates={[calendarStart, calendarEnd]}
+            // range='array'
+            reference={reference}
             onSelect={handleCalendarChange}
-            range='array'
+            onReference={date => handleOnReference(date)}
+            showAdjacentDays={false}
           />
-          <Box
-            direction='row'
-            fill
-            justify='end'
-            margin={{ top: 'xsmall' }}
-            // pad={{ horizontal: size === 'small' ? 'small' : 'none' }}
-          >
+          <Box direction='row' fill justify='end' margin={{ top: 'xsmall' }}>
             <StyledCalendarButton
               label={t('ProjectStats.calendarBtn')}
               onClick={handleCalendarSave}
