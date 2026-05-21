fa9b0fd1bd0d4e63aedde0463b4ddf4d4a76223a
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2026-05-20 21:09:26 -0500

    Fix EmptyPlaceholder's translation in the bar chart on Project Stats page (#7446)

--- packages/app-project/src/screens/ProjectStatsPage/components/Placeholders/EmptyPlaceholder.jsx
+++ packages/app-project/src/screens/ProjectStatsPage/components/Placeholders/EmptyPlaceholder.jsx
@@ -1,6 +1,9 @@
 import { Box, Paragraph } from 'grommet'
+import { useTranslation } from 'next-i18next'
 
 function EmptyPlaceholder() {
+  const { t } = useTranslation('screens')
+
   return (
     <Box fill align='center' pad={{ vertical: 'large' }}>
       <Paragraph>{t('ProjectStats.BarChart.empty')}</Paragraph>
