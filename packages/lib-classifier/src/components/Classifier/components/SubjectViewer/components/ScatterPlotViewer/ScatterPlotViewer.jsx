db66271ba0b0f17e7875dc5f426687256739fe28
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-05-27 17:25:22 +0100

    ScatterPlotViewer: allow filtering of data series. Add data series controls.

--- packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/ScatterPlotViewer/ScatterPlotViewer.jsx
+++ packages/lib-classifier/src/components/Classifier/components/SubjectViewer/components/ScatterPlotViewer/ScatterPlotViewer.jsx
@@ -5,24 +5,58 @@ import ZoomingScatterPlot from './components/ZoomingScatterPlot'
 import ScatterPlot from './components/ScatterPlot'
 import ZoomControlButton from '../ZoomControlButton'
 
+function DataSeriesControls ({ data }) {
+  return (
+    <ul className='ScatterPlotViewer-DataSeriesControls'>
+      {data?.map((dataSeries, index) => (
+        <li key={`TODO-${index}`}>{dataSeries.seriesOptions?.label?.trim() || `series ${index+1}`}</li>
+      ))}
+    </ul>
+  )
+}
+
 const ScatterPlotViewer = forwardRef(function ScatterPlotViewer (props, ref) {
   const {
     zoomControlFn,
     zooming = false
   } = props
+
+  // Is zooming enabled for this scatter plot?
   const Plot = (zooming) ? ZoomingScatterPlot : ScatterPlot
+
+  // Is this a multi-series data set?
+  // If it is, we'll show additional controls for showing/hiding and labelling
+  // each data series.
+  const indexesToHide = []
+  const { data: fullData } = props
+  const isMultiSeriesData = Array.isArray(fullData) && fullData?.length > 1
+  const data = (isMultiSeriesData)
+    ? fullData.filter((d, index) => !indexesToHide.includes(index))
+    : fullData
+
+  // Repackage the props with the (filtered-if-applicable) `data` value.
+  const newProps = {
+    ...props,
+    data
+  }
 
   return (
     <ParentSize>
       {(parent) => (
         <>
-          {zoomControlFn &&
-            <ZoomControlButton onClick={zoomControlFn} position='absolute' zooming={zooming} />}
+          {zoomControlFn && (
+            <ZoomControlButton onClick={zoomControlFn} position='absolute' zooming={zooming} />
+          )}
+          {isMultiSeriesData && (
+            <DataSeriesControls
+              data={data}
+            />
+          )}
           <Plot
             forwardedRef={ref}
             parentHeight={parent.height}
             parentWidth={parent.width}
-            {...props}
+            {...newProps}
           />
         </>
       )}
