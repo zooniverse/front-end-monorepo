75b115614c0435b255713a7db1f655a65e793422
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-05-27 10:03:35 -0500

    check if a project slug is held back on PFE when forming its project nav links

--- packages/app-project/src/components/Announcements/components/FinishedAnnouncement/FinishedAnnouncementConnector.jsx
+++ packages/app-project/src/components/Announcements/components/FinishedAnnouncement/FinishedAnnouncementConnector.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'next-i18next'
 
 import NavLink from '@shared/components/NavLink'
 import GenericAnnouncement from '../GenericAnnouncement'
+import PFE_SLUGS from '../../../../helpers/slugList'
 
 function useStores() {
   const { store } = useContext(MobXProviderContext)
@@ -21,10 +22,17 @@ function FinishedAnnouncementConnector() {
   const { t } = useTranslation('components')
   const { baseUrl = '', hasResultsPage = true, isVisible = false } = useStores()
 
+  const slugArr = baseUrl.split('/')
+  const owner = slugArr?.[0]
+  const projectName = slugArr?.[1]
+
+  const isPFEProject = PFE_SLUGS.includes(`${owner}/${projectName}`)
+
   const announcement = t('Announcements.FinishedAnnouncement.announcement')
   const link = {
-    href: `${baseUrl}/about/results`,
-    text: t('Announcements.FinishedAnnouncement.seeResults')
+    href: isPFEProject ? `https://www.zooniverse.org/projects${baseUrl}/about/results` : `${baseUrl}/about/results`,
+    text: t('Announcements.FinishedAnnouncement.seeResults'),
+    externalLink: isPFEProject
   }
 
   if (isVisible) {
