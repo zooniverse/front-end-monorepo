6c7fba29faf53939c1a378ebffd6aec18f7b295e
Mark Bouslog <mcbouslog@gmail.com>
2026-06-09 11:58:45 -0500

    lib-react-components: Create SubjectCard component (#7449)
    * Create SubjectCard component
    
    * Add SubjectCard tests
    
    * Add SubjectCard translation
    
    * Refactor SubjectCard shareUrl
    
    * Add SubjectCard comments
    
    * Add window type check to subjectTalkUrl
    
    * Refactor SubjectCard square story image
    
    * Refactor SubjectCard per review, including improved z-index values

---
+++ packages/lib-react-components/src/SubjectCard/SubjectCard.spec.jsx
@@ -0,0 +1,22 @@
+import { composeStory } from '@storybook/react'
+import { render, screen } from '@testing-library/react'
+
+import Meta, { LoggedInLandscape } from './SubjectCard.stories'
+
+describe('SubjectCard', function() {
+  const DefaultStory = composeStory(LoggedInLandscape, Meta)
+
+  it('should link to the subject talk page', function () {
+    render(<DefaultStory />)
+
+    // grabbing the first element because each Story renders all three SubjectCard sizes
+    expect(screen.getAllByRole('link', { href: '/projects/team/example-project/talk/subjects/75219502' })[0]).toBeTruthy()
+  })
+
+  it('should show the subject ID', function () {
+    render(<DefaultStory />)
+
+    // grabbing the first element because each Story renders all three SubjectCard sizes
+    expect(screen.getAllByText('Subject 75219502')[0]).toBeTruthy()
+  })
+})
