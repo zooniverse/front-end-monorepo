ac15fc9c0fb159319df1f73f7a2cdd0dc8828e25
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-07-08 19:22:28 +0100

    lib-content: add Unsubscribe page

---
+++ packages/lib-content/src/screens/Unsubscribe/Unsubscribe.jsx
@@ -0,0 +1,39 @@
+'use client'
+// TODO: 👆 is this correct? Or should this be in the UnsubscribeForm subcomponent? 🤔
+// https://nextjs.org/docs/app/api-reference/directives/use-client
+
+import { Box } from 'grommet'
+import SpacedHeading from '@zooniverse/react-components/SpacedHeading'
+
+import { Trans, useTranslation } from '@translations/i18n'
+import ContainerBox from '@components/PageLayout/ContainerBox'
+import MaxWidthContent from '@components/MaxWidthContent/MaxWidthContent'
+import {
+  MobileHeading,
+  StyledHeading
+} from '@components/SharedStyledComponents/SharedStyledComponents'
+
+function Unsubscribe() {
+  const { t } = useTranslation()
+
+  return (
+    <ContainerBox>
+      <MobileHeading level={1} size='1.5rem'>
+        Unsubscribe
+      </MobileHeading>
+      <Box pad={{ horizontal: 'medium', bottom: 'large' }} align='center'>
+        <MaxWidthContent color={{ light: 'black', dark: 'white' }}>
+          <StyledHeading
+            color={{ light: 'neutral-1', dark: 'accent-1' }}
+            level={1}
+            size='small'
+          >
+            Unsubscribe
+          </StyledHeading>
+        </MaxWidthContent>
+      </Box>
+    </ContainerBox>
+  )
+}
+
+export default Unsubscribe
---
+++ packages/lib-content/src/screens/Unsubscribe/Unsubscribe.stories.jsx
@@ -0,0 +1,8 @@
+import Unsubscribe from './Unsubscribe'
+
+export default {
+  title: 'Other / Unsubscribe',
+  component: Unsubscribe
+}
+
+export const Default = {}
