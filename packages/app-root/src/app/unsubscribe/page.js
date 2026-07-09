307a94cc05ff5bc82ae8d865b29762c58c524378
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-07-10 00:03:34 +0100

    app-root: add /unsubscribe/ route

---
+++ packages/app-root/src/app/unsubscribe/page.js
@@ -0,0 +1,12 @@
+import { Unsubscribe } from '@zooniverse/content'
+
+export const metadata = {
+  title: 'Unsubscribe',
+  description: ''
+}
+
+export default function UnsubscribePage() {
+  return (
+    <Unsubscribe />
+  )
+}
