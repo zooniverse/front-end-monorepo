2d31c5c04366c676c129b5cef0837fda126be521
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-07-22 17:44:39 +0100

    StatusMessage: update tests

--- packages/lib-react-components/src/StatusMessage/StatusMessage.spec.jsx
+++ packages/lib-react-components/src/StatusMessage/StatusMessage.spec.jsx
@@ -6,9 +6,13 @@ import Meta, { Default } from './StatusMessage.stories'
 describe('Component > Status Message', function () {
   const DefaultStory = composeStory(Default, Meta)
 
-  it('should be accessible', function () {
-    render(<DefaultStory />)
-    expect(true).to.equal(true)
-    // TODO
+  it('should render the message and icon in an accessible manner', function () {
+    render(<DefaultStory text="Oh no something went wrong" type="error" />)
+
+    const icon = screen.getByLabelText('Error')
+    const text = screen.getByText('Oh no something went wrong')
+
+    expect(icon).to.exist
+    expect(text).to.exist
   })
 })
