80f43ccdedcd8d179f0c8a56bcedc5f92999e0aa
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-07-23 00:14:44 +0100

    UnsubscribePage: move ?processed=true check to unsubscribe route in app-root

--- packages/app-root/src/app/unsubscribe/page.js
+++ packages/app-root/src/app/unsubscribe/page.js
@@ -5,8 +5,18 @@ export const metadata = {
   description: ''
 }
 
-export default function UnsubscribePage() {
+export default async function UnsubscribePage (props) {
+
+  // Check if there's a ?processed=true in the URL's query string
+  // If yes, it means the user was redirected straight from the Panoptes
+  // /unsubscribe route.
+
+  const searchParams = await props.searchParams
+  const processed = searchParams?.processed === 'true'
+
   return (
-    <Unsubscribe />
+    <Unsubscribe
+      processed={processed}
+    />
   )
 }
--- packages/lib-content/src/screens/Unsubscribe/Unsubscribe.jsx
+++ packages/lib-content/src/screens/Unsubscribe/Unsubscribe.jsx
@@ -8,19 +8,9 @@ import OtherLayout from '@components/PageLayout/OtherLayout'
 import MaxWidthContent from '@components/MaxWidthContent/MaxWidthContent'
 import { bool } from 'prop-types'
 
-const isBrowser = typeof window !== 'undefined'
-
 function Unsubscribe ({
-  processed = false,  // Passed straight to the Unsubscribe Form. This component param is only used for debugging, as the page actually looks for the ?processed=true query param.
+  processed = false,   // If processed is true, it means user was sent here from the Panoptes /unsubscribe route
 }) {
-
-  // Check if there's a ?processed=true in the URL's query string
-  // If yes, it means the user was redirected straight from the Panoptes
-  // /unsubscribe route.
-  const searchQuery = (isBrowser) ? window?.location?.search : ''
-  const searchParams = new URLSearchParams(searchQuery)
-  const _processed = processed || searchParams.get('processed') === 'true'
-
   return (
     <OtherLayout>
       <Box
@@ -32,7 +22,7 @@ function Unsubscribe ({
           color={{ light: 'black', dark: 'white' }}
         >
           <UnsubscribeForm
-            processed={_processed}
+            processed={processed}
           />
         </MaxWidthContent>
       </Box>
