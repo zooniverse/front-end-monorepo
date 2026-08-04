f3461b3dc4d9e4e946847274378dd98e9cce6c9c
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-08-04 23:03:38 +0100

    doRequestPasswordReset: implement functionality. Return 0 on success.

--- packages/lib-content/src/screens/ResetPassword/helpers/doRequestPasswordReset.js
+++ packages/lib-content/src/screens/ResetPassword/helpers/doRequestPasswordReset.js
@@ -5,22 +5,29 @@ This function asks Panoptes to send a "password my reset" email, for a specified
 user account (email address).
 
 How this works:
-- TODO
+- Resetting an account's password occurs in four steps.
+- Step one, user submits their email address to Panoptes's Request Reset
+  Password service (via a form on our website).
+- Step two, Panoptes sends the user an email with a "reset password token".
+- Step three, user clicks on the link to our website with the token.
+- Step four, user submits their new password (alongside the token) to Panoptes
+  (via a form on our website).
+- This function addresses Step ONE.
 
 Input: 
 - `email`: string, a valid email address.
 
 Output:
-- Returns null on success.
-- Returns an Error object if something goes wrong.
+- Returns 0 on success.
+- Returns an Error object if something goes wrong during the API call.
  */
 
-// import auth from 'panoptes-client/lib/auth'
+import auth from 'panoptes-client/lib/auth'
 
 export default async function doRequestPasswordReset ({ email }) {
   try {
-    // TODO
-    return null
+    await auth.requestPasswordReset({ email })
+    return 0
   } catch (error) {
     console.error(error)
     return error
