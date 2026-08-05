ebda87aec1ba38c408400a5e900d0facf897137b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-08-05 23:56:16 +0100

    CommitResetForm: styling update, header time

--- packages/lib-content/src/screens/ResetPassword/components/CommitResetForm/CommitResetForm.jsx
+++ packages/lib-content/src/screens/ResetPassword/components/CommitResetForm/CommitResetForm.jsx
@@ -7,6 +7,12 @@ import { Loader, StatusMessage } from '@zooniverse/react-components'
 import doCommitPasswordReset from '../../helpers/doCommitPasswordReset'
 import DarkTealPrimaryButton from '../../../Unsubscribe/components/DarkTealPrimaryButton/DarkTealPrimaryButton'
 
+const DecoDivider = styled('div')`
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #A6A7A9 49.62%, rgba(255, 255, 255, 0) 100%);
+`
+
 const InputBoxes = styled(Box)`
   gap: 1em;
 `
@@ -76,9 +82,15 @@ function CommitResetForm ({
         className='CommitResetForm'
         onSubmit={onSubmit}
       >
-        <Heading level={1}>
+        <Heading
+          color={{ light: 'neutral-1', dark: 'accent-1' }}
+          level={1}
+          textAlign='center'
+        >
           {t('ResetPassword.common.header')}
         </Heading>
+
+        <DecoDivider />
 
         <Paragraph>
           {t('ResetPassword.CommitResetForm.body')}
