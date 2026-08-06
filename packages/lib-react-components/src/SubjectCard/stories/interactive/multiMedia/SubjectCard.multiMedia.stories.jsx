afb69965a35a0e1e8c41986a6f22ebd38c8863f6
Mark Bouslog <mcbouslog@gmail.com>
2026-08-06 14:09:41 -0500

    Add additional multi-media stories

--- packages/lib-react-components/src/SubjectCard/stories/interactive/multiMedia/SubjectCard.multiMedia.stories.jsx
+++ packages/lib-react-components/src/SubjectCard/stories/interactive/multiMedia/SubjectCard.multiMedia.stories.jsx
@@ -1,6 +1,8 @@
 import SubjectCard from '../../../SubjectCard'
 import {
   InteractiveStory,
+  CHIMP_AND_SEE_VIDEO_IMAGES_SUBJECT,
+  NFN_IMAGE_TEXT_SUBJECT,
   SMITHSONIAN_WILDLIFE_MULTI_IMAGE_SUBJECT,
   SQUIRREL_MAPPER_MULTI_IMAGE_SUBJECT
 } from '../../SubjectCardStoryData'
@@ -22,4 +24,12 @@ export const LoggedInMultiImage = {
 
 export const LoggedInMultiImageExternal = {
   render: () => <InteractiveStory login={'TestUser'} subject={SQUIRREL_MAPPER_MULTI_IMAGE_SUBJECT} />
+}
+
+export const LoggedInMultiVideoImages = {
+  render: () => <InteractiveStory login={'TestUser'} subject={CHIMP_AND_SEE_VIDEO_IMAGES_SUBJECT} />
+}
+
+export const LoggedInMultiImageText = {
+  render: () => <InteractiveStory login={'TestUser'} subject={NFN_IMAGE_TEXT_SUBJECT} />
 }
