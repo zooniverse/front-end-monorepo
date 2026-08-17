c24f25c7c470b0ded078d2b6d1fd9113dc90ad04
Mark Bouslog <mcbouslog@gmail.com>
2026-08-17 08:16:49 -0500

    Pass showAxes and showLegend props to Media

--- packages/lib-react-components/src/SubjectCard/components/InteractiveMedia/components/MultiMedia/MultiMedia.jsx
+++ packages/lib-react-components/src/SubjectCard/components/InteractiveMedia/components/MultiMedia/MultiMedia.jsx
@@ -1,5 +1,5 @@
 import { Box } from 'grommet'
-import { number, arrayOf, string, shape, objectOf } from 'prop-types'
+import { bool, number, arrayOf, string, shape, objectOf } from 'prop-types'
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
@@ -149,6 +149,8 @@ function MultiMedia({
 							alt={subjectIdTitle}
 							defaultMimeType='application'
 							height={previewHeight}
+							showAxes={true}
+							showLegend={true}
 							src={currentSource.url}
 							width={width}
 						/>
