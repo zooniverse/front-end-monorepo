b3eb0b491ec29ccdff9c7ee819eebc1f330a2e25
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2026-08-18 15:28:52 -0500

    app-root: Enable locale prefixes (#7548)
    * use next-i18n-router middleware to handle locale prefixes
    
    * pass locale to ZooFooter so components rerender with translations

--- packages/app-root/src/app/[locale]/layout.js
+++ packages/app-root/src/app/[locale]/layout.js
@@ -1,6 +1,9 @@
 import RootLayout from '@/components/RootLayout'
 import StyledComponentsRegistry from './style-registry'
 import { GoogleTagManager } from '@next/third-parties/google'
+import { notFound } from 'next/navigation'
+
+import i18nConfig from '../../../i18nConfig'
 
 export const metadata = {
   title: {
@@ -28,12 +31,19 @@ export const metadata = {
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-export default function NextLayout({ children }) {
+export default async function NextLayout(props) {
+  const { locale } = await props.params // pass this locale from the dynamic URL subpath to ZooFooter because that client component is on every webpage
+
+  if (!i18nConfig.locales.includes(locale)) {
+    notFound()
+  }
+  const sanitizedLocale = locale === 'test' ? 'en' : locale
+
   return (
-    <html lang='en'>
+    <html lang={sanitizedLocale}>
       {isProduction && <GoogleTagManager gtmId='GTM-WDW6V4' />}
       <StyledComponentsRegistry>
-        <RootLayout>{children}</RootLayout>
+        <RootLayout locale={locale}>{props.children}</RootLayout>
       </StyledComponentsRegistry>
     </html>
   )
--- packages/app-root/src/components/PageFooter.js
+++ packages/app-root/src/components/PageFooter.js
@@ -4,12 +4,13 @@ import { useContext } from 'react'
 
 import { PanoptesAuthContext } from '@/contexts'
 
-export default function PageFooter() {
+export default function PageFooter({ locale }) {
   const { adminMode, toggleAdmin, user } = useContext(PanoptesAuthContext)
 
   return (
     <ZooFooter
       adminContainer={user?.admin ? <AdminCheckbox onChange={toggleAdmin} checked={adminMode} /> : null}
+      locale={locale}
     />
   )
 }
