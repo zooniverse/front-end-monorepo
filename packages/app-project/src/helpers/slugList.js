647fe27687b976cd704f8839412f08998c9ee623
Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
2026-08-19 12:50:46 -0500

    migrate md68135/diode to PFE (#7554)

--- packages/app-project/src/helpers/slugList.js
+++ packages/app-project/src/helpers/slugList.js
@@ -49,6 +49,7 @@ const PFE_SLUGS = [
   'lcjohnso/lcj-pfe-project', // LCJ PFE Reference Test Project
   'low-sky/bubblezoo-v1', // Oval drawing tool is a worse experience in FEM
   'mappingprejudice/mapping-prejudice',
+  'md68135/diode', // Dependent dropdowns aren't supported in FEM
   'md68135/notes-from-nature-big-bee-bonanza',
   'md68135/notes-from-nature-calbug', // Dependent dropdowns aren't supported in FEM
   'md68135/notes-from-nature-capturing-californias-flowers', // Dependent dropdowns aren't supported in FEM
