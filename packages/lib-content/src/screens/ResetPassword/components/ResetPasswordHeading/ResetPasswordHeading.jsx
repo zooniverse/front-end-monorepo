5fc5dc9690f74f90aa0aaa6c249f1f31695e34e4
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2026-08-20 22:13:01 +0100

    Reset Password Page pt1: add route, page, and forms (#7529)
    * ResetPassword page: add folder, add README
    
    * UnsubscribePage: fix typo in README
    
    * ResetPassword page: add page skeleton
    
    * app-root: add /reset-password route
    
    * /reset-password route: add checks for user login
    
    * ResetPasswordContainer: make Loader look a wee bit more presentable
    
    * ResetPassword: expand Stories
    
    * ResetPassword: add RequestResetForm
    
    * ResetPassword: expand RequestResetForm. Add translation keys
    
    * doRequestPasswordReset: implement functionality. Return 0 on success.
    
    * doUnsubscribe: add TODO to make helper functions return similar results
    
    * RequestResetForm: disable form on success
    
    * RequestResetForm: add error message for 429 Too Many Requests
    
    * RequestResetForm: improve text
    
    * ResetPassword: clean up locale file
    
    * doRequestPasswordReset: fix typo. derp!
    
    * CommitResetForm: add functional form
    
    * doCommitPasswordReset: fix actual API call
    
    * CommitResetForm: tweak error handling
    
    * ResetPassword: add NoAccessMessage component
    
    * CommitResetForm: styling update
    
    * CommitResetForm: styling update, header time
    
    * RequestResetForm: styling update
    
    * ResetPassword: consolidate ResetPasswordHeader
    
    * CommitResetForm: add check for matching passwords
    
    * CommitResetForm: implement minimum password length
    
    * ResetPassword: cleanup
    
    * CommitResetForm: update styling, move isNewPasswordValid() to own helper file
    
    * ResetPassword: change loggedin logic. Update docs.
    
    * ResetPassword: implement tests
    
    * CommitResetForm: fix missing success message
    
    * CommitResetForm, RequestResetForm: update styling for loader
    
    * RequestResetForm: update styling for invalid input
    
    * ResetPassword: feedback updates
    
    * CommitResetForm: add propTypes
    
    * Update packages/app-root/src/app/reset-password/page.js
    
    Fix typo in comments
    
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
    
    * Update packages/lib-content/src/screens/ResetPassword/components/ResetPasswordHeader/ResetPasswordHeader.jsx
    
    Code: improve CSS readability
    
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>
    
    * Rename ResetPasswordHeader to ResetPasswordHeading
    
    * CommitResetForm: refactor HelpInfo to use Grommet Text
    
    * CommitResetForm: update submit checks. isNewPasswordValid() simplified
    
    * RequestResetForm: add visually hidden label
    
    * app-root: move reset-password route into [locale] folder
    
    * app-root: fix reset-password imports
    
    * app-root: fix AGAIN reset-password imports
    
    ---------
    
    Co-authored-by: Delilah C. <23665803+goplayoutside3@users.noreply.github.com>

---
+++ packages/lib-content/src/screens/ResetPassword/components/ResetPasswordHeading/ResetPasswordHeading.jsx
@@ -0,0 +1,26 @@
+import { Heading } from 'grommet'
+import styled from 'styled-components'
+import { useTranslation } from '@translations/i18n'
+
+const DecoDivider = styled('div')`
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, #ffffff 0%, #A6A7A9 50%, #ffffff 100%);
+`
+
+export default function ResetPasswordHeading () {
+  const { t } = useTranslation()
+  return (
+    <>
+        <Heading
+          color={{ light: 'neutral-1', dark: 'accent-1' }}
+          level={1}
+          textAlign='center'
+        >
+          {t('ResetPassword.common.header')}
+        </Heading>
+        <DecoDivider />
+    </>
+  )
+
+}
