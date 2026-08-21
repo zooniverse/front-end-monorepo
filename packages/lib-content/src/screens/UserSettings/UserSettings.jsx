631cf87b337403a9dd012e2e6747ab91dee3d2a1
Delilah <23665803+goplayoutside3@users.noreply.github.com>
2026-08-21 16:37:02 -0500

    delete stray login prop

--- packages/lib-content/src/screens/UserSettings/UserSettings.jsx
+++ packages/lib-content/src/screens/UserSettings/UserSettings.jsx
@@ -5,11 +5,12 @@ import { Box } from 'grommet'
 import FormWithTextField from './components/FormWithTextField'
 import FormWithCheckbox from './components/FormWithCheckbox'
 
-function Settings({ authUser, login }) {
+function Settings({ authUser }) {
   // The page in app-root handled the check for if someone is authenticated and
   // got the basic information about who it is. However, we need to fetch more
   // info about this user in order to display all the details of their user
-  // settings in this UI.
+  // settings in this UI. `authUser` is the basic info, and more is fetched in
+  // each form component.
 
   return (
     <Box gap='large' margin='xlarge'>
