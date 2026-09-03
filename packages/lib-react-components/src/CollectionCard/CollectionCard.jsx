2896e9ebb11f24ac58297856821ecb242d62f2a7
Mark Bouslog <mcbouslog@gmail.com>
2026-09-03 13:35:11 -0500

    Add CollectionCard component and stories

---
+++ packages/lib-react-components/src/CollectionCard/CollectionCard.jsx
@@ -0,0 +1,150 @@
+import { Box, Text } from 'grommet'
+import { Group, Lock } from 'grommet-icons'
+import { object, string } from 'prop-types'
+import styled from 'styled-components'
+
+const StyledCollectionCard = styled(Box)`
+  overflow: hidden;
+  position: relative;
+  text-decoration: none;
+`
+
+const CollectionDetails = styled(Box)`
+  bottom: 0;
+  height: 45px;
+  position: absolute;
+  transition: height 0.5s ease;
+  width: 100%;
+  z-index: 1;
+
+  ${StyledCollectionCard}:hover &,
+  ${StyledCollectionCard}:focus & {
+    height: 115px;
+  }
+`
+
+const CollectionDescription = styled(Text)`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  height: 0;
+  overflow: hidden;
+
+  ${StyledCollectionCard}:hover &,
+  ${StyledCollectionCard}:focus & {
+    height: auto;
+    margin: 10px;
+  }
+`
+
+const Header = styled(Box)`
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7), transparent);
+`
+
+const Badge = styled(Text)`
+  backdrop-filter: blur(2px);
+  background: rgba(64, 64, 64, 0.6);
+  border-radius: 60px;
+  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, 0.25);
+  height: fit-content;
+  max-width: 80%;
+  padding: 6px 5px;
+`
+
+function CollectionCard({ collection, userId }) {
+  const {
+    default_subject_src: imageSrc,
+    description,
+    display_name: displayName,
+    links,
+    private: isPrivate,
+    slug
+  } = collection
+  const owner = links?.owner
+  const subjectCount = links?.subjects?.length || 0
+  const hasCollaborators = links?.collection_roles?.length > 1
+  const href = `https://www.zooniverse.org/collections/${slug}`
+
+  return (
+    <StyledCollectionCard
+      a11yTitle={`${displayName}, ${owner?.display_name}, ${subjectCount} subjects`}
+      elevation='small'
+      flex={false}
+      forwardedAs='a'
+      href={href}
+      height='245px'
+      round='8px'
+      width='280px'
+    >
+      <Box
+        background={imageSrc
+          ? { image: `url(${imageSrc})`, position: 'center', size: 'cover' }
+          : { dark: 'dark-3', light: 'light-3' }}
+        flex={false}
+        height='200px'
+        round={{ corner: 'top', size: '8px' }}
+      >
+        <Header
+          direction='row'
+          height='45px'
+          justify='between'
+          pad={{ horizontal: 'xsmall', vertical: 'xsmall' }}
+        >
+          <Badge
+            color='white'
+            size='0.75rem'
+            textAlign='center'
+          >
+            {owner?.display_name}
+          </Badge>
+          <Badge color='white' size='0.75rem'>
+            {subjectCount}
+          </Badge>
+        </Header>
+      </Box>
+      <CollectionDetails
+        align='center'
+        background={{ dark: 'dark-3', light: 'white' }}
+        flex={false}
+        justify='center'
+        round={{ corner: 'bottom', size: '8px' }}
+      >
+        <Box
+          align='center'
+          direction='row'
+          fill='horizontal'
+          height={{ max: '45px' }}
+          justify='between'
+          pad='xsmall'
+        >
+          {isPrivate ? <Lock aria-label='Private collection' size='12px' /> : <span />}
+          <Text
+            color={{ dark: 'neutral-6', light: 'dark-5' }}
+            size='0.875rem'
+            textAlign='center'
+            weight='bold'
+          >
+            {displayName}
+          </Text>
+          {hasCollaborators ? <Group aria-label='Collection has collaborators' size='12px' /> : null}
+        </Box>
+        {description ? (
+          <CollectionDescription
+            color={{ dark: 'neutral-6', light: 'dark-5' }}
+            size='0.875rem'
+            textAlign='center'
+          >
+            {description}
+          </CollectionDescription>
+        ) : null}
+      </CollectionDetails>
+    </StyledCollectionCard>
+  )
+}
+
+CollectionCard.propTypes = {
+  collection: object.isRequired,
+  userId: string
+}
+
+export default CollectionCard
---
+++ packages/lib-react-components/src/CollectionCard/CollectionCard.stories.jsx
@@ -0,0 +1,62 @@
+import { Box } from 'grommet'
+
+import CollectionCard from './CollectionCard'
+
+export default {
+  title: 'Components / CollectionCard',
+  component: CollectionCard,
+  decorators: [(Story) => <Box pad="medium"><Story /></Box>],
+}
+
+const collection = {
+  id: '574275',
+  display_name: 'Best of the Best',
+  slug: 'testuser/best-of-the-best',
+  default_subject_src: 'https://panoptes-uploads.zooniverse.org/subject_location/c6d3dd9b-4285-4ddb-90ae-08f9558dcae9.jpeg',
+  description: `Only the very best subjects I've found in this project. Cool!!`,
+  private: true,
+  links: {
+    owner: {
+      id: '1234',
+      display_name: 'Test User'
+    },
+    collection_roles: ['736999', '755009'],
+    subjects: [
+      '121787506', '120513648', '70200265', '118123956', '113453873', '96755809', '83746800',
+      '115968754', '119223066', '75219502', '70429104', '86525036', '78454341', '119560411'
+    ]
+  }
+}
+
+const collectionWithLongStrings = {
+  id: '574275',
+  display_name: 'Best of the Best of the Best of the Best of the Best',
+  slug: 'testuser/best-of-the-best',
+  default_subject_src: 'https://panoptes-uploads.zooniverse.org/subject_location/2ffdc5bd-c039-4596-b407-af231c00da5a.png',
+  description: `Only the very best subjects I've found in this project. Only the very best subjects I've found in this project. Only the very best subjects I've found in this project. Cool!!`,
+  private: true,
+  links: {
+    owner: {
+      id: '1234',
+      display_name: 'Test User Display Name Is This Long String'
+    },
+    collection_roles: ['736999', '755009'],
+    subjects: [
+      '121787506', '120513648', '70200265', '118123956', '113453873', '96755809', '83746800',
+      '115968754', '119223066', '75219502', '70429104', '86525036', '78454341', '119560411'
+    ]
+  }
+}
+
+export const Default = {
+  args: {
+    collection,
+    userId: '1234'
+  }
+}
+export const WithLongStrings = {
+  args: {
+    collection: collectionWithLongStrings,
+    userId: '1234'
+  }
+}
