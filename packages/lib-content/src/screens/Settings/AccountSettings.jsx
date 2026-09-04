c343a534779d2f915dc5e6b7744e97deda6982de
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-09-04 21:34:48 +0100

    AccountSettings: add common side/dropdown nav

--- packages/lib-content/src/screens/Settings/AccountSettings.jsx
+++ packages/lib-content/src/screens/Settings/AccountSettings.jsx
@@ -1,23 +1,77 @@
 'use client'
 
+import { Box, Heading } from 'grommet'
 import SettingsLayout from '@components/PageLayout/SettingsLayout'
 import MaxWidthContent from '@components/MaxWidthContent/MaxWidthContent'
-import { arrayOf, shape, string } from 'prop-types'
+import { shape, string } from 'prop-types'
+import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+
+import {
+  mobileBreakpoint,
+  StickyBox,
+  StickySidebar,
+  StyledGrid
+} from '@components/SharedStyledComponents/SharedStyledComponents'
+import DropdownNav from '@components/DropdownNav/DropdownNav'
+
+const StyledAboutHeading = styled(Heading)`
+  position: relative;
+  padding: 44px 0;
+  margin: 0;
+  text-align: center;
+
+  @media (width <= ${mobileBreakpoint}) {
+    display: none;
+  }
+`
 
 function AccountSettings ({
   user
 }) {
   if (!user) return null
 
+  const { t } = useTranslation()
+
+  const activeSection = 0
+  const sidebarSections = [
+    { name: t('Settings.AccountSettings.title'), slug: 'TODO1' },
+    { name: t('Settings.ProfileSettings.title'), slug: 'TODO2' },
+    { name: t('Settings.EmailSettings.title'), slug: 'TODO3' }
+  ]
+
   return (
     <SettingsLayout>
-      <MaxWidthContent
-        className='Account-Settings-Page'
-        color={{ light: 'black', dark: 'white' }}
-      >
-        <h1>Account Settings Placeholder</h1>
-        <p>User is {user.login} aka {user.display_name}</p>
-      </MaxWidthContent>
+      <StickyBox background={{ dark: 'dark-3', light: 'neutral-6' }}>
+        <DropdownNav
+          activeSection={activeSection}
+          sidebarLabel={t('AboutPage.sidebarLabel')}
+          sections={sidebarSections}
+          // setActiveSection={setActiveSection}
+        />
+      </StickyBox>
+
+      <StyledGrid>
+        <Box as='aside' align='center'>
+          <StickySidebar
+            activeSection={activeSection}
+            ariaLabel={t('AboutPage.sidebarLabel')}
+            sections={sidebarSections}
+            //setActiveSection={setActiveSection}
+          />
+        </Box>
+
+        <MaxWidthContent
+          className='Account-Settings-Page'
+          color={{ light: 'black', dark: 'white' }}
+          background='#f0e0e0'
+        >
+          <h1>Account Settings Placeholder</h1>
+          <p>User is {user.login} aka {user.display_name}</p>
+        </MaxWidthContent>
+      </StyledGrid>
+
+
     </SettingsLayout>
   )
 }
