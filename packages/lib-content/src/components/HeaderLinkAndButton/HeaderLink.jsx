e8cea9b4237d7419aa8aa66012ed9af5ad18a77e
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2026-09-15 23:44:23 +0100

    User Settings Page pt1: Shared Layout and Base Section (#7572)
    * app-root: add /settings paths
    
    * PageLayout: update README with more specs on Other Layout, Settings Layout
    
    * OtherLayout (ResetPassword, Unsubscribe pages): improve visual layout, follow Figma design better
    
    * lib-content: add folder for Settings component, add README
    
    * app-root: add UserSettingsContainer for /settings path
    
    * Settings: add AccountSettings subpage
    
    * docs: fix specifications of OtherLayout's header
    
    * lib-content: add SettingsLayout
    
    * SettingsLayout: add translations
    
    * AccountSettings: minor cleanup
    
    * AccountSettings: add common side/dropdown nav
    
    * AccountSettings: fix sidebar label
    
    * DropdownNav, Sidebar: allow sections[] array to have slug OR href
    
    * Change OtherLayout -> ContentLayout, SettingsLayout -> FormLayout. Update PageLayouts README
    
    * fix: whoops AccountSettings was supposed to use FormLayout (nee SettingsLayout)
    
    * FormLayout: use HeaderLink for back home link. Copy HeaderLink and HeaderButton from lib-user
    
    * DropdownNav: if section uses href, render it in Links
    
    * Sidebar: if section uses href, render it in Links

---
+++ packages/lib-content/src/components/HeaderLinkAndButton/HeaderLink.jsx
@@ -0,0 +1,28 @@
+import { Previous } from 'grommet-icons'
+import { string } from 'prop-types'
+
+import HeaderButton from './HeaderButton'
+import Link from 'next/link'
+
+function HeaderLink({
+  href,
+  label,
+  ...rest
+}) {
+  return (
+    <HeaderButton
+      forwardedAs={Link}
+      href={href}
+      icon={<Previous color='white' size='small' />}
+      label={label}
+      {...rest}
+    />
+  )
+}
+
+HeaderLink.propTypes = {
+  href: string.isRequired,
+  label: string.isRequired
+}
+
+export default HeaderLink
