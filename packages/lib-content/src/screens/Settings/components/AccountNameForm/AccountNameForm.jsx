30d5a0b5b1dd90f5e7805b9116583e9aa126c7ed
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2026-09-19 02:46:11 +0100

    AccountNameForm: completely overhaul data saving/updating. Update docs. useUserData: don't revalidate on focus.

--- packages/lib-content/src/screens/Settings/components/AccountNameForm/AccountNameForm.jsx
+++ packages/lib-content/src/screens/Settings/components/AccountNameForm/AccountNameForm.jsx
@@ -1,8 +1,11 @@
-import { useId, useRef } from 'react'
+import { useId, useRef, useState } from 'react'
 import { Box, Form, Text, TextInput } from 'grommet'
 import { Loader } from '@zooniverse/react-components'
 import styled from 'styled-components'
 import DarkTealPrimaryButton from '../../../Unsubscribe/components/DarkTealPrimaryButton/DarkTealPrimaryButton'
+
+import useUserData from '../../helpers/useUserData'
+import updateUserData from '../../helpers/updateUserData'
 
 const FormFieldsContainer = styled(Box)`
   gap: 1em;
@@ -25,62 +28,82 @@ const BigLabel = styled(Text)`
 async function DEFAULT_FUNCTION () {}
 
 export default function AccountNameForm ({
-  user,
-  mutateUser = DEFAULT_FUNCTION,
-  updateUserData = DEFAULT_FUNCTION,
   authUser,
-  isLoading = false,
-  isValidating = false,
 }) {
+  const { data: user, isLoading, error, isValidating, mutate } = useUserData({ login: authUser.login })
+  const [ isSaving, setIsSaving ] = useState(false)
+  const [ saveSuccess, setSaveSuccess ] = useState(false)
+  const [ saveError, setSaveError ] = useState(null)
+  const [ hasUnsavedChanges, setHasUnsavedChanges ] = useState(false)
+
   const displayNameInputId = useId()
   const displayNameInputRef = useRef()
 
   if (!user) return null
 
+  // On input change, update the LOCAL data copy.
+  // Changes aren't saved to Panoptes until updateUserData() is triggered.
   function onInputChange (e) {
     const value = e.target.value
     const field = e.target.dataset.field
 
-    // This mutates the data returned from useUserData(). Because the UI component's TextInput
-    // is "hooked in" to data returned from useUserData() as "state", this pattern is a replacement
-    // for a [value, setValue] = useState() pattern.
-    // No network requests until the user clicks "Save".
-    mutateUser(prevData => ({
+    mutate(prevData => ({
       ...prevData,
       [field]: value
-    }), { revalidate: false })  // Don't revalidate, the returned object is the new "state"
+    }), {
+      // DON'T revalidate. (i.e. don't overwrite local changes with data from Panoptes)
+      revalidate: false
+    })  
+
+    setHasUnsavedChanges(true)
   }
 
-  function onSubmit () {
+  async function onSubmit () {
 
     // TODO: validate input.
     const displayName = displayNameInputRef.current.value
 
-    mutateUser(
-      async prevData => {
-        await updateUserData({
-          ['display_name']: displayName
-        }, authUser.id)
-        // this async function must return something for the cache
-        return prevData // already updated by onInputChange mutate
-      },
-      {
-        optimisticData: prevData => {
-          return prevData // already updated by onInputChange mutate
-        },
-        // don't need to revalidate because optimisticData updates {data: user} returned
-        // from useUserData().
-        revalidate: false,
-        // rollbackOnError responds to an error thrown by updateCreditedName(). So make sure to throw in that helper function.
-        rollbackOnError(err) {
-          console.error(err)
-          return true
+    try {
+      let shouldRevalidate = false
+
+      await mutate(
+        async (prevData) => {
+
+          setIsSaving(true)
+          setSaveSuccess(false)
+          setSaveError(null)
+
+          await updateUserData({
+            ['display_name']: displayName
+          }, authUser.id)
+
+          setIsSaving(false)
+          setSaveSuccess(true)
+          setSaveError(null)
+          shouldRevalidate = true
+
+          return prevData  // Return existing data with no changes.
+        }, {
+          // DO revalidate, IF updateUserData() is successful. (i.e. please sync local data with Panoptes) 
+          revalidate: () => shouldRevalidate,
+
+          // Optimitic data isn't required here since we're using onInputChange() to make local changes.
+          // optimisticData: prevData => prevData,
+
+          // Rollback isn't required here because this mutate doesn't make any changes.
+          // rollbackOnError: (err) => { return true }
         }
-      }
-    )
+      )
+
+    } catch (err) {
+      console.error(err)
+      setIsSaving(false)
+      setSaveSuccess(false)
+      setSaveError(err?.response?.body || err)
+    }
   }
 
-  const disableInput = isLoading || isValidating
+  const disableInput = isLoading || isValidating || isSaving
 
   return (
     <Form
@@ -109,19 +132,23 @@ export default function AccountNameForm ({
           <Text>Extra information</Text>
         </InputField>
 
-        <Box>
-          {(isLoading || isValidating) && <Loader />}
-          <DarkTealPrimaryButton
-            type='submit'
-            disabled={disableInput}
-          >
-            Save
-          </DarkTealPrimaryButton>
-          <DarkTealPrimaryButton
-            disabled={disableInput}
-            label={'TODO: SAVE'}
-            type='submit'
-          />
+        <Box
+          direction='row'
+        >
+          {hasUnsavedChanges ? (
+            <DarkTealPrimaryButton
+              disabled={disableInput}
+              label={'TODO: SAVE'}
+              type='submit'
+            /> ) : (
+            <Text>No unsaved changes</Text>
+          )}
+
+          {(isLoading || isValidating || isSaving) && <Loader />}
+
+          {saveSuccess && '✅'}
+
+          {saveError?.errors?.[0]?.message || saveError?.toString() || (saveError && 'Unknown error')}
         </Box>
       </FormFieldsContainer>
     </Form>
