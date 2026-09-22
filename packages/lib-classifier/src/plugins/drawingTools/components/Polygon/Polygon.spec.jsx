4b5b520120280a2eb978d2032cc10331269acbdd
sktw <1399365+sktw@users.noreply.github.com>
2026-09-22 21:07:50 +0100

    Refactor Polygon to use local coordinates
    - Refactor Polygon model and component to use local coordinates
    - Add basic rendering test

--- packages/lib-classifier/src/plugins/drawingTools/components/Polygon/Polygon.spec.jsx
+++ packages/lib-classifier/src/plugins/drawingTools/components/Polygon/Polygon.spec.jsx
@@ -1,3 +1,31 @@
+import { render } from '@testing-library/react'
+import Polygon from './Polygon'
+
+describe('Drawing tools > Polygon', () => {
+  it('should render active finished mark without crashing', function () {
+    const mark = {
+      path: '10,20 90,10 45,60',
+      points: [
+        { x: 10, y: 20 },
+        { x: 90, y: 10 },
+        { x: 45, y: 60 }
+      ],
+      initialPoint: { x: 10, y: 20 },
+      lastPoint: { x: 45, y: 60 },
+      finished: true
+    }
+
+    render(
+      <svg xmlns='http://www.w3.org/2000/svg'>
+        <Polygon active mark={mark} />
+      </svg>
+    )
+    
+    const polygon = document.querySelector('g')
+    expect(polygon).to.exist
+  })
+})
+
 // Old enzyme tests are here for reference, but enzyme is no longer used in this library (Aug’ 25)
 
 describe.skip('Drawing Tools > Polygon tool', function () {})
